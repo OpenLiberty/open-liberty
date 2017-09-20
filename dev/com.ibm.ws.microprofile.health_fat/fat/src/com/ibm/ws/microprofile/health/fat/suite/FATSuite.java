6fd4a23aae40409e5087dfac347235258b30722a
Andy Mauer <ajmauer@us.ibm.com>
2017-09-20 11:31:41 -0400

    next major drop of microprofile health FAT

--- dev/com.ibm.ws.microprofile.health_fat/fat/src/com/ibm/ws/microprofile/health/fat/suite/FATSuite.java
+++ dev/com.ibm.ws.microprofile.health_fat/fat/src/com/ibm/ws/microprofile/health/fat/suite/FATSuite.java
@@ -1,25 +1,38 @@
 package com.ibm.ws.microprofile.health.fat.suite;
 
-/*******************************************************************************
- * Copyright (c) 2017 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.ibm.ws.microprofile.health.fat.CDIHealthCheckTest;
+import com.ibm.ws.microprofile.health.fat.HealthCheckExceptionTest;
 import com.ibm.ws.microprofile.health.fat.HealthTest;
+import com.ibm.ws.microprofile.health.fat.MultipleChecksTest;
+import com.ibm.ws.microprofile.health.fat.NoHealthCheckAPIImplTest;
+import com.ibm.ws.microprofile.health.fat.NoHealthCheckAnnotationTest;
+
+/*
+ * IBM Confidential
+ *
+ * OCO Source Materials
+ *
+ * Copyright IBM Corp. 2017
+ *
+ * The source code for this program is not published or other-
+ * wise divested of its trade secrets, irrespective of what has
+ * been deposited with the U.S. Copyright Office.
+ */
 
 @RunWith(Suite.class)
 @SuiteClasses({
-                HealthTest.class
+                HealthTest.class,
+                CDIHealthCheckTest.class,
+                MultipleChecksTest.class,
+                NoHealthCheckAPIImplTest.class,
+                NoHealthCheckAnnotationTest.class,
+                HealthCheckExceptionTest.class
 })
 
-public class FATSuite {}
+public class FATSuite {
+
+}
---
+++ dev/com.ibm.ws.microprofile.health_fat/test-applications/HealthCheckExceptionApp/src/com/ibm/ws/microprofile/health/exception/testapp/HealthCheckExceptionAppFAT.java
@@ -0,0 +1,29 @@
+package com.ibm.ws.microprofile.health.exception.testapp;
+
+/*
+* IBM Confidential
+*
+* OCO Source Materials
+*
+* WLP Copyright IBM Corp. 2017
+*
+* The source code for this program is not published or otherwise divested
+* of its trade secrets, irrespective of what has been deposited with the
+* U.S. Copyright Office.
+*/
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.health.Health;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+
+@Health
+@ApplicationScoped
+public class HealthCheckExceptionAppFAT implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        throw new RuntimeException();
+    }
+
+}
