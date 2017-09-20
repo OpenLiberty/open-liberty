2a0b22c1e1db47cbef15af0a928717b92ec9141a
Joseph Zoght <zoght@us.ibm.com>
2017-09-20 17:02:44 -0500

    Port of context_fat to open liberty

--- dev/com.ibm.ws.context_fat/fat/src/com/ibm/ws/context/fat/ContextServiceFATTest.java
+++ dev/com.ibm.ws.context_fat/fat/src/com/ibm/ws/context/fat/ContextServiceFATTest.java
@@ -10,17 +10,6 @@
  *******************************************************************************/
 package com.ibm.ws.context.fat;
 
-/*
- * IBM Confidential
- *
- * OCO Source Materials
- *
- * Copyright IBM Corp. 2012, 2014
- *
- * The source code for this program is not published or other-
- * wise divested of its trade secrets, irrespective of what has
- * been deposited with the U.S. Copyright Office.
- */
 import static org.junit.Assert.fail;
 
 import java.io.BufferedReader;
