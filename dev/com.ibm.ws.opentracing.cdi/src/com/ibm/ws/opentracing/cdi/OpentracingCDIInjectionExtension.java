51d0ce0154866d670d398fdc74cb9fbe67e2ce4d
Thomas Bitonti <bitonti@us.ibm.com>
2017-09-21 18:35:13 -0400

    Initial drop of com.ibm.ws.opentracing_FAT

--- dev/com.ibm.ws.opentracing.cdi/src/com/ibm/ws/opentracing/cdi/OpentracingCDIInjectionExtension.java
+++ dev/com.ibm.ws.opentracing.cdi/src/com/ibm/ws/opentracing/cdi/OpentracingCDIInjectionExtension.java
@@ -19,14 +19,27 @@ import javax.enterprise.inject.spi.Extension;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
 
+import com.ibm.websphere.ras.Tr;
+import com.ibm.websphere.ras.TraceComponent;
+import com.ibm.websphere.ras.annotation.Trivial;
 import com.ibm.ws.cdi.extension.WebSphereCDIExtension;
 
 @Component(service = WebSphereCDIExtension.class, configurationPolicy = ConfigurationPolicy.IGNORE)
 public class OpentracingCDIInjectionExtension implements WebSphereCDIExtension, Extension {
+    private static final TraceComponent tc = Tr.register(OpentracingProducerBean.class);
 
+    @Trivial
     void beforeBeanDiscovery(@Observes BeforeBeanDiscovery bbd, BeanManager bm) {
+        String methodName = "beforeBeanDiscover";
+        if ( TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled() ) {
+            Tr.entry(tc, methodName);
+        }
+
         AnnotatedType<OpentracingProducerBean> at = bm.createAnnotatedType(OpentracingProducerBean.class);
         bbd.addAnnotatedType(at);
-    }
 
+        if ( TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled() ) {
+            Tr.exit(tc, methodName);
+        }
+    }
 }
