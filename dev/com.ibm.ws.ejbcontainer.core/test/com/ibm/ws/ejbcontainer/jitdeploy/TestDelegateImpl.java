453e787811281717fafaaf1702532313afafb8ff
Jared Anderson <jhanders@us.ibm.com>
2017-09-21 17:08:16 -0500

    Update .classpath, .project, and bnd.bnd files to get eclipse to build

--- dev/com.ibm.ws.ejbcontainer.core/test/com/ibm/ws/ejbcontainer/jitdeploy/TestDelegateImpl.java
+++ dev/com.ibm.ws.ejbcontainer.core/test/com/ibm/ws/ejbcontainer/jitdeploy/TestDelegateImpl.java
@@ -10,11 +10,10 @@
  *******************************************************************************/
 package com.ibm.ws.ejbcontainer.jitdeploy;
 
-import junit.framework.Assert;
-
 import org.omg.CORBA.Context;
 import org.omg.CORBA.ContextList;
 import org.omg.CORBA.ExceptionList;
+import org.omg.CORBA.InterfaceDef;
 import org.omg.CORBA.NVList;
 import org.omg.CORBA.NamedValue;
 import org.omg.CORBA.Request;
@@ -23,81 +22,68 @@ import org.omg.CORBA.portable.Delegate;
 import org.omg.CORBA.portable.InputStream;
 import org.omg.CORBA.portable.OutputStream;
 
-public class TestDelegateImpl
-                extends Delegate
-{
+import junit.framework.Assert;
+
+public class TestDelegateImpl extends Delegate {
     TestMethodCalls ivCalls;
 
-    TestDelegateImpl(TestMethodCalls calls)
-    {
+    TestDelegateImpl(TestMethodCalls calls) {
         ivCalls = calls;
     }
 
     @Override
-    public org.omg.CORBA.Object get_interface_def(org.omg.CORBA.Object self)
-    {
+    public org.omg.CORBA.Object get_interface_def(org.omg.CORBA.Object self) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public org.omg.CORBA.Object duplicate(org.omg.CORBA.Object obj)
-    {
+    public org.omg.CORBA.Object duplicate(org.omg.CORBA.Object obj) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public void release(org.omg.CORBA.Object obj)
-    {
+    public void release(org.omg.CORBA.Object obj) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public boolean is_a(org.omg.CORBA.Object obj, String repository_id)
-    {
+    public boolean is_a(org.omg.CORBA.Object obj, String repository_id) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public boolean non_existent(org.omg.CORBA.Object obj)
-    {
+    public boolean non_existent(org.omg.CORBA.Object obj) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public boolean is_equivalent(org.omg.CORBA.Object obj, org.omg.CORBA.Object other)
-    {
+    public boolean is_equivalent(org.omg.CORBA.Object obj, org.omg.CORBA.Object other) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public int hash(org.omg.CORBA.Object obj, int max)
-    {
+    public int hash(org.omg.CORBA.Object obj, int max) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public Request request(org.omg.CORBA.Object obj, String operation)
-    {
+    public Request request(org.omg.CORBA.Object obj, String operation) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public Request create_request(org.omg.CORBA.Object obj, Context ctx, String operation, NVList arg_list, NamedValue result)
-    {
+    public Request create_request(org.omg.CORBA.Object obj, Context ctx, String operation, NVList arg_list, NamedValue result) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public Request create_request(org.omg.CORBA.Object obj, Context ctx, String operation, NVList arg_list, NamedValue result, ExceptionList exclist, ContextList ctxlist)
-    {
+    public Request create_request(org.omg.CORBA.Object obj, Context ctx, String operation, NVList arg_list, NamedValue result, ExceptionList exclist, ContextList ctxlist) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public OutputStream request(org.omg.CORBA.Object self, String operation, boolean responseExpected)
-    {
-        if (ivCalls.ivOperation != null)
-        {
+    public OutputStream request(org.omg.CORBA.Object self, String operation, boolean responseExpected) {
+        if (ivCalls.ivOperation != null) {
             Assert.assertEquals(ivCalls.ivOperation, operation);
         }
         Assert.assertTrue(responseExpected);
@@ -107,11 +93,8 @@ public class TestDelegateImpl
 
     @SuppressWarnings("resource")
     @Override
-    public InputStream invoke(org.omg.CORBA.Object self, OutputStream output)
-                    throws ApplicationException
-    {
-        if (ivCalls.ivApplicationException)
-        {
+    public InputStream invoke(org.omg.CORBA.Object self, OutputStream output) throws ApplicationException {
+        if (ivCalls.ivApplicationException) {
             throw new ApplicationException(null, new TestInputStreamImpl(ivCalls));
         }
 
@@ -120,4 +103,8 @@ public class TestDelegateImpl
 
     @Override
     public void releaseReply(org.omg.CORBA.Object self, InputStream input) { /* empty */}
+
+    public InterfaceDef get_interface(org.omg.CORBA.Object arg0) {
+        throw new UnsupportedOperationException();
+    }
 }
