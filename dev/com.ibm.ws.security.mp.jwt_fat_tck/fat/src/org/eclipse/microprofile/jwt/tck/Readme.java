6eff386efd0e3cdf4b5928ec2d0b3a539c939693
Bruce Tiffany <btiffany@us.ibm.com>
2017-09-25 09:36:23 -0500

    more tck files

--- dev/com.ibm.ws.security.mp.jwt_fat_tck/fat/src/org/eclipse/microprofile/jwt/tck/Readme.java
+++ dev/com.ibm.ws.security.mp.jwt_fat_tck/fat/src/org/eclipse/microprofile/jwt/tck/Readme.java
@@ -1,6 +1,13 @@
-/**
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- */
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.microprofile.jwt.tck;
 
 public class Readme {
@@ -15,7 +22,7 @@ public class Readme {
  * Web.xml has been adapted for the Liberty implementation.
  *
  * As of September 2017, a known issue prevents @RolesAllowed from working in JAX-RS
- * endpoints unless a @DeclarRoles annotation or web.xml is present to declare the roles.
+ * endpoints unless a @DeclareRoles annotation or web.xml is present to declare the roles.
  *
  * To create a signer certificate from the raw keys used in the TCK, the
  * following commands were used:
