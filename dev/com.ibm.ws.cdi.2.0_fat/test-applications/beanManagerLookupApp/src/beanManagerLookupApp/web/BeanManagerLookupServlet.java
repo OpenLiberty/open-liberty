1af2f215ada81ddf30b8485821061bf95e776173
Neil Young <neil_young@uk.ibm.com>
2017-09-28 16:51:38 +0100

    Issue 249-Add skeleton FAT test for CDI2.0
    Issue 249-Add skeleton FAT test for CDI2.0, review comments
    
    Issue 249-Add skeleton FAT test for CDI2.0, minor source corrections

---
+++ dev/com.ibm.ws.cdi.2.0_fat/test-applications/beanManagerLookupApp/src/beanManagerLookupApp/web/BeanManagerLookupServlet.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package beanManagerLookupApp.web;
+
+import static org.junit.Assert.assertTrue;
+
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.CDI;
+import javax.inject.Inject;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.annotation.WebServlet;
+
+import org.junit.Test;
+
+import componenttest.app.FATServlet;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = "/beanManagerLookupApp")
+public class BeanManagerLookupServlet extends FATServlet {
+
+    @Inject
+    BeanManager bmI;
+
+    @Inject
+    MyBeanCDI20 mb;
+
+    @Test
+    public void testbeanManagerLookup() throws Exception {
+        CDI cdi = CDI.current();
+        BeanManager bm = cdi.getBeanManager();
+        assertTrue("Bean manager from CDI.current().getBeanManager was not a BeanManager - " + bm, bm instanceof BeanManager);
+    }
+
+    @Test
+    public void testbeanManagerLookupInject() throws Exception {
+        assertTrue("Bean manager from inject was not a BeanManager - " + bmI, bmI instanceof BeanManager);
+    }
+
+    @Test
+    public void testbeanManagerLookupJndi() throws Exception {
+        Context c;
+        BeanManager bmJ = null;
+        try {
+            c = new InitialContext();
+            bmJ = (BeanManager) c.lookup("java:comp/BeanManager");
+            assertTrue("Bean manager from JNDI was not a BeanManager - " + bmJ, bmJ instanceof BeanManager);
+        } catch (NamingException e) {
+            throw new AssertionError("JNDI lookup failed");
+        }
+    }
+}
---
+++ dev/com.ibm.ws.cdi.2.0_fat/test-applications/beanManagerLookupApp/src/helloworldApp/web/HelloServlet.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package helloworldApp.web;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Set;
+
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+import javax.naming.InitialContext;
+import javax.servlet.annotation.WebServlet;
+
+import org.junit.Test;
+
+import componenttest.app.FATServlet;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = "/hello")
+public class HelloServlet extends FATServlet {
+
+    @Inject
+    HelloBeanCDI20 hello;
+
+    private static final long serialVersionUID = 8549700799591343964L;
+
+    @Test
+    public void testHelloWorldBeanManagerLookup() throws Exception {
+
+        System.out.println(hello.greeting());
+
+        try {
+            assertTrue("JNDI BeanManager from Bean FAILED - " + hello.getBeanMangerViaJNDI(), hello.getBeanMangerViaJNDI() > 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            BeanManager beanManager = (BeanManager) new InitialContext().lookup("java:comp/BeanManager");
+            Set<Bean<?>> beans = beanManager.getBeans(Object.class);
+            assertTrue("JNDI BeanManager from Servlet  FAILED - " + beans.size(), beans.size() > 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
