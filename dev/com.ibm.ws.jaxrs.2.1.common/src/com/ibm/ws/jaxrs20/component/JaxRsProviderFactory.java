269918ad99d4f4fd8865af1fa0201b3443d852b8
Adam Yoho <yoho.public@gmail.com>
2017-09-28 09:17:27 -0500

    Revert "Convert default JAX-RS 2.1 JSON provider to use the jsonp-1.1 feature"

--- dev/com.ibm.ws.jaxrs.2.1.common/src/com/ibm/ws/jaxrs20/component/JaxRsProviderFactory.java
+++ dev/com.ibm.ws.jaxrs.2.1.common/src/com/ibm/ws/jaxrs20/component/JaxRsProviderFactory.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-import javax.json.spi.JsonProvider;
-
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
@@ -35,11 +33,6 @@ import com.ibm.wsspi.kernel.service.utils.AtomicServiceReference;
  *******************************************************************************/
 @Component(immediate = true, property = { "service.vendor=IBM" }, configurationPolicy = ConfigurationPolicy.IGNORE)
 public class JaxRsProviderFactory implements JaxRsProviderFactoryService {
-    
-    // referencing the JsonProvider OSGI service reference ensures that the implementation
-    // service is available when ProviderFactory sets up the out-of-the-box providers
-    @Reference
-    private JsonProvider jsonpProvider;
 
     private static JaxRsProviderFactory serviceInstance = null;
 
