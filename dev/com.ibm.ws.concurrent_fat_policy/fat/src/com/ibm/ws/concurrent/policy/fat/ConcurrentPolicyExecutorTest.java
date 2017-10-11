106b092e8c2db3c35580f287ea26ffa681680717
Nathan Rauh <nathan.rauh@us.ibm.com>
2017-10-11 11:07:39 -0500

    Issue #364 create com.ibm.ws.concurrent_fat_policy bucket

---
+++ dev/com.ibm.ws.concurrent_fat_policy/fat/src/com/ibm/ws/concurrent/policy/fat/ConcurrentPolicyExecutorTest.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.concurrent.policy.fat;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import com.ibm.websphere.simplicity.ShrinkHelper;
+
+import componenttest.annotation.Server;
+import componenttest.annotation.TestServlet;
+import componenttest.custom.junit.runner.FATRunner;
+import componenttest.topology.impl.LibertyServer;
+import componenttest.topology.utils.FATServletClient;
+import web.ConcurrentPolicyFATServlet;
+
+/**
+ * Tests for managed executors using policyExecutor.
+ */
+@RunWith(FATRunner.class)
+public class ConcurrentPolicyExecutorTest extends FATServletClient {
+
+    public static final String APP_NAME = "concurrentpolicyfat";
+
+    @Server("com.ibm.ws.concurrent.fat.policy")
+    @TestServlet(servlet = ConcurrentPolicyFATServlet.class, path = APP_NAME + "/ConcurrentPolicyFATServlet")
+    public static LibertyServer server;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        WebArchive app = ShrinkWrap.create(WebArchive.class, APP_NAME + ".war")
+                        .addPackage("web");
+        ShrinkHelper.exportToServer(server, "dropins", app);
+        server.addInstalledAppForValidation(APP_NAME);
+        server.startServer();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        server.stopServer();
+    }
+}
---
+++ dev/com.ibm.ws.concurrent_fat_policy/test-applications/concurrentpolicyfat/src/web/CountingTask.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package web;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.enterprise.concurrent.ManagedTask;
+import javax.enterprise.concurrent.ManagedTaskListener;
+
+class CountingTask implements Callable<Integer>, ManagedTask {
+    final CountDownLatch beginLatch;
+    final CountDownLatch continueLatch;
+    final AtomicInteger counter;
+    final Map<String, String> execProps = new TreeMap<String, String>();
+    final ManagedTaskListener listener;
+
+    CountingTask(AtomicInteger counter, ManagedTaskListener listener, CountDownLatch beginLatch, CountDownLatch continueLatch) {
+        this.beginLatch = beginLatch == null ? new CountDownLatch(0) : beginLatch;
+        this.continueLatch = continueLatch == null ? new CountDownLatch(0) : continueLatch;
+        this.counter = counter == null ? new AtomicInteger() : counter;
+        this.listener = listener;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("> call " + toString());
+        beginLatch.countDown();
+        try {
+            if (!continueLatch.await(ConcurrentPolicyFATServlet.TIMEOUT_NS * 2, TimeUnit.NANOSECONDS)) {
+                System.out.println("< call " + toString() + " -1 (timed out)");
+                return -1;
+            }
+            int count = counter.incrementAndGet();
+            System.out.println("< call " + toString() + " " + count);
+            return count;
+        } catch (InterruptedException x) {
+            System.out.println("< call " + toString() + " " + x);
+            throw x;
+        }
+    }
+
+    @Override
+    public Map<String, String> getExecutionProperties() {
+        return execProps;
+    }
+
+    @Override
+    public ManagedTaskListener getManagedTaskListener() {
+        return listener;
+    }
+}
