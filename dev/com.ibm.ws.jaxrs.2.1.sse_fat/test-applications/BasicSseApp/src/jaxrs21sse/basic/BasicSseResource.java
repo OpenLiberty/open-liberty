6295ae4ca7aa2bef7414e8e82028554548502ec2
Jim Krueger <jim_krueger@us.ibm.com>
2017-10-13 15:49:18 -0500

    Add jaxb and jsonb variations to JAXRS SSE basic function test.

--- dev/com.ibm.ws.jaxrs.2.1.sse_fat/test-applications/BasicSseApp/src/jaxrs21sse/basic/BasicSseResource.java
+++ dev/com.ibm.ws.jaxrs.2.1.sse_fat/test-applications/BasicSseApp/src/jaxrs21sse/basic/BasicSseResource.java
@@ -13,7 +13,9 @@
  */
 package jaxrs21sse.basic;
 
-import static jaxrs21sse.basic.DataObject.DATA_OBJECTS;
+import static jaxrs21sse.basic.JaxbObject.JAXB_OBJECTS;
+import static jaxrs21sse.basic.JsonObject.JSON_OBJECTS;
+import static jaxrs21sse.basic.JsonbObject.JSONB_OBJECTS;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
@@ -63,6 +65,42 @@ public class BasicSseResource extends Application {
     }
 
     @GET
+    @Path("/integer3")
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    public void send3IntegerEvents(@Context SseEventSink eventSink, @Context Sse sse) {
+        Integer[] eventData = new Integer[] { 1, 2, 3 };
+        Runnable r = new Runnable() {
+
+            @Override
+            public void run() {
+                try (SseEventSink sink = eventSink) {
+                    for (int i = 0; i < eventData.length; i++) {
+                        OutboundSseEvent event = sse.newEventBuilder()
+                                        .name("IntegerEvent")
+                                        .data(eventData[i])
+                                        .build();
+                        System.out.println("BasicSseResource.sendIntegerEvents() sending: " + eventData[i].intValue());
+                        sink.send(event);
+                        try {
+                            Thread.sleep(200);
+                        } catch (InterruptedException ex) {
+                            BasicSseTestServlet.resourceFailures.add("InterrupedException while sleeping in BasicSseResource.send3IntegerEvents");
+                            ex.printStackTrace();
+                        }
+                    }
+
+                }
+                if (!eventSink.isClosed()) {
+                    BasicSseTestServlet.resourceFailures.add("AutoClose in BasicSseResource.send3IntegerEvents failed for eventSink");
+                }
+
+            }
+
+        };
+        new Thread(r).start();
+    }
+
+    @GET
     @Path("/json3")
     @Produces(MediaType.SERVER_SENT_EVENTS)
     public void send3JsonEvents(@Context SseEventSink eventSink, @Context Sse sse) {
@@ -71,12 +109,12 @@ public class BasicSseResource extends Application {
             @Override
             public void run() {
                 try (SseEventSink sink = eventSink) {
-                    for (int i = 0; i < DATA_OBJECTS.length; i++) {
+                    for (int i = 0; i < JSON_OBJECTS.length; i++) {
                         OutboundSseEvent event = sse.newEventBuilder()
                                         .mediaType(MediaType.APPLICATION_JSON_TYPE)
-                                        .data(DataObject.class, DATA_OBJECTS[i])
+                                        .data(JsonObject.class, JSON_OBJECTS[i])
                                         .build();
-                        System.out.println("BasicSseResource.send3JsonEvents() sending: " + DATA_OBJECTS[i]);
+                        System.out.println("BasicSseResource.send3JsonEvents() sending: " + JSON_OBJECTS[i]);
                         sink.send(event);
                         try {
                             Thread.sleep(200);
@@ -88,7 +126,7 @@ public class BasicSseResource extends Application {
 
                 }
                 if (!eventSink.isClosed()) {
-                    BasicSseTestServlet.resourceFailures.add("AutoClose in BasicSseResource.send3Json3Events failed for eventSink");
+                    BasicSseTestServlet.resourceFailures.add("AutoClose in BasicSseResource.send3JsonEvents failed for eventSink");
                 }
 
             }
@@ -96,4 +134,75 @@ public class BasicSseResource extends Application {
         };
         new Thread(r).start();
     }
+
+    @GET
+    @Path("/jsonb3")
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    public void send3JsonbEvents(@Context SseEventSink eventSink, @Context Sse sse) {
+        Runnable r = new Runnable() {
+
+            @Override
+            public void run() {
+                try (SseEventSink sink = eventSink) {
+                    for (int i = 0; i < JSONB_OBJECTS.length; i++) {
+                        OutboundSseEvent event = sse.newEventBuilder()
+                                        .mediaType(MediaType.APPLICATION_JSON_TYPE)
+                                        .data(JsonbObject.class, JSONB_OBJECTS[i])
+                                        .build();
+                        System.out.println("BasicSseResource.send3JsonbEvents() sending: " + JSONB_OBJECTS[i]);
+                        sink.send(event);
+                        try {
+                            Thread.sleep(200);
+                        } catch (InterruptedException ex) {
+                            BasicSseTestServlet.resourceFailures.add("InterrupedException while sleeping in BasicSseResource.send3JsonbEvents");
+                            ex.printStackTrace();
+                        }
+                    }
+
+                }
+                if (!eventSink.isClosed()) {
+                    BasicSseTestServlet.resourceFailures.add("AutoClose in BasicSseResource.send3JsonEvents failed for eventSink");
+                }
+
+            }
+
+        };
+        new Thread(r).start();
+    }
+
+    @GET
+    @Path("/jaxb3")
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    public void send3JaxbEvents(@Context SseEventSink eventSink, @Context Sse sse) {
+        Runnable r = new Runnable() {
+
+            @Override
+            public void run() {
+                try (SseEventSink sink = eventSink) {
+                    for (int i = 0; i < JAXB_OBJECTS.length; i++) {
+                        OutboundSseEvent event = sse.newEventBuilder()
+                                        .mediaType(MediaType.APPLICATION_XML_TYPE)
+                                        .data(JaxbObject.class, JAXB_OBJECTS[i])
+                                        .build();
+                        System.out.println("BasicSseResource.send3JaxbEvents() sending: " + JAXB_OBJECTS[i]);
+                        sink.send(event);
+                        try {
+                            Thread.sleep(200);
+                        } catch (InterruptedException ex) {
+                            BasicSseTestServlet.resourceFailures.add("InterrupedException while sleeping in BasicSseResource.send3JaxbEvents");
+                            ex.printStackTrace();
+                        }
+                    }
+
+                }
+                if (!eventSink.isClosed()) {
+                    BasicSseTestServlet.resourceFailures.add("AutoClose in BasicSseResource.send3JaxbEvents failed for eventSink");
+                }
+
+            }
+
+        };
+        new Thread(r).start();
+    }
+
 }
