6ca73a4958daf34f56fae0ab16c5bf4ad459186d
Thomas Watson <tjwatson@us.ibm.com>
2017-10-20 14:17:24 -0500

    Issue #372: Use StaticValue class for multiplexing SymbolRegistry and WSLocationAdminImpl

--- dev/com.ibm.ws.junit.extensions/src/test/common/SharedLocationManager.java
+++ dev/com.ibm.ws.junit.extensions/src/test/common/SharedLocationManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 IBM Corporation and others.
+ * Copyright (c) 2010, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -17,24 +17,26 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.ibm.ws.staticvalue.StaticValue;
+
 /**
  * The SharedLocationManager primes the configured locations used by
  * the WsLocationAdmin service and the WsLocationAdminManager. When used in
  * unit test, it allows your test code to drive methods that rely on
  * a configured WsLocationAdmin service or the static WsLocationAdminManager.
- * 
+ *
  * <h2>Unit Testing</h2>
  * <p>
  * Our unit test cases need to check for the presence of files, etc. in known
  * locations. There is one set of methods providing an abbreviated tree, and
  * another that replicates the default server tree structure, both starting at
  * specified root directoy
- * 
+ *
  * <h2>Component Testing</h2>
  * <p>
  * The JUnit drivers for our component tests need to check for the presence of
  * files, etc. in the relevant server directory in the target image.
- * 
+ *
  */
 public class SharedLocationManager {
     /**
@@ -44,7 +46,7 @@ public class SharedLocationManager {
      * underneath the provided root directory (instead of wlp/).
      * <p>
      * Directories will not be accessed/created until requested.
-     * 
+     *
      * @param rootDirStr
      *            Root directory for structure shown above
      * @param serverName
@@ -54,7 +56,7 @@ public class SharedLocationManager {
      *            be passed to the location service. The following properties are
      *            set by this method (and will be overwritten): wlp.install.dir,
      *            wlp.user.dir, wlp.lib.dir, wlp.server.name
-     * 
+     *
      * @return the configured WsLocationAdmin instance.
      */
     public static Object createDefaultLocations(String rootDirStr, String serverName, Map<String, Object> map) {
@@ -68,7 +70,7 @@ public class SharedLocationManager {
      * underneath the provided root directory (instead of wlp/).
      * <p>
      * Directories will not be accessed/created until requested.
-     * 
+     *
      * @param rootDirStr
      *            Root directory for structure shown above
      * @param serverName
@@ -80,7 +82,7 @@ public class SharedLocationManager {
      *            wlp.user.dir, wlp.lib.dir, wlp.server.name
      * @param isClient
      *            boolean value to determine whether this call is used for client or server.
-     * 
+     *
      * @return the configured WsLocationAdmin instance.
      */
     public static Object createDefaultLocations(String rootDirStr, String serverName, Map<String, Object> map, boolean isClient) {
@@ -106,7 +108,7 @@ public class SharedLocationManager {
         map.put("wlp.lib.dir", lib.getAbsolutePath() + '/');
         map.put("wlp.server.name", serverName);
 
-        // Defined in BootstrapConstants.PROCESS_TYPE_CLIENT 
+        // Defined in BootstrapConstants.PROCESS_TYPE_CLIENT
         // Defined in BootstrapConstants.PROCESS_TYPE_SERVER
         map.put("wlp.process.type", isClient ? "client" : "server");
 
@@ -139,12 +141,12 @@ public class SharedLocationManager {
      * document
      * will be the default "rootDir/usr/servers/serverName/server.xml" (does not
      * need to exist).
-     * 
+     *
      * @param rootDirStr
      *            Root directory for structure shown above
      * @param serverName
      *            Name of server
-     * 
+     *
      * @return the configured WsLocationAdmin instance.
      * @see #createDefaultLocations(String, String, Map)
      */
@@ -158,10 +160,10 @@ public class SharedLocationManager {
      * server directory name will be "defaultServer". The config root document
      * will be the default "rootDir/usr/servers/defaultServer/server.xml" (does
      * not need to exist).
-     * 
+     *
      * @param rootDirStr
      *            Root directory for structure shown above
-     * 
+     *
      * @return the configured WsLocationAdmin instance.
      * @see #createDefaultLocations(String, String, Map)
      */
@@ -173,7 +175,7 @@ public class SharedLocationManager {
      * Used in test: construct and replace the singleton instance of the
      * WsLocationAdmin service/manager based on the provided root directory. This
      * will create/look for the following directory structure:
-     * 
+     *
      * <pre>
      * rootDir/
      * applications/ (shared.app.dir)
@@ -190,9 +192,9 @@ public class SharedLocationManager {
      * workarea/
      * getServerWorkareaFile()
      * </pre>
-     * 
+     *
      * Directories will not be accessed/created until requested.
-     * 
+     *
      * @param rootDirStr
      *            Root directory for structure shown above
      * @param serverName
@@ -249,7 +251,7 @@ public class SharedLocationManager {
      * WsLocationAdmin service/manager based on the provided root directory. The
      * config root document will be the default "rootDir/serverName/server.xml"
      * (does not need to exist).
-     * 
+     *
      * @param rootDirStr
      *            Root directory for structure shown above
      * @param serverName
@@ -267,7 +269,7 @@ public class SharedLocationManager {
      * directory. The server directory name will be "defaultServer".
      * The config root document will be the default
      * "rootDir/defaultServer/server.xml" (does not need to exist).
-     * 
+     *
      * @param rootDirStr
      *            Root directory for structure shown above
      * @see #createLocations(String, String, Map)
@@ -280,7 +282,7 @@ public class SharedLocationManager {
      * For use with component tests: Populate an WsLocationAdmin instance with
      * paths pointing to the install image directory using the "image.dir" system
      * property so that the test clients can check for the presence of files, etc.
-     * 
+     *
      * @param serverName
      *            Name of server
      * @param map
@@ -343,14 +345,14 @@ public class SharedLocationManager {
      * WsLocationAdmin service/manager with the location of the build
      * image and the provided server name (e.g.
      * build.image/wlp/usr/servers/serverName).
-     * 
+     *
      * Will use the default configuration file(s) contained in the server
      * directory (if present, both are optional):
      * <ul>
      * <li>build.image/wlp/usr/servers/serverName/server.xml,</li>
      * <li>build.image/wlp/usr/servers/serverName/bootstrap.properties</li>
      * </ul>
-     * 
+     *
      * @param serverName
      *            Name of server
      * @return the configured WsLocationAdmin instance.
@@ -370,7 +372,7 @@ public class SharedLocationManager {
         try {
             f = impl.getDeclaredField("instance");
             f.setAccessible(true);
-            f.set(null, null);
+            f.set(null, StaticValue.mutateStaticValue((StaticValue<?>) f.get(null), null));
         } catch (Exception e) {
             System.err.println("Woops! Something is amiss; could not reset test locations");
             e.printStackTrace();
@@ -381,33 +383,38 @@ public class SharedLocationManager {
     /**
      * Return the instance of the WsLocationAdminManager for use with jmock
      * objects:
-     * 
+     *
      * <pre>
      * final ServiceReference mockServiceRef = context.mock(ServiceReference.class);
      * final BundleContext mockBundleContext = context.mock(BundleContext.class);
      * final WsLocationAdmin location = (WsLocationAdmin) SharedLocationManager.getLocationInstance();
-     * 
-     * context.checking(new Expectations()
-     * {
-     * {
-     * one(mockBundleContext).getServiceReference(with(WsLocationAdmin.class.getName()));
-     * will(returnValue(mockServiceRef));
-     * one(mockBundleContext).getService(mockServiceRef);
-     * will(returnValue(location));
-     * }
+     *
+     * context.checking(new Expectations() {
+     *     {
+     *         one(mockBundleContext).getServiceReference(with(WsLocationAdmin.class.getName()));
+     *         will(returnValue(mockServiceRef));
+     *         one(mockBundleContext).getService(mockServiceRef);
+     *         will(returnValue(location));
+     *     }
      * });
      * </pre>
-     * 
+     *
      * @return the configured WsLocationAdmin instance.
      */
     public static Object getLocationInstance() {
         Class<?> impl = getLocServiceImpl();
 
-        Field f;
+        Method m;
         try {
-            f = impl.getDeclaredField("instance");
-            f.setAccessible(true);
-            return f.get(null);
+            m = impl.getDeclaredMethod("getInstance");
+            return m.invoke(null);
+        } catch (InvocationTargetException e) {
+            if (e.getCause() instanceof IllegalStateException) {
+                return null;
+            }
+            System.err.println("Woops! Something is amiss; could not get WsLocationAdmin instance");
+            e.printStackTrace();
+            throw new java.lang.InstantiationError("Unable to get test WsLocationAdmin instance");
         } catch (Exception e) {
             System.err.println("Woops! Something is amiss; could not get WsLocationAdmin instance");
             e.printStackTrace();
@@ -417,7 +424,7 @@ public class SharedLocationManager {
 
     /**
      * Print out configured locations
-     * 
+     *
      * @return Formatted string describing configured locations
      */
     public static String debugConfiguredLocations() {
