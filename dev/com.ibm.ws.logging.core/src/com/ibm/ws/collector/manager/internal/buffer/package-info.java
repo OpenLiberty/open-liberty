1f5e8b2cf29b2ccf57619550a3483f9b25dfbf51
chdavid <chdavid@ca.ibm.com>
2017-10-24 14:13:32 -0400

    Moved two collector manager packages into logging.core, made appropriate changes to package-info.java. Modified the version or created one. Also modified logging.core bnd.bnd to export these 2 new packages

---
+++ dev/com.ibm.ws.logging.core/src/com/ibm/ws/collector/manager/internal/buffer/package-info.java
@@ -0,0 +1,26 @@
+/*
+ * IBM Confidential
+ *
+ * OCO Source Materials
+ *
+ * Copyright IBM Corp. 2015
+ *
+ * The source code for this program is not published or otherwise divested 
+ * of its trade secrets, irrespective of what has been deposited with the 
+ * U.S. Copyright Office.
+ * 
+ * 
+ * Change activity:
+ *
+ * Issue       Date        Name     Description
+ * ----------- ----------- -------- ------------------------------------
+ */
+/**
+ * @version 1.0.16
+ */
+@org.osgi.annotation.versioning.Version("1.0.16")
+@TraceOptions(traceGroup = "collectorManager", messageBundle = "com.ibm.ws.collector.manager.internal.resources.LoggingMessages")
+package com.ibm.ws.collector.manager.internal.buffer;
+
+import com.ibm.websphere.ras.annotation.TraceOptions;
+
