4ba4b9c944f29881b6f04c8858e74c45fa222904
Andy Mauer <ajmauer@us.ibm.com>
2017-10-26 16:56:05 -0400

    Group Security drop 1
    fix one item in group security after merging to up to date master
    
    check that group name provided in config is valid
    
    remove unneeded method from iface
    
    change authSubmitter to queryIssuer in the queryhelper
    
    clean up copyrights and dead code
    
    collapse WSJobRepo.getJobInstances into a single method using QueryHelper
    
    remove un-needed typed queries
    
    re-organize the order of JobInstanceEntity version discovery
    
    general code cleanup based on review comments
    
    add index to new table
    
    messages support
    
    move security code from jobrepo to auth service
    
    change col and class var from groupName to groupNames
    
    update WSBatchAuthServiceImpl based on review comments
    
    clean up dead code in WSBatchAuthServiceImpl based on review comments
    
    re-order access check logic
    
    update with nlsprops review comments
    
    change group security message id number
    
    update BatchSecurity messages based on review
    
    update messages with futher review comments

---
+++ dev/com.ibm.jbatch.container/src/com/ibm/jbatch/container/persistence/jpa/JobInstanceEntityV3.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.jbatch.container.persistence.jpa;
+
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+
+/**
+ *
+ */
+@Entity
+public class JobInstanceEntityV3 extends JobInstanceEntityV2 {
+
+    @ElementCollection
+    @CollectionTable(name = "GROUPASSOCIATION", joinColumns = @JoinColumn(name = "FK_JOBINSTANCEID"))
+    @Column(name = "GROUPNAMES")
+    private Set<String> groupNames;
+
+    // For JPA
+    public JobInstanceEntityV3() {
+        super();
+    }
+
+    // For in-memory persistence
+    public JobInstanceEntityV3(long jobInstanceId) {
+        super(jobInstanceId);
+    }
+
+    public void setGroupNames(Set<String> opGroupNames) {
+
+        groupNames = opGroupNames;
+    }
+
+    @Override
+    public Set<String> getGroupNames() {
+        return groupNames;
+    }
+
+}
