baaed550f24d292a4327f22ecde72e4b25298f52
Benjamin Confino <benjamic@uk.ibm.com>
2017-10-27 13:36:36 +0100

    code review and copyright changes

--- dev/com.ibm.ws.cdi.1.2_fat/test-applications/resourceWebServicesClient.war/src/servlets/TestWebServicesServlet.java
+++ dev/com.ibm.ws.cdi.1.2_fat/test-applications/resourceWebServicesClient.war/src/servlets/TestWebServicesServlet.java
@@ -1,16 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2012 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package servlets;
-
-/*
- * IBM Confidential
- *
- * OCO Source Materials
- *
- * Copyright IBM Corp. 2012
- *
- * The source code for this program is not published or otherwise divested
- * of its trade secrets, irrespective of what has been deposited with the
- * U.S. Copyright Office.
- */
 
 import java.io.IOException;
 import java.io.Writer;
