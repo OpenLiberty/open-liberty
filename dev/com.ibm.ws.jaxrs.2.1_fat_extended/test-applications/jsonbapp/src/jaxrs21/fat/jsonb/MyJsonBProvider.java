dbbee7eb426220f997ba8a7d292cbd5c2b56fb6d
Adam Anderson <atanders@us.ibm.com>
2017-10-30 14:19:33 -0500

    264 - In JAX-RS 2.0 we had tests where a user packages their own version of Jackson, and we ensure that their data is transformed correctly according to their Jackson configuration. In JAX-RS 2.1, we are changing the default JSON serialization technology to JSON-B, but like our 2.0 implementation, we are not exposing these APIs (by default -- it will be possible to expose these APIs if the user also enables the jsonb-1.0 feature).
    We need test cases for the scenario where a user packages their own version of JSON-B (both API and implementation) in their application.
    
    This commit contains 2 test suites: 1 where the customer packages
    Johnzon with jsonb-1.0 api and no jsonb-1.0 feature. and 2 where the
    customer enables the jsonb-1.0 feature and packages Johnzon and no api
    jar.

---
+++ dev/com.ibm.ws.jaxrs.2.1_fat_extended/test-applications/jsonbapp/src/jaxrs21/fat/jsonb/MyJsonBProvider.java
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * Copyright (c) 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package jaxrs21.fat.jsonb;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+
+@Produces({ MediaType.APPLICATION_JSON })
+@Consumes({ MediaType.APPLICATION_JSON })
+@Provider
+public class MyJsonBProvider implements MessageBodyWriter<Object>, MessageBodyReader<Object> {
+
+    private final Jsonb jsonb = JsonbBuilder.create();
+
+    @Override
+    public boolean isReadable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        boolean readable = false;
+        if (isJsonType(mediaType)) {
+            readable = true;
+        }
+        System.out.println("MyJsonBProvider:isReadable=" + readable);
+        return readable;
+    }
+
+    @Override
+    public Object readFrom(Class<Object> clazz, Type genericType, Annotation[] annotations,
+                           MediaType mediaType, MultivaluedMap<String, String> httpHeaders, InputStream entityStream) throws IOException, WebApplicationException {
+        Object obj = null;
+        obj = this.jsonb.fromJson(entityStream, clazz);
+        System.out.println("MyJsonBProvider:readFrom=" + obj);
+        return obj;
+    }
+
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        boolean writeable = false;
+        if (isJsonType(mediaType)) {
+            writeable = true;
+        }
+        System.out.println("MyJsonBProvider:isWriteable=" + writeable);
+        return writeable;
+    }
+
+    @Override
+    public void writeTo(Object obj, Class<?> type, Type genericType, Annotation[] annotations,
+                        MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+        System.out.println("MyJsonBProvider:writeTo=" + obj);
+        this.jsonb.toJson(obj, entityStream);
+    }
+
+    private boolean isJsonType(MediaType mediaType) {
+        return mediaType.getSubtype().toLowerCase().startsWith("json")
+               || mediaType.getSubtype().toLowerCase().contains("+json");
+    }
+}
