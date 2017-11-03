1344ca0e85d51487154e44fa057855e4d866769c
Steve Fontes <steve.m.fontes@gmail.com>
2017-11-03 12:33:49 -0400

    Add @Traced annotation
    Signed-off-by: Steve Fontes <steve.m.fontes@gmail.com>

--- dev/com.ibm.ws.opentracing.cdi/src/com/ibm/ws/opentracing/cdi/OpentracingCDIInjectionExtension.java
+++ dev/com.ibm.ws.opentracing.cdi/src/com/ibm/ws/opentracing/cdi/OpentracingCDIInjectionExtension.java
@@ -10,10 +10,6 @@
  *******************************************************************************/
 package com.ibm.ws.opentracing.cdi;
 
-import javax.enterprise.event.Observes;
-import javax.enterprise.inject.spi.AnnotatedType;
-import javax.enterprise.inject.spi.BeanManager;
-import javax.enterprise.inject.spi.BeforeBeanDiscovery;
 import javax.enterprise.inject.spi.Extension;
 
 import org.osgi.service.component.annotations.Component;
@@ -23,12 +19,12 @@ import com.ibm.ws.cdi.extension.WebSphereCDIExtension;
 
 @Component(service = WebSphereCDIExtension.class, configurationPolicy = ConfigurationPolicy.IGNORE)
 public class OpentracingCDIInjectionExtension implements WebSphereCDIExtension, Extension {
-    void beforeBeanDiscovery(@Observes BeforeBeanDiscovery bbd, BeanManager bm) {
-        AnnotatedType<OpentracingProducerBean> at = bm.createAnnotatedType(OpentracingProducerBean.class);
-        bbd.addAnnotatedType(at);
-        AnnotatedType<Traced> bindingType = bm.createAnnotatedType(Traced.class);
-        bbd.addInterceptorBinding(bindingType);
-        AnnotatedType<TracedInterceptor> interceptorType = bm.createAnnotatedType(TracedInterceptor.class);
-        bbd.addAnnotatedType(interceptorType);
-    }
+//    void beforeBeanDiscovery(@Observes BeforeBeanDiscovery bbd, BeanManager bm) {
+//        AnnotatedType<OpentracingProducerBean> at = bm.createAnnotatedType(OpentracingProducerBean.class);
+//        bbd.addAnnotatedType(at);
+//        AnnotatedType<Traced> bindingType = bm.createAnnotatedType(Traced.class);
+//        bbd.addInterceptorBinding(bindingType);
+//        AnnotatedType<TracedInterceptor> interceptorType = bm.createAnnotatedType(TracedInterceptor.class);
+//        bbd.addAnnotatedType(interceptorType);
+//    }
 }
