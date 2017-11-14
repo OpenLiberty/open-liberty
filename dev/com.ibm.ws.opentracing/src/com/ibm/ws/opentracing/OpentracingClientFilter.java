d45fbba462fed1a7707087166809941f3e0fd79d
kgibm <kevin.grigorenko@us.ibm.com>
2017-11-13 17:14:04 -0800

    Opentracing support for excluding/including spans by URI. (#909)
    There are two types of configuration elements: `excludeSpans` and `includeSpans`.
    
        <opentracing>
          <excludeSpans pattern="URI" type="INCOMING|OUTGOING|BOTH" ignoreCase="true|false" regex="true|false" />
          <includeSpans pattern="URI" type="INCOMING|OUTGOING|BOTH" ignoreCase="true|false" regex="true|false" />
        </opentracing>
    
    These are called filters. Each filter may be specified 0 or more times.
    
    When a JAX-RS request comes in to the container, all `INCOMING` or `BOTH` filters are checked with the incoming URI. If all checks return true, then a new span is created for the incoming request; otherwise, no new span is created, but any incoming spans are passed through.
    
    When a JAX-RS request is sent from the container, all `OUTGOING` or `BOTH` filters are checked with the outgoing URI. If all checks return true, then a new span is created for the outgoing request; otherwise, no new span is created, but any incoming spans are passed through.
    
    The default type of a filter is `BOTH`.
    
    All filters have a `pattern` attribute which is the URI to match to the incoming and/or outgoing URI. If the `pattern` starts with `http(s)://`, then the filter will compare to the [absolute URI](https://docs.oracle.com/javase/9/docs/api/java/net/URI.html#toString--); otherwise, the filter will compare to the URI starting with the [path](https://docs.oracle.com/javase/9/docs/api/java/net/URI.html#getRawPath--) (`/[...]`). In both cases, the filter will compare with [query](https://docs.oracle.com/javase/9/docs/api/java/net/URI.html#getRawQuery--) and [fragment](https://docs.oracle.com/javase/9/docs/api/java/net/URI.html#getRawFragment--), if available, for both the `pattern` and the URI.
    
    If `regex` is false (the default) and `pattern` ends with `*`, then the filter will check if the URI starts with the `pattern`; otherwise, the filter will do an exact match. In both cases, the comparison will be done in a case sensitive or insensitive way depending on the value of `ignoreCase`. The default of `ignoreCase` is `true`.
    
    If `regex` is true, `pattern` is compiled into a [java.util.regex.Pattern](https://docs.oracle.com/javase/9/docs/api/java/util/regex/Pattern.html) and the URI is matched interpreting the `pattern` as a regular expression. The comparison will be done in a case sensitive or [insensitive](https://docs.oracle.com/javase/9/docs/api/java/util/regex/Pattern.html#CASE_INSENSITIVE) way depending on the value of `ignoreCase`. Regular expressions are discouraged because they are resource intensive and run on every JAXRS request. One common mistake when using regular expressions is that some URL components such as the query separator (`?`) have special meaning in regular expressions and must be escaped with `\`.
    
    If no filters match the URI, then the default is that the span is created.
    
    The filters are processed in the order in which they appear in the configuration. For example, if we want to only process URIs that start with `/api`, then we can use an `excludeSpans` first with a `pattern` of `*` to exclude all URIs (this is needed since, by default, spans are created), and then we can use an `includeSpans` with a `pattern` of `/api*`.
    
    Both the `pattern` and URI are compared with [RFC 2396](https://www.ietf.org/rfc/rfc2396.txt)-compliant % URI-encoding, when needed.

--- dev/com.ibm.ws.opentracing/src/com/ibm/ws/opentracing/OpentracingClientFilter.java
+++ dev/com.ibm.ws.opentracing/src/com/ibm/ws/opentracing/OpentracingClientFilter.java
@@ -11,6 +11,7 @@
 package com.ibm.ws.opentracing;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -25,9 +26,11 @@ import org.osgi.service.component.annotations.Component;
 import com.ibm.websphere.ras.Tr;
 import com.ibm.websphere.ras.TraceComponent;
 import com.ibm.websphere.ras.annotation.Trivial;
+import com.ibm.ws.opentracing.filters.SpanFilterType;
 
 import io.opentracing.ActiveSpan;
 import io.opentracing.Span;
+import io.opentracing.SpanContext;
 import io.opentracing.Tracer;
 import io.opentracing.propagation.Format;
 import io.opentracing.propagation.TextMap;
@@ -40,7 +43,7 @@ import io.opentracing.tag.Tags;
  *
  * <p>Implements both {@link ClientRequestFilter} and {@link ClientResponseFilter}.</p>
  *
- * <p>This implementation is stateless.  A single client filter is used by all clients.</p>
+ * <p>This implementation is stateless. A single client filter is used by all clients.</p>
  */
 @Component
 public class OpentracingClientFilter implements ClientRequestFilter, ClientResponseFilter {
@@ -54,6 +57,8 @@ public class OpentracingClientFilter implements ClientRequestFilter, ClientRespo
      * <p>See {@link OpentracingContainerFilter#SERVER_SPAN_PROP_ID} for more information.</p>
      */
     public static final String CLIENT_SPAN_PROP_ID = OpentracingClientFilter.class.getName() + ".Span";
+
+    public static final String CLIENT_SPAN_SKIPPED_ID = OpentracingClientFilter.class.getName() + ".Skipped";
 
     /**
      * <p>Handle an outgoing request.</p>
@@ -74,56 +79,70 @@ public class OpentracingClientFilter implements ClientRequestFilter, ClientRespo
         String methodName = "filter(outgoing)";
 
         Tracer tracer = OpentracingTracerManager.getTracer();
-        if ( tracer == null ) {
+        if (tracer == null) {
             Tr.error(tc, "OPENTRACING_NO_TRACER_FOR_OUTBOUND_REQUEST");
             return;
         } else {
-            if ( TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled() ) {
+            if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
                 Tr.debug(tc, methodName, OpentracingUtils.getTracerText(tracer));
             }
         }
 
-        String outgoingURL = clientRequestContext.getUri().toURL().toString();
+        URI outgoingUri = clientRequestContext.getUri();
+        String outgoingURL = outgoingUri.toURL().toString();
         if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
             Tr.debug(tc, methodName + " outgoing URL", outgoingURL);
         }
 
-        Tracer.SpanBuilder spanBuilder = tracer.buildSpan(outgoingURL);
-
-        spanBuilder.withTag(Tags.SPAN_KIND.getKey(), Tags.SPAN_KIND_CLIENT);
-        spanBuilder.withTag(Tags.HTTP_URL.getKey(), outgoingURL);
-        spanBuilder.withTag(Tags.HTTP_METHOD.getKey(), clientRequestContext.getMethod());
-
         ActiveSpan priorIncomingSpan = tracer.activeSpan();
-        if ( TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled() ) {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
             Tr.debug(tc, methodName + " parent", priorIncomingSpan);
         }
-        if ( priorIncomingSpan != null ) {
-            spanBuilder.asChildOf(priorIncomingSpan.context());
-        } else {
-            // spanBuilder.ignoreActiveSpan();
-            //
-            // TODO:
-            // The work-around, which is a call 'spanBuilder.ignoreActiveSpan()',
-            // and which is used in the container filter, is not needed here.
-            //
-            // The code is a bit muddled, though, as the mock span defaults to
-            // look for and use the active span as the parent span, which means
-            // the call 'spanBuilder.asChildOf' may not be necessary.
-        }
 
-        Span newOutoingSpan = spanBuilder.startManual();
-        if ( TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled() ) {
-            Tr.debug(tc, methodName + " outgoingSpan", newOutoingSpan);
+        boolean process = OpentracingService.process(outgoingUri, SpanFilterType.OUTGOING);
+
+        SpanContext nextContext;
+
+        if (process) {
+            Tracer.SpanBuilder spanBuilder = tracer.buildSpan(outgoingURL);
+
+            spanBuilder.withTag(Tags.SPAN_KIND.getKey(), Tags.SPAN_KIND_CLIENT);
+            spanBuilder.withTag(Tags.HTTP_URL.getKey(), outgoingURL);
+            spanBuilder.withTag(Tags.HTTP_METHOD.getKey(), clientRequestContext.getMethod());
+
+            if (priorIncomingSpan != null) {
+                spanBuilder.asChildOf(priorIncomingSpan.context());
+            } else {
+                // spanBuilder.ignoreActiveSpan();
+                //
+                // TODO:
+                // The work-around, which is a call 'spanBuilder.ignoreActiveSpan()',
+                // and which is used in the container filter, is not needed here.
+                //
+                // The code is a bit muddled, though, as the mock span defaults to
+                // look for and use the active span as the parent span, which means
+                // the call 'spanBuilder.asChildOf' may not be necessary.
+            }
+
+            Span newOutoingSpan = spanBuilder.startManual();
+            if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+                Tr.debug(tc, methodName + " outgoingSpan", newOutoingSpan);
+            }
+
+            nextContext = newOutoingSpan.context();
+
+            // Note the use of the span stored under the client property: This is to
+            // create the association of the outgoing request to the incoming response.
+            clientRequestContext.setProperty(CLIENT_SPAN_PROP_ID, newOutoingSpan);
+        } else {
+            nextContext = priorIncomingSpan.context();
         }
 
         tracer.inject(
-            newOutoingSpan.context(),
-            Format.Builtin.HTTP_HEADERS, new MultivaluedMapToTextMap(clientRequestContext.getHeaders()));
+                      nextContext,
+                      Format.Builtin.HTTP_HEADERS, new MultivaluedMapToTextMap(clientRequestContext.getHeaders()));
 
-        // Note the use of the span stored under the client property: This is to
-        // create the association of the outgoing request to the incoming response.
-        clientRequestContext.setProperty(CLIENT_SPAN_PROP_ID, newOutoingSpan);
+        clientRequestContext.setProperty(CLIENT_SPAN_SKIPPED_ID, !process);
     }
 
     /**
@@ -142,26 +161,34 @@ public class OpentracingClientFilter implements ClientRequestFilter, ClientRespo
                        ClientResponseContext clientResponseContext) throws IOException {
         String methodName = "filter(incoming)";
 
+        if ((Boolean) clientRequestContext.getProperty(CLIENT_SPAN_SKIPPED_ID)) {
+            if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+                Tr.debug(tc, methodName, "skipped");
+            }
+            clientRequestContext.removeProperty(CLIENT_SPAN_SKIPPED_ID);
+            return;
+        }
+
         Span priorOutgoingSpan = (Span) clientRequestContext.getProperty(CLIENT_SPAN_PROP_ID);
-        if ( TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled() ) {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
             Tr.debug(tc, methodName, priorOutgoingSpan);
         }
 
-        if ( priorOutgoingSpan == null ) {
+        if (priorOutgoingSpan == null) {
             Tr.error(tc, "OPENTRACING_NO_SPAN_FOR_RESPONSE_TO_OUTBOUND_REQUEST");
             return;
         }
 
         try {
             Integer httpStatus = Integer.valueOf(clientResponseContext.getStatus());
-            if ( TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled() ) {
+            if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
                 Tr.debug(tc, methodName + " httpStatus", httpStatus);
             }
 
             priorOutgoingSpan.setTag(Tags.HTTP_STATUS.getKey(), httpStatus);
-            if ( clientResponseContext.getStatus() >= 400 ) {
+            if (clientResponseContext.getStatus() >= 400) {
                 priorOutgoingSpan.setTag(Tags.ERROR.getKey(), true);
-                if ( TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled() ) {
+                if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
                     Tr.debug(tc, methodName + " error", Boolean.TRUE);
                 }
             }
