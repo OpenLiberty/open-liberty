448838b4060bb10dc88257c2fda506b63d4e39fc
Nathan Rauh <nathan.rauh@us.ibm.com>
2017-11-14 12:18:23 -0600

    Issue #283 unit test for expedite behavior

--- dev/com.ibm.ws.threading/test/com/ibm/ws/threading/internal/PolicyExecutorTest.java
+++ dev/com.ibm.ws.threading/test/com/ibm/ws/threading/internal/PolicyExecutorTest.java
@@ -11,33 +11,131 @@
 package com.ibm.ws.threading.internal;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
 
 import com.ibm.ws.threading.PolicyExecutor;
 import com.ibm.ws.threading.PolicyExecutorProvider;
+
+import test.common.SharedOutputManager;
 
 /**
  * Unit tests for policy executor.
  */
 public class PolicyExecutorTest {
+    static SharedOutputManager outputMgr = SharedOutputManager.getInstance().trace("*=info:ConcurrencyPolicy=all:*=all");
+    @Rule
+    public TestRule managerRule = outputMgr;
+
+    // Maximum number of nanoseconds to wait for a task to finish.
+    private static final long TIMEOUT_NS = TimeUnit.MINUTES.toNanos(1);
 
     PolicyExecutorProvider provider = new PolicyExecutorProvider();
 
-    // Verify that core concurrency can be -1 (unlimited) but otherwise not negative or greater than maximum concurrency,
+    static class CommonTask implements Callable<Integer> {
+        private final CountDownLatch beginLatch;
+        private final CountDownLatch continueLatch;
+        private final AtomicInteger counter;
+        private final String name;
+
+        CommonTask(String name, CountDownLatch beginLatch, CountDownLatch continueLatch, AtomicInteger counter) {
+            this.beginLatch = beginLatch == null ? new CountDownLatch(1) : beginLatch;
+            this.continueLatch = continueLatch;
+            this.counter = counter == null ? new AtomicInteger(0) : counter;
+            this.name = name;
+        }
+
+        @Override
+        public Integer call() throws Exception {
+            System.out.println(this + " > call " + name);
+            beginLatch.countDown();
+            int result = continueLatch == null || continueLatch.await(TIMEOUT_NS * 5, TimeUnit.NANOSECONDS) ? counter.incrementAndGet() : -1;
+            System.out.println(this + " < call " + name + " " + result);
+            return result;
+        }
+    }
+
+    // Verify that tasks expedited by policy executors are executed ahead of normal tasks.
+    @Test
+    public void testExpedite() throws Exception {
+        ExecutorServiceImpl globalExecutor = new ExecutorServiceImpl();
+        Map<String, Object> globalExecutorConfig = new HashMap<String, Object>();
+        globalExecutorConfig.put("coreThreads", 1);
+        globalExecutorConfig.put("maxThreads", 1);
+        globalExecutorConfig.put("keepAlive", TimeUnit.MINUTES.toMillis(5));
+        globalExecutor.activate(globalExecutorConfig);
+        globalExecutor.threadPoolController.deactivate(); // disable autonomic tuning to guarantee maxThreads stays the same
+
+        ConcurrentHashMap<String, PolicyExecutorImpl> policyExecutors = new ConcurrentHashMap<String, PolicyExecutorImpl>();
+
+        PolicyExecutor executor0 = new PolicyExecutorImpl(globalExecutor, "testExpedite-0", policyExecutors).expedite(0).maxConcurrency(4).maxQueueSize(3);
+        PolicyExecutor executor2 = new PolicyExecutorImpl(globalExecutor, "testExpedite-2", policyExecutors).expedite(2).maxConcurrency(4).maxQueueSize(4);
+
+        // Share a counter between all tasks to record the order in which they run
+        AtomicInteger sharedCounter = new AtomicInteger(0);
+
+        // Use up the single thread in the global pool
+        CountDownLatch blockerStartedLatch = new CountDownLatch(1);
+        CountDownLatch blockerLatch = new CountDownLatch(1);
+        Future<Integer> future0a = executor0.submit(new CommonTask("blocker", blockerStartedLatch, blockerLatch, sharedCounter));
+        assertTrue(blockerStartedLatch.await(TIMEOUT_NS, TimeUnit.NANOSECONDS));
+
+        // Submit 3 non-expedited tasks that will be blocked from running
+        Future<Integer> future0b = executor0.submit(new CommonTask("0b", null, null, sharedCounter));
+        Future<Integer> future0c = executor0.submit(new CommonTask("0c", null, null, sharedCounter));
+        Future<Integer> future0d = executor0.submit(new CommonTask("0d", null, null, sharedCounter));
+
+        // Submit 4 tasks, the first 2 of which are expedited
+        // Note that when the expedited requests complete, current implementation does not replace the 2 non-expedited requests with expedited ones,
+        // as would have been allowed per the expedite setting. We could change this to be more precise.
+        Future<Integer> future2a = executor2.submit(new CommonTask("2a", null, null, sharedCounter));
+        Future<Integer> future2b = executor2.submit(new CommonTask("2b", null, null, sharedCounter));
+        Future<Integer> future2c = executor2.submit(new CommonTask("2c", null, null, sharedCounter));
+        Future<Integer> future2d = executor2.submit(new CommonTask("2d", null, null, sharedCounter));
+
+        // Release the blocker, allowing all tasks to run
+        blockerLatch.countDown();
+
+        // Verify the ordering (first the blocker task which had started, then the 2 expedited tasks, then the remaining tasks in order of submit)
+        assertEquals(Integer.valueOf(1), future0a.get(TIMEOUT_NS, TimeUnit.NANOSECONDS));
+        assertEquals(Integer.valueOf(2), future2a.get(TIMEOUT_NS, TimeUnit.NANOSECONDS)); // expedited
+        assertEquals(Integer.valueOf(3), future2b.get(TIMEOUT_NS, TimeUnit.NANOSECONDS)); // expedited
+        assertEquals(Integer.valueOf(4), future0b.get(TIMEOUT_NS, TimeUnit.NANOSECONDS));
+        assertEquals(Integer.valueOf(5), future0c.get(TIMEOUT_NS, TimeUnit.NANOSECONDS));
+        assertEquals(Integer.valueOf(6), future0d.get(TIMEOUT_NS, TimeUnit.NANOSECONDS));
+        assertEquals(Integer.valueOf(7), future2c.get(TIMEOUT_NS, TimeUnit.NANOSECONDS)); // not expedited per current impl
+        assertEquals(Integer.valueOf(8), future2d.get(TIMEOUT_NS, TimeUnit.NANOSECONDS)); // not expedited per current impl
+
+        assertEquals(Collections.emptyList(), executor0.shutdownNow());
+        assertEquals(Collections.emptyList(), executor2.shutdownNow());
+        globalExecutor.deactivate(0);
+    }
+
+    // Verify that expedite can be -1 (unlimited) but otherwise not negative or greater than maximum concurrency,
     // except where maximum concurrency is -1 (unlimited).
     @Test
-    public void testCoreConcurrencyConfiguration() {
-        PolicyExecutor executor = provider.create("testCoreConcurrencyConfiguration");
+    public void testExpediteConfiguration() {
+        PolicyExecutor executor = provider.create("testExpediteConfiguration");
 
         try {
             executor.expedite(-2);
-            fail("Should reject negative core concurrency.");
+            fail("Should reject negative expedite.");
         } catch (IllegalArgumentException x) {
             if (!x.getMessage().contains("-2"))
                 throw x;
@@ -46,7 +144,7 @@ public class PolicyExecutorTest {
         executor.maxConcurrency(10);
         try {
             executor.expedite(12);
-            fail("Should reject core concurrency set greater than max concurrency.");
+            fail("Should reject expedite set greater than max concurrency.");
         } catch (IllegalArgumentException x) {
             if (!x.getMessage().contains("12"))
                 throw x;
@@ -55,7 +153,7 @@ public class PolicyExecutorTest {
         executor.expedite(10);
         try {
             executor.maxConcurrency(5);
-            fail("Should reject max concurrency set less than core concurrency.");
+            fail("Should reject max concurrency set less than expedite.");
         } catch (IllegalArgumentException x) {
             if (!x.getMessage().contains("5"))
                 throw x;
