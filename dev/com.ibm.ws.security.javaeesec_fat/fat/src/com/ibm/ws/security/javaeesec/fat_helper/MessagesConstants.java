4b1bd47c871e4954cdcd6659ed1550572991015e
WILALBERTO RODRIGUEZ <wrodrig@us.ibm.com>
2017-11-14 13:13:55 -0600

    Changes and Merges

--- dev/com.ibm.ws.security.javaeesec_fat/fat/src/com/ibm/ws/security/javaeesec/fat_helper/MessagesConstants.java
+++ dev/com.ibm.ws.security.javaeesec_fat/fat/src/com/ibm/ws/security/javaeesec/fat_helper/MessagesConstants.java
@@ -1,14 +1,13 @@
-/*
-* IBM Confidential
-*
-* OCO Source Materials
-*
-* WLP Copyright IBM Corp. 2017
-*
-* The source code for this program is not published or otherwise divested 
-* of its trade secrets, irrespective of what has been deposited with the 
-* U.S. Copyright Office.
-*/
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package com.ibm.ws.security.javaeesec.fat_helper;
 
 /**
@@ -16,7 +15,7 @@ package com.ibm.ws.security.javaeesec.fat_helper;
  */
 public class MessagesConstants {
 
- // Values to be verified in messages
+    // Values to be verified in messages
     protected static final String MSG_JASPI_AUTHENTICATION_FAILED = "CWWKS1652A:.*";
     protected static final String PROVIDER_AUTHENTICATION_FAILED = "Invalid user or password";
     protected static final String MSG_AUTHORIZATION_FAILED = "CWWKS9104A:.*";
