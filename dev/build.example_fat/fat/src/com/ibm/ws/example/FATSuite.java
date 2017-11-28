773708b27280c6c25e0e8f78e5a8712141ffd96e
Andrew Guibert <andy.guibert@gmail.com>
2017-11-28 00:17:27 -0600

    Demonstrate how to use the RepeatTests rule in the example FAT

--- dev/build.example_fat/fat/src/com/ibm/ws/example/FATSuite.java
+++ dev/build.example_fat/fat/src/com/ibm/ws/example/FATSuite.java
@@ -10,14 +10,25 @@
  *******************************************************************************/
 package com.ibm.ws.example;
 
+import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+
+import componenttest.rules.FeatureReplacementAction;
+import componenttest.rules.RepeatTests;
 
 @RunWith(Suite.class)
 @SuiteClasses({
                 SimpleTest.class,
 })
 public class FATSuite {
+
+    // Using the RepeatTests @ClassRule will cause all tests to be run twice.
+    // First without any modifications, then again with all features upgraded to their EE8 equivalents.
+    @ClassRule
+    public static RepeatTests r = new RepeatTests()
+                    .withoutModification()
+                    .with(FeatureReplacementAction.EE8_FEATURES);
 
 }
