be0306064f493ce88b66f1a81836ae36dd5dab63
Bruce Tiffany <btiffany@us.ibm.com>
2017-12-18 14:39:17 -0600

    add typ header

--- dev/com.ibm.ws.security.jwt/src/com/ibm/ws/security/jwt/utils/JwsSigner.java
+++ dev/com.ibm.ws.security.jwt/src/com/ibm/ws/security/jwt/utils/JwsSigner.java
@@ -25,74 +25,79 @@ import com.ibm.ws.security.jwt.internal.JwtTokenException;
  */
 public class JwsSigner {
 
-    // private static TraceComponent tc = Tr.register(JwsSigner.class,
-    // TraceConstants.TRACE_GROUP, TraceConstants.MESSAGE_BUNDLE);
-    // set org.jose4j.jws.default-allow-none to true to behave the same as old
-    // jwt
-    // allow signatureAlgorithme as none
-    static {
-        AccessController.doPrivileged(new PrivilegedAction<String>() {
-            @Override
-            public String run() {
-                return System.setProperty("org.jose4j.jws.default-allow-none", "true");
-            }
-        });
-    };
+	// private static TraceComponent tc = Tr.register(JwsSigner.class,
+	// TraceConstants.TRACE_GROUP, TraceConstants.MESSAGE_BUNDLE);
+	// set org.jose4j.jws.default-allow-none to true to behave the same as old
+	// jwt
+	// allow signatureAlgorithme as none
+	static {
+		AccessController.doPrivileged(new PrivilegedAction<String>() {
+			@Override
+			public String run() {
+				return System.setProperty("org.jose4j.jws.default-allow-none", "true");
+			}
+		});
+	};
 
-    @FFDCIgnore({ Exception.class })
-    public static String getSignedJwt(JwtClaims claims, JwtData jwtData) throws JwtTokenException {
-        String jwt = null;
-        // jwtData.getConfig();
+	@FFDCIgnore({ Exception.class })
+	public static String getSignedJwt(JwtClaims claims, JwtData jwtData) throws JwtTokenException {
+		String jwt = null;
+		// jwtData.getConfig();
 
-        JsonWebSignature jws = new JsonWebSignature();
+		JsonWebSignature jws = new JsonWebSignature();
 
-        // The payload of the JWS is JSON content of the JWT Claims
-        jws.setPayload(claims.toJson());
+		// The payload of the JWS is JSON content of the JWT Claims
+		jws.setPayload(claims.toJson());
 
-        Key key = jwtData.getSigningKey();
-        if (key == null && !"none".equals(jwtData.getSignatureAlgorithm())) {
-            throw jwtData.getNoKeyException();
-        }
-        // The JWT is signed using the private key
-        jws.setKey(key);
+		Key key = jwtData.getSigningKey();
+		if (key == null && !"none".equals(jwtData.getSignatureAlgorithm())) {
+			throw jwtData.getNoKeyException();
+		}
+		// The JWT is signed using the private key
+		jws.setKey(key);
 
-        // Set the Key ID (kid) header because it's just the polite thing to do.
-        // We only have one key in this example but a using a Key ID helps
-        // facilitate a smooth key rollover process
-        String keyId = jwtData.getKeyID();
-        if (keyId != null) {
-            jws.setKeyIdHeaderValue(keyId);
-        }
+		// Set the Key ID (kid) header because it's just the polite thing to do.
+		// We only have one key in this example but a using a Key ID helps
+		// facilitate a smooth key rollover process
+		String keyId = jwtData.getKeyID();
+		if (keyId != null) {
+			jws.setKeyIdHeaderValue(keyId);
+		}
 
-        // Set the signature algorithm on the JWT/JWS that will integrity
-        // protect the claims
-        jws.setAlgorithmHeaderValue(jwtData.getSignatureAlgorithm());
-        jws.setDoKeyValidation(false);
+		// issue 1330
+		if (jwtData.isJwt()) {
+			jws.setHeader("typ", "JWT");
+		}
 
-        // Sign the JWS and produce the compact serialization or the complete
-        // JWT/JWS
-        // representation, which is a string consisting of three dot ('.')
-        // separated
-        // base64url-encoded parts in the form Header.Payload.Signature
-        // If you wanted to encrypt it, you can simply set this jwt as the
-        // payload
-        // of a JsonWebEncryption object and set the cty (Content Type) header
-        // to "jwt".
+		// Set the signature algorithm on the JWT/JWS that will integrity
+		// protect the claims
+		jws.setAlgorithmHeaderValue(jwtData.getSignatureAlgorithm());
+		jws.setDoKeyValidation(false);
 
-        try {
-            jwt = jws.getCompactSerialization();
-        } catch (Exception e) {
-            // Tr.error(tc, "JWT_CANNOT_GENERATE_JWT", objs);
-            throw new JwtTokenException(e.getLocalizedMessage(), e);
-            //            throw new JwtTokenException(/*
-            //                                         * Tr.formatMessage(tc,
-            //                                         * "JWT_CANNOT_GENERATE_JWT", objs),
-            //                                         */"Can not generate JWT", e);
-        }
-        // if (tc.isDebugEnabled()) {
-        // Tr.debug(tc, "JWT=", jwt);
-        // }
-        return jwt;
-    }
+		// Sign the JWS and produce the compact serialization or the complete
+		// JWT/JWS
+		// representation, which is a string consisting of three dot ('.')
+		// separated
+		// base64url-encoded parts in the form Header.Payload.Signature
+		// If you wanted to encrypt it, you can simply set this jwt as the
+		// payload
+		// of a JsonWebEncryption object and set the cty (Content Type) header
+		// to "jwt".
+
+		try {
+			jwt = jws.getCompactSerialization();
+		} catch (Exception e) {
+			// Tr.error(tc, "JWT_CANNOT_GENERATE_JWT", objs);
+			throw new JwtTokenException(e.getLocalizedMessage(), e);
+			// throw new JwtTokenException(/*
+			// * Tr.formatMessage(tc,
+			// * "JWT_CANNOT_GENERATE_JWT", objs),
+			// */"Can not generate JWT", e);
+		}
+		// if (tc.isDebugEnabled()) {
+		// Tr.debug(tc, "JWT=", jwt);
+		// }
+		return jwt;
+	}
 
 }
