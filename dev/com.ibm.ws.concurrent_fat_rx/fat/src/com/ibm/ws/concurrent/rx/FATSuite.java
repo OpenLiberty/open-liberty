dfd2f65caadc1bafafbbd9b4cea65a2e5bc0478e
Nathan Rauh <nathan.rauh@us.ibm.com>
2017-12-18 10:04:08 -0600

    Issue #1333 code review comments - removal of unnecessary files and c ode

--- dev/com.ibm.ws.concurrent_fat_rx/fat/src/com/ibm/ws/concurrent/rx/FATSuite.java
+++ dev/com.ibm.ws.concurrent_fat_rx/fat/src/com/ibm/ws/concurrent/rx/FATSuite.java
@@ -14,11 +14,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import componenttest.custom.junit.runner.AlwaysPassesTest;
-
 @RunWith(Suite.class)
 @SuiteClasses({
-                AlwaysPassesTest.class,
                 ConcurrentRxTest.class,
 })
 public class FATSuite {
