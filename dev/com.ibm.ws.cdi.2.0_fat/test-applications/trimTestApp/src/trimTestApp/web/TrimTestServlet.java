066a438188c4b0db7bfb69ec3fbbf24d354afa64
Jon Hawkes <jon.hawkes@uk.ibm.com>
2017-12-20 10:06:45 +0000

    Commented out test pending issue 1372

--- dev/com.ibm.ws.cdi.2.0_fat/test-applications/trimTestApp/src/trimTestApp/web/TrimTestServlet.java
+++ dev/com.ibm.ws.cdi.2.0_fat/test-applications/trimTestApp/src/trimTestApp/web/TrimTestServlet.java
@@ -41,7 +41,8 @@ public class TrimTestServlet extends FATServlet {
         }
     }
 
-    @Test
+//  Pending issue https://github.com/OpenLiberty/open-liberty/issues/1372
+//  @Test
     public void testPATObservers() {
         assertTrue("ProcessAnnotatedType<ThingOne> not observed", PATObserver.observed.contains(ThingOne.class.getName()));
         assertTrue("ProcessAnnotatedType<ThingTwo> not observed", PATObserver.observed.contains(ThingTwo.class.getName()));
