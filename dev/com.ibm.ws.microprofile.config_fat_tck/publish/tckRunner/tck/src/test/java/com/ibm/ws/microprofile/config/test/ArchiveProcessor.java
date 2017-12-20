d18d7b2e0be90b382da0582ba8a13aaa7f42124c
Gordon Hutchison <gordon.hutchison@gmail.com>
2017-12-20 14:49:58 +0000

    replace tabs with spaces
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>

--- dev/com.ibm.ws.microprofile.config_fat_tck/publish/tckRunner/tck/src/test/java/com/ibm/ws/microprofile/config/test/ArchiveProcessor.java
+++ dev/com.ibm.ws.microprofile.config_fat_tck/publish/tckRunner/tck/src/test/java/com/ibm/ws/microprofile/config/test/ArchiveProcessor.java
@@ -12,15 +12,15 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
  */
 public class ArchiveProcessor implements ApplicationArchiveProcessor {
 
-	/* (non-Javadoc)
-	 * @see org.jboss.arquillian.container.test.spi.client.deployment.ApplicationArchiveProcessor#process(org.jboss.shrinkwrap.api.Archive, org.jboss.arquillian.test.spi.TestClass)
-	 */
-	@Override
-	public void process(Archive<?> applicationArchive, TestClass testClass) {
-		if (applicationArchive instanceof WebArchive) {
-			File hamcrest = new File("../../../lib/hamcrest-all-1.3.jar");
-			System.out.println("WLP: Adding Jar:" + hamcrest.getAbsolutePath() + " to " + applicationArchive.getName());
-			((WebArchive) applicationArchive).addAsLibraries(hamcrest);
-		}
-	}
+    /* (non-Javadoc)
+     * @see org.jboss.arquillian.container.test.spi.client.deployment.ApplicationArchiveProcessor#process(org.jboss.shrinkwrap.api.Archive, org.jboss.arquillian.test.spi.TestClass)
+     */
+    @Override
+    public void process(Archive<?> applicationArchive, TestClass testClass) {
+        if (applicationArchive instanceof WebArchive) {
+            File hamcrest = new File("../../../lib/hamcrest-all-1.3.jar");
+            System.out.println("WLP: Adding Jar:" + hamcrest.getAbsolutePath() + " to " + applicationArchive.getName());
+            ((WebArchive) applicationArchive).addAsLibraries(hamcrest);
+        }
+    }
 }
--- dev/com.ibm.ws.microprofile.config_fat_tck/publish/tckRunner/tck/src/test/java/com/ibm/ws/microprofile/config/test/ArquillianLoadableExtension.java
+++ dev/com.ibm.ws.microprofile.config_fat_tck/publish/tckRunner/tck/src/test/java/com/ibm/ws/microprofile/config/test/ArquillianLoadableExtension.java
@@ -8,9 +8,9 @@ import org.jboss.arquillian.core.spi.LoadableExtension;
  * to add any libraries that are not in the default Liberty environment
  */
 public class ArquillianLoadableExtension implements LoadableExtension {
-	@Override
-	public void register(ExtensionBuilder extensionBuilder) {
-		System.out.println("WLP: Adding Extension com.ibm.ws.microprofile.config.test.ArchiveProcessor");
-		extensionBuilder.service(ApplicationArchiveProcessor.class, ArchiveProcessor.class);
-	}
+    @Override
+    public void register(ExtensionBuilder extensionBuilder) {
+        System.out.println("WLP: Adding Extension com.ibm.ws.microprofile.config.test.ArchiveProcessor");
+        extensionBuilder.service(ApplicationArchiveProcessor.class, ArchiveProcessor.class);
+    }
 }
