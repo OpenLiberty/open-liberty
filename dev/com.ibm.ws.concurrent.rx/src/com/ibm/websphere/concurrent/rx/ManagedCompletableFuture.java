f6d16adf501af65cd95b230fa3e420a85fb4c42d
Nathan Rauh <nathan.rauh@us.ibm.com>
2017-12-21 16:47:46 -0600

    Issue #1333 exceptionally for managed completable future

--- dev/com.ibm.ws.concurrent.rx/src/com/ibm/websphere/concurrent/rx/ManagedCompletableFuture.java
+++ dev/com.ibm.ws.concurrent.rx/src/com/ibm/websphere/concurrent/rx/ManagedCompletableFuture.java
@@ -295,9 +295,19 @@ public class ManagedCompletableFuture<T> extends CompletableFuture<T> {
      * @see java.util.concurrent.CompletionStage#exceptionally(java.util.function.Function)
      */
     @Override
-    public ManagedCompletableFuture<T> exceptionally(Function<Throwable, ? extends T> fn) {
-        // TODO contextualize fn and other steps
-        return new ManagedCompletableFuture<T>(completableFuture.exceptionally(fn), defaultExecutor);
+    public ManagedCompletableFuture<T> exceptionally(Function<Throwable, ? extends T> action) {
+        // Reject ManagedTask so that we have the flexibility to decide later how to handle ManagedTaskListener and execution properties
+        if (action instanceof ManagedTask)
+            throw new IllegalArgumentException(ManagedTask.class.getName());
+
+        WSContextService contextSvc = defaultExecutor.getContextService();
+
+        @SuppressWarnings("unchecked")
+        ThreadContextDescriptor contextDescriptor = contextSvc.captureThreadContext(XPROPS_SUSPEND_TRAN);
+        action = new ContextualFunction<>(contextDescriptor, action);
+
+        CompletableFuture<T> dependentStage = completableFuture.exceptionally(action);
+        return new ManagedCompletableFuture<T>(dependentStage, defaultExecutor);
     }
 
     /**
--- dev/com.ibm.ws.concurrent_fat_rx/test-applications/concurrentrxfat/src/web/ConcurrentRxTestServlet.java
+++ dev/com.ibm.ws.concurrent_fat_rx/test-applications/concurrentrxfat/src/web/ConcurrentRxTestServlet.java
@@ -26,6 +26,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -233,6 +234,84 @@ public class ConcurrentRxTestServlet extends FATServlet {
         assertFalse(cf2.isCompletedExceptionally());
         assertTrue(cf1.isDone());
         assertFalse(cf1.isCompletedExceptionally());
+    }
+
+    /**
+     * Verify that dependent stage for exceptionally is invoked in the event of an exception during the prior stage
+     * and runs with context captured from the thread that creates the dependent stage.
+     * Verify that dependent stage for exceptionally is not invoked when prior stage is successful.
+     */
+    @Test
+    public void testExceptionally() throws Exception {
+        AtomicInteger count = new AtomicInteger();
+        LinkedBlockingQueue<Object> results = new LinkedBlockingQueue<Object>();
+        String currentThreadName = Thread.currentThread().getName();
+
+        final Function<Throwable, Executor> lookup = (previousFailure) -> {
+            System.out.println("> lookup #" + count.incrementAndGet() + " from testExceptionally");
+            if (previousFailure != null)
+                results.add(previousFailure);
+            results.add(Thread.currentThread().getName());
+            try {
+                ManagedExecutorService result = InitialContext.doLookup("java:comp/env/executorRef");
+                System.out.println("< lookup: " + result);
+                return result;
+            } catch (NamingException x) {
+                System.out.println("< lookup failed");
+                x.printStackTrace(System.out);
+                throw new CompletionException(x);
+            }
+        };
+
+        String threadName;
+        Object previousFailure;
+
+        // Verify that exceptionally is skipped when no exception is raised by prior stage
+
+        CompletableFuture<?> cf1 = ManagedCompletableFuture
+                        .completedFuture((Throwable) null)
+                        .thenApplyAsync(lookup) // expect lookup to succeed because managed executor transfers thread context from the servlet
+                        .exceptionally(lookup); // should not be invoked due to lack of any failure in prior stage
+
+        assertTrue(cf1.toString(), cf1 instanceof ManagedCompletableFuture);
+
+        // thenApplyAsync on default execution facility
+        assertNotNull(threadName = results.poll(TIMEOUT_NS, TimeUnit.NANOSECONDS).toString());
+        assertTrue(threadName, threadName.startsWith("Default Executor-thread-")); // must run on Liberty global thread pool
+        assertNotSame(currentThreadName, threadName); // cannot be the servlet thread because operation is async
+
+        assertEquals(defaultManagedExecutor, cf1.get(TIMEOUT_NS, TimeUnit.NANOSECONDS));
+
+        assertEquals(1, count.get()); // lookup function only ran once
+
+        // Verify that exceptionally is invoked when exception is raised by prior stage
+
+        CompletableFuture<?> cf2 = ManagedCompletableFuture
+                        .completedFuture((Throwable) null)
+                        .thenApplyAsync(lookup, testThreads) // expect lookup to fail without the context of the servlet thread
+                        .exceptionally(lookup);
+
+        assertTrue(cf2.toString(), cf2 instanceof ManagedCompletableFuture);
+
+        // thenApplyAsync on unmanaged executor
+        assertNotNull(threadName = results.poll(TIMEOUT_NS, TimeUnit.NANOSECONDS).toString());
+        assertFalse(threadName, threadName.startsWith("Default Executor-thread-")); // must run async on unmanaged thread
+
+        // exceptionally on unmanaged thread or servlet thread
+        assertNotNull(previousFailure = results.poll(TIMEOUT_NS, TimeUnit.NANOSECONDS));
+        if (previousFailure instanceof CompletionException && ((CompletionException) previousFailure).getCause() instanceof NamingException)
+            ; // pass
+        else if (previousFailure instanceof Throwable)
+            throw new Exception((Throwable) previousFailure);
+        else
+            fail("Unexpected value supplied to function as previous failure: " + previousFailure);
+
+        String previousThreadName = threadName;
+        assertNotNull(threadName = results.poll(TIMEOUT_NS, TimeUnit.NANOSECONDS).toString());
+        assertTrue(threadName, previousThreadName.equals(threadName) || currentThreadName.equals(threadName)); // must run on same unmanaged thread or on servlet thread
+
+        assertEquals(defaultManagedExecutor, cf2.get(TIMEOUT_NS, TimeUnit.NANOSECONDS));
+        assertEquals(3, count.get()); // two additional executions of the lookup function
     }
 
     /**
