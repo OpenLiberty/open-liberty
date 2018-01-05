adfb81b2a5b903df3617fdbe7e9b9e02add133eb
Jim Krueger <jim_krueger@us.ibm.com>
2018-01-05 16:20:49 -0600

    Change to address NPE that may occur when multiple cdi-injected servlets are specified in the web.xml for a JAXRS application with load-on-startup specified

--- dev/com.ibm.ws.jaxrs.2.0.common/src/com/ibm/ws/jaxrs20/utils/CustomizerUtils.java
+++ dev/com.ibm.ws.jaxrs.2.0.common/src/com/ibm/ws/jaxrs20/utils/CustomizerUtils.java
@@ -10,7 +10,11 @@
  *******************************************************************************/
 package com.ibm.ws.jaxrs20.utils;
 
+import com.ibm.websphere.csi.J2EEName;
 import com.ibm.ws.jaxrs20.api.JaxRsFactoryBeanCustomizer;
+import com.ibm.ws.runtime.metadata.ComponentMetaData;
+import com.ibm.ws.runtime.metadata.ModuleMetaData;
+import com.ibm.ws.threadContext.ComponentMetaDataAccessorImpl;
 
 /**
  *
@@ -18,6 +22,12 @@ import com.ibm.ws.jaxrs20.api.JaxRsFactoryBeanCustomizer;
 public class CustomizerUtils {
 
     public static String createCustomizerKey(JaxRsFactoryBeanCustomizer customizer) {
-        return Integer.toString(customizer.hashCode());// + ":" + Thread.currentThread().getId();
+        J2EEName j2eeName = null;
+        ComponentMetaData cmd = ComponentMetaDataAccessorImpl.getComponentMetaDataAccessor().getComponentMetaData();
+        if (cmd != null) {
+            ModuleMetaData mmd = cmd.getModuleMetaData();
+            j2eeName = mmd.getJ2EEName();
+        }
+        return Integer.toString(customizer.hashCode()) + j2eeName;// + ":" + Thread.currentThread().getId();
     }
 }
