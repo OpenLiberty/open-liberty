b62969b42c891d05f2e043d1238c11919777592e
Levin Noronha <levin.noro@gmail.com>
2018-01-18 10:21:46 -0500

    Make echo() send events to LogSource if json configured and fix copyright dates

--- dev/com.ibm.ws.logging/src/com/ibm/ws/logging/internal/impl/JsonTraceService.java
+++ dev/com.ibm.ws.logging/src/com/ibm/ws/logging/internal/impl/JsonTraceService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 IBM Corporation and others.
+ * Copyright (c) 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -216,6 +216,13 @@ public class JsonTraceService extends BaseTraceService {
 
         if (!isMessageJsonConfigured) {
             messagesLog.writeRecord(message);
+        } else {
+            if (logSource != null) {
+                String formattedMsg = formatter.formatMessage(logRecord);
+                String formattedVerboseMsg = formatter.formatVerboseMessage(logRecord, formattedMsg);
+                RoutedMessage routedMessage = new RoutedMessageImpl(formattedMsg, formattedVerboseMsg, message, logRecord);
+                logSource.publish(routedMessage);
+            }
         }
 
         invokeMessageRouters(new RoutedMessageImpl(logRecord.getMessage(), logRecord.getMessage(), message, logRecord));
