683a36f1eceac4fc2428606ad270c8360ed760a2
Neil Young <neil_young@uk.ibm.com>
2018-01-22 16:03:25 +0000

    review comments

--- dev/com.ibm.ws.cdi.2.0_fat/test-applications/secureAsyncEventsApp/src/secureAsyncEventsApp/web/SecureAsyncEventsServlet.java
+++ dev/com.ibm.ws.cdi.2.0_fat/test-applications/secureAsyncEventsApp/src/secureAsyncEventsApp/web/SecureAsyncEventsServlet.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 IBM Corporation and others.
+ * Copyright (c) 2017, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -42,7 +42,6 @@ import componenttest.app.FATServlet;
 
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns = "/secureasyncevents")
-// @RunAs("student")
 public class SecureAsyncEventsServlet extends FATServlet {
     @Inject
     private MultiThreadCDIBean multiThreadCDIBean;
