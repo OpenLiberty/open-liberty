c15c482f762c87b66f799a54b52dab532e8e7a24
Andrew Guibert <andy.guibert@gmail.com>
2018-01-23 12:19:18 -0600

    Allow beanval 2.0 CDI Extension to fail gracefully

--- dev/com.ibm.ws.beanvalidation.v20_fat/test-applications/customBvalProviderApp/src/bval/v20/customprovider/BasicCDIBean.java
+++ dev/com.ibm.ws.beanvalidation.v20_fat/test-applications/customBvalProviderApp/src/bval/v20/customprovider/BasicCDIBean.java
@@ -1,14 +1,24 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2018 IBM Corporation and others.
+ * Copyright (c) 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- * IBM Corporation - initial API and implementation
+ *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-@TraceOptions(traceGroup = "BeanValidation", messageBundle = "com.ibm.ws.beanvalidation.v20.cdi.internal.BVNLSMessages")
-package com.ibm.ws.beanvalidation.v20.cdi.internal;
+package bval.v20.customprovider;
 
-import com.ibm.websphere.ras.annotation.TraceOptions;
+import javax.enterprise.context.RequestScoped;
+
+@RequestScoped
+public class BasicCDIBean {
+
+    // Put this bean in the app to enable CDI scanning on this app module
+
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+
+}
