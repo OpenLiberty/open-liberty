6a58ce3498d4b28df68aac2c4b73d54c4503caff
Bruce Tiffany <btiffany@us.ibm.com>
2018-01-24 10:48:21 -0600

    add project skeleton

---
+++ dev/com.ibm.ws.security.jwtsso/src/com/ibm/ws/security/jwtsso/config/JwtSsoConfig.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.security.jwtsso.config;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.List;
+
+import com.ibm.ws.security.common.jwk.interfaces.JSONWebKey;
+
+public interface JwtSsoConfig {
+
+  //todo: implement me
+
+}
