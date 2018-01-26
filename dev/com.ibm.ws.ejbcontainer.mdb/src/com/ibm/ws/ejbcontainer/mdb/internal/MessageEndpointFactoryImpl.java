55a8d7ecd6a51bb8fcdbc42796f76b2645e17e9d
Mark Swatosh <mjswatos@us.ibm.com>
2018-01-26 13:36:02 -0600

    Issue #325: Redesign of MessageEndpointFactory PauseableComponent name

--- dev/com.ibm.ws.ejbcontainer.mdb/src/com/ibm/ws/ejbcontainer/mdb/internal/MessageEndpointFactoryImpl.java
+++ dev/com.ibm.ws.ejbcontainer.mdb/src/com/ibm/ws/ejbcontainer/mdb/internal/MessageEndpointFactoryImpl.java
@@ -21,7 +21,6 @@ import javax.resource.spi.endpoint.MessageEndpoint;
 import javax.transaction.xa.XAResource;
 
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.Constants;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceRegistration;
 
@@ -75,8 +74,6 @@ public class MessageEndpointFactoryImpl extends BaseMessageEndpointFactory imple
      */
     private Object activationSpec;
 
-    private volatile String name = null;
-
     private final MDBRuntimeImpl mdbRuntime;
     private String activationSvcId;
 
@@ -118,13 +115,8 @@ public class MessageEndpointFactoryImpl extends BaseMessageEndpointFactory imple
         activationSvcId = bmd.ivActivationSpecJndiName;
 
         //Register this instance as a PauseableComponent
-
-        name = bmd.getJavaEELogicalName();
-
         BundleContext bundleContext = FrameworkUtil.getBundle(getClass()).getBundleContext();
         registration = bundleContext.registerService(PauseableComponent.class, this, null);
-        if (name == null)
-            name = "messageEndpoint-" + registration.getReference().getProperty(Constants.SERVICE_ID);
 
     }
 
@@ -466,7 +458,7 @@ public class MessageEndpointFactoryImpl extends BaseMessageEndpointFactory imple
      */
     @Override
     public String getName() {
-        return name;
+        return getActivationName();
     }
 
     /*
@@ -526,9 +518,4 @@ public class MessageEndpointFactoryImpl extends BaseMessageEndpointFactory imple
         }
 
     }
-
-    public String getMessageEndpointTarget() {
-        return getName();
-    }
-
 }
