4a4e42f696f2088195d8cdac3a5561d61a8e84b9
Andy McCright <j.andrew.mccright@gmail.com>
2018-01-29 17:11:13 -0600

    Ensure multiple MP Rest Client impls can be injected into the same app
    and resource.  This includes a couple runtime changes and a test that
    installs the same app twice - and the app injects multiple instances of
    the same Rest client and another one just for good measure.

---
+++ dev/com.ibm.ws.microprofile.rest.client_fat/fat/src/com/ibm/ws/microprofile/rest/client/fat/MultiClientCdiTest.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.microprofile.rest.client.fat;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import com.ibm.websphere.simplicity.ShrinkHelper;
+
+import componenttest.annotation.Server;
+import componenttest.annotation.TestServlet;
+import componenttest.custom.junit.runner.FATRunner;
+import componenttest.topology.impl.LibertyServer;
+import componenttest.topology.utils.FATServletClient;
+import mpRestClient10.multiClientCdi.MultiClientCdiTestServlet;
+
+@RunWith(FATRunner.class)
+public class MultiClientCdiTest extends FATServletClient {
+
+    private static final String appName = "multiClientCdiApp";
+
+    @Server("mpRestClient10.multi.client.cdi")
+    @TestServlet(servlet = MultiClientCdiTestServlet.class, contextRoot = appName)
+    public static LibertyServer server;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        ShrinkHelper.defaultDropinApp(server, appName, "mpRestClient10.multiClientCdi");
+        ShrinkHelper.defaultDropinApp(server, appName + "2", "mpRestClient10.multiClientCdi"); // install app twice
+        server.startServer();
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        server.stopServer();
+    }
+}
