726eaf5af1dca3006f107b88e4b93ef7e9b1b447
Nathan Rauh <nathan.rauh@us.ibm.com>
2018-01-30 08:53:57 -0600

    Issue #1825 copy insertSession based on DatabaseHashMap

--- dev/com.ibm.ws.session.cache/src/com/ibm/ws/session/store/cache/CacheHashMap.java
+++ dev/com.ibm.ws.session.cache/src/com/ibm/ws/session/store/cache/CacheHashMap.java
@@ -11,10 +11,20 @@
 
 package com.ibm.ws.session.store.cache;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.sql.Blob;
+import java.util.logging.Level;
 
+import javax.cache.CacheException;
+
+import com.ibm.websphere.ras.Tr;
+import com.ibm.websphere.ras.TraceComponent;
+import com.ibm.ws.ffdc.FFDCFilter;
 import com.ibm.ws.session.SessionManagerConfig;
+import com.ibm.ws.session.SessionStatistics;
 import com.ibm.ws.session.store.common.BackedHashMap;
 import com.ibm.ws.session.store.common.BackedSession;
 import com.ibm.wsspi.session.IStore;
@@ -25,6 +35,14 @@ import com.ibm.wsspi.session.IStore;
  */
 public class CacheHashMap extends BackedHashMap {
     private static final long serialVersionUID = 1L; // not serializable, rejects writeObject
+
+    private static final TraceComponent tc = Tr.register(CacheHashMap.class);
+
+    /**
+     * Array indices (and size) for Object array values that are stored in the cache.
+     * TODO this is a temporary approach that will almost certainly be replaced
+     */
+    private static final int LISTENER_COUNT = 0, LAST_ACCESS = 1, CREATION_TIME = 2, MAX_INACTIVE_TIME = 3, USER_NAME = 4, BYTES = 5, SIZE = 6;
 
     CacheStoreService cacheStoreService;
 
@@ -43,11 +61,79 @@ public class CacheHashMap extends BackedHashMap {
     }
 
     /**
+     * TODO rewrite this. For now, it is copied based on DatabaseHashMap.getValue
+     */
+    protected Object getValue(String id, BackedSession s) {
+        final boolean trace = TraceComponent.isAnyTracingEnabled();
+
+        Object tmp = null;
+
+        String key = s.getId() + '+' + id + '@' + getIStore().getId();
+        Object[] value = (Object[]) cacheStoreService.cache.get(key);
+
+        if (trace && tc.isDebugEnabled())
+            Tr.debug(this, tc, key, value);
+
+        if (value == null)
+            return null;
+
+        long startTime = System.currentTimeMillis();
+        byte[] bytes = (byte[]) value[BYTES];
+
+        if (bytes != null && bytes.length > 0) {
+            BufferedInputStream in = new BufferedInputStream(new ByteArrayInputStream(bytes));
+            try {
+                try {
+                    tmp = ((CacheStore) getIStore()).getLoader().loadObject(in);
+                } finally {
+                    in.close();
+                }
+            } catch (ClassNotFoundException | IOException x) {
+                FFDCFilter.processException(x, getClass().getName(), "96", s);
+                throw new RuntimeException(x);
+            }
+        }
+
+        SessionStatistics pmiStats = getIStore().getSessionStatistics();
+        if (pmiStats != null) {
+            pmiStats.readTimes(bytes == null ? 0 : bytes.length, System.currentTimeMillis() - startTime);
+        }
+
+        return tmp;
+    }
+
+    /**
      * @see com.ibm.ws.session.store.common.BackedHashMap#insertSession(com.ibm.ws.session.store.common.BackedSession)
      */
     @Override
     protected void insertSession(BackedSession d2) {
-        throw new UnsupportedOperationException();
+        // TODO rewrite this. For now, it is copied based on DatabaseHashMap.insertSession
+        String key = d2.getId() + '+' + d2.getId() + '@' + d2.getAppName();
+
+        listenerFlagUpdate(d2);
+
+        long tmpCreationTime = d2.getCreationTime();
+        d2.setLastWriteLastAccessTime(tmpCreationTime);
+
+        Object[] value = new Object[SIZE];
+        value[LISTENER_COUNT] = d2.listenerFlag;
+        value[LAST_ACCESS] = tmpCreationTime;
+        value[CREATION_TIME] = tmpCreationTime;
+        value[MAX_INACTIVE_TIME] = d2.getMaxInactiveInterval();
+        value[USER_NAME] = d2.getUserName();
+        //value[BYTES] = null;
+
+        if (!cacheStoreService.cache.putIfAbsent(key, value))
+            throw new IllegalStateException("Cache already contains " + key);
+
+        d2.needToInsert = false;
+
+        removeFromRecentlyInvalidatedList(d2.getId());
+
+        d2.update = null;
+        d2.userWriteHit = false;
+        d2.maxInactWriteHit = false;
+        d2.listenCntHit = false;
     }
 
     /**
