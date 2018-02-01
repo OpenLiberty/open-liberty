2d200c93a3fb0f85045bdd5d53b7a97b78c5e706
leochr <leojc@ca.ibm.com>
2018-02-01 13:41:03 -0500

    Issue #1929 - serialize reference with  instead of ref

--- dev/com.ibm.ws.microprofile.openapi/src/com/ibm/ws/microprofile/openapi/impl/core/jackson/mixin/ExtensionsMixin.java
+++ dev/com.ibm.ws.microprofile.openapi/src/com/ibm/ws/microprofile/openapi/impl/core/jackson/mixin/ExtensionsMixin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 IBM Corporation and others.
+ * Copyright (c) 2017, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,6 +16,11 @@ import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Define mix-in annotations to use for augmenting annotations that processable (serializable / deserializable) classes have.
+ * Mixing in is done when introspecting class annotations and properties.
+ * All annotations from mixinSource are taken to override annotations that target (or its supertypes) has.
+ */
 public abstract class ExtensionsMixin {
 
     @JsonAnyGetter
@@ -29,5 +34,8 @@ public abstract class ExtensionsMixin {
 
     @JsonProperty("default")
     public abstract void getDefaultValue();
+
+    @JsonProperty("$ref")
+    public abstract void getRef();
 
 }
