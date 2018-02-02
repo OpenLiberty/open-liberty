43197d0c62d11e863b8e9068c825ca0ed0f6c163
Anna Safonov <anna.safonov@ibm.com>
2018-02-02 13:28:23 -0500

    Added ExampleValidatorTest and ContactValidatorTest

--- dev/com.ibm.ws.microprofile.openapi/src/com/ibm/ws/microprofile/openapi/impl/validation/ContactValidator.java
+++ dev/com.ibm.ws.microprofile.openapi/src/com/ibm/ws/microprofile/openapi/impl/validation/ContactValidator.java
@@ -35,17 +35,19 @@ public class ContactValidator extends TypeValidator<Contact> {
     /** {@inheritDoc} */
     @Override
     public void validate(ValidationHelper helper, Context context, String key, Contact t) {
-        if (t.getUrl() != null) {
-            if (!ValidatorUtils.isValidURL(t.getUrl())) {
-                final String message = Tr.formatMessage(tc, "contactInvalidURL", t.getUrl());
-                helper.addValidationEvent(new ValidationEvent(ValidationEvent.Severity.ERROR, context.getLocation("url"), message));
+        if (t != null) {
+            if (t.getUrl() != null) {
+                if (!ValidatorUtils.isValidURL(t.getUrl())) {
+                    final String message = Tr.formatMessage(tc, "contactInvalidURL", t.getUrl());
+                    helper.addValidationEvent(new ValidationEvent(ValidationEvent.Severity.ERROR, context.getLocation("url"), message));
+                }
             }
-        }
 
-        if (t.getEmail() != null) {
-            if (!ValidatorUtils.isValidEmailAddress(t.getEmail())) {
-                final String message = Tr.formatMessage(tc, "contactInvalidEmail", t.getEmail());
-                helper.addValidationEvent(new ValidationEvent(ValidationEvent.Severity.ERROR, context.getLocation("email"), message));
+            if (t.getEmail() != null) {
+                if (!ValidatorUtils.isValidEmailAddress(t.getEmail())) {
+                    final String message = Tr.formatMessage(tc, "contactInvalidEmail", t.getEmail());
+                    helper.addValidationEvent(new ValidationEvent(ValidationEvent.Severity.ERROR, context.getLocation("email"), message));
+                }
             }
         }
     }
