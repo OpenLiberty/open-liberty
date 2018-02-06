8de48c8e2fb3dcb54a1c205936a9fafc7ca15662
Zemuel Roman <zaroman@us.ibm.com>
2018-02-06 16:28:33 -0500

    Add tracing to pushServletReference

--- dev/com.ibm.ws.webcontainer.servlet.4.0/src/com/ibm/ws/webcontainer40/osgi/webapp/WebAppDispatcherContext40.java
+++ dev/com.ibm.ws.webcontainer.servlet.4.0/src/com/ibm/ws/webcontainer40/osgi/webapp/WebAppDispatcherContext40.java
@@ -13,6 +13,9 @@ package com.ibm.ws.webcontainer40.osgi.webapp;
 import javax.servlet.http.HttpServletMapping;
 import javax.servlet.http.MappingMatch;
 
+import com.ibm.websphere.ras.Tr;
+import com.ibm.websphere.ras.TraceComponent;
+import com.ibm.ws.webcontainer.osgi.osgi.WebContainerConstants;
 import com.ibm.ws.webcontainer.osgi.webapp.WebApp;
 import com.ibm.ws.webcontainer.osgi.webapp.WebAppDispatcherContext;
 import com.ibm.wsspi.webcontainer.servlet.IExtendedRequest;
@@ -22,6 +25,8 @@ import com.ibm.wsspi.webcontainer.servlet.IServletWrapper;
  *
  */
 public class WebAppDispatcherContext40 extends WebAppDispatcherContext {
+
+    private static final TraceComponent tc = Tr.register(WebAppDispatcherContext40.class, WebContainerConstants.TR_GROUP, WebContainerConstants.NLS_PROPS);
 
     private HttpServletMapping _mapping;
     private MappingMatch _match;
@@ -59,11 +64,19 @@ public class WebAppDispatcherContext40 extends WebAppDispatcherContext {
         super.pushServletReference(wrapper);
 
         //Set the mapping to the parent's mapping if this is an include or an async dispatch
-        // or _match == null (when using a named dispatcher)
-        if (this.isInclude() || this.isAsync() || (_mapping == null && _match == null))
+        // or when using a named dispatcher (_match == null)
+        if (this.isInclude() || this.isAsync() || (_mapping == null && _match == null)) {
+            if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+                Tr.debug(tc, "This is an include, async or named dispatch. Setting the HttpServletMapping to the parent context's mapping.");
+            }
             _mapping = ((WebAppDispatcherContext40) this.getParentContext()).getServletMapping();
-        else
+
+        } else {
+            if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+                Tr.debug(tc, "Setting the HttpServletMapping to the current servlet's mapping.");
+            }
             setServletMapping();
+        }
     }
 
     @Override
