73337b20345f07db8c8f5d3cddef5ba1399588e9
Anna Safonov <anna.safonov@ibm.com>
2018-02-07 14:42:28 -0500

    Added OpenAPIValidatorTest

--- dev/com.ibm.ws.microprofile.openapi_test/test/com/ibm/ws/microprofile/openapi/validation/test/OperationValidatorTest.java
+++ dev/com.ibm.ws.microprofile.openapi_test/test/com/ibm/ws/microprofile/openapi/validation/test/OperationValidatorTest.java
@@ -10,9 +10,51 @@
  *******************************************************************************/
 package com.ibm.ws.microprofile.openapi.validation.test;
 
+import org.junit.Test;
+
+import com.ibm.ws.microprofile.openapi.impl.model.OpenAPIImpl;
+import com.ibm.ws.microprofile.openapi.impl.model.OperationImpl;
+import com.ibm.ws.microprofile.openapi.impl.model.PathItemImpl;
+import com.ibm.ws.microprofile.openapi.impl.model.PathsImpl;
+import com.ibm.ws.microprofile.openapi.impl.model.responses.APIResponseImpl;
+import com.ibm.ws.microprofile.openapi.impl.model.responses.APIResponsesImpl;
+import com.ibm.ws.microprofile.openapi.impl.validation.OASValidator;
+import com.ibm.ws.microprofile.openapi.impl.validation.OperationValidator;
+import com.ibm.ws.microprofile.openapi.test.utils.TestValidationContextHelper;
+import com.ibm.ws.microprofile.openapi.utils.OpenAPIModelWalker.Context;
+
 /**
  *
  */
 public class OperationValidatorTest {
 
+    String key = null;
+
+    @Test
+    public void testCorrectOperation() {
+        OperationValidator validator = OperationValidator.getInstance();
+        OASValidator vh = new OASValidator();
+
+        String pathNameOne = "/my-test-path-one/";
+        PathItemImpl pathItemOne = new PathItemImpl();
+
+        OperationImpl pathItemOneGet = new OperationImpl();
+
+        APIResponsesImpl responses = new APIResponsesImpl();
+        APIResponseImpl response = new APIResponseImpl();
+        responses.addApiResponse("200", response.description("Operation successful"));
+
+        pathItemOneGet.operationId("pathItemOneGetId").responses(responses);
+        pathItemOne.setGET(pathItemOneGet);
+
+        PathsImpl paths = new PathsImpl();
+        paths.addPathItem(pathNameOne, pathItemOne);
+
+        OpenAPIImpl model = new OpenAPIImpl();
+        Context context = new TestValidationContextHelper(model);
+        model.setPaths(paths);
+
+        validator.validate(vh, context, key, pathItemOneGet);
+        //Assert.assertEquals(0, vh.getEventsSize());
+    }
 }
