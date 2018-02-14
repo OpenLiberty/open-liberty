2b77a7d5297de1e504f1d0a5e767fbc03c555572
Bill Lucy <wtlucy@gmail.com>
2018-02-14 12:31:53 -0500

    update copyrights

--- dev/com.ibm.ws.transport.http/test/com/ibm/ws/transport/http2_test/headers/HeaderTest.java
+++ dev/com.ibm.ws.transport.http/test/com/ibm/ws/transport/http2_test/headers/HeaderTest.java
@@ -3,7 +3,7 @@
  *
  * OCO Source Materials
  *
- * WLP Copyright IBM Corp. 2013
+ * WLP Copyright IBM Corp. 2018
  *
  * The source code for this program is not published or otherwise divested
  * of its trade secrets, irrespective of what has been deposited with the
