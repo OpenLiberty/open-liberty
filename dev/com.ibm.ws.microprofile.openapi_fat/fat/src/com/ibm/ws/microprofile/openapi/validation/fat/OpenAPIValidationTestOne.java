1bd0d3d3e597b983f2b89839d9323fbffaedf411
Anna Safonov <anna.safonov@ibm.com>
2018-02-14 14:51:34 -0500

    Validation tests for the first group of models

--- dev/com.ibm.ws.microprofile.openapi_fat/fat/src/com/ibm/ws/microprofile/openapi/validation/fat/OpenAPIValidationTestOne.java
+++ dev/com.ibm.ws.microprofile.openapi_fat/fat/src/com/ibm/ws/microprofile/openapi/validation/fat/OpenAPIValidationTestOne.java
@@ -1,17 +1,12 @@
 package com.ibm.ws.microprofile.openapi.validation.fat;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import com.ibm.websphere.simplicity.config.ServerConfiguration;
-//import com.ibm.ws.openapi.fat.utils.OpenAPIConnection;
-
-import componenttest.annotation.MinimumJavaLevel;
 import componenttest.annotation.Server;
 import componenttest.custom.junit.runner.FATRunner;
 import componenttest.topology.impl.LibertyServer;
@@ -28,7 +23,7 @@ public class OpenAPIValidationTestOne {
     public static LibertyServer server;
     private static final String OPENAPI_VALIDATION_YAML = "openapi_validation";
 
-    private static final int TIMEOUT = 10000; //in milliseconds (10 seconds)
+    //private static final int TIMEOUT = 10000; //in milliseconds (10 seconds)
 
     @BeforeClass
     public static void setUpTest() throws Exception {
@@ -42,24 +37,115 @@ public class OpenAPIValidationTestOne {
                       server.waitForStringInLog("CWWKT0016I.*/openapi_validation/")); //wait for endpoint to become available
         assertNotNull("CWWKF0011I.* not recieved on relationServer",
                       server.waitForStringInLog("CWWKF0011I.*")); // wait for server is ready to run a smarter planet message
-        assertNotNull("CWWKO1660I.* The application openapi_validation was processed",
-                	  server.waitForStringInLog("CWWKO1660I.*")); //wait for application to be processed
+        assertNotNull("The application openapi_validation was processed and an OpenAPI document was produced.",
+                	  server.waitForStringInLog("CWWKO1660I.*and an OpenAPI document was produced.")); //wait for application to be processed
     }
     
     @AfterClass
     public static void tearDown() throws Exception {
         if (server.isStarted()) {
-            server.stopServer("CWWKO1603E");
+            server.stopServer("CWWKO1603E", "CWWKO1650E", "CWWKO1651W");
         }
     }
     
+    //Tests for correct validation messages provided for the validation errors in the following models:
+    //Info, Contact, License, ServerVariable(s), Server(s), PathItem, Operation, ExternalDocumentation, SecurityRequirement, RequestBody, Response, Responses
+    
     @Test
-    public void testValidationMessagesForInfo() throws Exception {
-    	//Tests that correct validation messages are provided for the validation errors in the following models:
-    	//Info, Contact, License, ServerVariable(s), Server(s), PathItem, Operation, ExternalDocumentation, Parameter, RequestBody
+    public void testInfoValidation() throws Exception {
     	
-    	assertNotNull("CWWKO1650E.* The Info object must contain a valid URL. The \"not in URL format\" value specified for \"termsOfService\" is not a valid URL, Location: #/info/termsOfService", 
-    			server.waitForStringInLog("CWWKO1660I.*"));
+    	assertNotNull("The Info object was not validated properly", 
+    			server.waitForStringInLog("Message: The Info Object must contain a valid URL. The \"not in URL format\" value specified for \"termsOfService\"*"));
+    	assertNotNull("The Info object was not validated properly", 
+    			server.waitForStringInLog("Message: Required \"version\" field is missing or is set to an invalid value, Location: #/info"));
+    	assertNotNull("The Info object was not validated properly", 
+    			server.waitForStringInLog("Message: Required \"title\" field is missing or is set to an invalid value, Location: #/info"));
+    }
+    
+    @Test
+    public void testContactValidation(){ 	
+    	assertNotNull("The Contact object was not validated properly", 
+    			server.waitForStringInLog("Message: The Contact Object must contain a valid URL. The \"not in URL Format\" value specified*"));
+    	assertNotNull("The Contact object was not validated properly", 
+    			server.waitForStringInLog("Message: The Contact Object must contain a valid email address. The \"not an email\" value*"));
+    }
+    
+    @Test
+    public void testLicenseValidation(){
+    	assertNotNull("The License object was not validated properly", 
+    			server.waitForStringInLog("Message: Required \"name\" field is missing or is set to an invalid value"));
+    	assertNotNull("The License object was not validated properly", 
+    			server.waitForStringInLog("The License Object must contain a valid URL. The \"not in URL format\" value"));
+    }
+    
+    @Test
+    public void testServerValidation(){
+    	assertNotNull("The Server object was not validated properly", 
+    			server.waitForMultipleStringsInLog(4, "Message: The Server Object must contain a valid URL*"));
+    }
+    
+    @Test
+    public void testServersValidation(){
+    	assertNotNull("The Server object was not validated properly", 
+    			server.waitForStringInLog("Message: Required \"url\" field is missing or is set to an invalid value, Location: #/paths/~1reviews/get/servers"));
+    }
+    
+    @Test
+    public void testServerVariableValidation(){
+    	assertNotNull("The Server Variable object was not validated properly", 
+    			server.waitForStringInLog("Message: Required \"default\" field is missing or is set to an invalid value*"));
+    	assertNotNull("The Server Variable object was not validated properly", 
+    			server.waitForStringInLog("he \"extraVariable\" variable in the Server Object is not defined*"));
+    	assertNotNull("The Server Variable object was not validated properly", 
+    			server.waitForStringInLog("Message: The \"id\" variable in the Server Object is not defined*"));
+    }
+    
+    @Test
+    public void testPathItemValidation(){
+    	assertNotNull("The PathItem object was not validated properly", 
+    			server.waitForStringInLog("The \"id\" path parameter from the \"GET\" operation of the path*"));
+    	assertNotNull("The PathItem object was not validated properly", 
+    			server.waitForStringInLog("The \"GET\" operation of the*"));
+    	assertNotNull("The PathItem object was not validated properly", 
+    			server.waitForStringInLog("The \"GET\" operation from the*"));
+    }
+    
+    @Test
+    public void testOperationValidation(){
+    	assertNotNull("The Operation object was not validated properly", 
+    			server.waitForStringInLog("Message: Required \"responses\" field is missing or is set to an invalid value, Location: #/paths/~1/get"));
+    	assertNotNull("The Operation object was not validated properly", 
+    			server.waitForStringInLog("Message: More than one Operation Objects with \"getReviewById\" value for \"operationId\" field was found. The \"operationId\" must be unique"));
+    }
+    
+    @Test
+    public void testExternalDocsValidation(){
+    	assertNotNull("The ExternalDocumentation object was not validated properly", 
+    			server.waitForStringInLog("Message: The External Documentation Object must contain a valid URL. The \"not a URL\" value"));
+    }
+    
+    @Test
+    public void testSecurityRequirementValidation(){
+    	assertNotNull("The Parameter object was not validated properly", 
+    			server.waitForStringInLog("The \"reviewoauth2\" name provided for the Security Requirement Object does not correspond to a declared security scheme"));
+    }
+    
+    @Test
+    public void testRequestBodyValidation(){
+    	assertNotNull("The RequestBody object was not validated properly", 
+    			server.waitForStringInLog("Message: Required \"content\" field is missing or is set to an invalid value, Location: #/paths/~1reviews/post/requestBody"));
+    }
+    
+    @Test
+    public void testResponseValidation(){
+    	assertNotNull("The Response object was not validated properly", 
+    			server.waitForStringInLog("Message: Required \"description\" field is missing or is set to an invalid value*"));
+    }
+    
+    @Test
+    public void testResponsesValidation(){
+    	assertNotNull("The Responses object was not validated properly", 
+    			server.waitForStringInLog("Message: The Responses Object should contain at least one response code for a successful operation"));
     }
 
 }
