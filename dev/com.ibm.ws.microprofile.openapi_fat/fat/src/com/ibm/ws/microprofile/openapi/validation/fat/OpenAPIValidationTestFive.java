477d8d15cf926c080efb56fe7224c9212e34b405
janamanoharan <jana.manoharan96@gmail.com>
2018-02-15 16:45:42 -0500

    added some more coverage for schema

--- dev/com.ibm.ws.microprofile.openapi_fat/fat/src/com/ibm/ws/microprofile/openapi/validation/fat/OpenAPIValidationTestFive.java
+++ dev/com.ibm.ws.microprofile.openapi_fat/fat/src/com/ibm/ws/microprofile/openapi/validation/fat/OpenAPIValidationTestFive.java
@@ -1,16 +1,17 @@
 package com.ibm.ws.microprofile.openapi.validation.fat;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
+import javax.servlet.http.HttpUtils;
+
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import componenttest.annotation.Server;
 import componenttest.custom.junit.runner.FATRunner;
 import componenttest.topology.impl.LibertyServer;
-import componenttest.topology.utils.HttpUtils;
 
 /**
  * A class to test the Tag, Discriminator, Schema and Extension validators.
@@ -18,8 +19,8 @@ import componenttest.topology.utils.HttpUtils;
  * Tag: the name field is missing
  * Discriminator: propertyName field is missing
  * Schema: inappropriate fields for certain Schema types (min/max items or uniqueOnly fields on String type, min/max Length on array types)
- *         invalid values for certain fields such as negative values for length
- *         conflicting fields such as the readOnly and writeOnly fields
+ * invalid values for certain fields such as negative values for length
+ * conflicting fields such as the readOnly and writeOnly fields
  * Extension: name not starting wiht "x-"
  *
  */
@@ -60,18 +61,20 @@ public class OpenAPIValidationTestFive {
     @Test
     public void testTags() throws Exception {
         assertNotNull("The Tag Validator should have been triggered by the missing 'name' field",
-                             server.waitForStringInLog(" - Message: Required \"name\" field is missing or is set to an invalid value, Location: #/tags"));
+                      server.waitForStringInLog(" - Message: Required \"name\" field is missing or is set to an invalid value, Location: #/tags"));
     }
-    
+
     @Test
     public void testDiscriminator() throws Exception {
-        assertNotNull("The Discriminator validator should have been triggered by the missing 'propertyName' field", server.waitForStringInLog("- Message: Required \"propertyName\" field is missing or is set to an invalid value,*"));
+        assertNotNull("The Discriminator validator should have been triggered by the missing 'propertyName' field",
+                      server.waitForStringInLog("- Message: Required \"propertyName\" field is missing or is set to an invalid value,*"));
     }
-    
+
     @Test
     public void testExtension() throws Exception {
         assertNotNull("The Extension validator should have been triggered by the invalid 'name' field", server.waitForStringInLog("extension must begin with \"x-\""));
     }
+
     @Test
     public void testSchema() throws Exception {
         assertNotNull("The Schema validator should have been triggered by the missing 'items' field",
@@ -84,8 +87,13 @@ public class OpenAPIValidationTestFive {
                       server.waitForStringInLog(" - Message: The \"maxItems\" property of the Schema Object must be greater than or equal to zero, Location: #/paths/~1availability/get/parameters/schema"));
         assertNotNull("The Schema validator should have been triggered by the invalid 'minProperties' field",
                       server.waitForStringInLog(" - Message: The \"minProperties\" property of the Schema Object must be greater than or equal to zero, Location: #/paths/~1availability/get/parameters/schema"));
+        assertNotNull("The Schema validator should have been triggered by the invalid 'maxProperties' field",
+                      server.waitForStringInLog(" - Message: The \"maxProperties\" property of the Schema Object must be greater than or equal to zero, Location: #/paths/~1availability/get/parameters/schema"));
         assertNotNull("The Schema validator should have been triggered by the invalid 'minItems' field",
                       server.waitForStringInLog(" - Message: The \"minItems\" property is not appropriate for the Schema Object of \"object\" type, Location: #/paths/~1availability/get/parameters/schema"));
+        assertNotNull("The Schema validator should have been triggered by the invalid 'maxItems' field",
+                      server.waitForStringInLog(" - Message: The \"maxItems\" property is not appropriate for the Schema Object of \"object\" type, Location: #/paths/~1availability/get/parameters/schema"));
+
         assertNotNull("The Schema validator should have been triggered by the conflicting field declarations",
                       server.waitForStringInLog("The Schema Object must not have both \"readOnly\" and \"writeOnly\" fields set to true"));
         assertNotNull("The Schema validator should have been triggered by the invalid 'uniqueItems'field",
@@ -94,8 +102,7 @@ public class OpenAPIValidationTestFive {
                       server.waitForStringInLog("The \"maxLength\" property of the Schema Object must be greater than or equal to zero"));
         assertNotNull("The Schema validator should have been triggered by the invalid 'minLength'field",
                       server.waitForStringInLog("The \"minLength\" property of the Schema Object must be greater than or equal to zero"));
-        
- 
+
     }
 
 }
