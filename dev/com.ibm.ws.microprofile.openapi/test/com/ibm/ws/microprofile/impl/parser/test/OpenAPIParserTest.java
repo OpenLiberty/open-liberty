f8d01b3a7510d42d7bdabfbae1328f34dc29a1ae
Anna Safonov <anna.safonov@ibm.com>
2018-03-15 10:08:57 -0400

    Resolved merge conflict

--- dev/com.ibm.ws.microprofile.openapi/test/com/ibm/ws/microprofile/impl/parser/test/OpenAPIParserTest.java
+++ dev/com.ibm.ws.microprofile.openapi/test/com/ibm/ws/microprofile/impl/parser/test/OpenAPIParserTest.java
@@ -51,58 +51,6 @@ public class OpenAPIParserTest {
 
     SwaggerParseResult result = new OpenAPIParser().readLocation("airlines_openapi.yaml", null, null);
 
-    @Before
-    public void testForNullResult() {
-        assertNotNull(result);
-        assertNotNull(result.getOpenAPI());
-    }
-
-    @Test
-    public void testOpenAPI() {
-        assertEquals(result.getOpenAPI().getOpenapi(), "3.0.0");
-    }
-
-    @Test
-    public void testInfo() {
-        //only checking that info is not null, the rest will be checked in the test for Info object
-        assertNotNull(result.getOpenAPI().getInfo());
-    }
-
-    @Test
-    public void testServers() {
-        //only checking that servers is not null, the rest will be checked in the test for Servers object
-        assertNotNull(result.getOpenAPI().getServers());
-    }
-
-    @Test
-    public void testPaths() {
-        //only checking that paths is not null, the rest will be checked in the test for Paths object
-        assertNotNull(result.getOpenAPI().getPaths());
-    }
-
-    @Test
-    public void testComponents() {
-        //only checking that components is not null, the rest will be checked in the test for Components object
-        assertNotNull(result.getOpenAPI().getComponents());
-    }
-
-    @Test
-    public void testSecurity() {
-        //only checking that security is null, the rest will be checked in the test for SecurityRequirements object
-        assertNull(result.getOpenAPI().getSecurity());
-    }
-
-    @Test
-    public void testTags() {
-        //only checking that tags is not null, the rest will be checked in the test for Tag object
-        assertNotNull(result.getOpenAPI().getTags());
-    }
-
-    @Test
-    public void testExternalDocs() {
-        //only checking that externalDocs is not null, the rest will be checked in the test for ExternalDocumentation object
-        assertNotNull(result.getOpenAPI().getExternalDocs());
-    }
     @Test
     public void testOpenAPI() {
         assertNotNull(result);
