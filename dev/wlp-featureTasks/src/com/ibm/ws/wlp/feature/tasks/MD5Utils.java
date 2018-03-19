24812465505078024c715e26837c5f9206c1fa60
Glenn Marcy <gmarcy@us.ibm.com>
2018-03-18 22:32:00 -0400

    changes to the rest of the workspace for new feature build

---
+++ dev/wlp-featureTasks/src/com/ibm/ws/wlp/feature/tasks/MD5Utils.java
@@ -0,0 +1,64 @@
+package com.ibm.ws.wlp.feature.tasks;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5Utils {
+
+    protected static char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
+    protected static MessageDigest messagedigest = null;
+
+    static {
+        try {
+            messagedigest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            //should not happen
+        }
+    }
+
+    public static String getMD5String(String str) {
+        messagedigest.update(str.getBytes());
+        return byteArrayToHexString(messagedigest.digest());
+    }
+
+    public static String getFileMD5String(File file) throws IOException {
+        InputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+
+            byte[] buffer = new byte[1024];
+            int numRead = 0;
+            while ((numRead = fis.read(buffer)) > 0) {
+                messagedigest.update(buffer, 0, numRead);
+            }
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (Exception e) {
+                }
+            }
+        }
+
+        return byteArrayToHexString(messagedigest.digest());
+    }
+
+    public static String byteArrayToHexString(byte[] byteArray) {
+
+        StringBuffer stringbuffer = new StringBuffer(2 * byteArray.length);
+        for (int i = 0; i < byteArray.length; i++) {
+            char upper = hexDigits[(byteArray[i] & 0xf0) >> 4];
+            char lower = hexDigits[byteArray[i] & 0xf];
+            stringbuffer.append(upper);
+            stringbuffer.append(lower);
+        }
+        return stringbuffer.toString();
+
+    }
+
+}
