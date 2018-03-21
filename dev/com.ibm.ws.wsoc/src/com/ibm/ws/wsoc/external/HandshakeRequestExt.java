8c749dca14794d46bd50c458e4294088a1c0fd5f
William Wigger <wigger@us.ibm.com>
2018-03-21 15:13:01 -0400

    Inlcude path paramters in the parameter list maintained by the configurtor that is used to examine and or modify the request response handshake.

--- dev/com.ibm.ws.wsoc/src/com/ibm/ws/wsoc/external/HandshakeRequestExt.java
+++ dev/com.ibm.ws.wsoc/src/com/ibm/ws/wsoc/external/HandshakeRequestExt.java
@@ -12,6 +12,7 @@ package com.ibm.ws.wsoc.external;
 
 import java.net.URI;
 import java.security.Principal;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -19,9 +20,13 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.websocket.Endpoint;
+import javax.websocket.EndpointConfig;
 import javax.websocket.server.HandshakeRequest;
+import javax.websocket.server.ServerEndpointConfig;
 
 import com.ibm.websphere.ras.annotation.Sensitive;
+import com.ibm.ws.wsoc.AnnotatedEndpoint;
 
 public class HandshakeRequestExt implements HandshakeRequest {
 
@@ -36,7 +41,7 @@ public class HandshakeRequestExt implements HandshakeRequest {
     public HandshakeRequestExt(HttpServletRequest _request,
                                Map<String, List<String>> _headers,
                                Map<String, List<String>> _parameterMap,
-                               URI _requestURI) {
+                               URI _requestURI, Endpoint _ep, EndpointConfig _epc) {
 
         headers = _headers;
         // if there are no headers, then _headers should be empty list, not null.  Following servlet spec (wsoc javadoc doesn't say)
@@ -47,7 +52,12 @@ public class HandshakeRequestExt implements HandshakeRequest {
         //as per WebSocket HandshakeRequest API, the headers should be read only
         this.headers = Collections.unmodifiableMap(headers);
         request = _request;
-        //Even though webSocket Spec/API doesn't specify this to be readonly, TCK needs this to be readonly because 
+
+        // add the path Params to the qurey parms that were already added
+        Map<String, List<String>> pathParams = determinePathParams(_ep, _epc, _requestURI);
+        _parameterMap.putAll(pathParams);
+
+        //Even though webSocket Spec/API doesn't specify this to be readonly, TCK needs this to be readonly because
         //HttpServletRequest's parameterMap is immutable.
         this.parameterMap = Collections.unmodifiableMap(_parameterMap);
         queryString = request.getQueryString();
@@ -55,6 +65,49 @@ public class HandshakeRequestExt implements HandshakeRequest {
         principal = request.getUserPrincipal();
         requestURI = _requestURI;
 
+    }
+
+    private Map<String, List<String>> determinePathParams(Endpoint endpoint, EndpointConfig endpointConfig, URI _uri) {
+
+        // automated debug should print out the returned map, so no other debug will be added.
+        HashMap<String, List<String>> pathParameters = new HashMap<String, List<String>>();
+
+        String[] endpointURIParts = null;
+        //endpoint path. e.g /basic/bookings/{guest-id}
+        if ((endpoint != null) && (endpoint instanceof AnnotatedEndpoint)) {
+            //annotated
+            endpointURIParts = ((AnnotatedEndpoint) endpoint).getEndpointPath().split("/");
+        } else if (endpointConfig != null) {
+            //programmatic
+            endpointURIParts = ((ServerEndpointConfig) endpointConfig).getPath().split("/");
+        }
+
+        if ((endpointURIParts == null) || (_uri == null)) {
+            // return empty array if there is nothing to look at
+            return pathParameters;
+        }
+
+        // This code is cut-n-paste from code in SessionImpl, which does the same thing.
+        //incoming request uri path. e.g /bookings/JohnDoe
+        String[] requestURIParts = _uri.getPath().split("/");
+        int i = endpointURIParts.length - 1;
+        int j = requestURIParts.length - 1;
+        //start to compare the endpoint path and request uri path, starting from the last segment because
+        //request uri at this point of execution does not have context root of the webapp, which in this case is '/basic'
+        //and endpoint uri on the other hand does have the context root of the webapp at this point of execution.
+        while (i > 1) { //skipping the first part because first part of split("/") for /../../.. is always an empty string
+            if (endpointURIParts[i].startsWith("{") && endpointURIParts[i].endsWith("}")) {
+                String endpointPart = endpointURIParts[i].substring(1, endpointURIParts[i].length() - 1); //guest-id
+
+                // Map will have pathName, pathvalue --> guest-id,JohnDoe
+                List<String> list = Arrays.asList(requestURIParts[j]);
+                pathParameters.put(endpointPart, list);
+            }
+            i--;
+            j--;
+        }
+
+        return pathParameters;
     }
 
     @Override
