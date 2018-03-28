1b482fc036fd9229ad935cc9f48819a78b1317d6
Thomas Bitonti <bitonti@us.ibm.com>
2018-03-28 15:12:15 -0400

    Reworked javaee8 FAT tests; added EAR deployment test.

---
+++ dev/com.ibm.ws.javaee.8.0_fat/test-applications/simpleJar.jar/src/testservlet40/jar/servlets/ServletContainerInitializerImpl.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package testservlet40.jar.servlets;
+
+import java.util.Set;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.HandlesTypes;
+
+//since our HandlesTypes annotation contains the class Servlet, then all classes within the
+//application that implement Servlet will be sent to the onStartup method below
+@HandlesTypes(javax.servlet.Servlet.class)
+public class ServletContainerInitializerImpl implements ServletContainerInitializer {
+
+    @Override
+    public void onStartup(Set<Class<?>> setOfClassesInterestedIn, ServletContext context) throws ServletException {
+
+        System.out.println("--- TestServlet40 CONTAINER INITIALIZER! ---");
+        //going to add a context attribute to show the set of classes that were passed in
+        if (setOfClassesInterestedIn != null) {
+            context.setRequestCharacterEncoding("KSC5601");
+            context.setResponseCharacterEncoding("KSC5601");
+            //context.setResponseCharacterEncoding("PMDINH"); //causes new message CWWWC0401E: Failed to set response character encoding
+            context.setAttribute("SET_OF_SERVLETS_IN_APP", setOfClassesInterestedIn);
+        } else {
+            context.setAttribute("SET_OF_SERVLETS_IN_APP", "null");
+        }
+    }
+
+}
