9bd5561f3562011919bcd349f362c38ca4825f0b
Adam Yoho <ayoho@us.ibm.com>
2018-03-29 12:54:14 -0500

    Issue 2755: Delivers golden path JWT SSO FAT case
    Delivers the following golden path JWT SSO FAT scenario:
    - `jwtSso-1.0` feature is enabled: Log in with valid credentials and verify that we receive a JWT SSO cookie, not an LTPA cookie

--- dev/com.ibm.ws.security.jwtsso_fat/fat/src/com/ibm/ws/security/jwtsso/fat/FATSuite.java
+++ dev/com.ibm.ws.security.jwtsso_fat/fat/src/com/ibm/ws/security/jwtsso/fat/FATSuite.java
@@ -14,10 +14,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import componenttest.custom.junit.runner.AlwaysPassesTest;
-
 @RunWith(Suite.class)
 @SuiteClasses({
-                AlwaysPassesTest.class,
+                FeatureOnlyTest.class,
 })
 public class FATSuite {}
