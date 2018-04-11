84ca96f4af25f9a1ee0dd99ef0db5fc0c8d15dc5
wtlucy <wtlucy@us.ibm.com>
2018-04-11 09:16:06 -0400

    un-quarantine http2 FATs

--- dev/com.ibm.ws.transport.http2_fat/fat/src/test/server/transport/http2/Http2FullTracingTests.java
+++ dev/com.ibm.ws.transport.http2_fat/fat/src/test/server/transport/http2/Http2FullTracingTests.java
@@ -29,7 +29,7 @@ import componenttest.topology.impl.LibertyServerFactory;
 import componenttest.topology.utils.FATServletClient;
 
 @RunWith(FATRunner.class)
-@Mode(TestMode.QUARANTINE)
+@Mode(TestMode.FULL)
 public class Http2FullTracingTests extends FATServletClient {
 
     private static final String CLASS_NAME = Http2FullTracingTests.class.getName();
