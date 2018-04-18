7f5915264602288c6f2999097cc8213e981a9798
janamanoharan <jana.manoharan96@gmail.com>
2018-04-18 13:58:14 -0400

    Issue #2311 used RepeatTests to check of ee8 compatability

--- dev/com.ibm.ws.microprofile.openapi_fat/fat/src/com/ibm/ws/microprofile/openapi/fat/FATSuite.java
+++ dev/com.ibm.ws.microprofile.openapi_fat/fat/src/com/ibm/ws/microprofile/openapi/fat/FATSuite.java
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package com.ibm.ws.microprofile.openapi.fat;
 
+import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -21,6 +22,9 @@ import com.ibm.ws.microprofile.openapi.validation.fat.OpenAPIValidationTestSix;
 import com.ibm.ws.microprofile.openapi.validation.fat.OpenAPIValidationTestThree;
 import com.ibm.ws.microprofile.openapi.validation.fat.OpenAPIValidationTestTwo;
 
+import componenttest.rules.repeater.FeatureReplacementAction;
+import componenttest.rules.repeater.RepeatTests;
+
 @RunWith(Suite.class)
 @SuiteClasses({
                 ApplicationProcessorTest.class,
@@ -31,7 +35,12 @@ import com.ibm.ws.microprofile.openapi.validation.fat.OpenAPIValidationTestTwo;
                 OpenAPIValidationTestFive.class,
                 OpenAPIValidationTestSix.class,
                 ProxySupportTest.class,
+                EE8CompatabilityTest.class,
                 EndpointAvailabilityTest.class,
                 UICustomizationTest.class
 })
-public class FATSuite {}
+public class FATSuite {
+
+    @ClassRule
+    public static RepeatTests r = RepeatTests.withoutModification().andWith(FeatureReplacementAction.EE8_FEATURES());
+}
