5e7832196f3aecf6a35b983f2596d53a0e1f2204
chdavid <chdavid@ca.ibm.com>
2018-04-26 14:31:46 -0400

    Replace the messageLogFormat second parameter with the appropriate value

--- dev/com.ibm.ws.logging/src/com/ibm/ws/logging/internal/impl/BaseTraceService.java
+++ dev/com.ibm.ws.logging/src/com/ibm/ws/logging/internal/impl/BaseTraceService.java
@@ -742,7 +742,7 @@ public class BaseTraceService implements TrService {
 
             RoutedMessage routedMessage = null;
             if (externalMessageRouter.get() != null) {
-                String message = formatter.messageLogFormat(logRecord, logRecord.getMessage());
+                String message = formatter.messageLogFormat(logRecord, formattedVerboseMsg);
                 routedMessage = new RoutedMessageImpl(formattedMsg, formattedVerboseMsg, message, logRecord);
             } else {
                 routedMessage = new RoutedMessageImpl(formattedMsg, formattedVerboseMsg, null, logRecord);
