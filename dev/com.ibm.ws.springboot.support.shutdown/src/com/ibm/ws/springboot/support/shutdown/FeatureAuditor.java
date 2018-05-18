d1e99c97e75ea25b4dca9f48cb8209ed17bdc899
Anjum Fatima <anjum.fatima@ibm.com>
2018-05-18 14:06:56 -0500

    Format file according to project settings

--- dev/com.ibm.ws.springboot.support.shutdown/src/com/ibm/ws/springboot/support/shutdown/FeatureAuditor.java
+++ dev/com.ibm.ws.springboot.support.shutdown/src/com/ibm/ws/springboot/support/shutdown/FeatureAuditor.java
@@ -18,66 +18,66 @@ import com.ibm.ws.app.manager.springboot.container.ApplicationError;
 import com.ibm.ws.app.manager.springboot.container.ApplicationError.Type;
 
 public class FeatureAuditor implements EnvironmentPostProcessor {
-	@Override
-	public void postProcessEnvironment(ConfigurableEnvironment env, SpringApplication app) {
-		/*
-		 * Throw an Application error if the wrong version of spring boot feature is
-		 * enabled
-		 */
-		try {
-			Class.forName("org.springframework.boot.context.embedded.EmbeddedServletContainerFactory");
-			Class.forName(
-					"com.ibm.ws.springboot.support.web.server.version20.container.LibertyServletContainerConfiguration");
-			checkSpringBootVersion15();
-		} catch (ClassNotFoundException e) {
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment env, SpringApplication app) {
+        /*
+         * Throw an Application error if the wrong version of spring boot feature is
+         * enabled
+         */
+        try {
+            Class.forName("org.springframework.boot.context.embedded.EmbeddedServletContainerFactory");
+            Class.forName(
+                          "com.ibm.ws.springboot.support.web.server.version20.container.LibertyServletContainerConfiguration");
+            checkSpringBootVersion15();
+        } catch (ClassNotFoundException e) {
 
-		}
+        }
 
-		try {
-			Class.forName("org.springframework.boot.web.servlet.server.ServletWebServerFactory");
-			Class.forName(
-					"com.ibm.ws.springboot.support.web.server.version15.container.LibertyServletContainerConfiguration");
-			checkSpringBootVersion20();
-		} catch (ClassNotFoundException e) {
+        try {
+            Class.forName("org.springframework.boot.web.servlet.server.ServletWebServerFactory");
+            Class.forName(
+                          "com.ibm.ws.springboot.support.web.server.version15.container.LibertyServletContainerConfiguration");
+            checkSpringBootVersion20();
+        } catch (ClassNotFoundException e) {
 
-		}
+        }
 
-		/* Throw an application error if servlet feature is not enabled */
-		try {
-			Class.forName("org.springframework.web.WebApplicationInitializer");
-			checkServletPresent();
-		} catch (ClassNotFoundException e) {
+        /* Throw an application error if servlet feature is not enabled */
+        try {
+            Class.forName("org.springframework.web.WebApplicationInitializer");
+            checkServletPresent();
+        } catch (ClassNotFoundException e) {
 
-		}
+        }
 
-	}
+    }
 
-	private void checkServletPresent() {
-		try {
-			Class.forName("javax.servlet.Servlet");
-		} catch (ClassNotFoundException e) {
-			throw new ApplicationError(Type.MISSING_SERVLET_FEATURE);
-		}
+    private void checkServletPresent() {
+        try {
+            Class.forName("javax.servlet.Servlet");
+        } catch (ClassNotFoundException e) {
+            throw new ApplicationError(Type.MISSING_SERVLET_FEATURE);
+        }
 
-	}
+    }
 
-	private void checkSpringBootVersion15() {
-		try {
-			Class.forName(
-					"com.ibm.ws.springboot.support.web.server.version15.container.LibertyServletContainerConfiguration");
-		} catch (ClassNotFoundException e) {
-			throw new ApplicationError(Type.NEED_SPRING_BOOT_VERSION_15);
-		}
+    private void checkSpringBootVersion15() {
+        try {
+            Class.forName(
+                          "com.ibm.ws.springboot.support.web.server.version15.container.LibertyServletContainerConfiguration");
+        } catch (ClassNotFoundException e) {
+            throw new ApplicationError(Type.NEED_SPRING_BOOT_VERSION_15);
+        }
 
-	}
+    }
 
-	private void checkSpringBootVersion20() {
-		try {
-			Class.forName(
-					"com.ibm.ws.springboot.support.web.server.version20.container.LibertyServletContainerConfiguration");
-		} catch (ClassNotFoundException e) {
-			throw new ApplicationError(Type.NEED_SPRING_BOOT_VERSION_20);
-		}
+    private void checkSpringBootVersion20() {
+        try {
+            Class.forName(
+                          "com.ibm.ws.springboot.support.web.server.version20.container.LibertyServletContainerConfiguration");
+        } catch (ClassNotFoundException e) {
+            throw new ApplicationError(Type.NEED_SPRING_BOOT_VERSION_20);
+        }
 
-	}
+    }
 }
