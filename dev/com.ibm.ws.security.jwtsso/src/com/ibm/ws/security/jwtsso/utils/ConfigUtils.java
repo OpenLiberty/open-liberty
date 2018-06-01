35d253a909369aa45485f7cf03068776c5076b24
Andrew Guibert <andy.guibert@gmail.com>
2018-06-01 12:47:32 -0500

    Revert "Issue #3757 : update jwtsso default cookie name"

--- dev/com.ibm.ws.security.jwtsso/src/com/ibm/ws/security/jwtsso/utils/ConfigUtils.java
+++ dev/com.ibm.ws.security.jwtsso/src/com/ibm/ws/security/jwtsso/utils/ConfigUtils.java
@@ -14,42 +14,41 @@ import com.ibm.websphere.ras.Tr;
 import com.ibm.websphere.ras.TraceComponent;
 
 public class ConfigUtils {
-	private static final TraceComponent tc = Tr.register(ConfigUtils.class);
-	public final static String CFG_DEFAULT_COOKIENAME = "JWT"; // needs to match
-																// metatype.xml
-	private final static String validCookieChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'*+-.^_`|~"; // rfc6265
+    private static final TraceComponent tc = Tr.register(ConfigUtils.class);
+    public final static String CFG_DEFAULT_COOKIENAME = "MPJWT"; // needs to match metatype.xml
+    private final static String validCookieChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'*+-.^_`|~"; // rfc6265
 
-	/**
-	 * reset cookieName to default value if it is not valid
-	 *
-	 * @param cookieName
-	 * @param quiet
-	 *            don't emit any error messages
-	 * @return original name or default if original was invalid
-	 */
-	public String validateCookieName(String cookieName, boolean quiet) {
-		if (cookieName == null || cookieName.length() == 0) {
-			if (!quiet) {
-				Tr.error(tc, "COOKIE_NAME_CANT_BE_EMPTY");
-			}
-			return CFG_DEFAULT_COOKIENAME;
-		}
-		String cookieNameUc = cookieName.toUpperCase();
-		boolean valid = true;
-		for (int i = 0; i < cookieName.length(); i++) {
-			String eval = cookieNameUc.substring(i, i + 1);
-			if (!validCookieChars.contains(eval)) {
-				if (!quiet) {
-					Tr.error(tc, "COOKIE_NAME_INVALID", new Object[] { cookieName, eval });
-				}
-				valid = false;
-			}
-		}
-		if (!valid) {
-			return CFG_DEFAULT_COOKIENAME;
-		} else {
-			return cookieName;
-		}
-	}
+    /**
+     * reset cookieName to default value if it is not valid
+     *
+     * @param cookieName
+     * @param quiet
+     *            don't emit any error messages
+     * @return original name or default if original was invalid
+     */
+    public String validateCookieName(String cookieName, boolean quiet) {
+        if (cookieName == null || cookieName.length() == 0) {
+            if (!quiet) {
+                Tr.error(tc, "COOKIE_NAME_CANT_BE_EMPTY");
+            }
+            return CFG_DEFAULT_COOKIENAME;
+        }
+        String cookieNameUc = cookieName.toUpperCase();
+        boolean valid = true;
+        for (int i = 0; i < cookieName.length(); i++) {
+            String eval = cookieNameUc.substring(i, i + 1);
+            if (!validCookieChars.contains(eval)) {
+                if (!quiet) {
+                    Tr.error(tc, "COOKIE_NAME_INVALID", new Object[] { cookieName, eval });
+                }
+                valid = false;
+            }
+        }
+        if (!valid) {
+            return CFG_DEFAULT_COOKIENAME;
+        } else {
+            return cookieName;
+        }
+    }
 
 }
