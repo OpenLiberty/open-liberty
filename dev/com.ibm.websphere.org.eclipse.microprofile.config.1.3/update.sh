30195131bc2c8745637acd5f8882ec90ee1ff1b9
Tom Evans <tevans@uk.ibm.com>
2018-06-11 16:27:50 +0100

    Issue #3774 - move to beta

--- dev/com.ibm.websphere.org.eclipse.microprofile.config.1.3/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -ex
-
-# Simple shell script to update this project to contain the latest MP Config api
-
-git clone https://github.com/eclipse/microprofile-config.git -b master --single-branch --depth 1
-rm -r src/* || true
-cp -r microprofile-config/api/src/main/java/* src/
-rm -rf microprofile-config
--- dev/com.ibm.websphere.org.eclipse.microprofile.faulttolerance.1.1/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -ex
-
-# Simple shell script to update this project to contain the latest MP Fault Tolerance api
-
-git clone https://github.com/eclipse/microprofile-fault-tolerance.git -b master --single-branch --depth 1
-rm -r src/* || true
-cp -r microprofile-fault-tolerance/api/src/main/java/* src/
-rm -rf microprofile-fault-tolerance
