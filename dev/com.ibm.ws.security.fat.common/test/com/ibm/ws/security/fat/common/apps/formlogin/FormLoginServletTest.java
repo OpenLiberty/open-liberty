c639d4762e1df789733264254633450c2c687ba8
chriscr <chriscr@us.ibm.com>
2018-06-14 11:18:49 -0500

    cleanup unittest

--- dev/com.ibm.ws.security.fat.common/test/com/ibm/ws/security/fat/common/apps/formlogin/FormLoginServletTest.java
+++ dev/com.ibm.ws.security.fat.common/test/com/ibm/ws/security/fat/common/apps/formlogin/FormLoginServletTest.java
@@ -91,11 +91,9 @@ public class FormLoginServletTest extends CommonTestClass {
                     will(returnValue(new Cookie[] { cookie }));
                     one(cookie).getName();
                     one(cookie).getValue();
-                    allowing(writer).write(with(any(String.class)));
                     one(request).getParameter("logout");
                     one(request).logout();
-                    allowing(writer).write(with(any(String.class)));
-                    allowing(writer).write(with(any(String.class)));
+                    one(writer).write(with(any(String.class)));
                     one(writer).flush();
                     one(writer).close();
                 }
