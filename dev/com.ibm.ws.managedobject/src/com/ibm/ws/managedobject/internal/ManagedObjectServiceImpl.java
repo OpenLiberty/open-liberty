6e66f3d718c063e4809ea82ecd16c76a08bf180e
Tom Evans <tevans@uk.ibm.com>
2018-06-18 17:20:35 +0100

    Issue #3877 - remove deprecated method

--- dev/com.ibm.ws.managedobject/src/com/ibm/ws/managedobject/internal/ManagedObjectServiceImpl.java
+++ dev/com.ibm.ws.managedobject/src/com/ibm/ws/managedobject/internal/ManagedObjectServiceImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2015 IBM Corporation and others.
+ * Copyright (c) 2012, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,7 +13,6 @@ package com.ibm.ws.managedobject.internal;
 import org.osgi.service.component.annotations.Component;
 
 import com.ibm.ws.managedobject.DefaultManagedObjectService;
-import com.ibm.ws.managedobject.ManagedObject;
 import com.ibm.ws.managedobject.ManagedObjectException;
 import com.ibm.ws.managedobject.ManagedObjectFactory;
 import com.ibm.ws.managedobject.ManagedObjectService;
@@ -41,15 +40,6 @@ public class ManagedObjectServiceImpl implements DefaultManagedObjectService {
     public <T> ManagedObjectFactory<T> createEJBManagedObjectFactory(ModuleMetaData mmd, Class<T> klass, String ejbName) throws ManagedObjectException {
         //since this is a fallback adapter, the ejbName is ignored
         return createManagedObjectFactory(mmd, klass, false);
-    }
-
-    //this method will be removed very soon
-    @Override
-    @SuppressWarnings("unchecked")
-    public <T> ManagedObject<T> createManagedObject(ModuleMetaData mmd, T instance) throws ManagedObjectException {
-        ManagedObjectFactory<T> factory = createManagedObjectFactory(mmd, (Class<T>) instance.getClass(), false);
-        ManagedObject<T> mo = factory.createManagedObject(instance, null);
-        return mo;
     }
 
     @Override
