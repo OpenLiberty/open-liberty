d10e94be8e2c02c158f4386c0959e93c55b22ce0
Brandon Berry <Brandon.Jinarain.Berry@ibm.com>
2018-07-03 11:13:43 -0400

    preparing to squash

--- dev/com.ibm.ws.anno/test/com/ibm/ws/anno/test/cases/LimitedIndexReaderTest.java
+++ dev/com.ibm.ws.anno/test/com/ibm/ws/anno/test/cases/LimitedIndexReaderTest.java
@@ -47,7 +47,6 @@ public class LimitedIndexReaderTest{
     
     String indexFileName;
 
-    
     public LimitedIndexReaderTest(Object indexName){   
         InputStream connection;
         org.jboss.jandex.Index fullIndex;
