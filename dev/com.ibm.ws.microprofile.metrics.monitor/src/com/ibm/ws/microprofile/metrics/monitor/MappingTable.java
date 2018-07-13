9ef8f07343d43b7737900c0728d807cb051522c0
Gilbert Kwan <gkwan@ca.ibm.com>
2018-07-13 18:45:12 -0400

    Issue #12352: Provide monitor-1.0 metrics to mpMetrics-1.1

---
+++ dev/com.ibm.ws.microprofile.metrics.monitor/src/com/ibm/ws/microprofile/metrics/monitor/MappingTable.java
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * Copyright (c) 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.microprofile.metrics.monitor;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.microprofile.metrics.MetricType;
+import org.eclipse.microprofile.metrics.MetricUnits;
+
+public class MappingTable {
+	
+	private static final String COUNTER = MetricType.COUNTER.toString().toUpperCase();
+	private static final String GAUGE = MetricType.GAUGE.toString().toUpperCase();
+	
+	private static MappingTable singleton = null;
+
+	private Map<String, String[][]> mappingTable = new HashMap<String, String[][]>();
+
+	public static MappingTable getInstance() {
+		if (singleton == null)
+			singleton = new MappingTable();
+		return singleton;
+	}
+	
+	public MappingTable() {
+		String[][] threadPoolTable = new String[][] {
+			{ "connectionpool.%s.create.total", "Create Count", "connectionpool.create.total.description", COUNTER, MetricUnits.NONE, "CreateCount" },
+			{ "connectionpool.%s.destroy.total", "Destory Count", "connectionpool.destroy.total.description", COUNTER, MetricUnits.NONE, "DestroyCount"},
+			{ "connectionpool.%s.managedConnections", "Managed Connection Count", "connectionpool.managedConnections.description", GAUGE, MetricUnits.NONE, "ManagedConnectionCount"},
+			{ "connectionpool.%s.connectionHandles", "Connection Handle Count", "connectionpool.connectionHandles.description", GAUGE, MetricUnits.NONE, "ConnectionHandleCount" },
+			{ "connectionpool.%s.freeConnections", "Free Connection Count", "connectionpool.freeConnections.description", GAUGE, MetricUnits.NONE,  "FreeConnectionCount" }
+		};
+		mappingTable.put("WebSphere:type=ThreadPoolStats,name=*", threadPoolTable);
+
+		String[][] servletTable = new String[][] {
+        	{ "servlet.%s.request.total", "Total Request", "servlet.request.total.description", COUNTER, MetricUnits.NONE, "RequestCount", null },
+        	{ "servlet.%s.responseTime.total", "Total Response Time", "servlet.responseTime.total.description", GAUGE, MetricUnits.NANOSECONDS, "ResponseTimeDetails", "total"}
+        };
+		mappingTable.put("WebSphere:type=ServletStats,name=*", servletTable);
+		
+		String[][] connectionPoolTable = new String[][]{
+			{ "threadpool.%s.activeThreads", "Active Threads", "threadpool.defaultExecutor.activeThreads.description", GAUGE, MetricUnits.NONE, "ActiveThreads" },
+			{ "threadpool.%s.size", "Thread Pool Size", "threadpool.defaultExecutor.size.description", GAUGE, MetricUnits.NONE, "PoolSize"}
+		};
+		mappingTable.put("WebSphere:type=ConnectionPoolStats,name=*", connectionPoolTable);
+	}
+	
+	public String[][] getData(String objectName) {
+		for (String k : mappingTable.keySet()) {
+			if (objectName.contains(k))
+				return mappingTable.get(k);
+		}
+		return null;
+	}
+	
+	public boolean contains(String objectName) {
+		for (String k : mappingTable.keySet()) {
+			if (objectName.contains(k))
+				return true;
+		}
+		return false;
+	}
+
+	public Set<String> getKeys() {
+		return mappingTable.keySet();
+	}
+}
