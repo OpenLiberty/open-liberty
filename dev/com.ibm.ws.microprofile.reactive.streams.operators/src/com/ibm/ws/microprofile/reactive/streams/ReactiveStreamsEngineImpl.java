cf4c690aec401f87d9b61cb1a6318d0eb9833540
Gordon Hutchison <gordon.hutchison@gmail.com>
2018-07-17 17:56:41 +0100

    Initial com.ibm.ws.microprofile.reactive.streams.operators folder
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>

---
+++ dev/com.ibm.ws.microprofile.reactive.streams.operators/src/com/ibm/ws/microprofile/reactive/streams/ReactiveStreamsEngineImpl.java
@@ -0,0 +1,5 @@
+package com.ibm.ws.microprofile.reactive.streams.spi;
+
+@Component(service = { ReactiveStreamsEngineImpl.class }, configurationPolicy = ConfigurationPolicy.IGNORE, immediate = true)
+public class ReactiveStreamsEngineImpl{
+}
