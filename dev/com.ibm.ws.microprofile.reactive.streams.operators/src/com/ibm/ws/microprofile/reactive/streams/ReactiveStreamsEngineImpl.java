ea04cd3526492218d1047472a3ee621372f1c5a1
Gordon Hutchison <gordon.hutchison@gmail.com>
2018-07-19 16:34:49 +0100

    Tidy reactive streams eclipse project files
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>

--- dev/com.ibm.ws.microprofile.reactive.streams.operators/src/com/ibm/ws/microprofile/reactive/streams/ReactiveStreamsEngineImpl.java
+++ dev/com.ibm.ws.microprofile.reactive.streams.operators/src/com/ibm/ws/microprofile/reactive/streams/ReactiveStreamsEngineImpl.java
@@ -1,9 +1,45 @@
-package com.ibm.ws.microprofile.reactive.streams.spi;
+package com.ibm.ws.microprofile.reactive.streams;
+
+import java.util.concurrent.CompletionStage;
+
+import org.eclipse.microprofile.reactive.streams.CompletionSubscriber;
+import org.eclipse.microprofile.reactive.streams.spi.Graph;
+import org.eclipse.microprofile.reactive.streams.spi.ReactiveStreamsEngine;
+import org.eclipse.microprofile.reactive.streams.spi.UnsupportedStageException;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
-import org.osgi.service.component.annotations.Reference;
+import org.reactivestreams.Processor;
+import org.reactivestreams.Publisher;
 
+@Component(name = "com.ibm.ws.microprofile.reactive.streams.ReactiveStreamsEngineImpl", service = { ReactiveStreamsEngine.class }, configurationPolicy = ConfigurationPolicy.IGNORE, immediate = true)
+public class ReactiveStreamsEngineImpl implements ReactiveStreamsEngine {
 
-@Component(service = { ReactiveStreamsEngineImpl.class }, configurationPolicy = ConfigurationPolicy.IGNORE, immediate = true)
-public class ReactiveStreamsEngineImpl{
+    /** {@inheritDoc} */
+    @Override
+    public <T> Publisher<T> buildPublisher(Graph graph) throws UnsupportedStageException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public <T, R> CompletionSubscriber<T, R> buildSubscriber(Graph graph) throws UnsupportedStageException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public <T, R> Processor<T, R> buildProcessor(Graph graph) throws UnsupportedStageException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public <T> CompletionStage<T> buildCompletion(Graph graph) throws UnsupportedStageException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 }
