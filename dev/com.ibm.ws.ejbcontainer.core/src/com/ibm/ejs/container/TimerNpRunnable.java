cf5bbf550847fe5748528e346b989f3b206441d3
olendvcook <orcok@us.ibm.com>
2018-08-07 10:14:59 -0500

    EJB NPTimer scheduleNext after timer hits max retries

--- dev/com.ibm.ws.ejbcontainer.core/src/com/ibm/ejs/container/TimerNpRunnable.java
+++ dev/com.ibm.ws.ejbcontainer.core/src/com/ibm/ejs/container/TimerNpRunnable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2016 IBM Corporation and others.
+ * Copyright (c) 2009, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -17,13 +17,12 @@ import com.ibm.ws.ffdc.FFDCFilter;
 
 /**
  * A task handler for all non-persistent EJB timers. <p>
- * 
+ *
  * This abstract class provides the common logic to run EJB timers when
  * they reach their scheduled expiration. A subclass must be provided
  * for each supported scheduling service. </p>
  */
-public abstract class TimerNpRunnable implements Runnable
-{
+public abstract class TimerNpRunnable implements Runnable {
     private static final String CLASS_NAME = TimerNpRunnable.class.getName();
     private static final TraceComponent tc = Tr.register(TimerNpRunnable.class, "EJBContainer", "com.ibm.ejs.container.container");
 
@@ -36,8 +35,7 @@ public abstract class TimerNpRunnable implements Runnable
 
     private static volatile boolean serverStopping;
 
-    public TimerNpRunnable(TimerNpImpl timerNpImpl, int retryLimit, long retryInterval)
-    {
+    public TimerNpRunnable(TimerNpImpl timerNpImpl, int retryLimit, long retryInterval) {
         if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled())
             Tr.debug(tc, "<init>: " + timerNpImpl);
 
@@ -56,14 +54,13 @@ public abstract class TimerNpRunnable implements Runnable
      * Overridden to improve trace.
      **/
     @Override
-    public String toString()
-    {
+    public String toString() {
         return getClass().getSimpleName() + "(" + ivTimer.ivTaskId + ", " + ivBeanId + ", " + ivRetries + ")";
     }
 
     /**
      * Executes the timer work with configured retries.
-     * 
+     *
      * The EJB 3.1 spec, section 18.4.3 says, "If the transaction fails or
      * is rolled back, the container must retry the timeout at least once."
      * We allow the user to configure a retry count of 0, which will cause
@@ -71,8 +68,7 @@ public abstract class TimerNpRunnable implements Runnable
      * retry once immediately, then every retryInterval, indefinitely.
      */
     @Override
-    public void run()
-    {
+    public void run() {
         final boolean isTraceOn = TraceComponent.isAnyTracingEnabled();
         if (isTraceOn && tc.isEntryEnabled()) // F743-425.CodRev
             Tr.entry(tc, "run: " + ivTimer.ivTaskId); // F743-425.CodRev
@@ -97,12 +93,9 @@ public abstract class TimerNpRunnable implements Runnable
         try // F743-425.CodRev
         {
             // Call the timeout method; last chance effort to abort if cancelled
-            if (!ivTimer.isIvDestroyed())
-            {
+            if (!ivTimer.isIvDestroyed()) {
                 doWork();
-            }
-            else
-            {
+            } else {
                 if (isTraceOn && tc.isEntryEnabled())
                     Tr.exit(tc, "Timer has been cancelled; aborting");
                 return;
@@ -117,16 +110,15 @@ public abstract class TimerNpRunnable implements Runnable
             // Do not FFDC... that has already been done when the method failed
             // All exceptions from timeout methods are system exceptions...
             // indicating the timeout method failed, and should be retried. d667153
-            if (isTraceOn && tc.isDebugEnabled())
-                Tr.debug(tc, "NP Timer failed : " + ex.getClass().getName() +
-                             ":" + ex.getMessage(), ex);
+            if (isTraceOn && tc.isDebugEnabled()) {
+                Tr.debug(tc, "NP Timer failed : " + ex.getClass().getName() + ":" + ex.getMessage(), ex);
+            }
 
             if ((ivRetryLimit != -1) && // not configured to retry indefinitely
                 (ivRetries >= ivRetryLimit)) // and retry limit reached
             {
                 // Note: ivRetryLimit==0 means no retries at all
-                // Note2: we're leaving the timer in a defunct state.  It will never fire again, but
-                // we're not destroying it, in case the user wants to query timers in diagnosing retry failures
+                ivTimer.scheduleNext();
                 Tr.warning(tc, "NP_TIMER_RETRY_LIMIT_REACHED_CNTR0179W", ivRetryLimit);
                 if (isTraceOn && tc.isEntryEnabled())
                     Tr.exit(tc, "Timer retry limit has been reached; aborting");
@@ -136,13 +128,10 @@ public abstract class TimerNpRunnable implements Runnable
             ivRetries++;
 
             // begin 597753
-            if (ivRetries == 1)
-            {
+            if (ivRetries == 1) {
                 // do first retry immediately, by re-entering this method
                 run();
-            }
-            else
-            {
+            } else {
                 // re-schedule the alarm to go off after the retry interval
                 // (if timer had not been canceled)
                 ivTimer.scheduleRetry(ivRetryInterval); // RTC107334
@@ -155,27 +144,24 @@ public abstract class TimerNpRunnable implements Runnable
 
     /**
      * Performs the work associated with the scheduled EJB Timer. <p>
-     * 
+     *
      * When the EJB Timer expires, this method will be called. A wrapper for
      * the target TimedObject bean will be obtained, and the ejbTimeout method
      * will be invoked. <p>
-     * 
+     *
      * The wrapper is similar to a generated Local wrapper, and will activate
      * the bean as needed. It will also perform exception handling /
      * mapping as required by the EJB Specification.
      **/
-    private void doWork()
-    {
+    private void doWork() {
         boolean isTraceOn = TraceComponent.isAnyTracingEnabled();
         if (isTraceOn && tc.isEntryEnabled())
             Tr.entry(tc, "doWork: " + ivTimer);
 
         TimedObjectWrapper timedObject;
-        try
-        {
+        try {
             ivBeanId = ivBeanId.getInitializedBeanId(); // F743-506CodRev
-        } catch (Exception ex)
-        {
+        } catch (Exception ex) {
             FFDCFilter.processException(ex, CLASS_NAME + ".doWork", "247", this);
             if (isTraceOn && tc.isEntryEnabled())
                 Tr.exit(tc, "doWork: " + ex);
@@ -189,12 +175,10 @@ public abstract class TimerNpRunnable implements Runnable
         // Invoke ejbTimeout on the wrapper.  No need to handle exceptions,
         // as the wrapper will have already handled/mapped any exceptions
         // as expected.
-        try
-        {
+        try {
             ivTimer.ivTimeoutThread = Thread.currentThread(); // d595255
             timedObject.invokeCallback(ivTimer, ivMethodId, false); // F743-506
-        } finally
-        {
+        } finally {
             ivTimer.ivTimeoutThread = null; // d595255
             // Always return the TimedObjectWrapper to the pool.
             home.putTimedObjectWrapper(timedObject);
@@ -206,7 +190,7 @@ public abstract class TimerNpRunnable implements Runnable
 
     /**
      * Schedules the first expiration for a non-persistent timer.
-     * 
+     *
      * @param expiration the point in time at which the first scheduled
      *            expiration must occur; specified in milliseconds since
      *            the standard base time (the epoch).
@@ -215,7 +199,7 @@ public abstract class TimerNpRunnable implements Runnable
 
     /**
      * Schedules recurring timers for the next scheduled expiration.
-     * 
+     *
      * @param nextExpiration the point in time at which the next scheduled
      *            expiration must occur; specified in milliseconds since
      *            the standard base time (the epoch).
@@ -224,7 +208,7 @@ public abstract class TimerNpRunnable implements Runnable
 
     /**
      * Schedules a timer that has failed to run to be retried.
-     * 
+     *
      * @param retryInterval the time in milliseconds from the current time when
      *            the timer should expire and attempt to run again.
      */
@@ -232,11 +216,11 @@ public abstract class TimerNpRunnable implements Runnable
 
     /**
      * Cancels the timer task and all future invocations. <p>
-     * 
+     *
      * If the timer task has already started to run, then this method
      * has no effect. If the timer task has no further expirations or
      * has previously been cancelled, then this method has no effect. <p>
-     * 
+     *
      * Aside from canceling a timer, implementations of this method
      * may also perform resource cleanup; depending on the underlying
      * scheduler implementation.
