d67700764f241ba5959be2aa47b5ab2a17b50069
Ut Le <utle@us.ibm.com>
2018-08-16 08:57:06 -0500

    create principal bean

--- dev/com.ibm.ws.security.mp.jwt.cdi/src/com/ibm/ws/security/mp/jwt/cdi/JwtCDIExtension.java
+++ dev/com.ibm.ws.security.mp.jwt.cdi/src/com/ibm/ws/security/mp/jwt/cdi/JwtCDIExtension.java
@@ -34,8 +34,8 @@ import javax.enterprise.inject.spi.ProcessInjectionTarget;
 import javax.inject.Provider;
 
 import org.eclipse.microprofile.jwt.Claim;
-import org.eclipse.microprofile.jwt.Claims;
 import org.eclipse.microprofile.jwt.ClaimValue;
+import org.eclipse.microprofile.jwt.Claims;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.osgi.service.component.annotations.Component;
 
@@ -46,13 +46,14 @@ import com.ibm.ws.cdi.extension.WebSphereCDIExtension;
 /**
  *
  */
-@Component(service = WebSphereCDIExtension.class, property = { "api.classes=org.eclipse.microprofile.jwt.Claim;org.eclipse.microprofile.jwt.Claims;org.eclipse.microprofile.jwt.ClaimValue;org.eclipse.microprofile.jwt.JsonWebToken;javax.json.JsonValue;javax.json.JsonNumber;javax.json.JsonString;javax.json.JsonStructure;javax.json.JsonArray;javax.json.JsonObject" }, immediate = true)
+@Component(service = WebSphereCDIExtension.class, property = { "api.classes=org.eclipse.microprofile.jwt.Claim;org.eclipse.microprofile.jwt.Claims;org.eclipse.microprofile.jwt.ClaimValue;org.eclipse.microprofile.jwt.JsonWebToken;javax.json.JsonValue;javax.json.JsonNumber;javax.json.JsonString;javax.json.JsonStructure;javax.json.JsonArray;javax.json.JsonObject, java.security.Principal" }, immediate = true)
 public class JwtCDIExtension implements Extension, WebSphereCDIExtension {
 
     private static final TraceComponent tc = Tr.register(JwtCDIExtension.class);
 
     private final Map<Claim, Set<Type>> injectionTypes = new HashMap<Claim, Set<Type>>();
     private boolean addJsonWebTokenBean = false;
+    private boolean addPrincipalBean = false;
 
     public void beforeBeanDiscovery(@Observes BeforeBeanDiscovery bbd, BeanManager bm) {
         AnnotatedType<ClaimProducer> producer = bm.createAnnotatedType(ClaimProducer.class);
@@ -81,13 +82,13 @@ public class JwtCDIExtension implements Extension, WebSphereCDIExtension {
                 Throwable configException = null;
                 AnnotatedType<?> annotatedType = pit.getAnnotatedType();
                 Class<?> annotatedClass = annotatedType.getJavaClass();
-                
+
                 if (type instanceof ParameterizedType) {
                     ParameterizedType pType = (ParameterizedType) type;
                     configException = processParameterizedType(pit, annotatedClass, injectionPoint, pType, classLoader, claim);
                 } else {
                     if (ClaimValue.class.isAssignableFrom((Class<?>) type) == false && (annotatedClass.getAnnotationsByType(ApplicationScoped.class).length != 0 ||
-                                    annotatedClass.getAnnotationsByType(SessionScoped.class).length != 0)) {
+                                                                                        annotatedClass.getAnnotationsByType(SessionScoped.class).length != 0)) {
                         String translatedMsg = Tr.formatMessage(tc, "MPJWT_CDI_INVALID_SCOPE_FOR_RAW_TYPE", injectionPoint);
                         pit.addDefinitionError(new DeploymentException(translatedMsg));
                     } else {
@@ -155,13 +156,17 @@ public class JwtCDIExtension implements Extension, WebSphereCDIExtension {
         if (addJsonWebTokenBean || injectionTypes.isEmpty() == false) {
             abd.addBean(new JsonWebTokenBean(beanManager));
         }
+        if (addPrincipalBean || injectionTypes.isEmpty() == false) {
+            abd.addBean(new PrincipalBean(beanManager));
+        }
 
         if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
             Tr.exit(tc, "afterBeanDiscovery");
         }
     }
 
-    private Throwable processParameterizedType(ProcessInjectionTarget<?> pit, Class<?> annotatedClass, InjectionPoint injectionPoint, ParameterizedType injectionType, ClassLoader classLoader, Claim claim) {
+    private Throwable processParameterizedType(ProcessInjectionTarget<?> pit, Class<?> annotatedClass, InjectionPoint injectionPoint, ParameterizedType injectionType,
+                                               ClassLoader classLoader, Claim claim) {
         if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
             Tr.entry(tc, "processParameterizedType", pit, annotatedClass, injectionPoint, injectionType, classLoader, claim);
         }
@@ -175,14 +180,14 @@ public class JwtCDIExtension implements Extension, WebSphereCDIExtension {
         } else if (Optional.class.isAssignableFrom((Class<?>) rawType)) {
             Type[] aTypes = injectionType.getActualTypeArguments();
             returnType = aTypes[0];
-            
+
             Type type = returnType;
             if (type instanceof ParameterizedType) {
                 type = ((ParameterizedType) type).getRawType();
             }
-            
+
             if (ClaimValue.class.isAssignableFrom((Class<?>) type) == false && (annotatedClass.getAnnotationsByType(ApplicationScoped.class).length != 0 ||
-                            annotatedClass.getAnnotationsByType(SessionScoped.class).length != 0)) {
+                                                                                annotatedClass.getAnnotationsByType(SessionScoped.class).length != 0)) {
                 String translatedMsg = Tr.formatMessage(tc, "MPJWT_CDI_INVALID_SCOPE_FOR_RAW_TYPE", injectionPoint);
                 pit.addDefinitionError(new DeploymentException(translatedMsg));
             } else {
@@ -194,7 +199,7 @@ public class JwtCDIExtension implements Extension, WebSphereCDIExtension {
             }
 
             if (ClaimValue.class.isAssignableFrom((Class<?>) rawType) == false && (annotatedClass.getAnnotationsByType(ApplicationScoped.class).length != 0 ||
-                            annotatedClass.getAnnotationsByType(SessionScoped.class).length != 0)) {
+                                                                                   annotatedClass.getAnnotationsByType(SessionScoped.class).length != 0)) {
                 String translatedMsg = Tr.formatMessage(tc, "MPJWT_CDI_INVALID_SCOPE_FOR_RAW_TYPE", injectionPoint);
                 pit.addDefinitionError(new DeploymentException(translatedMsg));
             } else {
@@ -296,6 +301,7 @@ public class JwtCDIExtension implements Extension, WebSphereCDIExtension {
         }
 
         addJsonWebTokenBean = true;
+        addPrincipalBean = true;
 
         if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
             Tr.exit(tc, "processInjectionPoint");
---
+++ dev/com.ibm.ws.security.mp.jwt.cdi/src/com/ibm/ws/security/mp/jwt/cdi/PrincipalBean.java
@@ -0,0 +1,164 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.security.mp.jwt.cdi;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.security.Principal;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.annotation.Priority;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.Alternative;
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.enterprise.inject.spi.PassivationCapable;
+import javax.enterprise.util.AnnotationLiteral;
+import javax.enterprise.util.TypeLiteral;
+import javax.security.auth.Subject;
+
+import com.ibm.websphere.ras.Tr;
+import com.ibm.websphere.ras.TraceComponent;
+import com.ibm.ws.security.context.SubjectManager;
+
+/**
+ *
+ */
+@Alternative
+@Priority(100)
+@RequestScoped
+//@Specializes
+public class PrincipalBean implements Bean<Principal>, PassivationCapable {
+
+    private static final TraceComponent tc = Tr.register(PrincipalBean.class);
+
+    private final String name;
+    private final String id;
+    private final Class<? extends Annotation> scope = RequestScoped.class;
+    private final HashSet<Annotation> qualifiers;
+    private final Type type;
+    private final Set<Type> types;
+    private final Set<InjectionPoint> injectionPoints;
+
+    public PrincipalBean(BeanManager beanManager) {
+        qualifiers = new HashSet<Annotation>();
+        qualifiers.add(new AnnotationLiteral<Default>() {});
+        type = new TypeLiteral<Principal>() {}.getType();
+        types = Collections.singleton(type);
+        injectionPoints = Collections.emptySet(); //Collections.singleton(ip);
+
+        this.name = this.getClass().getName() + "[" + type + "]";
+        this.id = beanManager.hashCode() + "#" + this.name;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Principal create(CreationalContext<Principal> creationalContext) {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.entry(tc, "create", creationalContext);
+        }
+
+        Principal Principal = null;
+
+        Subject subject = new SubjectManager().getCallerSubject();
+        if (subject != null) {
+            Set<Principal> PrincipalPrincipal = subject.getPrincipals(Principal.class);
+
+            if (!PrincipalPrincipal.isEmpty()) {
+                Principal = PrincipalPrincipal.iterator().next();
+            }
+        }
+
+        if (Principal == null) {
+            Tr.error(tc, "MPJWT_CDI_PRINCIPAL_UNAVAILABLE"); // CWWKS5604E
+            // limit info passed back to app.
+            throw new javax.enterprise.inject.CreationException();
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
+            Tr.exit(tc, "create", Principal);
+        }
+        return Principal;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void destroy(Principal arg0, CreationalContext<Principal> creationalContext) {
+        creationalContext.release();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Set<Annotation> getQualifiers() {
+        return qualifiers;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Class<? extends Annotation> getScope() {
+        return scope;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Set<Class<? extends Annotation>> getStereotypes() {
+        // TODO Auto-generated method stub
+        return Collections.emptySet();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Set<Type> getTypes() {
+        return types;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean isAlternative() {
+        return true;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Class<?> getBeanClass() {
+        return Principal.class;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Set<InjectionPoint> getInjectionPoints() {
+        return injectionPoints;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean isNullable() {
+        return false;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getId() {
+        return id;
+    }
+
+}
