5b1f716ebaf5a9726653c64a354decae5221a9f1
Benjamin Confino <benjamic@uk.ibm.com>
2018-09-05 11:22:57 +0100

    com.ibm.ws.messaging.comms_fat passing, FFDCs not checked.

---
+++ dev/com.ibm.ws.messaging.comms_ol_fat/fat/src/com/ibm/ws/messaging/comms/fat/FATSuite.java
@@ -0,0 +1,17 @@
+package com.ibm.ws.messaging.comms.fat;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	            
+				FeatureUpdate.class,
+				WasJmsOutBoundTest.class
+	})
+
+public class FATSuite {}
