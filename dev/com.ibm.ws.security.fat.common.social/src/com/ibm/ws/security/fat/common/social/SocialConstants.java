48e5ce1d8439f3aed69e90c16fa1dd5236ac17c4
Adam Yoho <ayoho@us.ibm.com>
2018-09-11 14:47:22 -0500

    Initial drop for social login OIDC certification tests

--- dev/com.ibm.ws.security.fat.common.social/src/com/ibm/ws/security/fat/common/social/SocialConstants.java
+++ dev/com.ibm.ws.security.fat.common.social/src/com/ibm/ws/security/fat/common/social/SocialConstants.java
@@ -6,14 +6,19 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
+ * IBM Corporation - initial API and implementation
  *******************************************************************************/
-package com.ibm.ws.security.social.fat;
+package com.ibm.ws.security.fat.common.social;
 
 import java.io.File;
 
-public class SocialConstants {
+import com.ibm.ws.security.fat.common.Constants;
+import com.ibm.ws.security.social.internal.Oauth2LoginConfigImpl;
 
-    public static String CONFIGS_DIR = "configs" + File.separator;
+public class SocialConstants extends Constants {
+
+    public static final String CONFIGS_DIR = "configs" + File.separator;
+
+    public static final String DEFAULT_CONTEXT_ROOT = Oauth2LoginConfigImpl.DEFAULT_CONTEXT_ROOT;
 
 }
