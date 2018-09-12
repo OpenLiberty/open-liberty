9bbfdd19b45d8403dd2fad4e0870e6601d284053
chdavid <chdavid@ca.ibm.com>
2018-09-12 12:21:21 -0400

    Revert "Revert "Fix to remove early messages and early traces from BaseTraceService""
    This reverts commit 95e57caec5fe34f6e78c5d2f9b3fd571f049dbb3.
    
    Reverting "RevertFix to remove early messages and early traces from
    BaseTraceService"

--- dev/com.ibm.ws.logging/src/com/ibm/ws/logging/internal/impl/BaseTraceService.java
+++ dev/com.ibm.ws.logging/src/com/ibm/ws/logging/internal/impl/BaseTraceService.java
@@ -18,6 +18,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Handler;
 import java.util.logging.Level;
@@ -125,6 +127,8 @@ public class BaseTraceService implements TrService {
     protected static RecursionCounter counterForTraceSource = new RecursionCounter();
     protected static RecursionCounter counterForLogSource = new RecursionCounter();
 
+    private static final int MINUTE = 60000;
+
     /**
      * Trivial interface for writing "trace" records (this includes logging to messages.log)
      */
@@ -185,8 +189,8 @@ public class BaseTraceService implements TrService {
     private volatile Collection<String> hideMessageids;
 
     /** Early msgs issued before MessageRouter is started. */
-    protected final Queue<RoutedMessage> earlierMessages = new SimpleRotatingSoftQueue<RoutedMessage>(new RoutedMessage[100]);
-    protected final Queue<RoutedMessage> earlierTraces = new SimpleRotatingSoftQueue<RoutedMessage>(new RoutedMessage[200]);
+    protected volatile Queue<RoutedMessage> earlierMessages = new SimpleRotatingSoftQueue<RoutedMessage>(new RoutedMessage[100]);
+    protected volatile Queue<RoutedMessage> earlierTraces = new SimpleRotatingSoftQueue<RoutedMessage>(new RoutedMessage[200]);
 
     protected volatile LogSource logSource = null;
     protected volatile TraceSource traceSource = null;
@@ -219,6 +223,9 @@ public class BaseTraceService implements TrService {
     public BaseTraceService() {
         systemOut = new SystemLogHolder(LoggingConstants.SYSTEM_OUT, System.out);
         systemErr = new SystemLogHolder(LoggingConstants.SYSTEM_ERR, System.err);
+
+        Timer earlyMessageTraceKiller_Timer = new Timer();
+        earlyMessageTraceKiller_Timer.schedule(new EarlyMessageTraceCleaner(), 5 * MINUTE); // 5 minutes wait time
     }
 
     /**
@@ -681,10 +688,14 @@ public class BaseTraceService implements TrService {
         }
         if (internalMsgRouter != null) {
             retMe &= internalMsgRouter.route(routedMessage);
-        } else {
+        } else if (earlierMessages != null) {
             String message = formatter.messageLogFormat(routedMessage.getLogRecord(), routedMessage.getFormattedVerboseMsg());
             RoutedMessage specialRoutedMessage = new RoutedMessageImpl(routedMessage.getFormattedMsg(), routedMessage.getFormattedVerboseMsg(), message, routedMessage.getLogRecord());
-            earlierMessages.add(specialRoutedMessage);
+            synchronized (this) {
+                if (earlierMessages != null) {
+                    earlierMessages.add(specialRoutedMessage);
+                }
+            }
         }
         return retMe;
     }
@@ -713,8 +724,12 @@ public class BaseTraceService implements TrService {
                             WsTraceRouter internalTrRouter = internalTraceRouter.get();
                             if (internalTrRouter != null) {
                                 retMe &= internalTrRouter.route(routedTrace);
-                            } else {
-                                earlierTraces.add(routedTrace);
+                            } else if (earlierTraces != null) {
+                                synchronized (this) {
+                                    if (earlierTraces != null) {
+                                        earlierTraces.add(routedTrace);
+                                    }
+                                }
                             }
                         }
                     }
@@ -894,7 +909,16 @@ public class BaseTraceService implements TrService {
         // NOT add any more messages to the earlierMessages queue.
         // The MessageRouter basically owns the earlierMessages queue
         // from now on.
-        msgRouter.setEarlierMessages(earlierMessages);
+
+        if (earlierMessages != null) {
+            synchronized (this) {
+                if (earlierMessages != null) {
+                    msgRouter.setEarlierMessages(earlierMessages);
+                }
+            }
+        } else {
+            msgRouter.setEarlierMessages(null);
+        }
     }
 
     /**
@@ -917,7 +941,15 @@ public class BaseTraceService implements TrService {
         // NOT add any more messages to the earlierMessages queue.
         // The MessageRouter basically owns the earlierMessages queue
         // from now on.
-        traceRouter.setEarlierTraces(earlierTraces);
+        if (earlierTraces != null) {
+            synchronized (this) {
+                if (earlierTraces != null) {
+                    traceRouter.setEarlierTraces(earlierTraces);
+                }
+            }
+        } else {
+            traceRouter.setEarlierTraces(null);
+        }
     }
 
     /**
@@ -1196,4 +1228,24 @@ public class BaseTraceService implements TrService {
         }
     }
 
+    private class EarlyMessageTraceCleaner extends TimerTask {
+        @Override
+        public void run() {
+            synchronized (BaseTraceService.this) {
+                earlierMessages = null;
+                earlierTraces = null;
+
+                /*
+                 * With earlierMessages and earlierTraces set to null now,
+                 * calling setWsMessageRouter and setTraceRouter will
+                 * subsequently null out the earlyMessageQueue and earlyTraceQueue
+                 * in their respective routers
+                 */
+                if (internalMessageRouter.get() != null)
+                    BaseTraceService.this.setWsMessageRouter(internalMessageRouter.get());
+                if (internalTraceRouter.get() != null)
+                    BaseTraceService.this.setTraceRouter(internalTraceRouter.get());
+            }
+        }
+    }
 }
