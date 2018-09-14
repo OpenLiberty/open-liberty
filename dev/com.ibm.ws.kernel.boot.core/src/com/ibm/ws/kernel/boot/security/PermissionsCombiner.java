b3aa04e95b7b6bb16493338851f3733e2739b23d
Chris Webster <websterc87@hotmail.com>
2018-09-14 12:47:48 +0100

    Revert "Update ClassLoadingService to depend on PermissionManager instead of the other way around for issue #1438"

--- dev/com.ibm.ws.kernel.boot.core/src/com/ibm/ws/kernel/boot/security/PermissionsCombiner.java
+++ dev/com.ibm.ws.kernel.boot.core/src/com/ibm/ws/kernel/boot/security/PermissionsCombiner.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2018 IBM Corporation and others.
+ * Copyright (c) 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,8 +12,6 @@ package com.ibm.ws.kernel.boot.security;
 
 import java.security.CodeSource;
 import java.security.PermissionCollection;
-import java.security.ProtectionDomain;
-import java.util.Map;
 
 /**
  *
@@ -22,13 +20,11 @@ public interface PermissionsCombiner {
 
     /**
      * Combine the static permissions with the configured permissions
-     *
+     * 
      * @param staticPolicyPermissions The static permissions.
      * @param codesource The code source to get the combined permissions for.
      * @return The combined permissions.
      */
     PermissionCollection getCombinedPermissions(PermissionCollection staticPolicyPermissions, CodeSource codesource);
-
-    Map<String, ProtectionDomain> getProtectionDomains();
 
 }
