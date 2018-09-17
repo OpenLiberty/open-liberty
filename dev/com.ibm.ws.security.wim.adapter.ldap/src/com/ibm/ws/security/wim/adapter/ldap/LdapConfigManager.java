10bcaacb38eeb5cb555f44c835f47187c85da8af
Arun Giridharan <ahgiridh@ibm.com>
2018-09-17 08:50:30 -0500

    added Java permissions to remove the ACE reports

--- dev/com.ibm.ws.security.wim.adapter.ldap/src/com/ibm/ws/security/wim/adapter/ldap/LdapConfigManager.java
+++ dev/com.ibm.ws.security.wim.adapter.ldap/src/com/ibm/ws/security/wim/adapter/ldap/LdapConfigManager.java
@@ -664,8 +664,13 @@ public class LdapConfigManager {
                     } else
                         hasLoginProperties = false;
                 }
-                if (ldapEntity != null)
-                    ldapEntity.addPropertyAttributeMap(SchemaConstants.PROP_PRINCIPAL_NAME, iLoginAttrs.get(0));
+                if (ldapEntity != null) {
+                    try {
+                        ldapEntity.addPropertyAttributeMap(SchemaConstants.PROP_PRINCIPAL_NAME, iLoginAttrs.get(0));
+                    } catch (Exception e) {
+                        throw new IllegalArgumentException("userFilter requires a %v value");
+                    }
+                }
             }
 
             // Parse the Group filter and extract the applicable objectclass names
@@ -1946,7 +1951,7 @@ public class LdapConfigManager {
      * Return the sub-list of properties, for the given entity, supported by this repository, from a given list of properties.
      *
      * @param inEntityTypes : List of entity types
-     * @param propNames : List of property names read from data object
+     * @param propNames     : List of property names read from data object
      * @return list of properties supported by repository for given entity type
      *         If the list propNames contain VALUE_ALL_PROPERTIES i.e '*', then return the list of properties without any modification
      *         Code will handle '*' later on
@@ -1988,7 +1993,7 @@ public class LdapConfigManager {
      * This is an overloaded method to support getSupportedProperties(String, List)
      *
      * @param ldapEntity : A given LDAP entity
-     * @param propNames : List of property names read from data object
+     * @param propNames  : List of property names read from data object
      * @return list of properties supported by repository for given LDAP entity
      *         If the list propNames contain VALUE_ALL_PROPERTIES i.e '*', then return the list of properties without any modification
      *         Code will handle '*' later on
