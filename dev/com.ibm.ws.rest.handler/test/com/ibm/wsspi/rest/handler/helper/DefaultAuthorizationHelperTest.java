ca1fe7d574d72f40fb42fdf583a2b99817d53cba
Michael C. Thompson <mcthomps@us.ibm.com>
2018-09-23 07:41:44 -0500

    Fix typo in DefaultAuthorizationHelperTest.java: Vewier -> Viewer

--- dev/com.ibm.ws.rest.handler/test/com/ibm/wsspi/rest/handler/helper/DefaultAuthorizationHelperTest.java
+++ dev/com.ibm.ws.rest.handler/test/com/ibm/wsspi/rest/handler/helper/DefaultAuthorizationHelperTest.java
@@ -54,11 +54,11 @@ public class DefaultAuthorizationHelperTest {
      * {@link com.ibm.wsspi.rest.handler.helper.DefaultAuthorizationHelper#checkAdministratorRole(com.ibm.wsspi.rest.handler.RESTRequest, com.ibm.wsspi.rest.handler.RESTResponse)}.
      */
     @Test
-    public void get_with_Vewier_is_authorized() throws Exception {
+    public void get_with_Viewer_is_authorized() throws Exception {
         DefaultAuthorizationHelper authz = new DefaultAuthorizationHelper();
-        RESTRequest request = new MockRESTRequest("GET", "Vewier");
+        RESTRequest request = new MockRESTRequest("GET", "Viewer");
         RESTResponse response = new MockRESTResponse();
-        assertTrue("GET method should be authorized when the user is in the Vewier role",
+        assertTrue("GET method should be authorized when the user is in the Viewer role",
                    authz.checkAdministratorRole(request, response));
     }
 
@@ -67,11 +67,11 @@ public class DefaultAuthorizationHelperTest {
      * {@link com.ibm.wsspi.rest.handler.helper.DefaultAuthorizationHelper#checkAdministratorRole(com.ibm.wsspi.rest.handler.RESTRequest, com.ibm.wsspi.rest.handler.RESTResponse)}.
      */
     @Test
-    public void post_with_Vewier_is_unauthorized() throws Exception {
+    public void post_with_Viewer_is_unauthorized() throws Exception {
         DefaultAuthorizationHelper authz = new DefaultAuthorizationHelper();
-        RESTRequest request = new MockRESTRequest("POST", "Vewier");
+        RESTRequest request = new MockRESTRequest("POST", "Viewer");
         RESTResponse response = new MockRESTResponse(403, "Administrator role needed.");
-        assertFalse("POST method should be authorized when the user is in the Vewier role",
+        assertFalse("POST method should be authorized when the user is in the Viewer role",
                     authz.checkAdministratorRole(request, response));
     }
 
@@ -83,7 +83,7 @@ public class DefaultAuthorizationHelperTest {
     public void get_with_no_role_is_unauthorized() throws Exception {
         DefaultAuthorizationHelper authz = new DefaultAuthorizationHelper();
         RESTRequest request = new MockRESTRequest("GET", "");
-        RESTResponse response = new MockRESTResponse(403, "Administrator or Vewier role needed.");
+        RESTResponse response = new MockRESTResponse(403, "Administrator or Viewer role needed.");
         assertFalse("GET method should not be authorized if the user has no role", authz.checkAdministratorRole(request, response));
     }
 
