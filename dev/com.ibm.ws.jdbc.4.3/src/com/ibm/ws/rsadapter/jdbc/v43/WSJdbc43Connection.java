8ece0c8edf1cddb284bd410bf1b8bb28e87fb3e4
Nathan Rauh <nathan.rauh@us.ibm.com>
2018-09-26 16:26:04 -0500

    Issue #4735 Allow setShardingKey on Connection, with basic test

--- dev/com.ibm.ws.jdbc.4.3/src/com/ibm/ws/rsadapter/jdbc/v43/WSJdbc43Connection.java
+++ dev/com.ibm.ws.jdbc.4.3/src/com/ibm/ws/rsadapter/jdbc/v43/WSJdbc43Connection.java
@@ -14,7 +14,11 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.ShardingKey;
 
+import com.ibm.ws.ffdc.FFDCFilter;
+import com.ibm.ws.jdbc.osgi.JDBCRuntimeVersion;
 import com.ibm.ws.rsadapter.AdapterUtil;
+import com.ibm.ws.rsadapter.ConnectionSharing;
+import com.ibm.ws.rsadapter.impl.WSConnectionRequestInfoImpl;
 import com.ibm.ws.rsadapter.impl.WSRdbManagedConnectionImpl;
 import com.ibm.ws.rsadapter.jdbc.v41.WSJdbc41Connection;
 
@@ -46,12 +50,65 @@ public class WSJdbc43Connection extends WSJdbc41Connection implements Connection
 
     @Override
     public void setShardingKey(ShardingKey shardingKey) throws SQLException {
-        throw new UnsupportedOperationException();
+        activate();
+
+        try {
+            // Setters are not permitted when multiple handles are sharing the same ManagedConnection,
+            // except when the specified value is the same as the current value, which is a no-op.
+            if (managedConn.getHandleCount() > 1 && !AdapterUtil.match(shardingKey, managedConn.getCurrentShardingKey()))
+                throw createSharingException("setShardingKey");
+
+            managedConn.setShardingKeys(shardingKey, JDBCRuntimeVersion.SUPER_SHARDING_KEY_UNCHANGED);
+
+            // Update the connection request information with the new value, so that
+            // requests for shared connections will match based on the updated criteria.
+            if (managedConn.connectionSharing == ConnectionSharing.MatchCurrentState) {
+                WSConnectionRequestInfoImpl cri = (WSConnectionRequestInfoImpl) managedConn.getConnectionRequestInfo();
+                if (!cri.isCRIChangable())
+                    managedConn.setCRI(cri = WSConnectionRequestInfoImpl.createChangableCRIFromNon(cri));
+
+                cri.setShardingKey(shardingKey);
+            }
+        } catch (SQLException x) {
+            FFDCFilter.processException(x, getClass().getName(), "71", this);
+            throw proccessSQLException(x);
+        } catch (NullPointerException x) {
+            // No FFDC code needed; we might be closed.
+            throw runtimeXIfNotClosed(x);
+        }
     }
 
     @Override
     public void setShardingKey(ShardingKey shardingKey, ShardingKey superShardingKey) throws SQLException {
-        throw new UnsupportedOperationException();
+        activate();
+
+        try {
+            // Setters are not permitted when multiple handles are sharing the same ManagedConnection,
+            // except when the specified value is the same as the current value, which is a no-op.
+            if (managedConn.getHandleCount() > 1 &&
+                (!AdapterUtil.match(shardingKey, managedConn.getCurrentShardingKey()) ||
+                 !AdapterUtil.match(superShardingKey, managedConn.getCurrentSuperShardingKey())))
+                throw createSharingException("setShardingKey");
+
+            managedConn.setShardingKeys(shardingKey, superShardingKey);
+
+            // Update the connection request information with the new value, so that
+            // requests for shared connections will match based on the updated criteria.
+            if (managedConn.connectionSharing == ConnectionSharing.MatchCurrentState) {
+                WSConnectionRequestInfoImpl cri = (WSConnectionRequestInfoImpl) managedConn.getConnectionRequestInfo();
+                if (!cri.isCRIChangable())
+                    managedConn.setCRI(cri = WSConnectionRequestInfoImpl.createChangableCRIFromNon(cri));
+
+                cri.setShardingKey(shardingKey);
+                cri.setSuperShardingKey(superShardingKey);
+            }
+        } catch (SQLException x) {
+            FFDCFilter.processException(x, getClass().getName(), "104", this);
+            throw proccessSQLException(x);
+        } catch (NullPointerException x) {
+            // No FFDC code needed; we might be closed.
+            throw runtimeXIfNotClosed(x);
+        }
     }
 
 }
