58688d6a5e7ec0521d586fc67420afab7b31caba
Gordon Hutchison <gordon.hutchison@gmail.com>
2018-09-28 12:03:58 +0100

    Initial set of files for Engine Producer code
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>

---
+++ dev/com.ibm.ws.microprofile.reactive.streams.operators.cdi/src/com/ibm/ws/microprofile/reactive/streams/cdi/ReactiveStreamsEngineInjectionExtension.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.microprofile.reactive.streams.cdi;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.AnnotatedType;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.BeforeBeanDiscovery;
+import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.enterprise.inject.spi.ProcessInjectionTarget;
+
+import org.osgi.service.component.annotations.Component;
+
+import com.ibm.websphere.ras.Tr;
+import com.ibm.websphere.ras.TraceComponent;
+import com.ibm.ws.cdi.extension.WebSphereCDIExtension;
+
+/*
+ * This is CDI extension for CDI to identify there is a bean which produces ReactiveStreamsEngine
+ * We need to register this extension so that CDI will identify this bundle needs scanning
+ * Addition to this extension,  we need to have beens.xml file in META-INF directory for the
+ * WELD implementation to scan the bean which uses @Produces
+ */
+@Component(service = WebSphereCDIExtension.class, property = { "api.classes=org.eclipse.microprofile.reactive.streams.spi.ReactiveStreamsEngine" }, immediate = true)
+public class ReactiveStreamsEngineInjectionExtension implements Extension, WebSphereCDIExtension {
+    
+    private final static TraceComponent tc = Tr.register(ReactiveStreamsEngineInjectionExtension.class);
+
+    public ReactiveStreamsEngineInjectionExtension() {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+            Tr.debug(tc, "ReactiveStreamsEngineInjectionExtension construction ");
+        }
+    }
+    
+    void processInjectionTarget(@Observes ProcessInjectionTarget<?> pit) {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+            Tr.debug(tc, "ReactiveStreamsEngineInjectionExtension pit ");
+        }
+    }
+
+    
+    void beforeBeanDiscovery(@Observes BeforeBeanDiscovery bbd, BeanManager bm) {
+        AnnotatedType<ReactiveStreamsEngineProducerBean> producer = bm.createAnnotatedType(ReactiveStreamsEngineProducerBean.class);
+        bbd.addAnnotatedType(producer);
+        
+        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+            Tr.debug(tc, "ReactiveStreamsEngineInjectionExtension BeforeBeanDiscoveryCalled added producer " + producer);
+        }
+    }
+    
+    protected Throwable processParameterizedType(InjectionPoint injectionPoint, ParameterizedType pType, ClassLoader classLoader) {
+        Throwable configException = null;
+        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+            Tr.debug(tc, "ReactiveStreamsEngineInjectionExtension ppt");
+        }
+        return configException;
+    }
+
+    protected Throwable processConversionType(InjectionPoint injectionPoint, Type injectionType, ClassLoader classLoader,
+                                              boolean optional) {
+        Throwable configException = null;
+        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+            Tr.debug(tc, "ReactiveStreamsEngineInjectionExtension pct");
+        }
+        return configException;
+    }
+}
---
+++ dev/com.ibm.ws.microprofile.reactive.streams.operators.cdi/src/com/ibm/ws/microprofile/reactive/streams/cdi/ReactiveStreamsEngineProducerBean.java
@@ -0,0 +1,91 @@
+/*******************************************************************************
+ * Copyright (c) 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.microprofile.reactive.streams.cdi;
+
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.AfterDeploymentValidation;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.enterprise.inject.spi.ProcessAnnotatedType;
+
+import org.eclipse.microprofile.reactive.streams.spi.ReactiveStreamsEngine;
+
+import com.ibm.websphere.ras.Tr;
+import com.ibm.websphere.ras.TraceComponent;
+
+/*
+ * This class is used for creating the Injected ReactiveStreamEngine instances.
+ */
+public class ReactiveStreamsEngineProducerBean {
+
+    private final Class<?> clientInterface;
+    private final BeanManager beanManager;
+
+    private final static TraceComponent tc = Tr.register(ReactiveStreamsEngineProducerBean.class);
+
+    public ReactiveStreamsEngineProducerBean(Class<?> clientInterface, BeanManager beanManager) {
+        this.clientInterface = clientInterface;
+        this.beanManager = beanManager;
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+            Tr.debug(tc, "<init> - clientInterface=" + clientInterface + " beanManager=" + beanManager);
+        }
+    }
+
+    @Produces
+    @ApplicationScoped
+    public ReactiveStreamsEngine getEngine(InjectionPoint injectionPoint) {
+        Iterator<ReactiveStreamsEngine> engines = ServiceLoader.load(ReactiveStreamsEngine.class).iterator();
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+            Tr.debug(tc, "Engines has next came out at " + engines.hasNext());
+        }
+
+        
+        if (engines.hasNext()) {
+            return engines.next();
+        } else {
+            throw new IllegalStateException(Tr.formatMessage(tc, "Unable to ServiceLoad ReactiveStreamsEngine from Producer."));
+        }
+
+    }
+
+    /**
+     * The CDI container will call this method once the injected
+     * ReactiveStreamsEngine goes out of scope
+     */
+    public void closeReactiveStreamsEngine(@Disposes ReactiveStreamsEngine context) {
+        // Left for now
+    }
+
+    
+    public void scanMyClasses(@Observes ProcessAnnotatedType pat)
+                    throws Exception
+    {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+            Tr.debug(tc, "Scanning class " + pat.getAnnotatedType().getJavaClass().getName() );
+        }
+    }
+
+    public void finished(@Observes AfterDeploymentValidation adv)
+    {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+            Tr.debug(tc, "We are almost finished with the CDI container boot now...");
+    
+        }
+    }
+}
