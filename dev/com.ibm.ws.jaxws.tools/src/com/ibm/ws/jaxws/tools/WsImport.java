d1b86136efd60b3ce344f615b7e7c3f1e5e3aabf
Nathan Mittlestat <nmittles@us.ibm.com>
2018-09-28 15:19:45 -0500

    JDK 11 updates to support wsgen and wsimport

--- dev/com.ibm.ws.jaxws.tools/src/com/ibm/ws/jaxws/tools/WsImport.java
+++ dev/com.ibm.ws.jaxws.tools/src/com/ibm/ws/jaxws/tools/WsImport.java
@@ -10,9 +10,12 @@
  *******************************************************************************/
 package com.ibm.ws.jaxws.tools;
 
+import java.io.File;
 import java.io.PrintStream;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
+import java.security.ProtectionDomain;
+import java.util.Arrays;
 
 import com.ibm.ws.jaxws.tools.internal.JaxWsToolsConstants;
 import com.ibm.ws.jaxws.tools.internal.JaxWsToolsUtil;
@@ -38,6 +41,47 @@ public class WsImport {
             }
         });
 
+        //Pass in the JWS and JAX-B APIs as a -classpath arg when Java 9 or above.
+        //Otherwise the javac process started by the tooling doesn't contain these APIs.
+        if (getMajorJavaVersion() > 8) {
+            String classpathValue = null;
+            Class<?> JAXB = null;
+            Class<?> WebService = null;
+            Class<?> Service = null;
+            try {
+                JAXB = Thread.currentThread().getContextClassLoader().loadClass("javax.xml.bind.JAXB");
+                WebService = Thread.currentThread().getContextClassLoader().loadClass("javax.jws.WebService");
+                Service = Thread.currentThread().getContextClassLoader().loadClass("javax.xml.ws.Service");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                System.exit(2);
+            }
+
+            classpathValue = getJarFileOfClass(JAXB);
+            classpathValue = classpathValue + File.pathSeparator + getJarFileOfClass(WebService);
+            classpathValue = classpathValue + File.pathSeparator + getJarFileOfClass(Service);
+
+            if (classpathValue != null) {
+                boolean classpathSet = false;
+
+                //Search for existing -cp or -classpath arg.
+                for (int i = 0; i < args.length; i++) {
+                    if (args[i].equals("-cp") || args[i].equals("-classpath")) {
+                        args[i + 1] = args[i + 1] + File.pathSeparator + classpathValue;
+                        classpathSet = true;
+                    }
+                }
+
+                //No existing -cp or -classpath arg was found so add it to the end (just before the SEI class).
+                if (!classpathSet && args.length > 0) {
+                    args = Arrays.copyOf(args, args.length + 2);
+                    args[args.length - 1] = args[args.length - 3]; //push SEI class to the end of args
+                    args[args.length - 2] = classpathValue; //insert paths for -classpath arg
+                    args[args.length - 3] = "-classpath"; //insert the -classpath arg
+                }
+            }
+        }
+
         System.exit(new WsimportTool(System.out).run(args) ? 0 : 1);
     }
 
@@ -59,5 +103,25 @@ public class WsImport {
         }
 
         return args.length > 0 && !helpExisted && !versionExisted && !targetExisted;
+    }
+
+    private static String getJarFileOfClass(final Class<?> javaClass) {
+        ProtectionDomain protectionDomain = AccessController.doPrivileged(
+                                                                          new PrivilegedAction<ProtectionDomain>() {
+                                                                              @Override
+                                                                              public ProtectionDomain run() {
+                                                                                  return javaClass.getProtectionDomain();
+                                                                              }
+                                                                          });
+
+        return protectionDomain.getCodeSource().getLocation().getPath();
+
+    }
+
+    private static int getMajorJavaVersion() {
+        String version = System.getProperty("java.version");
+        String[] versionElements = version.split("\\D");
+        int i = Integer.valueOf(versionElements[0]) == 1 ? 1 : 0;
+        return Integer.valueOf(versionElements[i]);
     }
 }
