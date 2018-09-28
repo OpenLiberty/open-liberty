b529e46203edf0fd3bc0f8e6c7d22e352fb9897b
Nathan Mittlestat <nmittles@us.ibm.com>
2018-09-28 15:20:53 -0500

    WsimportTool changes.

--- dev/com.ibm.ws.jaxws.tools/src/com/sun/tools/ws/wscompile/WsimportTool.java
+++ dev/com.ibm.ws.jaxws.tools/src/com/sun/tools/ws/wscompile/WsimportTool.java
@@ -1,17 +1,82 @@
-*DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * http://glassfish.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
 
 package com.sun.tools.ws.wscompile;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+
+import javax.xml.bind.JAXBPermission;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.ws.EndpointContext;
+
+import org.xml.sax.EntityResolver;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 import com.sun.codemodel.CodeWriter;
 import com.sun.codemodel.writer.ProgressCodeWriter;
 import com.sun.istack.tools.DefaultAuthenticator;
+import com.sun.istack.tools.ParallelWorldClassLoader;
 import com.sun.tools.ws.ToolVersion;
 import com.sun.tools.ws.api.TJavaGeneratorExtension;
 import com.sun.tools.ws.processor.generator.CustomExceptionGenerator;
 import com.sun.tools.ws.processor.generator.GeneratorBase;
+import com.sun.tools.ws.processor.generator.JwsImplGenerator;
 import com.sun.tools.ws.processor.generator.SeiGenerator;
 import com.sun.tools.ws.processor.generator.ServiceGenerator;
-import com.sun.tools.ws.processor.generator.JwsImplGenerator;
 import com.sun.tools.ws.processor.model.Model;
 import com.sun.tools.ws.processor.modeler.wsdl.ConsoleErrorReporter;
 import com.sun.tools.ws.processor.modeler.wsdl.WSDLModeler;
@@ -24,20 +89,6 @@ import com.sun.tools.ws.wsdl.parser.WSDLInternalizationLogic;
 import com.sun.tools.xjc.util.NullStream;
 import com.sun.xml.ws.api.server.Container;
 import com.sun.xml.ws.util.ServiceFinder;
-import com.sun.istack.tools.ParallelWorldClassLoader;
-import org.xml.sax.EntityResolver;
-import org.xml.sax.SAXParseException;
-
-import javax.xml.bind.JAXBPermission;
-import javax.xml.stream.*;
-import javax.xml.ws.EndpointContext;
-import java.io.*;
-import java.util.*;
-import java.text.MessageFormat;
-import java.util.jar.JarEntry;
-import java.util.jar.JarOutputStream;
-import org.xml.sax.Locator;
-import org.xml.sax.SAXException;
 
 /**
  * @author Vivek Pandey
@@ -503,10 +554,10 @@ public class WsimportTool {
             args.add("-d");
             args.add(classDir);
             args.add("-classpath");
-            args.add(classpathString);
+            args.add(classpathString + File.pathSeparator + options.classpath);
             //javac is not working in osgi as the url starts with a bundle
-            if (bootCP) {
-                args.add("-Xbootclasspath/p:"
+            if (bootCP && getMajorJavaVersion() < 9) {
+                args.add("-Xbootclasspath/a:"
                          + JavaCompilerHelper.getJarFile(EndpointContext.class)
                          + File.pathSeparator
                          + JavaCompilerHelper.getJarFile(JAXBPermission.class));
@@ -557,5 +608,12 @@ public class WsimportTool {
         System.out.println(WscompileMessages.WSIMPORT_HELP(WSIMPORT));
         System.out.println(WscompileMessages.WSIMPORT_USAGE_EXTENSIONS());
         System.out.println(WscompileMessages.WSIMPORT_USAGE_EXAMPLES());
+    }
+
+    private static int getMajorJavaVersion() {
+        String version = System.getProperty("java.version");
+        String[] versionElements = version.split("\\D");
+        int i = Integer.valueOf(versionElements[0]) == 1 ? 1 : 0;
+        return Integer.valueOf(versionElements[i]);
     }
 }
