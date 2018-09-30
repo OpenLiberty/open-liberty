e6babdc3eba8cdc5dda346f5d8eb2a582564e418
Gordon Hutchison <gordon.hutchison@gmail.com>
2018-09-30 22:06:46 +0100

    More Engine injection changes
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>

--- dev/com.ibm.ws.microprofile.reactive.streams.operators.cdi/src/com/ibm/ws/microprofile/reactive/streams/cdi/ReactiveStreamsEngineInjectionExtension.java
+++ dev/com.ibm.ws.microprofile.reactive.streams.operators.cdi/src/com/ibm/ws/microprofile/reactive/streams/cdi/ReactiveStreamsEngineInjectionExtension.java
@@ -10,16 +10,11 @@
  *******************************************************************************/
 package com.ibm.ws.microprofile.reactive.streams.cdi;
 
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.AnnotatedType;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.enterprise.inject.spi.BeforeBeanDiscovery;
 import javax.enterprise.inject.spi.Extension;
-import javax.enterprise.inject.spi.InjectionPoint;
-import javax.enterprise.inject.spi.ProcessInjectionTarget;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -27,15 +22,9 @@ import com.ibm.websphere.ras.Tr;
 import com.ibm.websphere.ras.TraceComponent;
 import com.ibm.ws.cdi.extension.WebSphereCDIExtension;
 
-/*
- * This is CDI extension for CDI to identify there is a bean which produces ReactiveStreamsEngine
- * We need to register this extension so that CDI will identify this bundle needs scanning
- * Addition to this extension,  we need to have beens.xml file in META-INF directory for the
- * WELD implementation to scan the bean which uses @Produces
- */
-@Component(service = WebSphereCDIExtension.class, property = { "api.classes=org.eclipse.microprofile.reactive.streams.spi.ReactiveStreamsEngine" }, immediate = true)
+@Component(service = WebSphereCDIExtension.class)
 public class ReactiveStreamsEngineInjectionExtension implements Extension, WebSphereCDIExtension {
-    
+
     private final static TraceComponent tc = Tr.register(ReactiveStreamsEngineInjectionExtension.class);
 
     public ReactiveStreamsEngineInjectionExtension() {
@@ -43,29 +32,61 @@ public class ReactiveStreamsEngineInjectionExtension implements Extension, WebSp
             Tr.debug(tc, "ReactiveStreamsEngineInjectionExtension construction ");
         }
     }
-    
-    void processInjectionTarget(@Observes ProcessInjectionTarget<?> pit) {
-        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
-            Tr.debug(tc, "ReactiveStreamsEngineInjectionExtension pit ");
-        }
-    }
 
-    
-    void beforeBeanDiscovery(@Observes BeforeBeanDiscovery bbd, BeanManager bm) {
-        AnnotatedType<ReactiveStreamsEngineProducerBean> producer = bm.createAnnotatedType(ReactiveStreamsEngineProducerBean.class);
+    public void beforeBeanDiscovery(@Observes BeforeBeanDiscovery bbd, BeanManager bm) {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+            Tr.debug(tc, "ReactiveStreamsEngineInjectionExtension bbd ");
+        }
+        AnnotatedType<RSBean> producer = bm.createAnnotatedType(RSBean.class);
         bbd.addAnnotatedType(producer);
-        
-        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
-            Tr.debug(tc, "ReactiveStreamsEngineInjectionExtension BeforeBeanDiscoveryCalled added producer " + producer);
-        }
     }
-    
-    protected Throwable processParameterizedType(InjectionPoint injectionPoint, ParameterizedType pType, ClassLoader classLoader) {
-        Throwable configException = null;
-        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
-            Tr.debug(tc, "ReactiveStreamsEngineInjectionExtension ppt");
-        }
-        return configException;
-    }
-
 }
+
+///*
+// * This is CDI extension for CDI to identify there is a bean which produces ReactiveStreamsEngine
+// * We need to register this extension so that CDI will identify this bundle needs scanning
+// * Addition to this extension,  we need to have beens.xml file in META-INF directory for the
+// * WELD implementation to scan the bean which uses @Produces
+// */
+//@Component(service = WebSphereCDIExtension.class, property = { "api.classes=org.eclipse.microprofile.reactive.streams.spi.ReactiveStreamsEngine" }, immediate = true)
+//public class ReactiveStreamsEngineInjectionExtension implements Extension, WebSphereCDIExtension {
+//    
+//    private final static TraceComponent tc = Tr.register(ReactiveStreamsEngineInjectionExtension.class);
+//
+//    public ReactiveStreamsEngineInjectionExtension() {
+//        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+//            Tr.debug(tc, "ReactiveStreamsEngineInjectionExtension construction ");
+//        }
+//    }
+//    
+//    void processInjectionTarget(@Observes ProcessInjectionTarget<?> pit) {
+//        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+//            Tr.debug(tc, "ReactiveStreamsEngineInjectionExtension pit ");
+//        }
+//    }
+//    
+//    void afterBeanDiscovery(@Observes AfterBeanDiscovery abd, BeanManager bm) {
+//        
+//        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+//            Tr.debug(tc, "ReactiveStreamsEngineInjectionExtension AfterBeanDiscoveryCalled");
+//        }
+//    }
+//    
+//    void beforeBeanDiscovery(@Observes BeforeBeanDiscovery bbd, BeanManager bm) {
+//        AnnotatedType<ReactiveStreamsEngineProducerBean> producer = bm.createAnnotatedType(ReactiveStreamsEngineProducerBean.class);
+//        bbd.addAnnotatedType(producer);
+//        
+//        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+//            Tr.debug(tc, "ReactiveStreamsEngineInjectionExtension BeforeBeanDiscoveryCalled added producer " + producer);
+//        }
+//    }
+//    
+//    protected Throwable processParameterizedType(InjectionPoint injectionPoint, ParameterizedType pType, ClassLoader classLoader) {
+//        Throwable x = null;
+//        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+//            Tr.debug(tc, "ReactiveStreamsEngineInjectionExtension ppt");
+//        }
+//        return x;
+//    }
+//
+//}
