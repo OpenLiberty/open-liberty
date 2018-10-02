310172651151bf7649d3eb6637ab66e2ee7c0f54
Alvin So <alvinso@ca.ibm.com>
2018-10-02 11:02:23 -0400

    Fixed imports

--- dev/com.ibm.ws.install/src/com/ibm/ws/install/internal/InstallUtils.java
+++ dev/com.ibm.ws.install/src/com/ibm/ws/install/internal/InstallUtils.java
@@ -62,7 +62,6 @@ import com.ibm.ws.install.internal.InstallLogUtils.Messages;
 import com.ibm.ws.install.internal.asset.SubsytemEntry;
 import com.ibm.ws.install.repository.download.RepositoryDownloadUtil;
 import com.ibm.ws.kernel.boot.cmdline.Utils;
-import com.ibm.ws.kernel.feature.Visibility;
 import com.ibm.ws.kernel.feature.internal.HashUtils;
 import com.ibm.ws.kernel.feature.internal.subsystem.SubsystemFeatureDefinitionImpl;
 import com.ibm.ws.kernel.feature.provisioning.ProvisioningFeatureDefinition;
@@ -70,6 +69,7 @@ import com.ibm.ws.kernel.provisioning.ProductExtension;
 import com.ibm.ws.kernel.provisioning.ProductExtensionInfo;
 import com.ibm.ws.product.utility.CommandConsole;
 import com.ibm.ws.repository.common.enums.ResourceType;
+import com.ibm.ws.repository.common.enums.Visibility;
 import com.ibm.ws.repository.connections.DirectoryRepositoryConnection;
 import com.ibm.ws.repository.connections.RepositoryConnection;
 import com.ibm.ws.repository.exceptions.RepositoryBackendException;
@@ -1063,16 +1063,6 @@ public class InstallUtils {
             }
         }
         return null;
-    }
-
-    public static String getEditionName(File installRoot, String editionCode) {
-
-        String editionCodeUpperCase = editionCode.toUpperCase();
-
-        if (editionCodeUpperCase.equals("OPEN")) {
-            editionCodeUpperCase = new Product(installRoot).getProductEdition().toUpperCase();
-        }
-        return getEditionName(editionCodeUpperCase);
     }
 
     public static String getEditionName(String editionCode) {
