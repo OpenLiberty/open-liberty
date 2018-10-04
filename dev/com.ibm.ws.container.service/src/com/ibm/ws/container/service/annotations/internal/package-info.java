19717346ccec299013e24aa8aa8c06bb65957283
Thomas Bitonti <bitonti@us.ibm.com>
2018-10-04 17:24:13 -0400

    BND version updates

--- dev/com.ibm.ws.container.service/src/com/ibm/ws/container/service/annotations/internal/package-info.java
+++ dev/com.ibm.ws.container.service/src/com/ibm/ws/container/service/annotations/internal/package-info.java
@@ -8,8 +8,8 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-/** @version 1.1 */
-@org.osgi.annotation.versioning.Version("1.1")
+/** @version 2.0 */
+@org.osgi.annotation.versioning.Version("2.0")
 @TraceOptions(traceGroup = "container.service", messageBundle = "com.ibm.ws.container.service.resources.Messages")
 package com.ibm.ws.container.service.annotations.internal;
 
