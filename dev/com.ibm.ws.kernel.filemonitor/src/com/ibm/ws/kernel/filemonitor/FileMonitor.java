b8c07e611331d5a7cd90f413d0e5d6840db6f7de
Edward Mezarina <mezarina@us.ibm.com>
2018-10-21 21:19:06 -0400

    Monitor and artifact updates

--- dev/com.ibm.ws.kernel.filemonitor/src/com/ibm/ws/kernel/filemonitor/FileMonitor.java
+++ dev/com.ibm.ws.kernel.filemonitor/src/com/ibm/ws/kernel/filemonitor/FileMonitor.java
@@ -10,6 +10,9 @@
  *******************************************************************************/
 package com.ibm.ws.kernel.filemonitor;
 
+import java.io.File;
+import java.util.Collection;
+
 /**
  * This is an internal interface that extends com.ibm.wsspi.kernel.filemonitor.FileMonitor
  * including all the property plus the MONITOR_IDENTIFICATION_NAME.
@@ -27,4 +30,14 @@ public interface FileMonitor extends com.ibm.wsspi.kernel.filemonitor.FileMonito
      */
     String MONITOR_IDENTIFICATION_NAME = "monitor.identification";
 
+    /**
+     * Called by the monitor service when a scheduled scan completes with
+     * changes (indicated by the presence of files in the corresponding collection).
+     *
+     * @param createdFiles A collection of files that were created since the last scan.
+     * @param modifiedFiles A collection of files that were modified since the last scan.
+     * @param deletedFiles A collection of files that were deleted since the last scan.
+     * @param filter A string used to filter listener notifications. Only the listeners matching the filter are called.
+     */
+    void onChange(Collection<File> createdFiles, Collection<File> modifiedFiles, Collection<File> deletedFiles, String filter);
 }
