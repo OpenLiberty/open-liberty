7906ba186d0d9b4deb32170c373aaac46ff7b847
Jared Anderson <jhanders@us.ibm.com>
2018-10-23 12:47:50 -0400

    Update ContainerClassLoader to take advantage of shared classes.
    ContainerClassLoader which is the parent class of AppClassLoader now can
    take advantage of the shared classes function in the IBM and Open J9
    JVMs with this change.

--- dev/com.ibm.ws.kernel.boot.common/src/com/ibm/ws/kernel/boot/classloader/ClassLoaderHook.java
+++ dev/com.ibm.ws.kernel.boot.common/src/com/ibm/ws/kernel/boot/classloader/ClassLoaderHook.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 IBM Corporation and others.
+ * Copyright (c) 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,27 +8,12 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-package com.ibm.ws.kernel.internal.classloader;
+package com.ibm.ws.kernel.boot.classloader;
 
-import java.io.IOException;
 import java.net.URL;
-import java.security.cert.Certificate;
-import java.util.jar.Manifest;
 
-/**
- */
-public interface ResourceEntry {
+public interface ClassLoaderHook {
+    byte[] loadClass(URL path, String name);
 
-    ResourceHandler getResourceHandler();
-
-    Manifest getManifest() throws IOException;
-
-    Certificate[] getCertificates();
-
-    byte[] getBytes() throws IOException;
-
-    URL toExternalURL();
-
-    URL toURL();
-
+    void storeClass(URL path, Class<?> clazz);
 }
--- dev/com.ibm.ws.kernel.boot.common/src/com/ibm/ws/kernel/boot/classloader/package-info.java
+++ dev/com.ibm.ws.kernel.boot.common/src/com/ibm/ws/kernel/boot/classloader/package-info.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 IBM Corporation and others.
+ * Copyright (c) 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,27 +8,8 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-package com.ibm.ws.kernel.internal.classloader;
-
-import java.io.IOException;
-import java.net.URL;
-import java.security.cert.Certificate;
-import java.util.jar.Manifest;
-
 /**
+ * @version 1.0.0
  */
-public interface ResourceEntry {
-
-    ResourceHandler getResourceHandler();
-
-    Manifest getManifest() throws IOException;
-
-    Certificate[] getCertificates();
-
-    byte[] getBytes() throws IOException;
-
-    URL toExternalURL();
-
-    URL toURL();
-
-}
+@org.osgi.annotation.versioning.Version("1.0.0")
+package com.ibm.ws.kernel.boot.classloader;
