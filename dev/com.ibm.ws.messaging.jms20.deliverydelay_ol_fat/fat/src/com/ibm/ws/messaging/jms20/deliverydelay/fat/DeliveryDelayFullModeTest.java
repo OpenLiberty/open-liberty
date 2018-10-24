38e1d5f330cea671ff64a8a8a80a4b0863dc2523
Benjamin Confino <benjamic@uk.ibm.com>
2018-10-24 12:59:55 +0100

    java7 rule com.ibm.ws.messaging.jms20.deliverydelay_ol_fat

--- dev/com.ibm.ws.messaging.jms20.deliverydelay_ol_fat/fat/src/com/ibm/ws/messaging/jms20/deliverydelay/fat/DeliveryDelayFullModeTest.java
+++ dev/com.ibm.ws.messaging.jms20.deliverydelay_ol_fat/fat/src/com/ibm/ws/messaging/jms20/deliverydelay/fat/DeliveryDelayFullModeTest.java
@@ -52,15 +52,11 @@ import org.junit.rules.TestRule;
 import com.ibm.websphere.simplicity.log.Log;
 import componenttest.custom.junit.runner.Mode;
 import componenttest.custom.junit.runner.Mode.TestMode;
-import componenttest.custom.junit.runner.OnlyRunInJava7Rule;
 import componenttest.topology.impl.LibertyServer;
 import componenttest.topology.impl.LibertyServerFactory;
 
 @Mode(TestMode.FULL)
 public class DeliveryDelayFullModeTest {
-
-	@ClassRule
-	public static final TestRule java7Rule = new OnlyRunInJava7Rule();
 
 	private static LibertyServer server = LibertyServerFactory
 			.getLibertyServer("DeliveryDelayFullModeTest_ClientServer");
