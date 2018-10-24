b7525f61279ab056210ed55a6c9d0d64a1ef5189
Adam Anderson <atanders@us.ibm.com>
2018-10-24 11:44:46 -0500

    5049 - refactor how we fixed a jaxrs sse NPE as to not break sub resource message body writer selection

--- dev/com.ibm.ws.org.apache.cxf.cxf.core.3.2/src/org/apache/cxf/interceptor/ServiceInvokerInterceptor.java
+++ dev/com.ibm.ws.org.apache.cxf.cxf.core.3.2/src/org/apache/cxf/interceptor/ServiceInvokerInterceptor.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2018 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
 /**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
@@ -68,13 +58,11 @@ public class ServiceInvokerInterceptor extends AbstractPhaseInterceptor<Message>
             @Override
             public void run() {
                 Exchange runableEx = message.getExchange();
-                Message outMessage = null;
-                // Liberty change (OLGH2084):  Move invoke after OutMessage is set on Exchange to allow 
-                // events to flow, such as those containing JAXBElements. 
+                Object result = invoker.invoke(runableEx, getInvokee(message));
                 if (!exchange.isOneWay()) {
                     Endpoint ep = exchange.getEndpoint();
 
-                    outMessage = runableEx.getOutMessage();
+                    Message outMessage = runableEx.getOutMessage();
                     if (outMessage == null) {
                         // Liberty perf change - avoid resize operation to set init size 16 and factor 1
                         outMessage = new MessageImpl(16, 1);
@@ -83,11 +71,6 @@ public class ServiceInvokerInterceptor extends AbstractPhaseInterceptor<Message>
                         exchange.setOutMessage(outMessage);
                     }
                     copyJaxwsProperties(message, outMessage);
-                }
-
-                Object result = invoker.invoke(runableEx, getInvokee(message));
-
-                if (!exchange.isOneWay()) {
                     if (result != null) {
                         MessageContentsList resList = null;
                         if (result instanceof MessageContentsList) {
