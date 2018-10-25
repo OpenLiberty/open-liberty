f82f626d380d0ca36e623ab52ee3de3de775d7fd
Manuel Saldana <mansal@us.ibm.com>
2018-10-25 15:49:52 -0400

    Initial commit for Forwarding and X-Forwarded-* header support

--- dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/channel/internal/values/AccessLogRemoteHost.java
+++ dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/channel/internal/values/AccessLogRemoteHost.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2008 IBM Corporation and others.
+ * Copyright (c) 2004, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,36 +11,50 @@
 package com.ibm.ws.http.channel.internal.values;
 
 import com.ibm.ws.http.channel.internal.HttpRequestMessageImpl;
+import com.ibm.ws.http.channel.internal.inbound.HttpInboundServiceContextImpl;
 import com.ibm.wsspi.http.channel.HttpRequestMessage;
 import com.ibm.wsspi.http.channel.HttpResponseMessage;
 
 public class AccessLogRemoteHost extends AccessLogData {
 
-	public AccessLogRemoteHost() {
-		super("%h");
-		// %h
-		// Remote host
-	}
+    public AccessLogRemoteHost() {
+        super("%h");
+        // %h
+        // Remote host
+    }
 
-	@Override
-	public boolean set(StringBuilder accessLogEntry,
-			HttpResponseMessage response, HttpRequestMessage request, Object data) {
-		HttpRequestMessageImpl requestMessageImpl = null;
-		String hostAddress = null;
-		if(request != null){
-			requestMessageImpl = (HttpRequestMessageImpl)request;
-		}
+    @Override
+    public boolean set(StringBuilder accessLogEntry,
+                       HttpResponseMessage response, HttpRequestMessage request, Object data) {
+        HttpRequestMessageImpl requestMessageImpl = null;
+        String hostAddress = null;
+        if (request != null) {
+            requestMessageImpl = (HttpRequestMessageImpl) request;
+        }
 
-		if(requestMessageImpl != null){
-			hostAddress = requestMessageImpl.getServiceContext().getRemoteAddr().getHostAddress();
-		}
+        if (requestMessageImpl != null) {
 
-		if(hostAddress != null){
-			accessLogEntry.append(hostAddress);
-		} else {
-			accessLogEntry.append("-");
-		}
-		return true;
+            hostAddress = null;
+
+            if (requestMessageImpl.getServiceContext() instanceof HttpInboundServiceContextImpl) {
+                HttpInboundServiceContextImpl serviceContext = (HttpInboundServiceContextImpl) requestMessageImpl.getServiceContext();
+                if (serviceContext.useForwardedHeadersInAccessLog()) {
+                    hostAddress = serviceContext.getForwardedRemoteHost();
+                }
+            }
+
+            if (hostAddress == null) {
+                hostAddress = requestMessageImpl.getServiceContext().getRemoteAddr().getHostAddress();
+            }
+
+        }
+
+        if (hostAddress != null) {
+            accessLogEntry.append(hostAddress);
+        } else {
+            accessLogEntry.append("-");
+        }
+        return true;
 
 //		String requestHost = null;
 //		if(request != null){
@@ -54,5 +68,5 @@ public class AccessLogRemoteHost extends AccessLogData {
 //		}
 //
 //		return true;
-	}
+    }
 }
--- dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/channel/internal/values/AccessLogRequestProtocol.java
+++ dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/channel/internal/values/AccessLogRequestProtocol.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2008 IBM Corporation and others.
+ * Copyright (c) 2004, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,6 +10,8 @@
  *******************************************************************************/
 package com.ibm.ws.http.channel.internal.values;
 
+import com.ibm.ws.http.channel.internal.HttpRequestMessageImpl;
+import com.ibm.ws.http.channel.internal.inbound.HttpInboundServiceContextImpl;
 import com.ibm.wsspi.http.channel.HttpRequestMessage;
 import com.ibm.wsspi.http.channel.HttpResponseMessage;
 
@@ -23,8 +25,25 @@ public class AccessLogRequestProtocol extends AccessLogData {
     public boolean set(StringBuilder accessLogEntry,
                        HttpResponseMessage response, HttpRequestMessage request, Object data) {
         String protocol = null;
+        HttpRequestMessageImpl requestMessageImpl = null;
         if (request != null) {
-            protocol = request.getVersion();
+
+            requestMessageImpl = (HttpRequestMessageImpl) request;
+        }
+
+        if (requestMessageImpl != null) {
+
+            if (requestMessageImpl.getServiceContext() instanceof HttpInboundServiceContextImpl) {
+                HttpInboundServiceContextImpl serviceContext = (HttpInboundServiceContextImpl) requestMessageImpl.getServiceContext();
+                if (serviceContext.useForwardedHeadersInAccessLog()) {
+                    protocol = serviceContext.getForwardedRemoteProto();
+                }
+            }
+
+            if (protocol == null) {
+                protocol = request.getVersion();
+            }
+
         }
 
         logSafe(accessLogEntry, protocol);
