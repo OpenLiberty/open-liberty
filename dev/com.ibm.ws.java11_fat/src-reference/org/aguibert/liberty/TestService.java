28665f12d9911e234070ac8c29d79a7ee18b497c
Andrew Guibert <andy.guibert@gmail.com>
2018-10-30 09:49:13 -0500

    Add FAT test for a Java 11 application

---
+++ dev/com.ibm.ws.java11_fat/src-reference/org/aguibert/liberty/TestService.java
@@ -0,0 +1,43 @@
+package org.aguibert.liberty;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/")
+@ApplicationScoped
+public class TestService {
+
+    @GET
+    public String test() {
+        try {
+            log(">>> ENTER");
+            doTest();
+            log("<<< EXIT SUCCESSFUL");
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+            e.printStackTrace(new PrintWriter(sb));
+            log("<<< EXIT FAILED");
+        }
+        String result = sb.toString();
+        sb = new StringWriter();
+        return result;
+    }
+
+    private void doTest() throws Exception {
+        log("Hello world");
+        log("This class is in: " + getClass().getModule());
+    }
+
+    private StringWriter sb = new StringWriter();
+
+    private void log(String msg) {
+        System.out.println(msg);
+        sb.append(msg);
+        sb.append("<br/>");
+    }
+
+}
