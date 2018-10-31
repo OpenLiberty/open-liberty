689526bf57b9dc693a19ab0df5a5993e61b25d8e
Toshi Yamamoto <yammer@us.ibm.com>
2018-10-31 09:33:31 -0400

    add System property for enabling the filter.

--- dev/com.ibm.ws.kernel.instrument.check/src/com/ibm/ws/kernel/instrument/EnvCheck.java
+++ dev/com.ibm.ws.kernel.instrument.check/src/com/ibm/ws/kernel/instrument/EnvCheck.java
@@ -10,8 +10,11 @@
  *******************************************************************************/
 package com.ibm.ws.kernel.instrument;
 
+
 import java.io.FileNotFoundException;
 import java.lang.instrument.Instrumentation;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.text.MessageFormat;
 import java.util.ResourceBundle;
 
@@ -24,7 +27,8 @@ public class EnvCheck {
     private static final int ERROR_BAD_JAVA_VERSION = 30;
     private static final int ERROR_LAUNCH_EXCEPTION = 24;
     
-    private static final String SERIALFILTER_AGENT_JAR="ws-serialfilteragent.jar";
+    private static final String SERIALFILTER_AGENT_JAR = "ws-serialfilteragent.jar";
+    private static final String KEY_ENABLE_SERIALFILTER_AGENT = "com.ibm.websphere.serialfilter.enable";
 
     /**
      * @param args - will just get passed onto BootstrapAgent if version check is successful
@@ -49,7 +53,15 @@ public class EnvCheck {
     public static void premain(String arg, Instrumentation inst) {
         try {
             BootstrapAgent.premain(arg, inst);
-            BootstrapAgent.loadAgent(SERIALFILTER_AGENT_JAR, null);
+            String enableSerialFilter = AccessController.doPrivileged(new PrivilegedAction<String>() {
+                public String run() {
+                    return System.getProperty(KEY_ENABLE_SERIALFILTER_AGENT);
+                }
+            });
+            // serialFilter won't be loaded by default.
+            if ("true".equalsIgnoreCase(enableSerialFilter)) {
+                BootstrapAgent.loadAgent(SERIALFILTER_AGENT_JAR, null);
+            }
         } catch (FileNotFoundException fnfe) {
             // CWWKE0948E message.
             System.out.println(MessageFormat.format(ResourceBundle.getBundle("com.ibm.ws.kernel.boot.resources.LauncherMessages").getString("error.noSerialfilteragent"), fnfe.getMessage()));
