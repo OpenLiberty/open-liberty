f0e2c17d8c59a6e4278856581c3bff50b8c50bf1
Ut Le <utle@us.ibm.com>
2018-11-03 18:26:34 -0500

    add Oracle JDK 8

--- dev/com.ibm.ws.security/src/com/ibm/ws/security/krb5/Krb5Common.java
+++ dev/com.ibm.ws.security/src/com/ibm/ws/security/krb5/Krb5Common.java
@@ -36,21 +36,21 @@ public class Krb5Common {
     // Kerberos mechanism OID
     static public Oid KRB5_MECH_OID;
 
-    static public boolean isJdk18Up = JavaInfo.majorVersion() >= 8;
+    // Is JDK 1.8 or higher
+    static public boolean isJdk18OrUp = JavaInfo.majorVersion() >= 8;
     // Is IBM JDK 1.8 or lower
-    static public boolean isIBMJdk18Lower = (JavaInfo.vendor() == Vendor.IBM && JavaInfo.majorVersion() <= 8);
-    // Is Oracle JDK 1.8
-    static public boolean isOracleJdk18Up = (JavaInfo.vendor() == Vendor.ORACLE && JavaInfo.majorVersion() >= 8);
+    static public boolean isIBMJdk18OrLower = (JavaInfo.vendor() == Vendor.IBM && JavaInfo.majorVersion() <= 8);
 
-    static public boolean isJava11Internal = false; //TODO - remove later
+    // Is Oracle JDK 1.8 or higher
+    static private boolean isOracleJdk18OrHigher = (JavaInfo.vendor() == Vendor.ORACLE && JavaInfo.majorVersion() >= 8);
+
+    static private boolean isJava11Internal = false; //TODO - remove later
 
     // Is IBM, Oracle and Open JDK 11 or higher
-    static public boolean isJdk11Up = JavaInfo.majorVersion() >= 11 || isJava11Internal;
-    // SPNEGO support IBM JDK 8 and lower, Oracle JDK 8 and JDK 11 and higher
-    static public boolean isSupportJDK = isIBMJdk18Lower || isOracleJdk18Up || isJdk11Up;
+    static private boolean isJdk11Up = JavaInfo.majorVersion() >= 11 || isJava11Internal;
 
     // SPNEGO support IBM JDK 8 and lower, Oracle JDK 8 and JDK 11 and higher
-    static public boolean isOtherSupportJDKs = isOracleJdk18Up || isJdk11Up;
+    static public boolean isOtherSupportJDKs = isOracleJdk18OrHigher || isJdk11Up;
 
     // Kerberos KDC host name
     static public final String KRB5_KDC = "java.security.krb5.kdc";
