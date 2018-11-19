1fa1c716ff35bf9e6e160cf15ea2d1abac6a03a5
Tom Evans <tevans@uk.ibm.com>
2018-11-19 11:34:30 +0000

    Improve MP Config Variable server.xml test

--- dev/com.ibm.ws.microprofile.config.1.3_fat/fat/src/com/ibm/ws/microprofile/config13/test/VariableServerXMLTest.java
+++ dev/com.ibm.ws.microprofile.config.1.3_fat/fat/src/com/ibm/ws/microprofile/config13/test/VariableServerXMLTest.java
@@ -53,6 +53,9 @@ import componenttest.topology.utils.FATServletClient;
 @RunWith(FATRunner.class)
 public class VariableServerXMLTest extends FATServletClient {
 
+    //The default refresh rate for dynamic sources
+    public static final long DEFAULT_DYNAMIC_REFRESH_INTERVAL = 500;
+
     public static final String APP_NAME = "variableServerXMLApp";
 
     @Server("ServerXMLVariableServer")
@@ -69,7 +72,6 @@ public class VariableServerXMLTest extends FATServletClient {
         // Automatically includes resources under 'test-applications/APP_NAME/resources/' folder
         // Exports the resulting application to the ${server.config.dir}/apps/ directory
         ShrinkHelper.defaultApp(server, APP_NAME, "com.ibm.ws.microprofile.config13.variableServerXML.*");
-
         server.startServer();
     }
 
@@ -88,20 +90,27 @@ public class VariableServerXMLTest extends FATServletClient {
         test(server, "/variableServerXMLApp/ServerXMLVariableServlet?testMethod=varPropertiesOrderTest");
     }
 
-    @Test
-    public void testChangeVariablesConfig() throws Exception {
-
-        // run the "before" test to check the value of the variable before the server.xml is updated
-        test(server, "/variableServerXMLApp/ServerXMLVariableServlet?testMethod=varPropertiesBeforeTest");
-
-        // switch to new configuration
-        server.copyFileToLibertyServerRoot("refreshVariables/variableServerXMLApp.xml");
+    private void copyFileToLibertyServerRoot(String filename) throws Exception {
+        server.setMarkToEndOfLog();
+        server.copyFileToLibertyServerRoot(filename);
 
         // Wait for message: "the server configuration was successfully updated"
         assertNotNull("The variableServerXMLApp.xml was not updated",
                       server.waitForStringInLog("CWWKG0017I"));
-        Thread.sleep(1000); // We need this pause so that the config change is picked up through the polling mechanism,
-                            // Something more deterministic would be better.
+
+        Thread.sleep(DEFAULT_DYNAMIC_REFRESH_INTERVAL * 2); // We need this pause so that the MP config change is picked up through the polling mechanism
+    }
+
+    @Test
+    public void testChangeVariablesConfig() throws Exception {
+
+        copyFileToLibertyServerRoot("original/variableServerXMLApp.xml");
+
+        // run the "before" test to check the value of the variable before the server.xml is updated
+        test(server, "/variableServerXMLApp/ServerXMLVariableServlet?testMethod=varPropertiesBeforeTest");
+
+        //update the config
+        copyFileToLibertyServerRoot("refreshVariables/variableServerXMLApp.xml");
 
         // run the "after" test to check the value of the variable after the server.xml is updated
         test(server, "/variableServerXMLApp/ServerXMLVariableServlet?testMethod=varPropertiesAfterTest");
@@ -110,20 +119,13 @@ public class VariableServerXMLTest extends FATServletClient {
     @Test
     public void testChangeAppPropertiesConfig() throws Exception {
 
-        // Restart the server
-        server.stopServer();
-        server.startServer();
+        copyFileToLibertyServerRoot("original/variableServerXMLApp.xml");
+
         // run the "before" test to check the value of the variable before the server.xml is updated
         test(server, "/variableServerXMLApp/ServerXMLVariableServlet?testMethod=appPropertiesBeforeTest");
 
-        // switch to new configuration
-        server.copyFileToLibertyServerRoot("refreshAppProperties/variableServerXMLApp.xml");
-
-        // Wait for message: "the server configuration was successfully updated"
-        assertNotNull("The variableServerXMLApp.xml was not updated",
-                      server.waitForStringInLog("CWWKG0017I"));
-        Thread.sleep(1000); // We need this pause so that the config change is picked up through the polling mechanism,
-                            // Something more deterministic would be better.
+        //update the config
+        copyFileToLibertyServerRoot("refreshAppProperties/variableServerXMLApp.xml");
 
         // run the "after" test to check the value of the variable after the server.xml is updated
         test(server, "/variableServerXMLApp/ServerXMLVariableServlet?testMethod=appPropertiesAfterTest");
