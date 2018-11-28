d8c2009c655256b31a91e9629564a90c7fa874f0
Anjum Fatima <anjum.fatima@ibm.com>
2018-11-28 13:43:26 -0600

    Test multi module Spring Boot application

--- dev/com.ibm.ws.springboot.support.version20.test.war.app/src/main/java/com/ibm/ws/springboot/support/version20/test/war/app/TestJSPApplication.java
+++ dev/com.ibm.ws.springboot.support.version20.test.war.app/src/main/java/com/ibm/ws/springboot/support/version20/test/war/app/TestJSPApplication.java
@@ -14,8 +14,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:application-context.xml")
 public class TestJSPApplication extends SpringBootServletInitializer {
 
 	// Test that this never gets called
