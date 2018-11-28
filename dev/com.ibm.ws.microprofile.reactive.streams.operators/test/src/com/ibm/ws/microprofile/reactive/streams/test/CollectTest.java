5c1f8084578300a101dd62015e729cc47ab7a7b5
Gordon Hutchison <gordon.hutchison@gmail.com>
2018-11-28 14:48:44 +0000

    Bundlize rxjava2 2.2.4
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    Added forked com.ibm.ws smallrye reactive streams operators to build - to be removed ultimately via upstream PRs
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    Compiles OK with zerodep replaced with smallrye/rxjava
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    Safety commit for file changes in flight
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    tidy up and move to RC4 of spec
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    added core
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    removed rxjava from api feature
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    missed out an operators and a version increment
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    Updated to 1.0 MP RSO
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    smallrye ORS 100 more
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    fist unit test passes on smallrye-rxjava
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    Test improvements
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    tidy up of bundles and versions
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    bundle name fix for rxjava
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    fixed feature rxjava 2 digit
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    missed initial request 1
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    named rxjava bundle in consultation with Emily
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    consume new rxjava bundle name
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    test tweek
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>
    
    publish smallrye to lib
    
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>

--- dev/com.ibm.ws.microprofile.reactive.streams.operators/test/src/com/ibm/ws/microprofile/reactive/streams/test/CollectTest.java
+++ dev/com.ibm.ws.microprofile.reactive.streams.operators/test/src/com/ibm/ws/microprofile/reactive/streams/test/CollectTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 IBM Corporation and others.
+ * Copyright (c) 2019 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,8 +12,9 @@
 
 package com.ibm.ws.microprofile.reactive.streams.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertNull;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -25,8 +26,9 @@ import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
-import org.eclipse.microprofile.reactive.streams.ReactiveStreams;
-import org.eclipse.microprofile.reactive.streams.SubscriberBuilder;
+import org.eclipse.microprofile.reactive.streams.operators.ReactiveStreams;
+import org.eclipse.microprofile.reactive.streams.operators.SubscriberBuilder;
+import org.eclipse.microprofile.reactive.streams.operators.spi.ReactiveStreamsEngine;
 import org.junit.Test;
 
 public class CollectTest extends WASReactiveUT {
@@ -147,6 +149,113 @@ public class CollectTest extends WASReactiveUT {
                 ReactiveStreams.of(1, 2, 3).to(toList).run(getEngine())), Arrays.asList(1, 2, 3));
         assertEquals(await(
                 ReactiveStreams.of(4, 5, 6).to(toList).run(getEngine())), Arrays.asList(4, 5, 6));
+    }
+
+    @Test
+    public void toListStageShouldReturnEmpty() {
+        assertEquals(await(
+                ReactiveStreams.of().toList().run(getEngine())), Collections.emptyList());
+    }
+
+    @Test
+    public void collectShouldAccumulateResult() {
+        assertEquals(await(
+                ReactiveStreams.of(1, 2, 3).collect(() -> new AtomicInteger(0),
+                        AtomicInteger::addAndGet).run(getEngine())).get(),
+                6);
+    }
+
+    @Test
+    public void collectShouldSupportEmptyStreams() {
+        assertEquals(await(
+                ReactiveStreams.<Integer>empty().collect(() -> new AtomicInteger(42),
+                        AtomicInteger::addAndGet).run(getEngine())).get(),
+                42);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void collectShouldPropagateErrors() {
+        await(ReactiveStreams.<Integer>failed(new RuntimeException("failed")).collect(() -> new AtomicInteger(0),
+                AtomicInteger::addAndGet).run(getEngine()));
+    }
+
+    @Test
+    public void finisherFunctionShouldBeInvoked() {
+        assertEquals(await(
+                ReactiveStreams.of("1", "2", "3")
+                        .collect(Collectors.joining(", ")).run(getEngine())),
+                "1, 2, 3");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void toListStageShouldPropagateErrors() {
+        await(ReactiveStreams.failed(new RuntimeException("failed")).toList().run(getEngine()));
+    }
+
+    @Test(expected = QuietRuntimeException.class)
+    public void collectShouldPropagateUpstreamErrors2() {
+        await(ReactiveStreams.<Integer>failed(new QuietRuntimeException("failed")).collect(() -> new AtomicInteger(0),
+                AtomicInteger::addAndGet).run(getEngine()));
+    }
+
+    @Test(expected = QuietRuntimeException.class)
+    public void toListStageShouldPropagateUpstreamErrors2() {
+        await(ReactiveStreams.failed(new QuietRuntimeException("failed")).toList().run(getEngine()));
+    }
+
+    @Test(expected = QuietRuntimeException.class)
+    public void collectStageShouldPropagateErrorsFromSupplierThroughCompletionStage() {
+        CompletableFuture<Void> cancelled = new CompletableFuture<>();
+        CompletionStage<Integer> result = null;
+        try {
+            result = infiniteStream().onTerminate(() -> cancelled.complete(null))
+                    .collect(Collector.<Integer, Integer, Integer>of(() -> {
+                        throw new QuietRuntimeException("failed");
+                    }, (a, b) -> {
+                    }, (a, b) -> a + b,
+                            Function.identity()))
+                    .run(getEngine());
+        } catch (Exception e) {
+            assertNull(
+                    "Exception thrown directly from stream, it should have been captured by the returned CompletionStage",
+                    e);
+        }
+        await(cancelled);
+        await(result);
+    }
+
+    @Test(expected = QuietRuntimeException.class)
+    public void collectStageShouldPropagateErrorsFromAccumulator() {
+        CompletableFuture<Void> cancelled = new CompletableFuture<>();
+        CompletionStage<String> result = infiniteStream().onTerminate(() -> cancelled.complete(null))
+                .collect(Collector.of(() -> "", (a, b) -> {
+                    throw new QuietRuntimeException("failed");
+                }, (a, b) -> a + b,
+                        Function.identity()))
+                .run(getEngine());
+        await(cancelled);
+        await(result);
+    }
+
+    @Test(expected = QuietRuntimeException.class)
+    public void collectStageShouldPropagateErrorsFromFinisher() {
+        CompletionStage<Integer> result = ReactiveStreams.of(1, 2, 3)
+                .collect(Collector.<Integer, Integer, Integer>of(() -> 0, (a, b) -> {
+                }, (a, b) -> a + b, r -> {
+                    throw new QuietRuntimeException("failed");
+                })).run(getEngine());
+        await(result);
+    }
+
+    @Test
+    public void collectStageBuilderShouldBeReusable() {
+        SubscriberBuilder<Integer, List<Integer>> toList = ReactiveStreams.<Integer>builder().toList();
+        assertEquals(await(
+                ReactiveStreams.of(1, 2, 3).to(toList).run(getEngine())),
+                Arrays.asList(1, 2,
+                        3));
+        assertEquals(await(ReactiveStreams.of(4, 5,
+                6).to(toList).run(getEngine())), Arrays.asList(4, 5, 6));
     }
 
 }
