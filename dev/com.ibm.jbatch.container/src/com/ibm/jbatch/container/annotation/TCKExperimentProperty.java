96f7fb148110567f70a2982e0b3578bded546ca6
wraschke@us.ibm.com <wraschke@us.ibm.com>
2018-12-13 10:26:21 -0600

    Test changes only; not for merging

--- dev/com.ibm.jbatch.container/src/com/ibm/jbatch/container/annotation/TCKExperimentProperty.java
+++ dev/com.ibm.jbatch.container/src/com/ibm/jbatch/container/annotation/TCKExperimentProperty.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 International Business Machines Corp.
+ * IBM Confidential OCO Source Material.
  * 
  * See the NOTICE file distributed with this work for additional information
  * regarding copyright ownership. Licensed under the Apache License, 
