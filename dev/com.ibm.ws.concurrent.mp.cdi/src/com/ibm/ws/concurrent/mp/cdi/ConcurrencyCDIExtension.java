236da72447a14e741755b3fd8631e976c31fa2ac
Andrew Guibert <andy.guibert@gmail.com>
2019-01-04 16:55:22 -0600

    Update MP Concurrency impl to produce a unique ManagedExecutor per @Default injection point

--- dev/com.ibm.ws.concurrent.mp.cdi/src/com/ibm/ws/concurrent/mp/cdi/ConcurrencyCDIExtension.java
+++ dev/com.ibm.ws.concurrent.mp.cdi/src/com/ibm/ws/concurrent/mp/cdi/ConcurrencyCDIExtension.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 IBM Corporation and others.
+ * Copyright (c) 2018, 2019 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,18 +13,21 @@ package com.ibm.ws.concurrent.mp.cdi;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Member;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
 import javax.enterprise.event.Observes;
+import javax.enterprise.inject.Default;
 import javax.enterprise.inject.spi.AfterBeanDiscovery;
 import javax.enterprise.inject.spi.AfterDeploymentValidation;
 import javax.enterprise.inject.spi.Annotated;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.enterprise.inject.spi.Extension;
 import javax.enterprise.inject.spi.ProcessInjectionPoint;
+import javax.enterprise.inject.spi.ProcessProducer;
 
 import org.eclipse.microprofile.concurrent.ManagedExecutor;
 import org.eclipse.microprofile.concurrent.ManagedExecutorConfig;
@@ -51,41 +54,60 @@ public class ConcurrencyCDIExtension implements Extension, WebSphereCDIExtension
 
     private final Map<String, ManagedExecutorConfig> beanMap = new HashMap<>();
     private final Set<Throwable> deploymentErrors = new LinkedHashSet<>();
+    private final Set<String> appDefinedProducers = new HashSet<>();
+
+    public void processProducer(@Observes ProcessProducer<?, ManagedExecutor> event, BeanManager bm) {
+        // Save off app-defined @NamedInstance producers so we know *not* to create a bean for them
+        NamedInstance producerName = event.getAnnotatedMember().getAnnotation(NamedInstance.class);
+        if (producerName != null) {
+            if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled())
+                Tr.debug(tc, "Found app-defined producer for: name=" + producerName.value());
+            appDefinedProducers.add(producerName.value());
+        }
+    }
 
     public void processInjectionPoint(@Observes ProcessInjectionPoint<?, ManagedExecutor> event, BeanManager bm) {
         Annotated injectionPoint = event.getInjectionPoint().getAnnotated();
         if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled())
             Tr.debug(tc, "processInjectionPoint " + injectionPoint);
 
-        // Skip if no @ManagedExecutorConfig or module is not CDI enabled
-        if (!injectionPoint.isAnnotationPresent(ManagedExecutorConfig.class))
-            return;
-
         ManagedExecutorConfig config = injectionPoint.getAnnotation(ManagedExecutorConfig.class);
+        boolean configAnnoPresent = config != null;
+        if (config == null)
+            config = MEC_LITERAL;
 
-        // Instance name is either @NamedInstance.value() or generated from fully-qualified class+field name
+        // Instance name is either @NamedInstance.value() or generated from fully-qualified field name
         NamedInstance nameAnno = injectionPoint.getAnnotation(NamedInstance.class);
         Member member = event.getInjectionPoint().getMember();
         String name = nameAnno == null ? member.getDeclaringClass().getTypeName() + "." + member.getName() : nameAnno.value();
         if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled())
             Tr.debug(tc, "InjectionPoint " + name + " has config " + config);
-        if (nameAnno == null)
+        // Automatically insert @NamedInstance("<generated name>") qualifier for @Default injection points
+        if (nameAnno == null && event.getInjectionPoint().getQualifiers().contains(Default.Literal.INSTANCE))
             event.configureInjectionPoint().addQualifiers(createNamedInstance(name));
-        ManagedExecutorConfig prevoiusConfig = beanMap.putIfAbsent(name, config);
 
-        // If 2 or more InjectionPoints define @NamedInstance("X") @ManagedExecutorConfig it is an error
-        if (prevoiusConfig != null) {
-            String msg = "ERROR: Found existing bean with name=" + name; // TODO NLS message
-            Tr.error(tc, msg);
-            deploymentErrors.add(new Throwable(msg));
+        // The container MUST register a bean if @MEC is present
+        // The container MUST register a bean for every @Default injection point
+        if (configAnnoPresent || event.getInjectionPoint().getQualifiers().contains(Default.Literal.INSTANCE)) {
+            ManagedExecutorConfig prevoiusConfig = beanMap.putIfAbsent(name, config);
+
+            // If 2 or more InjectionPoints define @NamedInstance("X") @ManagedExecutorConfig it is an error
+            if (prevoiusConfig != null) {
+                String msg = "ERROR: Found existing bean with name=" + name; // TODO NLS message
+                Tr.error(tc, msg);
+                deploymentErrors.add(new Throwable(msg));
+            }
         }
     }
 
     public void registerBeans(@Observes AfterBeanDiscovery event, BeanManager beanManager) {
-        // TODO: Right now this is coded to give 1 ManagedExecutor instance for all @Default injection points
-        // wait to see how discussion on this PR ends up: https://github.com/eclipse/microprofile-concurrency/pull/54
-        // Always register bean for default instance(s)
+        // Always register a bean for @Default programmatic CDI lookups
         event.addBean(new ManagedExecutorBean());
+
+        // Don't register beans for app-defined @NamedInstance producers
+        for (String appDefinedProducer : appDefinedProducers) {
+            beanMap.remove(appDefinedProducer);
+        }
 
         // Register 1 bean per un-named config, and 1 bean per unique NamedInstance
         for (Entry<String, ManagedExecutorConfig> e : beanMap.entrySet()) {
