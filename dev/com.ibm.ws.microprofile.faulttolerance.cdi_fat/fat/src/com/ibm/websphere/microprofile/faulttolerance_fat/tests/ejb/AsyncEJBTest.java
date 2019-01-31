47332eabb84432f49e5475d843ab50e628e7ea7e
Andrew Rouse <anrouse@uk.ibm.com>
2019-01-31 16:03:29 +0000

    Enable and enhance thread context tests for FT
    Existing tests were not annotated with @Test and so were not running.
    
    Added new tests for TCCL and EJB Security

---
+++ dev/com.ibm.ws.microprofile.faulttolerance.cdi_fat/fat/src/com/ibm/websphere/microprofile/faulttolerance_fat/tests/ejb/AsyncEJBTest.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.websphere.microprofile.faulttolerance_fat.tests.ejb;
+
+import static componenttest.custom.junit.runner.Mode.TestMode.FULL;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+
+import com.ibm.websphere.microprofile.faulttolerance_fat.suite.RepeatFaultTolerance;
+import com.ibm.websphere.simplicity.ShrinkHelper;
+import com.ibm.websphere.simplicity.ShrinkHelper.DeployOptions;
+
+import componenttest.annotation.Server;
+import componenttest.annotation.TestServlet;
+import componenttest.custom.junit.runner.FATRunner;
+import componenttest.custom.junit.runner.Mode;
+import componenttest.rules.repeater.RepeatTests;
+import componenttest.topology.impl.LibertyServer;
+import componenttest.topology.utils.FATServletClient;
+
+@Mode(FULL)
+@RunWith(FATRunner.class)
+public class AsyncEJBTest extends FATServletClient {
+
+    private static final String SERVER_NAME = "FaultToleranceEJB";
+    private static final String APP_NAME = "AsyncEJBTest";
+
+    @Server(SERVER_NAME)
+    @TestServlet(contextRoot = APP_NAME, servlet = AsyncEJBServlet.class)
+    public static LibertyServer server;
+
+    //run against both EE8 and EE7 features
+    @ClassRule
+    public static RepeatTests r = RepeatFaultTolerance.repeatAll(SERVER_NAME);
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        WebArchive app = ShrinkWrap.create(WebArchive.class, APP_NAME + ".war")
+                        .addPackage(AsyncEJBServlet.class.getPackage())
+                        .addAsManifestResource(AsyncEJBServlet.class.getPackage(), "permissions.xml", "permissions.xml");
+        ShrinkHelper.exportAppToServer(server, app, DeployOptions.SERVER_ONLY);
+        server.startServer();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        server.stopServer();
+        server.deleteFileFromLibertyServerRoot("apps/" + APP_NAME + ".war");
+    }
+
+}
