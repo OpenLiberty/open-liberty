77241ccb07f93f08fea4b773aa27779c64115257
Gordon Hutchison <gordon.hutchison@gmail.com>
2019-02-18 11:43:08 +0000

    Some more unit tests
    Signed-off-by: Gordon Hutchison <gordon.hutchison@gmail.com>

--- dev/com.ibm.ws.microprofile.reactive.streams.operators/test/src/com/ibm/ws/microprofile/reactive/streams/test/RsoElementsTest.java
+++ dev/com.ibm.ws.microprofile.reactive.streams.operators/test/src/com/ibm/ws/microprofile/reactive/streams/test/RsoElementsTest.java
@@ -12,46 +12,59 @@
 
 package com.ibm.ws.microprofile.reactive.streams.test;
 
-import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
 
 import org.eclipse.microprofile.reactive.streams.operators.ProcessorBuilder;
 import org.eclipse.microprofile.reactive.streams.operators.PublisherBuilder;
 import org.eclipse.microprofile.reactive.streams.operators.ReactiveStreams;
-import org.eclipse.microprofile.reactive.streams.operators.SubscriberBuilder;
-import org.eclipse.microprofile.reactive.streams.operators.spi.ReactiveStreamsEngine;
 import org.junit.Test;
 
-import io.reactivex.processors.PublishProcessor;
+import io.reactivex.Flowable;
 import io.reactivex.subscribers.TestSubscriber;
 
 public class RsoElementsTest extends WASReactiveUT {
 
     @Test
-    public void rxPublisherTest() {
+    public void testReactiveStreamsDotOrgSubscriber() throws InterruptedException, ExecutionException {
 
-        ReactiveStreamsEngine engine = getEngine();
+        PublisherBuilder<Integer> data = ReactiveStreams.of(1, 2, 3, 4, 5);
+        TestSubscriber<Integer> rxSub = new TestSubscriber<Integer>();
+        data.to(rxSub).run(getEngine());
+        rxSub.await()
+                .assertComplete()
+                .assertNoErrors()
+                .assertResult(1, 2, 3, 4, 5);
 
-        PublishProcessor<String> rxpub = PublishProcessor.create();
-        PublisherBuilder<String> mpPub = ReactiveStreams.fromPublisher(rxpub);
+    }
 
-        PublishProcessor<String> rxproc = PublishProcessor.create();
-        ProcessorBuilder<String, String> mpProc = ReactiveStreams.fromProcessor(rxproc);
+    @Test
+    public void testReactiveStreamsDotOrgPublisher() throws InterruptedException, ExecutionException {
 
-        TestSubscriber<String> rxSub = new TestSubscriber<>();
-        SubscriberBuilder mpSub = ReactiveStreams.fromSubscriber(rxSub);
+        Flowable<String> flowable = Flowable.fromArray("one", "two", "three");
+        PublisherBuilder<String> data = ReactiveStreams.fromPublisher(flowable);
 
-        CompletionStage result = mpPub.via(mpProc).to(mpSub).run(engine);
+        TestSubscriber<String> rxSub = new TestSubscriber<String>();
+        data.to(rxSub).run(getEngine());
+        rxSub.await()
+                .assertComplete()
+                .assertNoErrors()
+                .assertResult("one", "two", "three");
 
-        rxpub.onNext("one");
-        rxpub.onNext("two");
-        rxpub.onNext("three");
-        rxpub.onComplete();
+    }
 
-        // try {
-//            result.wait();
-        // } catch (InterruptedException e) {
-        // e.printStackTrace();
-        // }
+    @Test
+    public void testDropping() throws InterruptedException, ExecutionException {
+
+        PublisherBuilder<Integer> data = ReactiveStreams.of(1, 2, 3, 4, 5);
+        ProcessorBuilder<Integer, Integer> filter = ReactiveStreams.<Integer>builder().dropWhile(t -> t < 3);
+
+        TestSubscriber<Integer> rxSub = new TestSubscriber<Integer>();
+        data.via(filter).to(rxSub).run(getEngine());
+
+        TestSubscriber<Integer> o = rxSub.await();
+        o.assertComplete();
+        o.assertNoErrors();
+        o.assertResult(3, 4, 5);
 
     }
 
