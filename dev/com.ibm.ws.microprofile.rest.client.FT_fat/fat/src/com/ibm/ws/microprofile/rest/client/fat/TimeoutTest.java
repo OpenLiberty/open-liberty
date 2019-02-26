6475fc358b5f2d6781574354bc483e9eb27e0ef6
Andy McCright <j.andrew.mccright@gmail.com>
2019-02-26 11:22:03 -0600

    FATs for MP Rest Client 1.2 integration with CDI / MP Fault Tolerance

---
+++ dev/com.ibm.ws.microprofile.rest.client.FT_fat/fat/src/com/ibm/ws/microprofile/rest/client/fat/TimeoutTest.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.microprofile.rest.client.fat;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import com.ibm.websphere.simplicity.ShrinkHelper;
+
+import componenttest.annotation.Server;
+import componenttest.annotation.TestServlet;
+import componenttest.custom.junit.runner.FATRunner;
+import componenttest.topology.impl.LibertyServer;
+import componenttest.topology.utils.FATServletClient;
+import mpRestClientFT.timeout.TimeoutTestServlet;
+
+/**
+ * This test should only be run with mpRestClient-1.1 and above.
+ * Note that mpRestClient-1.1 can work with Java EE 7.0 (MP 1.4) and EE 8 (MP 2.0).
+ */
+@RunWith(FATRunner.class)
+public class TimeoutTest extends FATServletClient {
+
+    private static final String appName = "timeoutApp";
+
+    @Server("mpRestClientFT.timeout")
+    @TestServlet(servlet = TimeoutTestServlet.class, contextRoot = appName)
+    public static LibertyServer server;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        ShrinkHelper.defaultDropinApp(server, appName, "mpRestClientFT.timeout");
+        server.startServer();
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        server.stopServer();
+    }
+}
