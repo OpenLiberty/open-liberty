7042de7815da8072fc31d5001e8bc639562f8275
Aidan Polese <aidan.polese@ibm.com>
2019-03-06 15:55:37 -0500

    added custom flushing ability and a test suite for such

---
+++ dev/com.ibm.ws.logging_fat/test-applications/RealFlushTestApp/src/com/ibm/ws/logging/flush/fat/printBoolTests/PrintlnTrue.java
@@ -0,0 +1,33 @@
+package com.ibm.ws.logging.flush.fat.printBoolTests;
+
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.json.JsonObject;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.enterprise.inject.Produces;
+
+import com.ibm.websphere.filetransfer.FileServiceMXBean.MetaData;
+import com.ibm.websphere.logging.hpel.LogRecordContext;
+
+
+@ApplicationScoped
+@Path("/printlnTrue")
+public class PrintlnTrue {
+
+    private static final Logger MYlOGGER = Logger.getLogger(PrintlnTrue.class.getName());
+    
+    @GET
+    @Path("/printlnTrue")
+    public String makeString() {
+    	
+    	System.out.println(true);
+    	
+        return "---- DONE ----";
+    }
+}
