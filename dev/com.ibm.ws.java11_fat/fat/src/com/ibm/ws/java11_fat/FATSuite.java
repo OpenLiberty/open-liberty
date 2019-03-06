904908490570dbc32b1c9f5941cfcb226f690c0b
Andrew Guibert <andy.guibert@gmail.com>
2019-03-05 18:50:00 -0600

    Implement multi-release jar classloading

--- dev/com.ibm.ws.java11_fat/fat/src/com/ibm/ws/java11_fat/FATSuite.java
+++ dev/com.ibm.ws.java11_fat/fat/src/com/ibm/ws/java11_fat/FATSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 IBM Corporation and others.
+ * Copyright (c) 2018, 2019 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,6 +18,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({
                 Java11Test.class,
                 Java11CNFETest.class,
-                JavaInfoTest.class
+                JavaInfoTest.class,
+                MultiReleaseJarTest.class
 })
-public class FATSuite {}
+public class FATSuite {
+}
