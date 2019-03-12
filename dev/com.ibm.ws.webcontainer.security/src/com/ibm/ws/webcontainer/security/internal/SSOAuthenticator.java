8c80187212eb61ffa225019d8d5855b299cca221
Ut Le <utle@us.ibm.com>
2019-03-11 23:39:17 -0500

    Update unittest

--- dev/com.ibm.ws.webcontainer.security/src/com/ibm/ws/webcontainer/security/internal/SSOAuthenticator.java
+++ dev/com.ibm.ws.webcontainer.security/src/com/ibm/ws/webcontainer/security/internal/SSOAuthenticator.java
@@ -89,7 +89,7 @@ public class SSOAuthenticator implements WebAuthenticator {
         HttpServletRequest req = webRequest.getHttpServletRequest();
         HttpServletResponse res = webRequest.getHttpServletResponse();
         AuthenticationResult authResult = handleSSO(req, res);
-        if (authResult.getStatus() == AuthResult.SUCCESS) {
+        if (authResult != null && authResult.getStatus() == AuthResult.SUCCESS) {
             ssoCookieHelper.addJwtSsoCookiesToResponse(authResult.getSubject(), req, res);
         }
         return authResult;
