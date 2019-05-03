bc68d6030add1982d14cc4c981214139b8def04b
Arun Giridharan <ahgiridh@ibm.com>
2019-05-03 09:43:59 -0500

    added Fat test to check for SF issue

---
+++ dev/com.ibm.ws.security.wim.registry_fat/fat/src/com/ibm/ws/security/wim/registry/fat/SingleLDAPTest.java
@@ -0,0 +1,103 @@
+/*******************************************************************************
+ * Copyright (c) 2012, 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package com.ibm.ws.security.wim.registry.fat;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.ibm.websphere.simplicity.log.Log;
+import com.ibm.ws.security.registry.test.UserRegistryServletConnection;
+
+import componenttest.custom.junit.runner.FATRunner;
+import componenttest.custom.junit.runner.Mode;
+import componenttest.custom.junit.runner.Mode.TestMode;
+import componenttest.topology.impl.LibertyServer;
+import componenttest.topology.impl.LibertyServerFactory;
+import componenttest.topology.utils.LDAPUtils;
+
+@RunWith(FATRunner.class)
+@Mode(TestMode.LITE)
+public class SingleLDAPTest {
+    private static LibertyServer server = LibertyServerFactory.getLibertyServer("com.ibm.ws.security.wim.registry.fat.SingleLDAP");
+    private static final Class<?> c = SingleLDAPTest.class;
+    private static UserRegistryServletConnection servlet;
+
+    /**
+     * Updates the sample, which is expected to be at the hard-coded path.
+     * If this test is failing, check this path is correct.
+     */
+    @BeforeClass
+    public static void setUp() throws Exception {
+        // Add LDAP variables to bootstrap properties file
+        LDAPUtils.addLDAPVariables(server);
+        Log.info(c, "setUp", "Starting the server... (will wait for userRegistry servlet to start)");
+        // install our user feature
+
+        server.copyFileToLibertyInstallRoot("lib/features", "internalfeatures/securitylibertyinternals-1.0.mf");
+        server.addInstalledAppForValidation("userRegistry");
+        server.startServer(c.getName() + ".log");
+        //Make sure the application has come up before proceeding
+        assertNotNull("Application userRegistry does not appear to have started.",
+                      server.waitForStringInLog("CWWKZ0001I:.*userRegistry"));
+        assertNotNull("Security service did not report it was ready",
+                      server.waitForStringInLog("CWWKS0008I"));
+        assertNotNull("Server did not came up",
+                      server.waitForStringInLog("CWWKF0011I"));
+
+        Log.info(c, "setUp", "Creating servlet connection the server");
+        servlet = new UserRegistryServletConnection(server.getHostname(), server.getHttpDefaultPort());
+
+        if (servlet.getRealm() == null) {
+            Thread.sleep(5000);
+            servlet.getRealm();
+        }
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        Log.info(c, "tearDown", "Stopping the server...");
+        try {
+            server.stopServer("CWIML4537E");
+        } finally {
+            server.deleteFileFromLibertyInstallRoot("lib/features/testfileadapter-1.0.mf");
+        }
+    }
+
+    /**
+     * Hit the test servlet to see if getRealm works.
+     * This verifies the various required bundles got installed and are working.
+     */
+    @Test
+    public void getRealm() throws Exception {
+        Log.info(c, "getRealm", "Checking expected realm");
+        assertEquals("defaultWIMFileBasedRealm", servlet.getRealm());
+    }
+
+//    /**
+//     * Hit the test servlet to see if getRealm returns a works when passed in a valid user pattern
+//     * and a limit of 1; should only expect to find one entry, but in repo it should found 2 entries
+//     * This verifies the various required bundles got installed and are working.
+//     */
+    @Test
+    public void getRealmWithDelimiter() throws Exception {
+        String user = "CN=testuser//,cn=users,dc=vmm,dc=com";
+        Log.info(c, "getRealmWithDelimiter", "Checking to see if the default Realm is returned when the user contains a delimiter");
+        String result = servlet.getUserSecurityName(user);
+        assertNotNull("Default Realm should be returned if realm is not found", result);
+    }
+
+}
