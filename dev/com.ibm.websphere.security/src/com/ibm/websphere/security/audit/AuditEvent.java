ccb2c7c866abc28bdd3cf7f3937b133c9f53784b
Mehdi Merali <mmerali@ibm.com>
2019-05-07 13:22:38 -0400

    rebased with master audit changes

--- dev/com.ibm.websphere.security/src/com/ibm/websphere/security/audit/AuditEvent.java
+++ dev/com.ibm.websphere.security/src/com/ibm/websphere/security/audit/AuditEvent.java
@@ -131,7 +131,8 @@ public class AuditEvent {
     public static final String TARGET_SAF_PROFILE = "target.saf.profile";
     public static final String TARGET_SAF_CLASS = "target.saf.class";
     public static final String TARGET_AUTHORIZATION_DECISION = "target.authorization.decision";
-
+    public static final String TARGET_ACCESS_LEVEL = "target.access.level";
+    
     public final static String INITIATOR = "initiator";
     public final static String INITIATOR_ID = "initiator.id";
     public final static String INITIATOR_NAME = "initiator.name";
