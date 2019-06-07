fe482dc36fabebae4bbb96250bd76860099709cf
Andy McCright <j.andrew.mccright@gmail.com>
2019-06-06 21:01:12 -0500

    Issue 7755: Test case to ensure bean validation only occurs once

---
+++ dev/com.ibm.ws.jaxrs.2.0.cdi.1.2_fat/test-applications/beanvalidation/src/com/ibm/ws/jaxrs20/cdi12/fat/beanvalidation/ValidSomeString.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.jaxrs20.cdi12.fat.beanvalidation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.FIELD })
+@Constraint(validatedBy = { SomeObjectValidator.class })
+public @interface ValidSomeString {
+    String message() default "Some string is not valid!";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
