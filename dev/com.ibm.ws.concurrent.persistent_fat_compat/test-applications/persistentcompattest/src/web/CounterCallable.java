ce136664cdd4bfe96d34516a99c9305814099d23
KyleAure <KyleJAure@gmail.com>
2019-06-10 10:42:46 -0500

    Ported the compat test bucket to open liberty
    1. Original port
    2. Fixed creation of dropin app
    3. Local build and tests successful

---
+++ dev/com.ibm.ws.concurrent.persistent_fat_compat/test-applications/persistentcompattest/src/web/CounterCallable.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package web;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+import javax.enterprise.concurrent.ManagedTask;
+import javax.enterprise.concurrent.ManagedTaskListener;
+
+/**
+ * Callable that increments and returns a counter each time it runs.
+ */
+public class CounterCallable implements Callable<Integer>, ManagedTask, Serializable {
+    private static final long serialVersionUID = -3880167073113971050L;
+
+    int counter;
+    private final Map<String, String> execProps = new HashMap<String, String>();
+
+    @Override
+    public Integer call() throws Exception {
+        return ++counter;
+    }
+
+    @Override
+    public Map<String, String> getExecutionProperties() {
+        return execProps;
+    }
+
+    @Override
+    public ManagedTaskListener getManagedTaskListener() {
+        return null;
+    }
+}
---
+++ dev/com.ibm.ws.concurrent.persistent_fat_compat/test-applications/persistentcompattest/src/web/CounterCallableTriggerTask.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package web;
+
+import java.util.Date;
+
+import javax.enterprise.concurrent.LastExecution;
+import javax.enterprise.concurrent.Trigger;
+
+/**
+ * Callable that increments and returns a counter each time it runs, up to 3 times.
+ */
+public class CounterCallableTriggerTask extends CounterCallable implements Trigger {
+    private static final long serialVersionUID = -5686084710994254965L;
+
+    @Override
+    public Date getNextRunTime(LastExecution lastExecution, Date taskScheduledTime) {
+        if (lastExecution == null)
+            return taskScheduledTime;
+        if (counter >= 3)
+            return null;
+        long delay = (lastExecution.getRunEnd().getTime() - lastExecution.getRunStart().getTime()) / ((Integer) lastExecution.getResult()).longValue();
+        System.out.println("Delay for next execution of " + lastExecution.getIdentityName() + " will be " + delay);
+        return new Date(System.currentTimeMillis() + delay);
+    }
+
+    @Override
+    public boolean skipRun(LastExecution lastExecution, Date scheduledRunTime) {
+        return false;
+    }
+}
