0a2fae1dec82a894fb75d37c1b1f8a9837ef9115
chdavid <chdavid@ca.ibm.com>
2019-06-23 21:17:50 -0400

    appropriate copyrights

--- dev/com.ibm.ws.microprofile.metrics.2.0/src/com/ibm/ws/microprofile/metrics/impl/CounterImpl.java
+++ dev/com.ibm.ws.microprofile.metrics.2.0/src/com/ibm/ws/microprofile/metrics/impl/CounterImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2019 IBM Corporation and others.
+* Copyright (c) 2017, 2019 IBM Corporation and others.
 *
 * All rights reserved. This program and the accompanying materials
 * are made available under the terms of the Eclipse Public License v1.0
@@ -23,7 +23,7 @@
 *******************************************************************************/
 package com.ibm.ws.microprofile.metrics.impl;
 
-import org.eclipse.microprofile.metrics.Counter;
+import org.eclipse.microprofile.metrics.Counter;;
 
 /**
  * An incrementing and decrementing counter metric.
