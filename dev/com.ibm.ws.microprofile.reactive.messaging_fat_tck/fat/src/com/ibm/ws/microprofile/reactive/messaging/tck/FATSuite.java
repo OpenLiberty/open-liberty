9fc0bc1906241155e95174feb1f9592ee31a1f0b
Andrew Rouse <anrouse@uk.ibm.com>
2019-06-28 13:29:24 +0100

    Add MP Reactive Messaging TCK test project
    Experimental mode only until the TCK fixes are merged.

---
+++ dev/com.ibm.ws.microprofile.reactive.messaging_fat_tck/fat/src/com/ibm/ws/microprofile/reactive/messaging/tck/FATSuite.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.microprofile.reactive.messaging.tck;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import componenttest.custom.junit.runner.AlwaysPassesTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+                AlwaysPassesTest.class, //ReactiveMessagingTCKLauncher is set to full mode. This will prevent a build failure from zero tests running.
+                ReactiveMessagingTCKLauncher.class
+})
+
+public class FATSuite {
+}
