d076c12bcd583a9bb9fbfe3848d066a929290fea
KyleAure <KyleJAure@gmail.com>
2019-07-01 10:31:13 -0500

    intermediate commit

---
+++ dev/com.ibm.ws.concurrent.persistent_fat_jca/test-resourceadapters/PersistRA/src/fat/persistra/resourceadapter/RAResult.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package fat.persistra.resourceadapter;
+
+import java.io.Serializable;
+
+/**
+ * Result created by PCLBTask and PCLBSerializableTask.
+ */
+public class RAResult implements Serializable {
+    private static final long serialVersionUID = -6002499345514875821L;
+}
