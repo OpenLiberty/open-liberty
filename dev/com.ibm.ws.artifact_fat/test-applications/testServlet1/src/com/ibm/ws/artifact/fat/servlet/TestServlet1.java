98c1d44272d2d001e98eee3345235040979f170c
Brandon Berry <brandonberry@brandons-mbp.raleigh.ibm.com>
2019-07-02 10:31:50 -0400

    Updated test servlet with new archive

--- dev/com.ibm.ws.artifact_fat/test-applications/testServlet1/src/com/ibm/ws/artifact/fat/servlet/TestServlet1.java
+++ dev/com.ibm.ws.artifact_fat/test-applications/testServlet1/src/com/ibm/ws/artifact/fat/servlet/TestServlet1.java
@@ -11,15 +11,22 @@
 package com.ibm.ws.artifact.fat.servlet;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.net.URL;
+import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletContext;
 
-@WebServlet("/TestServlet1")
+
+@WebServlet("/")
 public class TestServlet1 extends HttpServlet {
     public static final String CLASS_NAME = "TestServlet1";
 
@@ -29,13 +36,61 @@ public class TestServlet1 extends HttpServlet {
     }
 
     @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException{
+        
+            String methodName = "doPost";
+
+            PrintWriter responseWriter = response.getWriter();
+            responseWriter.println(String.format("%s.%s: Enter",CLASS_NAME, methodName));
+            responseWriter.flush();
+            responseWriter.close();
+    }
+
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
         String methodName = "doGet";
+        ServletContext sc = request.getServletContext();
+        ClassLoader sccl = sc.getClassLoader();
 
+        URL testFile;
+        Class testClass;
+        InputStream in;
+        InputStream in2;
+        try{
+            testClass = sccl.loadClass("TestClass");
+            testFile = sccl.getResource("testfile.txt");
+            in = null;//sc.getResourceAsStream("WEB-INF/lib/TestJar.jar");
+            in2 = null; //testFile.openStream();
+        }catch(Exception e){
+
+            testClass = null;
+            testFile = null;
+            in = null;
+            in2 = null;
+            
+        }
+        
         PrintWriter responseWriter = response.getWriter();
-        responseWriter.println(String.format("%s.$s: Enter",CLASS_NAME, methodName));
+        responseWriter.println(testClass);
+        responseWriter.println(testFile);
+        responseWriter.println(in);
+        responseWriter.println(in2);
+        responseWriter.println("-----------------------");
+        responseWriter.println(request.getQueryString());
+        
         responseWriter.flush();
         responseWriter.close();
+
+
+
+        if(in != null){
+            //in.close();
+        }
+        if(in2 != null){
+            //in2.close();
+        }
+        
     }
 }
