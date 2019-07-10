06c95fa1becf0447d62811e2bf7e03bb653f1b35
kyleaure <kylejaure@gmail.com>
2019-07-10 09:41:11 -0500

    init upload

---
+++ dev/com.ibm.ws.concurrent.persistent_fat_feature/test-applications/CPFeatureApp/src/com/ibm/feature/ConcurrentPersistentFeatureServlet.java
@@ -0,0 +1,157 @@
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.feature;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class ConcurrentPersistentFeatureServlet extends HttpServlet {
+
+    static final LinkedBlockingQueue<Integer> lbqResult = new LinkedBlockingQueue<Integer>();
+    
+    /**
+     * Message written to servlet to indicate that is has been successfully invoked.
+     */
+    private static final String SUCCESS_MESSAGE = "COMPLETED SUCCESSFULLY";
+
+    @Resource(lookup = "concurrent/executorService")
+    private ScheduledExecutorService executorService;
+
+    @Resource(lookup = "test/TaskStoreTester")
+    // TODO how can we make this class accessible to the application at compile time?
+    //private TaskStoreTester taskStoreTester;
+    private Object taskStoreTester;
+
+    /**
+     * Entry point for a GET request. The test name is passed as  
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String test = request.getParameter("test");
+        PrintWriter out = response.getWriter();
+
+        try {
+            out.println("ContextSerializeServlet is starting " + test + "<br>");
+            System.out.println("-----> " + test + " starting");
+            getClass().getMethod(test, HttpServletRequest.class, HttpServletResponse.class).invoke(this, request, response);
+            System.out.println("<----- " + test + " successful");
+            out.println(test + " " + SUCCESS_MESSAGE);
+        } catch (Throwable x) {
+            if (x instanceof InvocationTargetException)
+                x = x.getCause();
+            System.out.println("<----- " + test + " failed:");
+            x.printStackTrace(System.out);
+            out.println("<pre>ERROR in " + test + ":");
+            x.printStackTrace(out);
+            out.println("</pre>");
+        } finally {
+            out.flush();
+            out.close();
+        }
+    }
+
+    /**
+     * Validates that an application cannot load classes such as:
+     * - javax.enterprise.concurrent.ManagedScheduledExecutorService
+     * - com.ibm.websphere.concurrent.persistent.PersistentExecutor
+     * because concurrent-1.0 or persistentExecutor-1.0 are not enabled.
+     */
+    public void validateAppInabilityToAccessConcurrentPersistentArtifacts(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    	try {
+    	    Class.forName("javax.enterprise.concurrent.ManagedScheduledExecutorService");
+    	    throw new Exception("ManagedScheduledExecutorService should not have been found. Feature concurrent-1.0 is not enabled.");
+    	} catch(ClassNotFoundException cnfe) {
+    		// Expected.
+    	}
+    	
+    	try {
+    	    Class.forName("com.ibm.websphere.concurrent.persistent.PersistentExecutor");
+    	    throw new Exception("PersistentExecutor should not have been found. Feature persistentExecutor-1.0 is not enabled.");
+    	} catch(ClassNotFoundException cnfe) {
+    		// Expected.
+    	}
+    }
+    
+    /**
+     * Schedule a basic callable using the ScheduledExecutorService reference.
+     * This reference should point to the configured persistentExecutor. Therefore, a call to
+     * future.get(timeToWait) should return an IllegalStateException for the initial snapshot of the task.
+     * The result is stored in a static queue.
+     * 
+     * @throws Exception
+     */
+    public void testCallToFuterGetWithWaitTimeThrowsAUnsupportedOperationException(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    	tidyupResultQueue();
+    	Callable<Integer> callable = new NoDbOpTestTask(lbqResult);
+        ScheduledFuture<Integer> future = executorService.schedule(callable, 0, TimeUnit.MILLISECONDS);
+        
+        try {
+            future.get(10, TimeUnit.SECONDS);
+            throw new Exception("An IllegalStateException should have been thrown when ScheduledFuture.get(timeToWait) is called.");
+        } catch(IllegalStateException x) {
+        	// Expected.
+        }
+
+        Integer result = lbqResult.poll(30, TimeUnit.SECONDS);
+        if (result == null || result != 1000) {
+        	throw new Exception("The result should have been 1000. Instead it was: " + result);
+        }
+
+    }
+
+    /**
+     * Have multiple threads invoke TaskStore.findOrCreate at the same time.
+     */
+    public void testFindOrCreate(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        taskStoreTester.getClass().getMethod("testFindOrCreate").invoke(taskStoreTester);
+    }
+
+    /**
+     * Schedule a basic Runnable using the ScheduledExecutorService reference we looked up. 
+     * This reference should point to the configured persistentExecutor. 
+     * The result is stored in a static queue.
+     * 
+     * @throws Exception
+     */
+    public void scheduleASimpleTaskNoDatabaseExecution(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    	tidyupResultQueue();
+    	Runnable runnable = new NoDbOpTestTask(lbqResult);
+    	executorService.schedule(runnable, 0, TimeUnit.MILLISECONDS);
+        Integer result = lbqResult.poll(30, TimeUnit.SECONDS);
+        if (result == null || result != 1000) {
+        	throw new Exception("The result should have been 1000. Instead it was: " + result);
+        }
+    }
+    
+    /**
+     * Cleans up the result queue.
+     * 
+     * @throws Exception
+     */
+    void tidyupResultQueue() throws Exception {
+    	lbqResult.clear();
+    	if (lbqResult.size() != 0)
+    		throw new Exception("The result queue was not cleared. Queue: " + lbqResult);
+    }
+}
---
+++ dev/com.ibm.ws.concurrent.persistent_fat_feature/test-bundles/userFeature/src/test/concurrent/persistent/feature/TaskStoreTester.java
@@ -0,0 +1,136 @@
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package test.concurrent.persistent.feature;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import javax.naming.InitialContext;
+import javax.transaction.Status;
+import javax.transaction.UserTransaction;
+
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+import test.concurrent.persistent.feature.internal.ExecutorFactoryServiceImpl;
+
+import com.ibm.wsspi.concurrent.persistent.PartitionRecord;
+import com.ibm.wsspi.concurrent.persistent.TaskStore;
+import com.ibm.wsspi.resource.ResourceFactory;
+import com.ibm.wsspi.resource.ResourceInfo;
+import com.ibm.wsspi.threadcontext.WSContextService;
+
+@Component(configurationPolicy = ConfigurationPolicy.IGNORE,
+           property = {"creates.objectClass=test.concurrent.persistent.feature.TaskStoreTester", "jndiName=test/TaskStoreTester"})
+public class TaskStoreTester implements ResourceFactory {
+	private WSContextService contextSvc;
+	private TaskStore taskStore;
+	private ExecutorService unmanagedExecutor; 
+
+	@Activate
+	protected void activate(ComponentContext context) {
+		unmanagedExecutor = Executors.newSingleThreadExecutor();
+	}
+
+	@Override
+	public Object createResource(ResourceInfo info) throws Exception {
+		return this;
+	}
+
+	@Deactivate
+	protected void deactivate(ComponentContext context) {
+        unmanagedExecutor.shutdownNow();
+	}
+
+    @Reference (target = "(component.name=com.ibm.ws.context.manager)")
+    protected void setContextService(WSContextService contextSvc) {
+    	this.contextSvc = contextSvc;
+    }
+
+    @Reference (target = "(service.factoryPid=concurrent.persistent.feature.test)")
+    protected void setExecutorFactoryServiceImpl(ResourceFactory factory) throws Exception {
+    	this.taskStore = ((ExecutorFactoryServiceImpl) factory).getTaskStore();
+    }
+
+    /**
+     * Have multiple threads invoke taskStore.findOrCreate at the same time
+     */
+    public void testFindOrCreate() throws Exception {
+    	final UserTransaction tran = (UserTransaction) new InitialContext().lookup("java:comp/UserTransaction");
+
+    	final PartitionRecord record = new PartitionRecord(false);
+    	record.setExecutor("executor1");
+    	record.setHostName("host1");
+    	record.setLibertyServer("libertyServer1");
+    	record.setUserDir("C:/myFolder");
+
+    	Callable<Long> findOrCreate = new Callable<Long>() {
+    		public Long call() throws Exception {
+    	    	tran.begin();
+    	    	try {
+    	    		return taskStore.findOrCreate(record);
+    	    	} finally {
+    	    		if (tran.getStatus() == Status.STATUS_MARKED_ROLLBACK)
+    	    			tran.rollback();
+    	    		else
+    	    		    tran.commit();
+    	    	}
+    		}
+    	};
+
+    	@SuppressWarnings("unchecked")
+		Callable<Long> findOrCreateWithContext = contextSvc.createContextualProxy(
+    			contextSvc.captureThreadContext(null), findOrCreate, Callable.class);
+
+    	Future<Long> future = unmanagedExecutor.submit(findOrCreateWithContext);
+
+    	long partitionId1;
+    	try {
+    		partitionId1 = findOrCreate.call();
+    	} catch (Exception x) {
+    		for (Throwable cause = x; cause != null; cause = cause.getCause())
+    			if (cause instanceof SQLIntegrityConstraintViolationException) {
+    				future.get();
+    				return; // expected if both threads try to create the same entry
+    			}
+    		throw x;
+    	}
+
+    	long partitionId2;
+    	try {
+    		partitionId2 = future.get();
+    	} catch (ExecutionException x) {
+    		for (Throwable cause = x; cause != null; cause = cause.getCause())
+    			if (cause instanceof SQLIntegrityConstraintViolationException)
+    				return; // expected if both threads try to create the same entry
+    		throw x;
+    	}
+
+    	if (partitionId1 != partitionId2)
+    		throw new Exception("Id " + partitionId1 + " does not match " + partitionId2);
+    }
+
+    protected void unsetContextService(WSContextService contextSvc) {
+    	this.contextSvc = null;
+    }
+
+    protected void unsetExecutorFactoryServiceImpl(ResourceFactory factory) {
+    	this.taskStore = null;
+    }
+}
