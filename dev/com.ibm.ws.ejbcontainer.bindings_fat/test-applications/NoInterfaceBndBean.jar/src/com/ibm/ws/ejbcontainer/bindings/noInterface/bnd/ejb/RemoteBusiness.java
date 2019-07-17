d702bd657bba8d059b2501a6131b764ea5799d05
Brian Decker <bmdecker@us.ibm.com>
2019-07-17 15:06:55 -0500

    Issue #8114: Port NoInterface Bindings Tests - Tests will be disabled for now

--- dev/com.ibm.ws.ejbcontainer.bindings_fat/test-applications/NoInterfaceBndBean.jar/src/com/ibm/ws/ejbcontainer/bindings/noInterface/bnd/ejb/RemoteBusiness.java
+++ dev/com.ibm.ws.ejbcontainer.bindings_fat/test-applications/NoInterfaceBndBean.jar/src/com/ibm/ws/ejbcontainer/bindings/noInterface/bnd/ejb/RemoteBusiness.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2019 IBM Corporation and others.
+ * Copyright (c) 2009, 2019 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,17 +8,11 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-package com.ibm.ws.ejbcontainer.bindings.fat;
+package com.ibm.ws.ejbcontainer.bindings.noInterface.bnd.ejb;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-import com.ibm.ws.ejbcontainer.bindings.fat.tests.DefaultBindingsTest;
-
-@RunWith(Suite.class)
-@SuiteClasses({
-                DefaultBindingsTest.class
-})
-public class FATSuite {
+/**
+ * EJB 3.x Remote interface for Enterprise Beans
+ */
+public interface RemoteBusiness {
+    public String remoteMethod(String text);
 }
