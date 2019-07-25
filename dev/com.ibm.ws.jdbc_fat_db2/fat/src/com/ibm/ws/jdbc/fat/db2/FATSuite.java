a5c5e07446c284c629457e4d5bba98e9ed025730
Andrew Guibert <andy.guibert@gmail.com>
2019-07-25 14:51:50 -0500

    Use new DB2 Testcontainers module instead of custom module

--- dev/com.ibm.ws.jdbc_fat_db2/fat/src/com/ibm/ws/jdbc/fat/db2/FATSuite.java
+++ dev/com.ibm.ws.jdbc_fat_db2/fat/src/com/ibm/ws/jdbc/fat/db2/FATSuite.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2017,2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package com.ibm.ws.jdbc.fat.db2;
 
 import java.time.Duration;
@@ -6,6 +16,7 @@ import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import org.testcontainers.containers.Db2Container;
 import org.testcontainers.containers.output.OutputFrame;
 
 import com.ibm.websphere.simplicity.log.Log;
@@ -20,7 +31,7 @@ import componenttest.custom.junit.runner.FATRunner;
 public class FATSuite {
 
     @ClassRule
-    public static DB2Container db2 = new DB2Container()
+    public static Db2Container db2 = new Db2Container()
                     .acceptLicense()
                     // Use 5m timeout for local runs, 15m timeout for remote runs
                     .withStartupTimeout(Duration.ofMinutes(FATRunner.FAT_TEST_LOCALRUN ? 5 : 15))
