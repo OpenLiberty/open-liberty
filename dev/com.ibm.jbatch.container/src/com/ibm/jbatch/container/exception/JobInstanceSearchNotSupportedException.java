81d98e64b1941892847a45d0f3da42fd25098ccc
Dan Belina <belina@us.ibm.com>
2019-08-03 22:14:19 -0400

    Use NLS message for exception at this location

---
+++ dev/com.ibm.jbatch.container/src/com/ibm/jbatch/container/exception/JobInstanceSearchNotSupportedException.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.jbatch.container.exception;
+
+public class JobInstanceSearchNotSupportedException extends UnsupportedOperationException {
+
+    private static final long serialVersionUID = 1L;
+
+    public JobInstanceSearchNotSupportedException() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public JobInstanceSearchNotSupportedException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public JobInstanceSearchNotSupportedException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public JobInstanceSearchNotSupportedException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+}
