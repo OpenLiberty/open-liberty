45997dc31b25b170f72be37e59d6a7e5d1bf7081
Brandon Berry <brandonberry@brandons-mbp-5.raleigh.ibm.com>
2019-08-06 12:57:25 -0400

    added overlay introspector

--- dev/com.ibm.ws.artifact.overlay/src/com/ibm/ws/artifact/overlay/internal/OverlayCache.java
+++ dev/com.ibm.ws.artifact.overlay/src/com/ibm/ws/artifact/overlay/internal/OverlayCache.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 IBM Corporation and others.
+ * Copyright (c) 2011, 2019 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,7 +10,9 @@
  *******************************************************************************/
 package com.ibm.ws.artifact.overlay.internal;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -45,6 +47,20 @@ public class OverlayCache {
         } else {
             return null;
         }
+    }
+    
+    public synchronized boolean isCacheEmpty(){
+        return cache.isEmpty();
+    }
+
+    public synchronized Map<String, Map<Class, Object>> getSnapshot(){
+        Map<String, Map<Class, Object>> retValue = new HashMap<String, Map<Class, Object>>();
+        
+        for( Map.Entry<String, Map<Class, Object>> stringTo: cache.entrySet() ){
+            retValue.put(stringTo.getKey(), new HashMap<Class, Object>(stringTo.getValue()));
+        }
+
+        return retValue;
     }
 
 }
