7095ff83301ef95d01f1b3491e049b6af64f33a3
Thomas Bitonti <bitonti@us.ibm.com>
2019-08-13 14:25:13 -0400

    Remove IBM Confidential from open source files

--- dev/com.ibm.ws.anno/src/com/ibm/ws/annocache/targets/internal/TargetsTableImpl.java
+++ dev/com.ibm.ws.anno/src/com/ibm/ws/annocache/targets/internal/TargetsTableImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2019 IBM Corporation and others.
+ * Copyright (c) 2014, 2019 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,7 +8,6 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-
 package com.ibm.ws.annocache.targets.internal;
 
 import java.io.IOException;
