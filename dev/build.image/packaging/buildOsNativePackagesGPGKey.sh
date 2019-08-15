ecba60c6f7764f62e1f895d71fe1b65f17512ba2
root <root@seaward1.fyre.ibm.com>
2019-08-15 11:27:46 -0700

    Added error handling for GPGkey signing and changed back is.release

--- dev/build.image/packaging/buildOsNativePackagesGPGKey.sh
+++ dev/build.image/packaging/buildOsNativePackagesGPGKey.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-#add rpm macro for rpm signing to identify key
-touch .rpmmacros
-echo "%_gpg_name Open Liberty Project" >> .rpmmacros
 
-#run the expect script to build the rpm
-expect -f rpmBuild.expect
+#check that GPG_PASS is defined
+if [ -z "$GPG_PASS" ]
+then
+      echo "GPG PASSPHRASE is undefined.  skip signing of rpm/deb packages"
+      exit 0
+else
+      echo "GPG PASSPHRASE is defined."
 
-#Run the expect script to build the deb
-expect -f debBuild.expect
+      #add rpm macro for rpm signing to identify key
+      touch .rpmmacros
+      echo "%_gpg_name Open Liberty Project" >> .rpmmacros
+
+      #run the expect script to build the rpm
+      echo -e “${GPG_PASS}\n" | rpmbuild -ba --sign rpmbuild/SPECS/openliberty.spec
+
+      #Run the expect script to build the deb
+      cd debuild/openliberty/debian
+      echo -e “${GPG_PASS}\n${GPG_PASS}\n" | debuild -d -b -sa
+fi
