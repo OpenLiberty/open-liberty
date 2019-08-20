a935fc93a9bcf2848f0c292fefb95a69cf887813
root <root@seaward1.fyre.ibm.com>
2019-08-20 10:07:46 -0700

    changed code for signing rpm and deb packages

--- dev/build.image/packaging/buildOsNativePackagesGPGKey.sh
+++ dev/build.image/packaging/buildOsNativePackagesGPGKey.sh
@@ -4,19 +4,22 @@
 #check that GPG_PASS is defined
 if [ -z "$GPG_PASS" ]
 then
-      echo "GPG PASSPHRASE is undefined.  skip signing of rpm/deb packages"
-      exit 0
+     echo "GPG PASSPHRASE is undefined.  skip signing of rpm/deb packages"
+     exit 0
 else
-      echo "GPG PASSPHRASE is defined."
+     echo "GPG PASSPHRASE is defined."
 
-      #add rpm macro for rpm signing to identify key
-      touch .rpmmacros
-      echo "%_gpg_name Open Liberty Project" >> .rpmmacros
+     #add rpm macro for rpm signing to identify key
+     touch .rpmmacros
+     echo "%_gpg_name Open Liberty Project" >> .rpmmacros
 
-      #run the expect script to build the rpm
-      echo -e “${GPG_PASS}\n" | rpmbuild -ba --sign rpmbuild/SPECS/openliberty.spec
+     #build the rpm
+     cd rpmbuild
+     echo -e "${GPG_PASS}\n" | rpmbuild -ba --sign SPECS/openliberty.spec
+     cd ..
 
-      #Run the expect script to build the deb
-      cd debuild/openliberty/debian
-      echo -e “${GPG_PASS}\n${GPG_PASS}\n" | debuild -d -b -sa
+     #build the deb
+     cd debuild/openliberty/debian
+     echo -e "${GPG_PASS}\n${GPG_PASS}\n" | debuild -d -b -sa
+     cd ..
 fi
