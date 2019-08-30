5f1aa18a54c534c865cd0f66a67c017e7c421f00
Mehdi Merali <mmerali@ibm.com>
2019-08-30 15:25:45 -0400

    Added code to create contextual proxy and added new attribute for ContextService in <timerService>

--- dev/com.ibm.ws.ejbcontainer.timer/src/com/ibm/ws/ejbcontainer/timer/osgi/internal/EJBTimerRuntimeImpl.java
+++ dev/com.ibm.ws.ejbcontainer.timer/src/com/ibm/ws/ejbcontainer/timer/osgi/internal/EJBTimerRuntimeImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2016 IBM Corporation and others.
+ * Copyright (c) 2013, 2019 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package com.ibm.ws.ejbcontainer.timer.osgi.internal;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -26,26 +27,31 @@ import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
 import org.osgi.service.component.annotations.ReferencePolicyOption;
 
+import com.ibm.ejs.container.BeanMetaData;
 import com.ibm.ejs.container.TimerNpImpl;
 import com.ibm.ejs.container.TimerNpRunnable;
+import com.ibm.websphere.csi.J2EEName;
 import com.ibm.websphere.ras.Tr;
 import com.ibm.websphere.ras.TraceComponent;
 import com.ibm.websphere.ras.annotation.Trivial;
 import com.ibm.ws.ejbcontainer.osgi.EJBPersistentTimerRuntime;
 import com.ibm.ws.ejbcontainer.osgi.EJBTimerRuntime;
 import com.ibm.wsspi.kernel.service.utils.AtomicServiceReference;
+import com.ibm.wsspi.threadcontext.ThreadContextDescriptor;
+import com.ibm.wsspi.threadcontext.WSContextService;
 
 @Component(name = "com.ibm.ws.ejbcontainer.osgi.EJBTimerRuntime",
            service = EJBTimerRuntime.class,
            configurationPid = "com.ibm.ws.ejbcontainer.timer.runtime",
            configurationPolicy = ConfigurationPolicy.OPTIONAL,
-           property = "service.vendor=IBM")
+           property = { "service.vendor=IBM", "contextService.target=(id=unbound)" })
 public class EJBTimerRuntimeImpl implements EJBTimerRuntime {
 
     private static final TraceComponent tc = Tr.register(EJBTimerRuntimeImpl.class);
 
     private static final String REFERENCE_PERSISTENT_EXECUTOR = "persistentExecutor";
     private static final String REFERENCE_EJB_PERSISTENT_TIMER_RUNTIME = "ejbPersistentTimerRuntime";
+    private static final String REFERENCE_NON_PERSISTENT_CONTEXT_SERVICE = "nonPersistentContextService";
 
     private static final String PERSISTENT_EXECUTOR_REF = "persistentExecutorRef";
     private static final String LATE_TIMER_THRESHOLD = "lateTimerThreshold";
@@ -57,6 +63,13 @@ public class EJBTimerRuntimeImpl implements EJBTimerRuntime {
     private volatile String configuredPersistentExecutor;
     private final AtomicServiceReference<ScheduledExecutorService> persistentExecutorRef = new AtomicServiceReference<ScheduledExecutorService>(REFERENCE_PERSISTENT_EXECUTOR);
     private final AtomicServiceReference<EJBPersistentTimerRuntime> ejbPersistentTimerRuntimeServiceRef = new AtomicServiceReference<EJBPersistentTimerRuntime>(REFERENCE_EJB_PERSISTENT_TIMER_RUNTIME);
+    private final AtomicServiceReference<WSContextService> nonPersistentContextServiceRef = new AtomicServiceReference<WSContextService>(REFERENCE_NON_PERSISTENT_CONTEXT_SERVICE);
+
+    /**
+     * This is the value of javax.enterprise.concurrent.ManagedTask.IDENTITY_NAME,
+     * but is hard-coded here to avoid a dependency on the concurrency feature.
+     */
+    private static final String MANAGEDTASK_IDENTITY_NAME = "javax.enterprise.concurrent.IDENTITY_NAME";
 
     /**
      * Late timer warning threshold in milliseconds, set from
@@ -68,7 +81,7 @@ public class EJBTimerRuntimeImpl implements EJBTimerRuntime {
      * Timer timeout roll backs will be retried the following number of times (once
      * immediately, and thereafter on the retry interval. The EJB 3.1 spec, section
      * 18.4.3 requires at least one retry be attempted. If -1, retry indefinitely.
-     * 
+     *
      * Set from nonPersistentTimerRetryCount attribute on EJBTimer config.
      * Default is not set, which is represented here as -1, and means
      * to retry indefinitely. Configuration of 0 is allowed, and means
@@ -120,6 +133,7 @@ public class EJBTimerRuntimeImpl implements EJBTimerRuntime {
     protected void activate(ComponentContext cc, Map<String, Object> properties) {
         persistentExecutorRef.activate(cc);
         ejbPersistentTimerRuntimeServiceRef.activate(cc);
+        nonPersistentContextServiceRef.activate(cc);
         updateConfiguration(properties);
     }
 
@@ -153,11 +167,15 @@ public class EJBTimerRuntimeImpl implements EJBTimerRuntime {
     protected void deactivate(ComponentContext cc) {
         persistentExecutorRef.deactivate(cc);
         ejbPersistentTimerRuntimeServiceRef.deactivate(cc);
+        nonPersistentContextServiceRef.deactivate(cc);
     }
 
     @Override
     public TimerNpRunnable createNonPersistentTimerTaskHandler(TimerNpImpl timer) {
-        return new TimerNpSERunnable(executorService, timer, npTimerServiceTimerRetryCount, npTimerServiceTimerRetryInterval);
+        TimerNpSERunnable timerRunnable = new TimerNpSERunnable(executorService, timer, npTimerServiceTimerRetryCount, npTimerServiceTimerRetryInterval);
+        Runnable r = createNonPersistantTimerContextualProxy(timerRunnable);
+        timerRunnable.setContextualProxyRunnable(r);
+        return timerRunnable;
     }
 
     @Override
@@ -203,4 +221,65 @@ public class EJBTimerRuntimeImpl implements EJBTimerRuntime {
         this.ejbPersistentTimerRuntimeServiceRef.unsetReference(ref);
     }
 
+    @Reference(name = REFERENCE_NON_PERSISTENT_CONTEXT_SERVICE,
+               service = WSContextService.class,
+               policy = ReferencePolicy.DYNAMIC,
+               policyOption = ReferencePolicyOption.GREEDY,
+               cardinality = ReferenceCardinality.OPTIONAL)
+    protected void setNonPersistentContextServiceRef(ServiceReference<WSContextService> npContextServiceRef) {
+        nonPersistentContextServiceRef.setReference(npContextServiceRef);
+    }
+
+    protected void updatedNonPersistentContextServiceRef(ServiceReference<WSContextService> npContextServiceRef) {
+        // Don't care if the referenced context service has been updated
+    }
+
+    protected void unsetNonPersistentContextServiceRef(ServiceReference<WSContextService> npContextServiceRef) {
+        nonPersistentContextServiceRef.unsetReference(npContextServiceRef);
+    }
+
+    /**
+     * Creates contextual proxy with thread contexts needed for context propagation of non persistent EJB timers.
+     * Only creates proxy if <contextService/> is configured with contexts, otherwise no contexts are propagated.
+     *
+     * @param taskHandler
+     * @return Runnable proxy
+     */
+    public Runnable createNonPersistantTimerContextualProxy(TimerNpSERunnable taskHandler) {
+        Runnable proxy = null;
+        WSContextService contextService = nonPersistentContextServiceRef.getService();
+
+        if (contextService != null) {
+            TimerNpImpl tmr = taskHandler.getNpTimer();
+            BeanMetaData beanMetaData = tmr.getBeanMetaData();
+            int methodId = taskHandler.getMethodId();
+            J2EEName j2eeName = beanMetaData.getJ2EEName();
+
+            // Get method name from BeanMetaData
+            String methodName;
+            if (beanMetaData.timedMethodInfos != null) {
+                methodName = beanMetaData.timedMethodInfos[methodId].getMethodName();
+            } else {
+                methodName = beanMetaData.ivInitData.ivTimerMethods.get(methodId).getMethod().getName();
+            }
+
+            String taskOwner = j2eeName.getApplication() + "/" + j2eeName.getModule() + "/" + j2eeName.getComponent();
+            String taskIdentity = taskOwner + "-" + methodName;
+            Map<String, String> executionProperties = new HashMap<String, String>();
+            executionProperties.put(MANAGEDTASK_IDENTITY_NAME, taskIdentity);
+            executionProperties.put(WSContextService.TASK_OWNER, taskOwner);
+            Map<String, ?>[] requiredContexts = null;
+            ThreadContextDescriptor tcDescriptor = contextService.captureThreadContext(executionProperties);
+            proxy = contextService.createContextualProxy(tcDescriptor, taskHandler, Runnable.class);
+
+            if (tc.isDebugEnabled()) {
+                Tr.debug(this, tc, "Created proxy for np timer.");
+            }
+        } else {
+            if (tc.isDebugEnabled()) {
+                Tr.debug(this, tc, "Context Service is null. Did not create proxy for np timer.");
+            }
+        }
+        return proxy;
+    }
 }
--- dev/com.ibm.ws.ejbcontainer.timer/src/com/ibm/ws/ejbcontainer/timer/osgi/internal/TimerNpSERunnable.java
+++ dev/com.ibm.ws.ejbcontainer.timer/src/com/ibm/ws/ejbcontainer/timer/osgi/internal/TimerNpSERunnable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2014 IBM Corporation and others.
+ * Copyright (c) 2013, 2019 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -35,9 +35,25 @@ public class TimerNpSERunnable extends TimerNpRunnable {
      */
     private ScheduledFuture<?> ivTaskFuture;
 
+    /** The contextual proxy runnable */
+    private Runnable npTask;
+
     TimerNpSERunnable(ScheduledExecutorService executorService, TimerNpImpl timerNpImpl, int retryLimit, long retryInterval) {
         super(timerNpImpl, retryLimit, retryInterval);
         ivExecutorService = executorService;
+    }
+
+    /**
+     * Sets the contextual proxy runnable. If r is null, it will use this class.
+     *
+     * @param r
+     */
+    public void setContextualProxyRunnable(Runnable r) {
+        if (r == null) {
+            this.npTask = this;
+        } else {
+            this.npTask = r;
+        }
     }
 
     // --------------------------------------------------------------------------
@@ -49,18 +65,18 @@ public class TimerNpSERunnable extends TimerNpRunnable {
     @Override
     protected void schedule(long expiration) {
         long delay = Math.max(0, expiration - System.currentTimeMillis());
-        ivTaskFuture = ivExecutorService.schedule(this, delay, TimeUnit.MILLISECONDS);
+        ivTaskFuture = ivExecutorService.schedule(this.npTask, delay, TimeUnit.MILLISECONDS);
     }
 
     @Override
     protected void scheduleNext(long nextExpiration) {
         long delay = Math.max(0, nextExpiration - System.currentTimeMillis());
-        ivTaskFuture = ivExecutorService.schedule(this, delay, TimeUnit.MILLISECONDS);
+        ivTaskFuture = ivExecutorService.schedule(this.npTask, delay, TimeUnit.MILLISECONDS);
     }
 
     @Override
     protected void scheduleRetry(long retryInterval) {
-        ivTaskFuture = ivExecutorService.schedule(this, retryInterval, TimeUnit.MILLISECONDS);
+        ivTaskFuture = ivExecutorService.schedule(this.npTask, retryInterval, TimeUnit.MILLISECONDS);
     }
 
     @Override
