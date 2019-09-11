941881d2399d3c43b0cae8c6cfffd9db1efb1323
root <root@genetic1.fyre.ibm.com>
2019-09-11 10:12:42 -0700

    went back to using expect scripts

--- dev/build.image/packaging/buildOsNativePackagesGPGKey.sh
+++ dev/build.image/packaging/buildOsNativePackagesGPGKey.sh
@@ -6,20 +6,23 @@ if [ -z "$GPG_PASS" ]
 then
      echo "GPG PASSPHRASE is undefined.  skip signing of rpm/deb packages"
      exit 0
-else
-     echo "GPG PASSPHRASE is defined. It is ${GPG_PASS}"
+else 
+     File=/usr/bin/expect
+     if [ -e $File ]; then
+         echo "/usr/bin/expect exist"
+         #!/bin/sh
 
-     #add rpm macro for rpm signing to identify key
-     touch .rpmmacros
-     echo "%_gpg_name Open Liberty Project" >> .rpmmacros
+         #add rpm macro for rpm signing to identify key
+         touch .rpmmacros
+         echo "%_gpg_name Open Liberty Project" >> .rpmmacros
 
-     #build the rpm
-     cd rpmbuild
-     echo -e "${GPG_PASS}\n" | rpmbuild -ba --sign SPECS/openliberty.spec
-     cd ..
+         #run the expect script to build the rpm
+         expect -f rpmBuild.expect
 
-     #build the deb
-     cd debuild/openliberty/debian
-     echo -e "${GPG_PASS}\n${GPG_PASS}\n" | debuild -d -b -sa
-     cd ..
+         #Run the expect script to build the deb
+         expect -f debBuild.expect
+     else 
+         echo "/usr/bin/expect does not exist"
+	 exit 0
+     fi
 fi
