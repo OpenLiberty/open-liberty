e492723b26ee19fc225d6e8ececc68a00c1c405b
John Koehler <jkoehler@us.ibm.com>
2019-09-24 13:55:37 -0500

    Issue #8037: Use ForkJoinPool when running in a Java SE environment like the TCK

--- dev/com.ibm.ws.org.apache.cxf.cxf.rt.rs.client.3.2/src/org/apache/cxf/jaxrs/client/CompletionStageRxInvokerImpl.java
+++ dev/com.ibm.ws.org.apache.cxf.cxf.rt.rs.client.3.2/src/org/apache/cxf/jaxrs/client/CompletionStageRxInvokerImpl.java
@@ -18,9 +18,11 @@
  */
 package org.apache.cxf.jaxrs.client;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutorService;
 import java.util.function.Supplier;
+import java.util.logging.Logger;
 
 import javax.ws.rs.HttpMethod;
 import javax.ws.rs.client.CompletionStageRxInvoker;
@@ -29,27 +31,23 @@ import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
 
 //Liberty code change start
-import org.eclipse.microprofile.context.ManagedExecutor;
+import org.apache.cxf.common.logging.LogUtils;
+
 import com.ibm.ws.jaxrs21.clientconfig.JAXRSClientCompletionStageFactoryConfig;
-
 import com.ibm.ws.concurrent.mp.spi.CompletionStageFactory;
-
 //Liberty code change end
+
 public class CompletionStageRxInvokerImpl implements CompletionStageRxInvoker {
     private WebClient wc;
     private ExecutorService ex;    
-  //Liberty code change start
-    private boolean isManagedExecutor = false;
+    //Liberty code change start
+    private static final Logger LOG = LogUtils.getL7dLogger(CompletionStageRxInvokerImpl.class);
     private CompletionStageFactory completionStageFactory = JAXRSClientCompletionStageFactoryConfig.getCompletionStageFactory();
-        
+    //Liberty code change end        
     CompletionStageRxInvokerImpl(WebClient wc, ExecutorService ex) {
-        this.ex = ex;        
-        if (this.ex instanceof ManagedExecutor) {            
-            isManagedExecutor = true;
-        }
+        this.ex = ex;
         this.wc = wc;
-    }   
-  //Liberty code change end
+    }
 
     @Override
     public CompletionStage<Response> get() {
@@ -186,10 +184,19 @@ public class CompletionStageRxInvokerImpl implements CompletionStageRxInvoker {
     }
 
   //Liberty code change start
-    private <T> CompletionStage<T> supplyAsync(Supplier<T> supplier) {      
-         if (isManagedExecutor) {
-             return ((ManagedExecutor) ex).supplyAsync(supplier);
-         }        
+    private <T> CompletionStage<T> supplyAsync(Supplier<T> supplier) {
+         
+        if (completionStageFactory == null) {
+            // completionStageFactory should only be null when running in a Java SE environment like the TCK.      
+            
+            LOG.warning("Running in an Java SE environment.  Using ForkJoinPool.commonPool()");
+            
+            if (ex == null) {
+                return CompletableFuture.supplyAsync(supplier);
+            }
+            return CompletableFuture.supplyAsync(supplier, ex);
+        }        
+
         return completionStageFactory.supplyAsync(supplier, ex); 
     }
   //Liberty code change end
