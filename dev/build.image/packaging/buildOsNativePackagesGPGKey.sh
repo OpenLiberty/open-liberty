fd86cc9c0f44b54a57ff40ebac8c2ffc1d4e3555
root <root@genetic1.fyre.ibm.com>
2019-10-04 08:33:49 -0700

    fixed changes for signing

--- dev/build.image/packaging/buildOsNativePackagesGPGKey.sh
+++ dev/build.image/packaging/buildOsNativePackagesGPGKey.sh
@@ -10,20 +10,19 @@ else
      #!/bin/sh
 
      #add rpm macro for rpm signing to identify key
-     touch .rpmmacros
-     echo "%_gpg_name Open Liberty Project" >> .rpmmacros
+     #touch .rpmmacros
+     #echo "%_gpg_name Open Liberty Project" >> .rpmmacros
 
      #run the expect script to build the rpm
      #expect -f rpmBuild.expect
 
-     #run commands to build the deb
-     sudo touch ~/.gnupg/pp.txt 
-     sudo echo "{$GPG_PASS}" >> ~/.gnupg/pp.txt
+     #run commands to build the deb 
+     echo "${GPG_PASS}" >> ~/.gnupg/pp.txt
      debuild -d -b -p'gpg --passphrase-file ~/.gnupg/pp.txt --batch'  -e"admin@openliberty.io"
      if gpg -k |grep admin ; then
          echo gpg key imported correctly
      else
-     	 echo gpg key not imported correctly
+         echo gpg key not imported correctly
      fi
-     sudo rm -f ~/.gnupg/pp.txt
+     rm -f ~/.gnupg/pp.txt
 fi
