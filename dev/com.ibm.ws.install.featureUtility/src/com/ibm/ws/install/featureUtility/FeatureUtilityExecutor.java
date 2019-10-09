2854e0aac453befc8bb406de5771ef19024339f3
Tayyab Waqar <r44zor@gmail.com>
2019-10-09 14:26:47 -0400

    fix copyright dates

--- dev/com.ibm.ws.install.featureUtility/src/com/ibm/ws/install/featureUtility/FeatureUtilityExecutor.java
+++ dev/com.ibm.ws.install.featureUtility/src/com/ibm/ws/install/featureUtility/FeatureUtilityExecutor.java
@@ -1,4 +1,13 @@
-/*******************************************************************************n * Copyright (c) 2019 IBM Corporation and others.n * All rights reserved. This program and the accompanying materialsn * are made available under the terms of the Eclipse Public License v1.0n * which accompanies this distribution, and is available atn * http://www.eclipse.org/legal/epl-v10.htmln *n * Contributors:n *     IBM Corporation - initial API and implementationn *******************************************************************************/
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package com.ibm.ws.install.featureUtility;
 
 
@@ -22,7 +31,7 @@ public class FeatureUtilityExecutor {
      * @throws InstallException
      * @throws IOException
      */
-    public static void main(String[] argsArray) throws IOException, InstallException {
+    public static void main(String[] argsArray) {
         ExitCode rc;
 
         Arguments args = new ArgumentsImpl(argsArray);
