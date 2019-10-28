819b16361d8bd89e5ed1ae23d143cc78655c793b
wraschke@us.ibm.com <wraschke@us.ibm.com>
2019-10-28 17:35:41 -0500

    Another test PR

--- dev/build.example_fat/fat/src/com/ibm/ws/example/SimpleTest.java
+++ dev/build.example_fat/fat/src/com/ibm/ws/example/SimpleTest.java
@@ -1,4 +1,5 @@
 /*******************************************************************************
+ * IBM Confidential
  * Copyright (c) 2017, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
