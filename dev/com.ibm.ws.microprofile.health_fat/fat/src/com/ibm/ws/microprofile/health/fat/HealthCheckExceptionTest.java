0acfb679655a04f929951fe0b405e5846ebd560f
Nathan Rauh <nathan.rauh@us.ibm.com>
2019-10-29 09:13:31 -0500

    Issue #9499 update Health Check test case that was erroneously expecting the FFDC

--- dev/com.ibm.ws.microprofile.health_fat/fat/src/com/ibm/ws/microprofile/health/fat/HealthCheckExceptionTest.java
+++ dev/com.ibm.ws.microprofile.health_fat/fat/src/com/ibm/ws/microprofile/health/fat/HealthCheckExceptionTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 IBM Corporation and others.
+ * Copyright (c) 2017,2019 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -56,8 +56,7 @@ public class HealthCheckExceptionTest {
     }
 
     @Test
-    @ExpectedFFDC({ "java.lang.reflect.InvocationTargetException",
-                    "com.ibm.ws.microprofile.health.services.HealthCheckBeanCallException", "java.lang.RuntimeException" })
+    @ExpectedFFDC({ "com.ibm.ws.microprofile.health.services.HealthCheckBeanCallException", "java.lang.RuntimeException" })
     public void testHealthCheckException() throws Exception {
 
         URL healthURL = new URL("http://" + server1.getHostname() + ":" + server1.getHttpDefaultPort() + "/health");
