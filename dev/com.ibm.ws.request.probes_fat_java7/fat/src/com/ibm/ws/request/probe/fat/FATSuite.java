4c0a304fd80a84a35e16082d26ac271272569579
Kavan Lam <johndoe@us.ibm.com>
2019-10-31 15:10:37 -0400

    Moving request probes Java 7 FAT test from CL

---
+++ dev/com.ibm.ws.request.probes_fat_java7/fat/src/com/ibm/ws/request/probe/fat/FATSuite.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.request.probe.fat;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import componenttest.custom.junit.runner.AlwaysPassesTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+                AlwaysPassesTest.class,
+                EventLoggingEE7.class,
+                RequestTimingEE7.class
+})
+/**
+ * Purpose: This suite collects and runs all known good test suites.
+ */
+public class FATSuite {}
