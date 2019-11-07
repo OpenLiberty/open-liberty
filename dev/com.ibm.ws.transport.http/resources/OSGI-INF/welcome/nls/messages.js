832bd6df5957b1b9e5fa39a3408ba3d2a0d043c4
Ellen Wyllie <ellen.wyllie@gmail.com>
2019-11-07 16:36:18 -0600

    Externalize strings, add aria labels and alt text

--- dev/com.ibm.ws.transport.http/resources/OSGI-INF/welcome/nls/messages.js
+++ dev/com.ibm.ws.transport.http/resources/OSGI-INF/welcome/nls/messages.js
@@ -1,19 +1,31 @@
 var messages = {
-    WELCOME_OPEN_LIBERTY: "Welcome to Open Liberty",
-    WELCOME_LIBERTY_SECTION: "Welcome to ",
-    WELCOME_IBM_OPEN_SOURCE: "An IBM Open Source Project",
-    RESOURCES: "Resources",
-    GROUPS_IO_FORUM: "Groups.io forum",
-    COPYRIGHT_OPEN_LIBERTY: "Licensed Materials — Property of the Open Liberty project, Copyright IBM Corp and others 1997, 2019. Project is distributed under the terms of the Eclipse Public License v1.", // COPYRIGHT_URL
-    OPEN_LIBERTY_FOOTER: "Liberty footer Logo",
     WELCOME_SECTION: "Welcome Section",
     WELCOME_SECTION_CONTENT: "Welcome Section Content",
+    WELCOME_MESSAGE: "Welcome to ",
+    WELCOME_IBM_OPEN_SOURCE: "An IBM Open Source Project",
+    WELCOME_LIGHTWEIGHT: "A lightweight open framework for building fast and efficient cloud-native Java microservices.",
+    UFO: "Ufo and beam",
+    CLOUDS: "Clouds",
+    LOGOS_SECTION: "Logos Section",
+    INTEGRATES: "Open Liberty integrates with the Open Source community",
+    MICROPROFILE: "MicroProfile Logo",
+    JAKARTA_EE: "Jakarta EE logo",
+    RESOURCES_SECTION: "Resources section",
+    RESOURCES: "Resources",
+    GITHUB: "GitHub logo",
+    TWITTER: "Twitter logo",
+    STACK_OVERFLOW: "Stack Overflow logo",
+    GROUPS_IO: "Groups.io logo",
+    OL_SUPPORT: "Open Liberty logo",
+    SUPPORT: "Support",
+    COPYRIGHT: "Copyright",
+    OPEN_LIBERTY_FOOTER: "Liberty footer Logo",
+    COPYRIGHT_OPEN_LIBERTY: "Licensed Materials — Property of the Open Liberty project, Copyright IBM Corp and others 1997, 2019. Project is distributed under the terms of the Eclipse Public License v1.", // COPYRIGHT_URL
+    UPDATE_BANNER_SECTION: "Update Banner Section",
+    UPDATE_BANNER_SECTION_CONTENT: "Update Banner Section Content",
     HEADER_UPDATE_AVAILABLE: "Update Available: ",
     HEADER_DOWNLOAD_LINK: "Click to download {0}", // Product name
     HEADER_UPDATE_AVAILABLE_DOWNLOAD_LINK: "Update Available. Click to download {0}", // Product name
-    COPYRIGHT: "Copyright",
-    CONTEXT_ROOT_NOT_FOUND: "Context Root Not Found",
-    UPDATE_BANNER_CLOSE_BUTTON: "Close Banner",
-    UPDATE_BANNER_SECTION: "Update Banner Section",
-    UPDATE_BANNER_SECTION_CONTENT: "Update Banner Section Content"
+    UPDATE_BANNER_CLOSE_BUTTON: "Close Update Banner",
+    CONTEXT_ROOT_NOT_FOUND: "Context Root Not Found"
 };
