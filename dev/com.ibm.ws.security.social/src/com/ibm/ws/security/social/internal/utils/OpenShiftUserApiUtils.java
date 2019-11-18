f1f65b42cd59442efda05cbba3c38084208d5cab
Adam Yoho <ayoho@us.ibm.com>
2019-11-18 08:07:41 -0600

    OpenShift social login serviceability work
    Resolves #9787, #9789

--- dev/com.ibm.ws.security.social/src/com/ibm/ws/security/social/internal/utils/OpenShiftUserApiUtils.java
+++ dev/com.ibm.ws.security.social/src/com/ibm/ws/security/social/internal/utils/OpenShiftUserApiUtils.java
@@ -13,17 +13,14 @@ package com.ibm.ws.security.social.internal.utils;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.json.Json;
-import javax.json.JsonObjectBuilder;
-
-import java.io.StringReader;
-
 import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.json.JsonValue;
 import javax.json.JsonValue.ValueType;
 import javax.json.stream.JsonParsingException;
@@ -58,7 +55,7 @@ public class OpenShiftUserApiUtils {
             HttpURLConnection connection = sendUserApiRequest(accessToken, sslSocketFactory);
             response = readUserApiResponse(connection);
         } catch (Exception e) {
-            throw new SocialLoginException("OPENSHIFT_ERROR_GETTING_USER_INFO", e, new Object[] { e });
+            throw new SocialLoginException("KUBERNETES_ERROR_GETTING_USER_INFO", e, new Object[] { e });
         }
         return response;
     }
@@ -90,7 +87,7 @@ public class OpenShiftUserApiUtils {
 
     String createUserApiRequestBody(@Sensitive String accessToken) throws SocialLoginException {
         if (accessToken == null) {
-            throw new SocialLoginException("OPENSHIFT_ACCESS_TOKEN_MISSING", null, null);
+            throw new SocialLoginException("KUBERNETES_ACCESS_TOKEN_MISSING", null, null);
         }
         JsonObjectBuilder bodyBuilder = Json.createObjectBuilder();
         bodyBuilder.add("kind", "TokenReview");
@@ -103,7 +100,7 @@ public class OpenShiftUserApiUtils {
         int responseCode = connection.getResponseCode();
         String response = httpUtils.readConnectionResponse(connection);
         if (responseCode != HttpServletResponse.SC_CREATED) {
-            throw new SocialLoginException("OPENSHIFT_USER_API_BAD_STATUS", null, new Object[] { responseCode, response });
+            throw new SocialLoginException("KUBERNETES_USER_API_BAD_STATUS", null, new Object[] { responseCode, response });
         }
         return modifyExistingResponseToJSON(response);
     }
@@ -111,14 +108,14 @@ public class OpenShiftUserApiUtils {
     String modifyExistingResponseToJSON(String response) throws JoseException, SocialLoginException {
 
         if (response == null || response.isEmpty()) {
-            throw new SocialLoginException("OPENSHIFT_USER_API_BAD_RESPONSE", null, null);
+            throw new SocialLoginException("KUBERNETES_USER_API_BAD_RESPONSE", null, null);
         }
 
         JsonObject jsonResponse;
         try {
             jsonResponse = Json.createReader(new StringReader(response)).readObject();
         } catch (JsonParsingException e) {
-            throw new SocialLoginException("OPENSHIFT_USER_API_BAD_RESPONSE", null, new Object[] { response, e });
+            throw new SocialLoginException("KUBERNETES_USER_API_BAD_RESPONSE", null, new Object[] { response, e });
         }
 
         //The response from the user API is not a valid JSON object. The full response is [{0}]. {1}" where insert {0} would be response and insert {1} would be e
@@ -134,32 +131,31 @@ public class OpenShiftUserApiUtils {
             }
             statusInnerMap = jsonResponse.getJsonObject("status");
         } else {
-            throw new SocialLoginException("OPENSHIFT_USER_API_RESPONSE_MISSING_KEY", null, new Object[] { "status", jsonResponse });
+            throw new SocialLoginException("KUBERNETES_USER_API_RESPONSE_MISSING_KEY", null, new Object[] { "status", jsonResponse });
         }
 
         if (statusInnerMap.containsKey("user")) {
             userInnerMap = statusInnerMap.getJsonObject("user");
-            if(userInnerMap.containsKey(config.getUserNameAttribute())){
-                modifiedResponse.add("username", userInnerMap.getString(config.getUserNameAttribute()));  
-            }
-            else {
-             throw new SocialLoginException("OPENSHIFT_USER_API_RESPONSE_MISSING_KEY", null, new Object[] { "user", jsonResponse });
+            if (userInnerMap.containsKey(config.getUserNameAttribute())) {
+                modifiedResponse.add("username", userInnerMap.getString(config.getUserNameAttribute()));
+            } else {
+                throw new SocialLoginException("KUBERNETES_USER_API_RESPONSE_MISSING_KEY", null, new Object[] { "user", jsonResponse });
             }
         } else {
-            throw new SocialLoginException("OPENSHIFT_USER_API_RESPONSE_MISSING_KEY", null, new Object[] { "user", jsonResponse });
+            throw new SocialLoginException("KUBERNETES_USER_API_RESPONSE_MISSING_KEY", null, new Object[] { "user", jsonResponse });
         }
-        
-      if (statusInnerMap.containsKey("error")) {
-          throw new SocialLoginException("OPENSHIFT_USER_API_RESPONSE_ERROR",null,new Object[] {jsonResponse});
-      }
-      
+
+        if (statusInnerMap.containsKey("error")) {
+            throw new SocialLoginException("KUBERNETES_USER_API_RESPONSE_ERROR", null, new Object[] { jsonResponse });
+        }
+
         if (userInnerMap.containsKey("groups")) {
             JsonValue groupsValue = userInnerMap.get("groups");
             if (groupsValue.getValueType() != ValueType.ARRAY) {
-                throw new SocialLoginException("OPENSHIFT_USER_API_RESPONSE_MISCONFIGURED_KEY", null, null);
+                throw new SocialLoginException("KUBERNETES_USER_API_RESPONSE_MISCONFIGURED_KEY", null, null);
             }
-                modifiedResponse.add("groups", userInnerMap.getJsonArray("groups"));  
-            
+            modifiedResponse.add("groups", userInnerMap.getJsonArray("groups"));
+
         }
         return modifiedResponse.build().toString();
     }
