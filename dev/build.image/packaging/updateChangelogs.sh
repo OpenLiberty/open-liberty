1ae7ff9f08ee28ad4ce3f24c259da76c56491b20
Benson Lam <bensonl@ca.ibm.com>
2019-11-18 08:17:03 -0800

    added error handling to changelog creation

--- dev/build.image/packaging/updateChangelogs.sh
+++ dev/build.image/packaging/updateChangelogs.sh
@@ -8,11 +8,24 @@ DEB_CHANGELOG_FILE=$(pwd)/debuild/openliberty/debian/changelog
 RPM_CHANGELOG_FILE=$(pwd)/rpmbuild/SPECS/openliberty.spec
 RPM_TEMPLATE=$(pwd)/openliberty.spec.template
 MAINTAINER="Admin <admin@openliberty.io>"
-DATE=$(date)
-DEB_TIMESTAMP=$(date -d "$DATE" +"%a, %d %b %Y %T %z")
-RPM_TIMESTAMP=$(date -d "$DATE" +"%a %b %d %Y")
+#check date command can be run successfully
+date
+RC=$?
+if [ "$RC" -eq "0" ] ; then
+    echo "using date command to generate datestamps"
+    DATE=$(date)
+    DEB_TIMESTAMP=$(date -d "$DATE" +"%a, %d %b %Y %T %z")
+    RPM_TIMESTAMP=$(date -d "$DATE" +"%a %b %d %Y")
+else
+    echo "date command executed with RC:$RC"
+    echo "falling back to manually set datestamps"
+    which date
+    ls -l /usr/bin/date
+    DEB_TIMESTAMP="Mon, 18 Nov 2019 07:46:01 -0800"
+    RPM_TIMESTAMP="Mon Nov 18 2019"
+fi
 
-echo "Generating .deb changelog"
+echo "Generating .deb changelog using datestamp:$DEB_TIMESTAMP"
 #Generate .deb changelog
 echo "openliberty (${driverVer}-1ubuntu1) stable; urgency=medium" > $DEB_CHANGELOG_FILE
 echo "" >> $DEB_CHANGELOG_FILE
@@ -20,7 +33,7 @@ echo "  * This package contains Open Liberty ${driverVer}" >> $DEB_CHANGELOG_FIL
 echo "" >> $DEB_CHANGELOG_FILE
 echo " -- ${MAINTAINER}  "${DEB_TIMESTAMP} >> $DEB_CHANGELOG_FILE
 
-echo "Generating .rpm changelog"
+echo "Generating .rpm changelog using datestamp:$RPM_TIMESTAMP"
 #generate updated RPM SPEC
 cp $RPM_TEMPLATE $RPM_CHANGELOG_FILE
 echo "" >> $RPM_CHANGELOG_FILE
