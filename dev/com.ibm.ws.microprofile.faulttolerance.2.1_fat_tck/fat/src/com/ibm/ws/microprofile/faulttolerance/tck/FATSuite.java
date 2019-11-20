fadbc2d4950a57110703eb356a05d08d962cb3b8
Joseph Cass <joseph.cass@ibm.com>
2019-11-20 11:24:02 +0000

    Added line to try and fix #build fails for LITE mode

--- dev/com.ibm.ws.microprofile.faulttolerance.2.1_fat_tck/fat/src/com/ibm/ws/microprofile/faulttolerance/tck/FATSuite.java
+++ dev/com.ibm.ws.microprofile.faulttolerance.2.1_fat_tck/fat/src/com/ibm/ws/microprofile/faulttolerance/tck/FATSuite.java
@@ -14,7 +14,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import componenttest.custom.junit.runner.AlwaysPassesTest;
+
 @RunWith(Suite.class)
-@SuiteClasses({ FaultToleranceTck21Launcher.class })
+@SuiteClasses({
+                AlwaysPassesTest.class, //LITE
+                FaultToleranceTck21Launcher.class //EXPERIMENTAL
+})
+
 public class FATSuite {
+
 }
