c57e84270b93b392df1a18680ea57d60f201bb82
Tom Evans <tevans@uk.ibm.com>
2019-11-22 14:53:14 +0000

    Create microProfile-3.3 feature

--- dev/com.ibm.ws.microprofile.1.0_fat/fat/src/com/ibm/ws/microprofile10/fat/suite/SimpleJAXRSCDITest.java
+++ dev/com.ibm.ws.microprofile.1.0_fat/fat/src/com/ibm/ws/microprofile10/fat/suite/SimpleJAXRSCDITest.java
@@ -69,8 +69,8 @@ public class SimpleJAXRSCDITest {
         }
     }
 
-    private static final String[] MP_VERSIONS = { "1.0", "1.2", "1.3", "1.4", "2.0", "2.1", "2.2", "3.0", "3.2" };
-    private static final String LITE_MODE = "3.2";
+    private static final String[] MP_VERSIONS = { "1.0", "1.2", "1.3", "1.4", "2.0", "2.1", "2.2", "3.0", "3.2", "3.3" };
+    private static final String LITE_MODE = "3.3";
 
     @ClassRule
     public static RepeatTests r = getMPRepeat();
