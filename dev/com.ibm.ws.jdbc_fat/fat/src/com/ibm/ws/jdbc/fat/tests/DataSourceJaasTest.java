0a7264e291e1de8aad5fc8cb34209558763f2234
Greg Watts <gwatts@us.ibm.com>
2019-11-26 15:13:02 -0600

    Initial commit

---
+++ dev/com.ibm.ws.jdbc_fat/fat/src/com/ibm/ws/jdbc/fat/tests/DataSourceJaasTest.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.jdbc.fat.tests;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import componenttest.annotation.Server;
+import componenttest.topology.impl.LibertyServer;
+import componenttest.topology.utils.FATServletClient;
+
+public class DataSourceJaasTest extends FATServletClient {
+
+    @Server("com.ibm.ws.jdbc.jaas.fat")
+    public static LibertyServer server;
+
+    private static final String basicfat = "basicfat";
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        server.configureForAnyDatabase();
+        server.addInstalledAppForValidation("jdbcapp");
+        server.startServer();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        server.stopServer("DSRA9543W"); //Expected since we're using a GSS Credential for authentication with Derby.
+    }
+
+    @Test
+    public void testDataSourceMappingConfigAlias() throws Exception {
+        runTest(server, basicfat + '/', testName);
+    }
+
+    @Test
+    public void testDataSourceCustomLoginConfiguration() throws Exception {
+        runTest(server, basicfat + '/', testName);
+    }
+
+    @Test
+    public void testJAASLoginWithGSSCredential() throws Exception {
+        runTest(server, basicfat + '/', testName);
+    }
+}
