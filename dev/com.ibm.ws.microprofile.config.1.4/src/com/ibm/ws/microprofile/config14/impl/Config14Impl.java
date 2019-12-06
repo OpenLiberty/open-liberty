91898d82d9ca03f88f7333dc3a6d8088b37e30d6
Tom Evans <tevans@uk.ibm.com>
2019-12-06 16:07:35 +0000

    Strip out async update and caching

--- dev/com.ibm.ws.microprofile.config.1.4/src/com/ibm/ws/microprofile/config14/impl/Config14Impl.java
+++ dev/com.ibm.ws.microprofile.config.1.4/src/com/ibm/ws/microprofile/config14/impl/Config14Impl.java
@@ -11,25 +11,109 @@
 package com.ibm.ws.microprofile.config14.impl;
 
 import java.lang.reflect.Type;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.concurrent.ScheduledExecutorService;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.config.spi.ConfigSource;
 
-import com.ibm.ws.microprofile.config.impl.ConfigImpl;
+import com.ibm.websphere.ras.annotation.Trivial;
+import com.ibm.ws.microprofile.config.impl.AbstractConfig;
 import com.ibm.ws.microprofile.config.impl.ConversionManager;
 import com.ibm.ws.microprofile.config.impl.SortedSources;
+import com.ibm.ws.microprofile.config.impl.SourcedValueImpl;
+import com.ibm.ws.microprofile.config.interfaces.SourcedValue;
+import com.ibm.ws.microprofile.config.interfaces.WebSphereConfig;
 
-public class Config14Impl extends ConfigImpl {
+public class Config14Impl extends AbstractConfig implements WebSphereConfig {
+
+    private final SortedSources sources;
+    private final ConversionManager conversionManager;
 
     /**
+     * The sources passed in should have already been wrapped up as an unmodifiable copy
+     *
      * @param conversionManager
      * @param sources
      * @param executor
      * @param refreshInterval
      */
     public Config14Impl(ConversionManager conversionManager, SortedSources sources, ScheduledExecutorService executor, long refreshInterval) {
-        super(conversionManager, sources, executor, refreshInterval);
+        super(conversionManager, sources);
+        this.sources = sources;
+        this.conversionManager = conversionManager;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public SourcedValue getSourcedValue(String propertyName, Type propertyType) {
+        SourcedValue sourcedValue = null;
+        SourcedValue rawProp = getRawCompositeValue(propertyName);
+        if (rawProp != null) {
+            Object value = this.conversionManager.convert((String) rawProp.getValue(), propertyType);
+            sourcedValue = new SourcedValueImpl(propertyName, value, propertyType, rawProp.getSource());
+        }
+        return sourcedValue;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Set<String> getKeySet() {
+        HashSet<String> result = new HashSet<>();
+
+        for (ConfigSource source : sources) {
+            Set<String> names = source.getPropertyNames();
+            result.addAll(names);
+        }
+
+        return result;
+    }
+
+    /**
+     * @param key
+     * @return
+     */
+    @Trivial
+    private SourcedValue getRawCompositeValue(String key) {
+        SourcedValue raw = null;
+        for (ConfigSource source : sources) {
+            String value = source.getValue(key);
+            if (value != null || source.getPropertyNames().contains(key)) {
+                String sourceID = source.getName();
+                raw = new SourcedValueImpl(key, value, String.class, sourceID);
+                break;
+            }
+        }
+        return raw;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @Trivial
+    public String dump() {
+        StringBuilder sb = new StringBuilder();
+        Set<String> keys = getKeySet();
+        keys = new TreeSet<String>(keys);
+        Iterator<String> keyItr = keys.iterator();
+        while (keyItr.hasNext()) {
+            String key = keyItr.next();
+            SourcedValue rawCompositeValue = getRawCompositeValue(key);
+            if (rawCompositeValue == null) {
+                sb.append("null");
+            } else {
+                sb.append(rawCompositeValue);
+            }
+            if (keyItr.hasNext()) {
+                sb.append("\n");
+            }
+        }
+
+        return sb.toString();
+
     }
 
     /**
