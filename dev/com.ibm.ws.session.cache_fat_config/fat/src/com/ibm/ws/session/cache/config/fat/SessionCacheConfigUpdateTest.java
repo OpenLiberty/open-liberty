94fa5ff589b2b07672c407270328fcb9593fdc65
Greg Watts <gwatts@us.ibm.com>
2019-12-06 16:35:32 -0600

    Missed code

--- dev/com.ibm.ws.session.cache_fat_config/fat/src/com/ibm/ws/session/cache/config/fat/SessionCacheConfigUpdateTest.java
+++ dev/com.ibm.ws.session.cache_fat_config/fat/src/com/ibm/ws/session/cache/config/fat/SessionCacheConfigUpdateTest.java
@@ -68,6 +68,14 @@ public class SessionCacheConfigUpdateTest extends FATServletClient {
         server.updateServerConfiguration(savedConfig);
         server.waitForConfigUpdateInLogUsingMark(APP_NAMES, cleanupList);
         cleanupList = EMPTY_RECYCLE_LIST;
+
+        // In addition to starting the application, must also wait for asynchronous web module initialization to complete,
+        // otherwise tests which attempt a configuration update could end up triggering a deactivate and close of the CachingProvider
+        // while the servlet initialization code is still attempting to use the CachingProvider and/or the CacheManager and Caches that it creates.
+        List<String> session = new ArrayList<>();
+        run("getSessionId", session);
+        run("invalidateSession", session);
+
         System.out.println("server configuration restored");
     }
 
