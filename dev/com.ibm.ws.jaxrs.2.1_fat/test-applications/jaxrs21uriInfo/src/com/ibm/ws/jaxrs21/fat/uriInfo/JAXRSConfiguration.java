d2d7fab697d6b768e5bb2cdf1b98641a153e8979
John Koehler <jkoehler@us.ibm.com>
2019-12-20 13:27:59 -0600

    Issue #10253: Add Reactive client to the UriInfoTest

---
+++ dev/com.ibm.ws.jaxrs.2.1_fat/test-applications/jaxrs21uriInfo/src/com/ibm/ws/jaxrs21/fat/uriInfo/JAXRSConfiguration.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.jaxrs21.fat.uriInfo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+    // nothing to configure
+
+}
