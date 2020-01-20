37d9565df63919fa5b063936fb7f8088b16553ba
ilewis <idlewis@users.noreply.github.com>
2020-01-20 16:54:03 +0000

    Turn fault tolerance publish dir into lra publish dir
    sort of, hopefully. Probably needs more work

--- dev/com.ibm.ws.lra_fat_tck/publish/tckRunner/tck/src/test/java/com/ibm/ws/lra/test/ArquillianLoadableExtension.java
+++ dev/com.ibm.ws.lra_fat_tck/publish/tckRunner/tck/src/test/java/com/ibm/ws/lra/test/ArquillianLoadableExtension.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-package com.ibm.ws.microprofile.faulttolerance.test;
+package com.ibm.ws.lra.test;
 
 import java.util.EnumSet;
 import java.util.Set;
