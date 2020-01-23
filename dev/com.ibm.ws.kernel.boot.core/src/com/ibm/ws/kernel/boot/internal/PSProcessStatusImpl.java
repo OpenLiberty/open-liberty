55294b4463fce8c1aa47258eb6a4350a0421180e
Brent Daniel <brenthdaniel@gmail.com>
2020-01-23 12:37:36 -0800

    Exclude defunct processes from running check

--- dev/com.ibm.ws.kernel.boot.core/src/com/ibm/ws/kernel/boot/internal/PSProcessStatusImpl.java
+++ dev/com.ibm.ws.kernel.boot.core/src/com/ibm/ws/kernel/boot/internal/PSProcessStatusImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 IBM Corporation and others.
+ * Copyright (c) 2013, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -62,13 +62,16 @@ public class PSProcessStatusImpl implements ProcessStatus {
             Process p = pb.start();
             in = p.getInputStream();
 
+            boolean defunct = false;
             reader = new BufferedReader(new InputStreamReader(in));
             for (String line; (line = reader.readLine()) != null;) {
                 Debug.println(line);
+                if (line.contains("<defunct>"))
+                    defunct = true;
             }
 
             p.waitFor();
-            if (p.exitValue() == 0)
+            if (p.exitValue() == 0 && !defunct)
                 return State.YES;
             else
                 return State.NO;
