88e316c32ea361ef32fa70e3483c2c805a61a9fd
Tayyab Waqar <r44zor@gmail.com>
2020-02-04 14:04:31 -0500

    add logic to view license

--- dev/com.ibm.ws.install.featureUtility/src/com/ibm/ws/install/featureUtility/licensing/LicenseUpgradeUtility.java
+++ dev/com.ibm.ws.install.featureUtility/src/com/ibm/ws/install/featureUtility/licensing/LicenseUpgradeUtility.java
@@ -1,15 +1,28 @@
 package com.ibm.ws.install.featureUtility.licensing;
 
+import com.ibm.ws.install.InstallException;
+import com.ibm.ws.install.InstallKernelFactory;
+import com.ibm.ws.install.InstallKernelInteractive;
+import com.ibm.ws.install.InstallLicense;
 import com.ibm.ws.install.internal.InstallUtils;
 import com.ibm.ws.kernel.boot.cmdline.Utils;
+import com.ibm.ws.kernel.productinfo.DuplicateProductInfoException;
+import com.ibm.ws.kernel.productinfo.ProductInfoParseException;
+import com.ibm.ws.kernel.productinfo.ProductInfoReplaceException;
+import wlp.lib.extract.SelfExtract;
 
 import java.io.*;
-import java.util.Properties;
+import java.util.*;
 
 public class LicenseUpgradeUtility {
 
     private String productId, productOwner, productVersion, productName, productReplaces, productInstallType, productEdition, productLicenseType;
     private String wlpDir;
+    private List<String> features;
+    private boolean acceptLicense;
+    Set<InstallLicense> featureLicenses = Collections.emptySet();
+    static final private String UNSPECIFIED_LICENSE_TYPE = "UNSPECIFIED";
+
 
     final String WEBSPHERE_PROPERTIES_FILE_DIR = "/lib/versions/";
     final String WEBSPHERE_PROPERTIES_FILE_NAME = "WebsphereApplicationServer.properties";
@@ -25,7 +38,7 @@ public class LicenseUpgradeUtility {
         this.productLicenseType = builder.productLicenseType;
 
         this.wlpDir = builder.wlpDir != null ? builder.wlpDir : Utils.getInstallDir().getAbsolutePath();
-
+        this.features = builder.features;
     }
 
     /**
@@ -54,11 +67,124 @@ public class LicenseUpgradeUtility {
         }
     }
 
+    public boolean handleLicenses() {
+        if(acceptLicense) {
+            return true;
+        }
+
+        InstallKernelInteractive installKernel = InstallKernelFactory.getInteractiveInstance();
+        try {
+            installKernel.resolve(this.features, false);
+            featureLicenses = installKernel.getFeatureLicense(Locale.getDefault());
+            Set<InstallLicense> licenseToAccept = InstallUtils.getLicenseToAccept(featureLicenses);
+            if (!acceptLicense) {
+                if (!licenseToAccept.isEmpty()) {
+                    showFeaturesForLicenseAcceptance(licenseToAccept);
+                }
+                for (InstallLicense license : licenseToAccept) {
+                    if (!!!handleLicenseAcceptance(license)) {
+                        return false;
+                    }
+                }
+            }
+        } catch (ProductInfoParseException e) {
+            System.out.println(e.getMessage());
+            return false;
+        } catch (DuplicateProductInfoException e) {
+            System.out.println(e.getMessage());
+            return false;
+        } catch (ProductInfoReplaceException e) {
+            System.out.println(e.getMessage());
+            return false;
+        } catch (InstallException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean handleLicenseAcceptance(InstallLicense licenseToAccept) {
+        //
+        // Display license requirement
+        //
+        SelfExtract.wordWrappedOut(SelfExtract.format("licenseStatement", new Object[] { licenseToAccept.getProgramName(), licenseToAccept.getName() }));
+        System.out.println();
+
+        if (!obtainLicenseAgreement(licenseToAccept)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean obtainLicenseAgreement(InstallLicense license) {
+        // Prompt for word-wrapped display of license agreement & information
+        boolean view;
+
+        if (featureLicenses.size() > 1) {
+            SelfExtract.wordWrappedOut(SelfExtract.format("showAgreement", "--viewLicenseAgreement"));
+            view = SelfExtract.getResponse(SelfExtract.format("promptAgreement"), "", "xX");
+            if (view) {
+                System.out.println(license.getAgreement());
+                System.out.println();
+            }
+
+            SelfExtract.wordWrappedOut(SelfExtract.format("showInformation", "--viewLicenseInfo"));
+            view = SelfExtract.getResponse(SelfExtract.format("promptInfo"), "", "xX");
+            if (view) {
+                System.out.println(license.getInformation());
+                System.out.println();
+            }
+
+        } else {
+            System.out.println();
+            System.out.println(license.getAgreement());
+            System.out.println();
+        }
+
+        System.out.println();
+        SelfExtract.wordWrappedOut(SelfExtract.format("licenseOptionDescription"));
+        System.out.println();
+
+        boolean accept = SelfExtract.getResponse(SelfExtract.format("licensePrompt", new Object[] { "[1]", "[2]" }),
+                "1", "2");
+        System.out.println();
+        return accept;
+    }
+
+    private void showFeaturesForLicenseAcceptance(Set<InstallLicense> licenseToAccept) {
+        Set<String> featuresToAccept = new HashSet<String>();
+        StringBuffer buf = new StringBuffer();
+        for (InstallLicense license : licenseToAccept) {
+            String featureToMatch = getFeaturesForLicense(license);
+            if (!featureToMatch.isEmpty()) {
+                featuresToAccept.add(featureToMatch);
+                buf.append(featureToMatch);
+            }
+        }
+        String featuresToAcceptMsg = buf.toString().trim();
+//        logger.info("Accept license for: " + featuresToAcceptMsg);
+    }
+
+    private String getFeaturesForLicense(InstallLicense license) {
+        StringBuffer buf = new StringBuffer();
+        for (String feature : license.getFeatures()) {
+            for (String featureToMatch : features) {
+                if (feature.contains(featureToMatch)) {
+                    buf.append(featureToMatch + " ");
+                }
+            }
+        }
+        String matchedFeatures = buf.toString();
+        return matchedFeatures;
+    }
+
 
 
     public static class LicenseUpgradeUtilityBuilder{
         String productId, productOwner, productVersion, productName, productReplaces, productInstallType, productEdition, productLicenseType;
         String wlpDir;
+        List<String> features;
 
         public LicenseUpgradeUtilityBuilder setProductId(String productId){
             this.productId = productId;
@@ -103,6 +229,11 @@ public class LicenseUpgradeUtility {
         public LicenseUpgradeUtilityBuilder setWlpDir(String wlpDir){
             this.wlpDir = wlpDir;
             return this;
+        }
+
+        public LicenseUpgradeUtilityBuilder setFeatures(List<String> features){
+            this.features = features;
+            return  this;
         }
 
         public LicenseUpgradeUtility build(){
