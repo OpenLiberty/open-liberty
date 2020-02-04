ec9b046c8b8a7a2f2644d7897b1ca314a782167a
FlynnIBM <40773993+FlynnIBM@users.noreply.github.com>
2020-02-04 11:09:48 -0500

    Update HttpDispatcherLink.java

--- dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/dispatcher/internal/channel/HttpDispatcherLink.java
+++ dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/dispatcher/internal/channel/HttpDispatcherLink.java
@@ -1030,6 +1030,80 @@ public class HttpDispatcherLink extends InboundApplicationLink implements HttpIn
         close(getVirtualConnection(), error);
     }
 
+     /**
+     * Searches the passed in String for any characters that could be
+     * used in a cross site scripting attack (<, >, +, &, ", ', (, ), %, ;)
+     * and converts them to their browser equivalent name or code specification.
+     *
+     * This method should stay in sync with webcontainer ResponseUtils.encodeDataString()
+     *
+     * @param iString contains the String to be encoded
+     *
+     * @return an encoded String
+     */
+    private static String encodeDataString(String iString) {
+        if (iString == null)
+            return "";
+
+        int strLen = iString.length(), i;
+
+        if (strLen < 1)
+            return iString;
+
+        // convert any special chars to their browser equivalent specification
+        StringBuffer retString = new StringBuffer(strLen * 2);
+
+        for (i = 0; i < strLen; i++) {
+            switch (iString.charAt(i)) {
+                case '<':
+                    retString.append("&lt;");
+                    break;
+
+                case '>':
+                    retString.append("&gt;");
+                    break;
+
+                case '&':
+                    retString.append("&amp;");
+                    break;
+
+                case '\"':
+                    retString.append("&quot;");
+                    break;
+
+                case '+':
+                    retString.append("&#43;");
+                    break;
+
+                case '(':
+                    retString.append("&#40;");
+                    break;
+
+                case ')':
+                    retString.append("&#41;");
+                    break;
+
+                case '\'':
+                    retString.append("&#39;");
+                    break;
+
+                case '%':
+                    retString.append("&#37;");
+                    break;
+
+                case ';':
+                    retString.append("&#59;");
+                    break;
+
+                default:
+                    retString.append(iString.charAt(i));
+                    break;
+            }
+        }
+
+        return retString.toString();
+    }
+    
     private Exception closeStreams() { // This is seperated for Upgrade Servlet3.1 WebConnection
         final HttpRequestImpl finalRequest = this.request;
         final HttpResponseImpl finalResponse = this.response;
