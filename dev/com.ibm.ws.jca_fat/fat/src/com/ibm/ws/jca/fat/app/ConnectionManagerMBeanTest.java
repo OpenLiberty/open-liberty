eb86e604d5047e9dddfa798a80cacacd52a025be
Nathan Rauh <nathan.rauh@us.ibm.com>
2020-02-05 09:08:27 -0600

    Issue #10735 ensure temporary JAR is created from all test classes that use the server config

--- dev/com.ibm.ws.jca_fat/fat/src/com/ibm/ws/jca/fat/app/ConnectionManagerMBeanTest.java
+++ dev/com.ibm.ws.jca_fat/fat/src/com/ibm/ws/jca/fat/app/ConnectionManagerMBeanTest.java
@@ -137,6 +137,12 @@ public class ConnectionManagerMBeanTest {
         ShrinkHelper.addDirectory(fvtapp_ear, "lib/LibertyFATTestFiles/fvtapp");
         ShrinkHelper.exportToServer(server, "apps", fvtapp_ear);
 
+        // TODO remove this temporary jar when login modules can be accessed from the resource adapter
+        JavaArchive jar = ShrinkWrap.create(JavaArchive.class, "tempLoginModule.jar");
+        jar.addPackage("fat.jca.resourceadapter.jar1");
+        jar.addPackage("fat.jca.resourceadapter.jar2");
+        ShrinkHelper.exportToServer(server, "/", jar);
+
         originalServerConfig = server.getServerConfiguration().clone();
         server.addInstalledAppForValidation(fvtapp);
         server.startServer();
