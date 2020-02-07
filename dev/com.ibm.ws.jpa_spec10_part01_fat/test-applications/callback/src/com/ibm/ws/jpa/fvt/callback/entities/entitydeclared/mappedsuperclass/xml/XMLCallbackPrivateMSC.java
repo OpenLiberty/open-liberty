716dbd0405a86d26d983d9bfb4768c473505de38
Will Dazey <dazeydev.3@gmail.com>
2020-02-07 16:03:37 -0600

    Update jpa_spec10_part01_fat
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

---
+++ dev/com.ibm.ws.jpa_spec10_part01_fat/test-applications/callback/src/com/ibm/ws/jpa/fvt/callback/entities/entitydeclared/mappedsuperclass/xml/XMLCallbackPrivateMSC.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2020 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package com.ibm.ws.jpa.fvt.callback.entities.entitydeclared.mappedsuperclass.xml;
+
+import com.ibm.ws.jpa.fvt.callback.entities.AbstractCallbackEntity;
+
+public abstract class XMLCallbackPrivateMSC extends AbstractCallbackEntity {
+    @SuppressWarnings("unused")
+    private void prePersistCallback() {
+        doPrePersist(ProtectionType.PT_PRIVATE);
+    }
+
+    @SuppressWarnings("unused")
+    private void postPersistCallback() {
+        doPostPersist(ProtectionType.PT_PRIVATE);
+    }
+
+    @SuppressWarnings("unused")
+    private void preUpdateCallback() {
+        doPreUpdate(ProtectionType.PT_PRIVATE);
+    }
+
+    @SuppressWarnings("unused")
+    private void postUpdateCallback() {
+        doPostUpdate(ProtectionType.PT_PRIVATE);
+    }
+
+    @SuppressWarnings("unused")
+    private void preRemoveCallback() {
+        doPreRemove(ProtectionType.PT_PRIVATE);
+    }
+
+    @SuppressWarnings("unused")
+    private void postRemoveCallback() {
+        doPostRemove(ProtectionType.PT_PRIVATE);
+    }
+
+    @SuppressWarnings("unused")
+    private void postLoadCallback() {
+        doPostLoad(ProtectionType.PT_PRIVATE);
+    }
+}
---
+++ dev/com.ibm.ws.jpa_spec10_part01_fat/test-applications/callback/src/com/ibm/ws/jpa/fvt/callback/entities/orderofinvocation/ano/AnoOOIProtectedMSC.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2020 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package com.ibm.ws.jpa.fvt.callback.entities.orderofinvocation.ano;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PostLoad;
+import javax.persistence.PostPersist;
+import javax.persistence.PostRemove;
+import javax.persistence.PostUpdate;
+import javax.persistence.PrePersist;
+import javax.persistence.PreRemove;
+import javax.persistence.PreUpdate;
+
+@MappedSuperclass
+@EntityListeners({
+                   com.ibm.ws.jpa.fvt.callback.listeners.orderofinvocation.b1.AnoCallbackListenerProtectedB1.class,
+                   com.ibm.ws.jpa.fvt.callback.listeners.orderofinvocation.b2.AnoCallbackListenerProtectedB2.class })
+public class AnoOOIProtectedMSC extends AnoOOIRootProtectedEntity {
+    public AnoOOIProtectedMSC() {
+        super();
+    }
+
+    @PrePersist
+    protected void entityBPrePersist() {
+        doPrePersist(ProtectionType.PT_PROTECTED);
+    }
+
+    @PostPersist
+    protected void entityBPostPersist() {
+        doPostPersist(ProtectionType.PT_PROTECTED);
+    }
+
+    @PreUpdate
+    protected void entityBPreUpdate() {
+        doPreUpdate(ProtectionType.PT_PROTECTED);
+    }
+
+    @PostUpdate
+    protected void entityBPostUpdate() {
+        doPostUpdate(ProtectionType.PT_PROTECTED);
+    }
+
+    @PreRemove
+    protected void entityBPreRemove() {
+        doPreRemove(ProtectionType.PT_PROTECTED);
+    }
+
+    @PostRemove
+    protected void entityBPostRemove() {
+        doPostRemove(ProtectionType.PT_PROTECTED);
+    }
+
+    @PostLoad
+    protected void entityBPostLoad() {
+        doPostLoad(ProtectionType.PT_PROTECTED);
+    }
+
+    @Override
+    public String toString() {
+        return "AnoOOIMSCProtectedEntity [id=" + getId() + ", name=" + getName() + "]";
+    }
+}
