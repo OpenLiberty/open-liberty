04c15935cfeb4ff0bbf317b2db999da0b7f45694
Paul Nicolucci <pnicolucci@gmail.com>
2020-02-10 13:25:41 -0500

    Issue #10820: Add ability to set a Cookie Attributes in RequestState

--- dev/com.ibm.ws.webcontainer/src/com/ibm/wsspi/webcontainer/WebContainerRequestState.java
+++ dev/com.ibm.ws.webcontainer/src/com/ibm/wsspi/webcontainer/WebContainerRequestState.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1997, 2006 IBM Corporation and others.
+ * Copyright (c) 1997, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -29,22 +29,24 @@ import com.ibm.wsspi.webcontainer.servlet.IExtendedResponse;
 */
 @SuppressWarnings("unchecked")
 public class WebContainerRequestState {
-	static protected Logger logger= Logger.getLogger("com.ibm.wsspi.webcontainer");
-	private static final String CLASS_NAME="com.ibm.wsspi.webcontainer.WebContainerRequestState";
-	private Map _attributes = null;
-	private boolean _invokedFilters;
-	private boolean completed;
-	private boolean ardRequest;
+    static protected Logger logger= Logger.getLogger("com.ibm.wsspi.webcontainer");
+    private static final String CLASS_NAME="com.ibm.wsspi.webcontainer.WebContainerRequestState";
+    private Map _attributes = null;
+    private boolean _invokedFilters;
+    private boolean completed;
+    private boolean ardRequest;
     private boolean startAsync;
     private IExtendedRequest currentThreadsIExtendedRequest;
     private IExtendedResponse currentThreadsIExtendedResponse;
+    private Map<String,String> cookieAttributes = null;
+
     public IExtendedResponse getCurrentThreadsIExtendedResponse() {
-		return currentThreadsIExtendedResponse;
-	}
+        return currentThreadsIExtendedResponse;
+    }
 
-	private AsyncContext asyncContext;
+    private AsyncContext asyncContext;
 
-	public void setCurrentThreadsIExtendedRequest(IExtendedRequest currentThreadsIExtendedRequest) {
+    public void setCurrentThreadsIExtendedRequest(IExtendedRequest currentThreadsIExtendedRequest) {
         this.currentThreadsIExtendedRequest = currentThreadsIExtendedRequest;
     }
 
@@ -67,44 +69,53 @@ public class WebContainerRequestState {
              logger.logp(Level.FINEST,CLASS_NAME,"getInstance","created->"+create+ ", request state->"+tempState);        //PM50111
          }
          return (tempState);
-   }
+    }
 
-	public static WebContainerRequestState createInstance() {
-		WebContainerRequestState newState = new WebContainerRequestState();
-		instance.set(newState);
+    public static WebContainerRequestState createInstance() {
+        WebContainerRequestState newState = new WebContainerRequestState();
+        instance.set(newState);
+
         if (com.ibm.ejs.ras.TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINEST) ){
             logger.logp(Level.FINEST,CLASS_NAME,"createInstance","created requestState -->"+ newState.toString());        
         }
-		return newState;
-	}
-	
-	public void init(){
-		if (com.ibm.ejs.ras.TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINEST) ){
-			logger.logp(Level.FINEST,CLASS_NAME,"init","init for request State");        //PM50111 
-		}
-		if (_attributes!=null)
-			_attributes.clear();
-		_invokedFilters=false;
-		completed=false;
-		ardRequest = false;
-		this.currentThreadsIExtendedRequest = null;
-	    startAsync=false;
-	    this.asyncContext = null;
-	}
 
-	public void setAttribute(String string, Object obj) {
-		if (_attributes==null)
-    		_attributes=new HashMap();
-		_attributes.put(string, obj);
+        return newState;
+    }
+	
+    public void init(){
+        if (com.ibm.ejs.ras.TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINEST) ){
+            logger.logp(Level.FINEST,CLASS_NAME,"init","init for request State");        //PM50111 
 	}
+        if (_attributes!=null) {
+            _attributes.clear();
+        }
+        if (cookieAttributes!=null) {
+            cookieAttributes.clear();
+        }
+        _invokedFilters=false;
+        completed=false;
+        ardRequest = false;
+        this.currentThreadsIExtendedRequest = null;
+        startAsync=false;
+        this.asyncContext = null;
+    }
+
+    public void setAttribute(String string, Object obj) {
+        if (_attributes==null) {
+            _attributes=new HashMap();
+        }
+        
+        _attributes.put(string, obj);
+    }
 	
     /* (non-Javadoc)
      * @see javax.servlet.ServletRequest#getAttribute(java.lang.String)
      */
     public Object getAttribute(String arg0) {
         //321485
-    	if (_attributes==null)
+    	if (_attributes==null) {
     		return null;
+    	}
         if (com.ibm.ejs.ras.TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {  //306998.15
             logger.logp(Level.FINE, CLASS_NAME,"getAttribute", " name --> " + arg0);
         }
@@ -125,48 +136,47 @@ public class WebContainerRequestState {
         Object oldValue=_attributes.remove(arg0);
     }
 
-	public boolean isInvokedFilters() {
-		if (com.ibm.ejs.ras.TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {
-            logger.logp(Level.FINE, CLASS_NAME,"isInvokedFilters", " _invokedFilters --> " + _invokedFilters);
-        }
-		return _invokedFilters;
+    public boolean isInvokedFilters() {
+	if (com.ibm.ejs.ras.TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {
+	    logger.logp(Level.FINE, CLASS_NAME,"isInvokedFilters", " _invokedFilters --> " + _invokedFilters);
 	}
+	return _invokedFilters;
+    }
 
-	public void setInvokedFilters(boolean filters) {
-		_invokedFilters = filters;
-		if (com.ibm.ejs.ras.TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {
+    public void setInvokedFilters(boolean filters) {
+	_invokedFilters = filters;
+	if (com.ibm.ejs.ras.TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {
             logger.logp(Level.FINE, CLASS_NAME,"setInvokedFilters", " invokedFilters --> " + _invokedFilters);
-        }
-	}
+       }
+    }
 
-	public boolean isCompleted() {
-		if (com.ibm.ejs.ras.TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {
+    public boolean isCompleted() {
+	if (com.ibm.ejs.ras.TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {
             logger.logp(Level.FINE, CLASS_NAME,"isCompleted", " completed --> " + completed);
-        }
-		return completed;
 	}
+	return completed;
+    }
 	
-	public void setCompleted(boolean completed){
-		this.completed = completed;
-		if (com.ibm.ejs.ras.TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {
+    public void setCompleted(boolean completed){
+	this.completed = completed;
+	if (com.ibm.ejs.ras.TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {
             logger.logp(Level.FINE, CLASS_NAME,"setCompleted", " completed --> " + completed);
-        }
 	}
+    }
 	
-	public boolean isArdRequest(){
-		return ardRequest;
-	}
+    public boolean isArdRequest(){
+        return ardRequest;
+    }
 	
-	public void setArdRequest(boolean ardRequest){
-		this.ardRequest = ardRequest;
-	}
+    public void setArdRequest(boolean ardRequest){
+	this.ardRequest = ardRequest;
+}
 
     public void setAsyncMode(boolean b) {
        this.startAsync = b;
     }
 
     public IExtendedRequest getCurrentThreadsIExtendedRequest() {
-        // TODO Auto-generated method stub
         return this.currentThreadsIExtendedRequest;
     }
 
@@ -178,11 +188,85 @@ public class WebContainerRequestState {
         this.asyncContext = asyncContext2;
     }
 
-	public void setCurrentThreadsIExtendedResponse(IExtendedResponse hres) {
-		this.currentThreadsIExtendedResponse = hres;
-	}
-
-
+    public void setCurrentThreadsIExtendedResponse(IExtendedResponse hres) {
+        this.currentThreadsIExtendedResponse = hres;
+    }
     
-	
+    /**
+     * Allows a Cookie attribute to be set on a particular Cookie specified by the
+     * cookieName parameter.
+     *
+     * Once the Cookie has been added to the Response the removeCookieAttribute
+     * method is called for the same cookieName.
+     *
+     * Currently the only Cookie attribute that is supported by the runtime here
+     * is the SameSite Cookie attribute.
+     *
+     * The cookieAttribute should be in the form: attributeName=attributeValue.
+     *
+     * @param cookieName - The Cookie name to add the attribute to.
+     * @param cookieAttribute - The Cookie attribute to be added in  the form: attributeName = attributeValue
+     */
+    public void setCookieAttribute(String cookieName, String cookieAttribute) {
+        String methodName = "setCookieAttribute";
+
+        if (com.ibm.ejs.ras.TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {
+            logger.logp(Level.FINE, CLASS_NAME, methodName, " cookieName --> " + cookieName + " cookieAttribute --> " + cookieAttribute);
+        }
+
+        if (cookieAttributes == null) {
+            cookieAttributes = new HashMap<String,String>();
+        }
+
+        cookieAttributes.put(cookieName, cookieAttribute);
+    }
+    
+    /**
+     * Return the Cookie attribute associated with the provided cookieName.
+     *
+     * @param cookieName - The name of the Cookie the attribute was set for.
+     * @return - The Cookie attribute associated with the specified Cookie name.
+     */
+    public String getCookieAttribute(String cookieName) {
+        String methodName = "getCookieAttribute";
+
+        if (cookieAttributes == null) {
+                return null;
+        }
+
+        if (com.ibm.ejs.ras.TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {
+            logger.logp(Level.FINE, CLASS_NAME, methodName, " cookieName --> " + cookieName);
+        }
+
+        String cookieAttribute = cookieAttributes.get(cookieName);
+
+        if (com.ibm.ejs.ras.TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {
+            logger.logp(Level.FINE, CLASS_NAME, methodName, " cookieAttribute --> " + cookieAttribute);
+        }
+
+        return cookieAttribute;
+    } 
+    
+    /**
+     * Removes the Cookie attribute associated with the cookieName.
+     *
+     * @param cookieName - The name of the Cookie the attribute was set for.
+     */
+    public void removeCookieAttribute(String cookieName) {
+        String methodName = "removeCookieAttribute";
+
+        if (cookieAttributes == null) {
+                return;
+        }
+
+        if (com.ibm.ejs.ras.TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {
+            logger.logp(Level.FINE, CLASS_NAME, methodName, " cookieName --> " + cookieName);
+        }
+
+        String removedAttribute = cookieAttributes.remove(cookieName);
+
+        if (com.ibm.ejs.ras.TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {
+            logger.logp(Level.FINE, CLASS_NAME, methodName, "removedAttribute --> " + removedAttribute);
+        }
+    }
 }
