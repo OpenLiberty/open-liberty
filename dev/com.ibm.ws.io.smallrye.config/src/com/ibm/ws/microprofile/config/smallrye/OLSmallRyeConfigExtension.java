08ea1a5fab3dae058ee1b283c8e8eaa05e01d21e
tevans <tevans@uk.ibm.com>
2020-02-13 16:29:22 +0000

    Create prototype MP Config 2.0 based on SmallRye

---
+++ dev/com.ibm.ws.io.smallrye.config/src/com/ibm/ws/microprofile/config/smallrye/OLSmallRyeConfigExtension.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2020 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.microprofile.config.smallrye;
+
+import javax.enterprise.inject.spi.Extension;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+
+import com.ibm.ws.cdi.extension.WebSphereCDIExtension;
+
+import io.smallrye.config.inject.ConfigExtension;
+
+@Component(service = WebSphereCDIExtension.class, configurationPolicy = ConfigurationPolicy.IGNORE, property = { "service.vendor=IBM" }, immediate = true)
+public class OLSmallRyeConfigExtension extends ConfigExtension implements Extension, WebSphereCDIExtension {
+}
