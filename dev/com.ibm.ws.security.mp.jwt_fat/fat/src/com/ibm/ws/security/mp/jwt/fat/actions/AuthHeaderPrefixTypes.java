dcf293f721e97b32a8c26d134734b0977edc23df
chriscr <chriscr@us.ibm.com>
2020-02-14 12:09:59 -0600

    fix repeat action problems in classes that do not use repeat

--- dev/com.ibm.ws.security.mp.jwt_fat/fat/src/com/ibm/ws/security/mp/jwt/fat/actions/AuthHeaderPrefixTypes.java
+++ dev/com.ibm.ws.security.mp.jwt_fat/fat/src/com/ibm/ws/security/mp/jwt/fat/actions/AuthHeaderPrefixTypes.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 IBM Corporation and others.
+ * Copyright (c) 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -20,9 +20,9 @@ public class AuthHeaderPrefixTypes implements RepeatTestAction {
     String tokenType = MpJwtFatConstants.TOKEN_TYPE_BEARER;
 //    protected String currentID = null;
 
-    AuthHeaderPrefixTypes(String id) {
+    AuthHeaderPrefixTypes(String inTokenType) {
 
-        tokenType = id;
+        tokenType = inTokenType;
     }
 
     @Override
