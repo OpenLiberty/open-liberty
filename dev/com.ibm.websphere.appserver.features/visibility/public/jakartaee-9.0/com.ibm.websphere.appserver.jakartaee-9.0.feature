02b02293ebb253ef19feff468b80c91a1ae08c44
cbridgha <cbridgha@us.ibm.com>
2020-02-24 18:08:51 -0500

    parent 889dfea5495a0b268358eacba21870afc8fdcc90 author cbridgha <cbridgha@us.ibm.com> 1582585731 -0500 committer cbridgha <cbridgha@us.ibm.com> 1585004927 -0400
    Jakarta transform work
    
    Enable transform in build
    
    add el-4.0 jakarta feature and api
    
    add jsp 3.0 feature
    
    change servlet 5 symbolic bundle name to jakartaee; update servlet-5.0/resources/l10n descriptions
    
    package & rule corrections
    
    update kind=noship
    
    Update transformer version
    
    rename fixes & add jsp resources
    
    update jsp 3.0 bnd.bnd buildpath
    
    Add Jak 9 feature and upgrade transformer
    
    Setup transform task
    
    Enable transform in build
    
    add el-4.0 jakarta feature and api
    
    add jsp 3.0 feature
    
    change servlet 5 symbolic bundle name to jakartaee; update servlet-5.0/resources/l10n descriptions
    
    package & rule corrections
    
    update kind=noship
    
    rename fixes & add jsp resources
    
    update jsp 3.0 bnd.bnd buildpath
    
    RemoveExtra feature file
    
    update jakarta servlet version rule; correct servlet5  eclipse package name
    
    delete jakarta jar before transformatiton

---
+++ dev/com.ibm.websphere.appserver.features/visibility/public/jakartaee-9.0/com.ibm.websphere.appserver.jakartaee-9.0.feature
@@ -0,0 +1,12 @@
+-include= ~${workspace}/cnf/resources/bnd/feature.props
+symbolicName=com.ibm.websphere.appserver.jakartaee-9.0
+visibility=public
+singleton=true
+IBM-App-ForceRestart: install, uninstall
+IBM-ShortName: jakartaee-9.0
+Subsystem-Version: 9.0.0
+Subsystem-Name: Jakarta EE Platform 9.0
+-features=\
+  com.ibm.websphere.appserver.servlet-5.0
+kind=noship
+edition=base
---
+++ dev/com.ibm.websphere.jakartaee.el.4.0/src/jakarta/el/ELContextEvent.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 1997, 2020 Oracle and/or its affiliates and others.
+ * All rights reserved.
+ * Copyright 2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jakarta.el;
+
+import java.util.EventObject;
+
+/**
+ * An event which indicates that an {@link ELContext} has been created. The source object is the ELContext that was
+ * created.
+ *
+ * @see ELContext
+ * @see ELContextListener
+ * 
+ * @since Jakarta Server Pages 2.1
+ */
+public class ELContextEvent extends EventObject {
+
+    private static final long serialVersionUID = 1255131906285426769L;
+
+    /**
+     * Constructs an ELContextEvent object to indicate that an <code>ELContext</code> has been created.
+     *
+     * @param source the <code>ELContext</code> that was created.
+     */
+    public ELContextEvent(ELContext source) {
+        super(source);
+    }
+
+    /**
+     * Returns the <code>ELContext</code> that was created. This is a type-safe equivalent of the {@link #getSource} method.
+     *
+     * @return the ELContext that was created.
+     */
+    public ELContext getELContext() {
+        return (ELContext) getSource();
+    }
+}
