a5479126e60f533c76eccf751904e1d829772a23
Prashanth G <pgunapal@ca.ibm.com>
2020-03-11 18:12:01 -0400

    Revert "Add new simple format for the consoleFormat logging attribute"

--- dev/com.ibm.ws.logging_fat/fat/src/com/ibm/ws/logging/fat/FATSuite.java
+++ dev/com.ibm.ws.logging_fat/fat/src/com/ibm/ws/logging/fat/FATSuite.java
@@ -1,5 +1,6 @@
 /*************************************************************************
- * Copyright (c) 2011, 2020 IBM Corporation and others.
+ * ******
+ * Copyright (c) 2011, 2019 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -34,8 +35,7 @@ import org.junit.runners.Suite.SuiteClasses;
                 HeaderFormatTest.class,
                 LogServiceTest.class,
                 RealFlushTest.class,
-                JSONFieldsTest.class,
-                ConsoleFormatTest.class
+                JSONFieldsTest.class
 })
 
 public class FATSuite {
