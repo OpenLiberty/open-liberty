c2e042ca1eec64290e661d6156c3729f8d723ce5
cbridgha <cbridgha@us.ibm.com>
2020-03-17 10:56:45 -0400

    Update copyright

--- dev/com.ibm.ws.kernel.boot_fat/fat/src/com/ibm/ws/kernel/boot/FATSuite.java
+++ dev/com.ibm.ws.kernel.boot_fat/fat/src/com/ibm/ws/kernel/boot/FATSuite.java
@@ -22,6 +22,7 @@ import com.ibm.ws.kernel.boot.commandport.ServerCommandPortTest;
 import com.ibm.ws.kernel.boot.internal.commands.LogLevelPropertyTest;
 import com.ibm.ws.kernel.boot.internal.commands.PackageCommandTest;
 import com.ibm.ws.kernel.boot.internal.commands.PackageLooseConfigTest;
+import com.ibm.ws.kernel.boot.internal.commands.PackageLooseConfigDropinsTest;
 import com.ibm.ws.kernel.boot.internal.commands.PackageLooseConfigParameterizedTest;
 import com.ibm.ws.kernel.osgi.OSGiEmbedManagerTest;
 import com.ibm.ws.kernel.provisioning.KernelChangeTest;
