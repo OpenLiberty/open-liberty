78de7e087d6b2a4f1ad2ca83ab209c198369b31e
Andy McCright <j.andrew.mccright@gmail.com>
2020-03-19 17:21:06 -0500

    Remove old graphql impl

--- dev/com.ibm.ws.io.leangen.graphql.spqr.0.9.9/src/io/leangen/graphql/execution/ResolverInterceptorFactory.java
+++
@@ -1,8 +0,0 @@
-package io.leangen.graphql.execution;
-
-import java.util.List;
-
-public interface ResolverInterceptorFactory {
-
-    List<ResolverInterceptor> getInterceptors(ResolverInterceptorFactoryParams params);
-}
