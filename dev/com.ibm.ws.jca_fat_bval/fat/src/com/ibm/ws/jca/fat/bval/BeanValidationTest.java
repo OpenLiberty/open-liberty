280e9cae85ae506cd16d453b78ffaf0b643db893
kyleaure <kylejaure@gmail.com>
2020-03-24 16:58:55 -0500

    init upload

---
+++ dev/com.ibm.ws.jca_fat_bval/fat/src/com/ibm/ws/jca/fat/bval/BeanValidationTest.java
@@ -0,0 +1,228 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2020 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.jca.fat.bval;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+import com.ibm.websphere.simplicity.log.Log;
+
+import componenttest.topology.impl.LibertyServer;
+import componenttest.topology.impl.LibertyServerFactory;
+
+public class BeanValidationTest {
+    private final Class<? extends BeanValidationTest> c = this.getClass();
+    private static final String BVAL_APP = "jca-bval";
+
+    @Rule
+    public TestName testName = new TestName();
+
+    protected static LibertyServer server;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        server = LibertyServerFactory.getLibertyServer("com.ibm.ws.jca.fat.bval");
+        server.addInstalledAppForValidation(BVAL_APP);
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        if (server.isStarted())
+            server.stopServer();
+    }
+
+    @Before
+    public void beforeEach() throws Exception {
+        System.out.println(">>> " + testName.getMethodName());
+        Log.info(c, testName.getMethodName(), "Starting test: " + testName.getMethodName());
+    }
+
+    @After
+    public void afterEach() throws Exception {
+        System.out.println("<<< " + testName.getMethodName());
+        Log.info(c, testName.getMethodName(), "Ending test: " + testName.getMethodName());
+
+        if (server.isStarted())
+            server.stopServer();
+    }
+
+    /**
+     * Utility method to run a test on RABValServlet.
+     *
+     * @param query query string for the servlet
+     * @return output of the servlet
+     * @throws IOException if an error occurs
+     */
+    private StringBuilder runInServlet(String query) throws IOException {
+        URL url = new URL("http://" + server.getHostname() + ":" + server.getHttpDefaultPort() + "/" + BVAL_APP + "/BValApp?testName=" + testName.getMethodName() + "&" + query);
+        Log.info(c, "runInJDBCFATServlet", "URL is " + url);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        try {
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.setUseCaches(false);
+            con.setRequestMethod("GET");
+
+            InputStream is = con.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+
+            String sep = System.getProperty("line.separator");
+            StringBuilder lines = new StringBuilder();
+            for (String line = br.readLine(); line != null; line = br.readLine())
+                lines.append(line).append(sep);
+
+            if (lines.indexOf("ERROR:") >= 0)
+                fail("Error in servlet output: " + lines);
+
+            return lines;
+        } finally {
+            con.disconnect();
+        }
+    }
+
+    @Test
+    public void testAddAndFind() throws Exception {
+
+        server.setServerConfigurationFile("server-base.xml");
+        server.startServer(testName.getMethodName() + ".log");
+
+        // attempt find for an entry that isn't in the table
+        StringBuilder output = runInServlet("functionName=FIND&capital=Saint%20Paul");
+        if (output.indexOf("Did not FIND any entries") < 0)
+            throw new Exception("Entry should not have been found. Output: " + output);
+
+        // add
+        output = runInServlet("functionName=ADD&state=Iowa&population=30741869&area=56272&capital=Des%20Moines");
+        output = runInServlet("functionName=ADD&state=Minnesota&population=5379139&area=86939&capital=Saint%20Paul");
+
+        // find
+        output = runInServlet("functionName=FIND&capital=Saint%20Paul");
+        if (output.indexOf("Successfully performed FIND with output: {area=86939, capital=Saint Paul, population=5379139, state=Minnesota}") < 0)
+            throw new Exception("Did not find entry. Output: " + output);
+    }
+
+    @Test
+    public void testAddAndRemove() throws Exception {
+
+        server.setServerConfigurationFile("server-base.xml");
+        server.startServer(testName.getMethodName() + ".log");
+
+        // add
+        StringBuilder output = runInServlet("functionName=ADD&city=Rochester&state=Minnesota&population=106769");
+        output = runInServlet("functionName=ADD&city=Stewartville&state=Minnesota&population=5916");
+        output = runInServlet("functionName=ADD&city=Byron&state=Minnesota&population=4914");
+
+        // remove
+        output = runInServlet("functionName=REMOVE&city=Stewartville");
+        if (output.indexOf("Successfully performed REMOVE with output: {city=Stewartville, population=5916, state=Minnesota}") < 0)
+            throw new Exception("Did not report entry removed. Output: " + output);
+
+        // attempt removal of something that doesn't exist
+        output = runInServlet("functionName=REMOVE&city=Stewartville");
+        if (output.indexOf("Successfully performed REMOVE") >= 0)
+            throw new Exception("Entry should not have been present to remove. Output: " + output);
+    }
+
+    @Test
+    public void testMessageDrivenBean() throws Exception {
+
+        server.setServerConfigurationFile("server-with-mdb.xml");
+        server.startServer(testName.getMethodName() + ".log");
+
+        StringBuilder output = runInServlet("functionName=ADD&county=Olmsted&state=Minnesota&population=147066&area=654.5");
+        if (output.indexOf("Successfully performed ADD with output: {area=654.5, county=Olmsted, population=147066, state=Minnesota}") < 0)
+            throw new Exception("Did not report entry added. Output: " + output);
+
+        // search messages log for MDB output
+        final String expectedMsg = "BValMessageDrivenBean.onMessage record = \\{area=654.5, county=Olmsted, population=147066, state=Minnesota\\}";
+        String msg = server.waitForStringInLog(expectedMsg);
+        assertNotNull("Expected to find message: " + expectedMsg, msg);
+    }
+
+    @Test
+    public void testInvalidActivationSpec() throws Exception {
+
+        server.setServerConfigurationFile("server-invalid-interaction-spec.xml");
+        server.startServer(testName.getMethodName() + ".log");
+
+        try {
+            runInServlet("functionName=ADD&county=Olmsted&state=Minnesota&population=147066&area=654.5");
+            fail("Unexpected success in error path, expecting servlet call to fail and get a J2CA0238E");
+        } catch (Exception e) {
+
+            if (server.waitForStringInLog("J2CA0238E") == null) {
+                fail("Didn't find expected message: J2CA0238E");
+            }
+            //Else pass
+        }
+
+        server.stopServer("(?s)J2CA0238E.*com.ibm.bval.jca.adapter.InteractionSpecImpl", // multiline match because some translations split this message into multiple lines
+                          "SRVE0319E(?=.*eis/iSpec_ADD)(?=.*javax.resource.cci.InteractionSpec)",
+                          "CWNEN1006E(?=.*eis/iSpec_ADD)(?=.*javax.resource.cci.InteractionSpec)");
+    }
+
+    @Test
+    public void testInvalidConnectionFactory() throws Exception {
+
+        server.setServerConfigurationFile("server-invalid-cf.xml");
+        server.startServer(testName.getMethodName() + ".log");
+
+        try {
+            runInServlet("functionName=ADD&city=Rochester&state=Minnesota&population=106769");
+            fail("Unexpected success in error path, expecting servlet call to fail and get a J2CA0238E");
+        } catch (Exception e) {
+            if (server.waitForStringInLog("J2CA0238E") == null) {
+                fail("Didn't find expected message: J2CA0238E");
+            }
+        }
+
+        server.stopServer("(?s)J2CA0238E.*com.ibm.bval.jca.adapter.ManagedConnectionFactoryImpl", // multiline match because some translations split this message into multiple lines
+                          "CWNEN1006E(?=.*eis/conFactory)(?=.*javax.resource.cci.ConnectionFactory)");
+    }
+
+    @Test
+    public void testInvalidResourceAdapter() throws Exception {
+
+        server.setServerConfigurationFile("server-invalid-ra.xml");
+        server.startServer(testName.getMethodName() + ".log");
+
+        try {
+            runInServlet("functionName=FIND&capital=Saint%20Paul");
+            fail("Unexpected success in error path, expecting servlet call to fail and get a J2CA0238E");
+        } catch (FileNotFoundException e) {
+            if (server.waitForStringInLog("J2CA0238E") == null) {
+                fail("Didn't find expected message: J2CA0238E");
+            }
+        }
+
+        server.stopServer("(?s)J2CA0238E.*com.ibm.bval.jca.adapter.ResourceAdapterImpl", // multiline match because some translations split this message into multiple lines
+                          "CWWKE0701E",
+                          "CWWKE0700W",
+                          "CWNEN1006E(?=.*eis/conFactory)(?=.*javax.resource.cci.ConnectionFactory)");
+    }
+}
---
+++ dev/com.ibm.ws.jca_fat_bval/test-applications/jca-bval/src/web/RABValServlet.java
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2020 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.resource.cci.Connection;
+import javax.resource.cci.ConnectionFactory;
+import javax.resource.cci.ConnectionSpec;
+import javax.resource.cci.Interaction;
+import javax.resource.cci.InteractionSpec;
+import javax.resource.cci.MappedRecord;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RABValServlet extends HttpServlet {
+    private static final long serialVersionUID = 7709282314904580335L;
+
+    @Resource(lookup = "eis/conFactory")
+    private ConnectionFactory conFactory;
+
+    @Resource(lookup = "eis/conSpec")
+    private ConnectionSpec conSpec;
+
+    @Resource(lookup = "eis/iSpec_ADD")
+    private InteractionSpec iSpec_ADD;
+
+    @Resource(lookup = "eis/iSpec_FIND")
+    private InteractionSpec iSpec_FIND;
+
+    @Resource(lookup = "eis/iSpec_REMOVE")
+    private InteractionSpec iSpec_REMOVE;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String function = request.getParameter("functionName");
+        if (function == null)
+            return;
+        PrintWriter out = response.getWriter();
+        System.out.println(">>>> Entering servlet. Query: " + request.getQueryString());
+
+        try {
+            MappedRecord output = conFactory.getRecordFactory().createMappedRecord("output");
+            MappedRecord input = conFactory.getRecordFactory().createMappedRecord("input");
+            for (Map.Entry<String, String[]> param : request.getParameterMap().entrySet())
+                if (!"functionName".equalsIgnoreCase(param.getKey()) && !"testName".equalsIgnoreCase(param.getKey()))
+                    input.put(param.getKey(), param.getValue()[0]);
+
+            InteractionSpec ispec = "ADD"
+                            .equalsIgnoreCase(function) ? iSpec_ADD : "FIND".equalsIgnoreCase(function) ? iSpec_FIND : "REMOVE".equalsIgnoreCase(function) ? iSpec_REMOVE : null;
+
+            String message;
+            Connection con = conFactory.getConnection(conSpec);
+            try {
+                Interaction interaction = con.createInteraction();
+                message = interaction.execute(ispec, input,
+                                              output) ? ("Successfully performed " + function + " with output: " + output) : ("Did not " + function + " any entries.");
+                interaction.close();
+            } finally {
+                con.close();
+            }
+
+            out.println(message);
+            System.out.println("<<<< Exiting servlet. " + message);
+        } catch (Throwable x) {
+            while (x instanceof InvocationTargetException)
+                x = x.getCause();
+            System.out.println("Exiting servlet with exception. " + x);
+            x.printStackTrace(System.out);
+            out.println("<pre>ERROR: ");
+            x.printStackTrace(out);
+            out.println("</pre>");
+        }
+    }
+}
