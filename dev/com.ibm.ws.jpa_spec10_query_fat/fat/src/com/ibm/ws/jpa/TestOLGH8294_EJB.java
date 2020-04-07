f50c38b7730c8f605528e334800d326c1f561793
Joe Grassel <jgrassel@us.ibm.com>
2020-04-07 15:03:40 -0500

    Update JPA Spec 1.0 Query FAT to be multi-db supportive.
    Signed-off-by: Joe Grassel <jgrassel@us.ibm.com>

--- dev/com.ibm.ws.jpa_spec10_query_fat/fat/src/com/ibm/ws/jpa/TestOLGH8294_EJB.java
+++ dev/com.ibm.ws.jpa_spec10_query_fat/fat/src/com/ibm/ws/jpa/TestOLGH8294_EJB.java
@@ -22,6 +22,7 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.testcontainers.containers.JdbcDatabaseContainer;
 
 import com.ibm.websphere.simplicity.ShrinkHelper;
 import com.ibm.websphere.simplicity.config.Application;
@@ -38,6 +39,8 @@ import componenttest.annotation.TestServlets;
 import componenttest.custom.junit.runner.FATRunner;
 import componenttest.custom.junit.runner.Mode;
 import componenttest.custom.junit.runner.Mode.TestMode;
+import componenttest.topology.database.container.DatabaseContainerType;
+import componenttest.topology.database.container.DatabaseContainerUtil;
 import componenttest.topology.impl.LibertyServer;
 import componenttest.topology.utils.PrivHelper;
 
@@ -69,6 +72,8 @@ public class TestOLGH8294_EJB extends JPAFATServletClient {
     })
     public static LibertyServer server;
 
+    public static final JdbcDatabaseContainer<?> testContainer = FATSuite.testContainer;
+
     @BeforeClass
     public static void setUp() throws Exception {
         PrivHelper.generateCustomPolicy(server, FATSuite.JAXB_PERMS);
@@ -84,6 +89,12 @@ public class TestOLGH8294_EJB extends JPAFATServletClient {
         if (configUpdateTimeout < (120 * 1000)) {
             server.setConfigUpdateTimeout(120 * 1000);
         }
+
+        //Get driver name
+        server.addEnvVar("DB_DRIVER", DatabaseContainerType.valueOf(testContainer).getDriverName());
+
+        //Setup server DataSource properties
+        DatabaseContainerUtil.setupDataSourceProperties(server, testContainer);
 
         server.startServer();
 
---
+++ dev/com.ibm.ws.jpa_spec10_query_fat/test-applications/olgh8294/ddl/WEB-INF/classes/JPA_OLGH8294_DROP_SQLSERVER.ddl
@@ -0,0 +1,1 @@
+DROP TABLE SimpleEntityOLGH8294;
