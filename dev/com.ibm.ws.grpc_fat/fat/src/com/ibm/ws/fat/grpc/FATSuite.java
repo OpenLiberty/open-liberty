a53c0bb31012a3760518b91c241f5504035c13bd
Lori Adington <loriad@us.ibm.com>
2020-04-08 13:29:52 -0400

    11645: grpc test updates

--- dev/com.ibm.ws.grpc_fat/fat/src/com/ibm/ws/fat/grpc/FATSuite.java
+++ dev/com.ibm.ws.grpc_fat/fat/src/com/ibm/ws/fat/grpc/FATSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2018 IBM Corporation and others.
+ * Copyright (c) 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,26 +8,19 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-package com.ibm.ws.grpc;
+package com.ibm.ws.fat.grpc;
 
-import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import componenttest.rules.repeater.FeatureReplacementAction;
-import componenttest.rules.repeater.RepeatTests;
-
 @RunWith(Suite.class)
 @SuiteClasses({
-                SimpleTest.class,
+                HelloWorldTest.class,
 })
+
 public class FATSuite {
 
-    // Using the RepeatTests @ClassRule will cause all tests to be run twice.
-    // First without any modifications, then again with all features upgraded to their EE8 equivalents.
-    @ClassRule
-    public static RepeatTests r = RepeatTests.withoutModification()
-                    .andWith(FeatureReplacementAction.EE8_FEATURES());
+    private static final Class<?> c = FATSuite.class;
 
 }
--- dev/com.ibm.ws.grpc_fat/test-applications/app1/src/app1/web/TestServletA.java
+++ dev/com.ibm.ws.grpc_fat/test-applications/app1/src/app1/web/TestServletA.java
@@ -11,52 +11,48 @@
 package app1.web;
 
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.junit.Assert;
-import org.junit.Test;
 
 import componenttest.app.FATServlet;
-import componenttest.custom.junit.runner.Mode;
-import componenttest.custom.junit.runner.Mode.TestMode;
 
+//http://localhost:9080/grpc_helloworld_client/grpcClient
 @SuppressWarnings("serial")
-@WebServlet("/TestServletA")
+@WebServlet("/grpc_helloworld_client")
 public class TestServletA extends FATServlet {
 
     // By extending FATServlet and using @TestServlet in the client side test class, @Test annotations
     // can be added directly to the test servlet.
     // In this test servlet, each @Test method is invoked in its own HTTP GET request.
 
-    @Test
-    public void basicTest() throws Exception {
+    //@Test
+    public void basicTestClient() throws Exception {
         System.out.println("Test is running in an HttpServlet");
         Assert.assertTrue("Can also use JUnit assertions", true);
     }
 
-    @Test
-    public void testHttpServletRequest(HttpServletRequest request, HttpServletResponse resp) throws Exception {
-        System.out.println("You can also use thee (HttpServletRequest, HttpServletResponse) signature " +
-                           " on a test method if you need to access the underlying HTTP request/response");
-        System.out.println("Got HTTP params: " + request.getParameterMap());
-        resp.getWriter().println("Running test method 'testHttpServletRequest'");
-    }
+    //   @Test
+    //   public void testHttpServletRequest(HttpServletRequest request, HttpServletResponse resp) throws Exception {
+    //       System.out.println("You can also use thee (HttpServletRequest, HttpServletResponse) signature " +
+    //                          " on a test method if you need to access the underlying HTTP request/response");
+    //       System.out.println("Got HTTP params: " + request.getParameterMap());
+    //       resp.getWriter().println("Running test method 'testHttpServletRequest'");
+    //   }
 
-    @Test
-    @Mode(TestMode.LITE)
+    //@Test
+    //@Mode(TestMode.LITE)
     public void liteTest() throws Exception {
         System.out.println("LITE test is running.");
     }
 
-    @Test
-    @Mode(TestMode.FULL)
+    //@Test
+    //@Mode(TestMode.FULL)
     public void testFull() throws Exception {
         System.out.println("This test should only run in Full or higher mode!");
     }
 
-    @Test
-    @Mode(TestMode.QUARANTINE)
+    //@Test
+    //@Mode(TestMode.QUARANTINE)
     public void testQuarantine() throws Exception {
         System.out.println("This test should only run in Quarantine mode!");
     }
