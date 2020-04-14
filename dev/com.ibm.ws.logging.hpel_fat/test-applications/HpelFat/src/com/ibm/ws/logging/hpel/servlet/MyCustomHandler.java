571db4e36c730ea1605d676f87cb3294cb1800b4
Yushan Lin <yushan.lin@ibm.com>
2020-04-14 17:07:06 -0400

    update copyright in HPEL FAT

--- dev/com.ibm.ws.logging.hpel_fat/test-applications/HpelFat/src/com/ibm/ws/logging/hpel/servlet/MyCustomHandler.java
+++ dev/com.ibm.ws.logging.hpel_fat/test-applications/HpelFat/src/com/ibm/ws/logging/hpel/servlet/MyCustomHandler.java
@@ -1,11 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2020 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package com.ibm.ws.logging.hpel.servlet;
 
 import java.io.IOException;
 import java.util.logging.FileHandler;
 
 public class MyCustomHandler extends FileHandler {
-    public MyCustomHandler(String pattern, int limit, int count, boolean append)
-        throws SecurityException, IOException {
+    public MyCustomHandler(String pattern, int limit, int count, boolean append) throws SecurityException, IOException {
         super(pattern, limit, count, append);
     }
 }
