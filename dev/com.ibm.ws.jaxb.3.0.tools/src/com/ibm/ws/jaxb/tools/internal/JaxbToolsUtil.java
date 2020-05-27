a502bb86162ff1bcda933ff5657b2f184561d660
Thomas Johnson <tjjohnso@us.ibm.com>
2020-05-27 16:26:52 -0500

    update more headers

--- dev/com.ibm.ws.jaxb.3.0.tools/src/com/ibm/ws/jaxb/tools/internal/JaxbToolsUtil.java
+++ dev/com.ibm.ws.jaxb.3.0.tools/src/com/ibm/ws/jaxb/tools/internal/JaxbToolsUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 IBM Corporation and others.
+ * Copyright (c) 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,7 +16,7 @@ package com.ibm.ws.jaxb.tools.internal;
 public class JaxbToolsUtil {
     /**
      * get the localized message provided in the message files in the com.ibm.ws.jaxb.tools.
-     * 
+     *
      * @param msgKey
      * @return
      */
