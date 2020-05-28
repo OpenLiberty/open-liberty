e73130b1493ba612f38cdd424881ae5e377b2d84
Thomas Johnson <tjjohnso@us.ibm.com>
2020-05-28 16:31:13 -0500

    add jakarta jaxb-3.0

--- dev/com.ibm.websphere.appserver.features/visibility/public/jakartaMail-1.6/com.ibm.websphere.appserver.jakartaMail-1.6.feature
+++ dev/com.ibm.websphere.appserver.features/visibility/public/jakartaMail-1.6/com.ibm.websphere.appserver.jakartaMail-1.6.feature
@@ -6,6 +6,6 @@ IBM-ShortName: jakartaaMail-1.6
 Subsystem-Version: 1.6
 Subsystem-Name: JakartaMail 1.6
 -bundles=\
-  com.ibm.websphere.jakarta.activation.2.0; location:="dev/api/spec/,lib/";mavenCoordinates="jakarta.activation:jakarta.activation-api:2.0"
+  io.openliberty.activation.2.0; location:="dev/api/spec/,lib/";mavenCoordinates="jakarta.activation:jakarta.activation-api:2.0"
 kind=noship
 edition=core
---
+++ dev/com.ibm.ws.jaxb.3.0.tools/src/com/ibm/ws/jaxb/tools/XJC.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2020 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.jaxb.tools;
+
+import java.io.PrintStream;
+
+import com.ibm.ws.jaxb.tools.internal.JaxbToolsConstants;
+import com.ibm.ws.jaxb.tools.internal.JaxbToolsUtil;
+
+/**
+ * IBM Wrapper for XJC tool.
+ */
+public class XJC {
+    private static final PrintStream err = System.err;
+
+    public static void main(String args[]) throws java.lang.Throwable {
+        if (isTargetRequired(args)) {
+            String errMsg = JaxbToolsUtil.formatMessage(JaxbToolsConstants.ERROR_PARAMETER_TARGET_MISSED_KEY);
+            err.println(errMsg);
+
+            return;
+        }
+
+        com.sun.tools.xjc.Driver.main(args);
+    }
+
+    private static boolean isTargetRequired(String[] args) {
+        boolean helpExisted = false;
+        boolean versionExisted = false;
+        boolean targetExisted = false;
+
+        for (String arg : args) {
+            if (arg.equals(JaxbToolsConstants.PARAM_HELP)) {
+                helpExisted = true;
+            } else if (arg.equals(JaxbToolsConstants.PARAM_VERSION)) {
+                versionExisted = true;
+            } else if (arg.equals(JaxbToolsConstants.PARAM_TARGET)) {
+                targetExisted = true;
+            }
+
+            continue;
+        }
+
+        return args.length > 0 && !helpExisted && !versionExisted && !targetExisted;
+    }
+}
