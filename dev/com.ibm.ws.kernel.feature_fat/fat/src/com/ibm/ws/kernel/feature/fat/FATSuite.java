9f90ce93100e0d9465c86041e6537966a0286f55
Dave Zavala <dzavala@us.ibm.com>
2020-06-18 15:07:43 -0500

    9214 Add eeCompatible toleration conflict error handling

--- dev/com.ibm.ws.kernel.feature_fat/fat/src/com/ibm/ws/kernel/feature/fat/FATSuite.java
+++ dev/com.ibm.ws.kernel.feature_fat/fat/src/com/ibm/ws/kernel/feature/fat/FATSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 IBM Corporation and others.
+ * Copyright (c) 2019, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -32,7 +32,8 @@ import org.junit.runners.Suite.SuiteClasses;
                 SystemBundleOverrideTest.class,
                 FeatureAPITest.class,
                 RegionProvisioningTest.class,
-                RemoteServerInclude.class
+                RemoteServerInclude.class,
+                EECompatibilityTest.class
 })
 /**
  * Purpose: This suite collects and runs all known good test suites.
