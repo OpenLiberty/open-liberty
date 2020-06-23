6094860a6e49a99ed3c707fb775750fc73c1337b
Tayyab Waqar <r44zor@gmail.com>
2020-06-23 15:09:26 -0400

    Support config dropins/overrides folder

--- dev/com.ibm.ws.install/src/com/ibm/ws/install/internal/Director.java
+++ dev/com.ibm.ws.install/src/com/ibm/ws/install/internal/Director.java
@@ -14,6 +14,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -471,6 +473,21 @@ public class Director extends AbstractDirector {
         for (ServerAsset sa : servers) {
             File serverXmlFile = sa.getServerXmlFile();
             Collection<String> requiredFeatures = InstallUtils.getFeatures(serverXmlFile.getAbsolutePath(), serverXmlFile.getName(), new HashSet<String>());
+
+            File serverDirectory = sa.getServerDirectory();
+            File overridesFolder = new File(serverDirectory, "/configDropins/overrides");
+            if(overridesFolder.exists() && overridesFolder.isDirectory()) {
+                logger.fine("Processing " + overridesFolder);
+                Files.newDirectoryStream(Paths.get(overridesFolder.toURI()),
+                        path -> path.toString().endsWith(".xml"))
+                        .forEach(path -> {
+                            try {
+                                requiredFeatures.addAll(InstallUtils.getFeatures(path.toString(), path.getFileName().toString(), new HashSet<String>()));
+                            } catch (IOException e) {
+                                logger.fine("Could not process " + path);
+                            }
+                        });
+            }
 
             if (!requiredFeatures.isEmpty()) {
                 logger.log(Level.FINEST, Messages.INSTALL_KERNEL_MESSAGES.getLogMessage("LOG_DEPLOY_SERVER_FEATURES",
