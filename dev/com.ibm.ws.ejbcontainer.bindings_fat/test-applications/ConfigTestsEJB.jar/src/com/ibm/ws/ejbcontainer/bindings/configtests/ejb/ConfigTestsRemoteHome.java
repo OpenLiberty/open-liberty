e843f917b76c932c878da0ad2275ec54d0495621
olendvcook <olendvcook@gmail.com>
2020-06-24 09:06:33 -0500

    Rename EJB Test App for re-use

---
+++ dev/com.ibm.ws.ejbcontainer.bindings_fat/test-applications/ConfigTestsEJB.jar/src/com/ibm/ws/ejbcontainer/bindings/configtests/ejb/ConfigTestsRemoteHome.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2019 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.ejbcontainer.bindings.configtests.ejb;
+
+import javax.ejb.CreateException;
+
+public interface ConfigTestsRemoteHome extends javax.ejb.EJBHome {
+    public ConfigTestsRemoteEJB create() throws CreateException, java.rmi.RemoteException;
+
+}
