14fc61a2a89c461c693c3cc660d1b33e7f7ee57a
tevans <tevans@uk.ibm.com>
2020-07-09 11:29:51 +0100

    Update CDI visibility tests

--- dev/fattest.simplicity/src/componenttest/rules/repeater/EE8FeatureReplacementAction.java
+++ dev/fattest.simplicity/src/componenttest/rules/repeater/EE8FeatureReplacementAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018,2020 IBM Corporation and others.
+ * Copyright (c) 2018, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -52,7 +52,9 @@ public class EE8FeatureReplacementAction extends FeatureReplacementAction {
     public static final Set<String> EE8_FEATURE_SET = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(EE8_FEATURES_ARRAY)));
 
     public EE8FeatureReplacementAction() {
-        super(EE7FeatureReplacementAction.EE7_FEATURE_SET, EE8_FEATURE_SET);
+        super(EE8_FEATURE_SET);
+        removeFeatures(EE7FeatureReplacementAction.EE7_FEATURE_SET);
+        removeFeatures(JakartaEE9Action.EE9_FEATURE_SET);
         forceAddFeatures(false);
         withID(ID);
     }
--- dev/fattest.simplicity/src/componenttest/rules/repeater/FeatureReplacementAction.java
+++ dev/fattest.simplicity/src/componenttest/rules/repeater/FeatureReplacementAction.java
@@ -43,8 +43,10 @@ public class FeatureReplacementAction implements RepeatTestAction {
 
     private static final Class<?> c = FeatureReplacementAction.class;
 
-    static final String ALL_SERVERS = "ALL_SERVERS";
-    static final String ALL_CLIENTS = "ALL_CLIENTS";
+    public static final String ALL_SERVERS = "ALL_SERVERS";
+    public static final String ALL_CLIENTS = "ALL_CLIENTS";
+    public static final String NO_SERVERS = "NO_SERVERS";
+    public static final String NO_CLIENTS = "NO_CLIENTS";
 
     private static final Map<String, String> featuresWithNameChangeOnEE9;
 
@@ -210,8 +212,12 @@ public class FeatureReplacementAction implements RepeatTestAction {
      * By default, all server config files in publish/servers/ and publish/files/ will be scanned for updates.
      */
     public FeatureReplacementAction forServers(String... serverNames) {
-        servers.remove(ALL_SERVERS);
-        servers.addAll(Arrays.asList(serverNames));
+        if (NO_SERVERS.equals(serverNames[0])) {
+            servers.clear();
+        } else {
+            servers.remove(ALL_SERVERS);
+            servers.addAll(Arrays.asList(serverNames));
+        }
         return this;
     }
 
@@ -221,8 +227,12 @@ public class FeatureReplacementAction implements RepeatTestAction {
      * By default, all server config files in publish/clients/ will be scanned for updates.
      */
     public FeatureReplacementAction forClients(String... clientNames) {
-        clients.remove(ALL_CLIENTS);
-        clients.addAll(Arrays.asList(clientNames));
+        if (NO_CLIENTS.equals(clientNames[0])) {
+            clients.clear();
+        } else {
+            clients.remove(ALL_CLIENTS);
+            clients.addAll(Arrays.asList(clientNames));
+        }
         return this;
     }
 
@@ -392,75 +402,75 @@ public class FeatureReplacementAction implements RepeatTestAction {
 
     /**
      * Obtain the replacement for a feature for this replacement action.
-     * 
+     *
      * The lookup uses the base feature name, which is the feature name up to
      * the first '-' character.
-     * 
+     *
      * If EE9 replacement is active, allow the base feature name to be replaced
      * with an EE9 replacement.
-     * 
+     *
      * If no replacement is located, answer null, which indicates that the feature
      * should be removed instead of being replaced.
      *
-     * Feature names are required to have a '-', for example, "servlet-3.1".  Null
+     * Feature names are required to have a '-', for example, "servlet-3.1". Null
      * is answered for feature names which do not have a '-'.
      *
-     * @param originalFeature The feature name which is to be replaced.
-     * @param replacementFeatures Table of replacement features.
+     * @param  originalFeature     The feature name which is to be replaced.
+     * @param  replacementFeatures Table of replacement features.
      *
-     * @return The replacement feature name.  Null if no replacement is available.
+     * @return                     The replacement feature name. Null if no replacement is available.
      */
     private static String getReplacementFeature(String originalFeature, Set<String> replacementFeatures) {
         String methodName = "getReplacementFeature";
         // Example: servlet-3.1 --> servlet-4.0
         int dashOffset = originalFeature.indexOf('-');
-        if ( dashOffset == -1 )
-            throw new IllegalArgumentException("Remove feature [ " + originalFeature +  " ]: No '-' was found.");
+        if (dashOffset == -1)
+            throw new IllegalArgumentException("Remove feature [ " + originalFeature + " ]: No '-' was found.");
         // "servlet-3.1" ==> "servlet"
         String baseFeature = originalFeature.substring(0, dashOffset + 1);
         // "servlet-4.0".contains("servlet-")
-        for ( String replacementFeature : replacementFeatures ) {
-            if ( replacementFeature.contains(baseFeature) )  {
-                Log.info(c, methodName, "Replace feature [ " + originalFeature +  " ] with [ " + replacementFeature + " ]");
+        for (String replacementFeature : replacementFeatures) {
+            if (replacementFeature.toLowerCase().contains(baseFeature.toLowerCase())) {
+                Log.info(c, methodName, "Replace feature [ " + originalFeature + " ] with [ " + replacementFeature + " ]");
                 return replacementFeature;
             }
         }
-        // We need to check that the feature passed is an EE7/EE8 feature which could have a name change on EE9 that doesnt match 
+        // We need to check that the feature passed is an EE7/EE8 feature which could have a name change on EE9 that doesnt match
         // the original feature name it replaces. We also check viceversa if the feature is an EE9 feature that involves a name change
         // to update from EE9 to EE7/EE8
-        Log.info(c, methodName, "No feature replacement found for [ " + originalFeature +  " ]. Verifying if feature name was changed on EE9.");
+        Log.info(c, methodName, "No feature replacement found for [ " + originalFeature + " ]. Verifying if feature name was changed on EE9.");
         // Reset base feature to not include the "-"
         baseFeature = baseFeature.substring(0, dashOffset);
         String featureReplacement = null;
         for (Map.Entry<String, String> nameChangeEntry : featuresWithNameChangeOnEE9.entrySet()) {
-            if(nameChangeEntry.getValue().equals(baseFeature)){
+            if (nameChangeEntry.getValue().equalsIgnoreCase(baseFeature)) {
                 featureReplacement = nameChangeEntry.getKey();
-                Log.info(c, methodName, "Replace EE9 feature [ " + baseFeature +  " ] with feature [ " + featureReplacement + " ]");
+                Log.info(c, methodName, "Replace EE9 feature [ " + baseFeature + " ] with feature [ " + featureReplacement + " ]");
                 baseFeature = featureReplacement;
                 break;
             }
-            if(nameChangeEntry.getKey().equals(baseFeature)){
+            if (nameChangeEntry.getKey().equalsIgnoreCase(baseFeature)) {
                 featureReplacement = nameChangeEntry.getValue();
-                Log.info(c, methodName, "Replace base feature [ " + baseFeature +  " ] with EE9 feature [ " + featureReplacement + " ]");
+                Log.info(c, methodName, "Replace base feature [ " + baseFeature + " ] with EE9 feature [ " + featureReplacement + " ]");
                 baseFeature = featureReplacement;
                 break;
             }
         }
-        if ( featureReplacement == null ) {
-            Log.info(c, methodName, "Remove feature [ " + originalFeature +  " ]: No replacement is available");
+        if (featureReplacement == null) {
+            Log.info(c, methodName, "Remove feature [ " + originalFeature + " ]: No replacement is available");
             return null;
         }
         baseFeature += "-";
         // Re-check the features with the name changes
-        for ( String replacementFeature : replacementFeatures ) {
-            if ( replacementFeature.contains(baseFeature) )  {
-                Log.info(c, methodName, "Replace feature [ " + originalFeature +  " ] with [ " + replacementFeature + " ]");
+        for (String replacementFeature : replacementFeatures) {
+            if (replacementFeature.toLowerCase().contains(baseFeature.toLowerCase())) {
+                Log.info(c, methodName, "Replace feature [ " + originalFeature + " ] with [ " + replacementFeature + " ]");
                 return replacementFeature;
             }
         }
         // We may need to remove a feature without adding any replacement
         // (e.g. jsonb-1.0 is EE8 only) so in this case return null
-        Log.info(c, methodName, "Remove feature [ " + originalFeature +  " ]: No replacement is available");
+        Log.info(c, methodName, "Remove feature [ " + originalFeature + " ]: No replacement is available");
         return null;
     }
 
