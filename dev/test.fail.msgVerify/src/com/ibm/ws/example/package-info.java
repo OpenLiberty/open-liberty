92b803d9a81bfa27b4f45060bc4b9e08ae906a92
Wendy Raschke <wraschke@us.ibm.com>
2020-07-21 16:58:13 -0500

    Add message verification ignores to Open Liberty

--- dev/test.fail.msgVerify/src/com/ibm/ws/example/package-info.java
+++ dev/test.fail.msgVerify/src/com/ibm/ws/example/package-info.java
@@ -1,17 +1,13 @@
-/*
- * IBM Confidential
+/*******************************************************************************
+ * Copyright (c) 2020 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * OCO Source Materials
- *
- * Copyright IBM Corp. 2011
- *
- * The source code for this program is not published or otherwise divested 
- * of its trade secrets, irrespective of what has been deposited with the 
- * U.S. Copyright Office.
- */
-/**
- * @version 1.0
- */
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 @org.osgi.annotation.versioning.Version("1.0")
 @TraceOptions(traceGroup = "projectExample", messageBundle = "com.ibm.ws.sample.internal.NonExist")
 package com.ibm.ws.example;
