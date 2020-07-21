a478c3b8efcfa5d26aca55d5e38badf981ed8af4
Wendy Raschke <wraschke@us.ibm.com>
2020-07-21 12:32:17 -0500

    Temp project to test message verification

---
+++ dev/test.fail.msgVerify/src/com/ibm/ws/example/internal/SampleComponent.java
@@ -0,0 +1,50 @@
+/*
+ * IBM Confidential
+ *
+ * OCO Source Materials
+ *
+ * Copyright IBM Corp. 2011
+ *
+ * The source code for this program is not published or otherwise divested 
+ * of its trade secrets, irrespective of what has been deposited with the 
+ * U.S. Copyright Office.
+ */
+package com.ibm.ws.example.internal;
+
+import java.util.Map;
+
+import com.ibm.websphere.ras.Tr;
+import com.ibm.websphere.ras.TraceComponent;
+
+/**
+ * A declarative services component can be completely POJO based
+ * (no awareness/use of OSGi services).
+ * 
+ * OSGi methods (activate/deactivate) should be protected.
+ */
+public class SampleComponent {
+    private static final TraceComponent tc = Tr.register(SampleComponent.class);
+
+    /**
+     * DS method to activate this component.
+     * Best practice: this should be a protected method, not public or private
+     * 
+     * @param properties : Map containing service & config properties
+     *            populated/provided by config admin
+     */
+    protected void activate(Map<String, Object> properties) {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEventEnabled())
+            Tr.event(tc, "SampleComponent activated", properties);
+    }
+
+    /**
+     * DS method to deactivate this component.
+     * Best practice: this should be a protected method, not public or private
+     * 
+     * @param reason int representation of reason the component is stopping
+     */
+    protected void deactivate(int reason) {
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEventEnabled())
+            Tr.event(tc, "SampleComponent deactivated, reason=" + reason);
+    }
+}
---
+++ dev/test.fail.msgVerify/src/com/ibm/ws/example/internal/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * IBM Confidential
+ *
+ * OCO Source Materials
+ *
+ * Copyright IBM Corp. 2011
+ *
+ * The source code for this program is not published or otherwise divested 
+ * of its trade secrets, irrespective of what has been deposited with the 
+ * U.S. Copyright Office.
+ */
+@TraceOptions(traceGroup = "projectExample", messageBundle = "com.ibm.ws.sample.internal.NonExist")
+package com.ibm.ws.example.internal;
+
+import com.ibm.websphere.ras.annotation.TraceOptions;
+
