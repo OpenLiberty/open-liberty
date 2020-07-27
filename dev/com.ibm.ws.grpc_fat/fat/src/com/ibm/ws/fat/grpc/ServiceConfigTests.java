5b9ee79784d68b07af094e51d2204f5d1938d10a
Bill Lucy <wtlucy@gmail.com>
2020-07-27 14:43:50 -0400

    enable testServiceTargetWildcard; wait for grpc service message

--- dev/com.ibm.ws.grpc_fat/fat/src/com/ibm/ws/fat/grpc/ServiceConfigTests.java
+++ dev/com.ibm.ws.grpc_fat/fat/src/com/ibm/ws/fat/grpc/ServiceConfigTests.java
@@ -257,7 +257,7 @@ public class ServiceConfigTests extends FATServletClient {
      *
      **/
 
-    //@Test
+    @Test
     // This test is failing when I think it should be passing, commenting it out for now
     //@Mode(FULL)
     public void testServiceTargetWildcard() throws Exception {
@@ -275,7 +275,7 @@ public class ServiceConfigTests extends FATServletClient {
                                       "com.ibm.ws.grpc.fat.beer");
         LOG.info("ServiceConfigTests : testServiceTargetWildcard() : dropped the beer app into dropins.");
         // Make sure the beer service has started
-        String appStarted = grpcServer.waitForStringInLogUsingMark("CWWKZ0001I: Application FavoriteBeerService started", STARTUP_TIMEOUT);
+        String appStarted = grpcServer.waitForStringInLogUsingMark("CWWKT0201I.*FavoriteBeerService", STARTUP_TIMEOUT);
         if (appStarted == null) {
             Assert.fail(c + ": application " + "FavoriteBeerService" + " failed to start within " + STARTUP_TIMEOUT + "ms");
         }
@@ -375,7 +375,7 @@ public class ServiceConfigTests extends FATServletClient {
                                       "com.ibm.ws.grpc.fat.beer");
         LOG.info("ServiceConfigTests : testServiceTargetSpecificMatch() : dropped the beer app into dropins.");
         // Make sure the beer service has started
-        String appStarted = grpcServer.waitForStringInLogUsingMark("CWWKZ0001I: Application FavoriteBeerService started", STARTUP_TIMEOUT);
+        String appStarted = grpcServer.waitForStringInLogUsingMark("CWWKT0201I.*FavoriteBeerService", STARTUP_TIMEOUT);
         if (appStarted == null) {
             Assert.fail(c + ": application " + "FavoriteBeerService" + " failed to start within " + STARTUP_TIMEOUT + "ms");
         }
