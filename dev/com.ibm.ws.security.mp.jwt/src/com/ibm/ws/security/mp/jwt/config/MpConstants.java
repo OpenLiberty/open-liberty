ae7cbedb0a16685096b1ef967211ba199b171d6c
Adam Yoho <ayoho@us.ibm.com>
2020-07-28 15:11:53 -0500

    Updates to com.ibm.ws.security.mp.jwt.1.1.config to prepare for 1.2

--- dev/com.ibm.ws.security.mp.jwt/src/com/ibm/ws/security/mp/jwt/config/MpConstants.java
+++ dev/com.ibm.ws.security.mp.jwt/src/com/ibm/ws/security/mp/jwt/config/MpConstants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 IBM Corporation and others.
+ * Copyright (c) 2018, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,4 +18,12 @@ public interface MpConstants {
     public final static String ISSUER = "mp.jwt.verify.issuer";
     public final static String PUBLIC_KEY = "mp.jwt.verify.publickey";
     public final static String KEY_LOCATION = "mp.jwt.verify.publickey.location";
+
+    // Properties added by 1.2 specification
+    public final static String PUBLIC_KEY_ALG = "mp.jwt.verify.publickey.algorithm";
+    public final static String DECRYPT_KEY_LOCATION = "mp.jwt.decrypt.key.location";
+    public final static String VERIFY_AUDIENCES = "mp.jwt.verify.audiences";
+    public final static String TOKEN_HEADER = "mp.jwt.token.header";
+    public final static String TOKEN_COOKIE = "mp.jwt.token.cookie";
+
 }
