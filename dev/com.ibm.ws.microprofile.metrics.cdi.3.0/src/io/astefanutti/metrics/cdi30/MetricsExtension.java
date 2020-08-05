0b2710d42487147e5a0c6ab63e20f6f8d6e8d1f0
David Chan <chdavid@ca.ibm.com>
2020-08-05 11:45:53 -0400

    Additional Implementation Changes to statistfy MP Metrics 3.0 specification.
    - Cache MP Config mp.metrics.appName tags in Metric Registry by Application level values for each application (defined in mp.config.properties) and on the Server level (i.e env vars or sys props)
    - Cache MP Config mp.metrics.tags tags with the Metric Registry and is appended during metrics export
    - CDI Producers are no longer supported by the @metrics annotation
    - New Gauge register/retrieve methods that take Functions as parameters
    - MP Metrics 3.0 FAT TCK

--- dev/com.ibm.ws.microprofile.metrics.cdi.3.0/src/io/astefanutti/metrics/cdi30/MetricsExtension.java
+++ dev/com.ibm.ws.microprofile.metrics.cdi.3.0/src/io/astefanutti/metrics/cdi30/MetricsExtension.java
@@ -46,8 +46,6 @@ import javax.enterprise.inject.spi.BeforeShutdown;
 import javax.enterprise.inject.spi.Extension;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.enterprise.inject.spi.ProcessAnnotatedType;
-import javax.enterprise.inject.spi.ProcessProducerField;
-import javax.enterprise.inject.spi.ProcessProducerMethod;
 import javax.enterprise.inject.spi.WithAnnotations;
 import javax.enterprise.util.AnnotationLiteral;
 import javax.enterprise.util.Nonbinding;
@@ -109,17 +107,6 @@ public class MetricsExtension implements Extension, WebSphereCDIExtension {
     protected <X> void metricsAnnotations(@Observes @WithAnnotations({ Counted.class, Gauge.class, Metered.class, Timed.class,
                                                                        ConcurrentGauge.class, SimplyTimed.class }) ProcessAnnotatedType<X> pat) {
         pat.setAnnotatedType(new AnnotatedTypeDecorator<>(pat.getAnnotatedType(), METRICS_BINDING));
-    }
-
-    protected void metricProducerField(@Observes ProcessProducerField<? extends Metric, ?> ppf) {
-        metrics.put(ppf.getBean(), ppf.getAnnotatedProducerField());
-    }
-
-    protected void metricProducerMethod(@Observes ProcessProducerMethod<? extends Metric, ?> ppm) {
-        // Skip the Metrics CDI alternatives
-        if (!ppm.getBean().getBeanClass().equals(MetricProducer.class)) {
-            metrics.put(ppm.getBean(), ppm.getAnnotatedProducerMethod());
-        }
     }
 
     protected void defaultMetricRegistry(@Observes AfterBeanDiscovery abd, BeanManager manager) {
