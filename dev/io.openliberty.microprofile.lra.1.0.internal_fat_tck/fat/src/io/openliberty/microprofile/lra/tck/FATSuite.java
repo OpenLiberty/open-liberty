a069497445b79e6f19da547c4458c4239350dfd9
ilewis <idlewis@users.noreply.github.com>
2020-08-12 13:58:56 +0100

    Add the always passes test
    As the real tests are currently experimental, and you need
    a passing test in every fat bucket

--- dev/io.openliberty.microprofile.lra.1.0.internal_fat_tck/fat/src/io/openliberty/microprofile/lra/tck/FATSuite.java
+++ dev/io.openliberty.microprofile.lra.1.0.internal_fat_tck/fat/src/io/openliberty/microprofile/lra/tck/FATSuite.java
@@ -14,7 +14,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import componenttest.custom.junit.runner.AlwaysPassesTest;
+
 @RunWith(Suite.class)
-@SuiteClasses({ LraTckLauncher.class })
+@SuiteClasses({ LraTckLauncher.class, AlwaysPassesTest.class })
 public class FATSuite {
 }
