da9b2fe4351ff0713ec69b2383b68933807d7e27
Phu Dinh <pmdinh@us.ibm.com>
2020-08-13 11:08:36 -0400

    Update deployment descriptor for Servlet5

--- dev/com.ibm.ws.javaee.dd/src/com/ibm/ws/javaee/dd/web/package-info.java
+++ dev/com.ibm.ws.javaee.dd/src/com/ibm/ws/javaee/dd/web/package-info.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 IBM Corporation and others.
+ * Copyright (c) 2010, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,7 +9,7 @@
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 /**
- * @version 1.2
+ * @version 1.3
  */
-@org.osgi.annotation.versioning.Version("1.2")
+@org.osgi.annotation.versioning.Version("1.3")
 package com.ibm.ws.javaee.dd.web;
