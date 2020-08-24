c3c4c3d3352e3c605b94035ffbc4b8d731edc0a9
Benson Lam <bensonl@ca.ibm.com>
2020-08-24 15:19:41 -0400

    refactored ServicesTest

--- dev/com.ibm.ws.install.packaging_fat/fat/src/com/ibm/ws/install/packaging/fat/ServicesTest.java
+++ dev/com.ibm.ws.install.packaging_fat/fat/src/com/ibm/ws/install/packaging/fat/ServicesTest.java
@@ -54,32 +54,52 @@ public class ServicesTest extends InstallPackagesToolTest {
         Boolean testsPassed = false;
 
         //Install package
+        Log.info(c, METHOD_NAME, "Installing Open Liberty package.");
         ProgramOutput po1 = installCurrentPackage(METHOD_NAME, packageExt);
 
         // append JAVA_HOME to server.env
-        //sudo sh -c 'echo line > file'
-
+        Log.info(c, METHOD_NAME, "Configure Open Liberty to use test java.");
         String[] param1j = { "sh -c ", " 'echo JAVA_HOME=" + javaHome + " >> /opt/ol/etc/server.env'" };
         ProgramOutput po1j = runCommand(METHOD_NAME, "sudo ", param1j);
         Log.info(c, METHOD_NAME, "setup server.env permissions RC:" + po1j.getReturnCode());
 
         // Output contents of server.env
-        Log.info(c, METHOD_NAME, "Contents of opt/ol/etc/server.env");
+        Log.info(c, METHOD_NAME, "Contents of opt/ol/etc/server.env:");
         String[] param2b = { "cat", "/opt/ol/etc/server.env" };
         ProgramOutput po2b = runCommand(METHOD_NAME, "sudo ", param2b);
 
         // service tests
+        Log.info(c, METHOD_NAME, "Starting defaultServer");
+
         ProgramOutput po2 = serviceCommand(METHOD_NAME, "start", "defaultServer");
+        wait(2000);
         ProgramOutput po2a = serviceCommand(METHOD_NAME, "status", "defaultServer");
+        
+        Log.info(c, METHOD_NAME, "Stopping defaultServer");
         ProgramOutput po3 = serviceCommand(METHOD_NAME, "stop", "defaultServer");
+        wait(2000);
         ProgramOutput po3a = serviceCommand(METHOD_NAME, "status", "defaultServer");
+
+        Log.info(c, METHOD_NAME, "Re-starting defaultServer");
         ProgramOutput po4 = serviceCommand(METHOD_NAME, "restart", "defaultServer");
+        wait(2000);
         ProgramOutput po4a = serviceCommand(METHOD_NAME, "status", "defaultServer");
+        Log.info(c, METHOD_NAME, "Stopping defaultServer");
         ProgramOutput po5 = serviceCommand(METHOD_NAME, "stop", "defaultServer");
 
         //Uninstall package
         ProgramOutput po6 = uninstallPackage(METHOD_NAME, packageExt);
-
+        Log.info(c, METHOD_NAME, "Test Results Summary:\n" 
+                          +"===================="
+                          + "Install Current openliberty RC1:" + po1.getReturnCode() + "\n"
+                          + "start defaultServer.service RC2:" + po2.getReturnCode() + "\n"
+                          + "status defaultServer.service RC2a:" + po2a.getReturnCode() + "\n"
+                          + "stop defaultServer.service RC3:" + po3.getReturnCode() + "\n"
+                          + "status defaultServer.service RC3a:" + po3a.getReturnCode() + "\n"
+                          + "restart defaultServer.service RC4:" + po4.getReturnCode() + "\n"
+                          + "status defaultServer.service RC4a:" + po4a.getReturnCode() + "\n"
+                          + "stop defaultServer.service RC5:" + po5.getReturnCode() + "\n"
+                          + "uninstall openliberty RC6:" + po6.getReturnCode() + "\n");
         testsPassed = ((po1.getReturnCode() == 0) && (po2.getReturnCode() == 0) && (po2a.getReturnCode() == 0) && (po3.getReturnCode() == 0) && (po4.getReturnCode() == 0)
                        && (po5.getReturnCode() == 0)
                        && (po6.getReturnCode() == 0));
@@ -88,11 +108,11 @@ public class ServicesTest extends InstallPackagesToolTest {
                           + "start defaultServer.service RC2:" + po2.getReturnCode() + "\n"
                           + "status defaultServer.service RC2a:" + po2a.getReturnCode() + "\n"
                           + "stop defaultServer.service RC3:" + po3.getReturnCode() + "\n"
-                          + "status defaultServer.service RC3a:" + po3.getReturnCode() + "\n"
-                          + "restart defaultServer.service RC4:" + po3.getReturnCode() + "\n"
-                          + "status defaultServer.service RC4a:" + po3.getReturnCode() + "\n"
-                          + "stop defaultServer.service RC5:" + po3.getReturnCode() + "\n"
-                          + "uninstall openliberty RC6:" + po3.getReturnCode() + "\n", testsPassed);
+                          + "status defaultServer.service RC3a:" + po3a.getReturnCode() + "\n"
+                          + "restart defaultServer.service RC4:" + po4.getReturnCode() + "\n"
+                          + "status defaultServer.service RC4a:" + po4a.getReturnCode() + "\n"
+                          + "stop defaultServer.service RC5:" + po5.getReturnCode() + "\n"
+                          + "uninstall openliberty RC6:" + po6.getReturnCode() + "\n", testsPassed);
         exiting(c, METHOD_NAME);
     }
 
