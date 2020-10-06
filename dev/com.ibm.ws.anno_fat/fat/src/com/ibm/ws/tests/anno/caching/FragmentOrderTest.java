6b196c694203a10e20227b73534c25a96f1e779a
Jim Blye <jimblye@us.ibm.com>
2020-10-06 15:24:35 -0400

    Change how web fragment absolute ordering test app is updated

--- dev/com.ibm.ws.anno_fat/fat/src/com/ibm/ws/tests/anno/caching/FragmentOrderTest.java
+++ dev/com.ibm.ws.anno_fat/fat/src/com/ibm/ws/tests/anno/caching/FragmentOrderTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018,2019 IBM Corporation and others.
+ * Copyright (c) 2018,2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,8 +13,11 @@ package com.ibm.ws.tests.anno.caching;
 import java.util.logging.Logger;
 
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import componenttest.topology.utils.FileUtils;
 
 /**
  * These tests to verify that updates to the absolute-ordering element in the web.xml
@@ -26,15 +29,36 @@ public class FragmentOrderTest extends AnnoCachingTest {
     enum ServerStartType { 
         NONE, CLEAN, DIRTY; 
     }
+    
+    public static boolean UPDATE_APP = true;
+    public static boolean DONT_UPDATE_APP = false;
+    public static String tempExpandedEarPath;      // Path of an expanded copy of the EAR
+    public static String tempWarPath;              // Path to the WAR file inside the expanded copy of the EAR
+    public static String backupEarPath;            // Path to a backup copy of the EAR
 
-    private void handleStart(String altWebXml, ServerStartType startType) throws Exception {
-        useAlternateExpandedWebXml(altWebXml);
-
-        // When the server restarts, it re-expands the .ear file into the expanded app.
-        // So it will wipe out the change we just made.  To prevent that we need to 
-        // replace the .ear file file with the contents of the expanded app directory tree.
-
-        unexpandApp(); // TODO: This is a temporary workaround.
+    /**
+     * Optionally updates the app, then depending on the start type, it eithe
+     * 
+     * @param altWebXml
+     * @param startType
+     * @throws Exception
+     */
+    private void handleStart(String altWebXml, ServerStartType startType, boolean updateApp) throws Exception {
+    	
+    	if (!updateApp && (altWebXml != null) ) {
+    		LOG.info("handleStart: ");
+    		throw new IllegalStateException("Don't need to update app, but altWebXml is supplied - Probably not what was intented: [" + altWebXml + "]");
+    		
+    	} else if (updateApp && (altWebXml == null) ) {
+    		LOG.info("handleStart: ");
+    		throw new IllegalStateException("Want to update app, but altWebXml not supplied.");		
+    	} 
+    	
+        if (updateApp) {
+        	displayWebXmlFromExpandedApp(altWebXml);
+    		copyEntryWithinZip("/WEB-INF/" + altWebXml, "/WEB-INF/web.xml", tempWarPath);
+    		replaceEarFromExpandedEar(tempExpandedEarPath, getApplicationPath());
+    	}
 
         if ( startType == ServerStartType.CLEAN ) {
         	startServerScrub();
@@ -90,10 +114,32 @@ public class FragmentOrderTest extends AnnoCachingTest {
         LOG.info("Server started in milliseconds: " + elapsed / 1000000);        
 
         stopServer();
+        
+        // Create a working directory for updating app for each test.  Flow will be to update
+        // the temp expanded EAR and then zip to replace the installed EAR file.
+        // Why not just update in .../apps/expanded folder? Changes to the expanded app might
+        // be overwritten, depending on the autoExpand setting, when the application restarts.
+        // So we just replace the ear, and don't worry about whether it auto expands.
+		tempExpandedEarPath = getTempDir() + "/expanded/TestServlet40.ear";
+        mkDir(tempExpandedEarPath, DELETE_IF_EXISTS);
+        
+    	tempWarPath = tempExpandedEarPath + "/TestServlet40.war";
+        
+        backupEarPath = getTempDir() + "/expanded/TestServlet40.original.ear";
+        makeBackupCopyOfEar(getApplicationPath(), backupEarPath);		
 
         LOG.info("setUp EXIT");
     }
-
+    
+    /**
+     * Replace the application EAR from the backup copy before each test
+     * @throws Exception
+     */
+    @Before
+    public void setup2() throws Exception {    
+    	expandEarToTemp(backupEarPath, tempExpandedEarPath, DELETE_IF_EXISTS);
+    }
+    	
     @AfterClass
     public static void cleanUp() throws Exception {
         LOG.info("cleanUp ENTRY");
@@ -234,7 +280,7 @@ public class FragmentOrderTest extends AnnoCachingTest {
     private void tryWith_A_B(ServerStartType serverStartType, ServerStartType stopType, String msg) throws Exception {
         logBlock(msg + " - Server start type -> " +  serverStartType);        
 
-        handleStart("web-absolute-ordering-a-b.xml", serverStartType);
+        handleStart("web-absolute-ordering-a-b.xml", serverStartType, UPDATE_APP );
 
         // MyServlet should work because WEB-INF/classes is never excluded.
         verifyResponse("/TestServlet40/MyServlet", "Hello World");
@@ -278,7 +324,7 @@ public class FragmentOrderTest extends AnnoCachingTest {
     private void tryWith_A_B_C(ServerStartType serverStartType, String msg) throws Exception {
         logBlock(msg + " - Server start type -> " +  serverStartType);
 
-        handleStart("web-absolute-ordering-a-b-c.xml", serverStartType);
+        handleStart("web-absolute-ordering-a-b-c.xml", serverStartType, UPDATE_APP);
 
         // MyServlet should work because WEB-INF/classes is never excluded.
         verifyResponse("/TestServlet40/MyServlet", "Hello World");
@@ -329,7 +375,7 @@ public class FragmentOrderTest extends AnnoCachingTest {
     private void tryWith_A_B_Others(ServerStartType serverStartType, String msg) throws Exception {
         logBlock(msg + " - Server start type -> " +  serverStartType);
 
-        handleStart("web-absolute-ordering-a-b-others.xml", serverStartType);
+        handleStart("web-absolute-ordering-a-b-others.xml", serverStartType, UPDATE_APP);
 
         // MyServlet should work because WEB-INF/classes is never excluded.
         verifyResponse("/TestServlet40/MyServlet", "Hello World");
@@ -389,7 +435,7 @@ public class FragmentOrderTest extends AnnoCachingTest {
     private void tryWith_C_D_A_B_Others(ServerStartType serverStartType, String msg) throws Exception {
         logBlock(msg + " - Server start type -> " +  serverStartType);
 
-        handleStart("web-absolute-ordering-c-d-a-b-others.xml", serverStartType);
+        handleStart("web-absolute-ordering-c-d-a-b-others.xml", serverStartType, UPDATE_APP );
 
         // MyServlet should work because WEB-INF/classes is never excluded.
         verifyResponse("/TestServlet40/MyServlet", "Hello World");
@@ -445,71 +491,77 @@ public class FragmentOrderTest extends AnnoCachingTest {
     /**
      * Test with "A", "B", and "others".  Start by removing the jar for "D", which
      * would be handled under "others".  Then re-add the jar for "D".  The content from
-     * "D" should disappear when its jar is removed, then re-appear when its jar is
+     * servlet "D" should disappear when its jar is removed, then re-appear when its jar is
      * re-added.
      */
     private void tryRemovingAndAddingAJar(ServerStartType serverStartType, String msg) throws Exception {
-        logBlock(msg + " - Server start type -> " +  serverStartType);        
+    	logBlock(msg + " - Server start type -> " +  serverStartType);        
+    	String altWebXml = "web-absolute-ordering-a-b-others.xml";
+    	LOG.info("Remove TestServletD.jar from the application");
+    	displayWebXmlFromExpandedApp(altWebXml);
 
-        LOG.info("Remove TestServletD.jar from the application");
+    	copyEntryWithinZip("/WEB-INF/" + altWebXml, "/WEB-INF/web.xml", tempWarPath);
+    	renameEntryInZip("/WEB-INF/lib/TestServletD.jar", "/WEB-INF/lib/TestServletD.jar_backup", tempWarPath);
+    	replaceEarFromExpandedEar(tempExpandedEarPath, getApplicationPath());
 
-        renameJarFileInApplication("TestServlet40.war",
-                                   "TestServletD.jar", "TestServletD.jar_backup");
+    	handleStart(null, serverStartType, DONT_UPDATE_APP );
 
-        handleStart("web-absolute-ordering-a-b-others.xml", serverStartType);
+    	// We can't use the standard "A", "B", "others" variation, since
+    	// that expects "D", which is not present.
 
-        // We can't use the standard "A", "B", "others" variation, since
-        // that expects "D", which is not present.
+    	// MyServlet should work because WEB-INF/classes is never excluded.
+    	verifyResponse("/TestServlet40/MyServlet", "Hello World");
 
-        // MyServlet should work because WEB-INF/classes is never excluded.
-        verifyResponse("/TestServlet40/MyServlet", "Hello World");
+    	String[] expectedResponses = new String[4];
+    	String[] unExpectedResponses = {};
 
-        String[] expectedResponses = new String[4];
-        String[] unExpectedResponses = {};
+    	// Servlet A
+    	expectedResponses[0] = "Hello From Servlet A"; 
+    	expectedResponses[1] = "SCI A actually ran!";
+    	expectedResponses[2] = "Listener A actually ran!";
+    	expectedResponses[3] = "Listener order [ AB";  // C&D are included by "others".  Order is undefined for those.
 
-        // Servlet A
-        expectedResponses[0] = "Hello From Servlet A"; 
-        expectedResponses[1] = "SCI A actually ran!";
-        expectedResponses[2] = "Listener A actually ran!";
-        expectedResponses[3] = "Listener order [ AB";  // C&D are included by "others".  Order is undefined for those.
+    	verifyResponse("/TestServlet40/ServletA", expectedResponses, unExpectedResponses);
 
-        verifyResponse("/TestServlet40/ServletA", expectedResponses, unExpectedResponses);
+    	// Servlet B
+    	expectedResponses[0] = "Hello From Servlet B"; 
+    	expectedResponses[1] = "SCI B actually ran!";
+    	expectedResponses[2] = "Listener B actually ran!";
+    	expectedResponses[3] = "Listener order [ AB";  // C&D are included by "others".  Order is undefined for those.
 
-        // Servlet B
-        expectedResponses[0] = "Hello From Servlet B"; 
-        expectedResponses[1] = "SCI B actually ran!";
-        expectedResponses[2] = "Listener B actually ran!";
-        expectedResponses[3] = "Listener order [ AB";  // C&D are included by "others".  Order is undefined for those.
+    	verifyResponse("/TestServlet40/ServletB",  expectedResponses, unExpectedResponses);
 
-        verifyResponse("/TestServlet40/ServletB",  expectedResponses, unExpectedResponses);
+    	// Servlet C
+    	expectedResponses[0] = "Hello From Servlet C";
+    	expectedResponses[1] = "SCI C actually ran!";
+    	expectedResponses[2] = "Listener C actually ran!";
+    	expectedResponses[3] = "Listener order [ AB";  // C&D are included by "others".  Order is undefined for those.
+    	verifyResponse("/TestServlet40/ServletC",  expectedResponses, unExpectedResponses);
 
-        // Servlet C
-        expectedResponses[0] = "Hello From Servlet C";
-        expectedResponses[1] = "SCI C actually ran!";
-        expectedResponses[2] = "Listener C actually ran!";
-        expectedResponses[3] = "Listener order [ AB";  // C&D are included by "others".  Order is undefined for those.
-        verifyResponse("/TestServlet40/ServletC",  expectedResponses, unExpectedResponses);
+    	// Servlet D
+    	verifyBadUrl("/TestServlet40/ServletD");   
 
-        // Servlet D
-        verifyBadUrl("/TestServlet40/ServletD");   
+    	// SimpleTestServlet
+    	expectedResponses[0] = "Hello World";
+    	expectedResponses[1] = "SCI says Hi";
+    	expectedResponses[2] = "";
+    	expectedResponses[3] = "";
+    	verifyResponse("/TestServlet40/SimpleTestServlet",  expectedResponses, unExpectedResponses);         
 
-        // SimpleTestServlet
-        expectedResponses[0] = "Hello World";
-        expectedResponses[1] = "SCI says Hi";
-        expectedResponses[2] = "";
-        expectedResponses[3] = "";
-        verifyResponse("/TestServlet40/SimpleTestServlet",  expectedResponses, unExpectedResponses);         
+    	handleStop(serverStartType);
 
-        handleStop(serverStartType);
 
-        LOG.info("Add TestServletD.jar back into the application");
+    	LOG.info("Add TestServletD.jar back into the application");
 
-        renameJarFileInApplication("TestServlet40.war", "TestServletD.jar_backup", "TestServletD.jar");
+    	// This will just restore the TestServletD.jar in the temp expanded EAR.   Then we call the
+    	// tryWith_A_B_Others which will zip the temp expanded EAR, replacing the installed EAR.
+    	renameEntryInZip("/WEB-INF/lib/TestServletD.jar_backup", "/WEB-INF/lib/TestServletD.jar", tempWarPath);
 
-        // We should be back to the standard "A", "B", "others" variation.
+    	// We should be back to the standard "A", "B", "others" variation.
 
-        tryWith_A_B_Others(serverStartType, "Test with web.xml with ordering A B others");
- 
-        LOG.info("RETURN");
+    	tryWith_A_B_Others(serverStartType, "Test with web.xml with ordering A B others");
+
+
+    	LOG.info("RETURN");
     }
 }
