8dc14e344eabe9f0c851d847b769b65e33cf352d
Felix Wong <fmhwong@ca.ibm.com>
2020-10-19 11:30:12 -0400

    Add debug

--- dev/com.ibm.ws.microprofile.opentracing.jaeger.adapter.impl/src/com/ibm/ws/microprofile/opentracing/jaeger/adapter/impl/LRCScopeManager.java
+++ dev/com.ibm.ws.microprofile.opentracing.jaeger.adapter.impl/src/com/ibm/ws/microprofile/opentracing/jaeger/adapter/impl/LRCScopeManager.java
@@ -1,8 +1,19 @@
-package com.ibm.ws.microprofile.opentracing.jaeger.adapter.impl;
+/*******************************************************************************
+ * Copyright (c) 2020 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/package com.ibm.ws.microprofile.opentracing.jaeger.adapter.impl;
 
 import java.util.HashMap;
 
 import com.ibm.websphere.logging.hpel.LogRecordContext;
+import com.ibm.websphere.ras.Tr;
+import com.ibm.websphere.ras.TraceComponent;
 
 import io.opentracing.Scope;
 import io.opentracing.ScopeManager;
@@ -11,6 +22,7 @@ import io.opentracing.util.ThreadLocalScopeManager;
 
 public class LRCScopeManager implements ScopeManager {
     
+    private static final TraceComponent tc = Tr.register(LRCScopeManager.class);
     private ScopeManager scopeManager = new ThreadLocalScopeManager();
     private static final String LRC_TRACE_ID_KEY = "traceId";
     private static final String LRC_SPAN_ID_KEY = "spanId";
@@ -34,6 +46,12 @@ public class LRCScopeManager implements ScopeManager {
             this.scope = scope;
             this.previousTraceId = getLRCValue(LRC_TRACE_ID_KEY);
             this.previousSpanId = getLRCValue(LRC_SPAN_ID_KEY);
+            if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+                Tr.debug(tc, "previousTraceId=" + previousTraceId);
+                Tr.debug(tc, "previousSpanId=" + previousSpanId);
+                Tr.debug(tc, "TraceId=" + span.context().toTraceId());
+                Tr.debug(tc, "SpanId=" + span.context().toSpanId());
+            }
             LogRecordContext.addExtension(LRC_TRACE_ID_KEY, span.context().toTraceId());
             LogRecordContext.addExtension(LRC_SPAN_ID_KEY, span.context().toSpanId());
         }
@@ -42,10 +60,26 @@ public class LRCScopeManager implements ScopeManager {
         public void close() {
             this.scope.close();
             if (previousTraceId != null) {
+                if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+                    Tr.debug(tc, "restore previous traceId=" + previousTraceId);
+                }
                 LogRecordContext.addExtension(LRC_TRACE_ID_KEY, previousTraceId);
+            } else {
+                if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+                    Tr.debug(tc, "remove traceId");
+                }
+                LogRecordContext.removeExtension(LRC_TRACE_ID_KEY);
             }
             if (previousSpanId != null) {
+                if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+                    Tr.debug(tc, "restore previous spanId=" + previousSpanId);
+                }
                 LogRecordContext.addExtension(LRC_SPAN_ID_KEY, previousSpanId);
+            } else {
+                if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+                    Tr.debug(tc, "remove spanId");
+                }
+                LogRecordContext.removeExtension(LRC_SPAN_ID_KEY);
             }
         }
      
