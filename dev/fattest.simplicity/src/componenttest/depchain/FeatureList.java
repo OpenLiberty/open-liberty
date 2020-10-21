dba1291a9ca451a8e481513d20b77a8093b40f98
Andrew Guibert <andy.guibert@gmail.com>
2020-10-21 12:31:55 -0500

    Fix synchronization issue starting 2 LibertyServer instances in parallel

--- dev/fattest.simplicity/src/componenttest/depchain/FeatureList.java
+++ dev/fattest.simplicity/src/componenttest/depchain/FeatureList.java
@@ -13,6 +13,8 @@ package componenttest.depchain;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import com.ibm.websphere.simplicity.log.Log;
 
@@ -22,32 +24,44 @@ public class FeatureList {
 
     private static final Class<?> c = FeatureList.class;
 
+    private static final String FAT_FEATURE_LIST = "fatFeatureList.xml";
+
     private static File featureList = null;
+    private static final AtomicBoolean generated = new AtomicBoolean();
 
     @SuppressWarnings("resource")
     public static File get(LibertyServer server) throws Exception {
         final String m = "createFeatureList";
 
-        featureList = new File(server.getUserDir() + "/servers", "fatFeatureList.xml");
-        if (featureList.exists())
+        featureList = new File(server.getUserDir() + "/servers", FAT_FEATURE_LIST);
+
+        if (featureList.exists() && generated.get())
             return featureList;
 
-        // If fat-featureList.xml doesn't exist already, generate it
-        Log.info(c, m, "fatFeatureList.xml not found.  Need to generate.");
-        String featureListJar = findRunnableJar(server.getInstallRoot());
-        Process featureListProc = new ProcessBuilder("java", "-jar", featureListJar, featureList.getAbsolutePath())
-                        .redirectErrorStream(true)
-                        .start();
-        int rc = featureListProc.waitFor();
-        String cmdOutput;
-        try (Scanner s = new Scanner(featureListProc.getInputStream()).useDelimiter("\\A")) {
-            cmdOutput = s.hasNext() ? s.next() : "";
+        synchronized (FeatureList.class) {
+            if (featureList.exists())
+                return featureList;
+
+            // If fatFeatureList.xml doesn't exist already, generate it
+            Log.info(c, m, FAT_FEATURE_LIST + " not found.  Need to generate.");
+            String featureListJar = findRunnableJar(server.getInstallRoot());
+            Process featureListProc = new ProcessBuilder("java", "-jar", featureListJar, featureList.getAbsolutePath())
+                            .redirectErrorStream(true)
+                            .start();
+            boolean completed = featureListProc.waitFor(45, TimeUnit.SECONDS);
+            if (!completed) {
+                String cmdOutput;
+                try (Scanner s = new Scanner(featureListProc.getInputStream()).useDelimiter("\\A")) {
+                    cmdOutput = s.hasNext() ? s.next() : "";
+                }
+                Exception e = new Exception(cmdOutput);
+                Log.error(c, m, e, "Process timed out running " + featureList.getAbsolutePath() + " generation:");
+                throw e;
+            }
+
+            generated.set(true);
         }
-        if (rc != 0) {
-            Exception e = new Exception(cmdOutput);
-            Log.error(c, m, e, "Got non-zero return code " + rc + " from running " + featureList.getAbsolutePath() + " generation:");
-            throw e;
-        }
+
         return featureList;
     }
 
@@ -65,9 +79,11 @@ public class FeatureList {
     }
 
     public static void reset() throws IOException {
+        Log.info(c, "reset", "Removing existing " + FAT_FEATURE_LIST);
+        generated.set(false);
         if (featureList != null)
             if (!featureList.delete())
-                throw new IOException("Unable to delete old featureList.xml at: " + featureList.getAbsolutePath());
+                throw new IOException("Unable to delete old " + FAT_FEATURE_LIST + " at: " + featureList.getAbsolutePath());
         featureList = null;
     }
 }
