ea48e2bef6f39bb98f1b738e0319d788e6df64e0
Nathan Mittlestat <nmittles@us.ibm.com>
2020-10-21 15:19:28 -0500

    Issue 12434: Enable CDI and EJB related tests for Jakarta Bean Validation 3.0.

--- dev/com.ibm.ws.beanvalidation.v20_fat/fat/src/com/ibm/ws/bval/v20/fat/BeanVal20Test.java
+++ dev/com.ibm.ws.beanvalidation.v20_fat/fat/src/com/ibm/ws/bval/v20/fat/BeanVal20Test.java
@@ -10,18 +10,34 @@
  *******************************************************************************/
 package com.ibm.ws.bval.v20.fat;
 
+import static com.ibm.websphere.simplicity.ShrinkHelper.addDirectory;
+import static com.ibm.websphere.simplicity.ShrinkHelper.buildDefaultApp;
+import static com.ibm.websphere.simplicity.ShrinkHelper.buildJavaArchive;
 import static com.ibm.websphere.simplicity.ShrinkHelper.defaultDropinApp;
+import static com.ibm.websphere.simplicity.ShrinkHelper.exportToServer;
 
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
+import com.ibm.websphere.simplicity.ShrinkHelper;
+
+import bval.v20.cdi.web.BeanValCDIServlet;
+import bval.v20.customprovider.CustomProviderTestServlet;
 import bval.v20.hibernateconfig.web.BeanValidationServlet;
+import bval.v20.multixml.web.BeanValidationTestServlet;
+import bval.v20.valueextractor.web.ValueExtractorServlet;
 import bval.v20.web.BeanVal20TestServlet;
 import componenttest.annotation.Server;
 import componenttest.annotation.TestServlet;
 import componenttest.annotation.TestServlets;
 import componenttest.custom.junit.runner.FATRunner;
+import componenttest.rules.repeater.JakartaEE9Action;
 import componenttest.topology.impl.LibertyServer;
 import componenttest.topology.utils.FATServletClient;
 
@@ -37,55 +53,55 @@ public class BeanVal20Test extends FATServletClient {
 
     private static final String CUSTOM_PROVIDER_CDI_WARNING = "CWNBV0200W.*" + CUSTOM_PROVIDER_APP;
 
-    @Server("beanval.v20.noEJBLite_fat")
+    @Server("beanval.v20_fat")
     @TestServlets({
                     @TestServlet(servlet = BeanVal20TestServlet.class, contextRoot = REG_APP),
-                    //TODO: Once CDI 3.0 and EJBLite 4.0 are ready, enable these four applications for Jakarta EE 9.
-//                    @TestServlet(servlet = BeanValCDIServlet.class, contextRoot = CDI_APP),
-//                    @TestServlet(servlet = BeanValidationTestServlet.class, contextRoot = MULTI_VAL_APP),
-//                    @TestServlet(servlet = ValueExtractorServlet.class, contextRoot = VAL_EXT_APP),
+                    @TestServlet(servlet = BeanValCDIServlet.class, contextRoot = CDI_APP),
+                    @TestServlet(servlet = BeanValidationTestServlet.class, contextRoot = MULTI_VAL_APP),
+                    @TestServlet(servlet = ValueExtractorServlet.class, contextRoot = VAL_EXT_APP),
                     @TestServlet(servlet = BeanValidationServlet.class, contextRoot = HIBERNATE_DEFAULT_APP),
-//                    @TestServlet(servlet = CustomProviderTestServlet.class, contextRoot = CUSTOM_PROVIDER_APP)
+                    @TestServlet(servlet = CustomProviderTestServlet.class, contextRoot = CUSTOM_PROVIDER_APP)
     })
     public static LibertyServer server;
 
     @BeforeClass
     public static void setUp() throws Exception {
 
-        //TODO: Delete these two lines once CDI 3.0 and EJBLite 4.0 are ready
-        server.deleteAllDropinApplications();
-        server.removeAllInstalledAppsForValidation();
+        JavaArchive multipleValidationXmlEjb1_jar = buildJavaArchive("MultipleValidationXmlEjb1.jar", "bval.v20.ejb1.*");
+        JavaArchive multipleValidationXmlEjb2_jar = buildJavaArchive("MultipleValidationXmlEjb2.jar", "bval.v20.ejb2.*");
+        WebArchive multipleValidationXmlWeb_war = buildDefaultApp("MultipleValidationXmlWeb.war", "bval.v20.multixml.*");
 
-        //TODO: Once CDI 3.0 and EJBLite 4.0 are ready, enable these four applications for Jakarta EE 9.
-//        EnterpriseArchive multiValXmlEar = ShrinkWrap.create(EnterpriseArchive.class, "MultipleValidationXmlEjb.ear")
-//                        .addAsModule(buildJavaArchive("MultipleValidationXmlEjb1.jar", "bval.v20.ejb1.*"))
-//                        .addAsModule(buildJavaArchive("MultipleValidationXmlEjb2.jar", "bval.v20.ejb2.*"))
-//                        .addAsModule(buildDefaultApp("MultipleValidationXmlWeb.war", "bval.v20.multixml.*"));
-//        addDirectory(multiValXmlEar, "test-applications/MultipleValidationXmlEjb.ear/resources");
-//        exportToServer(server, "dropins", multiValXmlEar);
-//        defaultDropinApp(server, CDI_APP, "bval.v20.cdi.web");
+        if (JakartaEE9Action.isActive()) {
+            multipleValidationXmlEjb1_jar.move("/META-INF/constraints/constraints-house_EE9.xml", "/META-INF/constraints/constraints-house.xml");
+            multipleValidationXmlEjb2_jar.move("/META-INF/constraints/constraints-house_EE9.xml", "/META-INF/constraints/constraints-house.xml");
+            multipleValidationXmlWeb_war.move("/WEB-INF/constraints3/constraints-house_EE9.xml", "/WEB-INF/constraints3/constraints-house.xml");
+        }
+        EnterpriseArchive multiValXmlEar = ShrinkWrap.create(EnterpriseArchive.class, "MultipleValidationXmlEjb.ear")
+                        .addAsModule(multipleValidationXmlEjb1_jar)
+                        .addAsModule(multipleValidationXmlEjb2_jar)
+                        .addAsModule(multipleValidationXmlWeb_war);
+        addDirectory(multiValXmlEar, "test-applications/MultipleValidationXmlEjb.ear/resources");
+        exportToServer(server, "dropins", multiValXmlEar);
 
         defaultDropinApp(server, REG_APP, "bval.v20.web");
-//        defaultDropinApp(server, VAL_EXT_APP, "bval.v20.valueextractor.web");
+        defaultDropinApp(server, CDI_APP, "bval.v20.cdi.web");
+        defaultDropinApp(server, VAL_EXT_APP, "bval.v20.valueextractor.web");
         defaultDropinApp(server, HIBERNATE_DEFAULT_APP, "bval.v20.hibernateconfig.web");
-//        WebArchive war = ShrinkHelper.buildDefaultApp(CUSTOM_PROVIDER_APP, "bval.v20.customprovider");
-//        ShrinkHelper.exportDropinAppToServer(server, war);
-//        defaultDropinApp(server, CUSTOM_PROVIDER_APP, "bval.v20.customprovider");
+        WebArchive war = ShrinkHelper.buildDefaultApp(CUSTOM_PROVIDER_APP, "bval.v20.customprovider");
+        ShrinkHelper.exportDropinAppToServer(server, war);
+        defaultDropinApp(server, CUSTOM_PROVIDER_APP, "bval.v20.customprovider");
 
         server.startServer();
 
-//      TODO: re-enable the CUSTOM_PROVIDER_CDI_WARNING once CDI 3.0 is enabled for Jakarta EE 9
-//        // Verify that we get CWNBV0200W for CUSTOM_PROVIDER_APP at deployment time
-//        Assert.assertNotNull("Expected to find warning at server start time: " + CUSTOM_PROVIDER_CDI_WARNING,
-//                             server.waitForStringInLog(CUSTOM_PROVIDER_CDI_WARNING));
+        // Verify that we get CWNBV0200W for CUSTOM_PROVIDER_APP at deployment time
+        Assert.assertNotNull("Expected to find warning at server start time: " + CUSTOM_PROVIDER_CDI_WARNING,
+                             server.waitForStringInLog(CUSTOM_PROVIDER_CDI_WARNING));
     }
 
     @AfterClass
     public static void tearDown() throws Exception {
         // Stop the server but ignore any ConstraintViolationExceptions thrown by validation tests
-        server.stopServer("ConstraintViolationException");
-//        TODO: re-enable the CUSTOM_PROVIDER_CDI_WARNING once CDI 3.0 is enabled for Jakarta EE 9
-//        server.stopServer("ConstraintViolationException",
-//                          CUSTOM_PROVIDER_CDI_WARNING);
+        server.stopServer("ConstraintViolationException",
+                          CUSTOM_PROVIDER_CDI_WARNING);
     }
 }
