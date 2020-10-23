c643a016ac7fb1da06f8adcd6636ad5546235775
Andy McCright <j.andrew.mccright@gmail.com>
2020-10-22 20:56:17 -0500

    Use App BDAs with MP GraphQL (allows removal of overlay GraphQLConfig)

--- dev/com.ibm.ws.io.smallrye.graphql/src/com/ibm/ws/io/smallrye/graphql/component/GraphQLExtension.java
+++ dev/com.ibm.ws.io.smallrye.graphql/src/com/ibm/ws/io/smallrye/graphql/component/GraphQLExtension.java
@@ -41,6 +41,7 @@ import org.osgi.service.component.annotations.ConfigurationPolicy;
 immediate = true,
 property = { "api.classes=org.eclipse.microprofile.graphql.GraphQLApi",
              "bean.defining.annotations=org.eclipse.microprofile.graphql.GraphQLApi",
+             "application.bdas.visible=true",
              "service.vendor=IBM" })
 public class GraphQLExtension implements Extension, WebSphereCDIExtension {
     private static final Logger LOG = Logger.getLogger(GraphQLExtension.class.getName());
