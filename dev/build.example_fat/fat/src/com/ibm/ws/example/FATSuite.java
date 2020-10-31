4bffe0c006bdd232b15d844d87c41fdbab67635c
Andrew Guibert <andy.guibert@gmail.com>
2020-10-31 13:55:17 -0500

    Only run example FAT once in LITE mode

--- dev/build.example_fat/fat/src/com/ibm/ws/example/FATSuite.java
+++ dev/build.example_fat/fat/src/com/ibm/ws/example/FATSuite.java
@@ -26,11 +26,12 @@ import componenttest.rules.repeater.RepeatTests;
 public class FATSuite {
 
     // Using the RepeatTests @ClassRule will cause all tests to be run three times.
-    // First without any modifications, then again with all features upgraded to their EE8 equivalents,
-    // and finally again with all features _and applications_ upgrade to Jakarta EE 9 equivalents
+    // 1) without any modifications
+    // 2) [FULL mode only] again with all features upgraded to their EE8 equivalents
+    // 3) [FULL mode only] again with all features _and applications_ upgrade to Jakarta EE 9 equivalents
     @ClassRule
     public static RepeatTests r = RepeatTests.withoutModification()
-                    .andWith(FeatureReplacementAction.EE8_FEATURES())
-                    .andWith(new JakartaEE9Action());
+                    .andWith(FeatureReplacementAction.EE8_FEATURES().fullFATOnly())
+                    .andWith(new JakartaEE9Action().fullFATOnly());
 
 }
