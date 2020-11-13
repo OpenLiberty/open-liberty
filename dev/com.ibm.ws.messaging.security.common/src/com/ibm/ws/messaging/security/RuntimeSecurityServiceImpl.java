e9a54bddc585e4f56b7217ca9b611e6f63c21342
Andrew D Banks <andrew_banks@uk.ibm.com>
2020-11-13 17:21:30 +0000

    Add diagnostics

--- dev/com.ibm.ws.messaging.security.common/src/com/ibm/ws/messaging/security/RuntimeSecurityServiceImpl.java
+++ dev/com.ibm.ws.messaging.security.common/src/com/ibm/ws/messaging/security/RuntimeSecurityServiceImpl.java
@@ -13,11 +13,11 @@ package com.ibm.ws.messaging.security;
 
 import javax.security.auth.Subject;
 
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
 
 import com.ibm.websphere.ras.TraceComponent;
 import com.ibm.ws.messaging.security.authentication.MessagingAuthenticationException;
@@ -25,11 +25,11 @@ import com.ibm.ws.sib.utils.ras.SibTr;
 
 /**
   * There is always exactly one RuntimeSecurityService
-  * If MessagingSecurity is not defined then a default (always pass) authentication and authorization 
-  * service is used.   
- * 
+  * If MessagingSecurity is not defined then a default (always pass) authentication and authorization
+  * service is used.
+ *
  * @author Sharath Chandra B
- * 
+ *
  */
 @Component(configurationPid = "com.ibm.ws.messaging.security.RuntimeSecurityServiceImpl",
            configurationPolicy = ConfigurationPolicy.REQUIRE,
@@ -45,28 +45,35 @@ public class RuntimeSecurityServiceImpl implements RuntimeSecurityService {
     // Absolute class name along with the package used for tracing
     private static final String CLASS_NAME = "com.ibm.ws.messaging.security.RuntimeSecurityService";
 
-    // The active MessagingSecurityService or null if no MessagingSecurity is defined. 
+    // The active MessagingSecurityService or null if no MessagingSecurity is defined.
     private MessagingSecurityService messagingSecurityService = null;
     // Containers for the Authentication and Authorization services provided by the MessagingSecurityService.
     private final Authentication authentication = new Authentication(this);
     private final Authorization authorization = new Authorization(this);
 
     private Subject unauthenticatedSubject = null;
-  
+
     public RuntimeSecurityServiceImpl() {
         final String methodName = "RuntimeSecurityServiceImpl";
         if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled())
           SibTr.entry(tc, methodName, new Object[] {this});
-        
-        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) 
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled())
             SibTr.exit(tc, methodName);
     }
 
-    /** 
-     * Start using the MessagingSecurityService 
-     * 
+    @Activate
+    void activate() {
+        final String methodName = "activate";
+        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled())
+          SibTr.debug(tc, methodName, new Object[] {this, messagingSecurityService==null});
+    }
+
+    /**
+     * Start using the MessagingSecurityService
+     *
      * If the MessagingSecurityService is defined, prevent this service from starting until it is available.
-     * 
+     *
      * @param messagingSecurityService which has been defined.
      */
     //TODO Note that security is not ReferencePolicy.DYNAMIC, it cannot be created or destroyed the while messaging is started.
@@ -75,40 +82,20 @@ public class RuntimeSecurityServiceImpl implements RuntimeSecurityService {
         final String methodName = "setMessagingSecurityService";
         if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled())
           SibTr.entry(tc, methodName, new Object[] {this, messagingSecurityService});
-    
+
         this.messagingSecurityService = messagingSecurityService;
         authentication.setMessagingAuthenticationService(messagingSecurityService.getMessagingAuthenticationService());
         authorization.setMessagingAuthorizationService(messagingSecurityService.getMessagingAuthorizationService());
-        
-        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) 
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled())
           SibTr.exit(tc, methodName);
     }
-    
-    /**
-     * The MessagingSecurityService has been removed, stop using it.
-     * @param messagingSecurityService
-     */
-    protected void unsetMessagingSecurityService(MessagingSecurityService messagingSecurityService) {
-        final String methodName = "unsetMessagingSecurityService";
-        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled())
-          SibTr.entry(tc, methodName, new Object[] {this, this.messagingSecurityService, messagingSecurityService});
-    
-        if (this.messagingSecurityService != messagingSecurityService)
-            throw new Error(methodName + " MessageingSecurityService:" + this.messagingSecurityService + " != "+ messagingSecurityService);
-        
-        this.messagingSecurityService = null;
-        authentication.setMessagingAuthenticationService(null);
-        authorization.setMessagingAuthorizationService(null);
-        
-        if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) 
-          SibTr.exit(tc, methodName, messagingSecurityService);
-    }
-    
+
     /**
      * Check if Messaging Security is enabled or not.
      * This is determined by the existence of the MessagingSecurityService.
      * If MessagingSecurity Service is not null, MessagingSecurity is enabled
-     * 
+     *
      * @return
      *         true : If MessasingSecurity is enabled
      *         false: If MessagingSecurity is disabled
@@ -132,7 +119,7 @@ public class RuntimeSecurityServiceImpl implements RuntimeSecurityService {
      * Create a Unauthenticated Subject
      * When Messaging Security is disabled, we will create a blank subject
      * This should be called only when Messaging Security is disabled
-     * 
+     *
      * @return
      *         Subject: A Unauthenticated Subject
      */
@@ -150,7 +137,7 @@ public class RuntimeSecurityServiceImpl implements RuntimeSecurityService {
 
     /**
      * Get Proxy Authentication instance
-     * 
+     *
      * @return
      *         Authentication
      */
@@ -161,7 +148,7 @@ public class RuntimeSecurityServiceImpl implements RuntimeSecurityService {
 
     /**
      * Get Proxy Authorization instance
-     * 
+     *
      * @return
      */
     @Override
@@ -171,7 +158,7 @@ public class RuntimeSecurityServiceImpl implements RuntimeSecurityService {
 
     /**
      * Get Unique User name for the Subject
-     * 
+     *
      * @param subject
      * @return
      * @throws MessagingSecurityException
@@ -191,7 +178,7 @@ public class RuntimeSecurityServiceImpl implements RuntimeSecurityService {
      * <li> If Messaging Security is disabled, it returns false
      * <li> If Messaging Security is enabled, it calls MessagingAuthenticationService
      * from Messaging Security component </li>
-     * 
+     *
      * @param subject
      *            Subject which need to be checked
      * @return
@@ -215,5 +202,10 @@ public class RuntimeSecurityServiceImpl implements RuntimeSecurityService {
 
         SibTr.exit(tc, CLASS_NAME + "isUnauthenticated", subject);
         return result;
+    }
+
+    @Override
+    public String toString() {
+        return (messagingSecurityService == null?"-":"+") + super.toString();
     }
 }
