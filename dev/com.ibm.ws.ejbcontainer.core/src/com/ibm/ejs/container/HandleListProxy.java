e85994783bb19a734f371c80369ee07ed0744442
Nathan Rauh <nathan.rauh@us.ibm.com>
2020-11-18 16:15:27 -0600

    Issue #14758 missed update to ejb class

--- dev/com.ibm.ws.ejbcontainer.core/src/com/ibm/ejs/container/HandleListProxy.java
+++ dev/com.ibm.ws.ejbcontainer.core/src/com/ibm/ejs/container/HandleListProxy.java
@@ -13,22 +13,77 @@ package com.ibm.ejs.container;
 import com.ibm.ejs.j2c.HandleListInterface;
 import com.ibm.ws.jca.cm.handle.HandleList;
 
-// TODO still need to port the implementation to Liberty
+/**
+ * This is a HandleList proxy that is pushed onto the
+ * {@link com.ibm.ws.threadContext.ConnectionHandleAccessorImpl} thread context
+ * for beans that don't yet have an associated HandleList. This class uses
+ * {@link EJBThreadData#getCallbackBeanO()} to determine the current bean, so
+ * the callback bean must always be current before pushing this proxy.
+ *
+ * <p>This proxy is used to avoid the per-bean memory overhead of a HandleList
+ * until a handle needs to be tracked. Since HandleList is only used for
+ * non-smart handles and all Liberty connection factories support smart handles,
+ * the majority of beans will never need a HandleList.
+ */
 class HandleListProxy implements HandleListInterface {
-    public static final HandleListInterface INSTANCE = null;
+    public static final HandleListInterface INSTANCE = new HandleListProxy();
 
-    @Override
-    public HandleList addHandle(Handle handle) {
-        return null;
+    private HandleListProxy() {
     }
 
     @Override
-    public void parkHandle() {}
+    public String toString() {
+        BeanO beanO = EJSContainer.getCallbackBeanO();
+        return super.toString() + '[' + beanO.getHandleList(false) + ", " + beanO + ']';
+    }
+
+    /**
+     * Gets or optionally creates a handle list for the current callback bean.
+     * If the current callback bean does not have a HandleList and
+     * {@code create} is {@code false}, then {@code null} is returned.
+     *
+     * @param create {@code true} if a HandleList must be created
+     * @return the HandleList or {@code null}
+     */
+    private HandleList getHandleList(boolean create) {
+        BeanO beanO = EJSContainer.getCallbackBeanO();
+        return beanO.getHandleList(create);
+    }
 
     @Override
-    public void reAssociate() {}
+    public HandleList addHandle(HandleDetails a) {
+        HandleList hl = getHandleList(true);
+
+        hl.addHandle(a);
+        return hl;
+    }
 
     @Override
-    public void removeHandle(Object handle) {
+    public HandleDetails removeHandle(Object handle) {
+        HandleList hl = getHandleList(false);
+
+        if (hl != null) {
+            return hl.removeHandle(handle);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void reAssociate() {
+        HandleList hl = getHandleList(false);
+
+        if (hl != null) {
+            hl.reAssociate();
+        }
+    }
+
+    @Override
+    public void parkHandle() {
+        HandleList hl = getHandleList(false);
+
+        if (hl != null) {
+            hl.parkHandle();
+        }
     }
 }
