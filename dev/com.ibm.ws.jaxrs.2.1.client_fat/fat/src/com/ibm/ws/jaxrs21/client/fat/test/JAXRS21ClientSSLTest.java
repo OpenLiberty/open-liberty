d58f9c1f910eff61ef022e93cf184a7d0dca895c
John Koehler <jkoehler@us.ibm.com>
2020-11-24 07:37:10 -0600

    Issue

--- dev/com.ibm.ws.jaxrs.2.1.client_fat/fat/src/com/ibm/ws/jaxrs21/client/fat/test/JAXRS21ClientSSLTest.java
+++ dev/com.ibm.ws.jaxrs.2.1.client_fat/fat/src/com/ibm/ws/jaxrs21/client/fat/test/JAXRS21ClientSSLTest.java
@@ -26,10 +26,12 @@ import org.junit.runner.RunWith;
 import com.ibm.websphere.simplicity.ShrinkHelper;
 
 import componenttest.annotation.Server;
+import componenttest.annotation.SkipForRepeat;
 import componenttest.custom.junit.runner.FATRunner;
 import componenttest.topology.impl.LibertyServer;
 
 @RunWith(FATRunner.class)
+@SkipForRepeat("EE9_FEATURES") // currently broken due to multiple issues
 public class JAXRS21ClientSSLTest extends JAXRS21AbstractTest {
     @Server("jaxrs21.client.JAXRS21ClientSSLTest")
     public static LibertyServer server;
