bf291c87d12959fa813a8a8356c4ae2fdfda0e08
Adam Anderson <atanderson9383@gmail.com>
2020-11-24 22:05:11 -0600

    14934 - refactor com.ibm.ws.jaxrs.2.0.security to get the SSLSocketFactory from JSSEHelper

--- dev/com.ibm.ws.jaxrs.2.0.security/src/com/ibm/ws/jaxrs20/appsecurity/component/JaxRsAppSecurityServiceImpl.java
+++ dev/com.ibm.ws.jaxrs.2.0.security/src/com/ibm/ws/jaxrs20/appsecurity/component/JaxRsAppSecurityServiceImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 IBM Corporation and others.
+ * Copyright (c) 2014, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -22,7 +22,6 @@ import org.osgi.service.component.annotations.Component;
 
 import com.ibm.websphere.security.web.WebSecurityHelper;
 import com.ibm.ws.jaxrs20.api.JaxRsAppSecurityService;
-import com.ibm.ws.jaxrs20.appsecurity.security.JaxRsSSLManager;
 
 @Component(name = "com.ibm.ws.jaxrs20.JaxRsAppSecurityServiceImpl", property = { "service.vendor=IBM" })
 public class JaxRsAppSecurityServiceImpl implements JaxRsAppSecurityService {
@@ -48,9 +47,11 @@ public class JaxRsAppSecurityServiceImpl implements JaxRsAppSecurityService {
 
     @Override
     public SSLSocketFactory getSSLSocketFactory(String sslRef, Map<String, Object> props) {
+//        SSLSocketFactory sslSocketFactory = JaxRsSSLManager.getProxySSLSocketFactoryBySSLRef(sslRef, null);
+//        return sslSocketFactory;
 
-        SSLSocketFactory sslSocketFactory = JaxRsSSLManager.getProxySSLSocketFactoryBySSLRef(sslRef, null);
-        return sslSocketFactory;
+        // I don't think this ever gets called, but I don't want to re-factor the interface right now
+        return null;
     }
 
 }
