e76776685909113c9b0b21ef8642e45a6b7d917e
Andrew Guibert <andy.guibert@gmail.com>
2020-11-30 16:03:17 -0600

    More progress(ish) on Oracle with SSL

--- dev/com.ibm.ws.jdbc_fat_oracle/publish/files/oracle-ssl/setup/enable_ssl.sh
+++ dev/com.ibm.ws.jdbc_fat_oracle/publish/files/oracle-ssl/setup/enable_ssl.sh
@@ -7,17 +7,41 @@ if [ -z ${ORACLE_HOME} ]; then
     exit 1
 fi
 
+echo "Creating server wallet and cert"
+mkdir -p /u01/app/oracle/wallet
+orapki wallet create -wallet "/u01/app/oracle/wallet" -pwd WalletPasswd123 -auto_login_local
+orapki wallet add -wallet "/u01/app/oracle/wallet" -pwd WalletPasswd123 -dn "CN=localhost" -keysize 1024 -self_signed -validity 36500
+orapki wallet display -wallet "/u01/app/oracle/wallet" -pwd WalletPasswd123
+orapki wallet export -wallet "/u01/app/oracle/wallet" -pwd WalletPasswd123 -dn "CN=localhost" -cert /tmp/oracle-server-certificate.crt
+cat /tmp/oracle-server-certificate.crt
+
+echo "Create client wallet and cert"
+mkdir -p /client/oracle/wallet
+orapki wallet create -wallet "/client/oracle/wallet" -pwd WalletPasswd123 -auto_login_local
+orapki wallet add -wallet "/client/oracle/wallet" -pwd WalletPasswd123 -dn "CN=localhost" -keysize 1024 -self_signed -validity 36500
+orapki wallet display -wallet "/client/oracle/wallet" -pwd WalletPasswd123
+orapki wallet export -wallet "/client/oracle/wallet" -pwd WalletPasswd123 -dn "CN=localhost" -cert /tmp/oracle-client-certificate.crt
+cat /tmp/oracle-client-certificate.crt
+
+echo "Exchange certs"
+orapki wallet add -wallet "/client/oracle/wallet" -pwd WalletPasswd123 -trusted_cert -cert /tmp/oracle-server-certificate.crt
+orapki wallet display -wallet "/client/oracle/wallet" -pwd WalletPasswd123
+orapki wallet add -wallet "/u01/app/oracle/wallet" -pwd WalletPasswd123 -trusted_cert -cert /tmp/oracle-client-certificate.crt
+orapki wallet display -wallet "/u01/app/oracle/wallet" -pwd WalletPasswd123
+echo "Done exchanging certs"
+
+
 # Step 6A: Configure Kerberos on the Client and on the Database Server
 SQLNETORA="$ORACLE_HOME/network/admin/sqlnet.ora"
 touch $SQLNETORA
-# Tell oracle to use kerberos for authentication
-echo "SQLNET.AUTHENTICATION_SERVICES=(BEQ, TCPS)" >> $SQLNETORA
+# tried (BEQ, TCPS)
+# tried (TCPS,NTS) and it failed to boot
+echo "SQLNET.AUTHENTICATION_SERVICES=(BEQ,TCPS)" >> $SQLNETORA
 echo "SSL_CLIENT_AUTHENTICATION=FALSE" >> $SQLNETORA
 echo "SSL_VERSION=UNDETERMINED" >> $SQLNETORA
-#echo "WALLET_LOCATION=(SOURCE=(METHOD=FILE)(METHOD_DATA=(DIRECTORY=/server/wallet/path)))" >> $SQLNETORA
-
-# Increase inbround connection timeout in-case response from kerberos is slow
+echo "WALLET_LOCATION=(SOURCE=(METHOD=FILE)(METHOD_DATA=(DIRECTORY=/u01/app/oracle/wallet)))" >> $SQLNETORA
 echo "SQLNET.INBOUND_CONNECT_TIMEOUT=120" >> $SQLNETORA
+echo "SSL_CIPHER_SUITES = (SSL_RSA_WITH_AES_256_CBC_SHA, SSL_RSA_WITH_3DES_EDE_CBC_SHA, SSL_DH_anon_WITH_3DES_EDE_CBC_SHA, SSL_DH_anon_WITH_RC4_128_MD5, SSL_DH_anon_WITH_DES_CBC_SHA)" >> $SQLNETORA
 echo "UPDATED SQLNET.ORA"
 
 cat > $ORACLE_HOME/network/admin/listener.ora << END_OF_LISTENER
@@ -37,18 +61,14 @@ cat > $ORACLE_HOME/network/admin/listener.ora << END_OF_LISTENER
              (DESCRIPTION =
                (ADDRESS = (PROTOCOL = IPC)(KEY = EXTPROC_FOR_XE))
                (ADDRESS = (PROTOCOL = TCP)(HOST = 0.0.0.0)(PORT = 1521))
+               (ADDRESS = (PROTOCOL = TCPS)(HOST = 0.0.0.0)(PORT = 2484))
              )
            )
            
-         LISTENER = (ADDRESS_LIST=
-           (ADDRESS=(PROTOCOL=tcps)(HOST=0.0.0.0)(PORT=2484))
-         )
-
          DEFAULT_SERVICE_LISTENER = (XE)
          INBOUND_CONNECT_TIMEOUT_LISTENER=110
          SSL_CLIENT_AUTHENTICATION=FALSE
 END_OF_LISTENER
-#echo "WALLET_LOCATION=(SOURCE=(METHOD=FILE)(METHOD_DATA=(DIRECTORY=/server/wallet/path)))" >> $LISTENERORA
 
 # For some reason this oracle image does not allow user 'oracle' to run the oracle process *sigh*
 chmod 6751 $ORACLE_HOME/bin/oracle
