71d324cd38265cd1e39743a423fd3d4c3618cc60
Mei-hsiang Chang <meichang@us.ibm.com>
2020-12-07 17:56:48 -0600

    add java permissions and few files resmoved

--- dev/com.ibm.ws.wssecurity_fat.wsscxf/publish/servers/com.ibm.ws.wssecurity_fat.sample/apps/WSSampleSeiClient/WEB-INF/classes/com/ibm/was/wssample/sei/echo/EchoProxyInterface.java
+++
@@ -1,7 +0,0 @@
-package com.ibm.was.wssample.sei.echo;
-
-
-public interface EchoProxyInterface {
-    public String getEchoResponse( Object object, String endpointURL, String input) throws Exception;
-
-}
