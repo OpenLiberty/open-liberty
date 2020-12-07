07e3e3ef8169ccf82b4d464602548a00e95a411e
Thomas Johnson <tjjohnso@us.ibm.com>
2020-12-07 00:51:37 -0600

    intial code review changes

--- dev/com.ibm.ws.jaxws.common/src/com/ibm/ws/jaxws/support/LibertyHTTPTransportFactory.java
+++ dev/com.ibm.ws.jaxws.common/src/com/ibm/ws/jaxws/support/LibertyHTTPTransportFactory.java
@@ -1,4 +1,4 @@
-s/*******************************************************************************
+/*******************************************************************************
  * Copyright (c) 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
