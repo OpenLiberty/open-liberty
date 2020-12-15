9771c988032ba5c19c5d934b4b1322e05755d9fb
Katie Vance <ksv@us.ibm.com>
2020-12-15 10:29:56 -0600

    Add copywrites

--- dev/ant_build.js/lib/dojo-doh-junit-report/doh-junit.js
+++ dev/ant_build.js/lib/dojo-doh-junit-report/doh-junit.js
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2016 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation
+ *******************************************************************************/
+
 define(["dojo/main", "doh/runner", "dojo/_base/declare"], function(dojo, doh, declare) {
 
     var debugOn = false;
--- dev/com.ibm.ws.ui/resources/WEB-CONTENT/404/nls/fr/messages.js
+++ dev/com.ibm.ws.ui/resources/WEB-CONTENT/404/nls/fr/messages.js
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2016 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
 var editorMessages = {
     PAGE_NOT_FOUND: "404 Page introuvable",
     REDIRECTING_MESSAGE: "Vous allez être redirigé dans {0} secondes.",
