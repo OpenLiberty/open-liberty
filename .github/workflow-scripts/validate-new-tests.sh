3c8105b5dd6f50a35897967cf4bc077eb39f947c
kyleaure <kylejaure@gmail.com>
2021-01-04 15:52:07 -0600

    add/remove fats

--- .github/workflow-scripts/validate-new-tests.sh
+++ .github/workflow-scripts/validate-new-tests.sh
@@ -11,21 +11,18 @@ if [ ! -d $TEMP_COMPAR_DIR ]; then
 fi
 
 #Collect existing fats in test-categories
-for f in $TEST_CATEGORY_DIR/*
-do
-    cat $f >> $TEMP_COMPAR_DIR/expected
-done
+awk 1 $TEST_CATEGORY_DIR/* > $TEMP_COMPAR_DIR/expected
 sort -o $TEMP_COMPAR_DIR/expected $TEMP_COMPAR_DIR/expected
 
 #Collect actual fats that exist in file system
 for dir in $DEV_DIR/*_fat*/
 do 
-    fname=$(basename -- "$dir")  # Strip /home/etc/...
-    echo $fname >> $TEMP_COMPAR_DIR/actual
+    dirName=$(basename -- "$dir")  # Strip /home/etc/...
+    echo $dirName >> $TEMP_COMPAR_DIR/actual
 done
 sort -o $TEMP_COMPAR_DIR/actual $TEMP_COMPAR_DIR/actual
 
-#diff the two
+#diff the two ignoring white space (-b) and line differences (-B)
 diff -b -B $TEMP_COMPAR_DIR/expected $TEMP_COMPAR_DIR/actual > $TEMP_COMPAR_DIR/diff
 
 comp_value=$?
@@ -34,11 +31,11 @@ then
     testsAdded=$(cat $TEMP_COMPAR_DIR/diff | grep ">" )
     testsRemoved=$(cat $TEMP_COMPAR_DIR/diff | grep "<" )
     if [ ! -z "$testsAdded" ]; then
-        echo "::error::The following fats need to be added to a category: $testsAdded"
+        echo "::error::The following fat(s) need to be added to a category: $testsAdded"
     fi
 
     if [ ! -z "$testsRemoved" ]; then
-        echo "::error::The following fats need to be removed from a category: $testsRemoved"
+        echo "::error::The following fat(s) need to be removed from a category: $testsRemoved"
     fi
     
     exit 1;
