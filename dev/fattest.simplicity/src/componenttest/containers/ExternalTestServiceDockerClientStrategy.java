4d9f401c0917973fa84892565ecc4f0d5645250c
Neil G Young <neil_young@uk.ibm.com>
2021-01-07 16:22:16 +0000

    close FileInputStream before delete

--- dev/fattest.simplicity/src/componenttest/containers/ExternalTestServiceDockerClientStrategy.java
+++ dev/fattest.simplicity/src/componenttest/containers/ExternalTestServiceDockerClientStrategy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2020 IBM Corporation and others.
+ * Copyright (c) 2019, 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -81,7 +81,10 @@ public class ExternalTestServiceDockerClientStrategy extends DockerClientProvide
         try {
             Properties tcProps = new Properties();
             if (testcontainersConfigFile.exists()) {
-                tcProps.load(new FileInputStream(testcontainersConfigFile));
+                Log.info(c, "generateTestcontainersConfig", "File already exists");
+                FileInputStream fis = new FileInputStream(testcontainersConfigFile);
+                tcProps.load(fis);
+                fis.close();
                 tcProps.remove("docker.client.strategy");
                 Files.delete(testcontainersConfigFile.toPath());
             }
