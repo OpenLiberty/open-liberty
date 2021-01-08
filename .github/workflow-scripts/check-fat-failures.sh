6ef0ea51eef32584115f63aab815404a41d9f9d2
kyleaure <kylejaure@gmail.com>
2021-01-08 15:36:05 -0600

    cleanup known failures in GH actions build

--- .github/workflow-scripts/check-fat-failures.sh
+++ .github/workflow-scripts/check-fat-failures.sh
@@ -20,7 +20,7 @@ else
     echo "::error::Test category $CATEGORY does not exist. A file containing a list of FAT buckets was not found at .github/test-categories/$CATEGORY";
     exit 1;
   fi
-  FAT_BUCKETS=$(cat .github/test-categories/$CATEGORY)
+  FAT_BUCKETS=$(awk '!/^ *#/ && NF' .github/test-categories/$CATEGORY) #ignore comments starting with # and empty lines
 fi
 
 cd dev
