82d4f9a45a823006841b51dd8671b58ae53e3f27
Andy McCright <j.andrew.mccright@gmail.com>
2021-01-26 14:20:16 -0600

    Wait for SSL initialization to complete before running tests

--- dev/com.ibm.ws.microprofile.rest.client_fat/fat/src/com/ibm/ws/microprofile/rest/client/fat/BasicTest.java
+++ dev/com.ibm.ws.microprofile.rest.client_fat/fat/src/com/ibm/ws/microprofile/rest/client/fat/BasicTest.java
@@ -60,9 +60,12 @@ public class BasicTest extends FATServletClient {
     public static void setUp() throws Exception {
         ShrinkHelper.defaultDropinApp(remoteAppServer, "basicRemoteApp", "remoteApp.basic");
         remoteAppServer.startServer();
+        remoteAppServer.waitForStringInLog("CWWKO0219I"); // CWWKO0219I: TCP Channel defaultHttpEndpoint-ssl has been started and is now listening for requests on host *  (IPv6) port 8040.
 
         ShrinkHelper.defaultDropinApp(server, appName, "mpRestClient10.basic");
         server.startServer();
+        server.waitForStringInLog("CWWKO0219I"); // CWWKO0219I: TCP Channel defaultHttpEndpoint-ssl has been started and is now listening for requests on host *  (IPv6) port 8020.
+
     }
 
     @AfterClass
