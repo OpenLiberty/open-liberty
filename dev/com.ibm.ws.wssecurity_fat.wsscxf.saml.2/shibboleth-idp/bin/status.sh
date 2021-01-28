30ca2e8c316356aff51024b7d902f608aef91fde
Mei-hsiang Chang <meichang@us.ibm.com>
2021-01-28 14:25:35 -0600

    using shibboleth-idp from common project

--- dev/com.ibm.ws.wssecurity_fat.wsscxf.saml.2/shibboleth-idp/bin/status.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-declare LOCATION
-
-LOCATION=$0
-LOCATION=${LOCATION%/*}
-
-$LOCATION/runclass.sh -Dnet.shibboleth.idp.cli.arguments=net.shibboleth.idp.cli.StatusArguments \
-    net.shibboleth.idp.cli.CLI "$@"
--- dev/com.ibm.ws.wssecurity_fat.wsscxf.saml.2/shibboleth-idp/bin/version.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-declare LOCATION
-
-LOCATION=$0
-LOCATION=${LOCATION%/*}
-
-$LOCATION/runclass.sh net.shibboleth.idp.Version
