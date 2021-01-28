211927cba49f8ab70b073d3f4ef9aee835a88fcc
Ut Le <utle@us.ibm.com>
2021-01-28 15:10:57 -0600

    Added ignore msg CWWKS5524E

--- dev/com.ibm.ws.webcontainer.security.jacc.1.5_fat/fat/src/com/ibm/ws/webcontainer/security/jacc15/fat/BasicAuthTest.java
+++ dev/com.ibm.ws.webcontainer.security.jacc.1.5_fat/fat/src/com/ibm/ws/webcontainer/security/jacc15/fat/BasicAuthTest.java
@@ -104,7 +104,7 @@ public class BasicAuthTest extends CommonServletTestScenarios {
     @AfterClass
     public static void tearDown() throws Exception {
         try {
-            myServer.stopServer();
+            myServer.stopServer("CWWKS5524E");
         } finally {
             JACCFatUtils.uninstallJaccUserFeature(myServer);
         }
