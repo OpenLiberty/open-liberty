da6674b36294e0202f1168af9477a33b15899fff
Ut Le <utle@us.ibm.com>
2021-02-04 08:30:28 -0600

    Add authfilter for JWT SSO

--- dev/com.ibm.ws.webcontainer.security/src/com/ibm/ws/webcontainer/security/util/SSOAuthFilter.java
+++ dev/com.ibm.ws.webcontainer.security/src/com/ibm/ws/webcontainer/security/util/SSOAuthFilter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 IBM Corporation and others.
+ * Copyright (c) 2020, 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -17,4 +17,6 @@ import javax.servlet.http.HttpServletRequest;
  */
 public interface SSOAuthFilter {
     public boolean processRequest(HttpServletRequest req);
+
+    public boolean processRequest(HttpServletRequest req, String pid);
 }
