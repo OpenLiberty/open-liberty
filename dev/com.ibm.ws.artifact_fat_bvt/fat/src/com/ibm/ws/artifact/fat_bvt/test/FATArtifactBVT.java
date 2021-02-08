81018ff1de1c963650a43355afc7bcd68c9fe89c
Harry L. Hoot, III <hlhoots@us.ibm.com>
2021-02-08 13:54:37 -0500

    Remove artifact manifest after bvt execution

--- dev/com.ibm.ws.artifact_fat_bvt/fat/src/com/ibm/ws/artifact/fat_bvt/test/FATArtifactBVT.java
+++ dev/com.ibm.ws.artifact_fat_bvt/fat/src/com/ibm/ws/artifact/fat_bvt/test/FATArtifactBVT.java
@@ -53,6 +53,8 @@ public class FATArtifactBVT {
     }
 
     public static void tearDownServer() throws Exception {
+        
+        server.deleteFileFromLibertyInstallRoot( "/lib/features/artifactinternals-1.0.mf" );
         server = null;
     }
 
