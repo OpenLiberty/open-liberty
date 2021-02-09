fe61b0d439cb13e65d5b7389b7afdafa33c082d5
olendvcook <olendvcook@gmail.com>
2021-02-09 14:52:10 -0600

    Change @SkipForRepeat to show up as skipped in junit results

--- dev/fattest.simplicity/src/componenttest/custom/junit/runner/FATRunner.java
+++ dev/fattest.simplicity/src/componenttest/custom/junit/runner/FATRunner.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2020 IBM Corporation and others.
+ * Copyright (c) 2011, 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -186,7 +186,7 @@ public class FATRunner extends BlockJUnit4ClassRunner {
             @Override
             public void evaluate() throws Throwable {
                 if (!RepeatTestFilter.shouldRun(method)) {
-                    return;
+                    throw new AssumptionViolatedException("Test skipped for current RepeatAction");
                 }
                 Map<String, Long> tmpDirFilesBeforeTest = createDirectorySnapshot("/tmp");
                 try {
