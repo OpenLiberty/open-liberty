97d724e5ba89979d73a8e706549189983a39344d
David Chan <chdavid@ca.ibm.com>
2021-02-25 11:43:09 -0500

    Move over Request Timing and make necessary changes to FAT to make it run properly

---
+++ dev/com.ibm.ws.request.timing.monitor/src/com/ibm/websphere/request/timing/package-info.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/**
+ * @version 1.0.18
+ */
+@org.osgi.annotation.versioning.Version("1.0.44")
+@TraceOptions(traceGroup = "requestTiming")
+package com.ibm.websphere.request.timing;
+
+import com.ibm.websphere.ras.annotation.TraceOptions;
+
