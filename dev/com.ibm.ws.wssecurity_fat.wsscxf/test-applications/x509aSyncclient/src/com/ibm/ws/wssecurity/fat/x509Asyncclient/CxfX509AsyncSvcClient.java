b528208eb6e9953f822dfad16754eb74d92766cc
Mei-hsiang Chang <meichang@us.ibm.com>
2021-03-03 17:18:48 -0600

    add wsSecurityProvider prop in server*.xml

--- dev/com.ibm.ws.wssecurity_fat.wsscxf/test-applications/x509aSyncclient/src/com/ibm/ws/wssecurity/fat/x509Asyncclient/CxfX509AsyncSvcClient.java
+++ dev/com.ibm.ws.wssecurity_fat.wsscxf/test-applications/x509aSyncclient/src/com/ibm/ws/wssecurity/fat/x509Asyncclient/CxfX509AsyncSvcClient.java
@@ -11,51 +11,31 @@
 
 package com.ibm.ws.wssecurity.fat.x509Asyncclient;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.io.StringReader;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
+import java.net.URL;
 import java.util.Map;
-import java.util.Properties;
-import java.io.FileInputStream;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeoutException;
 
-import javax.net.ssl.HostnameVerifier ;
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.ws.WebServiceRef;
-
 import javax.xml.namespace.QName;
 import javax.xml.soap.MessageFactory;
 import javax.xml.soap.SOAPMessage;
-import javax.xml.soap.SOAPPart;
-import javax.xml.soap.SOAPEnvelope;
-import javax.xml.soap.SOAPBody;
-import javax.xml.ws.Dispatch;
-import javax.xml.ws.Service.Mode;
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
+import javax.xml.ws.Dispatch;
 import javax.xml.ws.Response;
-
-import com.ibm.websphere.simplicity.log.Log;
 
 import test.wssecfvt.x509async.UrnX509AsyncType;
 import test.wssecfvt.x509async.X509AsyncService;
 import test.wssecfvt.x509async.types.RequestString;
 import test.wssecfvt.x509async.types.ResponseString;
-import test.wssecfvt.x509async.types.ObjectFactory;
 
 /**
  * Servlet implementation class CxfX509AsyncSvcClient
@@ -66,7 +46,7 @@ public class CxfX509AsyncSvcClient extends HttpServlet {
     static String strJksLocation = "sslServerTrust.jks";
     private static final long serialVersionUID = 1L;
     private static final String SERVICE_NS = "http://x509async.wssecfvt.test";
-    private static String testName = "" ;
+    private static String testName = "";
     private static String wsdlLocation = "";
     private static String httpPortNum = null;
     private static String httpSecurePortNum = null;
@@ -80,6 +60,9 @@ public class CxfX509AsyncSvcClient extends HttpServlet {
     private static String pw = null;
     private static String msgId = null;
     private static String msgToBeUsed = null;
+
+    //2/26/2021
+    private static String CBHVersion = "";
 
     private StringReader reqMsg = null;
 
@@ -101,13 +84,13 @@ public class CxfX509AsyncSvcClient extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doWorker(HttpServletRequest request,
-                         HttpServletResponse response) throws ServletException, IOException {
+                            HttpServletResponse response) throws ServletException, IOException {
 
-        // Extract valuse sent by the client
+        // Extract value sent by the client
         try {
-            testName = request.getParameter("testName") ;
+            testName = request.getParameter("testName");
             System.out.println("************************ "
-                               + testName 
+                               + testName
                                + " ************************");
             httpPortNum = request.getParameter("httpDefaultPort");
             httpSecurePortNum = request.getParameter("httpSecureDefaultPort");
@@ -116,8 +99,10 @@ public class CxfX509AsyncSvcClient extends HttpServlet {
             rawServiceName = request.getParameter("serviceName");
             clientWsdl = request.getParameter("clientWsdl").trim();
             serviceName = new QName(SERVICE_NS, rawServiceName);
-            servicePort = new QName(SERVICE_NS, request
-                            .getParameter("servicePort"));
+            servicePort = new QName(SERVICE_NS, request.getParameter("servicePort"));
+            //2/26/2021
+            String CBHVersion = request.getParameter("CallBackhandlerVersion");
+
             msgId = request.getParameter("msg");
             if (httpSecurePortNum.equals("")) {
                 thePort = httpPortNum;
@@ -131,8 +116,7 @@ public class CxfX509AsyncSvcClient extends HttpServlet {
         } catch (Exception ex) {
             System.out.println("Failed to find all required parameters");
             ex.printStackTrace();
-            ServletException e1 = new ServletException(
-                            "Failed to find all required parameters");
+            ServletException e1 = new ServletException("Failed to find all required parameters");
             throw e1;
         }
         System.out.println("rawWsdl:" + clientWsdl + ":rawWsdl");
@@ -170,25 +154,37 @@ public class CxfX509AsyncSvcClient extends HttpServlet {
             // setup to handle multiple services later
             if (rawServiceName.equals("X509AsyncService")) {
                 System.out.println("In X509AsyncService case");
-                X509AsyncService service = new X509AsyncService(wsdlURL,
-                                            serviceName);
+                X509AsyncService service = new X509AsyncService(wsdlURL, serviceName);
                 servicePort2 = service.getX509AsyncPort();
 
             }
 
             Map<String, Object> requestContext = ((javax.xml.ws.BindingProvider) servicePort2).getRequestContext();
 
+            //2/26/2021
+            System.out.println("CBHVersion: " + CBHVersion);
+
             if (id != null) {
                 System.out.println("Setting id: " + id + " with password: "
                                    + pw);
                 requestContext.put("ws-security.username", id);
-                requestContext.put("ws-security.password", pw);
+                //2/26/2021
+                //requestContext.put("ws-security.password", pw);
+                //2/26/2021
+                if (CBHVersion.equals("EE7")) {
+                    requestContext.put("ws-security.callback-handler",
+                                       "com.ibm.ws.wssecurity.example.cbh.CommonPasswordCallback");
+                }
+                if (CBHVersion.equals("EE8")) {
+                    requestContext.put("ws-security.callback-handler",
+                                       "com.ibm.ws.wssecurity.example.cbhwss4j.CommonPasswordCallbackWss4j");
+                } //End 2/26/2021
+
             }
 
             // may want to make this set the properties some times, and other
             // times use the defaults - tbd
-            String strServerDir = System.getProperty("server.config.dir")
-                            .replace('\\', '/');
+            String strServerDir = System.getProperty("server.config.dir").replace('\\', '/');
 
             System.out.println("Service Client request Header: "
                                + soapReq.getSOAPHeader());
@@ -198,31 +194,36 @@ public class CxfX509AsyncSvcClient extends HttpServlet {
 
             RequestString reqString = new RequestString();
             reqString.setStringreq(simpleSoapBody);
-            
+
             PrintWriter rsp = response.getWriter();
-            String answer = "" ;
-            
-            if (testName.contains("Block") ) {
-              ResponseString soapResp = null;
-              Response<ResponseString> responseRef = servicePort2.invokeAsync(reqString);
-              
-              if (request.getParameter("testName").contains("testCxfAsyncInvokeNonBlocking")) {
-                System.out.println("Non-Blocking Case");
-                int waitCount = 0;
-                while (!responseRef.isDone() && waitCount < 15) {
-                    Thread.sleep(1000);
-                    waitCount++;
-                }
-                if (responseRef.isDone() && waitCount < 15) {
-                    soapResp = responseRef.get();
+            String answer = "";
+
+            if (testName.contains("Block")) {
+                ResponseString soapResp = null;
+                Response<ResponseString> responseRef = servicePort2.invokeAsync(reqString);
+
+                if (request.getParameter("testName").contains("testCxfAsyncInvokeNonBlocking")) {
+                    System.out.println("In CxfX509AsyncSvcClient, Non-Blocking Case");
+                    int waitCount = 0;
+                    while (!responseRef.isDone() && waitCount < 15) {
+                        //2/2021
+                        System.out.println("In CxfX509AsyncSvcClient, thread sleep ... and waitCount: " + waitCount);
+                        Thread.sleep(1000);
+                        waitCount++;
+                    }
+                    if (responseRef.isDone() && waitCount < 15) {
+                        //2/2201
+                        System.out.println("In CxfX509AsyncSvcClient, response is done and waitCount: " + waitCount);
+                        soapResp = responseRef.get();
+                        System.out.println("after responseRef.get ");
+                    } else {
+                        rsp.print("<html><head><title>CXF x509 Async Service Cleint</title></head><body>");
+                        rsp.print("<p>Request: CXF x509 Async Service Client</p>");
+                        rsp.print("<p>Response: TIMEOUT: Exited wait loop with no indication that request has completed </p>");
+                        rsp.print("</body></html>");
+                    }
                 } else {
-                    rsp.print("<html><head><title>CXF x509 Async Service Cleint</title></head><body>");
-                    rsp.print("<p>Request: CXF x509 Async Service Client</p>");
-                    rsp.print("<p>Response: TIMEOUT: Exited wait loop with no indication that request has completed </p>");
-                    rsp.print("</body></html>");
-                }
-              } else {
-                
+
                     System.out.println("Blocking Case");
                     try {
                         soapResp = responseRef.get(120L, java.util.concurrent.TimeUnit.SECONDS);
@@ -233,12 +234,12 @@ public class CxfX509AsyncSvcClient extends HttpServlet {
                         rsp.print("<p>Response: TIMEOUT: " + te + "</p>");
                         rsp.print("</body></html>");
                     }
-              }
-              answer = soapResp.getStringres();
-              
+                }
+                answer = soapResp.getStringres();
+
             } else {
                 System.out.println("Callback Case");
-                TestAsyncHandler testAsyncHandler = new TestAsyncHandler() ;
+                TestAsyncHandler testAsyncHandler = new TestAsyncHandler();
                 Future<?> fresponseRef = servicePort2.invokeAsync(reqString, testAsyncHandler);
                 int waitCount = 0;
                 while (!fresponseRef.isDone() && waitCount < 15) {
@@ -294,6 +295,5 @@ public class CxfX509AsyncSvcClient extends HttpServlet {
         doWorker(request, response);
         return;
     }
-
 
 }
--- dev/com.ibm.ws.wssecurity_fat.wsscxf/test-bundles/com.ibm.ws.wssecurity.cbh/src/com/ibm/ws/wssecurity/example/cbh/CommonPasswordCallback.java
+++ dev/com.ibm.ws.wssecurity_fat.wsscxf/test-bundles/com.ibm.ws.wssecurity.cbh/src/com/ibm/ws/wssecurity/example/cbh/CommonPasswordCallback.java
@@ -10,11 +10,11 @@
  *******************************************************************************/
 package com.ibm.ws.wssecurity.example.cbh;
 
-import java.io.IOException;
-import java.io.File;
-import java.io.InputStreamReader;
-import java.io.FileInputStream;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,66 +28,60 @@ import org.apache.ws.security.WSPasswordCallback;
  */
 public class CommonPasswordCallback implements CallbackHandler {
 
-    private Map<String, String> userPasswords = 
-    new HashMap<String, String>();
+    private final Map<String, String> userPasswords = new HashMap<String, String>();
 
-    private Map<String, String> signaturePasswords = 
-    new HashMap<String, String>();
+    private final Map<String, String> signaturePasswords = new HashMap<String, String>();
 
-    private Map<String, String> encryptPasswords = 
-    new HashMap<String, String>();
+    private final Map<String, String> encryptPasswords = new HashMap<String, String>();
 
     //
     //  This class will be instanced in every WSSecurity related request.
-    //  So, be careful about the performnace.
+    //  So, be careful about the performance.
     //  In fat, we may need the ID and password to be changed every time.
     //  But may not be in a product environment
     //
     public CommonPasswordCallback() {
-        String strPath = System.getProperty( "server.config.dir" );
+        String strPath = System.getProperty("server.config.dir");
         File filePasswd = null;
         boolean bFileExist = false;
-        if( strPath != null ){
-            try{
-                filePasswd = new File( strPath + "/PasswordCallBack.props" );
+        if (strPath != null) {
+            try {
+                filePasswd = new File(strPath + "/PasswordCallBack.props");
                 bFileExist = filePasswd.exists();
-            } catch( Exception e ){
+            } catch (Exception e) {
             }
         }
-        if( bFileExist ){
-            
-            try
-            {
-               InputStreamReader inputStream = 
-                  new InputStreamReader( new FileInputStream( filePasswd ));
-               BufferedReader dataStream  = new BufferedReader( inputStream);
+        if (bFileExist) {
 
-               String strTmp = null;
-               while( (strTmp = dataStream.readLine()) != null )
-               {
-                  if(! putUserPass( userPasswords, "userPasswords.", strTmp ) ){
-                      if(! putUserPass( signaturePasswords, "signaturePasswords.", strTmp ) ){
-                          if(! putUserPass( encryptPasswords, "encryptPasswords.", strTmp ) ){
-                              System.out.println("A String is not handled in CommonPasswordCallback:'" + strTmp +"'");
-                          }
-                      }
-                  }
-               }
-               dataStream.close();
-            } catch( Exception e ){
+            try {
+                InputStreamReader inputStream = new InputStreamReader(new FileInputStream(filePasswd));
+                BufferedReader dataStream = new BufferedReader(inputStream);
+
+                String strTmp = null;
+                while ((strTmp = dataStream.readLine()) != null) {
+                    if (!putUserPass(userPasswords, "userPasswords.", strTmp)) {
+                        if (!putUserPass(signaturePasswords, "signaturePasswords.", strTmp)) {
+                            if (!putUserPass(encryptPasswords, "encryptPasswords.", strTmp)) {
+                                System.out.println("A String is not handled in CommonPasswordCallback:'" + strTmp + "'");
+                            }
+                        }
+                    }
+                }
+                dataStream.close();
+            } catch (Exception e) {
                 e.printStackTrace();
             }
 
         } else { // No property file exists. Using the original UserID and password
             userPasswords.put("user1", "security");
-            userPasswords.put("user2", "security" );
-            userPasswords.put("test1", "test1" );
-            userPasswords.put("test2", "test2" );
-            userPasswords.put("test3", "test3" );
-            userPasswords.put("test4", "test4" );
-            userPasswords.put("admin", "admin" );
-            userPasswords.put("joe", "joe" );
-            userPasswords.put("user1pw", "pw8cbh" );  // Its password is different from server.xml
+            userPasswords.put("user2", "security");
+            userPasswords.put("test1", "test1");
+            userPasswords.put("test2", "test2");
+            userPasswords.put("test3", "test3");
+            userPasswords.put("test4", "test4");
+            userPasswords.put("admin", "admin");
+            userPasswords.put("joe", "joe");
+            userPasswords.put("user1pw", "pw8cbh"); // Its password is different from server.xml
 
             userPasswords.put("Alice", "ecilA");
             userPasswords.put("Frank", "knarF");
@@ -101,78 +95,81 @@ public class CommonPasswordCallback implements CallbackHandler {
             signaturePasswords.put("x509clientdefault", "KLibertyX509Client");
             signaturePasswords.put("x509ServerDefault", "KLibertyX509Server");
             signaturePasswords.put("x509serverdefault", "KLibertyX509Server");
-            signaturePasswords.put("x509ClientSecond" , "KLibertyX509Client2");
-            signaturePasswords.put("x509clientsecond" , "KLibertyX509Client2");
-            signaturePasswords.put("x509ServerSecond" , "KLibertyX509Server2");
-            signaturePasswords.put("x509serversecond" , "KLibertyX509Server2");
-            signaturePasswords.put("soapprovider",      "server");
-            signaturePasswords.put("soaprequester",     "client");
-            signaturePasswords.put("bob",               "keypass");
-            signaturePasswords.put("alice",             "keypass");
+            signaturePasswords.put("x509ClientSecond", "KLibertyX509Client2");
+            signaturePasswords.put("x509clientsecond", "KLibertyX509Client2");
+            signaturePasswords.put("x509ServerSecond", "KLibertyX509Server2");
+            signaturePasswords.put("x509serversecond", "KLibertyX509Server2");
+            signaturePasswords.put("soapprovider", "server");
+            signaturePasswords.put("soaprequester", "client");
+            signaturePasswords.put("bob", "keypass");
+            signaturePasswords.put("alice", "keypass");
 
-            encryptPasswords.put  ("x509ClientDefault", "KLibertyX509Client");
-            encryptPasswords.put  ("x509clientdefault", "KLibertyX509Client");
-            encryptPasswords.put  ("x509ServerDefault", "KLibertyX509Server");
-            encryptPasswords.put  ("x509serverdefault", "KLibertyX509Server");
-            encryptPasswords.put  ("x509ClientSecond" , "KLibertyX509Client2");
-            encryptPasswords.put  ("x509clientsecond" , "KLibertyX509Client2");
-            encryptPasswords.put  ("x509ServerSecond" , "KLibertyX509Server2");
-            encryptPasswords.put  ("x509serversecond" , "KLibertyX509Server2");
-            encryptPasswords.put  ("bob" ,              "keypass");
-            encryptPasswords.put  ("alice" ,            "keypass");
+            encryptPasswords.put("x509ClientDefault", "KLibertyX509Client");
+            encryptPasswords.put("x509clientdefault", "KLibertyX509Client");
+            encryptPasswords.put("x509ServerDefault", "KLibertyX509Server");
+            encryptPasswords.put("x509serverdefault", "KLibertyX509Server");
+            encryptPasswords.put("x509ClientSecond", "KLibertyX509Client2");
+            encryptPasswords.put("x509clientsecond", "KLibertyX509Client2");
+            encryptPasswords.put("x509ServerSecond", "KLibertyX509Server2");
+            encryptPasswords.put("x509serversecond", "KLibertyX509Server2");
+            encryptPasswords.put("bob", "keypass");
+            encryptPasswords.put("alice", "keypass");
 
         }
-                
+
     }
 
     /**
-     * Here, we attempt to get the password from the private 
+     * Here, we attempt to get the password from the private
      * alias/passwords map.
      */
+    @Override
     public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
-        System.out.println("CommonPasswordCallback :" + callbacks);
+        System.out.println("From CommonPasswordCallback.java, using the callbacks:" + callbacks);
         for (int i = 0; i < callbacks.length; i++) {
-            WSPasswordCallback pwcb = (WSPasswordCallback)callbacks[i];
+            WSPasswordCallback pwcb = (WSPasswordCallback) callbacks[i];
             String id = pwcb.getIdentifier();
-            System.out.println("CommonPasswordCallback: id:" + id);
+            System.out.println("From CommonPasswordCallback.java: id:" + id);
             String pass = null;
 
             switch (pwcb.getUsage()) {
-            case WSPasswordCallback.USERNAME_TOKEN_UNKNOWN:  // depricated
-            case WSPasswordCallback.USERNAME_TOKEN:
+                //2/2021    
+                //case WSPasswordCallback.USERNAME_TOKEN_UNKNOWN:  // deprecated
+                case WSPasswordCallback.UNKNOWN:
+                case WSPasswordCallback.USERNAME_TOKEN:
 
-                System.out.println("CommonPasswordCallback: Getting userPassword id:" + id);
-                pass = userPasswords.get(id);
-                if (pass != null) {
-                    pwcb.setPassword(pass);
-                    return;
-                }
-                break;
+                    System.out.println("From CommonPasswordCallback.java: Getting userPassword id:" + id);
+                    pass = userPasswords.get(id);
+                    if (pass != null) {
+                        pwcb.setPassword(pass);
+                        return;
+                    }
+                    break;
 
-            case WSPasswordCallback.DECRYPT:
-                System.out.println("CommonPasswordCallback: Getting decryptPassword id:" + id);
-                pass = encryptPasswords.get(id);
-                if (pass != null) {
-                    System.out.println("CommonPasswordCallback: encryptPassword:" + pass);
-                    pwcb.setPassword(pass);
-                    return;
-                }
+                case WSPasswordCallback.DECRYPT:
+                    System.out.println("From CommonPasswordCallback.java: Getting decryptPassword id:" + id);
+                    pass = encryptPasswords.get(id);
+                    if (pass != null) {
+                        System.out.println("From CommonPasswordCallback.java: encryptPassword:" + pass);
+                        pwcb.setPassword(pass);
+                        return;
+                    }
 
-                break;
+                    break;
 
-            case WSPasswordCallback.SIGNATURE:
+                case WSPasswordCallback.SIGNATURE:
 
-                System.out.println("CommonPasswordCallback: Getting signaturePassword id:" + id);
-                pass = signaturePasswords.get(id);
-                if (pass != null) {
-                    pwcb.setPassword(pass);
-                    System.out.println("CommonPasswordCallback: signaturePassword:" + pass);
-                    return;
-                }
-                break;
-            default:
-                System.out.println("CommonPasswordCallback: Password not handled id:" + id + "  type:" + pwcb.getUsage());
-                break;
+                    System.out.println("CommonPasswordCallback: Getting signaturePassword id:" + id);
+                    pass = signaturePasswords.get(id);
+                    if (pass != null) {
+                        pwcb.setPassword(pass);
+                        System.out.println("From CommonPasswordCallback.java: signaturePassword:" + pass);
+                        return;
+                    }
+                    break;
+                default:
+                    System.out.println("From CommonPasswordCallback.java: Password not handled id:" + id + "  type:" + pwcb.getUsage());
+                    break;
             }
         }
     }
@@ -184,19 +181,18 @@ public class CommonPasswordCallback implements CallbackHandler {
         userPasswords.put(alias, password);
     }
 
-
-    boolean putUserPass( Map<String, String> mapPasswords, String strKeyWord, String strEntry ){ 
-        int index = strEntry.indexOf( strKeyWord);
-        if( index >= 0 ){
-            String strTmp = strEntry.substring( index + strKeyWord.length() );
+    boolean putUserPass(Map<String, String> mapPasswords, String strKeyWord, String strEntry) {
+        int index = strEntry.indexOf(strKeyWord);
+        if (index >= 0) {
+            String strTmp = strEntry.substring(index + strKeyWord.length());
             int indexEqual = strTmp.indexOf("=");
-            if( indexEqual >= 0 ){
-                String strKey = strTmp.substring( 0, indexEqual ).trim(); 
-                String strValue = strTmp.substring( indexEqual + 1 ).trim();
-                mapPasswords.put( strKey, strValue );
+            if (indexEqual >= 0) {
+                String strKey = strTmp.substring(0, indexEqual).trim();
+                String strValue = strTmp.substring(indexEqual + 1).trim();
+                mapPasswords.put(strKey, strValue);
                 return true;
             }
-        } 
+        }
         return false;
     }
 }
