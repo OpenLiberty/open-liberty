fb247685b175c41d0f07142a17de64fd39da1bb2
Nathan Rauh <nathan.rauh@us.ibm.com>
2021-03-03 13:30:42 -0600

    Issue #15158 disable parking of managed connections

--- dev/com.ibm.ws.jca.cm/src/com/ibm/ejs/j2c/J2CGlobalConfigProperties.java
+++ dev/com.ibm.ws.jca.cm/src/com/ibm/ejs/j2c/J2CGlobalConfigProperties.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1997, 2020 IBM Corporation and others.
+ * Copyright (c) 1997, 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -26,6 +26,7 @@ import com.ibm.websphere.ras.TraceComponent;
 import com.ibm.ws.jca.adapter.PurgePolicy;
 import com.ibm.ws.jca.cm.AbstractConnectionFactoryService;
 import com.ibm.ws.jca.cm.AppDefinedResource;
+import com.ibm.ws.jca.cm.ConnectionManagerService;
 
 /**
  * The <B>J2CGlobalConfigProperties</B> will contain all the Configuration Related
@@ -353,6 +354,7 @@ public final class J2CGlobalConfigProperties implements PropertyChangeListener, 
     public int numberOfFreeConnections = 0;
     public transient Integer numberOfFreeConnectionsLockObject = new Integer(0);
     protected transient Integer maxNumberOfMCsAllowableInThread = null;
+    private transient boolean parkIfDissociateUnavailable;
     protected transient Boolean throwExceptionOnMCThreadCheck = null;
 
     String appName;
@@ -381,6 +383,7 @@ public final class J2CGlobalConfigProperties implements PropertyChangeListener, 
                                      boolean autoCloseConnections,
                                      int numConnectionsPerThreadLocal,
                                      Integer maxNumberOfMCsAllowableInThread,
+                                     boolean parkIfDissociateUnavailable,
                                      Boolean throwExceptionOnMCThreadCheck) {
         if (TraceComponent.isAnyTracingEnabled() && tc.isEntryEnabled()) {
             Tr.entry(this, tc, "<init>", "Full Constructor");
@@ -422,6 +425,7 @@ public final class J2CGlobalConfigProperties implements PropertyChangeListener, 
         this.autoCloseConnections = autoCloseConnections;
         this.numConnectionsPerThreadLocal = numConnectionsPerThreadLocal;
         this.maxNumberOfMCsAllowableInThread = maxNumberOfMCsAllowableInThread;
+        this.parkIfDissociateUnavailable = parkIfDissociateUnavailable;
         this.throwExceptionOnMCThreadCheck = throwExceptionOnMCThreadCheck;
 
         /*
@@ -964,6 +968,15 @@ public final class J2CGlobalConfigProperties implements PropertyChangeListener, 
                                                    Integer maxNumberOfMCsAllowableInThread) {
         changeSupport.firePropertyChange("maxNumberOfMCsAllowableInThread", this.maxNumberOfMCsAllowableInThread, maxNumberOfMCsAllowableInThread);
         this.maxNumberOfMCsAllowableInThread = maxNumberOfMCsAllowableInThread;
+    }
+
+    public final boolean getParkIfDissociateUnavailable() {
+        return parkIfDissociateUnavailable;
+    }
+
+    public final void setParkIfDissociateUnavailable(boolean value) {
+        changeSupport.firePropertyChange(ConnectionManagerService.TEMPORARILY_ASSOCIATE_IF_DISSOCIATE_UNAVAILABLE, parkIfDissociateUnavailable, value);
+        parkIfDissociateUnavailable = value;
     }
 
     public boolean getThrowExceptionOnMCThreadCheck() {
