0d76772eb8f02a7263483daaa161265da8fb0261
Neil Richards <7203556+ngmr@users.noreply.github.com>
2021-03-10 19:30:03 +0000

    Use cleaner DS semantics in ORBWrapper

--- dev/com.ibm.ws.transport.iiop/src/com/ibm/ws/transport/iiop/internal/ORBWrapper.java
+++ dev/com.ibm.ws.transport.iiop/src/com/ibm/ws/transport/iiop/internal/ORBWrapper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 IBM Corporation and others.
+ * Copyright (c) 2015,2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,26 +13,28 @@
  */
 package com.ibm.ws.transport.iiop.internal;
 
+import static java.util.Collections.unmodifiableList;
+import static java.util.Collections.unmodifiableMap;
 import static org.osgi.service.component.annotations.ConfigurationPolicy.REQUIRE;
+import static org.osgi.service.component.annotations.ReferenceCardinality.MULTIPLE;
+import static org.osgi.service.component.annotations.ReferencePolicyOption.GREEDY;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
-import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.ComponentFactory;
+import org.osgi.service.component.ComponentInstance;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
 
-import com.ibm.websphere.ras.Tr;
-import com.ibm.websphere.ras.TraceComponent;
-import com.ibm.ws.transport.iiop.config.ConfigAdapter;
 import com.ibm.ws.transport.iiop.spi.IIOPEndpoint;
+import com.ibm.ws.transport.iiop.spi.ReadyListener;
 import com.ibm.ws.transport.iiop.spi.SubsystemFactory;
 
 /**
@@ -41,42 +43,56 @@ import com.ibm.ws.transport.iiop.spi.SubsystemFactory;
 @Component(configurationPolicy = REQUIRE,
                 service = {},
                 property = { "service.vendor=IBM", "service.ranking:Integer=5" })
-public class ORBWrapper extends ReadyListenerImpl {
-    private static final TraceComponent tc = Tr.register(ORBWrapper.class);
+public final class ORBWrapper implements ReadyListener {
     public static final String pid = ORBWrapperInternal.class.getName();
-
-    private final List<IIOPEndpoint> endpoints = new ArrayList<IIOPEndpoint>();
-
-    private final Map<String, Object> extraConfig = new HashMap<String, Object>();
+    private final Map<SubsystemFactory, ? extends AtomicBoolean> subsystemFactories;
+    private final Map<String, Object> properties;
+    private final ComponentFactory<ORBWrapperInternal> factory;
+    private final AtomicReference<ComponentInstance<ORBWrapperInternal>> instanceRef = new AtomicReference<>();
 
     @Activate
-    protected void activate(Map<String, Object> properties, ComponentContext cc) throws Exception {
-        try {
-            super.activate(properties, cc.getBundleContext());
-        } catch (Exception e) { // should auto-ffdc
-            throw e;
-        }
+    public ORBWrapper(
+               Map<String,Object> properties,
+               @Reference(target = "(component.factory=com.ibm.ws.transport.iiop.internal.ORBWrapperInternal)")
+               ComponentFactory<ORBWrapperInternal> factory,
+               @Reference(cardinality = MULTIPLE, policyOption = GREEDY)
+               List<IIOPEndpoint> endpoints,
+               @Reference(cardinality = MULTIPLE, policyOption = GREEDY)
+               List<SubsystemFactory> subsystemFactories
+               ) {
+        this.properties = unmodifiableMap(properties);
+        this.factory = factory;
+        this.subsystemFactories = unmodifiableMap(subsystemFactories.stream().sequential().collect(LinkedHashMap::new, (m,f) -> m.put(f, new AtomicBoolean()), Map::putAll));
+        subsystemFactories.forEach(sf -> sf.register(this, properties, unmodifiableList(endpoints)));
+    }
+
+    @Deactivate
+    void deactivate() {
+        subsystemFactories.keySet().forEach(sf -> sf.unregister(this));
     }
 
     /** {@inheritDoc} */
     @Override
-    void register() {
-        for (SubsystemFactory sf : subsystemFactories.keySet()) {
-            sf.register(this, properties, endpoints);
-        }
+    public void readyChanged(SubsystemFactory id, boolean ready) {
+        instanceRef.getAndUpdate(i -> {
+            try {
+                subsystemFactories.get(id).set(ready);
+            } catch (NullPointerException unexpected) { // FFDC and continue
+            }
+            if (subsystemFactories.values().stream().allMatch(AtomicBoolean::get)) return null == i ? factory.newInstance(copyProps()) : i;
+            if (null != i) i.dispose();
+            return null;                        
+        });
     }
 
-    @Deactivate
-    protected void deactivate(ComponentContext cc) {
-        super.deactivate();
+    private Hashtable<String, Object> copyProps() {
+        final Hashtable<String, Object> h = new Hashtable<>();
+        h.putAll(properties);
+        return h;
     }
 
-    @Reference
-    protected void setConfigAdapter(ConfigAdapter configAdapter) {}
-
-    @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.STATIC, policyOption = ReferencePolicyOption.GREEDY)
-    protected void setIiopEndpoint(IIOPEndpoint ep) {
-        endpoints.add(ep);
+    @Override
+    public String listenerId() {
+        return (String)properties.get("id");
     }
-
 }
