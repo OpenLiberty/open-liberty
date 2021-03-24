a92b19d2402c2b8bd8d9dde0a761caf5da5d843d
Nathan Rauh <nathan.rauh@us.ibm.com>
2021-03-24 13:38:26 -0500

    Remove unwanted trace and move empty handle list out of JCA trace group

--- dev/io.openliberty.handlelist.context.internal/src/io/openliberty/handlelist/context/internal/package-info.java
+++ dev/io.openliberty.handlelist.context.internal/src/io/openliberty/handlelist/context/internal/package-info.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 IBM Corporation and others.
+ * Copyright (c) 2020,2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,7 @@
  * @version 1.0.0
  */
 @org.osgi.annotation.versioning.Version("1.0.0")
-@TraceOptions(traceGroup = "WAS.j2c")
+@TraceOptions(traceGroup = "handlelist")
 package io.openliberty.handlelist.context.internal;
 
 import com.ibm.websphere.ras.annotation.TraceOptions;
