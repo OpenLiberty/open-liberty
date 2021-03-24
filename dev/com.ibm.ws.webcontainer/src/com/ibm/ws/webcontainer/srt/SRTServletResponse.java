22e3359ea3956664ed92425f64ed04d79d47b936
Bill Lucy <wtlucy@gmail.com>
2021-03-24 16:24:07 -0400

    avoid double releaseChannel() in async case

--- dev/com.ibm.ws.webcontainer/src/com/ibm/ws/webcontainer/srt/SRTServletResponse.java
+++ dev/com.ibm.ws.webcontainer/src/com/ibm/ws/webcontainer/srt/SRTServletResponse.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1997, 2020 IBM Corporation and others.
+ * Copyright (c) 1997, 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -555,8 +555,7 @@ public class SRTServletResponse implements HttpServletResponse, IResponseOutput,
             WebContainerRequestState reqState = WebContainerRequestState.getInstance(true);
             if (! reqState.isCompleted()) {
                 _response.releaseChannel();
-                if (reqState.getCurrentThreadsIExtendedRequest()==this.getRequest())
-                    WebContainerRequestState.getInstance(true).setCompleted(true);
+                WebContainerRequestState.getInstance(true).setCompleted(true);
             }
         }
         if (com.ibm.ejs.ras.TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)){
