005a8086b0550ef28e8564f4ee71eb46da0ad268
Ellen Lau <Ellen.lau@ibm.com>
2021-04-07 17:00:44 -0400

    Log message when /health/ready endpoint response code is 200

--- dev/com.ibm.ws.microprofile.health.2.0_fat/fat/src/com/ibm/ws/microprofile/health20/fat/DelayAppStartupHealthCheckTest.java
+++ dev/com.ibm.ws.microprofile.health.2.0_fat/fat/src/com/ibm/ws/microprofile/health20/fat/DelayAppStartupHealthCheckTest.java
@@ -189,19 +189,18 @@ public class DelayAppStartupHealthCheckTest {
                         }
                     } else {
                         if (responseCode == 200) {
+                            log("testReadinessEndpointOnServerStart", "The /health/ready endpoint response code was 200.");
                             app_ready = true;
                             repeat = false;
                             startServerThread.join();
                         } else if (System.currentTimeMillis() - start_time > time_out) {
-
-                            List<String> lines = server1.findStringsInFileInLibertyServerRoot("O Exiting init function - Thread.sleep completed.", MESSAGE_LOG);
+                            List<String> lines = server1.findStringsInFileInLibertyServerRoot("Exiting init function - Thread.sleep completed.", MESSAGE_LOG);
 
                             if (lines.size() == 0) {
                                 log("testReadinessEndpointOnServerStart", "waiting for DelayedServlet sleep to finish.");
-                                server1.waitForStringInLog("O Exiting init function - Thread.sleep completed.");
+                                server1.waitForStringInLog("Exiting init function - Thread.sleep completed.");
                                 log("testReadinessEndpointOnServerStart", "DelayedServlet sleep finished.");
                             }
-
                             else {
                                 log("testReadinessEndpointOnServerStart", "DelayedServlet sleep finished but timeout still reached.");
                                 throw new TimeoutException("Timed out waiting for server and app to be ready. Timeout set to " + time_out + "ms.");
