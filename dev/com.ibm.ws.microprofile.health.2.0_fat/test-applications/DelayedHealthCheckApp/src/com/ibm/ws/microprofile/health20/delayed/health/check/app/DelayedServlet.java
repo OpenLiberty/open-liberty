6c4cedbb1463f170e27af414815fb1083f0b670b
Ellen Lau <Ellen.lau@ibm.com>
2021-04-07 16:40:44 -0400

    Change delayedservlet timeout back to 65s

--- dev/com.ibm.ws.microprofile.health.2.0_fat/test-applications/DelayedHealthCheckApp/src/com/ibm/ws/microprofile/health20/delayed/health/check/app/DelayedServlet.java
+++ dev/com.ibm.ws.microprofile.health.2.0_fat/test-applications/DelayedHealthCheckApp/src/com/ibm/ws/microprofile/health20/delayed/health/check/app/DelayedServlet.java
@@ -33,9 +33,9 @@ public class DelayedServlet extends HttpServlet {
 
     @Override
     public void init() {
-        System.out.println("Entering init function - Starting Thread.sleep for 30 seconds.");
+        System.out.println("Entering init function - Starting Thread.sleep for 65 seconds.");
         try {
-            Thread.sleep(30000); //30 seconds = 30000 ms
+            Thread.sleep(65000); //30 seconds = 30000 ms
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
