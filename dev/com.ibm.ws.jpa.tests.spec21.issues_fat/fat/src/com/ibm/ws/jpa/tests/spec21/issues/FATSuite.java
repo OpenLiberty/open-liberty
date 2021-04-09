6afbc9557f7423982dce5b89ad68b39525524ac0
Joe Grassel <jgrassel@us.ibm.com>
2021-04-09 15:27:12 -0500

    Break apart JPA 2.1 FAT Bucket
    Signed-off-by: Joe Grassel <jgrassel@us.ibm.com>

---
+++ dev/com.ibm.ws.jpa.tests.spec21.issues_fat/fat/src/com/ibm/ws/jpa/tests/spec21/issues/FATSuite.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package com.ibm.ws.jpa.tests.spec21.issues;
+
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import componenttest.rules.repeater.FeatureReplacementAction;
+import componenttest.rules.repeater.RepeatTests;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+                TestOLGH8820_EJB.class,
+                TestOLGH8820_Web.class,
+                TestOLGH10240_EJB.class,
+                TestOLGH10240_Web.class,
+                componenttest.custom.junit.runner.AlwaysPassesTest.class
+})
+public class FATSuite {
+    public final static String[] JAXB_PERMS = { "permission java.lang.RuntimePermission \"accessClassInPackage.com.sun.xml.internal.bind.v2.runtime.reflect\";",
+                                                "permission java.lang.RuntimePermission \"accessClassInPackage.com.sun.xml.internal.bind\";" };
+
+    @ClassRule
+    public static RepeatTests r = RepeatTests
+                    .withoutModification()
+                    .andWith(FeatureReplacementAction.EE7_FEATURES())
+                    .andWith(FeatureReplacementAction.EE9_FEATURES());
+    public static String repeatPhase = "";
+}
