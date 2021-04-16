58b2edb86a6f0dee38e52be37cbb527ae7b3b35f
Thomas Watson <tjwatson@us.ibm.com>
2021-04-15 21:36:36 -0500

    Add checkpoint feature

---
+++ dev/io.openliberty.checkpoint/src/io/openliberty/checkpoint/internal/Activator.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2021 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.checkpoint.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+
+public class Activator implements BundleActivator {
+	private CheckpointImpl checkpoint;
+
+	public void start(BundleContext context) throws Exception {
+		checkpoint = new CheckpointImpl();
+		checkpoint.register(context);
+	}
+
+    public void stop(BundleContext context) throws Exception {
+    	checkpoint.unregister();
+	}
+
+}
---
+++ dev/io.openliberty.checkpoint/src/io/openliberty/checkpoint/spi/Checkpoint.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2021 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.checkpoint.spi;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+@ProviderType
+/**
+ * Service used to perform CRIU checkpoint operations.
+ */
+public interface Checkpoint {
+	/**
+	 * Phase which a snapshot is being taken.
+	 *
+	 */
+	public enum Phase {
+		KERNEL,
+		FEATURE,
+		APP,
+		SERVER
+	}
+
+	/**
+	 * Perform a snapshot for the specified phase with the specified name.
+	 * Before the snapshot is taken the registered {@link SnapshotHook#prepare(Phase)}
+	 * methods are called before the snapshot is taken.  After the snapshot
+	 * is take the registered {@link SnapshotHook#restore(Phase)} are called.
+	 * @param phase the phase to take the snapshot
+	 * @param name the name of the snapshot
+	 * @throws SnapshotFailed if the snapshot fails
+	 */
+	void snapshot(Phase phase, String name) throws SnapshotFailed;
+}
