e0f6f2036e144134832903067ff28b9c1b746015
Isaac Rivera-Rivas <isaac.rivera6@upr.edu>
2021-04-19 11:27:33 -0400

    Fixed comments and clean up

--- dev/io.openliberty.wsoc.internal_fat/fat/src/io/openliberty/wsoc/endpoints/client/basic/AnnotatedConfiguratorClientEP.java
+++ dev/io.openliberty.wsoc.internal_fat/fat/src/io/openliberty/wsoc/endpoints/client/basic/AnnotatedConfiguratorClientEP.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 IBM Corporation and others.
+ * Copyright (c) 2013, 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -129,7 +129,6 @@ public class AnnotatedConfiguratorClientEP implements TestHelper {
         @OnOpen
         public void onOpen(Session sess, EndpointConfig epc) {
             List<Extension> exts = sess.getNegotiatedExtensions();
-            LOG.info("TESTING TESTING TESTING: " + exts);
             _wtr.addMessage("CLIENTNEGOTIATED" + (exts==null ? 0 : exts.size()) );
         }
 
