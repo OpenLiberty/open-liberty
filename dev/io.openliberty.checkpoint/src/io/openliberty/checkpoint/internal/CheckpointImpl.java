f9865548db60edf1f07994fc4a47fe8b23664ecc
Thomas Watson <tjwatson@us.ibm.com>
2021-04-20 10:14:32 -0500

    Change snapshot to take a File

--- dev/io.openliberty.checkpoint/src/io/openliberty/checkpoint/internal/CheckpointImpl.java
+++ dev/io.openliberty.checkpoint/src/io/openliberty/checkpoint/internal/CheckpointImpl.java
@@ -11,7 +11,6 @@
 package io.openliberty.checkpoint.internal;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
@@ -27,89 +26,80 @@ import io.openliberty.checkpoint.spi.SnapshotFailed;
 import io.openliberty.checkpoint.spi.SnapshotFailed.Type;
 import io.openliberty.checkpoint.spi.SnapshotHook;
 
-public class CheckpointImpl implements Checkpoint{
-	private volatile ServiceTracker<SnapshotHook, SnapshotHook> hooks;
-	private volatile ServiceRegistration<Checkpoint> reg;
+public class CheckpointImpl implements Checkpoint {
+    private volatile ServiceTracker<SnapshotHook, SnapshotHook> hooks;
+    private volatile ServiceRegistration<Checkpoint> reg;
 
-    void register(BundleContext context)
-    {
-    	hooks = new ServiceTracker<>(context, SnapshotHook.class, null);
-    	hooks.open();
+    void register(BundleContext context) {
+        hooks = new ServiceTracker<>(context, SnapshotHook.class, null);
+        hooks.open();
         Hashtable<String, Object> props = new Hashtable<>();
-        props.put("osgi.command.function", new String[] {"snapshot"});
+        props.put("osgi.command.function", new String[] { "snapshot" });
         props.put("osgi.command.scope", "criu");
         reg = context.registerService(Checkpoint.class, this, props);
     }
 
     void unregister() {
-    	reg.unregister();
-    	hooks.close();
+        reg.unregister();
+        hooks.close();
     }
 
-	@Override
+    @Override
     @Descriptor("Take a snapshot")
-	public void snapshot(@Descriptor("The phase to snapshot") Phase phase, @Descriptor("Name of snapshot") String name) throws SnapshotFailed {
-		String directory = determineDirectory(name);
-		doSnapshot(phase, directory);
-	}
+    public void snapshot(@Descriptor("The phase to snapshot") Phase phase, @Descriptor("Directory to store the snapshot") File directory) throws SnapshotFailed {
+        doSnapshot(phase, directory);
+    }
 
-	private String determineDirectory(String name) {
-		// TODO need to be more smart than use the current working directory
-		File dir = new File(name);
-		dir.mkdir();
-		return dir.getAbsolutePath();
-	}
+    private void doSnapshot(Phase phase, File directory) throws SnapshotFailed {
+        SnapshotHook[] snapshotHooks = hooks.getServices(new SnapshotHook[0]);
+        prepare(phase, snapshotHooks);
+        System.out.println("Go save the world");
+        try {
+            CheckpointRestore.saveTheWorld(directory.getAbsolutePath());
+        } catch (Exception e) {
+            throw new SnapshotFailed(Type.SNAPSHOT_FAILED, "Failed to create snapshot.", e);
+        } finally {
+            restore(phase, snapshotHooks);
+        }
+        System.out.println("Saved the world");
 
-	private void doSnapshot(Phase phase, String directory) throws SnapshotFailed {
-		SnapshotHook[] snapshotHooks = hooks.getServices(new SnapshotHook[0]);
-		prepare(phase, snapshotHooks);
-		System.out.println("Go save the world");
-		try {
-			CheckpointRestore.saveTheWorld(directory);
-		} catch (Exception e) {
-			throw new SnapshotFailed(Type.SNAPSHOT_FAILED, "Failed to create snapshot.", e);
-		} finally {
-			restore(phase, snapshotHooks);
-		}
-		System.out.println("Saved the world");
+    }
 
-	}
+    private void prepare(Phase phase, SnapshotHook[] snapshotHooks) throws SnapshotFailed {
+        List<SnapshotHook> called = new ArrayList<>(snapshotHooks.length);
+        for (SnapshotHook snapshotHook : snapshotHooks) {
+            try {
+                snapshotHook.prepare(phase);
+                called.add(snapshotHook);
+            } catch (Exception abortCause) {
+                for (SnapshotHook abortHook : called) {
+                    try {
+                        abortHook.abortPrepare(phase, abortCause);
+                    } catch (Exception unexpected) {
+                        // TODO should log
+                    }
+                }
+                throw new SnapshotFailed(Type.PREPARE_ABORT, "Failed to prepare for snapshot.", abortCause);
+            }
+        }
+    }
 
-	private void prepare(Phase phase, SnapshotHook[] snapshotHooks) throws SnapshotFailed {
-		List<SnapshotHook> called = new ArrayList<>(snapshotHooks.length);
-		for (SnapshotHook snapshotHook : snapshotHooks) {
-			try {
-				snapshotHook.prepare(phase);
-				called.add(snapshotHook);
-			} catch (Exception abortCause) {
-				for (SnapshotHook abortHook : called) {
-					try {
-						abortHook.abortPrepare(phase, abortCause);
-					} catch (Exception unexpected) {
-						// TODO should log
-					}
-				}
-				throw new SnapshotFailed(Type.PREPARE_ABORT, "Failed to prepare for snapshot.", abortCause);
-			}
-		}
-	}
-
-	private void restore(Phase phase, SnapshotHook[] snapshotHooks) throws SnapshotFailed {
-		List<SnapshotHook> called = new ArrayList<>(snapshotHooks.length);
-		for (SnapshotHook snapshotHook : snapshotHooks) {
-			try {
-				snapshotHook.restore(phase);
-				called.add(snapshotHook);
-			} catch (Exception abortCause) {
-				for (SnapshotHook abortHook : called) {
-					try {
-						abortHook.abortRestore(phase, abortCause);
-					} catch (Exception unexpected) {
-						// TODO should log
-					}
-				}
-				throw new SnapshotFailed(Type.RESTORE_ABORT, "Failed to restore from snapshot.", abortCause);
-			}
-		}
-	}
+    private void restore(Phase phase, SnapshotHook[] snapshotHooks) throws SnapshotFailed {
+        List<SnapshotHook> called = new ArrayList<>(snapshotHooks.length);
+        for (SnapshotHook snapshotHook : snapshotHooks) {
+            try {
+                snapshotHook.restore(phase);
+                called.add(snapshotHook);
+            } catch (Exception abortCause) {
+                for (SnapshotHook abortHook : called) {
+                    try {
+                        abortHook.abortRestore(phase, abortCause);
+                    } catch (Exception unexpected) {
+                        // TODO should log
+                    }
+                }
+                throw new SnapshotFailed(Type.RESTORE_ABORT, "Failed to restore from snapshot.", abortCause);
+            }
+        }
+    }
 }
