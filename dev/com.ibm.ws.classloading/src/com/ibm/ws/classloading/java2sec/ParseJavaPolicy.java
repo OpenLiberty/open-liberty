6eeadada922e3e87218d0bb6170f53f7bc8b5cc6
Emily Tuczkowski <emilyt@us.ibm.com>
2021-04-27 14:47:53 -0400

    Issue16807-SupportNewJavaPolicyLocationPerOpenJDK10

--- dev/com.ibm.ws.classloading/src/com/ibm/ws/classloading/java2sec/ParseJavaPolicy.java
+++ dev/com.ibm.ws.classloading/src/com/ibm/ws/classloading/java2sec/ParseJavaPolicy.java
@@ -94,6 +94,7 @@ public class ParseJavaPolicy {
                 if (javaHome != null) {
                     // first let's check if this is oracle openjdk version 10 or up, where the java.policy file would 
                     // be under java.home/conf/security
+                    
                     if (javaVendor != null && javaVendor.contains("openjdk") && javaVendor.contains("oracle")) {
                         if (javaVersion != null && javaVersion.indexOf(".") != -1) {
                              String major = javaVersion.substring(0, javaVersion.indexOf(".") - 1 );
