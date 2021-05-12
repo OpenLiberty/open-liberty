ab1b80c0397617f5d7cfab5f67056d4204150663
Andrew Rouse <anrouse@uk.ibm.com>
2021-05-12 13:33:26 +0100

    mpOpenAPI-20: Prep for multiple applications

--- dev/io.openliberty.microprofile.openapi.2.0.internal/src/io/openliberty/microprofile/openapi20/ApplicationRegistry.java
+++ dev/io.openliberty.microprofile.openapi.2.0.internal/src/io/openliberty/microprofile/openapi20/ApplicationRegistry.java
@@ -10,8 +10,12 @@
  *******************************************************************************/
 package io.openliberty.microprofile.openapi20;
 
-import java.util.HashMap;
+import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
@@ -38,74 +42,60 @@ import io.openliberty.microprofile.openapi20.utils.LoggingUtils;
 public class ApplicationRegistry {
 
     private static final TraceComponent tc = Tr.register(ApplicationRegistry.class);
-    
+
     @Reference
     private ApplicationProcessor applicationProcessor;
-    
+
     // Thread safety: access to these fields must be synchronized on this
-    private Map<String, ApplicationInfo> applications = new HashMap<>();
-    private ApplicationInfo currentApp = null;
-    
-    // Thread safety: writes to this field must be synchronized on this
-    private volatile OpenAPIProvider currentProvider = null;
+    private Map<String, ApplicationRecord> applications = new TreeMap<>();
 
     /**
      * The addApplication method is invoked by the {@link ApplicationListener} when it is notified that an application
      * is starting. It only needs to process the application if we have not already found an application that implements
-     * a JAX-RS based REST API. 
+     * a JAX-RS based REST API.
      * 
      * @param newAppInfo
-     *           The ApplicationInfo for the application that is starting.
+     *            The ApplicationInfo for the application that is starting.
      */
     public void addApplication(ApplicationInfo newAppInfo) {
+        ApplicationRecord record = new ApplicationRecord(newAppInfo);
         synchronized (this) {
             if (LoggingUtils.isEventEnabled(tc)) {
                 Tr.event(tc, "Application Processor: Adding application started: appInfo=" + newAppInfo);
             }
 
             // Store the app in our collection
-            applications.put(newAppInfo.getName(), newAppInfo);
+            applications.put(newAppInfo.getName(), record);
+        }
 
-            // Process the application... this will only scan the application if we do not have a current application
-            processApplication(newAppInfo);
+        // Process the application
+        OpenAPIProvider openApiProvider = applicationProcessor.processApplication(newAppInfo);
+        if (openApiProvider != null) {
+            record.providers.add(openApiProvider);
+        }
 
-            if (LoggingUtils.isEventEnabled(tc)) {
-                Tr.event(tc, "Application Processor: Adding application ended: appInfo=" + newAppInfo);
-            }
+        if (LoggingUtils.isEventEnabled(tc)) {
+            Tr.event(tc, "Application Processor: Adding application ended: appInfo=" + newAppInfo);
         }
     }
-    
+
     /**
      * The removeApplication method is invoked by the {@link ApplicationListener} when it is notified that an
-     * application is stopping.  If the application that is stopping is also the currentApp, we need to iterate over the
+     * application is stopping. If the application that is stopping is also the currentApp, we need to iterate over the
      * remaining applications in the collection to find the next one that implements a JAX-RS based REST API, if any.
      * 
      * @param removedAppInfo
-     *           The ApplicationInfo for the application that is stopping.
+     *            The ApplicationInfo for the application that is stopping.
      */
     public void removeApplication(ApplicationInfo removedAppInfo) {
-        synchronized(this) {
+        synchronized (this) {
             if (LoggingUtils.isEventEnabled(tc)) {
                 Tr.event(tc, "Application Processor: Removing application started: appInfo=" + removedAppInfo);
             }
-            
+
             // Remove the app from our collection
             applications.remove(removedAppInfo.getName());
-            
-            // Check to see if the application being remove is the currentApp
-            if (currentApp != null && currentApp.getName().equals(removedAppInfo.getName())) {
-                
-                // The currentApp is being removed... see if any of the other applications implement a JAX-RS REST API
-                currentApp = null;
-                currentProvider = null;
-                for (ApplicationInfo appInfo : applications.values()) {
-                    processApplication(appInfo);
-                    if (currentApp != null) {
-                        break;
-                    }
-                } // FOR
-            }
-            
+
             if (LoggingUtils.isEventEnabled(tc)) {
                 Tr.event(tc, "Application Processor: Removing application ended: appInfo=" + removedAppInfo);
             }
@@ -116,28 +106,49 @@ public class ApplicationRegistry {
      * The getCurrentOpenAPIProvider method returns the OpenAPIProvider for the current application, if any.
      * 
      * @return OpenAPIProvider
-     *          The current OpenAPIProvider.
+     *         The current OpenAPIProvider.
      */
     public OpenAPIProvider getCurrentOpenAPIProvider() {
-        return currentProvider;
+        synchronized (this) {
+            for (ApplicationRecord record : applications.values()) {
+                if (!record.providers.isEmpty()) {
+                    return record.providers.get(0);
+                }
+            }
+        }
+        return null;
     }
 
     /**
-     * The processApplication method checks to see if we have already found an application that implements
-     * a JAX-RS based REST API. If not, it attempts to proces the specified application, it it exposes a JAX-RS REST
-     * API, it sets it as the currentApp. 
-     *
-     * @param appInfo
-     *          The ApplicationInfo for the application to be processed.
+     * Merges the models from all configured applications and returns them.
+     * 
+     * @return an {@code OpenAPIProvider} holding a merged OpenAPI model or {@code null} if there are no OpenAPI models to merge
      */
-    private void processApplication(ApplicationInfo appInfo) {
-        // Only scan the application if we do not have a current application
-        if (currentApp == null) {
-            currentProvider = applicationProcessor.processApplication(appInfo);
-            if (currentProvider != null) {
-                currentApp = appInfo;
-            }
+    public OpenAPIProvider getMergedOpenAPIProvider() {
+        List<OpenAPIProvider> providers = getProvidersToMerge();
+        if (providers.isEmpty()) {
+            return null;
         }
+        
+        return providers.get(0);
+    }
+
+    private List<OpenAPIProvider> getProvidersToMerge() {
+        synchronized (this) {
+            return applications.values().stream()
+                               .flatMap(r -> r.providers.stream())
+                               .collect(toList());
+        }
+    }
+
+    private static class ApplicationRecord {
+        public ApplicationRecord(ApplicationInfo info) {
+            this.info = info;
+        }
+
+        @SuppressWarnings("unused")
+        private ApplicationInfo info;
+        private List<OpenAPIProvider> providers = new ArrayList<>();
     }
 
 }
