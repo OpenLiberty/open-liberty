fe312b4e1f2136d0c3eeae612975626cee347159
Paul Nicolucci <pnicolucci@gmail.com>
2021-06-04 09:22:40 -0400

    Issue #17421: Remove SharedServer from CDITests

--- dev/com.ibm.ws.webcontainer.servlet.3.1_fat/fat/src/com/ibm/ws/webcontainer/servlet31/fat/tests/CDITests.java
+++ dev/com.ibm.ws.webcontainer.servlet.3.1_fat/fat/src/com/ibm/ws/webcontainer/servlet31/fat/tests/CDITests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2020 IBM Corporation and others.
+ * Copyright (c) 2015, 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,33 +10,37 @@
  *******************************************************************************/
 package com.ibm.ws.webcontainer.servlet31.fat.tests;
 
-import java.util.Set;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.util.logging.Logger;
 
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.ibm.websphere.simplicity.ShrinkHelper;
-import com.ibm.ws.fat.util.LoggingTest;
-import com.ibm.ws.fat.util.SharedServer;
-import com.ibm.ws.fat.util.browser.WebBrowser;
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
 
+import componenttest.annotation.Server;
 import componenttest.custom.junit.runner.FATRunner;
 import componenttest.custom.junit.runner.Mode;
 import componenttest.custom.junit.runner.Mode.TestMode;
+import componenttest.topology.impl.LibertyServer;
 
 @RunWith(FATRunner.class)
-public class CDITests extends LoggingTest {
+public class CDITests {
 
     private static final Logger LOG = Logger.getLogger(CDITests.class.getName());
 
-    @ClassRule
-    public static SharedServer SHARED_SERVER = new SharedServer("servlet31_cdiServer");
+    @Server("servlet31_cdiServer")
+    public static LibertyServer server;
 
     private static final String CDI12_TEST_JAR_NAME = "CDI12Test";
     private static final String CDI12_TEST_APP_NAME = "CDI12Test";
@@ -44,61 +48,55 @@ public class CDITests extends LoggingTest {
     @BeforeClass
     public static void setupClass() throws Exception {
         // Build the CDI12TestV2 jar to add to the war app as a lib
-        JavaArchive CDI12TestJar = ShrinkHelper.buildJavaArchive(CDI12_TEST_JAR_NAME + ".jar",
+        JavaArchive cdi12TestJar = ShrinkHelper.buildJavaArchive(CDI12_TEST_JAR_NAME + ".jar",
                                                                  "com.ibm.ws.webcontainer.servlet_31_fat.cdi12test.jar.cdi.beans");
-        CDI12TestJar = (JavaArchive) ShrinkHelper.addDirectory(CDI12TestJar, "test-applications/CDI12Test.jar/resources");
+        cdi12TestJar = (JavaArchive) ShrinkHelper.addDirectory(cdi12TestJar, "test-applications/CDI12Test.jar/resources");
         // Build the war app CDI12Test.war and add the dependencies
-        WebArchive CDI12TestApp = ShrinkHelper.buildDefaultApp(CDI12_TEST_APP_NAME + ".war",
+        WebArchive cdi12TestApp = ShrinkHelper.buildDefaultApp(CDI12_TEST_APP_NAME + ".war",
                                                                "com.ibm.ws.webcontainer.servlet_31_fat.cdi12test.war.beans",
                                                                "com.ibm.ws.webcontainer.servlet_31_fat.cdi12test.war.cdi.interceptors",
                                                                "com.ibm.ws.webcontainer.servlet_31_fat.cdi12test.war.interfaces",
                                                                "com.ibm.ws.webcontainer.servlet_31_fat.cdi12test.war.listeners",
                                                                "com.ibm.ws.webcontainer.servlet_31_fat.cdi12test.war.servlets");
-        CDI12TestApp = (WebArchive) ShrinkHelper.addDirectory(CDI12TestApp, "test-applications/CDI12Test.war/resources");
-        CDI12TestApp = CDI12TestApp.addAsLibrary(CDI12TestJar);
-        // Verify if the apps are in the server before trying to deploy them
-        if (SHARED_SERVER.getLibertyServer().isStarted()) {
-            Set<String> appInstalled = SHARED_SERVER.getLibertyServer().getInstalledAppNames(CDI12_TEST_APP_NAME);
-            LOG.info("addAppToServer : " + CDI12_TEST_APP_NAME + " already installed : " + !appInstalled.isEmpty());
-            if (appInstalled.isEmpty())
-                ShrinkHelper.exportDropinAppToServer(SHARED_SERVER.getLibertyServer(), CDI12TestApp);
-        }
-        SHARED_SERVER.startIfNotStarted();
-        SHARED_SERVER.getLibertyServer().waitForStringInLog("CWWKZ0001I.* " + CDI12_TEST_APP_NAME);
+        cdi12TestApp = (WebArchive) ShrinkHelper.addDirectory(cdi12TestApp, "test-applications/CDI12Test.war/resources");
+        cdi12TestApp = cdi12TestApp.addAsLibrary(cdi12TestJar);
+
+        // Export the application.
+        ShrinkHelper.exportDropinAppToServer(server, cdi12TestApp);
+
+        // Start the server and use the class name so we can find logs easily.
+        server.startServer(CDITests.class.getSimpleName() + ".log");
     }
 
     @AfterClass
     public static void testCleanup() throws Exception {
         // test cleanup
-        if (SHARED_SERVER.getLibertyServer() != null && SHARED_SERVER.getLibertyServer().isStarted()) {
-            SHARED_SERVER.getLibertyServer().stopServer(null);
+        if (server != null && server.isStarted()) {
+            server.stopServer();
         }
     }
 
     @Test
     @Mode(TestMode.LITE)
     public void testServletInjection() throws Exception {
-        WebBrowser wb = createWebBrowserForTestCase();
         String[] expected = { "Test Exit", "ConstructorBean::Servlet", "MethodBean::Servlet", "ServletFieldBean", "ProducerInjected::Servlet", "postConstructCalled::Servlet" };
-        String[] unexpected = { "Test Exception" };
-        verifyResponse(wb, "/CDI12Test/CDIServletInjected", expected, unexpected);
+
+        verifyStringsInResponse("/CDI12Test", "/CDIServletInjected", expected);
     }
 
     @Test
     @Mode(TestMode.LITE)
     public void testServletIntercepor() throws Exception {
-        WebBrowser wb = createWebBrowserForTestCase();
         String[] expected = { "Test Passed! InterceptedBean : ServiceMethodInterceptor was called.",
                               "Test Passed! CDIServletIntercepted : SendResponseInterceptor was called.",
                               "Test Passed! InterceptedBean : ServiceMethodInterceptor was called." };
-        String[] unexpected = { "Test Failed!" };
-        verifyResponse(wb, "/CDI12Test/CDIServletIntercepted", expected, unexpected);
+
+        verifyStringsInResponse("/CDI12Test", "/CDIServletIntercepted", expected);
     }
 
     @Test
     @Mode(TestMode.LITE)
     public void testAsyncListeenerCDI() throws Exception {
-        WebBrowser wb = createWebBrowserForTestCase();
         String[] expected = { "onStartAsync :class com.ibm.ws.webcontainer.servlet_31_fat.cdi12test.jar.cdi.beans.ConstructorBean:",
                               "onStartAsync :class com.ibm.ws.webcontainer.servlet_31_fat.cdi12test.jar.cdi.beans.MethodBean:",
                               "onStartAsync :class com.ibm.ws.webcontainer.servlet_31_fat.cdi12test.jar.cdi.beans.AsyncListenerFieldBean:",
@@ -109,17 +107,25 @@ public class CDITests extends LoggingTest {
                               "onComplete :class com.ibm.ws.webcontainer.servlet_31_fat.cdi12test.jar.cdi.beans.AsyncListenerFieldBean:",
                               "onComplete :Interceptor was called:",
                               "onStartAsync ::postConstructCalled:" };
-        String[] unexpected = { "Test Exception" };
-        verifyResponse(wb, "/CDI12Test/CDIAsyncServlet", expected, unexpected);
+
+        verifyStringsInResponse("/CDI12Test", "/CDIAsyncServlet", expected);
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see com.ibm.ws.fat.util.LoggingTest#getSharedServer()
-     */
-    @Override
-    protected SharedServer getSharedServer() {
-        return SHARED_SERVER;
+    private void verifyStringsInResponse(String contextRoot, String path, String[] expectedResponseStrings) throws Exception {
+        WebConversation wc = new WebConversation();
+        wc.setExceptionsThrownOnErrorStatus(false);
+
+        WebRequest request = new GetMethodWebRequest("http://" + server.getHostname() + ":" + server.getHttpDefaultPort() + contextRoot + path);
+        WebResponse response = wc.getResponse(request);
+        LOG.info("Response : " + response.getText());
+
+        assertEquals("Expected " + 200 + " status code was not returned!",
+                     200, response.getResponseCode());
+
+        String responseText = response.getText();
+
+        for (String expectedResponse : expectedResponseStrings) {
+            assertTrue("The response did not contain: " + expectedResponse, responseText.contains(expectedResponse));
+        }
     }
 }
