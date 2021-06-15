c3c2ece0fcbc5ba9c1a740c89c5078dc3c320842
Andy McCright <j.andrew.mccright@gmail.com>
2021-06-15 17:56:34 -0500

    Removing @SkipForRepeat for Jakarta REST 3.0 tests.
    - many tests just work now
    - some updates to tests required for different implementation
    - implemented app security autofeature
    - more detailed comments when tests legitimately should be skipped

---
+++ dev/com.ibm.websphere.appserver.features/visibility/auto/io.openliberty.restfulWS3.0-appSecurity4.0.feature
@@ -0,0 +1,13 @@
+-include= ~${workspace}/cnf/resources/bnd/feature.props
+symbolicName=io.openliberty.restfulWS3.0-appSecurity4.0
+visibility=private
+IBM-App-ForceRestart: install, \
+ uninstall
+Subsystem-Version: 1.1.0
+IBM-Provision-Capability: osgi.identity; filter:="(&(type=osgi.subsystem.feature)(osgi.identity=io.openliberty.restfulWS-3.0))", \
+ osgi.identity; filter:="(&(type=osgi.subsystem.feature)(osgi.identity=io.openliberty.appSecurity-4.0))"
+-bundles=io.openliberty.restfulWS30.appSecurity
+IBM-Install-Policy: when-satisfied
+kind=beta
+edition=core
+WLP-Activation-Type: parallel
--- dev/com.ibm.ws.jaxrs.2.0_fat/fat/src/com/ibm/ws/jaxrs20/fat/securitycontext/SecurityContextTest.java
+++ dev/com.ibm.ws.jaxrs.2.0_fat/fat/src/com/ibm/ws/jaxrs20/fat/securitycontext/SecurityContextTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 IBM Corporation and others.
+ * Copyright (c) 2019, 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -20,13 +20,11 @@ import com.ibm.websphere.simplicity.ShrinkHelper;
 import com.ibm.ws.jaxrs.fat.security.servlet.SecurityContextTestServlet;
 
 import componenttest.annotation.Server;
-import componenttest.annotation.SkipForRepeat;
 import componenttest.annotation.TestServlet;
 import componenttest.custom.junit.runner.FATRunner;
 import componenttest.topology.impl.LibertyServer;
 
 @RunWith(FATRunner.class)
-@SkipForRepeat("EE9_FEATURES") // currently broken due to multiple issues
 public class SecurityContextTest {
 
     private static final String secwar = "security";
