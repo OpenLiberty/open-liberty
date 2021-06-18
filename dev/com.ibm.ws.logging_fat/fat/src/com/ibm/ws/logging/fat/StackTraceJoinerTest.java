92a58bc4e48e61240bfc69902d6073dc5d470b84
Adrian Kalafut <Adrian.Kalafut@ibm.com>
2021-06-18 13:44:12 -0400

    Add an additional stack joiner test to verify that events printed are not longer than the system stream print event size threshold

--- dev/com.ibm.ws.logging_fat/fat/src/com/ibm/ws/logging/fat/StackTraceJoinerTest.java
+++ dev/com.ibm.ws.logging_fat/fat/src/com/ibm/ws/logging/fat/StackTraceJoinerTest.java
@@ -45,6 +45,9 @@ public class StackTraceJoinerTest {
     protected static RemoteFile consoleLog;
 
     protected static final int CONN_TIMEOUT = 10;
+    protected static final int threadsToRun = 5;
+    final int expectedErrOut = 2500;
+    protected static final int pageHits = 500;
 
     protected static void hitWebPage(String contextRoot, String servletName, boolean failureAllowed) throws MalformedURLException, IOException, ProtocolException {
         try {
@@ -117,7 +120,82 @@ public class StackTraceJoinerTest {
     }
 
     @Test
-    public void testPrintStackTraceMultipleThreads() {
-        // TODO: Test printStackTrace in multiple threads and ensure that messages are not leaking from one stack trace to another
+    public void testPrintStackTraceMultipleThreads() throws Exception {
+        messagesLog = server.getDefaultLogFile();
+        server.setMarkToEndOfLog(messagesLog);
+        Thread[] threads = new Thread[threadsToRun];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        for (int j = 0; j < pageHits; j++) {
+                            //Try: Numbering hits to see which thread is missing
+                            hitWebPage("broken-servlet", "ExceptionPrintingServlet", false);
+                        }
+                    } catch (Exception e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            });
+            threads[i].start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        //sleep to check, keep redoing multiple times, fail if failure
+        List<String> systemErrOutput = server.findStringsInLogsUsingMark("SystemErr.*", messagesLog);
+        for (int j = 0; j < 5; j++) {
+            if (systemErrOutput.size() != expectedErrOut) {
+                systemErrOutput = server.findStringsInLogsUsingMark("SystemErr.*", messagesLog);
+                //Try: print number to check if its growing or not
+                Thread.sleep(500);
+            }
+        }
+        assertTrue("Expected " + expectedErrOut + " events but found " + systemErrOutput.size() + " messages.", systemErrOutput.size() == expectedErrOut);
+
+    }
+
+    @Test
+    public void testPrintStackTraceEventSizeThreshold() throws Exception {
+        // Set default to the maximum system stream print event size threshold
+        int maxSystemStreamPrintEventThresholdSize = 256 * 1024;
+
+        // JDK's system stream print event flush size threshold
+        int jdkSystemStreamPrintEventThreshold = 8 * 1024;
+        int expectedSystemStreamPrintEventSize = jdkSystemStreamPrintEventThreshold;
+
+        //Get the system stream print event threshold size which was set
+        if (server.getServerEnv().getProperty("WLP_LOGGING_MAX_SYSTEM_STREAM_PRINT_EVENT_SIZE") != null) {
+            try {
+                maxSystemStreamPrintEventThresholdSize = Integer.valueOf(server.getServerEnv().getProperty("WLP_LOGGING_MAX_SYSTEM_STREAM_PRINT_EVENT_SIZE"));
+            } catch (NumberFormatException e) {
+            }
+        }
+
+        // Find the maximum possible length that the stack trace can be
+        while (expectedSystemStreamPrintEventSize < maxSystemStreamPrintEventThresholdSize)
+            expectedSystemStreamPrintEventSize += jdkSystemStreamPrintEventThreshold;
+
+        // Specify the length of the error to be longer than maxSystemStreamPrintEventSize
+        String sizeRequestParamater = String.valueOf((expectedSystemStreamPrintEventSize + 1) * 2);
+
+        messagesLog = server.getDefaultLogFile();
+        server.setMarkToEndOfLog(messagesLog);
+        hitWebPage("broken-servlet", "ExtremelyLongStackTrace?size=" + sizeRequestParamater, false);
+        List<String> systemErrOutput = server.findStringsInLogsUsingMark("SystemErr.*", messagesLog);
+
+        String errorMessagePrefix = "SystemErr                                                    R ";
+
+        // Check that each system error output message does not surpass the threshold
+        for (int i = 0; i < systemErrOutput.size(); i++) {
+            // Get the original message output in the stacktrace
+            String currentSystemStreamPrintEvent = systemErrOutput.get(i).substring(systemErrOutput.get(i).indexOf(errorMessagePrefix) + errorMessagePrefix.length());
+            assertTrue("Expected system stream print event size to be <= " + expectedSystemStreamPrintEventSize + " but found that the size is "
+                       + currentSystemStreamPrintEvent.length() + ".",
+                       currentSystemStreamPrintEvent.length() <= expectedSystemStreamPrintEventSize);
+        }
+
     }
 }
