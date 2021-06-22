7541de334446504f58b02624d8a6e745a9a016b3
Ut Le <utle@us.ibm.com>
2021-06-22 17:54:11 -0500

    Revert "Revert "Create the lock base on hashtable in subject instead of authentication data""

--- dev/com.ibm.ws.security.authentication/src/com/ibm/ws/security/authentication/AuthenticationData.java
+++ dev/com.ibm.ws.security.authentication/src/com/ibm/ws/security/authentication/AuthenticationData.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 IBM Corporation and others.
+ * Copyright (c) 2011, 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -92,4 +92,9 @@ public interface AuthenticationData {
      */
     @Sensitive
     public Object get(String key);
+
+    /**
+     * Empty authentication data.
+     */
+    public boolean isEmpty();
 }
