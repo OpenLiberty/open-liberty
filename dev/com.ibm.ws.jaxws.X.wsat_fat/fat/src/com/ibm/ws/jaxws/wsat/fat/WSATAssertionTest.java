2c56bd4c6536b061866aa2dbb327828c2625d0f2
Thomas Johnson <tjjohnso@us.ibm.com>
2021-06-23 13:14:07 -0500

    Fix copyrights

--- dev/com.ibm.ws.jaxws.X.wsat_fat/fat/src/com/ibm/ws/jaxws/wsat/fat/WSATAssertionTest.java
+++ dev/com.ibm.ws.jaxws.X.wsat_fat/fat/src/com/ibm/ws/jaxws/wsat/fat/WSATAssertionTest.java
@@ -1,14 +1,13 @@
-/*
- * IBM Confidential
+/*******************************************************************************
+ * Copyright (c) 2021 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * OCO Source Materials
- *
- * WLP Copyright IBM Corp. 2015
- *
- * The source code for this program is not published or otherwise divested 
- * of its trade secrets, irrespective of what has been deposited with the 
- * U.S. Copyright Office.
- */
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package com.ibm.ws.jaxws.wsat.fat;
 
 import static org.junit.Assert.assertNotNull;
