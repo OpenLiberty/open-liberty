f5cf38d6efd0071b6aa2d5c2b615fbfcd2714a7e
Thomas Johnson <tjjohnso@us.ibm.com>
2021-06-24 15:15:31 -0500

    code review changes

--- dev/com.ibm.ws.jaxws.X.wsat_fat/test-applications/policyAttachmentsService1/src/com/ibm/ws/policyattachments/service1/HelloService1.java
+++ dev/com.ibm.ws.jaxws.X.wsat_fat/test-applications/policyAttachmentsService1/src/com/ibm/ws/policyattachments/service1/HelloService1.java
@@ -1,14 +1,5 @@
+package com.ibm.ws.policyattachments.service1;
 
-/*******************************************************************************
- * Copyright (c) 2021 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/package com.ibm.ws.policyattachments.service1;
 
 import javax.jws.WebService;
 
