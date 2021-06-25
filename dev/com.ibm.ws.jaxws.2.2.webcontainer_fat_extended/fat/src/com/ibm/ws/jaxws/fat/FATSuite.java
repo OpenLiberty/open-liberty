5390fead6177b6227eb46c9ccaae97b5db897699
Berksan Ates <ates.berksan@gmail.com>
2021-06-25 14:46:16 -0500

    Test loads are balanced between splitted test buckets

--- dev/com.ibm.ws.jaxws.2.2.webcontainer_fat_extended/fat/src/com/ibm/ws/jaxws/fat/FATSuite.java
+++ dev/com.ibm.ws.jaxws.2.2.webcontainer_fat_extended/fat/src/com/ibm/ws/jaxws/fat/FATSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 IBM Corporation and others.
+ * Copyright (c) 2019,2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -24,12 +24,9 @@ import componenttest.rules.repeater.RepeatTests;
  */
 @RunWith(Suite.class)
 @SuiteClasses({
-
                 EJBServiceRefBndTest.class,
-                EndpointPropertiesTest.class,
                 HandlerChainTest.class,
                 HandlerChainWithWebServiceClientTest.class,
-                HttpConduitPropertiesTest.class,
                 POJOServiceSecurityTest.class,
                 PortComponentRefTest.class,
                 WebServiceInWebXMLTest.class,
