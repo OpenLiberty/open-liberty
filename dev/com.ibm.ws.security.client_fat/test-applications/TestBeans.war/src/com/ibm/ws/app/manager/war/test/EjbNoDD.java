ec6d4e1954eacfb66b1faefb5f9eecb9935c4019
Jesse Van Hill <jlvanhil@us.ibm.com>
2021-07-21 16:46:47 -0500

    Issue #14501: For the applications that are stored as binary in com.ibm.ws.security.client_fat, decompile them so they are built at build time.

---
+++ dev/com.ibm.ws.security.client_fat/test-applications/TestBeans.war/src/com/ibm/ws/app/manager/war/test/EjbNoDD.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2021 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package com.ibm.ws.app.manager.war.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(description = "A servlet to drive the test EJB beans", urlPatterns = { "/EjbNoDD" })
+public class EjbNoDD extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @EJB
+    private InterfaceTestStatelessEjbNoDD test1;
+
+    @EJB
+    private InterfaceTestStatefulEjbNoDD test2;
+
+    @EJB
+    private InterfaceTestSingletonEjbNoDD test3;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        test(request, response);
+    }
+
+    private void test(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("Testing EJB with no DD...");
+        String message = this.test1.test();
+        writer.println(message);
+        message = this.test2.test();
+        writer.println(message);
+        message = this.test3.test();
+        writer.println(message);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        test(request, response);
+    }
+}
---
+++ dev/com.ibm.ws.security.client_fat/test-applications/ejb31DD.jar/src/com/ibm/ws/app/manager/war/test/TestBeanStatelessDD.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2021 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package com.ibm.ws.app.manager.war.test;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class TestBeanStatelessDD implements InterfaceTestStatelessEjbDD {
+    @Override
+    public String test() {
+        return "Stateless bean. DD.";
+    }
+}
