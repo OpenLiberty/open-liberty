3f411433f6033d97c062e5269d5db4f6c1f5e214
Alaine DeMyers <alaine@us.ibm.com>
2021-07-28 10:32:30 -0500

    Make an spi out of the keyStringResolver up version

--- dev/com.ibm.ws.crypto.passwordutil/src/com/ibm/wsspi/security/crypto/KeyStringResolver.java
+++ dev/com.ibm.ws.crypto.passwordutil/src/com/ibm/wsspi/security/crypto/KeyStringResolver.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 IBM Corporation and others.
+ * Copyright (c) 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,8 +8,23 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-/**
- * @version 1.1.0
- */
-@org.osgi.annotation.versioning.Version("1.2.0")
 package com.ibm.wsspi.security.crypto;
+
+/**
+ * The interface for resolving the aes encryption key.
+ *
+ * @ibm-spi
+ *
+ */
+public interface KeyStringResolver {
+
+    /**
+     * Getting the key to used to for aes encryption. Takes a string that may hold the key
+     * value and returns a char[] of the encryption key.
+     *
+     * @param val
+     * @return char[]
+     */
+    public char[] getKey(String val);
+
+}
