0124b3b8b4b8c7246a981d47abd246c18fac43c0
Andy McCright <j.andrew.mccright@gmail.com>
2021-08-12 16:04:08 -0500

    [18147] Initial impl of `restfulWSLogging-3.0` feature to log HTTP data

---
+++ dev/io.openliberty.restfulWS.internal.logging.filter/src/io/openliberty/restfulWS/internal/logging/filter/LoggingFilterFeature.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2021 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.restfulWS.internal.logging.filter;
+
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
+import javax.ws.rs.container.DynamicFeature;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.FeatureContext;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Determines whether or not to apply the {@link LoggingFilter}.
+ */
+@Provider
+public class LoggingFilterFeature implements DynamicFeature {
+
+    private static final String WILDCARD = ".*";
+    private static final String FILTER_TERM = AccessController.doPrivileged((PrivilegedAction<String>) () -> 
+        System.getProperty("io.openliberty.restfulWS.logging.filter.regex", WILDCARD));
+
+
+    @Override
+    public void configure(ResourceInfo resourceInfo, FeatureContext featureContext) {
+        //TODO: determine via config property whether to register the filter or not
+        if (FILTER_TERM.equals(WILDCARD)
+            || (resourceInfo.getClass() + "#" + resourceInfo.getResourceMethod()).matches(FILTER_TERM)) {
+
+            System.out.println("configure registering LoggingFilter");
+            featureContext.register(LoggingFilter.class);
+        }
+    }
+}
