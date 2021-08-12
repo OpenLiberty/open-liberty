18d46747c8c6fa107338b701c0a2c7e430667027
Ut Le <utle@us.ibm.com>
2021-08-12 16:54:41 -0500

    fixed comments format

--- dev/com.ibm.ws.security.authentication.filter_fat/fat/src/com/ibm/ws/security/authentication/filter/fat/DynamicAuthFilterTest.java
+++ dev/com.ibm.ws.security.authentication.filter_fat/fat/src/com/ibm/ws/security/authentication/filter/fat/DynamicAuthFilterTest.java
@@ -46,12 +46,14 @@ public class DynamicAuthFilterTest extends CommonTest {
     }
 
     /**
-     * Test description: - Reconfigure the server to set a malformed IP in the
-     * authfilter - Do a successful a servlet call since all resources will be
-     * protected. - Reconfigure the server to add elements on the authfilter. -
-     * do another successful a servlet call. Expected results: - 1) A 200 should
-     * be received since all resources will be protected.(but expect the error
-     * message to pop out). - 2) Another 200 since we use a valid configuration.
+     * Test description:
+     * - Reconfigure the server to set a malformed IP in the authfilter
+     * - Do a successful a servlet call since all resources will be protected.
+     * - Reconfigure the server to add elements on the authfilter.
+     * - Do another successful a servlet call.
+     * Expected results:
+     * - 1) A 200 should be received since all resources will be protected.(but expect the error message to pop out).
+     * - 2) Another 200 since we use a valid configuration.
      */
 
     // @Test
@@ -70,21 +72,19 @@ public class DynamicAuthFilterTest extends CommonTest {
         commonSuccessfulLtpaServletCall(ssoCookie);
 
         List<String> checkMsgs = new ArrayList<String>();
-        // Since the CWWKS4357I message should not show since the SPN is being
-        // specified in the server.xml
         checkMsgs.add(MALFORMED_IPRANGE_SPECIFIED_CWWKS4354E);
         testHelper.waitForMessages(checkMsgs, AuthFilterConstants.MESSAGE_NOT_EXPECTED,
                                    AuthFilterConstants.MESSAGE_NOT_EXPECTED_LOG_SEARCH_TIMEOUT);
     }
 
     /**
-     * Test description: - Do a Servlet call with valid LTPA token and correct
-     * URL Pattern. - Then reconfigure the server in order to set an authFilter
-     * config with badURLPattern. - Do another a Servlet call with valid LTPA
-     * Token but set to a different URL Pattern. Expected results: - 1) A 200
-     * should be received since we reconfigured the server to include all
-     * required configs. - 2) After the server has been reconfigured to add the
-     * bad URL pattern, we should get a 401
+     * Test description:
+     * - Do a Servlet call with valid LTPA token and correct URL Pattern.
+     * - Then reconfigure the server in order to set an authFilter config with badURLPattern.
+     * - Do another a Servlet call with valid LTPA Token but set to a different URL Pattern.
+     * Expected results:
+     * - 1) A 200 should be received since we reconfigured the server to include all required configs.
+     * - 2) After the server has been reconfigured to add the bad URL pattern, we should get a 401
      */
 
     @Test
@@ -99,14 +99,14 @@ public class DynamicAuthFilterTest extends CommonTest {
     }
 
     /**
-     * Test description: - Then reconfigure the server in order to set an
-     * authFilter config with badURLPattern. - Do a Servlet call with valid LTPA
-     * Token but set to a different URL Pattern. - Then reconfigure the server
-     * in order to set a good url pattern. - Do a Servlet call with correct LTPA
-     * token and valid URL Pattern. Expected results: - 1) After the server has
-     * been reconfigured to add the bad URL pattern, we should get a 401 - 2) A
-     * 200 should be received since we reconfigured the server to include all
-     * required configs.
+     * Test description:
+     * - Then reconfigure the server in order to set an authFilter config with badURLPattern.
+     * - Do a Servlet call with valid LTPA Token but set to a different URL Pattern.
+     * - Then reconfigure the server in order to set a good url pattern.
+     * - Do a Servlet call with correct LTPA token and valid URL Pattern.
+     * Expected results:
+     * - 1) After the server has been reconfigured to add the bad URL pattern, we should get a 401
+     * - 2) A 200 should be received since we reconfigured the server to include all required configs.
      */
     @Test
     public void testAuthFilterURLPatternInvalidThenValid() throws Exception {
@@ -120,13 +120,13 @@ public class DynamicAuthFilterTest extends CommonTest {
     }
 
     /**
-     * Test description: - Do a Servlet call with valid LTPA token and correct
-     * URL Pattern. - Then reconfigure the server in order to modify the
-     * requestURL to use an invalid url pattern - Do another Servlet call with
-     * valid LTPA Token but bad requestURL URL pattern. Expected results: - 1) A
-     * 200 should be received since we reconfigured the server to include all
-     * required configs. - 2) After the server has been reconfigured to add the
-     * bad URL pattern, we should get a 401
+     * Test description:
+     * - Do a Servlet call with valid LTPA token and correct URL Pattern.
+     * - Then reconfigure the server in order to modify the requestURL to use an invalid url pattern
+     * - Do another Servlet call with valid LTPA Token but bad requestURL URL pattern.
+     * Expected results:
+     * - 1) A 200 should be received since we reconfigured the server to include all required configs.
+     * - 2) After the server has been reconfigured to add the bad URL pattern, we should get a 401
      */
 
     @Test
@@ -145,20 +145,17 @@ public class DynamicAuthFilterTest extends CommonTest {
     }
 
     /**
-     * Test description: - Then reconfigure the server in order to modify the
-     * requestURL to use an invalid url pattern - Do a Servlet call with valid
-     * LTPA Token but bad requestURL URL pattern. - Then reconfigure the server
-     * in order to set a good url pattern. - Do a Servlet call with correct LTPA
-     * token and valid URL Pattern. Expected results: - 1) After the server has
-     * been reconfigured to add the bad URL pattern, we should get a 401 - 2) A
-     * 200 should be received since we reconfigured the server to include all
-     * required configs.
+     * Test description:
+     * - Then reconfigure the server in order to modify the requestURL to use an invalid url pattern
+     * - Do a Servlet call with valid LTPA Token but bad requestURL URL pattern. - Then reconfigure the server in order to set a good url pattern.
+     * - Do a Servlet call with correct LTPA token and valid URL Pattern.
+     * Expected results:
+     * - 1) After the server has been reconfigured to add the bad URL pattern, we should get a 401
+     * - 2) A 200 should be received since we reconfigured the server to include all required configs.
      */
 
     @Test
     public void testAuthFilterRequestURLPatternInvalidThenValid() throws Exception {
-        // reconfigure the server to add bad url pattern then do a unsucessful
-        // LTPA cookie call
         testHelper.reconfigureServer("serverRequestURLBadUrlPattern.xml", name.getMethodName(),
                                      AuthFilterConstants.NO_MSGS, AuthFilterConstants.DONT_RESTART_SERVER);
         commonUnsuccessfulLtpaServletCall(ssoCookie);
@@ -171,14 +168,13 @@ public class DynamicAuthFilterTest extends CommonTest {
     }
 
     /**
-     * Test description: - Do a Servlet call with valid LTPA token and
-     * requestUrl matchType equals to contain - Then reconfigure the server in
-     * order to modify the requestURL to use matchtype equals to notContain - Do
-     * another Servlet call with valid LTPA Token but matchtype equals to
-     * notContain Expected results: - 1) A 200 should be received since we
-     * reconfigured the server to include all required configs. - 2) After the
-     * server has been reconfigured to matchtype equals to notContain, we should
-     * get a 401
+     * Test description:
+     * - Do a Servlet call with valid LTPA token and requestUrl matchType equals to contain
+     * - Then reconfigure the server in order to modify the requestURL to use matchtype equals to notContain
+     * - Do another Servlet call with valid LTPA Token but matchtype equals to notContain
+     * Expected results:
+     * - 1) A 200 should be received since we reconfigured the server to include all required configs.
+     * - 2) After the server has been reconfigured to matchtype equals to notContain, we should get a 401
      */
 
     @Test
@@ -187,8 +183,6 @@ public class DynamicAuthFilterTest extends CommonTest {
                                      AuthFilterConstants.DONT_RESTART_SERVER);
         commonSuccessfulLtpaServletCall(ssoCookie);
 
-        // reconfigure the server to use notcontain then do a unsucessful LTPA
-        // call
         List<String> startMsgs = new ArrayList<String>();
         startMsgs.add(AUTHENTICATION_FILTER_MODIFIED_CWWKS4359I);
         testHelper.reconfigureServer("serverRequestURLBadUrlPattern.xml", name.getMethodName(), startMsgs,
@@ -197,20 +191,18 @@ public class DynamicAuthFilterTest extends CommonTest {
     }
 
     /**
-     * Test description: - Reconfigure the server in order to modify the
-     * requestURL matchType to notContain - Do a Servlet call with valid LTPA
-     * Token but setting the matchType notContain - Then reconfigure the server
-     * in order to set the match type to contain - Do a Servlet call with
-     * correct LTPA token and valid URL Pattern. Expected results: - 1) After
-     * the server has been reconfigured to set the matchType to notContain, we
-     * should get a 401 - 2) A 200 should be received since we reconfigured the
-     * server to include all required configs.
+     * Test description:
+     * - Reconfigure the server in order to modify the requestURL matchType to notContain
+     * - Do a Servlet call with valid LTPA Token but setting the matchType notContain
+     * - Then reconfigure the server in order to set the match type to contain
+     * - Do a Servlet call with correct LTPA token and valid URL Pattern.
+     * Expected results:
+     * - 1) After the server has been reconfigured to set the matchType to notContain, we should get a 401
+     * - 2) A 200 should be received since we reconfigured the server to include all required configs.
      */
 
     @Test
     public void testAuthFilterRequestUrlMatchNotContainsToContain() throws Exception {
-        // reconfigure the server to use notcontain then do a unsucessful LTPA
-        // call
         testHelper.reconfigureServer("serverRequestURLBadUrlPattern.xml", name.getMethodName(),
                                      AuthFilterConstants.NO_MSGS, AuthFilterConstants.DONT_RESTART_SERVER);
         commonUnsuccessfulLtpaServletCall(ssoCookie);
@@ -221,14 +213,14 @@ public class DynamicAuthFilterTest extends CommonTest {
     }
 
     /**
-     * Test description: - Reconfigure the server to change the requestURL to
-     * webapp with matchtype equals contains. - Do a servlet call - Then
-     * reconfigure the server in order to modify the webAppL to use the match
-     * type notContain - Do another Servlet call with valid LTPA Token but the
-     * matchtype set to notContain Expected results: - 1) A 200 should be
-     * received since we reconfigured the server to include all required
-     * configs. - 2) After the server has been reconfigured to add the
-     * notContain, we should get a 401
+     * Test description:
+     * - Reconfigure the server to change the requestURL to webapp with matchtype equals contains.
+     * - Do a servlet call
+     * - Then reconfigure the server in order to modify the webAppL to use the match type notContain
+     * - Do another Servlet call with valid LTPA Token but the matchtype set to notContain
+     * Expected results:
+     * - 1) A 200 should be received since we reconfigured the server to include all required configs.
+     * - 2) After the server has been reconfigured to add the notContain, we should get a 401
      */
 
     @Test
@@ -246,14 +238,14 @@ public class DynamicAuthFilterTest extends CommonTest {
     }
 
     /**
-     * Test description: - Reconfigure the server in order to modify the webAppL
-     * to use the match type notContain - Do another LTAP Servlet call with
-     * valid SPENGO Token but the matchtype set to notContain - Reconfigure the
-     * server to change the requestURL to webapp with matchtype equals contains.
-     * - Do a servlet call Expected results: - 1) After the server has been
-     * reconfigured to add the notContain, we should get a 401 - 2) A 200 should
-     * be received since we reconfigured the server to include all required
-     * configs.
+     * Test description:
+     * - Reconfigure the server in order to modify the webAppL to use the match type notContain
+     * - Do another LTAP Servlet call with valid SPENGO Token but the matchtype set to notContain
+     * - Reconfigure the server to change the requestURL to webapp with matchtype equals contains.
+     * - Do a servlet call
+     * Expected results:
+     * - 1) After the server has been reconfigured to add the notContain, we should get a 401
+     * - 2) A 200 should be received since we reconfigured the server to include all required configs.
      */
 
     @Test
@@ -273,14 +265,15 @@ public class DynamicAuthFilterTest extends CommonTest {
     }
 
     /**
-     * Test description: - Do a servlet call using the reqUrl. - Reconfigure the
-     * server to change the requestURL to webapp with match type equals
-     * notContain - Reconfigure the server to change the matchtype equals to
-     * contains. - Do a servlet call with webAPP specified in the server.xml
-     * Expected results: - 1) A 200 should be obtained since we are using the
-     * default config. - 2) A 401 should be obtain because we are using a
-     * matchtype notContain in the webAPP - 3) A 200 should be received since we
-     * reconfigured the server to include all required configs.
+     * Test description:
+     * - Do a servlet call using the reqUrl.
+     * - Reconfigure the server to change the requestURL to webapp with match type equals notContain
+     * - Reconfigure the server to change the matchtype equals to contains.
+     * - Do a servlet call with webAPP specified in the server.xml
+     * Expected results:
+     * - 1) A 200 should be obtained since we are using the default config.
+     * - 2) A 401 should be obtain because we are using a matchtype notContain in the webAPP
+     * - 3) A 200 should be received since we reconfigured the server to include all required configs.
      */
 
     @Test
