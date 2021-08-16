ad113396dcbec63aa0830f74beff2672566de16e
Tracy Burroughs <29557092+tkburroughs@users.noreply.github.com>
2021-08-16 11:42:13 -0500

    Issue #13717: EE 9 EJB Bindings FAT Enable the EJB bindings FAT tests to run with Jakarta EE 9 features.

--- dev/com.ibm.ws.ejbcontainer.bindings_fat/fat/src/com/ibm/ws/ejbcontainer/bindings/fat/tests/SimpleBindingNameTest.java
+++ dev/com.ibm.ws.ejbcontainer.bindings_fat/fat/src/com/ibm/ws/ejbcontainer/bindings/fat/tests/SimpleBindingNameTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2020 IBM Corporation and others.
+ * Copyright (c) 2019, 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -24,6 +24,7 @@ import org.junit.runner.RunWith;
 
 import com.ibm.ejb3x.SimpleBindingName.web.SimpleBindingNameTestServlet;
 import com.ibm.websphere.simplicity.ShrinkHelper;
+import com.ibm.websphere.simplicity.ShrinkHelper.DeployOptions;
 
 import componenttest.annotation.Server;
 import componenttest.annotation.TestServlet;
@@ -59,7 +60,7 @@ public class SimpleBindingNameTest extends FATServletClient {
     public static LibertyServer server;
 
     @ClassRule
-    public static RepeatTests r = RepeatTests.with(FeatureReplacementAction.EE7_FEATURES().fullFATOnly().forServers("com.ibm.ws.ejbcontainer.bindings.fat.server")).andWith(FeatureReplacementAction.EE8_FEATURES().forServers("com.ibm.ws.ejbcontainer.bindings.fat.server"));
+    public static RepeatTests r = RepeatTests.with(FeatureReplacementAction.EE7_FEATURES().fullFATOnly().forServers("com.ibm.ws.ejbcontainer.bindings.fat.server")).andWith(FeatureReplacementAction.EE8_FEATURES().forServers("com.ibm.ws.ejbcontainer.bindings.fat.server")).andWith(FeatureReplacementAction.EE9_FEATURES().fullFATOnly().forServers("com.ibm.ws.ejbcontainer.bindings.fat.server"));
 
     @BeforeClass
     public static void setUp() throws Exception {
@@ -76,7 +77,7 @@ public class SimpleBindingNameTest extends FATServletClient {
         SimpleBindingNameTestApp.addAsModules(SimpleBindingNameEJB, SimpleBindingNameWeb);
         ShrinkHelper.addDirectory(SimpleBindingNameTestApp, "test-applications/SimpleBindingNameTestApp.ear/resources");
 
-        ShrinkHelper.exportDropinAppToServer(server, SimpleBindingNameTestApp);
+        ShrinkHelper.exportDropinAppToServer(server, SimpleBindingNameTestApp, DeployOptions.SERVER_ONLY);
 
         server.startServer();
     }
