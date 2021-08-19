e5bdbd902d72871e48b1f35711c316bad71570ad
Aruna Vemulapalli <aruna@us.ibm.com>
2021-08-18 21:43:00 -0500

    wssecurity async flow updates

--- dev/com.ibm.ws.org.apache.cxf.rt.ws.security.3.4.1/src/org/apache/cxf/ws/security/wss4j/WSS4JInInterceptor.java
+++ dev/com.ibm.ws.org.apache.cxf.rt.ws.security.3.4.1/src/org/apache/cxf/ws/security/wss4j/WSS4JInInterceptor.java
@@ -43,12 +43,16 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
+import com.ibm.ws.ffdc.annotation.FFDCIgnore;
+
 import org.apache.cxf.attachment.AttachmentUtil;
 import org.apache.cxf.binding.soap.SoapFault;
 import org.apache.cxf.binding.soap.SoapMessage;
 import org.apache.cxf.binding.soap.SoapVersion;
 import org.apache.cxf.binding.soap.saaj.SAAJInInterceptor;
 import org.apache.cxf.binding.soap.saaj.SAAJUtils;
+import org.apache.cxf.common.classloader.ClassLoaderUtils;
+import org.apache.cxf.common.classloader.ClassLoaderUtils.ClassLoaderHolder;
 import org.apache.cxf.common.i18n.Message;
 import org.apache.cxf.common.logging.LogUtils;
 import org.apache.cxf.common.util.PropertyUtils;
@@ -648,14 +652,35 @@ public class WSS4JInInterceptor extends AbstractWSS4JInterceptor {
             throw new WSSecurityException(WSSecurityException.ErrorCode.FAILURE, ex);
         }
     }
-
+    @FFDCIgnore(ClassNotFoundException.class)
     protected CallbackHandler getCallback(RequestData reqData) throws WSSecurityException, TokenStoreException {
         Object o =
             SecurityUtils.getSecurityPropertyValue(SecurityConstants.CALLBACK_HANDLER,
                                                    (SoapMessage)reqData.getMsgContext());
         CallbackHandler cbHandler = null;
+        ClassLoaderHolder origLoader = null;
         try {
             cbHandler = SecurityUtils.getCallbackHandler(o);
+        } catch (ClassNotFoundException cnfe) { //Liberty code change start
+            SoapMessage message = ((SoapMessage)reqData.getMsgContext());
+            boolean inboundAndRequester = (!MessageUtils.isOutbound(message)) && (MessageUtils.isRequestor(message));
+            boolean isAsync = message != null && message.getExchange() != null && !message.getExchange().isSynchronous();
+            if (inboundAndRequester && isAsync) {                
+                ClassLoader loader = ((SoapMessage)reqData.getMsgContext()).getExchange().getBus().getExtension(ClassLoader.class);
+                LOG.log(Level.FINE, "async and requester, we are in the cnfe path, tccl = " + loader);
+                if (loader != null) {
+                    origLoader = ClassLoaderUtils.setThreadContextClassloader(loader);
+                }
+                try {
+                    cbHandler = SecurityUtils.getCallbackHandler(o);
+                } catch (Exception ex2) {
+                    throw new WSSecurityException(WSSecurityException.ErrorCode.FAILURE, ex2);
+                } finally {
+                    if (origLoader != null) {
+                        origLoader.reset();
+                    }
+                }
+            } //Liberty code change end
         } catch (Exception ex) {
             throw new WSSecurityException(WSSecurityException.ErrorCode.FAILURE, ex);
         }
