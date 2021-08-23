1ed5f15018b56304f7561c49ab757585ce415cc7
Andy McCright <j.andrew.mccright@gmail.com>
2021-08-23 15:05:15 -0500

    Ensure consistent handling of Accept-Language header
    - including "blank" languages - should be treated as "und" (undefined)

---
+++ dev/com.ibm.ws.jaxrs.2.1_fat_extended/test-applications/acceptLanguage/src/jaxrs21/fat/acceptlanguage/MyFilter.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2021 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package jaxrs21.fat.acceptlanguage;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Response;
+
+public class MyFilter implements ContainerRequestFilter {
+
+    @Override
+    public void filter(ContainerRequestContext ctx) throws IOException {
+        List<Locale> locales = ctx.getAcceptableLanguages();
+        String localesStr = locales.stream().map(Locale::toLanguageTag).collect(Collectors.joining(","));
+        ctx.abortWith(Response.ok(localesStr).build());
+    }
+
+}
