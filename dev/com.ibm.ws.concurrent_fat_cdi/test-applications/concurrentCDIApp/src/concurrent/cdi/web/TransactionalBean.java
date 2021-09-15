6073beb9a75444edac1fe3ceaf58082f1ed17d1a
Nathan Rauh <nathan.rauh@us.ibm.com>
2021-09-15 14:28:55 -0500

    Issue #18400 enable usage of Transactional by existing tests

--- dev/com.ibm.ws.concurrent_fat_cdi/test-applications/concurrentCDIApp/src/concurrent/cdi/web/TransactionalBean.java
+++ dev/com.ibm.ws.concurrent_fat_cdi/test-applications/concurrentCDIApp/src/concurrent/cdi/web/TransactionalBean.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017,2020 IBM Corporation and others.
+ * Copyright (c) 2017,2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,6 +14,8 @@ import java.io.Serializable;
 
 import jakarta.inject.Singleton;
 import jakarta.transaction.TransactionSynchronizationRegistry;
+import jakarta.transaction.Transactional;
+import jakarta.transaction.Transactional.TxType;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -22,15 +24,37 @@ import javax.naming.NamingException;
 public class TransactionalBean implements Serializable {
     private static final long serialVersionUID = 8518443344930037109L;
 
-    private static Object getTransactionKey() throws NamingException {
+    static Object getTransactionKey() throws NamingException {
         TransactionSynchronizationRegistry tranSyncRegistry = (TransactionSynchronizationRegistry) new InitialContext().lookup("java:comp/TransactionSynchronizationRegistry");
         return tranSyncRegistry.getTransactionKey();
     }
 
+    @Transactional(TxType.MANDATORY)
+    public Object runAsMandatory() throws Exception {
+        return getTransactionKey();
+    }
+
+    @Transactional(TxType.NEVER)
     public Object runAsNever() throws Exception {
         return getTransactionKey();
     }
 
+    @Transactional(TxType.NOT_SUPPORTED)
+    public Object runAsNotSupported() throws Exception {
+        return getTransactionKey();
+    }
+
+    @Transactional(TxType.REQUIRED)
+    public Object runAsRequired() throws Exception {
+        return getTransactionKey();
+    }
+
+    @Transactional(TxType.REQUIRES_NEW)
+    public Object runAsRequiresNew() throws Exception {
+        return getTransactionKey();
+    }
+
+    @Transactional(TxType.SUPPORTS)
     public Object runAsSupports() throws Exception {
         return getTransactionKey();
     }
