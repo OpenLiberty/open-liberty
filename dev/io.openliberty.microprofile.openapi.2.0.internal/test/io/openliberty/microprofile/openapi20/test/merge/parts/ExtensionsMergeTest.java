e11db4cf0ea3431bf80e48c06a28131a3bc35992
Andrew Rouse <anrouse@uk.ibm.com>
2021-11-02 17:41:52 +0000

    mpOpenApi-2.0: Test model equality on extensions
    Check that having different extensions is sufficient to cause two
    otherwise equal model objects to be non-equal.

--- dev/io.openliberty.microprofile.openapi.2.0.internal/test/io/openliberty/microprofile/openapi20/test/merge/parts/ExtensionsMergeTest.java
+++ dev/io.openliberty.microprofile.openapi.2.0.internal/test/io/openliberty/microprofile/openapi20/test/merge/parts/ExtensionsMergeTest.java
@@ -11,14 +11,22 @@
 package io.openliberty.microprofile.openapi20.test.merge.parts;
 
 import static io.openliberty.microprofile.openapi20.test.merge.AssertModel.assertModelMaps;
+import static org.hamcrest.Matchers.hasKey;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.microprofile.openapi.OASFactory;
+import org.eclipse.microprofile.openapi.models.Extensible;
 import org.eclipse.microprofile.openapi.models.OpenAPI;
 import org.junit.Assert;
 import org.junit.Test;
+
+import io.openliberty.microprofile.openapi20.merge.ModelEquality;
+import io.openliberty.microprofile.openapi20.merge.ModelType;
 
 /**
  *
@@ -56,6 +64,41 @@ public class ExtensionsMergeTest {
         validateMap(primaryOpenAPI.getExtensions(), doc3.getExtensions());
         primaryOpenAPI = TestUtil.merge();
         Assert.assertNull(primaryOpenAPI.getExtensions());
+    }
+
+    @Test
+    public void testExtensionEquality() {
+        for (ModelType mt : ModelType.values()) {
+            Object testInstance = mt.createInstance();
+            if (testInstance instanceof Extensible) {
+                testExtensionEquality(mt);
+            }
+        }
+    }
+
+    public void testExtensionEquality(ModelType mt) {
+        Extensible<?> a = (Extensible<?>) mt.createInstance();
+        Extensible<?> b = (Extensible<?>) mt.createInstance();
+
+        // Note, we're testing the ModelEquality.equals method here
+        // which is why we're using assertTrue, rather than assertEquals()
+        // or assertModelsEqual()
+        assertTrue(mt + " not equal for original object", ModelEquality.equals(a, b));
+
+        a.addExtension("x-foo", "bar");
+        assertThat(a.getExtensions(), hasKey("x-foo"));
+        assertFalse(mt + " equal after adding extension", ModelEquality.equals(a, b));
+
+        b.addExtension("x-foo", "bar");
+        assertTrue(mt + " not equal after adding similar extensions", ModelEquality.equals(a, b));
+        b.removeExtension("x-foo");
+
+        b.addExtension("x-foo", "baz");
+        assertFalse(mt + " equal after adding equal extensions", ModelEquality.equals(a, b));
+        b.removeExtension("x-foo");
+
+        b.addExtension("x-qux", "bar");
+        assertFalse(mt + " equal after adding different extension key", ModelEquality.equals(a, b));
     }
 
     private void validateMap(Map<String, ?> map, Map<?, ?>... maps) {
