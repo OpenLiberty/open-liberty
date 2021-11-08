26d6a79b412d6e6a86d34fa3810f3c9523bed43a
Thomas Watson <tjwatson@us.ibm.com>
2021-11-08 10:40:11 -0600

    Updates from review
    - Update copyright dates
    - Update debug message to use "system" transformer instead of "global"

--- dev/com.ibm.ws.classloading/src/com/ibm/ws/classloading/internal/AppClassLoader.java
+++ dev/com.ibm.ws.classloading/src/com/ibm/ws/classloading/internal/AppClassLoader.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2020 IBM Corporation and others.
+ * Copyright (c) 2011, 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -337,7 +337,7 @@ public class AppClassLoader extends ContainerClassLoader implements SpringLoader
                 }
             } else {
                 if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
-                    Tr.debug(tc, "Attempt to transform " + name + " from shared class cache because we have global tranformers");
+                    Tr.debug(tc, "Attempt to transform " + name + " from shared class cache because we have system tranformers");
                 }
                 try {
                     bytes = toTransform.getActualBytes();
