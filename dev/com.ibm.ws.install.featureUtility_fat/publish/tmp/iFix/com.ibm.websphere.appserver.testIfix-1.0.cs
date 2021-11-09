398760b236b2b7374acd9bd8f005792c965d9fbc
Jiwoo-Lim <jiwoo.lim@ibm.com>
2021-11-09 17:17:01 -0500

    set install dir
    fix

---
+++ dev/com.ibm.ws.install.featureUtility_fat/publish/tmp/iFix/com.ibm.websphere.appserver.testIfix-1.0.cs
@@ -0,0 +1,4 @@
+#Wed Aug 25 04:36:56 BST 2021
+lib/platform/testIfix-1.0.mf=b834bdfe85c74e13e1fe8b2d65398f21
+#incorrect checksum to "fake" iFix -> will need to check checksum inside iFix/xml and iFix/lpmf files
+lib/com.ibm.ws.install.testIfix_1.0.jar=9c91bc743e4b880fc3e240795b22ced3
