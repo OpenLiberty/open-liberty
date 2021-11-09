adc052bd6794e9c4edb3aa3dd1ede617ec3fbec4
Anthony <owner.tony@live.ca>
2021-11-09 09:38:10 -0500

    Adding OpenTracing-3.0 fat bucket

---
+++ dev/io.openliberty.opentracing.3.x_fat/test-applications/mpOpenTracing/src/io/openliberty/opentracing/internal/test/mpot/POJO.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2020 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.opentracing.internal.test.mpot;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.opentracing.Traced;
+
+/**
+ * POJO to test Traced annotation.
+ */
+@ApplicationScoped
+@Traced
+public class POJO {
+    /**
+     * Method that we expect to be Traced implicitly.
+     */
+    public void annotatedClassMethodImplicitlyTraced() {
+        System.out.println("Called annotatedClassMethodImplicitlyTraced");
+    }
+}
