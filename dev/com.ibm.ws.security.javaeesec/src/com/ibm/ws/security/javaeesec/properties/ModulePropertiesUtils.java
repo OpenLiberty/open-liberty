d5373bcbd74471c124603f910a2448e1c0466ab4
jdmcclur <jdmcclur@us.ibm.com>
2021-12-06 11:16:41 -0600

    revert chanegs for ModulePropertiesUtils.java since I didnt end up changing it.

--- dev/com.ibm.ws.security.javaeesec/src/com/ibm/ws/security/javaeesec/properties/ModulePropertiesUtils.java
+++ dev/com.ibm.ws.security.javaeesec/src/com/ibm/ws/security/javaeesec/properties/ModulePropertiesUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2021 IBM Corporation and others.
+ * Copyright (c) 2017, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -78,7 +78,7 @@ public class ModulePropertiesUtils {
         HttpAuthenticationMechanism ham = getHttpAuthenticationMechanism(false);
         if (ham != null) {
             return true;
-        } 
+        }
         return false;
     }
 
