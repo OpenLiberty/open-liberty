c7838b31d2127ed01af8769bcbe733f473f1d208
Nathan Rauh <nathan.rauh@us.ibm.com>
2021-12-06 13:39:50 -0600

    Issue #18668 test case for asynchronous method join

--- dev/com.ibm.ws.concurrent_fat_cdi/test-applications/concurrentCDIApp/src/concurrent/cdi/web/DependentScopedBean.java
+++ dev/com.ibm.ws.concurrent_fat_cdi/test-applications/concurrentCDIApp/src/concurrent/cdi/web/DependentScopedBean.java
@@ -10,11 +10,16 @@
  *******************************************************************************/
 package concurrent.cdi.web;
 
+import java.util.AbstractMap;
+import java.util.Map.Entry;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.CompletionStage;
 
 import jakarta.enterprise.concurrent.Asynchronous;
 import jakarta.enterprise.context.Dependent;
+import jakarta.transaction.SystemException;
+import jakarta.transaction.UserTransaction;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -39,6 +44,21 @@ public class DependentScopedBean {
             future.completeExceptionally(x);
         }
         return future;
+    }
+
+    /**
+     * Return an entry consisting of (transaction status, lookup result)
+     */
+    @Asynchronous(executor = "concurrent/sampleExecutor")
+    public CompletableFuture<Entry<Integer, Object>> lookUpAndGetTransactionStatus(String jndiName) {
+        try {
+            UserTransaction tx = InitialContext.doLookup("java:comp/UserTransaction");
+            Entry<Integer, Object> result = new AbstractMap.SimpleEntry<Integer, Object>( //
+                            tx.getStatus(), InitialContext.doLookup(jndiName));
+            return Asynchronous.Result.complete(result);
+        } catch (NamingException | SystemException x) {
+            throw new CompletionException(x);
+        }
     }
 
     public void setBoolean(boolean value) {
