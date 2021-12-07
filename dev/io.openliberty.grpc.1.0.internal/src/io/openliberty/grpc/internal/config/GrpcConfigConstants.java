376c52143a55b41c7f752d54f943a8a23b9cd64b
Bill Lucy <wtlucy@gmail.com>
2021-12-07 09:14:35 -0500

    remove invalid and unused grpc httpEndpoint config prop

--- dev/io.openliberty.grpc.1.0.internal/src/io/openliberty/grpc/internal/config/GrpcConfigConstants.java
+++ dev/io.openliberty.grpc.1.0.internal/src/io/openliberty/grpc/internal/config/GrpcConfigConstants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 IBM Corporation and others.
+ * Copyright (c) 2020, 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,7 +15,6 @@ package io.openliberty.grpc.internal.config;
  */
 public class GrpcConfigConstants {
     public static final String TARGET_PROP = "target";
-    public static final String HTTP_ENDPOINTS_PROP = "httpEndpoints";
     public static final String MAX_INBOUND_MSG_SIZE_PROP = "maxInboundMessageSize";
     public static final String SERVER_INTERCEPTORS_PROP = "serverInterceptors";
 }
