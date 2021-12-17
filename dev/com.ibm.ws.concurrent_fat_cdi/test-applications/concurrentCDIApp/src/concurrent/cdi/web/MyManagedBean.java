0af0ddb815532e224f48ffa27fbec6c79808dbbc
Nathan Rauh <nathan.rauh@us.ibm.com>
2021-12-17 08:52:15 -0600

    Issue #18668 interrupt and obtrude asynchronous methods

--- dev/com.ibm.ws.concurrent_fat_cdi/test-applications/concurrentCDIApp/src/concurrent/cdi/web/MyManagedBean.java
+++ dev/com.ibm.ws.concurrent_fat_cdi/test-applications/concurrentCDIApp/src/concurrent/cdi/web/MyManagedBean.java
@@ -12,6 +12,10 @@ package concurrent.cdi.web;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import jakarta.enterprise.concurrent.Asynchronous;
 
@@ -25,6 +29,22 @@ public class MyManagedBean {
         try {
             return Asynchronous.Result.complete(InitialContext.doLookup(jndiName));
         } catch (NamingException x) {
+            throw new CompletionException(x);
+        }
+    }
+
+    /**
+     * Exchanges a reference to the running thread for a CountDownLatch,
+     * and then awaits the latch.
+     * This is useful for giving the test case access to the thread upon which
+     * the asynchronous method is running and holding up its completion.
+     */
+    @Asynchronous(executor = "java:app/env/concurrent/sampleExecutorRef")
+    public CompletableFuture<Boolean> exchangeAndAwait(Exchanger<Object> exchanger, long timeout, TimeUnit unit) {
+        try {
+            CountDownLatch latch = (CountDownLatch) exchanger.exchange(Thread.currentThread(), timeout, unit);
+            return Asynchronous.Result.complete(latch.await(timeout, unit));
+        } catch (InterruptedException | TimeoutException x) {
             throw new CompletionException(x);
         }
     }
