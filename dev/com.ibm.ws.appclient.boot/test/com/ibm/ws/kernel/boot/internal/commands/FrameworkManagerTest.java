824da3a80b1039c17f5aa739b605df8707bced80
Thomas Watson <tjwatson@us.ibm.com>
2022-01-07 15:43:27 -0600

    Adjust unit test for FrameworkManager changes

--- dev/com.ibm.ws.appclient.boot/test/com/ibm/ws/kernel/boot/internal/commands/FrameworkManagerTest.java
+++ dev/com.ibm.ws.appclient.boot/test/com/ibm/ws/kernel/boot/internal/commands/FrameworkManagerTest.java
@@ -33,6 +33,7 @@ import com.ibm.ws.kernel.boot.cmdline.Utils;
 import com.ibm.ws.kernel.launch.internal.FrameworkManager;
 import com.ibm.ws.kernel.launch.service.ClientRunner;
 import com.ibm.ws.kernel.launch.service.FrameworkReady;
+import com.ibm.wsspi.logprovider.LogProvider;
 
 import junit.framework.Assert;
 import test.common.SharedOutputManager;
@@ -162,7 +163,7 @@ public class FrameworkManagerTest {
     public void testClientStartFrameworkException() throws Throwable {
         TestFrameworkManager fm = new TestFrameworkManager() {
             @Override
-            protected Framework initFramework(BootstrapConfig config) {
+            protected Framework initFramework(BootstrapConfig config, LogProvider logProvider) throws BundleException {
                 throw new TestException();
             }
         };
@@ -202,7 +203,7 @@ public class FrameworkManagerTest {
         }
 
         @Override
-        protected Framework initFramework(BootstrapConfig config) throws BundleException {
+        protected Framework initFramework(BootstrapConfig config, LogProvider logProvider) throws BundleException {
             frameworkStarted = true;
             return FrameworkManagerTest.this.framework;
         }
