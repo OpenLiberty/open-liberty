f670821f88e82d30cead805f39036def86833144
Andrew Rouse <anrouse@uk.ibm.com>
2022-01-13 17:02:05 +0000

    Update restConnector features for mpOpenApi-3.0
    These were missed during development of mpOpenApi-3.0

--- dev/io.openliberty.rest.handler.validator.openapi.2.0/src/io/openliberty/rest/handler/validator/openapi20/ValidatorSchemaRESTHandler.java
+++ dev/io.openliberty.rest.handler.validator.openapi.2.0/src/io/openliberty/rest/handler/validator/openapi20/ValidatorSchemaRESTHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 IBM Corporation and others.
+ * Copyright (c) 2019, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -200,8 +200,14 @@ public class ValidatorSchemaRESTHandler implements RESTHandler {
             try {
                 return getClass().getClassLoader().loadClass("javax.jms.ConnectionFactory");
             } catch (ClassNotFoundException e) {
-                return null;
             }
+
+            try {
+                return getClass().getClassLoader().loadClass("jakarta.jms.ConnectionFactory");
+            } catch (ClassNotFoundException e) {
+            }
+
+            return null;
         });
         return jmsClass != null;
     }
