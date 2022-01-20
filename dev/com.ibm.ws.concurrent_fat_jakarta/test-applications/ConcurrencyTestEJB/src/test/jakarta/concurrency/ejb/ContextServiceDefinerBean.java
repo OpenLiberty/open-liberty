360701368312b10fe5e11d5f996eb2193bc4b7a3
Nathan Rauh <nathan.rauh@us.ibm.com>
2022-01-20 16:27:10 -0600

    Issue #18669 forgot to check in a file

---
+++ dev/com.ibm.ws.concurrent_fat_jakarta/test-applications/ConcurrencyTestEJB/src/test/jakarta/concurrency/ejb/ContextServiceDefinerBean.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package test.jakarta.concurrency.ejb;
+
+import static jakarta.enterprise.concurrent.ContextServiceDefinition.APPLICATION;
+
+import jakarta.ejb.EJBException;
+import jakarta.ejb.Stateless;
+import jakarta.enterprise.concurrent.ContextService;
+import jakarta.enterprise.concurrent.ContextServiceDefinition;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import com.ibm.websphere.simplicity.config.ManagedExecutorService;
+
+import test.context.list.ListContext;
+import test.context.location.ZipCode;
+import test.context.timing.Timestamp;
+
+// Use the same name as a context service in ConcurrencyTestServlet,
+// which must be permitted because it is scoped to a different module.
+@ContextServiceDefinition(name = "java:module/concurrent/ZLContextSvc",
+                          propagated = { ZipCode.CONTEXT_NAME, ListContext.CONTEXT_NAME, APPLICATION },
+                          cleared = Timestamp.CONTEXT_NAME,
+                          unchanged = "Priority")
+@Stateless
+public class ContextServiceDefinerBean {
+    public ContextService lookup() {
+        try {
+            return InitialContext.doLookup("java:module/concurrent/ZLContextSvc");
+        } catch (NamingException x) {
+            throw new EJBException(x);
+        }
+    }
+}
