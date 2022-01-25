514a88f7b0e0008bb16f18bd6638b1eb637eb98c
Jesse Van Hill <jlvanhil@us.ibm.com>
2022-01-25 14:33:22 -0600

    Fix test failure in RTC283807.

--- dev/com.ibm.ws.security.javaeesec_fat.2/fat/src/com/ibm/ws/security/javaeesec/fat/MultipleModuleGlobalLoginTest.java
+++ dev/com.ibm.ws.security.javaeesec_fat.2/fat/src/com/ibm/ws/security/javaeesec/fat/MultipleModuleGlobalLoginTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2021 IBM Corporation and others.
+ * Copyright (c) 2018, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -40,6 +40,7 @@ import componenttest.custom.junit.runner.Mode.TestMode;
 import componenttest.topology.impl.LibertyServer;
 import componenttest.topology.impl.LibertyServerFactory;
 
+@SuppressWarnings("restriction")
 @RunWith(FATRunner.class)
 @Mode(TestMode.FULL)
 public class MultipleModuleGlobalLoginTest extends JavaEESecTestBase {
@@ -231,6 +232,7 @@ public class MultipleModuleGlobalLoginTest extends JavaEESecTestBase {
     @Test
     public void testMultipleModuleWarsOverrideFormHAMNoRootContext() throws Exception {
         Log.info(logClass, getCurrentTestName(), "-----Entering " + getCurrentTestName());
+        assumeNotWindowsEe9();
 
         // create module1, form login, redirect, ldap1. grouponly.
         WCApplicationHelper.createWar(myServer, TEMP_DIR, WAR1_NAME, true, JAR_NAME, false, "web.jar.base", "web.war.servlets.form.get.redirect",
