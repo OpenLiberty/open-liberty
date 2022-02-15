e391eae1db181687aeddb2de60b8cf59cb7e2af4
Adam Yoho <ayoho@us.ibm.com>
2022-02-15 16:11:13 -0600

    Issue 20058 and 20060: Add initial logout token validation
    Adds validation of the logout token format and signature. Specifically, this verifies that:
    - The logout token is in JWS format if the client configuration requires it (namely, there is no `keyManagementKeyAlias` configured)
    - The logout token is in JWE format if the client configuration requires it (in this case, there _is_ a `keyManagementKeyAlias` configured)
    - The JWT can be parsed successfully (either as a JWS or a JWE)
        - In the case of a JWE, the token is decrypted and the nested JWS is extracted
    - The signature of the token is valid
    
    For #20058
    For #20060

--- dev/com.ibm.ws.security.openidconnect.clients.common/src/com/ibm/ws/security/openidconnect/backchannellogout/LogoutTokenValidator.java
+++ dev/com.ibm.ws.security.openidconnect.clients.common/src/com/ibm/ws/security/openidconnect/backchannellogout/LogoutTokenValidator.java
@@ -10,12 +10,67 @@
  *******************************************************************************/
 package com.ibm.ws.security.openidconnect.backchannellogout;
 
-import com.ibm.websphere.security.jwt.JwtToken;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.consumer.JwtContext;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
+import com.ibm.websphere.ras.Tr;
+import com.ibm.websphere.ras.TraceComponent;
+import com.ibm.websphere.security.jwt.JwtToken;
+import com.ibm.ws.ffdc.annotation.FFDCIgnore;
+import com.ibm.ws.security.openidconnect.client.jose4j.util.Jose4jUtil;
+import com.ibm.ws.security.openidconnect.clients.common.ConvergedClientConfig;
+import com.ibm.wsspi.ssl.SSLSupport;
+
+@Component(name = "com.ibm.ws.security.openidconnect.backchannellogout.LogoutTokenValidator", service = {}, property = { "service.vendor=IBM" })
 public class LogoutTokenValidator {
 
+    private static TraceComponent tc = Tr.register(LogoutTokenValidator.class);
+
+    private static SSLSupport SSL_SUPPORT = null;
+
+    private ConvergedClientConfig config = null;
+    private Jose4jUtil jose4jUtil = null;
+
+    @Reference
+    protected void setSslSupport(SSLSupport sslSupport) {
+        SSL_SUPPORT = sslSupport;
+    }
+
+    protected void unsetSslSupport() {
+        SSL_SUPPORT = null;
+    }
+
+    /**
+     * Do not use; needed for this to be a valid @Component object.
+     */
+    public LogoutTokenValidator() {
+    }
+
+    public LogoutTokenValidator(ConvergedClientConfig config) {
+        this.config = config;
+        jose4jUtil = new Jose4jUtil(SSL_SUPPORT);
+    }
+
+    /**
+     * Valides an OIDC back-channel logout token per https://openid.net/specs/openid-connect-backchannel-1_0.html.
+     */
+    @FFDCIgnore(Exception.class)
     public JwtToken validateToken(String logoutTokenString) throws BackchannelLogoutException {
-        // TODO
+        try {
+            JwtContext jwtContext = jose4jUtil.validateJwtStructureAndGetContext(logoutTokenString, config);
+            JwtClaims claims = jose4jUtil.validateJwsSignature(jwtContext, config);
+            // TODO;
+            // 3. Validate the iss, aud, and iat Claims in the same way they are validated in ID Tokens
+            // 4. Verify that the Logout Token contains a sub Claim, a sid Claim, or both.
+            // 5. Verify that the Logout Token contains an events Claim whose value is JSON object containing the member name http://schemas.openid.net/event/backchannel-logout.
+            // 6. Verify that the Logout Token does not contain a nonce Claim.
+
+        } catch (Exception e) {
+            String errorMsg = Tr.formatMessage(tc, "BACKCHANNEL_LOGOUT_TOKEN_ERROR", new Object[] { e });
+            throw new BackchannelLogoutException(errorMsg, e);
+        }
         return null;
     }
 
