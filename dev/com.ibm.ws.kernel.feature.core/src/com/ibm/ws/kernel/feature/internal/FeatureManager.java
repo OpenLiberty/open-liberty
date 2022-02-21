f6cbe4b32ff96669222e71fc2323063900a625b6
Chuck Bridgham <cbridgha@us.ibm.com>
2022-02-21 11:54:09 -0500

    Remove WL Resolver error reporting differences

--- dev/com.ibm.ws.kernel.feature.core/src/com/ibm/ws/kernel/feature/internal/FeatureManager.java
+++ dev/com.ibm.ws.kernel.feature.core/src/com/ibm/ws/kernel/feature/internal/FeatureManager.java
@@ -1322,7 +1322,7 @@ public class FeatureManager implements FeatureProvisioner, FrameworkReady, Manag
     private Result createResultFromCompleteList(Repository repository, Set<String> rootFeatures) {
         final Set<String> missing = new HashSet<>();
         final Set<String> resolved = new LinkedHashSet<>();
-
+        System.out.println("In Complete List Resolver!!!!!!!!!!!!!!!!!!!!!!!!!");
         for (String featureName : rootFeatures) {
             ProvisioningFeatureDefinition feature = repository.getFeature(featureName);
             if (feature == null) {
@@ -1775,17 +1775,17 @@ public class FeatureManager implements FeatureProvisioner, FrameworkReady, Manag
             boolean isExtension = missing.indexOf(":") > -1;
             String altName = featureRepository.matchesAlternate(missing);
             //Check if using Open Liberty before suggesting install util for missing features
-            if (!getProductInfoDisplayName().startsWith(PRODUCT_INFO_STRING_OPEN_LIBERTY)) {
+            /** if (!getProductInfoDisplayName().startsWith(PRODUCT_INFO_STRING_OPEN_LIBERTY)) {
                 if (isRootFeature && !isExtension) {
                     // Only report this message for core features included as root features in the server.xml
                     Tr.error(tc, "UPDATE_MISSING_CORE_FEATURE_ERROR", missing, locationService.getServerName());
                 } else {
                     Tr.error(tc, "UPDATE_MISSING_FEATURE_ERROR", missing);
                 }
-            } else {
-                // Not on Open Liberty
+            } else { **/
+                // On Open Liberty
                 Tr.error(tc, "UPDATE_MISSING_FEATURE_ERROR", missing);
-            }
+            //}
             if (altName != null && isRootFeature && !isExtension) {
                 Tr.error(tc, "MISSING_FEATURE_HAS_ALT_NAME", missing, altName);
             }
