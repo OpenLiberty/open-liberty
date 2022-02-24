e97b4a80fa40af6fde7147f54521fb8230499f38
Adam Yoho <ayoho@us.ibm.com>
2022-02-24 11:25:34 -0600

    Issue 20058 and 20060: Add BCL supported and session required config attributes to RPs

--- dev/com.ibm.ws.security.openidconnect.clients.common/src/com/ibm/ws/security/openidconnect/clients/common/ConvergedClientConfig.java
+++ dev/com.ibm.ws.security.openidconnect.clients.common/src/com/ibm/ws/security/openidconnect/clients/common/ConvergedClientConfig.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2021 IBM Corporation and others.
+ * Copyright (c) 2018, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -132,5 +132,9 @@ public interface ConvergedClientConfig extends JwtConsumerConfig {
     public List<String> getForwardLoginParameter();
 
     String getIntrospectionTokenTypeHint();
+
+    public boolean isBackchannelLogoutSupported();
+
+    public boolean isBackchannelLogoutSessionRequired();
 
 }
