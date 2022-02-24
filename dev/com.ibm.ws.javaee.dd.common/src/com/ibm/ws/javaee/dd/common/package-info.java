9959be3fcf51a2109fb4fdf990c037fdb5223241
Thomas Bitonti <bitonti@us.ibm.com>
2022-02-24 11:51:41 -0500

    Update java EE DD feature version from 1.5 to 2.0. Change com.ibm.ws.javaee.dd.common to version 2.0.

--- dev/com.ibm.ws.javaee.dd.common/src/com/ibm/ws/javaee/dd/common/package-info.java
+++ dev/com.ibm.ws.javaee.dd.common/src/com/ibm/ws/javaee/dd/common/package-info.java
@@ -10,7 +10,7 @@
  *******************************************************************************/
 
 /**
- * @version 1.3.0
+ * @version 2.0.0
  */
-@org.osgi.annotation.versioning.Version("1.3.0")
+@org.osgi.annotation.versioning.Version("2.0.0")
 package com.ibm.ws.javaee.dd.common;
