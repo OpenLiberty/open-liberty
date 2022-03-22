4a0607dcf5c68e0ecbc0514721e39c179f121bc1
Joe Grassel <jgrassel@us.ibm.com>
2022-03-22 10:42:27 -0500

    Update Java 18 FAT to be build with GM build Java 18.
    Signed-off-by: Joe Grassel <jgrassel@us.ibm.com>

---
+++ dev/io.openliberty.java.internal_fat/fat/src/io/openliberty/java/internal/fat/Java18TestJava2SecurityDisabled.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.java.internal.fat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import componenttest.annotation.MinimumJavaLevel;
+import componenttest.annotation.Server;
+import componenttest.custom.junit.runner.FATRunner;
+import componenttest.topology.impl.LibertyServer;
+import junit.framework.Assert;
+
+@RunWith(FATRunner.class)
+@MinimumJavaLevel(javaLevel = 18)
+public class Java18TestJava2SecurityDisabled {
+    @Server("java18-java2sec-disabled-server")
+    public static LibertyServer server;
+
+    @Test
+    public void testJava2SecDisabledWithJava18() throws Exception {
+        server.startServer();
+        try {
+            Assert.assertNotNull(server.waitForStringInLog("CWWKE0955E"));
+        } finally {
+            server.stopServer("CWWKE0955E");
+        }
+    }
+}
