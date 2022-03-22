6097c37145c16680aeadfb7e1f26301bb717ced2
James Stephens <hunting@us.ibm.com>
2022-03-22 07:09:54 -0500

    code review and copyright dates

--- dev/com.ibm.ws.jca.cm/src/com/ibm/ejs/j2c/ConnectionManagerServiceImpl.java
+++ dev/com.ibm.ws.jca.cm/src/com/ibm/ejs/j2c/ConnectionManagerServiceImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2021 IBM Corporation and others.
+ * Copyright (c) 2011, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -542,8 +542,9 @@ public class ConnectionManagerServiceImpl extends ConnectionManagerService {
             if (pm.gConfigProps.getAgedTimeout() != agedTimeout)
                 pm.gConfigProps.setAgedTimeout(agedTimeout);
 
-            if (pm.gConfigProps.getAbortLongRunningInuseConnections() != abortLongRunningInuseConnections)
+            if (pm.gConfigProps.getAbortLongRunningInuseConnections() != abortLongRunningInuseConnections) {
                 pm.gConfigProps.setAbortLongRunningInuseConnections(abortLongRunningInuseConnections);
+            }
 
             if (pm.gConfigProps.getAutoCloseConnections() != autoCloseConnections)
                 pm.gConfigProps.setAutoCloseConnections(autoCloseConnections);
--- dev/com.ibm.ws.jca.cm/src/com/ibm/ejs/j2c/J2CGlobalConfigProperties.java
+++ dev/com.ibm.ws.jca.cm/src/com/ibm/ejs/j2c/J2CGlobalConfigProperties.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1997, 2021 IBM Corporation and others.
+ * Copyright (c) 1997, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1032,7 +1032,6 @@ public final class J2CGlobalConfigProperties implements PropertyChangeListener, 
      * @return
      */
     public int getAbortLongRunningInuseConnections() {
-        // TODO Auto-generated method stub
         return abortLongRunningInuseConnections;
     }
 
