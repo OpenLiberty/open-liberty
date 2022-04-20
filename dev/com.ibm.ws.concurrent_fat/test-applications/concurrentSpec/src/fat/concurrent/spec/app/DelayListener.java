3674753958867d3f18396eeec59ce079fa56b7e3
Nathan Rauh <nathan.rauh@us.ibm.com>
2022-04-20 14:00:59 -0500

    Issue #20880 timing tolerance when testing getDelay

---
+++ dev/com.ibm.ws.concurrent_fat/test-applications/concurrentSpec/src/fat/concurrent/spec/app/DelayListener.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package fat.concurrent.spec.app;
+
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import javax.enterprise.concurrent.ManagedExecutorService;
+import javax.enterprise.concurrent.ManagedTaskListener;
+
+/**
+ * Upon taskSubmitted, records the delay until the next execution.
+ */
+public class DelayListener implements ManagedTaskListener {
+    final LinkedBlockingQueue<Long> delays = new LinkedBlockingQueue<Long>();
+
+    @Override
+    public void taskAborted(Future<?> future, ManagedExecutorService executor, Object task, Throwable x) {
+    }
+
+    @Override
+    public void taskDone(Future<?> future, ManagedExecutorService executor, Object task, Throwable x) {
+    }
+
+    @Override
+    public void taskStarting(Future<?> future, ManagedExecutorService executor, Object task) {
+    }
+
+    @Override
+    public void taskSubmitted(Future<?> future, ManagedExecutorService executor, Object task) {
+        delays.add(((ScheduledFuture<?>) future).getDelay(TimeUnit.MILLISECONDS));
+    }
+}
