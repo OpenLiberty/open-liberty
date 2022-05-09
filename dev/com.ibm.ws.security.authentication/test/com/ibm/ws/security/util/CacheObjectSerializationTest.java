85944d94dfc8193319091cebe10a93eae1fa2cb8
ryanesch <ryanesch@yahoo.com>
2022-05-09 14:08:20 -0500

    Add JCache serialization tests

---
+++ dev/com.ibm.ws.security.authentication/test/com/ibm/ws/security/util/CacheObjectSerializationTest.java
@@ -0,0 +1,133 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.security.util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Field;
+import java.security.Principal;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Set;
+
+import javax.security.auth.Subject;
+
+import org.junit.Test;
+
+import com.ibm.ws.security.authentication.cache.CacheObject;
+import com.ibm.ws.security.authentication.principals.WSPrincipal;
+
+/**
+ * Ensure CacheObject and all serialized instances of CacheObject remain serializable.
+ * If CacheObject is changed, create a new CacheObject_x.ser file and write a new test.
+ * Each new version should be able to deserialze every previous version.
+ * This is necessary to maintain version to version compatibility for the distributed authentication cache.
+ */
+public class CacheObjectSerializationTest {
+
+    /**
+     * Test to deserialize CacheObject_1.ser.
+     * Validate lookup keys, principals, private creds, and public creds.
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public void deserializeCacheObject_1() throws Exception {
+        String filename = "test-resources/ser-files/CacheObject_1.ser";
+        FileInputStream file = new FileInputStream(filename);
+        ObjectInputStream in = new ObjectInputStream(file);
+
+        CacheObject object = (CacheObject) in.readObject();
+        in.close();
+
+        //Check serializationVersion via reflection
+        Field field = CacheObject.class.getDeclaredField("serializationVersion");
+        field.setAccessible(true);
+        Object value = field.get(object);
+        assertEquals("The serialization version should be: 1.", (short) 1, value);
+
+        //Check lookup keys
+        List<Object> keys = object.getLookupKeys();
+        assertEquals("There should be two lookup keys.", 2, keys.size());
+        assertEquals("There lookup keys should be: [lookupkey1, lookupkey2].", "[lookupkey1, lookupkey2]", keys.toString());
+
+        //Check principals
+        Set<Principal> principals = object.getSubject().getPrincipals();
+        assertEquals("There should be one principal.", 1, principals.size());
+        Principal principal = principals.iterator().next();
+        assertEquals("The principal name should be: securityName1.", "securityName1", principal.getName());
+
+        //Check private credentials
+        Hashtable<String, Object> privateCreds = (Hashtable<String, Object>) object.getSubject().getPrivateCredentials().iterator().next();
+        assertEquals("There should be one private credential.", 1, privateCreds.size());
+        assertEquals("The credential should be: {privatekey1=privatevalue1}", "{privatekey1=privatevalue1}", privateCreds.toString());
+
+        //Check public credentials
+        Hashtable<String, Object> publicCreds = (Hashtable<String, Object>) object.getSubject().getPublicCredentials().iterator().next();
+        assertEquals("There should be one public credential.", 1, publicCreds.size());
+        assertEquals("The credential should be: {publickey1=publicvalue1}", "{publickey1=publicvalue1}", publicCreds.toString());
+
+    }
+
+    /**
+     * Method used to create and serialize the CacheObject for testing.
+     *
+     * If CacheObject changes, previously serialized versions of
+     * CacheObject must remain deserializable.
+     * Use this method to create a new CacheObject_x.ser file,
+     * replacing the x with the current version + 1.
+     * Then write a test that deserializes that version and all
+     * previous CacheObject_x.ser files.
+     */
+    public static void main(String[] args) throws Exception {
+        Subject subject = new Subject();
+
+        //Add private creds
+        Hashtable<String, Object> hashtable = new Hashtable<String, Object>();
+        hashtable.put("privatekey1", "privatevalue1");
+        subject.getPrivateCredentials().add(hashtable);
+
+        //Add public creds
+        Hashtable<String, Object> hashtable2 = new Hashtable<String, Object>();
+        hashtable2.put("publickey1", "publicvalue1");
+        subject.getPublicCredentials().add(hashtable2);
+
+        //Add principal
+        Principal principal = new WSPrincipal("securityName1", "accessId1", "authMethod1");
+        subject.getPrincipals().add(principal);
+
+        //Create CacheObject
+        CacheObject object = new CacheObject(subject);
+
+        //Add lookup keys
+        object.addLookupKey("lookupkey1");
+        object.addLookupKey("lookupkey2");
+
+        String filename = "test-resources/ser-files/CacheObject_x.ser";
+
+        // Serialization
+        //Saving of object in a file
+        FileOutputStream file = new FileOutputStream(filename);
+        ObjectOutputStream output = new ObjectOutputStream(file);
+
+        // Method for serialization of object
+        output.writeObject(object);
+
+        output.close();
+        file.close();
+
+        System.out.println("Object has been serialized");
+
+    }
+}
---
+++ dev/com.ibm.ws.security.social/test/com/ibm/websphere/security/social/UserProfileSerializationTest.java
@@ -0,0 +1,119 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.websphere.security.social;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Hashtable;
+import org.junit.Test;
+
+import com.ibm.websphere.security.jwt.Claims;
+import com.ibm.websphere.security.jwt.InvalidBuilderException;
+import com.ibm.websphere.security.jwt.JwtException;
+import com.ibm.websphere.security.jwt.JwtToken;
+import com.ibm.websphere.security.jwt.KeyException;
+import com.ibm.ws.security.jwt.internal.ClaimsImpl;
+
+/**
+ * Ensure UserProfile and all serialized instances of UserProfile remain serializable.
+ * If UserProfile is changed, create a new UserProfile_x.ser file and write a new test.
+ * Each new version should be able to deserialze every previous version.
+ * This is necessary to maintain version to version compatibility for the distributed authentication cache.
+ */
+public class UserProfileSerializationTest {
+
+    /**
+     * Test to deserialize UserProfile_1.ser.
+     * Validate JWT, Claims, CustomProps, and userInfo
+     */
+	//TODO: This test will need to be updated when JwtToken is made serializable.
+	// Currently (08/22), TestJwtToken implements Serializable so that the UserProfile
+	// object can be serialized. When JwtToken is made serializable, remove
+	// Serializable from TestJwtToken and create a new .ser file.
+    @Test
+    public void deserializeUserProfile_1() throws Exception {
+        String filename = "test-resources/ser-files/UserProfile_1.ser";
+        FileInputStream file = new FileInputStream(filename);
+        ObjectInputStream in = new ObjectInputStream(file);
+
+        UserProfile object = (UserProfile) in.readObject();
+        in.close();
+    	
+        //Check claims
+        Claims claims = object.getClaims();
+        assertEquals("The claims issuer should be: issuer.", "issuer", claims.getIssuer());
+        assertEquals("The claims subject should be: subject.", "subject", claims.getSubject());
+
+        //Check userInfo
+        String userInfo = object.getUserInfo();
+        assertEquals("The userInfo should be: userInfo.", "userInfo", userInfo);
+
+        
+        //Check customProps via accessTokenAlias
+        String accessTokenAlias = object.getAccessTokenAlias();
+        assertEquals("The accessTokenAlias should be: accessTokenAliasValue.", "accessTokenAliasValue", accessTokenAlias);
+
+        //Check JWT
+        JwtToken jwt = object.getIdToken();
+        assertEquals("The jtw header should be: header1", "header1", jwt.getHeader(null));
+
+    }
+
+    /**
+     * Method used to create and serialize the UserProfile for testing.
+     *
+     * If UserProfile changes, previously serialized versions of
+     * UserProfile must remain deserializable. 
+     * Use this method to create a new UserProfile_x.ser file, 
+     * replacing the x with the current version + 1.
+     * Then write a test that deserializes that version and all
+     * previous UserProfile_x.ser files.
+     */
+    public static void main(String[] args) throws Exception {
+        
+        //Add private creds
+        Hashtable<String, Object> customProps = new Hashtable<String, Object>();
+        customProps.put("accessTokenAlias", "accessTokenAliasValue");
+        customProps.put("id_token", "jwt");
+
+        //Add public creds
+        Claims claims = new ClaimsImpl();
+        claims.put("iss", "issuer");
+        claims.put("sub", "subject");
+        
+        //Create the JWT
+        JwtToken j = new TestJwtToken();
+
+        //Create UserProfile
+        UserProfile object = new UserProfile(j, customProps, claims, "userInfo");
+
+        // Serialization
+        String filename = "test-resources/ser-files/UserProfile_x.ser";
+        //Saving of object in a file
+        FileOutputStream file = new FileOutputStream(filename);
+        ObjectOutputStream output = new ObjectOutputStream(file);
+
+        // Method for serialization of object
+        output.writeObject(object);
+
+        output.close();
+        file.close();
+
+        System.out.println("Object has been serialized");
+
+    }
+}
