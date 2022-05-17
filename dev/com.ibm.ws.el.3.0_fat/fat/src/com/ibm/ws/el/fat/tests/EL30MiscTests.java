71c906271f8a1a282c14891bdf74d90af757a31d
Volodymyr Siedlecki <volosied@gmail.com>
2022-05-17 16:15:45 -0400

    Enable EE10 Repeat Action in com.ibm.ws.el.3.0_fat

--- dev/com.ibm.ws.el.3.0_fat/fat/src/com/ibm/ws/el/fat/tests/EL30MiscTests.java
+++ dev/com.ibm.ws.el.3.0_fat/fat/src/com/ibm/ws/el/fat/tests/EL30MiscTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021 IBM Corporation and others.
+ * Copyright (c) 2021, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -103,7 +103,7 @@ public class EL30MiscTests {
      *
      * @throws Exception
      */
-    @SkipForRepeat(SkipForRepeat.EE9_FEATURES)
+    @SkipForRepeat({SkipForRepeat.EE9_FEATURES,SkipForRepeat.EE10_FEATURES})
     @Test
     public void testEL22ServiceLookup() throws Exception {
 
