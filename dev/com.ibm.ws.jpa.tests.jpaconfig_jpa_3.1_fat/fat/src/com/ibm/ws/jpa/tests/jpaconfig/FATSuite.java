57c34c3a45b885f7dc47612490702c05bdd836bc
Will Dazey <dazeydev.3@gmail.com>
2022-05-18 10:14:10 -0500

    Add Hibernate testing to JPA 3.1 FAT
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

--- dev/com.ibm.ws.jpa.tests.jpaconfig_jpa_3.1_fat/fat/src/com/ibm/ws/jpa/tests/jpaconfig/FATSuite.java
+++ dev/com.ibm.ws.jpa.tests.jpaconfig_jpa_3.1_fat/fat/src/com/ibm/ws/jpa/tests/jpaconfig/FATSuite.java
@@ -32,6 +32,7 @@ public class FATSuite extends AbstractFATSuite {
 
     @ClassRule
     public static RepeatTests r = RepeatTests
-                    .with(new RepeatWithJPA31());
+                    .with(new RepeatWithJPA31())
+                    .andWith(new RepeatWithJPA31Hibernate());
 
 }
