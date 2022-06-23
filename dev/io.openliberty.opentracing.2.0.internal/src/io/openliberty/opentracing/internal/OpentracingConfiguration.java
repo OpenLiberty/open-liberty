9bf54e5c194624bda9cbbf1877f92d0a0cb1f9da
jdmcclur <jdmcclur@us.ibm.com>
2022-06-23 08:17:04 -0500

    back out config change

--- dev/io.openliberty.opentracing.2.0.internal/src/io/openliberty/opentracing/internal/OpentracingConfiguration.java
+++ dev/io.openliberty.opentracing.2.0.internal/src/io/openliberty/opentracing/internal/OpentracingConfiguration.java
@@ -19,27 +19,12 @@ import org.eclipse.microprofile.config.ConfigProvider;
 public class OpentracingConfiguration {
 
     public static final String MP_OT_SERVER_SKIP_PATTERN_KEY = "mp.opentracing.server.skip-pattern";
-    public static final String MP_OT_SERVER_SKIP_PATTERN_MAY_CHANGE_KEY = "mp.opentracing.server.skip-pattern.may.change";
     public static final String MP_OT_SERVER_OPERATION_NAME_PROVIDER_KEY = "mp.opentracing.server.operation-name-provider";
     public static final String MP_OT_SERVER_OPERATION_NAME_PROVIDER_HTTP_PATH = "http-path";
 
-    private static boolean MP_OT_SERVER_SKIP_PATTERN_MAY_CHANGE = false;
-    private static boolean MP_OT_SERVER_SKIP_PATTERN_IS_SET = false;
-    private static String MP_OT_SERVER_SKIP_PATTERN = null;
-
-    static {
-        Config config = ConfigProvider.getConfig(Thread.currentThread().getContextClassLoader());
-        MP_OT_SERVER_SKIP_PATTERN_MAY_CHANGE = config.getOptionalValue(MP_OT_SERVER_SKIP_PATTERN_MAY_CHANGE_KEY, Boolean.class).orElse(false);
-    }
-
     public static String getServerSkipPattern() {
-        if (!MP_OT_SERVER_SKIP_PATTERN_IS_SET || MP_OT_SERVER_SKIP_PATTERN_MAY_CHANGE) {
-            Config config = ConfigProvider.getConfig(Thread.currentThread().getContextClassLoader());
-            MP_OT_SERVER_SKIP_PATTERN = config.getOptionalValue(MP_OT_SERVER_SKIP_PATTERN_KEY, String.class).orElse(null);
-            MP_OT_SERVER_SKIP_PATTERN_IS_SET = true;
-        }
-
-        return MP_OT_SERVER_SKIP_PATTERN;
+        Config config = ConfigProvider.getConfig(Thread.currentThread().getContextClassLoader());
+        return config.getOptionalValue(MP_OT_SERVER_SKIP_PATTERN_KEY, String.class).orElse(null);
     }
 
     static String getOpertionNameProvider() {
