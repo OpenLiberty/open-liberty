fb35f29cd01fd3526292cc38b95abe6c7cf6d003
Christina Crane <chriscr@us.ibm.com>
2022-06-29 16:15:09 -0500

    Relocate oidc client backchannelLogout tests to a new project

---
+++ dev/com.ibm.ws.security.oidc.client_fat.backchannelLogout/fat/src/com/ibm/ws/security/backchannelLogout/fat/LogoutTokenValidationTests.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package com.ibm.ws.security.backchannelLogout.fat;
+
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+
+import com.ibm.ws.security.fat.common.actions.SecurityTestRepeatAction;
+import com.ibm.ws.security.oauth_oidc.fat.commonTest.Constants;
+
+import componenttest.annotation.AllowedFFDC;
+import componenttest.custom.junit.runner.FATRunner;
+import componenttest.custom.junit.runner.Mode;
+import componenttest.custom.junit.runner.Mode.TestMode;
+import componenttest.rules.repeater.RepeatTests;
+import componenttest.topology.impl.LibertyServerWrapper;
+
+/**
+ * This test class contains tests that validate the proper behavior of the back channel logout endpoint's logout token validation.
+ * The tests in this class will test with logout tokens that contain both valid and invalid content.
+ * This instance of these tests will validate the behaviour when using the OIDC endpoint
+ **/
+
+@RunWith(FATRunner.class)
+@LibertyServerWrapper
+@Mode(TestMode.FULL)
+@AllowedFFDC({ "org.apache.http.NoHttpResponseException" })
+public class LogoutTokenValidationTests extends com.ibm.ws.security.backchannelLogout.fat.CommonTests.LogoutTokenValidationTests {
+
+    public static Class<?> thisClass = LogoutTokenValidationTests.class;
+
+    @ClassRule
+    public static RepeatTests repeat = RepeatTests.with(new SecurityTestRepeatAction(Constants.OIDC));
+
+}
