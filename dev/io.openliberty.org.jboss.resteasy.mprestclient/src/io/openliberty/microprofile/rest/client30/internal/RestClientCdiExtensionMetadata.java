9cfa8edd60869d5244c186e406bfc441b843ecdb
Benjamin Confino <benjamic@uk.ibm.com>
2022-07-07 13:19:49 +0100

    register RegisterRestClient as a BDA

--- dev/io.openliberty.org.jboss.resteasy.mprestclient/src/io/openliberty/microprofile/rest/client30/internal/RestClientCdiExtensionMetadata.java
+++ dev/io.openliberty.org.jboss.resteasy.mprestclient/src/io/openliberty/microprofile/rest/client30/internal/RestClientCdiExtensionMetadata.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021 IBM Corporation and others.
+ * Copyright (c) 2021, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,7 +10,9 @@
  *******************************************************************************/
 package io.openliberty.microprofile.rest.client30.internal;
 
+import java.lang.annotation.Annotation;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.enterprise.inject.spi.Extension;
@@ -23,6 +25,8 @@ import com.ibm.ws.cdi.extension.CDIExtensionMetadataInternal;
 
 import io.openliberty.cdi.spi.CDIExtensionMetadata;
 
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
 /**
  * Registers the RestEasy MP Rest Client extension
  */
@@ -32,6 +36,13 @@ public class RestClientCdiExtensionMetadata implements CDIExtensionMetadata, CDI
     @Override
     public Set<Class<? extends Extension>> getExtensions() {
         return Collections.singleton(RestClientExtension.class);
+    }
+
+    @Override
+    public Set<Class<? extends Annotation>> getBeanDefiningAnnotationClasses() {
+        Set<Class<? extends Annotation>> BDAs = new HashSet<Class<? extends Annotation>>();
+        BDAs.add(RegisterRestClient.class);
+        return BDAs;
     }
 
     @Override
