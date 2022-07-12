b2d31279c0850ffbd99fed144ad232f38912b2f4
Paul Nicolucci <pnicolucci@gmail.com>
2022-07-11 20:36:07 -0400

    Issue #21717: Fix comment typo:  require should be requires.

--- dev/com.ibm.ws.el.3.0_fat/fat/src/com/ibm/ws/el/fat/FATSuite.java
+++ dev/com.ibm.ws.el.3.0_fat/fat/src/com/ibm/ws/el/fat/FATSuite.java
@@ -77,7 +77,7 @@ public class FATSuite {
     public static RepeatTests repeat;
 
     static {
-        // EE10 require Java 11.  If we only specify EE10 for lite mode it will cause no tests to run which causes an error.
+        // EE10 requires Java 11.  If we only specify EE10 for lite mode it will cause no tests to run which causes an error.
         // If we are running on Java 8 have EE9 be the lite mode test to run.
         if (JavaInfo.JAVA_VERSION >= 11) {
             repeat = RepeatTests.with(new EmptyAction().fullFATOnly())
