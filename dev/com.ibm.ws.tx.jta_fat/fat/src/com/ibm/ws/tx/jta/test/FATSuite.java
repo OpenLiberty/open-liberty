9effc679731dcda69a33321d695088788deff413
Jon Hawkes <jon.hawkes@uk.ibm.com>
2022-07-26 17:02:38 +0100

    Split long running test suite - part 3

--- dev/com.ibm.ws.tx.jta_fat/fat/src/com/ibm/ws/tx/jta/test/FATSuite.java
+++ dev/com.ibm.ws.tx.jta_fat/fat/src/com/ibm/ws/tx/jta/test/FATSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 IBM Corporation and others.
+ * Copyright (c) 2020, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -34,4 +34,7 @@ public class FATSuite {
     public static RepeatTests r = RepeatTests.withoutModification()
                     .andWith(FeatureReplacementAction.EE8_FEATURES())
                     .andWith(new JakartaEE9Action());
+
+    public void placeHolder() {
+    }
 }
