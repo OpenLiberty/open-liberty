6a44d86f6850507ffdf4bcc48f3dfd9e762e45e1
Nathan Rauh <nathan.rauh@us.ibm.com>
2022-07-28 11:35:33 -0500

    Issue #18259 errant config tests

---
+++ dev/com.ibm.ws.concurrent_fat_jakarta/test-applications/ConcurrencyTestError/src/test/jakarta/concurrency/web/error/scheduledexecutordef/ManagedScheduledExecutorDefinitionErrorServlet.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package test.jakarta.concurrency.web.error.scheduledexecutordef;
+
+import jakarta.enterprise.concurrent.ManagedScheduledExecutorDefinition;
+import jakarta.servlet.annotation.WebServlet;
+
+import componenttest.app.FATServlet;
+
+@ManagedScheduledExecutorDefinition(name = "java:module/concurrent/executor-err-0", maxAsync = 0)
+@SuppressWarnings("serial")
+@WebServlet("/*")
+public class ManagedScheduledExecutorDefinitionErrorServlet extends FATServlet {
+}
