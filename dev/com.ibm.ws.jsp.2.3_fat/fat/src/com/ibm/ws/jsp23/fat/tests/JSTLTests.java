4dd2d4ba2a71d54edef8fa4744555734a3017109
Isaac Rivera-Rivas <Isaac.Rivera-Rivas@ibm.com>
2022-08-03 12:33:47 -0400

    Added tags transformation tests

--- dev/com.ibm.ws.jsp.2.3_fat/fat/src/com/ibm/ws/jsp23/fat/tests/JSTLTests.java
+++ dev/com.ibm.ws.jsp.2.3_fat/fat/src/com/ibm/ws/jsp23/fat/tests/JSTLTests.java
@@ -53,6 +53,18 @@ public class JSTLTests {
 
     private String newLine = System.getProperty("line.separator");
 
+    private final String replacePattern = "["+newLine+"\t ]";
+
+    private final String EXPECTED_NAME_SORTED_STOCK_TABLE = "<h2>Name Sorted Stock Table</h2><table id=\"nameSortedStockTable\" border=\"1\" width=\"100%\">" +
+    "<tr><th style=\"text-align:left\">Sorted Stock</th><th style=\"text-align:left\">Price</th></tr>" +
+    "<tr><td><i>AAPL</i></td><td>131</td></tr><tr><td><i>GME</i></td><td>46</td></tr><tr><td><i>IBM</i></td><td>120</td></tr></table>";
+    private final String EXPECTED_PRICE_SORTED_STOCK_TABLE = "<h2>Price Sorted Stock Table</h2><table id=\"priceSortedStockTable\" border=\"1\" width=\"100%\">" + 
+    "<tr><th style=\"text-align:left\">Sorted Stock</th><th style=\"text-align:left\">Price</th></tr>" + 
+    "<tr><td><i>GME</i></td><td>46</td></tr><tr><td><i>IBM</i></td><td>120</td></tr><tr><td><i>AAPL</i></td><td>131</td></tr></table>";
+    private final String EXPECTED_STOCK_TABLE = "<h2>Stock Table</h2><table id=\"stockTable\" border=\"1\" width=\"100%\">" +
+    "<tr><th style=\"text-align:left\">Stock</th><th style=\"text-align:left\">Price</th></tr>" +
+    "<tr><td><i>AAPL</i></td><td>131</td></tr><tr><td><i>IBM</i></td><td>120</td></tr><tr><td><i>GME</i></td><td>46</td></tr></table>";
+
     @Server("jstlServer")
     public static LibertyServer server;
 
@@ -136,6 +148,28 @@ public class JSTLTests {
 
         assertTrue("Something went wrong with the xml tags", response.getText().contains("IBM is trading above 100"));
 
+    }
+
+    /**
+     * Basic Test for transform in XML Tags
+     *
+     * @throws Exception if something goes horribly wrong
+     */
+    @Test
+    public void testXMLTransformTag() throws Exception ,Throwable {
+        WebConversation wc = new WebConversation();
+        wc.setExceptionsThrownOnErrorStatus(false);
+
+        String url = JSPUtils.createHttpUrlString(server, APP_NAME, "xmlTransformTag.jsp");
+        LOG.info("url: " + url);
+
+        WebRequest request = new GetMethodWebRequest(url);
+        WebResponse response = wc.getResponse(request);
+        LOG.info("Servlet response : " + response.getText());
+        String cleanedResponse = response.getText().replaceAll(replacePattern, "");
+        assertTrue("Something went wrong with the xml transform tags. Couldn't match expected stock table.", cleanedResponse.contains(EXPECTED_STOCK_TABLE.replaceAll(replacePattern, "")));
+        assertTrue("Something went wrong with the xml transform tags. Couldn't match expected name sorted stock table.", cleanedResponse.contains(EXPECTED_NAME_SORTED_STOCK_TABLE.replaceAll(replacePattern, "")));
+        assertTrue("Something went wrong with the xml transform tags. Couldn't match expected price sorted stock table.", cleanedResponse.contains(EXPECTED_PRICE_SORTED_STOCK_TABLE.replaceAll(replacePattern, "")));
     }
 
     /**
---
+++ dev/com.ibm.ws.jsp.2.3_fat/test-applications/TestJSTL.war/resources/xmlTransformTag.jsp
@@ -0,0 +1,46 @@
+<!--
+    Copyright (c) 2021 IBM Corporation and others.
+    All rights reserved. This program and the accompanying materials
+    are made available under the terms of the Eclipse Public License v1.0
+    which accompanies this distribution, and is available at
+    http://www.eclipse.org/legal/epl-v10.html
+   
+    Contributors:
+        IBM Corporation - initial API and implementation
+ -->
+ <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>  
+ <%@ taglib prefix="x" uri="http://java.sun.com/jsp/jstl/xml" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>Function Transform Tag</title>
+</head>
+<body>
+
+    <h1>Testing x:transform</h1>
+
+    <!-- As of Feb 17th, 2020 :) -->
+    <c:set var="stocks">  
+       <stocks>
+            <stock>  
+                <name>AAPL</name>  
+                <price>131</price>    
+            </stock>  
+            <stock>  
+                <name>IBM</name>  
+                <price>120</price>   
+            </stock>
+            <stock>  
+                <name>GME</name>  
+                <price>46</price>   
+            </stock>  
+        </stocks> 
+    </c:set>
+
+    <c:import url = "transformStyle.xsl" var = "xslt"/>
+    <x:transform xml = "${stocks}" xslt = "${xslt}"/>
+
+
+</body>
+</html>
+
