b6f9b0d4adbec60ad32a5dfad91407765f6c47c8
tevans <tevans@uk.ibm.com>
2022-08-04 16:37:26 +0100

    Create MP 6 FAT

---
+++ dev/io.openliberty.microprofile6.internal_fat/fat/src/io/openliberty/microprofile6/internal/test/suite/FATSuite.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2020, 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.microprofile6.internal.test.suite;
+
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.ibm.ws.fat.util.FatLogHandler;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+                MP6CompatibleTest.class,
+                MP6EE10CompatibleTest.class,
+                MP6EE9CompatibleTest.class,
+                MP6EE8CompatibleTest.class
+})
+
+public class FATSuite {
+
+    /**
+     * @see {@link FatLogHandler#generateHelpFile()}
+     */
+    @BeforeClass
+    public static void generateHelpFile() {
+        FatLogHandler.generateHelpFile();
+    }
+
+}
