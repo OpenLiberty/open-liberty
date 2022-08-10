c9a79d82966f0b6c0772a660e55a5fdf2744fd90
Lavena Chan <lavena@us.ibm.com>
2022-08-10 08:20:43 -0500

    remove unexpected import

--- dev/com.ibm.ws.ui_rest_fat/fat/src/com/ibm/ws/ui/fat/rest/v1/ToolboxAPIv1Test.java
+++ dev/com.ibm.ws.ui_rest_fat/fat/src/com/ibm/ws/ui/fat/rest/v1/ToolboxAPIv1Test.java
@@ -12,9 +12,6 @@ package com.ibm.ws.ui.fat.rest.v1;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
-import java.beans.Transient;
-
 import static org.junit.Assert.assertFalse;
 
 import javax.json.JsonArray;
