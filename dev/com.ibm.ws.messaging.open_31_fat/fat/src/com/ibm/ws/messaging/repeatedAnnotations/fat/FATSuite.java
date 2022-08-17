c70da6d17a518a66e7371568677ea6263fedb645
Jared Anderson <jhanders@us.ibm.com>
2022-08-17 12:19:24 -0500

    Update messaging 3.1 fat to use Java 11 and EE10 APIs.

--- dev/com.ibm.ws.messaging.open_31_fat/fat/src/com/ibm/ws/messaging/repeatedAnnotations/fat/FATSuite.java
+++ dev/com.ibm.ws.messaging.open_31_fat/fat/src/com/ibm/ws/messaging/repeatedAnnotations/fat/FATSuite.java
@@ -10,14 +10,11 @@
  *******************************************************************************/
 package com.ibm.ws.messaging.repeatedAnnotations.fat;
 
-import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import componenttest.custom.junit.runner.AlwaysPassesTest;
-import componenttest.rules.repeater.JakartaEE10Action;
-import componenttest.rules.repeater.RepeatTests;
 
 // AlwaysPassesTest is required because RepeatedAnnotations are supported for JakartaEE10 and above.
 @RunWith(Suite.class)
@@ -25,8 +22,5 @@ import componenttest.rules.repeater.RepeatTests;
                 RepeatedAnnotationsTest.class
 })
 public class FATSuite {
-
-    @ClassRule
-    public static RepeatTests r = RepeatTests.with(new JakartaEE10Action());
 
 }
