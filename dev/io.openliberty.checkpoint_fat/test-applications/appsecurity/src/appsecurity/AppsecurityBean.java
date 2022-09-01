50470d3e9d145b1e14116414e402eb1aa7129767
Anjum Fatima <anjum.fatima@ibm.com>
2022-09-01 13:57:26 -0500

    Add tests for appSecurity with checkpoint

---
+++ dev/io.openliberty.checkpoint_fat/test-applications/appsecurity/src/appsecurity/AppsecurityBean.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package appsecurity;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.security.enterprise.SecurityContext;
+
+@RequestScoped
+public class AppsecurityBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    public static final String ADMIN = "admin";
+    public static final String USER = "user";
+
+    @Inject
+    private SecurityContext securityContext;
+
+    public String getUsername() {
+        return securityContext.getCallerPrincipal().getName();
+    }
+
+    public String getRoles() {
+        String roles = "";
+        if (securityContext.isCallerInRole(ADMIN)) {
+            roles = ADMIN;
+        }
+        if (securityContext.isCallerInRole(USER)) {
+            if (!roles.isEmpty()) {
+                roles += ", ";
+            }
+            roles += USER;
+        }
+        return roles;
+    }
+}
---
+++ dev/io.openliberty.checkpoint_fat/test-applications/appsecurity/src/appsecurity/AppsecurityServlet.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package appsecurity;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.inject.Inject;
+import javax.security.enterprise.SecurityContext;
+import javax.security.enterprise.authentication.mechanism.http.FormAuthenticationMechanismDefinition;
+import javax.security.enterprise.authentication.mechanism.http.LoginToContinue;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.ServletSecurity;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/home")
+@FormAuthenticationMechanismDefinition(
+                                       loginToContinue = @LoginToContinue(errorPage = "/error.html",
+                                                                          loginPage = "/welcome.html"))
+@ServletSecurity(value = @HttpConstraint(rolesAllowed = { "user", "admin" },
+                                         transportGuarantee = ServletSecurity.TransportGuarantee.CONFIDENTIAL))
+public class AppsecurityServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    public static final String ADMIN = "admin";
+    public static final String USER = "user";
+    
+    @Inject
+    private SecurityContext securityContext;
+    
+    @Inject
+    private AppsecurityBean bean;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletOutputStream outstream = response.getOutputStream();
+        if (securityContext.isCallerInRole(ADMIN)) {       
+            outstream.println("GROUP: " + ADMIN + ", NAME: " + bean.getUsername() + ", ROLES: " + bean.getRoles());
+        } else if (securityContext.isCallerInRole(USER)) {
+            outstream.println("GROUP: " + USER + ", NAME: " + bean.getUsername() + ", ROLES: " + bean.getRoles());
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
