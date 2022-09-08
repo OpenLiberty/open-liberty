1583d5d1bc7b09d12498db6408b682e6edba671b
Nathan Rauh <nathan.rauh@us.ibm.com>
2022-09-08 14:34:15 -0500

    Issue #22443 start of NoSQL test bucket for Jakarta Data

---
+++ dev/io.openliberty.data.internal_fat_nosql/fat/src/test/jakarta/data/nosql/FATSuite.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package test.jakarta.data.nosql;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import componenttest.custom.junit.runner.AlwaysPassesTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+                AlwaysPassesTest.class,
+                DataNoSQLTest.class
+})
+public class FATSuite {
+    /**
+     * Pre-bucket execution setup.
+     */
+    @BeforeClass
+    public static void beforeSuite() throws Exception {
+    }
+
+    /**
+     * Post-bucket execution setup.
+     */
+    @AfterClass
+    public static void cleanUpSuite() throws Exception {
+    }
+}
