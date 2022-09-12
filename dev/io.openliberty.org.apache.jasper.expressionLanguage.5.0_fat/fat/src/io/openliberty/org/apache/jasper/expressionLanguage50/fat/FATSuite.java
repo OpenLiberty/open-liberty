1479d48fee97ba5bd1b4af49f389e7812010eb59
Volodymyr Siedlecki <volosied@gmail.com>
2022-09-12 16:20:41 -0400

    Added lambda tests according to spec

--- dev/io.openliberty.org.apache.jasper.expressionLanguage.5.0_fat/fat/src/io/openliberty/org/apache/jasper/expressionLanguage50/fat/FATSuite.java
+++ dev/io.openliberty.org.apache.jasper.expressionLanguage.5.0_fat/fat/src/io/openliberty/org/apache/jasper/expressionLanguage50/fat/FATSuite.java
@@ -18,6 +18,7 @@ import org.junit.runners.Suite.SuiteClasses;
 import com.ibm.ws.fat.util.FatLogHandler;
 
 import io.openliberty.org.apache.jasper.expressionLanguage50.fat.tests.EL50DefaultMethodsTest;
+import io.openliberty.org.apache.jasper.expressionLanguage50.fat.tests.EL50LambdaExpressionCoercionTest;
 import io.openliberty.org.apache.jasper.expressionLanguage50.fat.tests.EL50MethodReferenceTest;
 
 /**
@@ -37,8 +38,10 @@ import io.openliberty.org.apache.jasper.expressionLanguage50.fat.tests.EL50Metho
  */
 @RunWith(Suite.class)
 @SuiteClasses({
-                EL50DefaultMethodsTest.class,
-                EL50MethodReferenceTest.class
+                // EL50DefaultMethodsTest.class,
+                // EL50MethodReferenceTest.class,
+                EL50LambdaExpressionCoercionTest.class
+
 })
 public class FATSuite {
 
---
+++ dev/io.openliberty.org.apache.jasper.expressionLanguage.5.0_fat/test-applications/EL50Coercion.war/src/io/openliberty/el50/fat/coercion/servlets/lambda/EL50LambdaExpressionCoercionServlet.java
@@ -0,0 +1,118 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.el50.fat.coercion.servlets.lambda;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import componenttest.app.FATServlet;
+import componenttest.custom.junit.runner.Mode;
+import componenttest.custom.junit.runner.Mode.TestMode;
+import jakarta.el.BeanELResolver;
+import jakarta.el.ELContext;
+import jakarta.el.ELManager;
+import jakarta.el.StandardELContext;
+import jakarta.servlet.annotation.WebServlet;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import java.util.function.Predicate;
+
+import jakarta.el.ELException;
+import jakarta.el.ELProcessor;
+
+/**
+ * Servlet for testing LambdaExpression coercion in EL 5.0
+ * https://github.com/jakartaee/expression-language/issues/45
+ */
+@WebServlet({ "/EL50LambdaExpressionCoercion" })
+public class EL50LambdaExpressionCoercionServlet extends FATServlet {
+    private static final long serialVersionUID = 1L;
+
+    ELProcessor elp;
+
+    public EL50LambdaExpressionCoercionServlet() {
+        super();
+
+        elp = new ELProcessor();
+        elp.defineBean("lambdaBean", new LambdaBean());
+    }
+
+    @Test
+    public void testLambdaCoercion() throws Exception {
+        // Test lambda coercion basic test passing a lambda as a parameter from a defined bean
+        String result;
+        String expectedResult ="success";
+        Object obj = elp.eval("lambdaBean.testLambdaCoercion(param -> true)");
+        assertNotNull(obj);
+        result = obj.toString();
+
+        assertEquals("The expression did not evaluate to: " + expectedResult + " but was: " + result, expectedResult, result);
+    }
+
+    @Test
+    public void testLambdaCoercionWrongType() throws Exception {
+        // Test lambda with a wrong type by passing a lambda evaluating with a wrong type
+        Object result = null;
+        boolean exceptionCaught = false;
+        try {
+            result = elp.eval("lambdaBean.testIntLambdaCoercion(param -> param.equals('should be int not string'))");
+        } catch (ELException e) {
+            exceptionCaught = true;
+        }
+        assertEquals("The expression did not throw expected ELException. Got result: "+result, true, exceptionCaught);
+    }
+
+    @Test
+    public void testLambdaCoercionMissingLambda() throws Exception {
+        // Test lambda coercion by passing a lambda that is not defined
+        Object result = null;
+        boolean exceptionCaught = false;
+        try {
+            result = elp.eval("lambdaBean.missingLambda(param -> true)");
+        } catch (ELException e) {
+            exceptionCaught = true;
+        }
+        assertEquals("The expression did not throw expected ELException. Got result: "+result, true, exceptionCaught);
+    }
+
+    @Test
+    public void testLambdaCoercionNotALambda() throws Exception {
+        // Test by not passing a lambda as parameter but a normal string
+        Object result = null;
+        boolean exceptionCaught = false;
+        try {
+            result = elp.eval("lambdaBean.testLambdaCoercion('noLambdaPassed')");
+        } catch (ELException e) {
+            exceptionCaught = true;
+        }
+        assertEquals("The expression did not throw expected ELException. Got result: "+result, true, exceptionCaught);
+    }
+
+    public class LambdaBean {
+
+        public String testLambdaCoercion(Predicate<String> filter) {
+            return "success";
+        }
+
+        public String testIntLambdaCoercion(Predicate<Integer> filter) {
+            // Expected string to test against
+            Integer expectedInteger = 184;
+            if(filter.test(expectedInteger)){
+                return "success";
+            }
+            return "failed";
+        }
+
+    }
+
+}
