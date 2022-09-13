940e31f599df3f25eb8f00fee2c9793430c1e7c4
Adam Yoho <ayoho@us.ibm.com>
2022-09-13 14:23:16 -0500

    Revert "implement session based storage (#22450)"
    This reverts commit 925ad1ddd3aa7a4052c4d7b68788b7fb3ce88d4b.

--- dev/io.openliberty.security.oidcclientcore.internal/src/io/openliberty/security/oidcclientcore/storage/SessionBasedStorage.java
+++ dev/io.openliberty.security.oidcclientcore.internal/src/io/openliberty/security/oidcclientcore/storage/SessionBasedStorage.java
@@ -10,48 +10,23 @@
  *******************************************************************************/
 package io.openliberty.security.oidcclientcore.storage;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
-import com.ibm.websphere.ras.Tr;
-import com.ibm.websphere.ras.TraceComponent;
-
 public class SessionBasedStorage implements Storage {
-
-    public static final TraceComponent tc = Tr.register(SessionBasedStorage.class);
-
-    private final HttpServletRequest request;
-
-    public SessionBasedStorage(HttpServletRequest request) {
-        this.request = request;
-    }
 
     @Override
     public void store(String name, String value) {
+        // TODO
         store(name, value, null);
     }
 
     @Override
     public void store(String name, String value, StorageProperties properties) {
-        // ignore storage properties, since http session attributes
-        // can't set properties (e.g., expiration time) like cookies can
-        HttpSession session = request.getSession();
-        session.setAttribute(name, value);
+        // TODO
     }
 
     @Override
     public String get(String name) {
-        HttpSession session = request.getSession();
-        Object value = session.getAttribute(name);
-        if (value == null) {
-            return null;
-        }
-        return (String) value;
-    }
-
-    public void remove(String name) {
-        HttpSession session = request.getSession();
-        session.removeAttribute(name);
+        // TODO
+        return null;
     }
 
 }
