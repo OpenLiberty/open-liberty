550c1f7202f4a9f5bf3483dd6a8559f0935ce99c
Paul Nicolucci <pnicolucci@gmail.com>
2022-09-26 22:15:30 -0400

    Add test case for faces-config.xml version 4.0

---
+++ dev/io.openliberty.org.apache.myfaces.4.0_fat/fat/src/io/openliberty/org/apache/myfaces40/fat/tests/FacesConfigTest.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.org.apache.myfaces40.fat.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.ibm.websphere.simplicity.ShrinkHelper;
+
+import componenttest.annotation.Server;
+import componenttest.custom.junit.runner.FATRunner;
+import componenttest.custom.junit.runner.Mode;
+import componenttest.custom.junit.runner.Mode.TestMode;
+import componenttest.topology.impl.LibertyServer;
+
+/**
+ * Faces 4.0 faces-config.xml version="4.0" test.
+ *
+ *
+ * This test is run on a server that has an application deployed that contains a
+ * faces-config.xml with a version element of 4.0.
+ *
+ * This test will ensure the application with the faces-config.xml we are testing
+ * has been started.
+ *
+ * The test will ensure that the following exception is not found in the trace.log:
+ *
+ * CWWKC2261E: The FacesConfigTest.war : WEB-INF/faces-config.xml deployment descriptor on line 15 specifies unsupported version 4.0.
+ *
+ */
+@RunWith(FATRunner.class)
+@Mode(TestMode.FULL)
+public class FacesConfigTest {
+    private static final String APP_NAME = "FacesConfigTest";
+
+    @Server("faces40_facesConfigServer")
+    public static LibertyServer server;
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        ShrinkHelper.defaultDropinApp(server, APP_NAME + ".war");
+
+        server.startServer(FacesConfigTest.class.getSimpleName() + ".log");
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        // Stop the server
+        if (server != null && server.isStarted()) {
+            server.stopServer();
+        }
+    }
+
+    /**
+     *
+     * The test will ensure that the following exception is not found in the trace.log:
+     *
+     * CWWKC2261E: The FacesConfigTest.war : WEB-INF/faces-config.xml deployment descriptor on line 15 specifies unsupported version 4.0.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testFacesConfigVersion40() throws Exception {
+        assertTrue("The CWWKC2261E exception was found in the trace.log when it should not have been.",
+                   server.findStringsInTrace("CWWKC2261E").isEmpty());
+    }
+
+}
