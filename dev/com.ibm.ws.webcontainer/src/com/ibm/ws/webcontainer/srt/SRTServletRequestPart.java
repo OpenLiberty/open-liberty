3b69ea3bc9172904c9edd3bb53e4f0c8bbd4b6c2
Phu Dinh <pmdinh@us.ibm.com>
2022-09-30 18:02:45 -0400

    Add trace for WC SRTServletRequestPart

--- dev/com.ibm.ws.webcontainer/src/com/ibm/ws/webcontainer/srt/SRTServletRequestPart.java
+++ dev/com.ibm.ws.webcontainer/src/com/ibm/ws/webcontainer/srt/SRTServletRequestPart.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1997, 2008 IBM Corporation and others.
+ * Copyright (c) 1997, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,6 +18,8 @@ import java.security.PrivilegedActionException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.http.Part;
 
@@ -25,61 +27,78 @@ import org.apache.commons.fileupload.FileItemHeaders;
 import org.apache.commons.fileupload.disk.DiskFileItem;
 
 import com.ibm.ejs.ras.TraceNLS;
+import com.ibm.websphere.ras.TraceComponent;
+import com.ibm.wsspi.webcontainer.logging.LoggerFactory;
 
 public class SRTServletRequestPart implements Part {
 
-	protected DiskFileItem _part;
-	private static final TraceNLS nls = TraceNLS.getTraceNLS(SRTServletRequestPart.class, "com.ibm.ws.webcontainer.resources.Messages");
-	
-	public SRTServletRequestPart(DiskFileItem item) {
-		_part = item;
-	}
-	@Override
-	public void delete() throws IOException {
-	    if (System.getSecurityManager() != null) {
-	        try {
-	            AccessController.doPrivileged(new java.security.PrivilegedExceptionAction<Object>() {
+    protected DiskFileItem _part;
+    private static final TraceNLS nls = TraceNLS.getTraceNLS(SRTServletRequestPart.class, "com.ibm.ws.webcontainer.resources.Messages");
+    private static final String CLASS_NAME="com.ibm.ws.webcontainer.srt.SRTServletRequestPart";
+    protected static final Logger logger = LoggerFactory.getInstance().getLogger("com.ibm.ws.webcontainer.srt");
+
+
+    public SRTServletRequestPart(DiskFileItem item) {
+        _part = item;
+    }
+    @Override
+    public void delete() throws IOException {
+        if (System.getSecurityManager() != null) {
+            if (TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {  
+                logger.logp(Level.FINE, CLASS_NAME,"delete", "via security manager. Location from part ["+ _part.getStoreLocation() +"]");
+            }
+            
+            try {
+                AccessController.doPrivileged(new java.security.PrivilegedExceptionAction<Object>() {
                     public Object run() throws IOException {
                         _part.delete();
                         return null;
                     }
                 });
-	        } catch (PrivilegedActionException e) {
-	            Exception e1 = e.getException();
-	            if (e1 instanceof IOException) {
-	                throw (IOException) e1;
-	            } else if (e1 instanceof RuntimeException) {
-	                throw (RuntimeException) e1;
-	            } else {
-	                throw new IOException(e1);
-	            }
-	        }
-	    } else {
-	        _part.delete();
-	    }
-	}
+            } catch (PrivilegedActionException e) {
+                Exception e1 = e.getException();
+                if (e1 instanceof IOException) {
+                    throw (IOException) e1;
+                } else if (e1 instanceof RuntimeException) {
+                    throw (RuntimeException) e1;
+                } else {
+                    throw new IOException(e1);
+                }
+            }
+        } else {
+            if (TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {  
+                logger.logp(Level.FINE, CLASS_NAME,"delete", "via DiskFileItem.delete. Location from part ["+ _part.getStoreLocation() +"]");
+            }
+            _part.delete();
+        }
+    }
 
-	@Override
-	public String getContentType() {
-	    String returnValue;
-	    if (System.getSecurityManager() != null) {
+    @Override
+    public String getContentType() {
+        String returnValue;
+        if (System.getSecurityManager() != null) {
             returnValue = AccessController.doPrivileged(new java.security.PrivilegedAction<String>() {
                 public String run() {
                     return _part.getContentType();
                 }
             });
-	    } else {
-	        returnValue = _part.getContentType();
-	    }
-	    return returnValue;
-	}
+        } else {
+            returnValue = _part.getContentType();
+        }
+        
+        if (TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {  
+            logger.logp(Level.FINE, CLASS_NAME,"getContentType", " ["+returnValue+"]");
+        }
 
-	@Override
-	public String getHeader(String headerName) {
-	    String returnValue;
-	    if (System.getSecurityManager() != null) {
+        return returnValue;
+    }
+
+    @Override
+    public String getHeader(String headerName) {
+        String returnValue;
+        if (System.getSecurityManager() != null) {
             final String finalHeaderName = headerName;
-	        returnValue = AccessController.doPrivileged(new java.security.PrivilegedAction<String>() {
+            returnValue = AccessController.doPrivileged(new java.security.PrivilegedAction<String>() {
                 public String run() {
                     FileItemHeaders headers =_part.getHeaders();
                     return headers.getHeader(finalHeaderName);
@@ -89,13 +108,20 @@ public class SRTServletRequestPart implements Part {
             FileItemHeaders headers =_part.getHeaders();
             returnValue = headers.getHeader(headerName);
         }
-		return returnValue;
-	}
-	
-	@Override
-	public Collection<String> getHeaderNames() {
-	    final ArrayList<String> headerNamesList = new ArrayList<String>();;
-	    if (System.getSecurityManager() != null) {
+        
+        if (TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {  
+            logger.logp(Level.FINE, CLASS_NAME,"getHeader", " headerName ["+ headerName +"] , return value ["+returnValue+"]");
+        }
+        return returnValue;
+    }
+
+    @Override
+    public Collection<String> getHeaderNames() {
+        if (TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {  
+            logger.logp(Level.FINE, CLASS_NAME,"getHeaderNames", "");
+        }
+        final ArrayList<String> headerNamesList = new ArrayList<String>();;
+        if (System.getSecurityManager() != null) {
             AccessController.doPrivileged(new java.security.PrivilegedAction<Object>() {
                 public Object run() {
                     FileItemHeaders headers =_part.getHeaders();
@@ -111,20 +137,24 @@ public class SRTServletRequestPart implements Part {
                 headerNamesList.add((String)it.next());
             }
         }
-	    return headerNamesList;
-	}
+        return headerNamesList;
+    }
 
-	@Override
-	public Collection<String> getHeaders(String headerName) {
-	    final ArrayList<String> headersList = new ArrayList<String>();
-	    if (System.getSecurityManager() != null) {
-	        final String finalHeaderName = headerName;
-	        AccessController.doPrivileged(new java.security.PrivilegedAction<Object>() {
-	            public Object run() {
-	                FileItemHeaders headers =_part.getHeaders();
-	                for (Iterator it =headers.getHeaders(finalHeaderName);it.hasNext();) {
-	                    headersList.add((String)it.next());
-	                }
+    @Override
+    public Collection<String> getHeaders(String headerName) {
+        if (TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {  
+            logger.logp(Level.FINE, CLASS_NAME,"getHeaders", "");
+        }
+        
+        final ArrayList<String> headersList = new ArrayList<String>();
+        if (System.getSecurityManager() != null) {
+            final String finalHeaderName = headerName;
+            AccessController.doPrivileged(new java.security.PrivilegedAction<Object>() {
+                public Object run() {
+                    FileItemHeaders headers =_part.getHeaders();
+                    for (Iterator it =headers.getHeaders(finalHeaderName);it.hasNext();) {
+                        headersList.add((String)it.next());
+                    }
                     return null;
                 }
             });
@@ -134,13 +164,13 @@ public class SRTServletRequestPart implements Part {
                 headersList.add((String)it.next());
             }
         }
-		return headersList;
-	}
+        return headersList;
+    }
 
-	@Override
-	public InputStream getInputStream() throws IOException {
-	    InputStream returnValue;
-	    if (System.getSecurityManager() != null) {
+    @Override
+    public InputStream getInputStream() throws IOException {
+        InputStream returnValue;
+        if (System.getSecurityManager() != null) {
             try {
                 returnValue = AccessController.doPrivileged(new java.security.PrivilegedExceptionAction<InputStream>() {
                     public InputStream run() throws IOException {
@@ -160,13 +190,17 @@ public class SRTServletRequestPart implements Part {
         } else {
             returnValue = _part.getInputStream(); 
         }
-	    return returnValue; 
-	}
+        
+        if (TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {  
+            logger.logp(Level.FINE, CLASS_NAME,"getInputStream", " ["+returnValue+"]");
+        }
+        return returnValue; 
+    }
 
-	@Override
-	public String getName() {
-	    String returnValue;
-	    if (System.getSecurityManager() != null) {
+    @Override
+    public String getName() {
+        String returnValue;
+        if (System.getSecurityManager() != null) {
             returnValue = AccessController.doPrivileged(new java.security.PrivilegedAction<String>() {
                 public String run() {
                     return _part.getFieldName();
@@ -175,12 +209,16 @@ public class SRTServletRequestPart implements Part {
         } else {
             returnValue = _part.getFieldName();
         }
-	    return returnValue;
-	}
+        
+        if (TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {  
+            logger.logp(Level.FINE, CLASS_NAME,"getName", " ["+returnValue+"]");
+        }
+        return returnValue;
+    }
 
-	@Override
-	public long getSize() {
-	    long returnValue;
+    @Override
+    public long getSize() {
+        long returnValue;
         if (System.getSecurityManager() != null) {
             Long longObjectValue = AccessController.doPrivileged(new java.security.PrivilegedAction<Long>() {
                 public Long run() {
@@ -191,17 +229,31 @@ public class SRTServletRequestPart implements Part {
         } else {
             returnValue = _part.getSize();
         }
+        
+        if (TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {  
+            logger.logp(Level.FINE, CLASS_NAME,"getSize", " ["+returnValue+"]");
+        }
+        
         return returnValue;
-	}
+    }
 
-	@Override
-	public void write(String fileName) throws IOException {
-	    if (System.getSecurityManager() != null) {
+    @Override
+    public void write(String fileName) throws IOException {
+        if (TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {  
+            logger.logp(Level.FINE, CLASS_NAME,"write", "fileName [" + fileName + "]");
+        }
+        if (System.getSecurityManager() != null) {
             try {
                 final String finalFileName = fileName;
                 AccessController.doPrivileged(new java.security.PrivilegedExceptionAction<Object>() {
                     public Object run() throws Exception {
                         File f = new File(_part.getStoreLocation().getParentFile(), finalFileName);
+
+                        if (TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {  
+                            logger.logp(Level.FINE, CLASS_NAME,"write", "location [" + f +"]");
+                            logger.logp(Level.FINE, CLASS_NAME,"write", "location from part.getStoreLocation [" + _part.getStoreLocation() +"]");
+                        }
+
                         _part.write(f);
                         return null;
                     }
@@ -212,16 +264,22 @@ public class SRTServletRequestPart implements Part {
             }
         } else {
             File f = new File(_part.getStoreLocation().getParentFile(), fileName);
+
+            if (TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {  
+                logger.logp(Level.FINE, CLASS_NAME,"write", "location [" + f +"]");
+                logger.logp(Level.FINE, CLASS_NAME,"write", "location from part.getStoreLocation [" + _part.getStoreLocation() +"]");
+            }
+            
             try {
                 _part.write(f);
             } catch (Exception e) {
                 throw new IOException(e);
             }
         }	    
-	}
-	
-	public boolean isFormField() {
-	    boolean returnValue;
+    }
+
+    public boolean isFormField() {
+        boolean returnValue;
         if (System.getSecurityManager() != null) {
             Boolean booleanObjectValue = AccessController.doPrivileged(new java.security.PrivilegedAction<Boolean>() {
                 public Boolean run() {
@@ -232,7 +290,11 @@ public class SRTServletRequestPart implements Part {
         } else {
             returnValue = _part.isFormField();
         }
+        
+        if (TraceComponent.isAnyTracingEnabled()&&logger.isLoggable (Level.FINE)) {  
+            logger.logp(Level.FINE, CLASS_NAME,"isFormField", " ["+returnValue+"]");
+        }
+        
         return returnValue;
-	}
-
+    }
 }
