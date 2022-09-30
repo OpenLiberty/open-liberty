e5530545f69ff744947a1c9838cad602f569746a
Greg Watts <gwatts@us.ibm.com>
2022-09-29 21:13:07 -0500

    Disable MP JWT 1.2 and 2.0 for Java 19+

--- dev/io.openliberty.microprofile.jwt.2.0.internal_fat_tck/fat/src/com/ibm/ws/microprofile/mpjwt20/tck/Mpjwt20TCKLauncher_noaud_noenv.java
+++ dev/io.openliberty.microprofile.jwt.2.0.internal_fat_tck/fat/src/com/ibm/ws/microprofile/mpjwt20/tck/Mpjwt20TCKLauncher_noaud_noenv.java
@@ -11,27 +11,24 @@
 package com.ibm.ws.microprofile.mpjwt20.tck;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import componenttest.annotation.MaximumJavaLevel;
 import componenttest.annotation.Server;
 import componenttest.custom.junit.runner.FATRunner;
 import componenttest.topology.impl.LibertyServer;
-import componenttest.topology.impl.JavaInfo;
 import componenttest.topology.utils.MvnUtils;
 
 /**
  * This is a test class that runs a whole Maven TCK as one test FAT test. *
  */
 //@Mode(TestMode.QUARANTINE)
+@MaximumJavaLevel(javaLevel = 18)
 @RunWith(FATRunner.class)
 public class Mpjwt20TCKLauncher_noaud_noenv {
 
