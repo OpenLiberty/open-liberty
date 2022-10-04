dcca4ae25dd1e64503d102fe192286a0bf1ddcc0
Paul Nicolucci <pnicolucci@gmail.com>
2022-10-03 21:36:20 -0400

    Update beans for Faces 4.0

--- dev/com.ibm.ws.jsf.2.2_fat/test-applications/JSF22HTML5.war/src/com/ibm/ws/jsf22/fat/html5/faces40/html5TestBean.java
+++ dev/com.ibm.ws.jsf.2.2_fat/test-applications/JSF22HTML5.war/src/com/ibm/ws/jsf22/fat/html5/faces40/html5TestBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2016  IBM Corporation and others.
+ * Copyright (c) 2015, 2022  IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,13 +13,13 @@ package com.ibm.ws.jsf22.fat.html5.faces40;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.faces.bean.ApplicationScoped;
-import javax.faces.bean.ManagedBean;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
 
 /**
  * The test bean for the HTML 5 tests for JSF 2.2
  */
-@ManagedBean(name = "html5TestBean")
+@Named("html5TestBean")
 @ApplicationScoped
 public class html5TestBean {
 
