9554846f50baf645e312f0fde45ec37b2e7fe754
Paul Nicolucci <pnicolucci@gmail.com>
2022-10-04 21:56:21 -0400

    MYFACES-4447 updates

--- dev/io.openliberty.org.apache.myfaces.4.0/src/org/apache/myfaces/webapp/MyFacesContainerInitializer.java
+++ dev/io.openliberty.org.apache.myfaces.4.0/src/org/apache/myfaces/webapp/MyFacesContainerInitializer.java
@@ -102,6 +102,16 @@ public class MyFacesContainerInitializer implements ServletContainerInitializer
      */
     public static final String FACES_SERVLET_FOUND = "org.apache.myfaces.FACES_SERVLET_FOUND";
 
+    /**
+     * Store the FacesServlet ServletRegistration using this key in the ServletContext.
+     * The is necessary for the Faces 4.0 Extensionless Mapping feature. This is used
+     * in FacesInitializerImpl when configuring the Extensionless Mapping feature since
+     * an UnsupportedOperationException is thrown when calling the ServletContext.getServletRegistrations
+     * method if the StartupServletContextListener was added programmatically.
+     */
+    public static final String FACES_SERVLET_SERVLETREGISTRATION =
+                "org.apache.myfaces.FACES_SERVLET_SERVLETREGISTRATION";
+
     private static final String FACES_CONFIG_RESOURCE = "/WEB-INF/faces-config.xml";
     private static final Logger log = Logger.getLogger(MyFacesContainerInitializer.class.getName());
     private static final String[] FACES_SERVLET_MAPPINGS = { "/faces/*", "*.jsf", "*.faces" };
@@ -177,6 +187,9 @@ public class MyFacesContainerInitializer implements ServletContainerInitializer
                     // now we have to set a field in the ServletContext to indicate that we have
                     // added the mapping dynamically.
                     servletContext.setAttribute(FACES_SERVLET_ADDED_ATTRIBUTE, Boolean.TRUE);
+
+                    // Add the FacesServlet ServletRegistration as an attribute for use during initialization.
+                    servletContext.setAttribute(FACES_SERVLET_SERVLETREGISTRATION, servlet);
 
                     // add a log message
                     log.log(Level.INFO, "Added FacesServlet with mappings=" + Arrays.toString(mappings));
@@ -321,6 +334,9 @@ public class MyFacesContainerInitializer implements ServletContainerInitializer
                 // we found a FacesServlet; set an attribute for use during initialization
                 servletContext.setAttribute(FACES_SERVLET_FOUND, Boolean.TRUE);
                 isFacesServletPresent = true;
+
+                // Add the FacesServlet ServletRegistration as an attribute for use during initialization.
+                servletContext.setAttribute(FACES_SERVLET_SERVLETREGISTRATION, servletEntry.getValue());
             }
         }
         return isFacesServletPresent;
