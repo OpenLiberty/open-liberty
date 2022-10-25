1728ebdebf42962d4fd4aaac2cc683bb916610f8
jim-krueger <jckofbyron@gmail.com>
2022-10-25 15:02:03 -0500

    Add Spec multipart providers and ensure historic internal multipart support is unaffected.

--- dev/io.openliberty.org.jboss.resteasy.common.ee10/src/org/jboss/resteasy/plugins/providers/multipart/IBMMultipartListProvider.java
+++ dev/io.openliberty.org.jboss.resteasy.common.ee10/src/org/jboss/resteasy/plugins/providers/multipart/IBMMultipartListProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020, 2022 IBM Corporation and others.
+ * Copyright (c) 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,14 +14,23 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.security.AccessController;
 import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.jboss.resteasy.spi.InternalServerErrorException;
+
+import com.ibm.websphere.jaxrs20.multipart.IAttachment;
+import com.ibm.websphere.jaxrs20.multipart.IMultipartBody;
+
+import jakarta.annotation.Priority;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.WebApplicationException;
@@ -33,15 +42,10 @@ import jakarta.ws.rs.ext.MessageBodyWriter;
 import jakarta.ws.rs.ext.Provider;
 import jakarta.ws.rs.ext.Providers;
 
-import com.ibm.websphere.jaxrs20.multipart.IAttachment;
-import com.ibm.websphere.jaxrs20.multipart.IMultipartBody;
-
-import org.jboss.resteasy.spi.InternalServerErrorException;
-
 @Provider
 @Consumes({"multipart/related", "multipart/mixed", "multipart/alternative", "multipart/form-data" })
 @Produces({"multipart/related", "multipart/mixed", "multipart/alternative", "multipart/form-data" })
-public class IBMMultipartProvider implements MessageBodyReader<Object>, MessageBodyWriter<Object> {
+public class IBMMultipartListProvider implements MessageBodyReader<List<Object>>, MessageBodyWriter<List<Object>> {
 
     private static final Set<Class<?>> MULTIPART_CLASSES = new HashSet<>();
     private static final Set<String> MULTIPART_SUBTYPES = new HashSet<>();
@@ -72,7 +76,8 @@ public class IBMMultipartProvider implements MessageBodyReader<Object>, MessageB
 
     private boolean isSupported(Class<?> type, Annotation[] anns, MediaType mt) {
         return mediaTypeSupported(mt)
-            && (MULTIPART_CLASSES.contains(type));
+            && (MULTIPART_CLASSES.contains(type)
+                || Collection.class.isAssignableFrom(type));
     }
 
     private boolean mediaTypeSupported(MediaType mt) {
@@ -80,13 +85,28 @@ public class IBMMultipartProvider implements MessageBodyReader<Object>, MessageB
     }
 
     @Override
-    public Object readFrom(Class<Object> clazz, Type genericType, Annotation[] anns, MediaType mt, MultivaluedMap<String, String> headers,
+    public List<Object> readFrom(Class<List<Object>> clazz, Type genericType, Annotation[] anns, MediaType mt, MultivaluedMap<String, String> headers,
                            InputStream entityStream) throws IOException, WebApplicationException {
         MultipartReader reader = new MultipartReader();
         reader.workers = providers;
         MultipartInput multiInput = reader.readFrom(MultipartInput.class,  genericType, anns, mt, headers, entityStream);
-        if (clazz.equals(IMultipartBody.class)) {
-            return new IMultipartBodyImpl((MultipartInputImpl)multiInput);
+        if (Collection.class.isAssignableFrom(clazz)) {
+            if (genericType instanceof ParameterizedType
+                            && ((ParameterizedType)genericType).getActualTypeArguments()[0].getTypeName().equals(IAttachment.class.getName())) {
+                List<Object> attachments = new ArrayList<>();
+                for (InputPart inputPart : multiInput.getParts()) {
+                    attachments.add(new IAttachmentImpl(inputPart));
+                }
+                return attachments;
+            }
+            if (genericType instanceof ParameterizedType
+                            && ((ParameterizedType)genericType).getActualTypeArguments()[0].getTypeName().equals(String.class.getName())) {
+                List<Object> parts = new ArrayList<>();
+                for (InputPart inputPart : multiInput.getParts()) {
+                    parts.add(inputPart.getBodyAsString());
+                }
+                return parts;
+            }
         }
         String genericTypeStr = genericType == null ? "null" : genericType.getTypeName();
         throw new InternalServerErrorException("Unexpected multipart type: " + clazz.getName() + " / " + genericTypeStr);
@@ -94,37 +114,37 @@ public class IBMMultipartProvider implements MessageBodyReader<Object>, MessageB
 
     @SuppressWarnings({ "unchecked", "rawtypes" })
     @Override
-    public void writeTo(Object entity, Class<?> clazz, Type genericType, Annotation[] anns, MediaType mt, MultivaluedMap<String, Object> headers,
+    public void writeTo(List<Object> entities, Class<?> clazz, Type genericType, Annotation[] anns, MediaType mt, MultivaluedMap<String, Object> headers,
                         OutputStream outputStream) throws IOException, WebApplicationException {
 
-        List<IAttachment> attachments;
-        if (entity instanceof IMultipartBody) {
-            attachments = ((IMultipartBody)entity).getAllAttachments();
-        } else {
-            throw new WebApplicationException("Unexpected output type");
-        }
         MultipartOutput outputObj = new MultipartOutput();
-        for (IAttachment attachment : attachments) {
-            Object content;
-            boolean java2SecurityEnabled = System.getSecurityManager() != null;
-            if (java2SecurityEnabled) {
-                try {
-                    content = AccessController.doPrivileged(new PrivilegedExceptionAction<Object>() {
-                       @Override
-                       public Object run() throws Exception
-                       {        return attachment.getDataHandler().getContent();        }
+        for (Object entity : entities) {
+            if (entity instanceof IAttachment) {
+                IAttachment attachment = (IAttachment)entity;
+                Object content;
+                boolean java2SecurityEnabled = System.getSecurityManager() != null;
+                if (java2SecurityEnabled) {
+                    try {
+                        content = AccessController.doPrivileged(new PrivilegedExceptionAction<Object>() {
+                           @Override
+                           public Object run() throws Exception
+                           {        return attachment.getDataHandler().getContent();        }
 
-                    });
-                 } catch (PrivilegedActionException pae)
-                 {      throw new RuntimeException(pae);    }
+                        });
+                     } catch (PrivilegedActionException pae)
+                     {      throw new RuntimeException(pae);    }
+                } else {
+                    content = attachment.getDataHandler().getContent();
+                }
+                
+                OutputPart part = outputObj.addPart(content, content.getClass(), null, attachment.getContentType(),
+                                                    ((IAttachmentImpl)attachment).getFileName());
+                attachment.getHeaders().entrySet().stream().forEach(entry -> {part.getHeaders().put(entry.getKey(), (List)entry.getValue());});
+
             } else {
-                content = attachment.getDataHandler().getContent();
+                throw new WebApplicationException("Unexpected output type");
             }
-            
-            OutputPart part = outputObj.addPart(content, content.getClass(), null, attachment.getContentType(),
-                                                ((IAttachmentImpl)attachment).getFileName());
-            attachment.getHeaders().entrySet().stream().forEach(entry -> {part.getHeaders().put(entry.getKey(), (List)entry.getValue());});
-           // part.getHeaders().putAll((Map<? extends String, ? extends List<Object>>) attachment.getHeaders());
+
         }
         writer.init();
         writer.writeTo(outputObj, clazz, genericType, anns, mt, headers, outputStream);
