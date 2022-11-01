71632fea6b400298c4702bc3973381345844e7bf
Adam Yoho <ayoho@us.ibm.com>
2022-11-01 15:06:13 -0500

    OIDC FAT updates after splitting out discovery tests

--- dev/com.ibm.ws.security.oidc.client_fat.1/fat/src/com/ibm/ws/security/openidconnect/client/fat/IBM/OidcClientDiscoveryErrorNoOpTrustStoreTests.java
+++ dev/com.ibm.ws.security.oidc.client_fat.1/fat/src/com/ibm/ws/security/openidconnect/client/fat/IBM/OidcClientDiscoveryErrorNoOpTrustStoreTests.java
@@ -12,7 +12,6 @@
 package com.ibm.ws.security.openidconnect.client.fat.IBM;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import org.junit.BeforeClass;
@@ -20,12 +19,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.ibm.ws.security.oauth_oidc.fat.commonTest.CommonTest;
-import com.ibm.ws.security.oauth_oidc.fat.commonTest.CommonTestHelpers;
 import com.ibm.ws.security.oauth_oidc.fat.commonTest.Constants;
 import com.ibm.ws.security.oauth_oidc.fat.commonTest.DiscoveryUtils;
+import com.ibm.ws.security.oauth_oidc.fat.commonTest.MessageConstants;
 import com.ibm.ws.security.oauth_oidc.fat.commonTest.TestSettings;
 import com.ibm.ws.security.oauth_oidc.fat.commonTest.ValidationData.validationData;
-import com.ibm.ws.security.openidconnect.client.fat.CommonTests.GenericOidcClientTests;
 import com.meterware.httpunit.WebConversation;
 
 import componenttest.annotation.AllowedFFDC;
@@ -34,44 +32,28 @@ import componenttest.custom.junit.runner.Mode;
 import componenttest.custom.junit.runner.Mode.TestMode;
 
 /**
- * This is the test class that will run error scenarios with OpenID Connect RP tests with an RP configuration (using access token
- * and X509 cert) which includes discovery of OP endpoints.
- * Discovery of OP endpoints is configured in the openidConnectClient section in server.xml by specifying the discoveryEndpointUrl
- * instead of configuring
- * each individual OP endpoint URL (such as authorizationEndpointUrl and tokenEndpointUrl) separately. This line below is used to
- * configure RP discovery:
- *
- * discoveryEndpointUrl="https://localhost:${bvt.prop.OP_HTTP_default.secure}/oidc/endpoint/OidcConfigSample/.well-known/openid-configuration"
- *
- * With discovery, all the discovered endpoints are HTTPS. Any endpoints which are specified in the configuration will be ignored
- * and the
- * discovered endpoints will be used instead.
- *
- * This test configures the sslRef as part of the openidConnectClient configuration so that the SSL processing is handled by the
- * client rather than
- * outside this configuration.
- *
- **/
-
+ * Test Purpose:
+ * <OL>
+ * <LI>When the RP is configured with discovery, attempt to access a test servlet specifying valid OP url
+ * <LI>The OP keystore and truststore is not configured.
+ * </OL>
+ * <P>
+ * Expected Results:
+ * <OL>
+ * <LI>SSL connection from RP to OP is refused and the discovery process cannot complete.
+ * <LI>The login page cannot be displayed and returns 401.
+ * </OL>
+ */
 @Mode(TestMode.FULL)
-@AllowedFFDC({ "org.apache.http.NoHttpResponseException", "java.security.cert.CertPathBuilderException", "sun.security.validator.ValidatorException", "javax.net.ssl.SSLHandshakeException", "com.ibm.websphere.ssl.SSLException", "java.lang.Exception" })
+@AllowedFFDC({ "org.apache.http.conn.HttpHostConnectException", "javax.net.ssl.SSLException", "java.net.SocketException" })
 @RunWith(FATRunner.class)
 public class OidcClientDiscoveryErrorNoOpTrustStoreTests extends CommonTest {
-    public static Class<?> thisClass = GenericOidcClientTests.class;
-    public static HashMap<String, Integer> defRespStatusMap = null;
 
-    public static String[] test_GOOD_LOGIN_ACTIONS = Constants.GOOD_OIDC_LOGIN_ACTIONS_SKIP_CONSENT;
-    public static String[] test_GOOD_POST_LOGIN_ACTIONS = Constants.GOOD_OIDC_POST_LOGIN_ACTIONS_SKIP_CONSENT;
-    public static String[] test_GOOD_LOGIN_AGAIN_ACTIONS = Constants.GOOD_OIDC_LOGIN_AGAIN_ACTIONS;
-    public static String[] test_LOGIN_PAGE_ONLY = Constants.GET_LOGIN_PAGE_ONLY;
-    public static String test_FinalAction = Constants.LOGIN_USER;
-    protected static String hostName = "localhost";
+    public static Class<?> thisClass = OidcClientDiscoveryErrorNoOpTrustStoreTests.class;
 
     @SuppressWarnings("serial")
     @BeforeClass
     public static void setUp() throws Exception {
-
-        thisClass = OidcClientDiscoveryErrorNoOpTrustStoreTests.class;
 
         List<String> apps = new ArrayList<String>() {
             {
@@ -81,91 +63,39 @@ public class OidcClientDiscoveryErrorNoOpTrustStoreTests extends CommonTest {
 
         testSettings = new TestSettings();
 
-        // Set config parameters for Access token with X509 Certificate
-        String tokenType = Constants.ACCESS_TOKEN_KEY;
-        String certType = Constants.X509_CERT;
-
         // Start the OIDC OP server
-        testOPServer = commonSetUp("com.ibm.ws.security.openidconnect.client-1.0_fat.op", "op_server_basic_ssl_no_truststore.xml", Constants.OIDC_OP, Constants.NO_EXTRA_APPS, Constants.DO_NOT_USE_DERBY, Constants.NO_EXTRA_MSGS, Constants.OPENID_APP, Constants.IBMOIDC_TYPE, true, true, tokenType, certType);
+        testOPServer = commonSetUp("com.ibm.ws.security.openidconnect.client-1.0_fat.op", "op_server_basic_ssl_no_truststore.xml", Constants.OIDC_OP, Constants.NO_EXTRA_APPS, Constants.DO_NOT_USE_DERBY, Constants.NO_EXTRA_MSGS, Constants.OPENID_APP, Constants.IBMOIDC_TYPE, true, false);
 
         DiscoveryUtils.waitForOPDiscoveryToBeReady(testSettings);
 
         //Start the OIDC RP server and setup default values
-        testRPServer = commonSetUp("com.ibm.ws.security.openidconnect.client-1.0_fat.rpd", "rp_server_orig.xml", Constants.OIDC_RP, apps, Constants.DO_NOT_USE_DERBY,
-                Constants.NO_EXTRA_MSGS, Constants.OPENID_APP, Constants.IBMOIDC_TYPE, true, true, tokenType, certType);
+        testRPServer = commonSetUp("com.ibm.ws.security.openidconnect.client-1.0_fat.rpd", "rp_server_orig.xml", Constants.OIDC_RP, apps, Constants.DO_NOT_USE_DERBY, Constants.NO_EXTRA_MSGS, Constants.OPENID_APP, Constants.IBMOIDC_TYPE);
 
-        testOPServer.addIgnoredServerException("CWOAU0039W"); // Ignore errors from the malformed discovery url
-        testOPServer.addIgnoredServerException("CWWKO0801E");
-        testRPServer.addIgnoredServerException("CWWKS1859E"); //Ignore exceptions from bad client secret test
-        testRPServer.addIgnoredServerException("SRVE8094W");
-        testRPServer.addIgnoredServerException("SRVE0190E");
-        testRPServer.addIgnoredServerException("CWWKS1741W"); // Ignore from sigAlgMismatch2 - message is not always logged
-        testRPServer.addIgnoredServerException("SRVE8115W"); // Ignore setStatus WARNING
-        testRPServer.addIgnoredServerException("CWWKS1521W"); // Ignore message indicating conflicting configured endpoints are ignored
-        testRPServer.addIgnoredServerException("CWWKS1524E"); // Ignore message indicating unsuccessful response from endpoint
+        testOPServer.addIgnoredServerException("CWWKG0058E"); // Ignore SSL failure message
         testRPServer.addIgnoredServerException("CWWKS1525E"); // Ignore message indicating discovery failed
-        testRPServer.addIgnoredServerException("CWWKS1522W"); // Ignore message indicating conflicting endpoints for issuer identifier
-        testRPServer.addIgnoredServerException("CWPKI0022E"); // Ignore message indicating conflicting configured endpoints are ignored
-        testRPServer.addIgnoredServerException("CWWKG0058E"); // Ignore SSL failure message
-
-        // override actions that generic tests should use - Need to skip consent form as httpunit
-        // cannot process the form because of embedded javascript
-
-        test_GOOD_LOGIN_ACTIONS = Constants.GOOD_OIDC_LOGIN_ACTIONS_SKIP_CONSENT;
-        test_GOOD_POST_LOGIN_ACTIONS = Constants.GOOD_OIDC_POST_LOGIN_ACTIONS_SKIP_CONSENT;
-        test_GOOD_LOGIN_AGAIN_ACTIONS = Constants.GOOD_OIDC_LOGIN_AGAIN_ACTIONS;
-        test_FinalAction = Constants.LOGIN_USER;
-        testSettings.setFlowType(Constants.RP_FLOW);
-
-        // Force discovery to run to access login page so that the discovery service is active. Then, in the actual tests the reconfig will initiate discovery and cause the
-        // error messages to be generated on discovery failures during the reconfig (rather than waiting until the first authorization request in the runtime).
-        CommonTestHelpers helpers = new CommonTestHelpers();
+        testRPServer.addIgnoredServerException("CWWKS1524E"); // Ignore message indicating unsuccessful response from endpoint
+        testRPServer.addIgnoredServerException("CWWKS1741W"); // Ignore from sigAlgMismatch2 - message is not always logged
+        testRPServer.addIgnoredServerException("CWWKS1859E"); // Ignore exceptions from bad client secret test
+        testRPServer.addIgnoredServerException("CWWKS1534E");
 
         // try to wait for discovery to have populated the RP config
         // Don't stop if this fails (there is a chance it could be ready by the time the tests actually run
         DiscoveryUtils.waitForRPDiscoveryToBeReady(testSettings);
-
     }
 
-    /**
-     * Test Purpose:
-     * <OL>
-     * <LI>When the RP is configured with discovery, attempt to access a test servlet specifying valid OP url
-     * <LI>In this scenario, the consent form is disabled in OP by setting the oauthProvider attribute
-     * <LI>in server.xml as: autoAuthorize="true". The OP keystore and truststore is not configured.
-     * <LI>SSL connection from RP to OP is refused and the discovery process cannot complete.
-     * </OL>
-     * <P>
-     * Expected Results:
-     * <OL>
-     * <LI>The login page cannot be displayed and returns 401
-     * <LI>Messages are logged to indicate that discovery did not complete:
-     * <LI>CWWKS1525E: A successful response was not returned from the URL
-     * [https://localhost:8947/oidc/endpoint/OidcConfigSample/.well-known/openid-configuration]. This is the [404] response status
-     * and the [CWOAU0073E: An error was encountered while authenticating a user.
-     * Please try authenticating again, or contact the site administrator if the problem persists.] error from the discovery
-     * request.
-     * <LI>CWWKS1524E: The OpenID Connect client [client01] failed to obtain Open ID Connect Provider endpoint information through
-     * the discovery endpoint URL [https://localhost:8947/oidc/endpoint/OidcConfigSample/.mal-formed/openid-configuration].
-     * Update the configuration for the OpenID Connect client with the correct HTTPS discovery endpoint URL.
-     * </OL>
-     */
-    @AllowedFFDC({ "org.apache.http.conn.HttpHostConnectException", "javax.net.ssl.SSLException", "java.net.SocketException" })
     @Test
-    public void OidcClientDiscoverySSLTest_NoOPTrustStore() throws Exception {
+    public void test_discoveryError_NoOPTrustStore() throws Exception {
 
         TestSettings updatedTestSettings = testSettings.copyTestSettings();
         updatedTestSettings.setScope("openid profile");
         updatedTestSettings.setTestURL(updatedTestSettings.getTestURL().replace(Constants.DEFAULT_SERVLET, "simple/goodSSL"));
 
-        List<validationData> expectations = vData.addSuccessStatusCodes(null);
+        List<validationData> expectations = validationTools.add401Responses(Constants.GET_LOGIN_PAGE);
+        validationTools.addMessageExpectation(testRPServer, expectations, Constants.GET_LOGIN_PAGE, Constants.MESSAGES_LOG, Constants.STRING_MATCHES, "Did not find CWWKS1534E message saying the authorization endpoint URL was missing.", MessageConstants.CWWKS1534E_MISSING_AUTH_ENDPOINT);
 
-        expectations = vData.addExpectation(expectations, Constants.GET_LOGIN_PAGE, Constants.EXCEPTION_MESSAGE, Constants.STRING_CONTAINS, "Should have received an exception for a connection refused exception", null, "java.net.ConnectException");
         WebConversation wc = new WebConversation();
 
-        testOPServer.addIgnoredServerException("CWWKG0058E");
-
-        genericRP(_testName, wc, updatedTestSettings, test_LOGIN_PAGE_ONLY, expectations);
+        genericRP(_testName, wc, updatedTestSettings, Constants.GET_LOGIN_PAGE_ONLY, expectations);
 
     }
 
--- dev/com.ibm.ws.security.oidc.client_fat.1/fat/src/com/ibm/ws/security/openidconnect/client/fat/IBM/OidcClientDiscoveryErrorNoRpTrustStoreTests.java
+++ dev/com.ibm.ws.security.oidc.client_fat.1/fat/src/com/ibm/ws/security/openidconnect/client/fat/IBM/OidcClientDiscoveryErrorNoRpTrustStoreTests.java
@@ -12,7 +12,6 @@
 package com.ibm.ws.security.openidconnect.client.fat.IBM;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import org.junit.BeforeClass;
@@ -20,12 +19,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.ibm.ws.security.oauth_oidc.fat.commonTest.CommonTest;
-import com.ibm.ws.security.oauth_oidc.fat.commonTest.CommonTestHelpers;
 import com.ibm.ws.security.oauth_oidc.fat.commonTest.Constants;
 import com.ibm.ws.security.oauth_oidc.fat.commonTest.DiscoveryUtils;
 import com.ibm.ws.security.oauth_oidc.fat.commonTest.TestSettings;
 import com.ibm.ws.security.oauth_oidc.fat.commonTest.ValidationData.validationData;
-import com.ibm.ws.security.openidconnect.client.fat.CommonTests.GenericOidcClientTests;
 import com.meterware.httpunit.WebConversation;
 
 import componenttest.annotation.AllowedFFDC;
@@ -34,44 +31,27 @@ import componenttest.custom.junit.runner.Mode;
 import componenttest.custom.junit.runner.Mode.TestMode;
 
 /**
- * This is the test class that will run error scenarios with OpenID Connect RP tests with an RP configuration (using access token
- * and X509 cert) which includes discovery of OP endpoints.
- * Discovery of OP endpoints is configured in the openidConnectClient section in server.xml by specifying the discoveryEndpointUrl
- * instead of configuring
- * each individual OP endpoint URL (such as authorizationEndpointUrl and tokenEndpointUrl) separately. This line below is used to
- * configure RP discovery:
- *
- * discoveryEndpointUrl="https://localhost:${bvt.prop.OP_HTTP_default.secure}/oidc/endpoint/OidcConfigSample/.well-known/openid-configuration"
- *
- * With discovery, all the discovered endpoints are HTTPS. Any endpoints which are specified in the configuration will be ignored
- * and the
- * discovered endpoints will be used instead.
- *
- * This test configures the sslRef as part of the openidConnectClient configuration so that the SSL processing is handled by the
- * client rather than
- * outside this configuration.
- *
- **/
-
+ * Test Purpose:
+ * <OL>
+ * <LI>With discovery enabled on the RP, attempt to access a test servlet specifying valid OP url
+ * <LI>The RP keystore and truststore is not configured.
+ * </OL>
+ * <P>
+ * Expected Results:
+ * <OL>
+ * <LI>Client connection is refused as RP server is not listening on SSL port due to missing keystore and truststore.
+ * </OL>
+ */
 @Mode(TestMode.FULL)
-@AllowedFFDC({ "org.apache.http.NoHttpResponseException", "java.security.cert.CertPathBuilderException", "sun.security.validator.ValidatorException", "javax.net.ssl.SSLHandshakeException", "com.ibm.websphere.ssl.SSLException", "java.lang.Exception" })
+@AllowedFFDC({ "java.security.cert.CertPathBuilderException", "sun.security.validator.ValidatorException", "javax.net.ssl.SSLHandshakeException", "com.ibm.websphere.ssl.SSLException" })
 @RunWith(FATRunner.class)
 public class OidcClientDiscoveryErrorNoRpTrustStoreTests extends CommonTest {
-    public static Class<?> thisClass = GenericOidcClientTests.class;
-    public static HashMap<String, Integer> defRespStatusMap = null;
 
-    public static String[] test_GOOD_LOGIN_ACTIONS = Constants.GOOD_OIDC_LOGIN_ACTIONS_SKIP_CONSENT;
-    public static String[] test_GOOD_POST_LOGIN_ACTIONS = Constants.GOOD_OIDC_POST_LOGIN_ACTIONS_SKIP_CONSENT;
-    public static String[] test_GOOD_LOGIN_AGAIN_ACTIONS = Constants.GOOD_OIDC_LOGIN_AGAIN_ACTIONS;
-    public static String[] test_LOGIN_PAGE_ONLY = Constants.GET_LOGIN_PAGE_ONLY;
-    public static String test_FinalAction = Constants.LOGIN_USER;
-    protected static String hostName = "localhost";
+    public static Class<?> thisClass = OidcClientDiscoveryErrorNoRpTrustStoreTests.class;
 
     @SuppressWarnings("serial")
     @BeforeClass
     public static void setUp() throws Exception {
-
-        thisClass = OidcClientDiscoveryErrorNoRpTrustStoreTests.class;
 
         List<String> apps = new ArrayList<String>() {
             {
@@ -81,71 +61,19 @@ public class OidcClientDiscoveryErrorNoRpTrustStoreTests extends CommonTest {
 
         testSettings = new TestSettings();
 
-        // Set config parameters for Access token with X509 Certificate
-        String tokenType = Constants.ACCESS_TOKEN_KEY;
-        String certType = Constants.X509_CERT;
-
         // Start the OIDC OP server
-        testOPServer = commonSetUp("com.ibm.ws.security.openidconnect.client-1.0_fat.op", "op_server_orig.xml", Constants.OIDC_OP, Constants.NO_EXTRA_APPS, Constants.DO_NOT_USE_DERBY, Constants.NO_EXTRA_MSGS, Constants.OPENID_APP, Constants.IBMOIDC_TYPE, true, true, tokenType, certType);
+        testOPServer = commonSetUp("com.ibm.ws.security.openidconnect.client-1.0_fat.op", "op_server_orig.xml", Constants.OIDC_OP, Constants.NO_EXTRA_APPS, Constants.DO_NOT_USE_DERBY, Constants.NO_EXTRA_MSGS, Constants.OPENID_APP, Constants.IBMOIDC_TYPE);
 
         DiscoveryUtils.waitForOPDiscoveryToBeReady(testSettings);
 
         //Start the OIDC RP server and setup default values
-        testRPServer = commonSetUp("com.ibm.ws.security.openidconnect.client-1.0_fat.rpd", "rp_server_basic_ssl_no_truststore.xml", Constants.OIDC_RP, apps, Constants.DO_NOT_USE_DERBY,
-                Constants.NO_EXTRA_MSGS, Constants.OPENID_APP, Constants.IBMOIDC_TYPE, true, true, tokenType, certType);
+        testRPServer = commonSetUp("com.ibm.ws.security.openidconnect.client-1.0_fat.rpd", "rp_server_basic_ssl_no_truststore.xml", Constants.OIDC_RP, apps, Constants.DO_NOT_USE_DERBY, Constants.NO_EXTRA_MSGS, Constants.OPENID_APP, Constants.IBMOIDC_TYPE, true, false);
 
-        testOPServer.addIgnoredServerException("CWOAU0039W"); // Ignore errors from the malformed discovery url
-        testOPServer.addIgnoredServerException("CWWKO0801E");
-        testRPServer.addIgnoredServerException("CWWKS1859E"); //Ignore exceptions from bad client secret test
-        testRPServer.addIgnoredServerException("SRVE8094W");
-        testRPServer.addIgnoredServerException("SRVE0190E");
-        testRPServer.addIgnoredServerException("CWWKS1741W"); // Ignore from sigAlgMismatch2 - message is not always logged
-        testRPServer.addIgnoredServerException("SRVE8115W"); // Ignore setStatus WARNING
-        testRPServer.addIgnoredServerException("CWWKS1521W"); // Ignore message indicating conflicting configured endpoints are ignored
-        testRPServer.addIgnoredServerException("CWWKS1524E"); // Ignore message indicating unsuccessful response from endpoint
-        testRPServer.addIgnoredServerException("CWWKS1525E"); // Ignore message indicating discovery failed
-        testRPServer.addIgnoredServerException("CWWKS1522W"); // Ignore message indicating conflicting endpoints for issuer identifier
-        testRPServer.addIgnoredServerException("CWPKI0022E"); // Ignore message indicating conflicting configured endpoints are ignored
         testRPServer.addIgnoredServerException("CWWKG0058E"); // Ignore SSL failure message
-
-        // override actions that generic tests should use - Need to skip consent form as httpunit
-        // cannot process the form because of embedded javascript
-
-        test_GOOD_LOGIN_ACTIONS = Constants.GOOD_OIDC_LOGIN_ACTIONS_SKIP_CONSENT;
-        test_GOOD_POST_LOGIN_ACTIONS = Constants.GOOD_OIDC_POST_LOGIN_ACTIONS_SKIP_CONSENT;
-        test_GOOD_LOGIN_AGAIN_ACTIONS = Constants.GOOD_OIDC_LOGIN_AGAIN_ACTIONS;
-        test_FinalAction = Constants.LOGIN_USER;
-        testSettings.setFlowType(Constants.RP_FLOW);
-
-        // Force discovery to run to access login page so that the discovery service is active. Then, in the actual tests the reconfig will initiate discovery and cause the
-        // error messages to be generated on discovery failures during the reconfig (rather than waiting until the first authorization request in the runtime).
-        CommonTestHelpers helpers = new CommonTestHelpers();
-
-        // try to wait for discovery to have populated the RP config
-        // Don't stop if this fails (there is a chance it could be ready by the time the tests actually run
-        DiscoveryUtils.waitForRPDiscoveryToBeReady(testSettings);
-
     }
 
-    /**
-     * Test Purpose:
-     * <OL>
-     * <LI>With discovery enabled on the RP, attempt to access a test servlet specifying valid OP url
-     * <LI>The RP keystore and truststore is not configured.
-     * <LI>The login attempt should fail with appropriate error.
-     * </OL>
-     * <P>
-     * Expected Results:
-     * <OL>
-     * <LI>Client connection is refused as RP server is not listening on SSL port
-     * <LI>due to missing keystore and truststore with the following message logged:
-     * <LI>CWWKS1524E: The OpenID Connect client failed to obtain OpenID Connect Provider information through the discovery
-     * endpoint...
-     * </OL>
-     */
     @Test
-    @AllowedFFDC({ "java.security.cert.CertPathBuilderException", "sun.security.validator.ValidatorException", "javax.net.ssl.SSLHandshakeException", "com.ibm.websphere.ssl.SSLException" })
-    public void OidcClientDiscoverySSLTest_NoRPTrustStore() throws Exception {
+    public void test_discoveryError_NoRPTrustStore() throws Exception {
 
         TestSettings updatedTestSettings = testSettings.copyTestSettings();
         updatedTestSettings.setScope("openid profile");
@@ -154,10 +82,8 @@ public class OidcClientDiscoveryErrorNoRpTrustStoreTests extends CommonTest {
 
         expectations = vData.addExpectation(expectations, Constants.GET_LOGIN_PAGE, Constants.EXCEPTION_MESSAGE, Constants.STRING_CONTAINS, "Should have received an exception for a connection refused exception", null, "java.net.ConnectException");
 
-        testRPServer.addIgnoredServerExceptions("CWWKG0058E");
-
         WebConversation wc = new WebConversation();
-        genericRP(_testName, wc, updatedTestSettings, test_LOGIN_PAGE_ONLY, expectations);
+        genericRP(_testName, wc, updatedTestSettings, Constants.GET_LOGIN_PAGE_ONLY, expectations);
     }
 
 }
