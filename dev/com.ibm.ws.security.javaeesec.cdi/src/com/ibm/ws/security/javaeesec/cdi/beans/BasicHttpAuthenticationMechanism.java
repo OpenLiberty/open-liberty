c77ff78186889724dd99a132a2a58223f145eac0
Zechariah Hein <zechariah.hein@ibm.com>
2022-11-07 13:45:28 -0600

    add WWW-Authenticate header for basicHAM Authentication failures

--- dev/com.ibm.ws.security.javaeesec.cdi/src/com/ibm/ws/security/javaeesec/cdi/beans/BasicHttpAuthenticationMechanism.java
+++ dev/com.ibm.ws.security.javaeesec.cdi/src/com/ibm/ws/security/javaeesec/cdi/beans/BasicHttpAuthenticationMechanism.java
@@ -135,6 +135,7 @@ public class BasicHttpAuthenticationMechanism implements HttpAuthenticationMecha
                                                            HttpMessageContext httpMessageContext) throws AuthenticationException {
         AuthenticationStatus status = AuthenticationStatus.SEND_FAILURE;
         int rspStatus = HttpServletResponse.SC_UNAUTHORIZED;
+        HttpServletResponse rsp = httpMessageContext.getResponse();
         if (authorizationHeader.startsWith("Basic ")) {
             String encodedHeader = authorizationHeader.substring(6);
             String basicAuthHeader = decodeCookieString(encodedHeader);
@@ -151,10 +152,13 @@ public class BasicHttpAuthenticationMechanism implements HttpAuthenticationMecha
                 } else if (status == AuthenticationStatus.NOT_DONE) {
                     // set SC_OK, since if the target is not protected, it'll be processed.
                     rspStatus = HttpServletResponse.SC_OK;
+                } else if (status == AuthenticationStatus.SEND_FAILURE) {
+                    rsp.setHeader("WWW-Authenticate", "Basic realm=\"" + realmName + "\"");
+                    httpMessageContext.getMessageInfo().getMap().put(AttributeNameConstants.WSCREDENTIAL_REALM, realmName);
                 }
             }
         }
-        httpMessageContext.getResponse().setStatus(rspStatus);
+        rsp.setStatus(rspStatus);
         return status;
     }
 
