b44bf636358285832fdded8373f4dcc10a3fc832
Adam Yoho <ayoho@us.ibm.com>
2022-11-08 14:24:59 -0600

    Jakarta Security 3.0 general refactoring
    - Adds a `io.openliberty.security.oidcclientcore.jwt.JwtUtils` class
        - Contains utility methods to help verifying JWS signatures
    - Refactors the `validate` method in `TokenResponseValidator` to break it down a bit, and to use the common JWT code for verifying the ID token
    - Miscellaneous refactoring

--- dev/io.openliberty.security.oidcclientcore.internal/src/io/openliberty/security/oidcclientcore/token/TokenResponseValidator.java
+++ dev/io.openliberty.security.oidcclientcore.internal/src/io/openliberty/security/oidcclientcore/token/TokenResponseValidator.java
@@ -10,11 +10,16 @@
  *******************************************************************************/
 package io.openliberty.security.oidcclientcore.token;
 
+import java.security.Key;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.jose4j.jws.JsonWebSignature;
 import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.MalformedClaimException;
+import org.jose4j.jwt.consumer.InvalidJwtException;
+import org.jose4j.jwt.consumer.JwtConsumer;
+import org.jose4j.jwt.consumer.JwtConsumerBuilder;
 import org.jose4j.jwt.consumer.JwtContext;
 import org.jose4j.jwx.JsonWebStructure;
 
@@ -22,6 +27,7 @@ import com.ibm.json.java.JSONObject;
 import com.ibm.websphere.ras.ProtectedString;
 import com.ibm.websphere.ras.Tr;
 import com.ibm.websphere.ras.TraceComponent;
+import com.ibm.websphere.ras.annotation.Sensitive;
 import com.ibm.ws.ffdc.annotation.FFDCIgnore;
 import com.ibm.ws.security.common.jwk.impl.JWKSet;
 
@@ -29,11 +35,11 @@ import io.openliberty.security.common.jwt.JwtParsingUtils;
 import io.openliberty.security.oidcclientcore.authentication.AuthorizationRequestParameters;
 import io.openliberty.security.oidcclientcore.client.OidcClientConfig;
 import io.openliberty.security.oidcclientcore.config.MetadataUtils;
-import io.openliberty.security.oidcclientcore.config.OidcMetadataService;
+import io.openliberty.security.oidcclientcore.exceptions.OidcClientConfigurationException;
+import io.openliberty.security.oidcclientcore.exceptions.OidcDiscoveryException;
+import io.openliberty.security.oidcclientcore.jwt.JwtUtils;
 import io.openliberty.security.oidcclientcore.storage.Storage;
 import io.openliberty.security.oidcclientcore.storage.StorageFactory;
-import io.openliberty.security.oidcclientcore.utils.CommonJose4jUtils;
-import io.openliberty.security.oidcclientcore.utils.CommonJose4jUtils.TokenSignatureValidationBuilder;
 
 public class TokenResponseValidator {
 
@@ -42,7 +48,6 @@ public class TokenResponseValidator {
     static boolean initialized = false;
 
     OidcClientConfig clientConfig;
-    CommonJose4jUtils jose4jutil = new CommonJose4jUtils();
     JWKSet jwkset;
     private HttpServletRequest request;
     private HttpServletResponse response;
@@ -53,81 +58,94 @@ public class TokenResponseValidator {
         this.clientConfig = oidcClientConfig;
     }
 
-    @FFDCIgnore(Exception.class)
+    @FFDCIgnore({ TokenValidationException.class, Exception.class })
     public JwtClaims validate(TokenResponse tokenResponse) throws TokenValidationException {
         String idtoken = null;
-
         if (tokenResponse != null) {
             idtoken = tokenResponse.getIdTokenString();
         }
-
         if (idtoken == null) {
             return new JwtClaims();
         }
 
-        JwtContext jwtcontext = null;
+        JwtContext jwtcontext = getJwtContextForIdToken(idtoken);
+        JwtClaims jwtClaims = getJwtClaimsFromIdTokenContext(jwtcontext);
+
+        String clientSecret = null;
+        ProtectedString clientSecretProtectedString = clientConfig.getClientSecret();
+        if (clientSecretProtectedString != null) {
+            clientSecret = new String(clientSecretProtectedString.getChars());
+        }
         try {
-            jwtcontext = JwtParsingUtils.parseJwtWithoutValidation(idtoken);
-
+            String issuerconfigured = validateIdTokenClaimsAndGetIssuer(jwtClaims, clientSecret);
+            return validateIdTokenFormat(idtoken, jwtcontext, clientSecret, issuerconfigured);
+        } catch (TokenValidationException e) {
+            throw e;
         } catch (Exception e) {
-            String error = e.getMessage() != null ? e.getMessage() : "not a valid id token";
-            throw new TokenValidationException(this.clientConfig.getClientId(), error);
+            throw new TokenValidationException(this.clientConfig.getClientId(), e.toString());
         }
+    }
 
+    @FFDCIgnore(InvalidJwtException.class)
+    JwtContext getJwtContextForIdToken(String idTokenString) throws TokenValidationException {
+        JwtContext jwtContext = null;
+        try {
+            jwtContext = JwtParsingUtils.parseJwtWithoutValidation(idTokenString);
+        } catch (InvalidJwtException e) {
+            throw new TokenValidationException(this.clientConfig.getClientId(), e.toString());
+        }
+        return jwtContext;
+    }
+
+    JwtClaims getJwtClaimsFromIdTokenContext(JwtContext jwtContext) throws TokenValidationException {
         JwtClaims jwtClaims = null;
-        if (jwtcontext != null && jwtcontext.getJwtClaims() != null) {
-            jwtClaims = jwtcontext.getJwtClaims();
-            String clientSecret = null;
-            ProtectedString clientSecretProtectedString = clientConfig.getClientSecret();
-            if (clientSecretProtectedString != null) {
-                clientSecret = new String(clientSecretProtectedString.getChars());
-            }
-            String issuerconfigured = null;
-            // must have claims - iat and exp
-            try {
-                if (jwtClaims.getIssuedAt() == null) {
-                    throw new TokenValidationException(this.clientConfig.getClientId(), "token is missing required iat claim");
-                }
-                if (jwtClaims.getExpirationTime() == null) {
-                    throw new TokenValidationException(this.clientConfig.getClientId(), "token is missing required exp claim");
-                }
-                TokenValidator tokenValidator = new IdTokenValidator(clientConfig);
-                issuerconfigured = MetadataUtils.getIssuer(clientConfig);
-                tokenValidator.issuer(jwtClaims.getIssuer()).subject(jwtClaims.getSubject()).audiences(jwtClaims.getAudience()).azp(((String) jwtClaims.getClaimValue("azp"))).iat(jwtClaims.getIssuedAt()).exp(jwtClaims.getExpirationTime()).nbf(jwtClaims.getNotBefore()).issuerconfigured(issuerconfigured);
-                tokenValidator.validate();
-                if (this.clientConfig.isUseNonce()) {
-                    ((IdTokenValidator) tokenValidator).nonce(((String) jwtClaims.getClaimValue("nonce")));
-                    ((IdTokenValidator) tokenValidator).state(getStateParameter());
-                    ((IdTokenValidator) tokenValidator).secret(clientSecret);
-                    storage = StorageFactory.instantiateStorage(request, response, clientConfig.isUseSession());
-                    ((IdTokenValidator) tokenValidator).storage(storage);
-
-                    ((IdTokenValidator) tokenValidator).validateNonce();
-                }
-            } catch (Exception e) {
-                throw new TokenValidationException(this.clientConfig.getClientId(), e.getMessage());
-            }
-
-            try {
-                JsonWebStructure jsonStruct = JwtParsingUtils.getJsonWebStructureFromJwtContext(jwtcontext);
-                if (jsonStruct == null || !(jsonStruct instanceof JsonWebSignature)) {
-                    throw new TokenValidationException(this.clientConfig.getClientId(), "jsonwebsignature error");
-                }
-                TokenSignatureValidationBuilder tokenSignatureValidationBuilder = jose4jutil.signaturevalidationbuilder();
-                String jwksUri = MetadataUtils.getJwksUri(clientConfig);
-                tokenSignatureValidationBuilder.signature(jsonStruct).sslsupport(OidcMetadataService.getSSLSupport()).jwkuri(jwksUri).issuer(issuerconfigured).clientid(clientConfig.getClientId());
-
-                tokenSignatureValidationBuilder.clientsecret(clientSecret);
-                tokenSignatureValidationBuilder.jwkset(jwkset);
-                tokenSignatureValidationBuilder.jwksConnectTimeout(clientConfig.getJwksConnectTimeout());
-                tokenSignatureValidationBuilder.jwksReadTimeout(clientConfig.getJwksReadTimeout());
-
-                return tokenSignatureValidationBuilder.parseJwtWithValidation(idtoken);
-            } catch (Exception e) {
-                throw new TokenValidationException(this.clientConfig.getClientId(), e.getMessage());
-            }
+        if (jwtContext != null) {
+            jwtClaims = jwtContext.getJwtClaims();
         }
-        throw new TokenValidationException(this.clientConfig.getClientId(), "not a valid token to continue the flow");
+        if (jwtContext == null || jwtClaims == null) {
+            // TODO - NLS message
+            throw new TokenValidationException(this.clientConfig.getClientId(), "not a valid token to continue the flow");
+        }
+        return jwtClaims;
+    }
+
+    String validateIdTokenClaimsAndGetIssuer(JwtClaims jwtClaims,
+                                             @Sensitive String clientSecret) throws MalformedClaimException, TokenValidationException, OidcDiscoveryException, OidcClientConfigurationException {
+        // must have claims - iat and exp
+        if (jwtClaims.getIssuedAt() == null) {
+            String nlsMsg = Tr.formatMessage(tc, "TOKEN_MISSING_REQUIRED_CLAIM", "iat");
+            throw new TokenValidationException(this.clientConfig.getClientId(), nlsMsg);
+        }
+        if (jwtClaims.getExpirationTime() == null) {
+            String nlsMsg = Tr.formatMessage(tc, "TOKEN_MISSING_REQUIRED_CLAIM", "exp");
+            throw new TokenValidationException(this.clientConfig.getClientId(), nlsMsg);
+        }
+        TokenValidator tokenValidator = new IdTokenValidator(clientConfig);
+        String issuerconfigured = MetadataUtils.getIssuer(clientConfig);
+        tokenValidator.issuer(jwtClaims.getIssuer()).subject(jwtClaims.getSubject()).audiences(jwtClaims.getAudience()).azp(((String) jwtClaims.getClaimValue("azp"))).iat(jwtClaims.getIssuedAt()).exp(jwtClaims.getExpirationTime()).nbf(jwtClaims.getNotBefore()).issuerconfigured(issuerconfigured);
+        tokenValidator.validate();
+        if (this.clientConfig.isUseNonce()) {
+            ((IdTokenValidator) tokenValidator).nonce(((String) jwtClaims.getClaimValue("nonce")));
+            ((IdTokenValidator) tokenValidator).state(getStateParameter());
+            ((IdTokenValidator) tokenValidator).secret(clientSecret);
+            storage = StorageFactory.instantiateStorage(request, response, clientConfig.isUseSession());
+            ((IdTokenValidator) tokenValidator).storage(storage);
+
+            ((IdTokenValidator) tokenValidator).validateNonce();
+        }
+        return issuerconfigured;
+    }
+
+    JwtClaims validateIdTokenFormat(String idtoken, JwtContext jwtcontext, String clientSecret, String issuerconfigured) throws Exception {
+        JsonWebStructure jws = JwtParsingUtils.getJsonWebStructureFromJwtContext(jwtcontext);
+        Key verificationKey = JwtUtils.getJwsVerificationKey(jws, clientConfig);
+
+        JwtConsumerBuilder builder = new JwtConsumerBuilder();
+        builder.setRequireExpirationTime().setAllowedClockSkewInSeconds(120).setExpectedAudience(clientConfig.getClientId()).setExpectedIssuer(false,
+                                                                                                                                               issuerconfigured).setRequireSubject().setSkipDefaultAudienceValidation().setVerificationKey(verificationKey).setRelaxVerificationKeyValidation();
+
+        JwtConsumer jwtConsumer = builder.build();
+        return JwtParsingUtils.parseJwtWithValidation(idtoken, jwtConsumer);
     }
 
     public String getStateParameter() {
