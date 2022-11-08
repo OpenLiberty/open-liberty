902c1269333a31e27a865d5517c090f741d1c161
Jesse Van Hill <jlvanhil@us.ibm.com>
2022-11-08 16:58:05 -0600

    Disable Infinispan tests in remote builds.

--- dev/io.openliberty.jcache.internal_fat.hazelcast/fat/src/io/openliberty/jcache/internal/fat/JCacheAuthenticationCacheServerRestartTest.java
+++ dev/io.openliberty.jcache.internal_fat.hazelcast/fat/src/io/openliberty/jcache/internal/fat/JCacheAuthenticationCacheServerRestartTest.java
@@ -42,10 +42,15 @@ import componenttest.topology.impl.LibertyServer;
 @Mode(TestMode.FULL)
 public class JCacheAuthenticationCacheServerRestartTest extends BaseTestCase {
 
-    private static BasicAuthClient basicAuthClient1;
+    private static BasicAuthClient basicAuthClient2;
 
+    // This server will stay up for cache topologies where the servers are members of the cache.
     @Server("io.openliberty.jcache.internal.fat.auth.cache.restart.1")
     public static LibertyServer server1;
+
+    // This server will be restarted.
+    @Server("io.openliberty.jcache.internal.fat.auth.cache.restart.2")
+    public static LibertyServer server2;
 
     @BeforeClass
     public static void beforeClass() {
@@ -61,38 +66,51 @@ public class JCacheAuthenticationCacheServerRestartTest extends BaseTestCase {
          */
         server1.addInstalledAppForValidation("basicauth");
         startServer1(server1, groupName, null, null);
-        basicAuthClient1 = new BasicAuthClient(server1);
         waitForCreatedOrExistingJCache(server1, AUTH_CACHE_NAME);
         waitForDefaultHttpsEndpoint(server1);
+
+        /*
+         * Start server 2.
+         */
+        server2.addInstalledAppForValidation("basicauth");
+        startServer2(server2, groupName);
+        basicAuthClient2 = new BasicAuthClient(server2);
+        waitForCreatedOrExistingJCache(server2, AUTH_CACHE_NAME);
+        waitForDefaultHttpsEndpoint(server2);
     }
 
     @After
     public void after() throws Exception {
-
         try {
             /*
              * We should not have cleared the authentication cache's JCache.
              */
             assertAuthCacheJCacheNotCleared(server1);
+            assertAuthCacheJCacheNotCleared(server2);
         } finally {
             /*
-             * Stop the server.
+             * Stop servers in the reverse order they were started.
              */
-            stopServer(server1);
+            try {
+                stopServer(server2);
+            } finally {
+                stopServer(server1);
+            }
         }
     }
 
     /**
      * Test caching of LTPA cookies in the distributed JCache authentication cache with
-     * a server restart.
+     * a server restart. One server (server1) will remain up to maintain the cache in
+     * topologies where the Liberty servers are members of the cache.
      *
      * <pre>
-     * 1. Call server1 using basic authentication. This will result in a cache miss and will insert an entry
+     * 1. Call server2 using basic authentication. This will result in a cache miss and will insert an entry
      *    into the distributed authentication cache.
-     * 2. Call server1 using LTPA cookie from the first request to show a cache hit.
-     * 3. Restart server1.
-     * 4. Call server1 using the LTPA cookie from the first request. This will result in an authentication cache hit since the request
-     *    to server1 cached it in the distributed authentication cache and the server restart should not clear the cache.
+     * 2. Call server2 using LTPA cookie from the first request to show a cache hit.
+     * 3. Restart server2.
+     * 4. Call server2 using the LTPA cookie from the first request. This will result in an authentication cache hit since the request
+     *    to server2 cached it in the distributed authentication cache and the server restart should not clear the cache.
      * </pre>
      *
      * @throws Exception if the test fails for some unforeseen reason.
@@ -102,36 +120,36 @@ public class JCacheAuthenticationCacheServerRestartTest extends BaseTestCase {
     public void authCache_restart() throws Exception {
 
         /*
-         * 1. Call server1 using basic authentication. This will result in a cache miss and will insert an entry
+         * 1. Call server2 using basic authentication. This will result in a cache miss and will insert an entry
          * into the distributed authentication cache.
          */
-        String response = basicAuthClient1.accessProtectedServletWithAuthorizedCredentials(ServletClient.PROTECTED_ALL_ROLE, USER1_NAME, USER1_PASSWORD);
-        assertTrue("Did not get the expected response", basicAuthClient1.verifyResponse(response, USER1_NAME, false, false));
+        String response = basicAuthClient2.accessProtectedServletWithAuthorizedCredentials(ServletClient.PROTECTED_ALL_ROLE, USER1_NAME, USER1_PASSWORD);
+        assertTrue("Did not get the expected response", basicAuthClient2.verifyResponse(response, USER1_NAME, false, false));
         assertResponseContainsCustomCredentials(response);
-        assertJCacheBasicAuthCacheHit(false, server1);
+        assertJCacheBasicAuthCacheHit(false, server2);
 
         /*
-         * 2. Call server1 using LTPA cookie from the first request to verify the entry is
+         * 2. Call server2 using LTPA cookie from the first request to verify the entry is
          * cached before restarting the server.
          */
-        response = basicAuthClient1.accessProtectedServletWithAuthorizedCookie(ServletClient.PROTECTED_ALL_ROLE, basicAuthClient1.getCookieFromLastLogin());
-        assertTrue("Did not get the expected response", basicAuthClient1.verifyResponse(response, USER1_NAME, false, false));
+        response = basicAuthClient2.accessProtectedServletWithAuthorizedCookie(ServletClient.PROTECTED_ALL_ROLE, basicAuthClient2.getCookieFromLastLogin());
+        assertTrue("Did not get the expected response", basicAuthClient2.verifyResponse(response, USER1_NAME, false, false));
         assertResponseContainsCustomCredentials(response);
-        assertJCacheLtpaAuthCacheHit(true, server1, basicAuthClient1.getCookieFromLastLogin());
+        assertJCacheLtpaAuthCacheHit(true, server2, basicAuthClient2.getCookieFromLastLogin());
 
         /*
-         * 3. Restart server1.
+         * 3. Restart server2.
          */
-        server1.restartServer();
+        server2.restartServer();
 
         /*
-         * 4. Call server1 using LTPA token from first request. This will result in a cache hit because
+         * 4. Call server2 using LTPA token from first request. This will result in a cache hit because
          * the distributed authentication cache has maintained entries across the sever restart.
          */
-        response = basicAuthClient1.accessProtectedServletWithAuthorizedCookie(ServletClient.PROTECTED_ALL_ROLE, basicAuthClient1.getCookieFromLastLogin());
-        assertTrue("Did not get the expected response", basicAuthClient1.verifyResponse(response, USER1_NAME, false, false));
+        response = basicAuthClient2.accessProtectedServletWithAuthorizedCookie(ServletClient.PROTECTED_ALL_ROLE, basicAuthClient2.getCookieFromLastLogin());
+        assertTrue("Did not get the expected response", basicAuthClient2.verifyResponse(response, USER1_NAME, false, false));
         assertResponseContainsCustomCredentials(response);
-        assertJCacheLtpaAuthCacheHit(true, server1, basicAuthClient1.getCookieFromLastLogin());
+        assertJCacheLtpaAuthCacheHit(true, server2, basicAuthClient2.getCookieFromLastLogin());
 
     }
 }
