a51f2f4e6cbaa4de46095d74026ca0adeb1a4d5d
Nathan Rauh <nathan.rauh@us.ibm.com>
2022-11-15 19:39:46 -0600

    Issue #22673 test for DataProvider

---
+++ dev/io.openliberty.data.internal_fat/test-applications/ProviderTestApp/src/test/jakarta/data/inmemory/web/Palindromes.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+package test.jakarta.data.inmemory.web;
+
+import java.util.List;
+
+import jakarta.data.repository.DataRepository;
+import jakarta.data.repository.Limit;
+import jakarta.data.repository.Repository;
+
+/**
+ *
+ */
+@Repository
+public interface Palindromes extends DataRepository<Palindrome, Long> {
+    List<Palindrome> findByLengthOrderByLettersAsc(int length, Limit limit);
+}
---
+++ dev/io.openliberty.data.internal_fat/test-applications/ProviderTestApp/src/test/jakarta/data/inmemory/web/ProviderTestServlet.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package test.jakarta.data.inmemory.web;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import jakarta.data.repository.Limit;
+import jakarta.inject.Inject;
+import jakarta.servlet.annotation.WebServlet;
+
+import org.junit.Test;
+
+import componenttest.app.FATServlet;
+
+@WebServlet("/*")
+public class ProviderTestServlet extends FATServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Inject
+    Palindromes palindromes;
+
+    /**
+     * Uses a template to insert, update, find, and delete entities.
+     */
+    @Test
+    public void testDataProvider() {
+        assertEquals(List.of("civic", "kayak", "level", "radar"),
+                     palindromes.findByLengthOrderByLettersAsc(5, Limit.of(4))
+                                     .stream()
+                                     .map(p -> p.letters)
+                                     .collect(Collectors.toList()));
+
+        assertEquals(List.of("deed", "noon"),
+                     palindromes.findByLengthOrderByLettersAsc(4, Limit.of(2))
+                                     .stream()
+                                     .map(p -> p.letters)
+                                     .collect(Collectors.toList()));
+    }
+}
