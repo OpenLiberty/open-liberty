13c597bd4303a5f1c3d017ef72647f4d1dd3a58a
Paul Nicolucci <pnicolucci@gmail.com>
2022-11-17 19:15:33 -0500

    Move beans to two new packages

--- dev/com.ibm.ws.jsf.2.2_fat/test-applications/JSF22FacesFlows.war/src/com/ibm/ws/jsf22/fat/flows/beans/jsf22/InitializerBean.java
+++ dev/com.ibm.ws.jsf.2.2_fat/test-applications/JSF22FacesFlows.war/src/com/ibm/ws/jsf22/fat/flows/beans/jsf22/InitializerBean.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  */
-package com.ibm.ws.jsf22.fat.flows.beans;
+package com.ibm.ws.jsf22.fat.flows.beans.jsf22;
 
 import java.util.Map;
 
