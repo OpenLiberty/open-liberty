0db3ba984af69a6b72f25838acb66e06c26c2a84
Zechariah Hein <zechariah.hein@ibm.com>
2022-12-01 13:12:00 -0600

    add basicHAM decorated and contextWrapper test

--- dev/com.ibm.ws.security.javaeesec_fat/fat/src/com/ibm/ws/security/javaeesec/fat/BasicAuthenticationMechanismTest.java
+++ dev/com.ibm.ws.security.javaeesec_fat/fat/src/com/ibm/ws/security/javaeesec/fat/BasicAuthenticationMechanismTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2019 IBM Corporation and others.
+ * Copyright (c) 2017, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -179,6 +179,29 @@ public class BasicAuthenticationMechanismTest extends JavaEESecTestBase {
     public void testBasicAuthValidUserInRole_DeniedAccess_WrongPassword() throws Exception {
         executeGetRequestBasicAuthCreds(httpclient, urlHttp + queryString, Constants.javaeesec_basicRoleUser, Constants.jaspi_invalidPwd,
                                         HttpServletResponse.SC_FORBIDDEN);
+    }
+
+    /**
+     * Verify the following:
+     * <OL>
+     * <LI> Attempt to access a protected servlet configured for basic HAM using annotation, Decorator, and HttpMessageContextWrapper.
+     * <LI> Submit the request with no authorization header and verify a challenge 401 is sent back.
+     * </OL>
+     * <P> Expected Results:
+     * <OL>
+     * <LI> Return code 401
+     * <LI> Verify response contains:
+     * <LI> 1. the 401 challenge
+     * <LI> 2. the HttpMessageContextWrapper Header {"BasicHAMMessageContextWrapper", "I have been wrapped!"}
+     * <LI> 3. the BasicHAMDecorator Header {"BasicHAMDecorator", "I have been decorated!"}
+     * </OL>
+     */
+    @Test
+    public void testBasicAuth_challenge401_noUser() throws Exception {
+        httpclient.getCredentialsProvider().clear();
+        HttpResponse response = accessPageWithChallenge(httpclient, urlHttp + "/JavaEESecAnnotatedBasicAuthServlet/JavaEESecAnnotatedBasic", HttpServletResponse.SC_UNAUTHORIZED);
+        checkHeader(response, "BasicHAMMessageContextWrapper", "I have been wrapped!");
+        checkHeader(response, "BasicHAMDecorator", "I have been decorated!");
     }
 
     @Test
---
+++ dev/com.ibm.ws.security.javaeesec_fat/test-applications/JavaEESecAnnotatedBasicAuthServlet.war/src/web/war/annotatedbasic/BasicHAMMessageContextWrapper.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package web.war.annotatedbasic;
+
+import java.util.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Default;
+import javax.security.enterprise.AuthenticationStatus;
+import javax.security.enterprise.authentication.mechanism.http.HttpMessageContext;
+import javax.security.enterprise.authentication.mechanism.http.HttpMessageContextWrapper;
+import javax.servlet.http.HttpServletResponse;
+
+@Default
+@ApplicationScoped
+public class BasicHAMMessageContextWrapper extends HttpMessageContextWrapper {
+    @SuppressWarnings("unused")
+    private static Logger log = Logger.getLogger(BasicHAMMessageContextWrapper.class.getName());
+
+    private final HttpServletResponse response;
+
+    public BasicHAMMessageContextWrapper(HttpServletResponse response, HttpMessageContext httpMessageContext) {
+        super(httpMessageContext);
+        this.response = response;
+    }
+
+    @Override
+    public AuthenticationStatus responseUnauthorized() {
+        response.addHeader("BasicHAMMessageContextWrapper", "I have been wrapped!");
+        return super.responseUnauthorized();
+    }
+
+}
