50013490a9aebfc53c1fade4c107cce910b9c382
Adam Anderson <atanders@us.ibm.com>
2022-12-02 17:34:40 -0600

    [22598] create a SimpleGlobalHandlerTest FAT for JAX-RS and repeat the JAX-RS Global Handler tests for EE10

---
+++ dev/io.openliberty.ws.jaxrs.global.handler.internal_fat/test-applications/SimpleGlobalHandler/src/io/openliberty/jaxrs/fat/globalhandler/simple/SimpleGlobalHandlerApplication.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.jaxrs.fat.globalhandler.simple;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class SimpleGlobalHandlerApplication extends Application {}
