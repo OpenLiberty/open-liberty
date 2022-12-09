81fc8964608d372334811a63d9c599e76fe47d8f
Kyle Aure <KyleJAure@gmail.com>
2022-12-09 11:22:02 -0600

    Introduce multiple file testing

--- dev/io.openliberty.org.apache.myfaces.4.0_fat/fat/src/io/openliberty/org/apache/myfaces40/fat/FATSuite.java
+++ dev/io.openliberty.org.apache.myfaces.4.0_fat/fat/src/io/openliberty/org/apache/myfaces40/fat/FATSuite.java
@@ -26,6 +26,7 @@ import io.openliberty.org.apache.myfaces40.fat.tests.ExternalContextAddResponseC
 import io.openliberty.org.apache.myfaces40.fat.tests.Faces40ThirdPartyApiTests;
 import io.openliberty.org.apache.myfaces40.fat.tests.FacesConfigTest;
 import io.openliberty.org.apache.myfaces40.fat.tests.FacesContextGetLifecycleTest;
+import io.openliberty.org.apache.myfaces40.fat.tests.MultipleInputFileTest;
 import io.openliberty.org.apache.myfaces40.fat.tests.InputTextTypeTest;
 import io.openliberty.org.apache.myfaces40.fat.tests.LayoutAttributeTests;
 import io.openliberty.org.apache.myfaces40.fat.tests.SelectItemTests;
@@ -38,7 +39,8 @@ import io.openliberty.org.apache.myfaces40.fat.tests.UIViewRootGetDoctypeTest;
                 ExternalContextAddResponseCookieTest.class,
                 FacesConfigTest.class,
                 InputTextTypeTest.class,
-                LayoutAttributeTests.class,
+				LayoutAttributeTests.class,
+                MultipleInputFileTest.class,
                 ExtensionlessMappingTest.class,
                 SelectItemTests.class,
                 FacesContextGetLifecycleTest.class,
