7d50dc9a0946c7a8ef76c88c9aa335857817d571
Daniel Belina <belina@us.ibm.com>
2022-12-11 16:27:31 -0500

    Split batch open fats for run time length purposes

---
+++ dev/com.ibm.ws.jbatch.open.security_fat/fat/src/batch/fat/junit/FATSuite.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package batch.fat.junit;
+
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import componenttest.rules.repeater.EmptyAction;
+import componenttest.rules.repeater.FeatureReplacementAction;
+import componenttest.rules.repeater.JakartaEE10Action;
+import componenttest.rules.repeater.JakartaEE9Action;
+import componenttest.rules.repeater.RepeatTests;
+
+/**
+ * Collection of all example tests
+ */
+@RunWith(Suite.class)
+/*
+ * The classes specified in the @SuiteClasses annotation
+ * below should represent all of the test cases for this FAT.
+ *
+ */
+@SuiteClasses({
+                BatchJobOperatorApiWithAppSecurityTest.class,
+                BatchSecurityTest.class,
+                BatchEveryoneSecurityTest.class
+})
+public class FATSuite {
+    @ClassRule
+    public static RepeatTests r = RepeatTests.with(new EmptyAction().fullFATOnly())
+                    .andWith(new JakartaEE9Action().conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_11))
+                    .andWith(new JakartaEE10Action());
+}
---
+++ dev/com.ibm.ws.jbatch.open.security_fat/test-applications/batchFAT.war/src/batch/fat/artifacts/SleepyExitStatusWithStepNameBatchlet.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package batch.fat.artifacts;
+
+import javax.batch.api.AbstractBatchlet;
+import javax.batch.api.BatchProperty;
+import javax.batch.runtime.context.StepContext;
+import javax.inject.Inject;
+
+public class SleepyExitStatusWithStepNameBatchlet extends AbstractBatchlet {
+
+    @Inject
+    StepContext ctx;
+    @Inject
+    @BatchProperty
+    String forceFailure;
+
+    @Inject
+    @BatchProperty(name = "sleep.time.seconds")
+    String sleepTimeSeconds;
+
+    @Override
+    public String process() throws Exception {
+
+        int sleepTime = Integer.parseInt(sleepTimeSeconds);
+        if (sleepTime > 0) {
+            Thread.sleep(sleepTime * 1000);
+        }
+
+        if (Boolean.parseBoolean(forceFailure)) {
+            throw new IllegalStateException("Forcing failure in batchlet.");
+        } else {
+            return ctx.getStepName();
+        }
+    }
+}
