d64fe9916f3324ff1e25d298d61a0f6e128b9e48
Paul Nicolucci <pnicolucci@gmail.com>
2022-12-29 08:33:52 -0500

    Update the new CDIConfigByACP jars

--- dev/com.ibm.ws.jsf.2.3_fat/test-applications/CDIConfigByACPJSF23.jar/src/com/ibm/ws/jsf23/fat/cdi/jar/appconfigpop/jsf23/ACPJarInitializer.java
+++ dev/com.ibm.ws.jsf.2.3_fat/test-applications/CDIConfigByACPJSF23.jar/src/com/ibm/ws/jsf23/fat/cdi/jar/appconfigpop/jsf23/ACPJarInitializer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 IBM Corporation and others.
+ * Copyright (c) 2018, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -36,7 +36,7 @@ public class ACPJarInitializer extends ApplicationConfigurationPopulator {
         a.appendChild(sel);
 
         Element nh = document.createElementNS(ns, "navigation-handler");
-        nh.setTextContent("com.ibm.ws.jsf23.fat.cdi.common.managed.CustomNavigationHandler");
+        nh.setTextContent("com.ibm.ws.jsf23.fat.cdi.common.managed.jsf23.CustomNavigationHandler");
         a.appendChild(nh);
 
         Element rh = document.createElementNS(ns, "resource-handler");
