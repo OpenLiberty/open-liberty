1c866571e69d6c86f973e6aa27103e998e894878
Paul Nicolucci <pnicolucci@gmail.com>
2023-01-05 07:03:32 -0500

    Move JSF22TestResources.war beans to two new packages

--- dev/com.ibm.ws.jsf.2.2_fat/test-applications/JSF22TestResources.war/src/com/ibm/ws/jsf22/fat/resources/beans/faces40/ResourceUserAgentBean.java
+++ dev/com.ibm.ws.jsf.2.2_fat/test-applications/JSF22TestResources.war/src/com/ibm/ws/jsf22/fat/resources/beans/faces40/ResourceUserAgentBean.java
@@ -10,7 +10,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  */
-package com.ibm.ws.jsf22.fat.resources.beans;
+package com.ibm.ws.jsf22.fat.resources.beans.faces40;
 
 import javax.faces.application.Resource;
 import javax.faces.application.ResourceHandler;
