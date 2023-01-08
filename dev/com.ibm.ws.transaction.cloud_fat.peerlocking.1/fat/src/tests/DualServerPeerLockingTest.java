01054e42c3ec257e1e57cb145a17decb12a45c3d
Jon Hawkes <jon.hawkes@uk.ibm.com>
2023-01-08 10:31:46 +0000

    f

--- dev/com.ibm.ws.transaction.cloud_fat.peerlocking.1/fat/src/tests/DualServerPeerLockingTest.java
+++ dev/com.ibm.ws.transaction.cloud_fat.peerlocking.1/fat/src/tests/DualServerPeerLockingTest.java
@@ -1,9 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2019, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * http://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
@@ -21,6 +23,7 @@ import com.ibm.tx.jta.ut.util.XAResourceImpl;
 import com.ibm.websphere.simplicity.log.Log;
 import com.ibm.ws.transaction.fat.util.FATUtils;
 import com.ibm.ws.transaction.fat.util.TxShrinkHelper;
+import com.ibm.ws.transaction.test.tests.DualServerDynamicTestBase;
 
 import componenttest.annotation.Server;
 import componenttest.annotation.TestServlet;
--- dev/com.ibm.ws.transaction.cloud_fat.peerlocking.1/fat/src/tests/DualServerPeerLockingTest1.java
+++ dev/com.ibm.ws.transaction.cloud_fat.peerlocking.1/fat/src/tests/DualServerPeerLockingTest1.java
@@ -1,18 +1,27 @@
 /*******************************************************************************
  * Copyright (c) 2019, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * http://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package tests;
 
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import com.ibm.websphere.simplicity.ProgramOutput;
+import com.ibm.websphere.simplicity.log.Log;
+import com.ibm.ws.transaction.fat.util.FATUtils;
+
+import componenttest.annotation.AllowedFFDC;
 import componenttest.custom.junit.runner.FATRunner;
 
 @RunWith(FATRunner.class)
