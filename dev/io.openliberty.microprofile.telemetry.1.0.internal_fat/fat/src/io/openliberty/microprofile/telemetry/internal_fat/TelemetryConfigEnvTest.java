3735b8d50f3fee56403b59805dd95e8ca5509f46
yasmin-aumeeruddy <52958137+yasmin-aumeeruddy@users.noreply.github.com>
2023-01-10 14:33:52 +0000

    Test Telemetry config

--- dev/io.openliberty.microprofile.telemetry.1.0.internal_fat/fat/src/io/openliberty/microprofile/telemetry/internal_fat/TelemetryConfigEnvTest.java
+++ dev/io.openliberty.microprofile.telemetry.1.0.internal_fat/fat/src/io/openliberty/microprofile/telemetry/internal_fat/TelemetryConfigEnvTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022 IBM Corporation and others.
+ * Copyright (c) 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -15,7 +15,6 @@ package io.openliberty.microprofile.telemetry.internal_fat;
 import static com.ibm.websphere.simplicity.ShrinkHelper.DeployOptions.SERVER_ONLY;
 
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -29,32 +28,41 @@ import componenttest.annotation.TestServlets;
 import componenttest.custom.junit.runner.FATRunner;
 import componenttest.topology.impl.LibertyServer;
 import componenttest.topology.utils.FATServletClient;
-import io.openliberty.microprofile.telemetry.internal_fat.apps.bean.TelemetryBeanTestServlet;
+import io.openliberty.microprofile.telemetry.internal_fat.apps.telemetry.BaggageServlet;
+import io.openliberty.microprofile.telemetry.internal_fat.apps.telemetry.ConfigServlet;
+import io.openliberty.microprofile.telemetry.internal_fat.apps.telemetry.MetricsDisabledServlet;
+import io.openliberty.microprofile.telemetry.internal_fat.apps.telemetry.OpenTelemetryBeanServlet;
+import io.openliberty.microprofile.telemetry.internal_fat.apps.telemetry.PatchTestApp;
+import io.openliberty.microprofile.telemetry.internal_fat.apps.telemetry.SpanCurrentServlet;
+import io.openliberty.microprofile.telemetry.internal_fat.apps.telemetry.Telemetry10Servlet;
+import io.openliberty.microprofile.telemetry.internal_fat.apps.telemetry.WithSpanServlet;
 
 @RunWith(FATRunner.class)
-public class TelemetryBeanTest extends FATServletClient {
+public class TelemetryConfigEnvTest extends FATServletClient {
 
-    public static final String SERVER_NAME = "Telemetry10Bean";
-    public static final String APP_NAME = "TelemetryBeanApp";
+    public static final String SERVER_NAME = "Telemetry10ConfigEnv";
+    public static final String APP_NAME = "TelemetryApp";
 
     @Server(SERVER_NAME)
     @TestServlets({
-                    @TestServlet(servlet = TelemetryBeanTestServlet.class, contextRoot = APP_NAME)
+                    @TestServlet(servlet = ConfigServlet.class, contextRoot = APP_NAME),
     })
     public static LibertyServer server;
 
     @BeforeClass
     public static void setUp() throws Exception {
         WebArchive app = ShrinkWrap.create(WebArchive.class, APP_NAME + ".war")
-                        .addClasses(TelemetryBeanTestServlet.class)
-                        .addAsResource(new StringAsset("otel.sdk.disabled=false"),
-                                       "META-INF/microprofile-config.properties");
+                        .addAsResource(ConfigServlet.class.getResource("microprofile-config.properties"), "META-INF/microprofile-config.properties")
+                        .addClasses(ConfigServlet.class);
+
         ShrinkHelper.exportAppToServer(server, app, SERVER_ONLY);
+        server.addEnvVar("OTEL_SERVICE_NAME", "overrideDone");
+        server.addEnvVar("OTEL_SDK_DISABLED", "false");
         server.startServer();
     }
 
     @AfterClass
     public static void tearDown() throws Exception {
-        server.stopServer();
+        server.stopServer("CWNEN0047W.*ConfigServlet");
     }
 }
