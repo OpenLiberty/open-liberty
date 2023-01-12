c93554e5aeb1c0394cd49b75b85508a29fe57f9c
Berksan Ates <ates.berksan@gmail.com>
2023-01-12 15:49:12 -0600

    Another attempt for fix increasing timeout

--- dev/com.ibm.ws.jaxws.2.2.webcontainer_fat/fat/src/com/ibm/ws/jaxws/fat/LibertyCXFPositivePropertiesTest.java
+++ dev/com.ibm.ws.jaxws.2.2.webcontainer_fat/fat/src/com/ibm/ws/jaxws/fat/LibertyCXFPositivePropertiesTest.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -45,7 +45,7 @@ public class LibertyCXFPositivePropertiesTest {
     public static LibertyServer server;
 
     private final static Class<?> c = LibertyCXFPositivePropertiesTest.class;
-    private static final int CONN_TIMEOUT = 500;
+    private static final int CONN_TIMEOUT = 800;
 
     @BeforeClass
     public static void setUp() throws Exception {
