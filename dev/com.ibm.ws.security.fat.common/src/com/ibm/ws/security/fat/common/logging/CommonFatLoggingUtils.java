c5c3be709cb32d7513cbaad0ff0b90aded844667
Christina Crane <chriscr@us.ibm.com>
2023-01-18 18:04:16 -0600

    resolve unittest issue

--- dev/com.ibm.ws.security.fat.common/src/com/ibm/ws/security/fat/common/logging/CommonFatLoggingUtils.java
+++ dev/com.ibm.ws.security.fat.common/src/com/ibm/ws/security/fat/common/logging/CommonFatLoggingUtils.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -140,7 +140,6 @@ public class CommonFatLoggingUtils {
     }
 
     void printRequestHeaders(WebRequest request, String testName) {
-        Log.info(thisClass, testName, "Request header: HttpMethod, set to: " + request.getHttpMethod());
 
         Map<String, String> requestHeaders = request.getAdditionalHeaders();
         if (requestHeaders != null) {
