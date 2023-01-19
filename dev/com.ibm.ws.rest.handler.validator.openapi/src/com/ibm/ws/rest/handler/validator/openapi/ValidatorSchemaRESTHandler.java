c89e980e42613e2ba7267de2ee016ca9e0a68262
Nathan Mittlestat <nmittles@us.ibm.com>
2023-01-19 11:17:08 -0600

    Revert "Issue 21120: gate new function on beta edition" Reverting to prepare Admin Center test connection function fo GA. This revert removes the beta gates in place for the new rest handler validor/config endpoints.
    This reverts commit 6f94b84f08f0dc5762a9d0cb17ab7709e186da4a.

--- dev/com.ibm.ws.rest.handler.validator.openapi/src/com/ibm/ws/rest/handler/validator/openapi/ValidatorSchemaRESTHandler.java
+++ dev/com.ibm.ws.rest.handler.validator.openapi/src/com/ibm/ws/rest/handler/validator/openapi/ValidatorSchemaRESTHandler.java
@@ -34,7 +34,6 @@ import org.osgi.service.component.annotations.Deactivate;
 import com.ibm.websphere.ras.Tr;
 import com.ibm.websphere.ras.TraceComponent;
 import com.ibm.ws.ffdc.annotation.FFDCIgnore;
-import com.ibm.ws.kernel.productinfo.ProductInfo;
 import com.ibm.ws.microprofile.openapi.Constants;
 import com.ibm.ws.microprofile.openapi.impl.core.util.Json;
 import com.ibm.ws.microprofile.openapi.impl.core.util.Yaml;
@@ -73,14 +72,6 @@ public class ValidatorSchemaRESTHandler implements RESTHandler {
             }
             response.setResponseHeader("Accept", "GET");
             response.sendError(405); // Method Not Allowed
-            return;
-        }
-
-        // Delete once feature 18696 is GA.
-        // Remove com.ibm.ws.kernel.boot from bnd buildpath once the Beta check is no longer needed.
-        if (!ProductInfo.getBetaEdition() && request.getContextPath().contains("/ibm/api")) {
-            response.setResponseHeader("Accept", "GET");
-            response.sendError(404); // Not Found
             return;
         }
 
@@ -136,16 +127,16 @@ public class ValidatorSchemaRESTHandler implements RESTHandler {
         try {
             cloudantEnabled = getServiceReferences(context.getBundleContext(), Validator.class,
                                                    "(component.name=com.ibm.ws.rest.handler.validator.cloudant.CloudantDatabaseValidator)")
-                            .iterator()
-                            .hasNext();
+                                                                   .iterator()
+                                                                   .hasNext();
             jcaEnabled = getServiceReferences(context.getBundleContext(), Validator.class,
                                               "(component.name=com.ibm.ws.rest.handler.validator.jca.ConnectionFactoryValidator)")
-                            .iterator()
-                            .hasNext();
+                                                              .iterator()
+                                                              .hasNext();
             jdbcEnabled = getServiceReferences(context.getBundleContext(), Validator.class,
                                                "(component.name=com.ibm.ws.rest.handler.validator.jdbc.DataSourceValidator)")
-                            .iterator()
-                            .hasNext();
+                                                               .iterator()
+                                                               .hasNext();
         } catch (InvalidSyntaxException e) {
             //Should never happen.
             e.printStackTrace();
