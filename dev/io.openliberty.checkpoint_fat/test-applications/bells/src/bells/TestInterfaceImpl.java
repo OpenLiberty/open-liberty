4ea2ff59207e150e34bfdb0e4238122314e09707
Anjum Fatima <anjum.fatima@ibm.com>
2023-01-20 13:26:35 -0600

    Add tests for updated bell properties with checkpoint/restore.

---
+++ dev/io.openliberty.checkpoint_fat/test-applications/bells/src/bells/TestInterfaceImpl.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2023 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package bells;
+
+import java.util.Map;
+
+import test.checkpoint.bells.bundle.TestInterface;
+
+public class TestInterfaceImpl implements TestInterface {
+
+    Map<String, String> updatedBellProps = null;
+
+    public TestInterfaceImpl() {
+    }
+
+    public void updateBell(Map<String, String> ubProps) {
+        updatedBellProps = ubProps;
+        System.out.println("Updated bell properties: " + updatedBellProps);
+    }
+}
