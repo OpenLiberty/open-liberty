d2623ba64de962b11d4ba953d092a142b96b2a23
Teddy J. Torres <ttorres@us.ibm.com>
2023-01-23 10:05:25 -0500

    Implement OidcHttpAuthenticationMechanism's cleanSubject for issue #21177

--- dev/io.openliberty.security.oidcclientcore.internal/src/io/openliberty/security/oidcclientcore/logout/LogoutHandler.java
+++ dev/io.openliberty.security.oidcclientcore.internal/src/io/openliberty/security/oidcclientcore/logout/LogoutHandler.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2022 IBM Corporation and others.
+ * Copyright (c) 2022, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -50,13 +50,18 @@ public class LogoutHandler {
         this.idTokenString = idTokenString;
     }
 
-    @FFDCIgnore(OidcClientConfigurationException.class)
     public ProviderAuthenticationResult logout() throws ServletException {
 
         LocalLogoutStrategy localLogout = new LocalLogoutStrategy(req);
         localLogout.logout();
 
+        return logoutWithoutLocalLogout();
+    }
+
+    @FFDCIgnore(OidcClientConfigurationException.class)
+    public ProviderAuthenticationResult logoutWithoutLocalLogout() throws ServletException {
         String endSessionEndPoint = null;
+
         try {
             endSessionEndPoint = MetadataUtils.getEndSessionEndpoint(oidcClientConfig);
         } catch (OidcDiscoveryException | OidcClientConfigurationException e) {
@@ -80,7 +85,6 @@ public class LogoutHandler {
             JakartaOidcAuthorizationRequest oidcAuthorizationRequest = new JakartaOidcAuthorizationRequest(req, resp, oidcClientConfig);
             return oidcAuthorizationRequest.sendRequest();
         }
-
     }
 
 }
