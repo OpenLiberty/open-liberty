6525c0e777cb5012d43a2ba9d9c2de399ecfc1a2
Jimmy Wu <jimmywu0605@gmail.com>
2023-01-25 15:07:24 -0500

    add fat tests for OpenIdAuthenticationMechanismDefinition display

--- dev/io.openliberty.security.jakartasec.3.0.internal_fat.config/fat/src/io/openliberty/security/jakartasec/fat/config/FATSuite.java
+++ dev/io.openliberty.security.jakartasec.3.0.internal_fat.config/fat/src/io/openliberty/security/jakartasec/fat/config/FATSuite.java
@@ -18,6 +18,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import componenttest.custom.junit.runner.AlwaysPassesTest;
 import io.openliberty.security.jakartasec.fat.config.tests.ConfigurationClaimsDefinitionTests;
+import io.openliberty.security.jakartasec.fat.config.tests.ConfigurationDisplayTests;
 import io.openliberty.security.jakartasec.fat.config.tests.ConfigurationELValuesOverrideTests;
 import io.openliberty.security.jakartasec.fat.config.tests.ConfigurationELValuesOverrideWithoutHttpSessionTests;
 import io.openliberty.security.jakartasec.fat.config.tests.ConfigurationExtraParametersTests;
@@ -37,6 +38,7 @@ import io.openliberty.security.jakartasec.fat.config.tests.ConfigurationUserInfo
                 ConfigurationResponseModeTests.class,
                 ConfigurationUseNonceTests.class,
                 ConfigurationExtraParametersTests.class,
+                ConfigurationDisplayTests.class,
                 // LogoutDefinition tests are handled in a separate FAT project as the test use sleeps to wait for tokens to expire and that causes the tests to take quite some time to run
                 ConfigurationELValuesOverrideTests.class,
                 ConfigurationELValuesOverrideWithoutHttpSessionTests.class,
