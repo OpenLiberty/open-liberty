ac7485d5cd8f8a74d9070da41e284577ebd28d80
Lori Adington <loriad@us.ibm.com>
2023-01-26 16:55:54 -0500

    24077: DoNotAllowDuplicateSetCookie property not working

--- dev/com.ibm.ws.transport.http.remoteip_fat/test-applications/EndpointInformation/src/com/ibm/ws/transport/http/servlets/EndpointInformationServlet.java
+++ dev/com.ibm.ws.transport.http.remoteip_fat/test-applications/EndpointInformation/src/com/ibm/ws/transport/http/servlets/EndpointInformationServlet.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2018 IBM Corporation and others.
+ * Copyright (c) 2018, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -37,6 +37,9 @@ public class EndpointInformationServlet extends HttpServlet {
 
         res.addHeader("My-Response-Header", "resHeader1");
         res.addHeader("My-Response-Header", "resHeader2");
+
+        res.addHeader("Set-Cookie", "JSESSIONID=gorp1");
+        res.addHeader("Set-Cookie", "JSESSIONID=gorp2");
 
         PrintWriter pw = res.getWriter();
 
