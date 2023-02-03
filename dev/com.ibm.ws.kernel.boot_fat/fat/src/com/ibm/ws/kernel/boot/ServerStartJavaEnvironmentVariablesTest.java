957b13a01e78b7df422bc2825bbe41a09786852b
Mark Swatosh <mark.swatosh@gmail.com>
2023-02-03 15:19:38 -0600

    update copyright dates

--- dev/com.ibm.ws.kernel.boot_fat/fat/src/com/ibm/ws/kernel/boot/ServerStartJavaEnvironmentVariablesTest.java
+++ dev/com.ibm.ws.kernel.boot_fat/fat/src/com/ibm/ws/kernel/boot/ServerStartJavaEnvironmentVariablesTest.java
@@ -1,7 +1,7 @@
 package com.ibm.ws.kernel.boot;
 
 /*******************************************************************************
- * Copyright (c) 2019, 2020 IBM Corporation and others.
+ * Copyright (c) 2019, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
