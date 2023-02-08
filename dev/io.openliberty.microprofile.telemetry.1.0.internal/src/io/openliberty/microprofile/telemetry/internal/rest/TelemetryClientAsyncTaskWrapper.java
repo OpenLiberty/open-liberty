f3b9873e3deed6ed9d213f261d6e5dcf658cd42f
Andrew Rouse <anrouse@uk.ibm.com>
2023-02-08 14:06:17 +0000

    Telemetry: update jaxrs client task wrapper
    Implement the new method on the ClientAsyncTaskWrapper to wrap a
    Supplier.

--- dev/io.openliberty.microprofile.telemetry.1.0.internal/src/io/openliberty/microprofile/telemetry/internal/rest/TelemetryClientAsyncTaskWrapper.java
+++ dev/io.openliberty.microprofile.telemetry.1.0.internal/src/io/openliberty/microprofile/telemetry/internal/rest/TelemetryClientAsyncTaskWrapper.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2022 IBM Corporation and others.
+ * Copyright (c) 2022, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -13,6 +13,7 @@
 package io.openliberty.microprofile.telemetry.internal.rest;
 
 import java.util.concurrent.Callable;
+import java.util.function.Supplier;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
@@ -36,6 +37,11 @@ public class TelemetryClientAsyncTaskWrapper implements ClientAsyncTaskWrapper {
     @Override
     public <T> Callable<T> wrap(Callable<T> c) {
         return Context.current().wrap(c);
+    }
+
+    @Override
+    public <T> Supplier<T> wrap(Supplier<T> s) {
+        return Context.current().wrapSupplier(s);
     }
 
 }
