7ef3c31a8b059efd1783164dc5001ac923b01e81
Paul Nicolucci <pnicolucci@gmail.com>
2023-02-12 15:00:49 -0500

    Enable JSF22FlowsTests for EE10

--- dev/com.ibm.ws.jsfContainer_fat_2.3/fat/src/com/ibm/ws/jsf/container/fat/tests/JSF22FlowsTests.java
+++ dev/com.ibm.ws.jsfContainer_fat_2.3/fat/src/com/ibm/ws/jsf/container/fat/tests/JSF22FlowsTests.java
@@ -4,11 +4,8 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
- * SPDX-License-Identifier: EPL-2.0
  *
- * Contributors:
- *     IBM Corporation - initial API and implementation
+ * SPDX-License-Identifier: EPL-2.0
  *******************************************************************************/
 package com.ibm.ws.jsf.container.fat.tests;
 
@@ -31,7 +28,6 @@ import com.ibm.websphere.simplicity.ShrinkHelper;
 import com.ibm.ws.jsf.container.fat.FATSuite;
 
 import componenttest.annotation.ExpectedFFDC;
-import componenttest.annotation.SkipForRepeat;
 import componenttest.custom.junit.runner.FATRunner;
 import componenttest.rules.repeater.JakartaEE10Action;
 import componenttest.topology.impl.LibertyServer;
@@ -199,7 +195,6 @@ public class JSF22FlowsTests extends FATServletClient {
         JSF22Flows_TestDeclarativeNavigation(MOJARRA_APP);
     }
 
-    @SkipForRepeat(SkipForRepeat.EE10_FEATURES) // Fails due to "jakarta.el.PropertyNotFoundException: Target Unreachable, identifier [flowScope] resolved to null"
     @Test
     public void JSF22Flows_TestDeclarativeNavigation_MyFaces() throws Exception {
         JSF22Flows_TestDeclarativeNavigation(MYFACES_APP);
@@ -333,7 +328,6 @@ public class JSF22FlowsTests extends FATServletClient {
     }
 
     @Test
-    @SkipForRepeat(SkipForRepeat.EE10_FEATURES) // Fixed in MYFACES-4542, targeted in next release after 4.0.0-RC3 
     public void JSF22Flows_TestDeclarativeNestedFlows_MyFaces() throws Exception {
         // Navigate to the index
         testNestedFlows("declarativeNested1", "declarativeNested2", "declarativeNested", MYFACES_APP);
