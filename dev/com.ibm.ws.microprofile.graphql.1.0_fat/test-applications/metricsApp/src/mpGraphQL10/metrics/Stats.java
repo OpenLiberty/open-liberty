50ab3641282f29912ec7db6d91d044c22262eb72
Jared Anderson <jhanders@us.ibm.com>
2023-02-13 13:53:35 -0600

    GraphQL updates for MP 6 support
    - Update metrics integration to be specific for different metrics
    levels.  mpMetrics 5.0 has breaking changes that need to be handled.
    - Update Metrics test to skip the JSON testing since mpMetrics doesn't
    include JSON any longer.
    - Update security mpGraphQL auto feature to include EE 10 security
    feature.
    - Add repeat for MP 6 in the GraphQL FAT bucket.

--- dev/com.ibm.ws.microprofile.graphql.1.0_fat/test-applications/metricsApp/src/mpGraphQL10/metrics/Stats.java
+++ dev/com.ibm.ws.microprofile.graphql.1.0_fat/test-applications/metricsApp/src/mpGraphQL10/metrics/Stats.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 IBM Corporation and others.
+ * Copyright (c) 2019, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -34,9 +34,9 @@ public class Stats {
         }
     }
 
-    @JsonbProperty("mp_graphql_Query_getCountWidget")
+    @JsonbProperty("mp_graphql_QUERY_getCountWidget")
     private SimpleTimerStat count;
-    @JsonbProperty("mp_graphql_Query_getTimeWidget")
+    @JsonbProperty("mp_graphql_QUERY_getTimeWidget")
     private SimpleTimerStat time;
 
     public SimpleTimerStat getCount() {
