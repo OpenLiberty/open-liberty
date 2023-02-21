01c5869fc911ed1f687fdb23c78879f76fcd2449
Kyle Aure <KyleJAure@gmail.com>
2023-02-21 17:24:05 -0600

    Update jakarta data TCK to new snapshot

--- dev/io.openliberty.jakarta.data.1.0_fat_tck/fat/src/io/openliberty/jakarta/data/tck/FATSuite.java
+++ dev/io.openliberty.jakarta.data.1.0_fat_tck/fat/src/io/openliberty/jakarta/data/tck/FATSuite.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -20,9 +20,8 @@ import componenttest.custom.junit.runner.AlwaysPassesTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-                AlwaysPassesTest.class, //Need to have a passing test for java 8
+                AlwaysPassesTest.class, //Need to have a passing test for java 8 & 11
                 DataCoreTckLauncher.class,
-                DataStandaloneTckLauncher.class,
                 DataWebTckLauncher.class //full mode
 })
 public class FATSuite {
