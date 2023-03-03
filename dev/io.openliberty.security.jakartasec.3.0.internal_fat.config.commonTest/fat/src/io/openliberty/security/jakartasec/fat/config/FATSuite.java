cab59c5b8dc395bfd9f3ad3ca8a35e872d656e48
Christina Crane <chriscr@us.ibm.com>
2023-03-03 14:56:21 -0600

    Split jakartasec config FAT (it is timing out in LITE mode on slow machines)

---
+++ dev/io.openliberty.security.jakartasec.3.0.internal_fat.config.commonTest/fat/src/io/openliberty/security/jakartasec/fat/config/FATSuite.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2022, 2023 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.security.jakartasec.fat.config;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import componenttest.custom.junit.runner.AlwaysPassesTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+                AlwaysPassesTest.class
+// tests are run from io.openliberty.security.jakartasec.3.0.internal_fat.config.1
+//                and io.openliberty.security.jakartasec.3.0.internal_fat.config.2
+
+})
+public class FATSuite {
+
+}
