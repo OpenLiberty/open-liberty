01478b2273eae0a3cfa49f0329f436286b165b1c
alex-butcher <21243172+abutch3r@users.noreply.github.com>
2023-03-09 15:44:47 +0000

    Update All TestNG installs to custom 7.5.0 build
    Updated Arquillian related variables to improve consistency and findability across individual projects when updates are to be made.
    
    Update arquillian versions to latest known tested versions that should be compatible with TestNG 7.5.0
    
    7.5.0 of TestNG now includes SLF4J, so update ArquillianLoadableExtension to include SLF4J.

--- dev/fattest.simplicity/src/com/ibm/ws/fat/util/tck/Slf4JArchiveAppender.java
+++ dev/fattest.simplicity/src/com/ibm/ws/fat/util/tck/Slf4JArchiveAppender.java
@@ -1,15 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2022 IBM Corporation and others.
+ * Copyright (c) 2022, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
  * 
  * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
+  *******************************************************************************/
 package com.ibm.ws.fat.util.tck;
 
 import org.jboss.arquillian.container.test.spi.client.deployment.CachedAuxilliaryArchiveAppender;
@@ -22,12 +19,16 @@ import org.jboss.shrinkwrap.api.spec.JavaArchive;
  */
 public class Slf4JArchiveAppender extends CachedAuxilliaryArchiveAppender {
 
+    private static final String SLF4J_SERVICE_PROVIDER = "META-INF/services/org.slf4j.spi.SLF4JServiceProvider";
+
     @Override
     protected Archive<?> buildArchive() {
-        return ShrinkWrap.create(JavaArchive.class, "slf4j.jar")
-                        .addPackages(true, "org.slf4j")
-                        .addAsResource("META-INF/services/org.slf4j.spi.SLF4JServiceProvider",
-                                       "META-INF/services/org.slf4j.spi.SLF4JServiceProvider");
+        JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "slf4j.jar");
+        archive.addPackages(true, "org.slf4j");
+        if (Thread.currentThread().getContextClassLoader().getResource(SLF4J_SERVICE_PROVIDER) != null) {
+            archive.addAsResource(SLF4J_SERVICE_PROVIDER, SLF4J_SERVICE_PROVIDER);
+        }
+        return archive;
     }
 
 }
