69a3e0ca282ba75ed215bf847a4a722feb5e4177
Jared Anderson <jhanders@us.ibm.com>
2023-03-10 15:10:02 -0600

    Update to EPL 2.0 for files that got checked in after EPL 2.0 change

--- dev/com.ibm.ws.jpa.tests.spec10.embeddable_fat.common/test-applications/olgh10310/src/com/ibm/ws/jpa/olgh10310/model/SimpleNestedEmbeddableOLGH10310Id.java
+++ dev/com.ibm.ws.jpa.tests.spec10.embeddable_fat.common/test-applications/olgh10310/src/com/ibm/ws/jpa/olgh10310/model/SimpleNestedEmbeddableOLGH10310Id.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
  * Copyright (c) 2023 IBM Corporation. All rights reserved.
- * This program and the accompanying materials are made available under the 
- * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
- * which accompanies this distribution. 
- * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-2.0/
+ * and the Eclipse Distribution License is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
  * Contributors:
@@ -19,13 +19,14 @@ import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
-public class SimpleNestedEmbeddableOLGH10310Id implements Serializable{
+public class SimpleNestedEmbeddableOLGH10310Id implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Column(name = "NESTED_VALUE")
     private Integer nestedValue;
 
-    public SimpleNestedEmbeddableOLGH10310Id() { }
+    public SimpleNestedEmbeddableOLGH10310Id() {
+    }
 
     public SimpleNestedEmbeddableOLGH10310Id(Integer nestedValue) {
         this.nestedValue = nestedValue;
