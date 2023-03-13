411225071a3f260e3527900ef602699039fe6607
Greg Watts <gwatts@us.ibm.com>
2023-03-13 13:41:19 -0500

    Actually call Java 20 FAT test

--- dev/io.openliberty.java.internal_fat/fat/src/io/openliberty/java/internal/fat/FATSuite.java
+++ dev/io.openliberty.java.internal_fat/fat/src/io/openliberty/java/internal/fat/FATSuite.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2022 IBM Corporation and others.
+ * Copyright (c) 2017, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -26,6 +26,7 @@ import componenttest.custom.junit.runner.AlwaysPassesTest;
                 Java18CDITest.class,
                 Java18TestJava2SecurityDisabled.class,
                 Java19Test.class,
+                Java20Test.class,
                 AlwaysPassesTest.class
 })
 public class FATSuite {
