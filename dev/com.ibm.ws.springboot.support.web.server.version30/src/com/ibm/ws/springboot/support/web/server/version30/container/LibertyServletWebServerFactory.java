4cee56e9394f4944e8b2eef89106daed9daebd46
Brent Daniel <brenthdaniel@gmail.com>
2023-03-15 08:52:00 -0700

    Add spring boot 3.0 as a noship feature

---
+++ dev/com.ibm.ws.springboot.support.web.server.version30/src/com/ibm/ws/springboot/support/web/server/version30/container/LibertyServletWebServerFactory.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2023 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.springboot.support.web.server.version30.container;
+
+import org.springframework.beans.BeansException;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.AbstractServletWebServerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ *
+ */
+@ConfigurationProperties(prefix = "server.liberty", ignoreUnknownFields = true)
+public class LibertyServletWebServerFactory extends AbstractServletWebServerFactory implements ApplicationContextAware, LibertyFactoryBase {
+    private boolean useDefaultHost = true;
+    private ApplicationContext context;
+
+    @Override
+    public WebServer getWebServer(ServletContextInitializer... initializers) {
+        return new LibertyWebServer(this, this, mergeInitializers(initializers));
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        this.context = context;
+    }
+
+    String getApplicationID() {
+        return context.getId();
+    }
+
+    public void setUseDefaultHost(boolean useDefaultHost) {
+        this.useDefaultHost = useDefaultHost;
+    }
+
+    @Override
+    public boolean shouldUseDefaultHost(LibertyWebServer container) {
+        // only use default host if configured to and
+        // we can acquire the default host
+        return useDefaultHost && acquireDefaultHost(container);
+    }
+}
