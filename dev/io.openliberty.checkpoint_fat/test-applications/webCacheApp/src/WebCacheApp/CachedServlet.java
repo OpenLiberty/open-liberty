c996f29c85cf07359c4f577a573f55551a6f04a8
Keenan McCarthy <John.KEENAN.MCCARTHY@ibm.com>
2023-03-23 23:44:58 -0400

    Added testcase for WebCache with checkpoint and restore
    Fixed classpath entry and added SkipIfCheckpointNotSupported annotation. Updated WebCacheTest to cache nanotime in servlet response. WebCacheTest now compares nanotime in cached response and passes in a parameter to ensure that a different time is returned.

---
+++ dev/io.openliberty.checkpoint_fat/test-applications/webCacheApp/src/WebCacheApp/CachedServlet.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package WebCacheApp;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/cachedServlet", loadOnStartup = 1)
+public class CachedServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().append("Hello from cachedServlet: " + System.nanoTime());
+    }
+
+}
