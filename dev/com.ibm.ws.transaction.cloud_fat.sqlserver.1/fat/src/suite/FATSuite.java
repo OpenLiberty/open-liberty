4b14b6656f5c32c42d0552d6b252742a314e5401
Jon Hawkes <jon.hawkes@uk.ibm.com>
2023-03-27 15:06:06 +0100

    Repeat on JDK8

--- dev/com.ibm.ws.transaction.cloud_fat.sqlserver.1/fat/src/suite/FATSuite.java
+++ dev/com.ibm.ws.transaction.cloud_fat.sqlserver.1/fat/src/suite/FATSuite.java
@@ -37,6 +37,6 @@ public class FATSuite extends TxTestContainerSuite {
 	@ClassRule
 	public static RepeatTests r = RepeatTests.withoutModification()
 	.andWith(FeatureReplacementAction.EE8_FEATURES().fullFATOnly().forServers(DualServerDynamicDBRotationTest1.serverNames))
-	.andWith(FeatureReplacementAction.EE9_FEATURES().fullFATOnly().forServers(DualServerDynamicDBRotationTest1.serverNames))
+	.andWith(FeatureReplacementAction.EE9_FEATURES().conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_11).forServers(DualServerDynamicDBRotationTest1.serverNames))
 	.andWith(FeatureReplacementAction.EE10_FEATURES().fullFATOnly().forServers(DualServerDynamicDBRotationTest1.serverNames));
 }
