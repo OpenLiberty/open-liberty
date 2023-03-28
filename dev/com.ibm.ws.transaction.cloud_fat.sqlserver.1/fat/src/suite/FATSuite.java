2ed5c1f4d065d993e5513a960dabbbfaa738bfab
Jon Hawkes <jon.hawkes@uk.ibm.com>
2023-03-28 09:17:11 +0100

    Run tests on iSeries

--- dev/com.ibm.ws.transaction.cloud_fat.sqlserver.1/fat/src/suite/FATSuite.java
+++ dev/com.ibm.ws.transaction.cloud_fat.sqlserver.1/fat/src/suite/FATSuite.java
@@ -35,8 +35,8 @@ public class FATSuite extends TxTestContainerSuite {
 	}
 
 	@ClassRule
-	public static RepeatTests r = RepeatTests.withoutModification()
+	public static RepeatTests r = RepeatTests.with(FeatureReplacementAction.NO_REPLACEMENT().fullFATOnly())
 	.andWith(FeatureReplacementAction.EE8_FEATURES().fullFATOnly().forServers(DualServerDynamicDBRotationTest1.serverNames))
 	.andWith(FeatureReplacementAction.EE9_FEATURES().conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_11).forServers(DualServerDynamicDBRotationTest1.serverNames))
-	.andWith(FeatureReplacementAction.EE10_FEATURES().fullFATOnly().forServers(DualServerDynamicDBRotationTest1.serverNames));
+	.andWith(FeatureReplacementAction.EE10_FEATURES().forServers(DualServerDynamicDBRotationTest1.serverNames));
 }
