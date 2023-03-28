9a15703180e61042fde2ef5bce082ad87a78a61d
Christina Crane <chriscr@us.ibm.com>
2023-03-28 11:47:02 -0500

    Update repeat rules for Social

--- dev/com.ibm.ws.security.social_fat.LibertyOP.backchannelLogout/fat/src/com/ibm/ws/security/backchannelLogout/fat/FATSuite.java
+++ dev/com.ibm.ws.security.social_fat.LibertyOP.backchannelLogout/fat/src/com/ibm/ws/security/backchannelLogout/fat/FATSuite.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2022 IBM Corporation and others.
+ * Copyright (c) 2022, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -18,12 +18,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import com.ibm.ws.security.fat.common.actions.SecurityTestFeatureEE10RepeatAction;
-import com.ibm.ws.security.fat.common.actions.SecurityTestFeatureEE9RepeatAction;
-import com.ibm.ws.security.fat.common.actions.SecurityTestRepeatAction;
+import com.ibm.ws.security.fat.common.actions.LargeProjectRepeatActions;
 
 import componenttest.custom.junit.runner.AlwaysPassesTest;
-import componenttest.rules.repeater.EmptyAction;
 import componenttest.rules.repeater.RepeatTests;
 
 @RunWith(Suite.class)
@@ -40,14 +37,9 @@ import componenttest.rules.repeater.RepeatTests;
 public class FATSuite {
 
     /*
-     * Run EE9 and EE10 tests in only FULL mode and run EE7/EE8 tests only in LITE mode.
-     *
-     * This was done to increase coverage of EE9 and EE10 while not adding a large amount of test runtime.
+     * Run EE10 tests in LITE mode and run all tests in FULL mode.
      */
     @ClassRule
-    public static RepeatTests repeat = RepeatTests.with(new EmptyAction().liteFATOnly())
-            .andWith(new SecurityTestRepeatAction().onlyOnWindows().fullFATOnly())
-            .andWith(new SecurityTestFeatureEE9RepeatAction().notOnWindows().alwaysAddFeature("servlet-5.0").fullFATOnly())
-            .andWith(new SecurityTestFeatureEE10RepeatAction().notOnWindows().alwaysAddFeature("servlet-6.0").fullFATOnly());
+    public static RepeatTests repeat = LargeProjectRepeatActions.createEE9OrEE10Repeats("servlet-5.0", "servlet-6.0");
 
 }
