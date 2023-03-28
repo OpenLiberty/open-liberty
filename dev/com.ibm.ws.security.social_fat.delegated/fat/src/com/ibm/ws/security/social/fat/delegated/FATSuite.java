d584a9d173877e9b3b173fdcc2537c5ade13cce9
Christina Crane <chriscr@us.ibm.com>
2023-03-28 11:47:02 -0500

    Update repeat rules for Social

--- dev/com.ibm.ws.security.social_fat.delegated/fat/src/com/ibm/ws/security/social/fat/delegated/FATSuite.java
+++ dev/com.ibm.ws.security.social_fat.delegated/fat/src/com/ibm/ws/security/social/fat/delegated/FATSuite.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2022 IBM Corporation and others.
+ * Copyright (c) 2017, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -13,11 +13,15 @@
 
 package com.ibm.ws.security.social.fat.delegated;
 
+import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.ibm.ws.security.fat.common.actions.LargeProjectRepeatActions;
+
 import componenttest.custom.junit.runner.AlwaysPassesTest;
+import componenttest.rules.repeater.RepeatTests;
 
 @RunWith(Suite.class)
 @SuiteClasses({
@@ -31,5 +35,11 @@ import componenttest.custom.junit.runner.AlwaysPassesTest;
  * Purpose: This suite collects and runs all known good test suites.
  */
 public class FATSuite {
+
+    /*
+     * Run EE10 tests in LITE mode and run all tests in FULL mode.
+     */
+    @ClassRule
+    public static RepeatTests repeat = LargeProjectRepeatActions.createEE9OrEE10Repeats("servlet-5.0", "servlet-6.0");
 
 }
