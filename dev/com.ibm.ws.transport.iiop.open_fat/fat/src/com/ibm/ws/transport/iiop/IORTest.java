2f63678ff292dc8b1a5f727e81cdc833a04cc9ba
habib <habiblawal@hotmail.co.uk>
2023-03-31 17:12:19 +0100

    Updated copyright years

--- dev/com.ibm.ws.transport.iiop.open_fat/fat/src/com/ibm/ws/transport/iiop/IORTest.java
+++ dev/com.ibm.ws.transport.iiop.open_fat/fat/src/com/ibm/ws/transport/iiop/IORTest.java
@@ -1,17 +1,15 @@
-/*
- * =============================================================================
- * Copyright (c) 2020 IBM Corporation and others.
+/*******************************************************************************
+ * Copyright (c) 2020-2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- *
+ * 
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- * =============================================================================
- */
+ *******************************************************************************/
 package com.ibm.ws.transport.iiop;
 
 import com.ibm.websphere.simplicity.ShrinkHelper;
@@ -44,5 +42,4 @@ public class IORTest extends FATServletClient {
     public static void afterClass() throws Exception {
         server.stopServer();
     }
-
 }
--- dev/com.ibm.ws.transport.iiop.open_fat/fat/src/test/iiop/common/package-info.java
+++ dev/com.ibm.ws.transport.iiop.open_fat/fat/src/test/iiop/common/package-info.java
@@ -1,10 +1,15 @@
-/*
- * WLP Copyright IBM Corp. 2023
+/*******************************************************************************
+ * Copyright (c) 2023 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
  *
- * The source code for this program is not published or otherwise divested
- * of its trade secrets, irrespective of what has been deposited with the
- * U.S. Copyright Office.
- */
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 
 @Version("1.0.0")
 package test.iiop.common;
