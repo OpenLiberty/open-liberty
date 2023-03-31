667bd332d27db8ffc768afcec06fefc987b3a8ef
habib <habiblawal@hotmail.co.uk>
2023-03-31 17:12:19 +0100

    Updated copyright years

--- dev/com.ibm.ws.transport.iiop.open_fat/fat/src/test/iiop/client/ClientCallbackServiceImpl.java
+++ dev/com.ibm.ws.transport.iiop.open_fat/fat/src/test/iiop/client/ClientCallbackServiceImpl.java
@@ -1,14 +1,15 @@
-/*
- * IBM Confidential
+/*******************************************************************************
+ * Copyright (c) 2015-2023 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
  *
- * OCO Source Materials
- *
- * WLP Copyright IBM Corp. 2015
- *
- * The source code for this program is not published or otherwise divested
- * of its trade secrets, irrespective of what has been deposited with the
- * U.S. Copyright Office.
- */
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package test.iiop.client;
 
 import java.rmi.RemoteException;
