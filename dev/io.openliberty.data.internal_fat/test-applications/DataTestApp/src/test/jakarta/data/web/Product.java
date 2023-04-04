eba7e9a5a79201c993270bab293e807bbffe69df
Nathan Rauh <nathan.rauh@us.ibm.com>
2023-04-04 14:02:32 -0500

    Issue #24858 UUID attribute as Id on unannotated entity

--- dev/io.openliberty.data.internal_fat/test-applications/DataTestApp/src/test/jakarta/data/web/Product.java
+++ dev/io.openliberty.data.internal_fat/test-applications/DataTestApp/src/test/jakarta/data/web/Product.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2022 IBM Corporation and others.
+ * Copyright (c) 2022,2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -12,15 +12,17 @@
  *******************************************************************************/
 package test.jakarta.data.web;
 
+import java.util.UUID;
+
 /**
- *
+ * Simple unannotated entity with a UUID type indicating the Id.
  */
 public class Product {
     public String description;
 
-    public String id;
-
     public String name;
+
+    public UUID pk; // Do not add Id to this name. It should be detectable based on type alone.
 
     public float price;
 
