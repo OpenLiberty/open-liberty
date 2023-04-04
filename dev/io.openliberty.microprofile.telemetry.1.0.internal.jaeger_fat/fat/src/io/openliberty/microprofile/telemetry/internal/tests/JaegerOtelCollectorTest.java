dd3ca9890b6243ce630c07556364eb684f844921
yasmin-aumeeruddy <yasmin.aumeeruddy@ibm.com>
2023-04-04 16:52:44 +0100

    Add OpenTelemetry Collector tests

---
+++ dev/io.openliberty.microprofile.telemetry.1.0.internal.jaeger_fat/fat/src/io/openliberty/microprofile/telemetry/internal/tests/JaegerOtelCollectorTest.java
@@ -0,0 +1,98 @@
+/*******************************************************************************
+ * Copyright (c) 2023 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.microprofile.telemetry.internal.tests;
+
+import static com.ibm.websphere.simplicity.ShrinkHelper.DeployOptions.SERVER_ONLY;
+
+import java.io.File;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.testcontainers.containers.Network;
+
+import com.ibm.websphere.simplicity.ShrinkHelper;
+
+import componenttest.containers.SimpleLogConsumer;
+import componenttest.custom.junit.runner.FATRunner;
+import componenttest.custom.junit.runner.Mode;
+import componenttest.custom.junit.runner.Mode.TestMode;
+import io.openliberty.microprofile.telemetry.internal.apps.spanTest.TestResource;
+import io.openliberty.microprofile.telemetry.internal.utils.TestConstants;
+import io.openliberty.microprofile.telemetry.internal.utils.jaeger.JaegerContainer;
+import io.openliberty.microprofile.telemetry.internal.utils.jaeger.JaegerQueryClient;
+import io.openliberty.microprofile.telemetry.internal.utils.otelCollector.OtelCollectorContainer;
+
+/**
+ * Test exporting traces to a Jaeger server with OpenTelemetry Collector
+ */
+@RunWith(FATRunner.class)
+@Mode(TestMode.FULL)
+public class JaegerOtelCollectorTest extends JaegerBaseTest {
+
+    public static final int OTLP_GRPC_PORT = 4317;
+
+    @ClassRule
+    public static Network network = Network.newNetwork();
+
+    @ClassRule
+    public static JaegerContainer jaegerContainer = new JaegerContainer()
+                                                                         .withLogConsumer(new SimpleLogConsumer(JaegerBaseTest.class, "jaeger"))
+                                                                         .withNetwork(network)
+                                                                         .withNetworkAliases("jaeger-all-in-one");
+    @ClassRule
+    public static OtelCollectorContainer otelCollectorContainer = new OtelCollectorContainer(new File("lib/LibertyFATTestFiles/otel-collector-config-jaeger.yaml"))
+                                                                                                                                                                   .withNetwork(network)
+                                                                                                                                                                   .withLogConsumer(new SimpleLogConsumer(JaegerBaseTest.class,
+                                                                                                                                                                                                          "otelCol"))
+                                                                                                                                                                   .withNetworkAliases("otel-collector-jaeger");
+
+    public static JaegerQueryClient client;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+
+        client = new JaegerQueryClient(jaegerContainer);
+
+        server.addEnvVar(TestConstants.ENV_OTEL_TRACES_EXPORTER, "otlp");
+        server.addEnvVar(TestConstants.ENV_OTEL_EXPORTER_OTLP_ENDPOINT, otelCollectorContainer.getOtlpGrpcUrl());
+        server.addEnvVar(TestConstants.ENV_OTEL_SERVICE_NAME, "Test service");
+        server.addEnvVar(TestConstants.ENV_OTEL_BSP_SCHEDULE_DELAY, "100"); // Wait no more than 100ms to send traces to the server
+        server.addEnvVar(TestConstants.ENV_OTEL_SDK_DISABLED, "false"); //Enable tracing
+
+        // Construct the test application
+        WebArchive jaegerTest = ShrinkWrap.create(WebArchive.class, "spanTest.war")
+                                          .addClass(TestResource.class);
+        ShrinkHelper.exportAppToServer(server, jaegerTest, SERVER_ONLY);
+        server.startServer();
+    }
+
+    @AfterClass
+    public static void teardown() throws Exception {
+        server.stopServer();
+    }
+
+    @AfterClass
+    public static void closeClient() throws Exception {
+        client.close();
+    }
+
+    @Override
+    protected JaegerQueryClient getJaegerClient() {
+        return client;
+    }
+
+}
---
+++ dev/io.openliberty.microprofile.telemetry.1.0.internal.jaeger_fat/fat/src/io/openliberty/microprofile/telemetry/internal/utils/otelCollector/OtelCollectorContainer.java
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * Copyright (c) 2023 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.microprofile.telemetry.internal.utils.otelCollector;
+
+import java.io.File;
+
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.ImageNameSubstitutor;
+
+/**
+ * A container for the otelCollector trace server
+ * <p>
+ * Usage:
+ *
+ * <pre>
+ * {@code @ClassName}
+ * public static otelCollectorContainer otelCollectorContainer = new OtelCollectorContainer("exporter").withLogConsumer(new SimpleLogConsumer(MyTest.class, "otelCol"));
+ * </pre>
+ *
+ */
+
+public class OtelCollectorContainer extends GenericContainer<OtelCollectorContainer> {
+
+    public static final DockerImageName IMAGE_NAME = DockerImageName.parse("otel/opentelemetry-collector:0.74.0");
+
+    public static final int OTLP_GRPC_PORT = 4317;
+
+    public OtelCollectorContainer(File configFile) {
+        this(IMAGE_NAME, configFile);
+    }
+
+    public OtelCollectorContainer(DockerImageName imageName, File configFile) {
+        super(new ImageFromDockerfile().withDockerfileFromBuilder(builder -> builder.from(
+                                                                        ImageNameSubstitutor.instance()
+                                                                        .apply(DockerImageName.parse("otel/opentelemetry-collector:0.74.0"))
+                                                                        .asCanonicalNameString())
+                                                                        .copy("/etc/otel-collector-config.yaml", "/etc/otel-collector-config.yaml")
+                                                                        .build())
+                                                                        .withFileFromFile("/etc/otel-collector-config.yaml", configFile, 0644));
+        withExposedPorts(OTLP_GRPC_PORT);
+        withCommand("--config=/etc/otel-collector-config.yaml");
+    }
+
+    /**
+     * Get the port to use to send OTLP spans via gRPC
+     * <p>
+     * Only valid when the container is started
+     *
+     * @return the OTLP gRPC port
+     */
+    public int getOtlpGrpcPort() {
+        return getMappedPort(OTLP_GRPC_PORT);
+    }
+
+    /**
+     * Get the URL to use to send OTLP spans via gRPC
+     * <p>
+     * Only valid when the container is started
+     *
+     * @return the OTLP gRPC URL
+     */
+    public String getOtlpGrpcUrl() {
+        return "http://" + getHost() + ":" + getOtlpGrpcPort();
+    }
+}
