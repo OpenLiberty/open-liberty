39d37c9e5f76ea2a0548c659e86e636e7c70084c
Christina Crane <chriscr@us.ibm.com>
2023-04-04 14:58:31 -0500

    Additional changes to support OAuth tests with EE9/EE10

--- dev/com.ibm.ws.security.oidc.server_fat/fat/src/com/ibm/ws/security/openidconnect/server/fat/OAuth/OAuthGrantTypesTest.java
+++ dev/com.ibm.ws.security.oidc.server_fat/fat/src/com/ibm/ws/security/openidconnect/server/fat/OAuth/OAuthGrantTypesTest.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2021 IBM Corporation and others.
+ * Copyright (c) 2021, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -18,6 +18,7 @@ import java.util.List;
 
 import org.junit.Assume;
 import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
 
 import com.ibm.websphere.simplicity.log.Log;
 import com.ibm.ws.security.oauth_oidc.fat.commonTest.Constants;
@@ -25,6 +26,7 @@ import com.ibm.ws.security.oauth_oidc.fat.commonTest.TestServer;
 import com.ibm.ws.security.oauth_oidc.fat.commonTest.TestSettings;
 import com.ibm.ws.security.openidconnect.server.fat.CommonTests.GenericGrantTypesTests;
 
+import componenttest.custom.junit.runner.FATRunner;
 import componenttest.custom.junit.runner.Mode;
 import componenttest.custom.junit.runner.Mode.TestMode;
 import componenttest.topology.impl.LibertyServerWrapper;
@@ -32,19 +34,20 @@ import componenttest.topology.utils.LDAPUtils;
 
 @LibertyServerWrapper
 @Mode(TestMode.FULL)
+@RunWith(FATRunner.class)
 public class OAuthGrantTypesTest extends GenericGrantTypesTests {
 
     private static final Class<?> thisClass = OAuthGrantTypesTest.class;
 
     @BeforeClass
     public static void setupBeforeTest() throws Exception {
-    	/*
-    	 * These tests have not been configured to run with the local LDAP server.
-    	 */
-    	Assume.assumeTrue(!LDAPUtils.USE_LOCAL_LDAP_SERVER);
+        /*
+         * These tests have not been configured to run with the local LDAP server.
+         */
+        Assume.assumeTrue(!LDAPUtils.USE_LOCAL_LDAP_SERVER);
         msgUtils.printClassName(thisClass.toString());
         Log.info(thisClass, "setupBeforeTest", "Prep for test");
-        // add any additional messages that you want the "start" to wait for 
+        // add any additional messages that you want the "start" to wait for
         // we should wait for any providers that this test requires
         List<String> extraMsgs = new ArrayList<String>();
         //extraMsgs.add("CWWKS1402I.*" + Constants.OAUTHCONFIGMEDIATOR_APP);
@@ -65,6 +68,7 @@ public class OAuthGrantTypesTest extends GenericGrantTypesTests {
         goodActions = Constants.BASIC_PROTECTED_RESOURCE_ACTIONS;
         flowType = Constants.WEB_CLIENT_FLOW;
 
+        testOPServer.addIgnoredServerExceptions("CWWKG0032W");
     }
 
 }
