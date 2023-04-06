041b04297339d351cca8e91ad73d843acb607435
andrew-banks <Andrew_Banks@uk.ibm.com>
2023-04-06 13:39:55 +0100

    split jmsContextSecurty Fat tests

--- dev/com.ibm.ws.messaging.open_jms20contextSecurity2_fat/fat/src/com/ibm/ws/messaging/JMS20contextSecurity2/fat/FATSuite.java
+++ dev/com.ibm.ws.messaging.open_jms20contextSecurity2_fat/fat/src/com/ibm/ws/messaging/JMS20contextSecurity2/fat/FATSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020, 2023 IBM Corporation and others.
+ * Copyright (c) 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -10,14 +10,17 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-package com.ibm.ws.messaging.JMS20contextSecurity.fat;
+package com.ibm.ws.messaging.JMS20contextSecurity2.fat;
 
 import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import com.ibm.ws.messaging.JMS20contextSecurity.fat.JMSContextTest.JMSContextTest;
+import com.ibm.ws.messaging.JMS20.fat.ContextInject.JMSContextInjectTest;
+import com.ibm.ws.messaging.JMS20.fat.TemporaryQueue.JMSContextTest_118066;
+import com.ibm.ws.messaging.JMS20.fat.TemporaryQueue.JMSContextTest_118068;
+import com.ibm.ws.messaging.JMS20.fat.Transaction.JMSContextTest_118065;
 
 import componenttest.rules.repeater.EE7FeatureReplacementAction;
 import componenttest.rules.repeater.JakartaEE10Action;
@@ -27,8 +30,10 @@ import componenttest.rules.repeater.RepeatTests;
 @RunWith(Suite.class)
 @SuiteClasses({
                 DummyTest.class,
-                JMSContextTest.class,
-              
+                JMSContextTest_118066.class,
+                JMSContextTest_118068.class,
+                JMSContextTest_118065.class,
+                JMSContextInjectTest.class
 })
 public class FATSuite {
     @ClassRule
