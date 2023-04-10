55956828fafbad40220626cb0f226ae9ed860b4c
James Stephens <hunting@us.ibm.com>
2023-04-10 13:17:06 -0500

    add maximum connections to pmi

--- dev/com.ibm.ws.jca.cm/src/com/ibm/websphere/jca/pmi/JCAPMIHelper.java
+++ dev/com.ibm.ws.jca.cm/src/com/ibm/websphere/jca/pmi/JCAPMIHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 IBM Corporation and others.
+ * Copyright (c) 2013, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -25,4 +25,6 @@ public interface JCAPMIHelper {
     public String getJNDIName();
 
     public boolean getParkedValue();
+
+    public int getMaximumConnectionValue();
 }
