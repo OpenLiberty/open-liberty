af704d2e85103f6abe30a61405b21cf6517d1cec
Timothy Bishop <tabishop@us.ibm.com>
2023-04-12 16:28:31 -0500

    fix copyright

--- dev/com.ibm.ws.jca.cm/src/com/ibm/ejs/j2c/MCWrapper.java
+++ dev/com.ibm.ws.jca.cm/src/com/ibm/ejs/j2c/MCWrapper.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 1997, 20213 IBM Corporation and others.
+ * Copyright (c) 1997, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -3321,6 +3321,7 @@ public final class MCWrapper implements com.ibm.ws.j2c.MCWrapper, JCAPMIHelper {
             Tr.exit(tc, "abortMC", aborted);
         return aborted;
     }
+
     public boolean isMCAborted() {
         if (aborted)
             return true;
@@ -3330,7 +3331,7 @@ public final class MCWrapper implements com.ibm.ws.j2c.MCWrapper, JCAPMIHelper {
         }
         return false;
     }
-    
+
     /**
      * @return
      */
