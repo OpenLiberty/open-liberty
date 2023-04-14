9ff1d2af8c8215b417ce4f3d4b36ef317b71c66b
Jon Hawkes <jon.hawkes@uk.ibm.com>
2023-04-14 20:12:21 +0100

    Repeat cloud tests properly

--- dev/com.ibm.ws.transaction.cloud_fat.db2.1/fat/src/suite/FATSuite.java
+++ dev/com.ibm.ws.transaction.cloud_fat.db2.1/fat/src/suite/FATSuite.java
@@ -38,8 +38,8 @@ public class FATSuite extends TxTestContainerSuite {
 	}
 
 	@ClassRule
-	public static RepeatTests r = RepeatTests.withoutModification()
-	.andWith(FeatureReplacementAction.EE8_FEATURES().forServers(DualServerDynamicDBRotationTest1.serverNames))
-	.andWith(FeatureReplacementAction.EE9_FEATURES().forServers(DualServerDynamicDBRotationTest1.serverNames))
-	.andWith(FeatureReplacementAction.EE10_FEATURES().forServers(DualServerDynamicDBRotationTest1.serverNames));
+    public static RepeatTests r = RepeatTests.withoutModificationInFullMode()
+    .andWith(FeatureReplacementAction.EE8_FEATURES().fullFATOnly().forServers(DualServerDynamicDBRotationTest1.serverNames))
+    .andWith(FeatureReplacementAction.EE9_FEATURES().conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_11).forServers(DualServerDynamicDBRotationTest1.serverNames))
+    .andWith(FeatureReplacementAction.EE10_FEATURES().forServers(DualServerDynamicDBRotationTest1.serverNames));
 }
--- dev/com.ibm.ws.transaction.cloud_fat.postgresql.0/fat/src/suite/FATSuite.java
+++ dev/com.ibm.ws.transaction.cloud_fat.postgresql.0/fat/src/suite/FATSuite.java
@@ -29,9 +29,9 @@ import tests.DBRotationTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-	//Ensure failures in @BeforeClass do not result in zero tests run
-	AlwaysPassesTest.class,
-	DBRotationTest.class,
+                //Ensure failures in @BeforeClass do not result in zero tests run
+                AlwaysPassesTest.class,
+                DBRotationTest.class,
 })
 public class FATSuite extends TxTestContainerSuite {
     private static final String POSTGRES_DB = "testdb";
@@ -55,8 +55,10 @@ public class FATSuite extends TxTestContainerSuite {
     }
 
     @ClassRule
-    public static RepeatTests r = RepeatTests.withoutModification()
+    public static RepeatTests r = RepeatTests.withoutModificationInFullMode()
                     .andWith(FeatureReplacementAction.EE8_FEATURES().fullFATOnly().forServers(DBRotationTest.serverNames))
-                    .andWith(FeatureReplacementAction.EE9_FEATURES().fullFATOnly().forServers(DBRotationTest.serverNames))
-                    .andWith(FeatureReplacementAction.EE10_FEATURES().fullFATOnly().forServers(DBRotationTest.serverNames));
+                    .andWith(FeatureReplacementAction.EE9_FEATURES()
+                                    .conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_11)
+                                    .forServers(DBRotationTest.serverNames))
+                    .andWith(FeatureReplacementAction.EE10_FEATURES().forServers(DBRotationTest.serverNames));
 }
