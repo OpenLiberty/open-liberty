c76a6b8e9b9830200fe55ba2f22820af6f0ff957
Joe Grassel <jgrassel@us.ibm.com>
2023-04-18 13:10:17 -0500

    Add Java 21 support to com.ibm.ws.jpa.tests.javasupport_fat
    Signed-off-by: Joe Grassel <jgrassel@us.ibm.com>

--- dev/com.ibm.ws.jpa.tests.javasupport_fat/fat/src/io/openliberty/jpa/test/javasupport/TestJPA22_JavaSourceLevel.java
+++ dev/com.ibm.ws.jpa.tests.javasupport_fat/fat/src/io/openliberty/jpa/test/javasupport/TestJPA22_JavaSourceLevel.java
@@ -125,4 +125,10 @@ public class TestJPA22_JavaSourceLevel extends AbstractTestJavaSourceLevel {
     public void testJPA22WithJava20() throws Exception {
         runTest(server_jss_jpa22, 20);
     }
+
+    @Test
+    @MinimumJavaLevel(javaLevel = 21)
+    public void testJPA22WithJava21() throws Exception {
+        runTest(server_jss_jpa22, 21);
+    }
 }
