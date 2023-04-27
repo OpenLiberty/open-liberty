d4bd35ddddf62c1cb5fff8078541d5a7edbe840c
Samuel E Bratton <sebratton@users.noreply.github.com>
2023-04-27 02:14:39 -0500

    Disable entry exit debug on methods
    The methods in question are invoked via callback from the jvm running in a special single threaded mode. The entry traces are undesirable there since they can lead to a deadlock.

--- dev/io.openliberty.checkpoint/src/io/openliberty/checkpoint/internal/CheckpointImpl.java
+++ dev/io.openliberty.checkpoint/src/io/openliberty/checkpoint/internal/CheckpointImpl.java
@@ -493,6 +493,7 @@ public class CheckpointImpl implements RuntimeUpdateListener, ServerReadyStatus 
     }
 
     @FFDCIgnore(Exception.class)
+    @Trivial
     private void callHooks(String operation, List<CheckpointHook> checkpointHooks,
                            Consumer<CheckpointHook> perform,
                            Function<Exception, CheckpointFailedException> failed) throws CheckpointFailedException {
@@ -507,6 +508,7 @@ public class CheckpointImpl implements RuntimeUpdateListener, ServerReadyStatus 
         }
     }
 
+    @Trivial
     private void prepare(List<CheckpointHook> checkpointHooks) throws CheckpointFailedException {
         callHooks("prepare",
                   checkpointHooks,
