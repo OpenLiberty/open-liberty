f528908c712ae7221844f4e0873eda6ab836566e
Adam Yoho <ayoho@us.ibm.com>
2023-05-15 16:09:12 -0500

    Issue 25137: Log error and description returned in OIDC redirect
    Adds a new NLS message that's logged when an `error` parameter is included in the redirect URI back from the OpenID Connect provider.
    
    Resolves #25137

--- dev/com.ibm.ws.security.openidconnect.client/src/com/ibm/ws/security/openidconnect/client/web/OidcRedirectServlet.java
+++ dev/com.ibm.ws.security.openidconnect.client/src/com/ibm/ws/security/openidconnect/client/web/OidcRedirectServlet.java
@@ -1,14 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2022 IBM Corporation and others.
+ * Copyright (c) 2013, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
- * SPDX-License-Identifier: EPL-2.0
  *
- * Contributors:
- * IBM Corporation - initial API and implementation
+ * SPDX-License-Identifier: EPL-2.0
  *******************************************************************************/
 package com.ibm.ws.security.openidconnect.client.web;
 
@@ -207,6 +204,7 @@ public class OidcRedirectServlet extends HttpServlet {
             return;
         }
 
+        Tr.error(tc, "REDIRECT_URI_CONTAINED_ERROR", error, request.getParameter(Constants.ERROR_DESCRIPTION));
         StringBuilder query = new StringBuilder();
         if (error != null && OAuth20Exception.INVALID_SCOPE.equals(error)) {
             query.append(Constants.ERROR + "=" + OAuth20Exception.INVALID_SCOPE);
