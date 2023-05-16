1e2fb42abc6374441c4c3c85c55e0e66261feb0f
Thomas Watson <tjwatson@us.ibm.com>
2023-05-16 10:29:26 -0500

    Remove other references from tests for checkpoint-1.0 feature

--- dev/io.openliberty.jakartaee10.internal_fat/fat/src/io/openliberty/jakartaee10/internal/tests/JakartaEE10Test.java
+++ dev/io.openliberty.jakartaee10.internal_fat/fat/src/io/openliberty/jakartaee10/internal/tests/JakartaEE10Test.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -59,7 +59,6 @@ public class JakartaEE10Test extends FATServletClient {
         compatFeatures.remove("acmeCA-2.0");
 
         // remove noship features
-        compatFeatures.remove("checkpoint-1.0");
         compatFeatures.remove("jcacheContainer-1.1");
         compatFeatures.remove("netty-1.0");
         compatFeatures.remove("noShip-1.0");
