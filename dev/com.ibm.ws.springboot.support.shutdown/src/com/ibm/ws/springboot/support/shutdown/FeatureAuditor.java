ebc3385601fb056784f9cc0061bad8dbc5a22458
Thomas Bitonti <bitonti@us.ibm.com>
2023-05-22 16:34:46 -0400

    Initial FeatureAuditor update. *** NOT YET TESTED ***

--- dev/com.ibm.ws.springboot.support.shutdown/src/com/ibm/ws/springboot/support/shutdown/FeatureAuditor.java
+++ dev/com.ibm.ws.springboot.support.shutdown/src/com/ibm/ws/springboot/support/shutdown/FeatureAuditor.java
@@ -21,125 +21,203 @@ import com.ibm.ws.app.manager.springboot.container.ApplicationError;
 import com.ibm.ws.app.manager.springboot.container.ApplicationTr;
 import com.ibm.ws.app.manager.springboot.container.ApplicationTr.Type;
 
+/**
+ * Liberty environment verifier. Verify that the liberty environment
+ * is provisioned to run the current spring version.
+ */
+//@formatter:off
 public class FeatureAuditor implements EnvironmentPostProcessor {
 
+    /**
+     * Generate the resource name for a specified class.
+     *
+     * Convert '.' into '/' and append '.class'.
+     *
+     * Note: This will not work for inner classes, which convert one or
+     * more '.' into '$' instead of '/'.
+     *
+     * @param className A fully qualified non-inner class name.
+     *
+     * @return The name of the resource of the class.
+     */
+    protected static String asResourceName(String className) {
+        return className.replace('.', '/') + ".class";
+    }
+
+    /**
+     * Tell if a class is available in the current classloading environment.
+     *
+     * That is, tell if the resource of the class is available using this class's
+     * classloader.
+     *
+     * Note: This does not work for inner classes.  See {@link #asResourceName(String)}.
+     *
+     * @param className The name of the class which is to be located.
+     *
+     * @return True or false telling if the class resource was located.
+     */
+    protected static boolean isClassAvailable(String className) {
+        ClassLoader classLoader = FeatureAuditor.class.getClassLoader();
+        String resourceName = asResourceName(className);
+        boolean foundClass = ( classLoader.getResource(resourceName) != null );
+
+        System.out.println("Found [ " + foundClass + " ] class [ " + className + " ] as [ " + resourceName + " ]");
+        System.out.println("Using [ " + classLoader + " ]");
+
+        return foundClass;
+    }
+
+    protected static void warning(Type msgId, Object...parms) {
+        ApplicationTr.warning(msgId, parms);
+    }
+
+    public static final String SPRING15_FACTORY = "org.springframework.boot.context.embedded.EmbeddedServletContainerFactory";
+    public static final String SPRING20_FACTORY = "org.springframework.boot.web.servlet.server.ServletWebServerFactory";
+    // The Spring 3.0 factory is the same as the Spring 2.0 factory.
+
+    public static final String LIBERTY15_CONFIG = "com.ibm.ws.springboot.support.web.server.version15.container.LibertyConfiguration";
+    public static final String LIBERTY20_CONFIG = "com.ibm.ws.springboot.support.web.server.version20.container.LibertyConfiguration";
+    public static final String LIBERTY30_CONFIG = "com.ibm.ws.springboot.support.web.server.version30.container.LibertyConfiguration";
+
+    /**
+     * Verify that the liberty server is provisioned correctly for the current spring version.
+     *
+     * <ul><li>Verify that the java version is sufficient to run the spring version.</li>
+     *     <li>Verify that the necessary liberty spring feature is provisioned.</li>
+     *     <li>Verify that the necessary web featres are provisioned.</li>
+     * </ul>
+     *
+     * Java checks are done by cross-checking the java version with the spring version.
+     *
+     * Provisioning checks are done by verifying that specific classes are available in the
+     * server environment.
+     *
+     * Provisioning failures result in a thrown {@link ApplicationExcepption}.
+     *
+     * Java version failures result in a warning.
+     *
+     * @param env The configuration environment.  Currently ignored.
+     * @param app The spring application.  Currently ignored.
+     */
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment env, SpringApplication app) {
+        String springBootVersion = SpringBootVersion.getVersion();
+        System.out.println("spring.boot.version = " + springBootVersion);
+        boolean springIsAtLeast20 = ( springBootVersion.compareTo("2.0.0") >= 0 );
+        boolean springIsAtLeast30 = ( springBootVersion.compareTo("3.0.0") >= 0 );
 
-        String sbVersion = SpringBootVersion.getVersion();
-        checkJavaVersion(sbVersion);
-
-        /*
-         * Throw an Application error if the wrong version of spring boot feature is
-         * enabled
-         */
-        try {
-            Class.forName("org.springframework.boot.context.embedded.EmbeddedServletContainerFactory");
-            Class.forName(
-                          "com.ibm.ws.springboot.support.web.server.version20.container.LibertyConfiguration");
-            checkSpringBootVersion15();
-
-        } catch (ClassNotFoundException e) {
-
-        }
-
-        try {
-            Class.forName("org.springframework.boot.web.servlet.server.ServletWebServerFactory");
-            Class.forName(
-                          "com.ibm.ws.springboot.support.web.server.version15.container.LibertyConfiguration");
-            checkSpringBootVersion20();
-
-        } catch (ClassNotFoundException e) {
-
-        }
-
-        try {
-            Class.forName("org.springframework.boot.web.servlet.server.ServletWebServerFactory");
-            Class.forName("io.openliberty.springboot.support.web.server.version30.container.LibertyConfiguration");
-            checkSpringBootVersion30();
-
-        } catch (ClassNotFoundException e) {
-
-        }
-
-        /* Throw an application error if servlet feature is not enabled */
-        try {
-            Class.forName("org.springframework.web.WebApplicationInitializer");
-            checkServletPresent(sbVersion);
-        } catch (ClassNotFoundException e) {
-
-        }
-
-        /* Throw an application error if websocket feature is not enabled */
-        try {
-            Class.forName("org.springframework.web.socket.WebSocketHandler");
-            checkWebSocketPresent();
-        } catch (ClassNotFoundException e) {
-
-        }
-    }
-
-    private void checkJavaVersion(String sbVersion) {
         String javaVersion = System.getProperty("java.version");
+        System.out.println("java.version = " + javaVersion);
 
-        // java version isnt supported by sb version, upgrade to 2.x or higher
-        if (!javaVersion.startsWith("1.")) {
-            try {
-                Class.forName("org.springframework.boot.context.embedded.EmbeddedServletContainerFactory");
-                ApplicationTr.warning(Type.WARNING_UNSUPPORTED_JAVA_VERSION, javaVersion, sbVersion);
-            } catch (ClassNotFoundException e) {
+        String javaSpecVersion = System.getProperty("java.vm.specification.version");
+        System.out.println("java.vm.specification.version = " + javaSpecVersion);
 
+        int javaSpecVersionNo = Integer.parseInt(javaSpecVersion);
+
+        int requiredJavaVersion;
+        String requiredVersionText;
+        boolean isSatisfied;
+
+        if ( !springIsAtLeast20 ) {
+            requiredJavaVersion = 7;
+            requiredVersionText = "Java 7";
+        } else if ( !springIsAtLeast30 ) {
+            requiredJavaVersion = 8;
+            requiredVersionText = "Java 8";
+        } else {
+            requiredJavaVersion = 17;
+            requiredVersionText = "Java 17";
+        }
+
+        if ( javaSpecVersionNo < requiredJavaVersion ) {
+            warning(Type.WARNING_UNSUPPORTED_JAVA_VERSION, javaVersion, springBootVersion, requiredVersionText);
+        } else {
+            System.out.println("Validated required java version [ " + requiredVersionText + " ]" +
+                               " for Spring Boot version [ " + springBootVersion + " ]");
+        }
+
+        if ( springIsAtLeast30 ) {
+            if ( isClassAvailable(SPRING20_FACTORY) &&
+                 (isClassAvailable(LIBERTY15_CONFIG) || isClassAvailable(LIBERTY20_CONFIG)) ) {
+                if ( !isClassAvailable(LIBERTY30_CONFIG) ) {
+                    System.out.println("Failed to liberty spring configuration class [ " + LIBERTY30_CONFIG + " ]");
+                    throw new ApplicationError(Type.ERROR_NEED_SPRING_BOOT_VERSION_30);
+                }
+            }
+        } else if ( springIsAtLeast20 ) {
+            if ( isClassAvailable(SPRING20_FACTORY) &&
+                 (isClassAvailable(LIBERTY15_CONFIG) || isClassAvailable(LIBERTY30_CONFIG)) ) {
+                if ( !isClassAvailable(LIBERTY20_CONFIG) ) {
+                    System.out.println("Failed to liberty spring configuration class [ " + LIBERTY20_CONFIG + " ]");
+                    throw new ApplicationError(Type.ERROR_NEED_SPRING_BOOT_VERSION_20);
+                }
+            }
+        } else {
+            if ( isClassAvailable(SPRING15_FACTORY) &&
+                 (isClassAvailable(LIBERTY20_CONFIG) || isClassAvailable(LIBERTY30_CONFIG)) ) {
+                if ( !isClassAvailable(LIBERTY15_CONFIG) ) {
+                    System.out.println("Failed to liberty spring configuration class [ " + LIBERTY15_CONFIG + " ]");
+                    throw new ApplicationError(Type.ERROR_NEED_SPRING_BOOT_VERSION_15);
+                }
             }
         }
-    }
 
-    private void checkWebSocketPresent() {
-        try {
-            Class.forName("javax.websocket.WebSocketContainer");
-        } catch (ClassNotFoundException e) {
-            throw new ApplicationError(Type.ERROR_MISSING_WEBSOCKET_FEATURE);
-        }
-    }
+        // Servlet and WebSocket might not be used by the application.
+        // This is detected by the inclusion of a spring class.
+        //
+        // Note: This is an approximate test, depending on whether the
+        // spring packaging is minimal for the function in use.  That is
+        // to say, the spring class might be included in the packaging but
+        // is unused.
+        //
+        // Note: This component is not transformed for jakarta.  That means that
+        // support for both javax and jakarta versions of the required API classes
+        // is provided, and means that the class availability test must not be coded
+        // with reference the target class.
 
-    private void checkServletPresent(String springBootVersion) {
-        try {
-            if (springBootVersion.startsWith("1.") || springBootVersion.startsWith("2.")) {
-                Class.forName("javax.servlet.Servlet"); // SB 1 & 2
-            } else if (springBootVersion.startsWith("3.")) {
-                Class.forName("jakarta.servlet.Servlet"); // SB 3
+        // if ( isClassAvailable("org.springframework.web.WebApplicationInitializer") ) {
+        //     if ( !isClassAvailable("javax.servlet.Servlet") ) {
+        //         throw new ApplicationError(Type.ERROR_MISSING_SERVLET_FEATURE);
+        //     }
+        // }
+
+        if ( isClassAvailable("org.springframework.web.WebApplicationInitializer") ) {
+            String servletClassName =
+                ( springIsAtLeast30 ? "jakarta.servlet.Servlet" : "javax.servlet.Servlet");
+            if ( !isClassAvailable(servletClassName) ) {
+                System.out.println("Failed to locate servlet class [ " + servletClassName + " ]");
+                throw new ApplicationError(Type.ERROR_MISSING_SERVLET_FEATURE);
+            } else {
+                System.out.println("The Spring Servlet function was located;" +
+                                   " the base Servlet API is provisioned.");
             }
-        } catch (ClassNotFoundException e) {
-            throw new ApplicationError(Type.ERROR_MISSING_SERVLET_FEATURE);
-        }
-    }
-
-    private void checkSpringBootVersion15() {
-        try {
-            Class.forName(
-                          "com.ibm.ws.springboot.support.web.server.version15.container.LibertyConfiguration");
-        } catch (ClassNotFoundException e) {
-            throw new ApplicationError(Type.ERROR_NEED_SPRING_BOOT_VERSION_15);
+        } else {
+            System.out.println("The Spring Servlet function was not located.");
         }
 
-    }
+        // if ( isClassAvailable("org.springframework.web.socket.WebSocketHandler") ) {
+        //     if ( !isClassAvailable("javax.websocket.WebSocketContainer") ) {
+        //         throw new ApplicationError(Type.ERROR_MISSING_WEBSOCKET_FEATURE);
+        //     }
+        // }
 
-    private void checkSpringBootVersion20() {
-        try {
-            Class.forName(
-                          "com.ibm.ws.springboot.support.web.server.version20.container.LibertyConfiguration");
-        } catch (ClassNotFoundException e) {
-            throw new ApplicationError(Type.ERROR_NEED_SPRING_BOOT_VERSION_20);
+        String webSocketHandlerClassName = "org.springframework.web.socket.WebSocketHandler";
+
+        if ( isClassAvailable(webSocketHandlerClassName) ) {
+            System.out.println("Noted WebSocket spring class [ " + webSocketHandlerClassName + " ]");
+
+            String webSocketClassName =
+                ( springIsAtLeast30 ? "jakarta.websocket.WebSocketContainer" : " javax.websocket.WebSocketContainer" );
+            if ( !isClassAvailable(webSocketClassName) ) {
+                System.out.println("Failed to locate websocket class [ " + webSocketClassName + " ]");
+                throw new ApplicationError(Type.ERROR_MISSING_WEBSOCKET_FEATURE);
+            } else {
+                System.out.println("The Spring WebSocket function was located;" +
+                                   " the base WebSocket API is provisioned.");
+            }
+        } else {
+            System.out.println("The Spring WebSocket function was not located.");
         }
-
-    }
-
-    private void checkSpringBootVersion30() {
-        try {
-            Class.forName(
-                          "io.openliberty.springboot.support.web.server.version30.container.LibertyConfiguration");
-        } catch (ClassNotFoundException e) {
-            throw new ApplicationError(Type.ERROR_NEED_SPRING_BOOT_VERSION_30);
-        }
-
     }
 }
+//@formatter:on
