72aa59b9d17f4182c46db90dabb84e894c2382b9
Malhar Shah <malharshah2000@gmail.com>
2023-06-13 17:27:18 -0400

    Moving LTPA FAT project
    Integration changes
    +
    Resolving FAT run errors and failures
    +
    Converting ant xml to build Gradle

--- dev/com.ibm.ws.security.token.ltpa_fat/fat/src/com/ibm/ws/security/token/ltpa/fat/FATSuite.java
+++ dev/com.ibm.ws.security.token.ltpa_fat/fat/src/com/ibm/ws/security/token/ltpa/fat/FATSuite.java
@@ -32,7 +32,8 @@ import componenttest.rules.repeater.RepeatTests;
 @RunWith(Suite.class)
 @SuiteClasses({
                 AlwaysPassesTest.class,
-//ContextRootCookiePathTests.class,
+                //ContextRootCookiePathTests.class,
+                FATTest.class,
 })
 /**
  * Purpose: This suite collects and runs all known good test suites.
