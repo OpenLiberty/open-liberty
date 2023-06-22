53c0cc6aa4b02a14e1e2d85b871c24e09d8d6cb5
Jim Blye <jimblye@us.ibm.com>
2023-06-22 13:49:39 -0400

    Better clean up after ServerEnvTest fat

--- dev/com.ibm.ws.kernel.boot_fat/fat/src/com/ibm/ws/kernel/boot/ServerEnvTest.java
+++ dev/com.ibm.ws.kernel.boot_fat/fat/src/com/ibm/ws/kernel/boot/ServerEnvTest.java
@@ -12,11 +12,11 @@
  *******************************************************************************/
 package com.ibm.ws.kernel.boot;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -49,8 +49,16 @@ public class ServerEnvTest {
     private static final String SERVER_NAME = "com.ibm.ws.kernel.boot.serverstart.fat";
     private static final String OS = System.getProperty("os.name").toLowerCase();
     private static Locale saveLocale;
+    private static final String BACKUP_EXT = ".backup";
+    private static boolean FIRST_TEST_CASE = true;
+    private String tempConsoleLogName_fullPath; // This is global because we can't delete the log file until the 'after' method.
 
     private static LibertyServer server;
+
+    // Make these global so that we can delete then in the 'AfterClass'.
+    private static String backup_etc_ServerEnv_FileName;
+    private static String backup_shared_ServerEnv_FileName;
+    private static String backup_server_ServerEnv_FileName;
 
     // Describes the 3 locations for the server.env file
     private enum ServerEnvType {
@@ -76,20 +84,127 @@ public class ServerEnvTest {
     @Before
     public void before() {
         server = LibertyServerFactory.getLibertyServer(SERVER_NAME);
+        if (FIRST_TEST_CASE) {
+            // Save the original server.env files to be restored when we are finished.
+            backupServerEnvFiles();
+            FIRST_TEST_CASE = false;
+        }
     }
 
     @AfterClass
     public static void tearDownAfterClass() throws Exception {
 
         Locale.setDefault(saveLocale);
-
+        deleteFile(backup_etc_ServerEnv_FileName);
+        deleteFile(backup_shared_ServerEnv_FileName);
+        deleteFile(backup_server_ServerEnv_FileName);
     }
 
     @After
     public void after() throws Exception {
+
+
+
         if (server.isStarted()) {
+            // Set the console.log file name in the server so that it knows which logs to search for messages
+            // during the server stop.   This avoids a 30 second delay caused by searching the wrong log file.
+            if (tempConsoleLogName_fullPath != null) {
+               server.setConsoleLogName(tempConsoleLogName_fullPath);
+            }
             displayDirectoryContents("after", new File(server.getLogsRoot())); // DEBUG
             server.stopServer();
+        }
+        restoreServerEnvFiles();
+
+        // Some tests create a log file other than console.log.  Delete these as part of clean up.
+        if (tempConsoleLogName_fullPath != null) {
+            deleteFile(tempConsoleLogName_fullPath);
+            tempConsoleLogName_fullPath = null;
+        }
+    }
+
+    /**
+     * Make a copy of any existing server.env files. The tests modify server.env. So restore them after the tests.
+     */
+    private void backupServerEnvFiles() {
+        final String METHOD_NAME = "backupServerEnvFiles";
+
+        try {
+            File etc_ServerEnv_File = new File(server.getInstallRoot() + "/" + ServerEnvType.ETC);
+            File shared_ServerEnv_File = new File(server.getInstallRoot() + "/" + ServerEnvType.SHARED);
+            File server_ServerEnv_File = new File(server.getInstallRoot() + "/" + ServerEnvType.SERVER);
+            backup_etc_ServerEnv_FileName = etc_ServerEnv_File.getAbsolutePath() + BACKUP_EXT;
+            backup_shared_ServerEnv_FileName = shared_ServerEnv_File.getAbsolutePath() + BACKUP_EXT;
+            backup_server_ServerEnv_FileName = server_ServerEnv_File.getAbsolutePath() + BACKUP_EXT;
+
+            if (etc_ServerEnv_File.exists()) {
+                copyFile(etc_ServerEnv_File, backup_etc_ServerEnv_FileName);
+            }
+
+            if (shared_ServerEnv_File.exists()) {
+                copyFile(shared_ServerEnv_File, backup_shared_ServerEnv_FileName);
+            }
+
+            if (server_ServerEnv_File.exists()) {
+                copyFile(server_ServerEnv_File, backup_server_ServerEnv_FileName);
+            }
+        } catch (IOException ioe) {
+            Log.warning(c, METHOD_NAME + " Failed to backup server.env files");
+            Log.error(c, METHOD_NAME, ioe);
+        }
+    }
+
+    /**
+     * Restore the original server.env files from backups. If the server.env file was not originally there, delete it.
+     */
+    private void restoreServerEnvFiles() {
+        final String METHOD_NAME = "restoreServerEnvFiles";
+        String etc_ServerEnv_Path = server.getInstallRoot() + "/" + ServerEnvType.ETC;
+        String shared_ServerEnv_Path = server.getInstallRoot() + "/" + ServerEnvType.SHARED;
+        String server_ServerEnv_Path = server.getInstallRoot() + "/" + ServerEnvType.SERVER;
+
+        File etc_ServerEnv_File = new File(etc_ServerEnv_Path);
+        File shared_ServerEnv_File = new File(shared_ServerEnv_Path);
+        File server_ServerEnv_File = new File(server_ServerEnv_Path);
+
+        File backup_etc_ServerEnv_File = new File(backup_etc_ServerEnv_FileName);
+        File backup_shared_ServerEnv_File = new File(backup_shared_ServerEnv_FileName);
+        File backup_server_ServerEnv_File = new File(backup_server_ServerEnv_FileName);
+
+        // Initially delete all server.env files.  If a backup was created, it will be restored.
+        deleteFile(etc_ServerEnv_File);
+        deleteFile(shared_ServerEnv_File);
+        deleteFile(server_ServerEnv_File);
+
+        try {
+            if (backup_etc_ServerEnv_File.exists()) {
+                copyFile(backup_etc_ServerEnv_File, etc_ServerEnv_Path);
+            }
+
+            if (backup_shared_ServerEnv_File.exists()) {
+                copyFile(backup_etc_ServerEnv_File, shared_ServerEnv_Path);
+            }
+
+            if (backup_server_ServerEnv_File.exists()) {
+                copyFile(backup_etc_ServerEnv_File, server_ServerEnv_Path);
+            }
+        } catch (IOException ioe) {
+            Log.warning(c, METHOD_NAME + " Failed to backup server.env files");
+            Log.error(c, METHOD_NAME, ioe);
+        }
+    }
+
+    public static void copyFile(File sourceFile, String destinationFilePath) throws IOException {
+        try (
+                        FileInputStream inputStream = new FileInputStream(sourceFile);
+                        FileOutputStream outputStream = new FileOutputStream(destinationFilePath)) {
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
         }
     }
 
@@ -175,7 +290,7 @@ public class ServerEnvTest {
 
         // Start the server passing null for the environment variable properties.
         // We don't need the properties set for this test.
-        startServer(null);
+        executeStartServerCommand(null);
         assertTrue("the server should have been started", server.isStarted());
 
         // Create file contents for server.env.   Contents will echo Hello World
@@ -191,8 +306,6 @@ public class ServerEnvTest {
         String output = executeServerVersionCommand();
         assertTrue("The server's server.env did not get invoked as expected. "
                    + HELLO_WORLD + "not found in output", output.contains(HELLO_WORLD));
-
-        deleteFile(serverEnvFile);
 
         Log.exiting(c, METHOD_NAME);
     }
@@ -224,12 +337,12 @@ public class ServerEnvTest {
         String environmentVariableName = "LOG_FILE_NAME_VARIABLE";
 
         // Get the proper syntax !!, ${} for the environment variable to be referenced in .../server.env
-        final String logEnvVarReferenceSyntax = createReferenceSyntaxForVariableName(environmentVariableName);
+        final String envVarReferenceSyntax = createReferenceSyntaxForVariableName(environmentVariableName);
 
         // Create file contents for server.env with or without expansion enabled.
         // Sets the LOG_FILE variable to an environment variable reference.
         // This should cause the name of the "console.log" to be something different.
-        String fileContents = createFileContentsForServerEnv(expansionEnabled, "LOG_FILE=" + logEnvVarReferenceSyntax);
+        String fileContents = createFileContentsForServerEnv(expansionEnabled, "LOG_FILE=" + envVarReferenceSyntax);
 
         File serverEnvFile = createServerEnvFile(fileContents, envType.path);
         assertNotNull("The server.env file was not created. Null returned.", serverEnvFile);
@@ -240,8 +353,15 @@ public class ServerEnvTest {
         Properties envVars = new Properties();
         envVars.put(environmentVariableName, newLogFileName);
 
-        startServer(envVars);
-        assertTrue("the server should have been started", server.isStarted());
+        // The temporary log file name will depend on whether variables are expanded in server.env
+        if (expansionEnabled) {
+            tempConsoleLogName_fullPath = server.getLogsRoot() + "/" + newLogFileName; // store name for clean up later.
+        } else {
+            tempConsoleLogName_fullPath = server.getLogsRoot() + "/" + newLogFileName; // store name for clean up later.
+        }
+
+        executeStartServerCommand(envVars);
+        assertTrue("The server should have been started", server.isStarted());
 
         displayDirectoryContents(METHOD_NAME, new File(server.getLogsRoot())); // DEBUG
 
@@ -249,18 +369,12 @@ public class ServerEnvTest {
         String newLogFullPathName = server.getLogsRoot() + newLogFileName;
         File logFile = new File(newLogFullPathName);
         if (expansionEnabled) {
-            assertTrue("the log file with the new name [ " + newLogFullPathName + " ] does not exist", logFile.exists());
-            renameFileTo(logFile, server.getLogsRoot() + "console.log"); // cleanup
+            assertTrue("The log file with the new name [ " + newLogFullPathName + " ] does not exist", logFile.exists());
         } else {
             // if expansion is not enabled, we should see a log like ${LOG_FILE_NAME_VARIABLE}
-            File logFileUnExpandedCase = new File(server.getLogsRoot() + logEnvVarReferenceSyntax);
-            assertTrue("Was expecting the log file with the unexpanded name [ " + logEnvVarReferenceSyntax + " ] to exist.  It does not.", logFileUnExpandedCase.exists());
-            // and we should not see the log name that has been de-referenced
-            assertTrue("Was not expecting the log file with the new name [ " + newLogFullPathName + " ] to exist, but it sure does.", !logFile.exists());
-            renameFileTo(logFileUnExpandedCase, server.getLogsRoot() + "console.log"); // cleanup
+            File logFileUnExpandedCase = new File(server.getLogsRoot() + envVarReferenceSyntax);
+            assertTrue("Was expecting the log file with the unexpanded name [ " + envVarReferenceSyntax + " ] to exist.  It does not.", logFileUnExpandedCase.exists());
         }
-
-        deleteFile(serverEnvFile);
 
         Log.exiting(c, METHOD_NAME);
     }
@@ -301,8 +415,8 @@ public class ServerEnvTest {
      * @param envVars Properties containing environment variable names and values.
      * @throws Exception
      */
-    private void startServer(Properties envVars) throws Exception {
-        final String METHOD_NAME = "startServer";
+    private String executeStartServerCommand(Properties envVars) throws Exception {
+        final String METHOD_NAME = "executeStartServer";
 
         // Execute the server start command and display stdout and stderr
         String executionDir = server.getInstallRoot() + File.separator + "bin";
@@ -310,9 +424,13 @@ public class ServerEnvTest {
         String[] parms = new String[2];
         parms[0] = "start";
         parms[1] = SERVER_NAME;
+
+        Log.info(c, METHOD_NAME, "\nExecuting 'server start'...");
         ProgramOutput po = server.getMachine().execute(command, parms, executionDir, envVars);
-        Log.info(c, METHOD_NAME, "\nserver start stdout: \n[\n{0}\n]", po.getStdout());
-        Log.info(c, METHOD_NAME, "\nserver start stderr: \n[\n{0}\n]", po.getStderr());
+
+        String stdoutAndstdErr = "\nStandard Out:\n[\n" + po.getStdout() + "\n]" +
+                                 "\nStandard Error:\n[\n" + po.getStderr() + "\n]\n";
+        Log.info(c, METHOD_NAME, stdoutAndstdErr);
 
         // Check for server ready
         String serverReady = server.waitForStringInLog("CWWKF0011I");
@@ -323,6 +441,7 @@ public class ServerEnvTest {
         // Because we didn't start the server using the LibertyServer APIs, we need
         // to have it detect its started state so it will stop and save logs properly
         server.resetStarted();
+        return stdoutAndstdErr;
     }
 
     /**
@@ -389,7 +508,7 @@ public class ServerEnvTest {
     /**
      * @param file to delete
      */
-    private void deleteFile(File file) {
+    private static void deleteFile(File file) {
 
         if (!file.exists()) {
             return;
@@ -402,6 +521,13 @@ public class ServerEnvTest {
         }
 
         return;
+    }
+
+    /**
+     * @param full path and file name of file to delete
+     */
+    private static void deleteFile(String fileName) {
+        deleteFile(new File(fileName));
     }
 
     /**
@@ -425,12 +551,14 @@ public class ServerEnvTest {
         // This should cause the name of the "console.log" to be something different.
         String fileContents;
         String logFileName = "default.log";
+        tempConsoleLogName_fullPath = server.getLogsRoot() + "/" + logFileName; // store name for clean up later.
+
         fileContents = "LOG_FILE=" + logFileName;
         File serverEnvFile = createServerEnvFile(fileContents, ServerEnvType.ETC.path);
         assertNotNull("The server.env file was not created. Null returned.", serverEnvFile);
         assertTrue("The server.env file was not created.", serverEnvFile.exists());
 
-        startServer(null);
+        executeStartServerCommand(null);
         assertTrue("the server should have been started", server.isStarted());
 
         displayDirectoryContents(METHOD_NAME, new File(server.getLogsRoot())); // DEBUG
@@ -439,9 +567,6 @@ public class ServerEnvTest {
         String logFullPathName = server.getLogsRoot() + logFileName;
         File logFile = new File(logFullPathName);
         assertTrue("the log file with the new name [ " + logFullPathName + " ] does not exist", logFile.exists());
-
-        renameFileTo(logFile, server.getLogsRoot() + "console.log");
-        deleteFile(serverEnvFile);
 
         Log.exiting(c, METHOD_NAME);
     }
@@ -486,19 +611,19 @@ public class ServerEnvTest {
         }
     }
 
-    public static void renameFileTo(File originalFile, String newFileName) {
-        final String METHOD_NAME = "renameFileTo";
-        Log.info(c, METHOD_NAME, "renaming:\n[{0}]\n to\n[{1}]", new String[] { originalFile.getAbsolutePath(), newFileName });
-        File newFile = new File(newFileName);
-        assertTrue("Original file [ " + originalFile.getAbsolutePath() + " ] does not exist.", originalFile.exists());
-        assertFalse("File [ " + newFileName + " ] already exists", newFile.exists());
-
-        boolean renameSucceeded = originalFile.renameTo(newFile);
-        assertFalse("Original file [ " + originalFile.getAbsolutePath() + " ] still exists after rename.", originalFile.exists());
-        assertTrue("File [ " + newFileName + " ] does not exist after rename", newFile.exists());
-
-        Log.info(c, METHOD_NAME, "renameSucceeded[{0}]", renameSucceeded);
-    }
+//    public static void renameFileTo(File originalFile, String newFileName) {
+//        final String METHOD_NAME = "renameFileTo";
+//        Log.info(c, METHOD_NAME, "renaming:\n[{0}]\n to\n[{1}]", new String[] { originalFile.getAbsolutePath(), newFileName });
+//        File newFile = new File(newFileName);
+//        assertTrue("Original file [ " + originalFile.getAbsolutePath() + " ] does not exist.", originalFile.exists());
+//        assertFalse("File [ " + newFileName + " ] already exists", newFile.exists());
+//
+//        boolean renameSucceeded = originalFile.renameTo(newFile);
+//        assertFalse("Original file [ " + originalFile.getAbsolutePath() + " ] still exists after rename.", originalFile.exists());
+//        assertTrue("File [ " + newFileName + " ] does not exist after rename", newFile.exists());
+//
+//        Log.info(c, METHOD_NAME, "renameSucceeded[{0}]", renameSucceeded);
+//    }
 
     /**
      * @param methodName
