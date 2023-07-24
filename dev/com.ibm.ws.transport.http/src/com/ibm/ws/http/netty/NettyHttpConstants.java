9f4a8c3f6412edda663ceb8fcceae04f2cf1ebaa
mansal <mansal@us.ibm.com>
2023-07-24 02:20:55 -0400

    Forwarded Header Handler

---
+++ dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/netty/NettyHttpConstants.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2023 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package com.ibm.ws.http.netty;
+
+import io.netty.util.AttributeKey;
+
+/**
+ *
+ */
+public class NettyHttpConstants {
+
+    public static final AttributeKey<String> FORWARDED_PROTO_KEY = AttributeKey.valueOf("forwardedProto");
+    public static final AttributeKey<String> FORWARDED_HOST_KEY = AttributeKey.valueOf("forwardedHost");
+    public static final AttributeKey<String> FORWARDED_PORT_KEY = AttributeKey.valueOf("forwardedPort");
+    public static final AttributeKey<String[]> FORWARDED_BY_KEY = AttributeKey.valueOf("forwardedBy");
+    public static final AttributeKey<String[]> FORWARDED_FOR_KEY = AttributeKey.valueOf("forwardedFor");
+
+}
