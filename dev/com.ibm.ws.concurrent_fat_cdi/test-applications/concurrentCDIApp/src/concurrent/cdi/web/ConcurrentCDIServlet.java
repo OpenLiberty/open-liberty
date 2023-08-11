b5f76e15b551d34acce91e00898df6e3c5246e74
Nathan Rauh <nathan.rauh@us.ibm.com>
2023-08-11 15:31:13 -0500

    Issue #25735 injection of default ContextService

--- dev/com.ibm.ws.concurrent_fat_cdi/test-applications/concurrentCDIApp/src/concurrent/cdi/web/ConcurrentCDIServlet.java
+++ dev/com.ibm.ws.concurrent_fat_cdi/test-applications/concurrentCDIApp/src/concurrent/cdi/web/ConcurrentCDIServlet.java
@@ -12,19 +12,30 @@
  *******************************************************************************/
 package concurrent.cdi.web;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
+import jakarta.enterprise.concurrent.ContextService;
+import jakarta.inject.Inject;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import javax.naming.InitialContext;
 
 import org.junit.Test;
 
@@ -37,12 +48,19 @@ public class ConcurrentCDIServlet extends HttpServlet {
      */
     private static final long TIMEOUT_NS = TimeUnit.MINUTES.toNanos(2);
 
+    @Inject
+    ContextService defaultContextSvc;
+
+    private ExecutorService unmanagedThreads;
+
     @Override
     public void destroy() {
+        unmanagedThreads.shutdownNow();
     }
 
     @Override
     public void init(ServletConfig config) throws ServletException {
+        unmanagedThreads = Executors.newFixedThreadPool(5); // TODO switch to virtual threads?
     }
 
     @Override
@@ -92,9 +110,18 @@ public class ConcurrentCDIServlet extends HttpServlet {
     }
 
     /**
-     * TODO Inject default instance of ContextService.
+     * Inject default instance of ContextService and use it.
      */
     @Test
-    public void testInjectContextServiceDefaultInstance() {
+    public void testInjectContextServiceDefaultInstance() throws Exception {
+        assertNotNull(defaultContextSvc);
+
+        // Use the ContextService to contextualize a task that require the application's context (to look up a java:comp name)
+        Callable<?> task = defaultContextSvc.contextualCallable(() -> InitialContext.doLookup("java:comp/env/entry2"));
+
+        Future<?> future = unmanagedThreads.submit(task);
+
+        Object found = future.get(TIMEOUT_NS, TimeUnit.NANOSECONDS);
+        assertEquals("value2", found);
     }
 }
