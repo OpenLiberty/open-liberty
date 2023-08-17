a1d53f034c00baa7c0e718b5d9cb8ace364bf4c5
Kyle Aure <KyleJAure@gmail.com>
2023-08-16 20:24:23 -0500

    Test aws advanced jdbc wrapper

---
+++ dev/com.ibm.ws.jdbc_fat_postgresql/test-applications/postgresqlApp/src/jdbc/fat/postgresql/web/PostgreSQLAWSTestServlet.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright (c) 2023 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package jdbc.fat.postgresql.web;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.Statement;
+
+import javax.annotation.Resource;
+import javax.servlet.annotation.WebServlet;
+import javax.sql.DataSource;
+
+import org.junit.Test;
+
+import componenttest.app.FATServlet;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = "/PostgreSQLAWSTestServlet")
+public class PostgreSQLAWSTestServlet extends FATServlet {
+
+    @Resource(lookup = "jdbc/common-ds")
+    private DataSource common_ds;
+
+    @Resource(lookup = "jdbc/driver-ds")
+    private DataSource driver_ds;
+
+    // Verify wrapped DataSource class can be used to access PostgreSQL database
+    @Test
+    public void testCommonDataSource() throws Exception {
+        //assert configuration
+        assertNotNull(common_ds);
+        assertTrue(common_ds.isWrapperFor(DataSource.class));
+
+        //assert connection can be used
+        try (Connection con = common_ds.getConnection(); Statement stmt = con.createStatement()) {
+            //assert that the connection that is created used the basic URL
+            assertTrue(con.getMetaData().getURL().contains("jdbc:postgresql:"));
+
+            stmt.executeQuery("SELECT 1");
+        }
+    }
+
+    // Verify wrapped Driver class can be used to access PostgreSQL database
+    @Test
+    public void testDriver() throws Exception {
+        //assert driver cannot be unwrapped to logical driver
+        assertNotNull(driver_ds);
+        assertFalse(driver_ds.isWrapperFor(Driver.class));
+
+        try (Connection con = driver_ds.getConnection(); Statement stmt = con.createStatement()) {
+            //assert that the connection that is created used the basic URL
+            assertTrue(con.getMetaData().getURL().contains("jdbc:postgresql:"));
+
+            //assert that the driver name was correctly configured
+            assertEquals("PostgreSQL JDBC Driver", con.getMetaData().getDriverName());
+
+            //assert connection can be used
+            stmt.executeQuery("SELECT 1");
+        }
+    }
+}
