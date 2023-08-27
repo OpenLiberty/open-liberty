8f299ddc7bc2aedd0228aabe63cb25f0fd6d252b
Jared Anderson <jhanders@us.ibm.com>
2023-08-27 08:13:47 -0500

    Revert "Environment variable LOG_DIR not respected in server.xml"

--- dev/com.ibm.ws.kernel.boot.ws-server/publish/bin/server.bat
+++ dev/com.ibm.ws.kernel.boot.ws-server/publish/bin/server.bat
@@ -528,7 +528,7 @@ goto:eof
   )
 
   @REM Unset these variables to prevent collisions with nested process invocations
-  @REM set LOG_DIR=
+  set LOG_DIR=
   set LOG_FILE=
 
   if NOT defined JAVA_HOME (
