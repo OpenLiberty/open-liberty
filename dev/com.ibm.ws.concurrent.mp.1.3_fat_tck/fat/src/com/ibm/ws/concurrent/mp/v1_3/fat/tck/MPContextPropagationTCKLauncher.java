26817b15236de8f93ccfae8d84e2febc2f4d977b
Tom Evans <tevans@uk.ibm.com>
2023-09-01 12:02:27 +0100

    Add MP61 Repeats to Concurrency FATs

--- dev/com.ibm.ws.concurrent.mp.1.3_fat_tck/fat/src/com/ibm/ws/concurrent/mp/v1_3/fat/tck/MPContextPropagationTCKLauncher.java
+++ dev/com.ibm.ws.concurrent.mp.1.3_fat_tck/fat/src/com/ibm/ws/concurrent/mp/v1_3/fat/tck/MPContextPropagationTCKLauncher.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -33,7 +33,7 @@ public class MPContextPropagationTCKLauncher {
     private static final String SERVER_NAME = "tckServerForMPContextPropagation13";
 
     @ClassRule
-    public static RepeatTests r = MicroProfileActions.repeat(SERVER_NAME, MicroProfileActions.MP60, MicroProfileActions.MP50);
+    public static RepeatTests r = MicroProfileActions.repeat(SERVER_NAME, MicroProfileActions.MP61, MicroProfileActions.MP60, MicroProfileActions.MP50);
 
     @Server(SERVER_NAME)
     public static LibertyServer server;
