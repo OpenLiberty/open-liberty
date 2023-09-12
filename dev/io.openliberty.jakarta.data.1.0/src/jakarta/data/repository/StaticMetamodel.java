40966ee14f1f36daf871e46dbb4ec6aca2722c09
Nathan Rauh <nathan.rauh@us.ibm.com>
2023-09-12 15:57:49 -0500

    fix copyright header

--- dev/io.openliberty.jakarta.data.1.0/src/jakarta/data/repository/StaticMetamodel.java
+++ dev/io.openliberty.jakarta.data.1.0/src/jakarta/data/repository/StaticMetamodel.java
@@ -1,20 +1,15 @@
-/*
- * Copyright (c) 2023 Contributors to the Eclipse Foundation
+/*******************************************************************************
+ * Copyright (c) 2023 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * SPDX-License-Identifier: EPL-2.0
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package jakarta.data.repository;
 
 import java.lang.annotation.Documented;
