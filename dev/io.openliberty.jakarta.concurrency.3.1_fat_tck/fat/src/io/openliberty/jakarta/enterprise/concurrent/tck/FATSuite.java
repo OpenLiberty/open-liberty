2e32f09e371b9f9903e882a501856f490be2a4a2
Kyle Aure <KyleJAure@gmail.com>
2023-09-15 11:04:46 -0500

    Fix concurrency tck signature testing on non-LTS java

--- dev/io.openliberty.jakarta.concurrency.3.1_fat_tck/fat/src/io/openliberty/jakarta/enterprise/concurrent/tck/FATSuite.java
+++ dev/io.openliberty.jakarta.concurrency.3.1_fat_tck/fat/src/io/openliberty/jakarta/enterprise/concurrent/tck/FATSuite.java
@@ -16,7 +16,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.ibm.websphere.simplicity.log.Log;
+
 import componenttest.custom.junit.runner.AlwaysPassesTest;
+import componenttest.custom.junit.runner.Mode;
+import componenttest.custom.junit.runner.Mode.TestMode;
+import componenttest.topology.impl.JavaInfo;
 
 @RunWith(Suite.class)
 @SuiteClasses({
@@ -25,4 +30,39 @@ import componenttest.custom.junit.runner.AlwaysPassesTest;
                 ConcurrentTckLauncherWeb.class //LITE MODE
 })
 public class FATSuite {
+
+    public static boolean shouldRunSignatureTests(Class<?> testClass) {
+        boolean result = false;
+        String reason = "";
+
+        try {
+            if (!testClass.isAnnotationPresent(Mode.class)) {
+                reason = testClass.getCanonicalName() + " is not run in full mode.";
+                return result = false;
+            }
+
+            if (testClass.getAnnotation(Mode.class).value() != TestMode.FULL) {
+                reason = testClass.getCanonicalName() + " is not run in full mode.";
+                return result = false;
+            }
+
+            if (System.getProperty("os.name").contains("Windows")) {
+                reason = "signature test plugin not supported on Windows.";
+                return result = false;
+            }
+
+            if (JavaInfo.JAVA_VERSION != 17 || JavaInfo.JAVA_VERSION != 21) {
+                reason = "signature test not supported on non-LTS java versions: " + JavaInfo.JAVA_VERSION;
+                return result = false;
+            }
+
+            //default option
+            reason = "signature test can run as configured";
+            return result = true;
+
+        } finally {
+            Log.info(testClass, "shouldRunSignatureTests", "Return: " + result + ", because " + reason);
+        }
+
+    }
 }
