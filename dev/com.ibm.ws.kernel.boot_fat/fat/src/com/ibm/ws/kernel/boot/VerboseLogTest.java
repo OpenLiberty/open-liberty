c737f84215bd02c03729e7b2e43aa5ce21ac0552
Ricky Herget <richard.herget@ibm.com>
2023-09-19 12:44:51 -0400

    add FAT testing for verbosegc

--- dev/com.ibm.ws.kernel.boot_fat/fat/src/com/ibm/ws/kernel/boot/VerboseLogTest.java
+++ dev/com.ibm.ws.kernel.boot_fat/fat/src/com/ibm/ws/kernel/boot/VerboseLogTest.java
@@ -5,17 +5,27 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.beans.Transient;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.Properties;
 
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.TestName;
 import org.junit.runner.RunWith;
 
 import com.ibm.websphere.simplicity.Machine;
 import com.ibm.websphere.simplicity.ProgramOutput;
 import com.ibm.websphere.simplicity.OperatingSystem;
+import com.ibm.websphere.simplicity.log.Log;
 
 import componenttest.common.apiservices.Bootstrap;
 import componenttest.custom.junit.runner.FATRunner;
@@ -27,9 +37,16 @@ import componenttest.topology.impl.LibertyServerFactory;
 
 @RunWith(FATRunner.class)
 public class VerboseLogTest {
+    private static final Class<?> c = VerboseLogTest.class;
+
+    @Rule
+    public TestName testName = new TestName();
+
     private static LibertyServer server;
-    private final static String SERVER_NAME = "com.ibm.ws.kernel.boot.verbose";
+    private final static String SERVER_NAME = "com.ibm.ws.kernel.boot.verbose.fat";
     static String executionDir;
+    static File verboseLog;
+    static File verboseLogServerRoot;
     static String serverCommand;
     static File jvmoptionsserverroot;
 
@@ -37,6 +54,8 @@ public class VerboseLogTest {
     public static void before() throws Exception {
         server = LibertyServerFactory.getLibertyServer(SERVER_NAME);
         executionDir = server.getInstallRoot();
+        verboseLog = new File(executionDir + "/usr/servers/" + SERVER_NAME + "/logs/verbosegc.001.log");
+        verboseLogServerRoot = new File(executionDir + "/usr/servers/" + SERVER_NAME + "/verbosegc.001.log");
         jvmoptionsserverroot = new File(executionDir + "/usr/servers/" + SERVER_NAME + "/jvm.options");
         if (server.getMachine().getOperatingSystem() == OperatingSystem.WINDOWS)
             serverCommand = "bin\\server.bat";
@@ -52,89 +71,135 @@ public class VerboseLogTest {
     }
 
     @Test
-    public static void testDefaultVerboseLogging() {
+    public void testDefaultVerboseLogging() throws Exception {
         // Test with no jvm.options, verbose log should appear by default
+        Log.entering(c, testName.getMethodName());
 
-        final String METHOD_NAME = "testDefaultVerboseLogging";
+        delJvmOptions();
 
         String[] parms = new String[2];
         parms[0] = "start";
         parms[1] = SERVER_NAME;
+
+        Properties envVars = new Properties();
+        envVars.put("CDPATH", ".");
 
         ProgramOutput po = server.getMachine().execute(serverCommand, parms, executionDir, envVars);
 
-        server.waitForStringInLog("CWWKF0011I");
+        Log.info(c, testName.getMethodName(), "server start stdout = " + po.getStdout());
+        Log.info(c, testName.getMethodName(), "server start stderr = " + po.getStderr());
 
-        //check if verbosegc log exists
+        server.waitForStringInLog("CWWKF0011I");
+        server.resetStarted();
+
+        assertTrue("the server should have been started", server.isStarted());
+        assertTrue("verbosegc log should have been created", verboseLog.exists());
+
+        server.stopServer();
     }
 
     @Test
-    public static void testJvmTurnOfVerbose() {
+    public void testJvmTurnOfVerbose() throws Exception {
         // Test with jvm.options to turn off verbose log, no verbose log should appear
+        Log.entering(c, testName.getMethodName());
 
-        final String METHOD_NAME = "testJvmTurnOfVerbose";
+        delJvmOptions();
 
         String[] parms = new String[2];
         parms[0] = "start";
         parms[1] = SERVER_NAME;
 
-        ProgramOutput po = server.getMachine().execute(command, parms, executionDir, envVars);
+        Writer isw = new OutputStreamWriter(new FileOutputStream(jvmoptionsserverroot), "UTF-8");
+        BufferedWriter bw = new BufferedWriter(isw);
+        bw.write("-Dnogclog\n");
+        bw.close();
+
+        Properties envVars = new Properties();
+        envVars.put("CDPATH", ".");
+
+        ProgramOutput po = server.getMachine().execute(serverCommand, parms, executionDir, envVars);
+
+        Log.info(c, testName.getMethodName(), "server start stdout = " + po.getStdout());
+        Log.info(c, testName.getMethodName(), "server start stderr = " + po.getStderr());
 
         server.waitForStringInLog("CWWKF0011I");
+        server.resetStarted();
 
-        //check if no verbosegc log exists
+        assertTrue("the server should have been started", server.isStarted());
+        assertTrue("verbosegc log should not be created", !verboseLog.exists());
+        assertTrue("verbosegc log should not be created in server root", !verboseLogServerRoot.exists());
+
+        server.stopServer();
     }
 
     @Test
-    public static void testJvmChangeVerbose() {
+    public void testJvmChangeVerbose() throws Exception {
         // Test with jvm.options, change location or file name, the jvm.options log should be the one shown
+        Log.entering(c, testName.getMethodName());
 
-        final String METHOD_NAME = "testJvmChangeVerbose";
-
-        String[] parms = new String[2];
-        parms[0] = "start";
-        parms[1] = SERVER_NAME;
-
-        ProgramOutput po = server.getMachine().execute(command, parms, executionDir, envVars);
-
-        server.waitForStringInLog("CWWKF0011I");
-
-        //check if verbosegc log exists
-    }
-
-    @Test
-    public static void testEnvVarChangeVerbose() {
-        // Test with environment variable, change location or file name, the jvm.options log should be the one shown
-
-        final String METHOD_NAME = "testEnvVarChangeVerbose";
+        delJvmOptions();
 
         String[] parms = new String[2];
         parms[0] = "start";
         parms[1] = SERVER_NAME;
 
-        ProgramOutput po = server.getMachine().execute(command, parms, executionDir, envVars);
+        Writer isw = new OutputStreamWriter(new FileOutputStream(jvmoptionsserverroot), "UTF-8");
+        BufferedWriter bw = new BufferedWriter(isw);
+        bw.write("-Xverbosegclog:verbosegc.%seq.log,10,1024\n");
+        bw.close();
+
+        Properties envVars = new Properties();
+        envVars.put("CDPATH", ".");
+
+        ProgramOutput po = server.getMachine().execute(serverCommand, parms, executionDir, envVars);
+
+        Log.info(c, testName.getMethodName(), "server start stdout = " + po.getStdout());
+        Log.info(c, testName.getMethodName(), "server start stderr = " + po.getStderr());
 
         server.waitForStringInLog("CWWKF0011I");
+        server.resetStarted();
 
-        //check if verbosegc log exists
+        assertTrue("the server should have been started", server.isStarted());
+        assertTrue("verbosegc log should have been created in server root", verboseLogServerRoot.exists());
+        assertTrue("verbosegc log should not be created in log dir", !verboseLog.exists());
+
+        server.stopServer();
     }
 
     @Test
-    public static void testStartChangeVerbose() {
+    public void testStartChangeVerbose() throws Exception {
         // Test with command variable, change location or file name, the jvm.options log should be the one shown
         // ex. ./bin/server start --verbose:gc
+        Log.entering(c, testName.getMethodName());
 
-        final String METHOD_NAME = "testStartChangeVerbose";
+        delJvmOptions();
 
-        String[] parms = new String[4];
+        String[] parms = new String[3];
         parms[0] = "start";
         parms[1] = SERVER_NAME;
-        parms[2] = "--verbose:gc";
+        parms[2] = "-Xverbosegclog:verbosegc.%seq.log,10,1024";
 
-        ProgramOutput po = server.getMachine().execute(command, parms, executionDir, envVars);
+        Properties envVars = new Properties();
+        envVars.put("CDPATH", ".");
+
+        ProgramOutput po = server.getMachine().execute(serverCommand, parms, executionDir, envVars);
+
+        Log.info(c, testName.getMethodName(), "server start stdout = " + po.getStdout());
+        Log.info(c, testName.getMethodName(), "server start stderr = " + po.getStderr());
 
         server.waitForStringInLog("CWWKF0011I");
+        server.resetStarted();
 
-        //check if verbosegc log exists
+        assertTrue("the server should have been started", server.isStarted());
+        assertTrue("verbosegc log should have been created in server root", verboseLogServerRoot.exists());
+        assertTrue("verbosegc log should not be created in log dir", !verboseLog.exists());
+
+        server.stopServer();
+    }
+
+    private void delJvmOptions() {
+        if(jvmoptionsserverroot.exists()){
+            jvmoptionsserverroot.delete();
+        }
     }
 }
