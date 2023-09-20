f48b17af8d4f217e99c0c99ef6c0cff4156b6394
meiaus <ykkmhc@sbcglobal.net>
2023-09-20 06:04:37 -0700

    LTPAKeyUtil.java-fix one extra } from merge conflict

--- dev/com.ibm.ws.crypto.ltpakeyutil/src/com/ibm/ws/crypto/ltpakeyutil/LTPAKeyUtil.java
+++ dev/com.ibm.ws.crypto.ltpakeyutil/src/com/ibm/ws/crypto/ltpakeyutil/LTPAKeyUtil.java
@@ -14,30 +14,28 @@ package com.ibm.ws.crypto.ltpakeyutil;
 
 import java.security.AccessController;
 import java.security.PrivilegedAction;
-import java.security.PrivilegedActionException;
-import java.security.Provider;
-import java.security.Security;
+
 import com.ibm.ws.kernel.service.util.JavaInfo;
 
 public final class LTPAKeyUtil {
 
-  //public static boolean ibmJCEAvailable = false;
-  public static boolean openJCEPlusAvailable = false;
-  //public static boolean providerChecked = false;
-  public static boolean zosProviderChecked = false;
+	// public static boolean ibmJCEAvailable = false;
+	public static boolean openJCEPlusAvailable = false;
+	// public static boolean providerChecked = false;
+	public static boolean zosProviderChecked = false;
 
-  public static boolean javaVersionChecked = false;
-  public static boolean isJava11orHigher = false;
+	public static boolean javaVersionChecked = false;
+	public static boolean isJava11orHigher = false;
 
-  public static boolean zOSAndJAVA11orHigherChecked = false; 
-  public static boolean iszOSAndJava11orHigher = false; 
+	public static boolean zOSAndJAVA11orHigherChecked = false;
+	public static boolean iszOSAndJava11orHigher = false;
 
-  public static String osName = System.getProperty("os.name");
-  public static boolean isZOS = false;
-  public static boolean osVersionChecked = false;
+	public static String osName = System.getProperty("os.name");
+	public static boolean isZOS = false;
+	public static boolean osVersionChecked = false;
 
-  //public static String IBM_JCE_PROVIDER = "com.ibm.crypto.provider.IBMJCE";
-  public static String OPENJCEPLUS_PROVIDER = "com.ibm.crypto.plus.provider.OpenJCEPlus";
+	// public static String IBM_JCE_PROVIDER = "com.ibm.crypto.provider.IBMJCE";
+	public static String OPENJCEPLUS_PROVIDER = "com.ibm.crypto.plus.provider.OpenJCEPlus";
 
 	public static boolean ibmJCEAvailable = false;
 	public static boolean ibmJCEPlusFIPSAvailable = false;
@@ -82,80 +80,76 @@ public final class LTPAKeyUtil {
 		return LTPACrypto.generate3DESKey();
 	}
 
-  public static boolean isIBMJCEAvailable() {
-    if (providerChecked) {
-      return ibmJCEAvailable;
-    } else {
-      ibmJCEAvailable = JavaInfo.isSystemClassAvailable(IBM_JCE_PROVIDER);
-      providerChecked = true;
-      return ibmJCEAvailable;
-    }
+	public static boolean isIBMJCEAvailable() {
+		if (providerChecked) {
+			return ibmJCEAvailable;
+		} else {
+			ibmJCEAvailable = JavaInfo.isSystemClassAvailable(IBM_JCE_PROVIDER);
+			providerChecked = true;
+			return ibmJCEAvailable;
+		}
 
-   }
+	}
 
-  public static boolean isOpenJCEPlusAvailable() {
-    if (zosProviderChecked) {
-      return openJCEPlusAvailable;
-    } else {
-      openJCEPlusAvailable = JavaInfo.isSystemClassAvailable(OPENJCEPLUS_PROVIDER);
-      zosProviderChecked = true;
-      return openJCEPlusAvailable;
-    }
+	public static boolean isOpenJCEPlusAvailable() {
+		if (zosProviderChecked) {
+			return openJCEPlusAvailable;
+		} else {
+			openJCEPlusAvailable = JavaInfo.isSystemClassAvailable(OPENJCEPLUS_PROVIDER);
+			zosProviderChecked = true;
+			return openJCEPlusAvailable;
+		}
 
-  }
+	}
 
-  private static boolean isJava11orHigher() {
-    if (javaVersionChecked) {
-      return isJava11orHigher;
-    } else {
-      isJava11orHigher = JavaInfo.majorVersion() >= 11;
-      javaVersionChecked = true;
-      return isJava11orHigher;
-    }
-  }
+	private static boolean isJava11orHigher() {
+		if (javaVersionChecked) {
+			return isJava11orHigher;
+		} else {
+			isJava11orHigher = JavaInfo.majorVersion() >= 11;
+			javaVersionChecked = true;
+			return isJava11orHigher;
+		}
+	}
 
-  private static boolean isZOS() {
-    if (osVersionChecked) {
-      return isZOS;
-    } else {
-      isZOS = (osName.equalsIgnoreCase("z/OS") || osName.equalsIgnoreCase("OS/390"));
-      osVersionChecked = true;
-      return isZOS;
-    }
-  }
+	private static boolean isZOS() {
+		if (osVersionChecked) {
+			return isZOS;
+		} else {
+			isZOS = (osName.equalsIgnoreCase("z/OS") || osName.equalsIgnoreCase("OS/390"));
+			osVersionChecked = true;
+			return isZOS;
+		}
+	}
 
-  public static boolean isZOSandRunningJava11orHigher() {
-    if (zOSAndJAVA11orHigherChecked){
-      return iszOSAndJava11orHigher;
-    }
-    else {
-      iszOSAndJava11orHigher = isJava11orHigher() && isZOS();
-      zOSAndJAVA11orHigherChecked = true;
-      return iszOSAndJava11orHigher;
-    }
-  }
-
-}
+	public static boolean isZOSandRunningJava11orHigher() {
+		if (zOSAndJAVA11orHigherChecked) {
+			return iszOSAndJava11orHigher;
+		} else {
+			iszOSAndJava11orHigher = isJava11orHigher() && isZOS();
+			zOSAndJAVA11orHigherChecked = true;
+			return iszOSAndJava11orHigher;
+		}
+	}
 
 	public static boolean isIBMJCEPlusFIPSAvailable() {
 		if (providerChecked) {
 			return ibmJCEPlusFIPSAvailable;
 		} else {
 
-
-                    String ibmjceplusfipsprovider = AccessController.doPrivileged(new PrivilegedAction<String>() {
-                    @Override
-                    public String run() {
-                            return System.getProperty("com.ibm.jsse2.usefipsProviderName");
-                    }
-                     });
-                    if (ibmjceplusfipsprovider == "IBMJCEPlusFIPS") {
-                         ibmJCEPlusFIPSAvailable = true;
-                         providerChecked = true;
-                         return ibmJCEPlusFIPSAvailable;
-                     } else {
-                         return false;
-                     }             
+			String ibmjceplusfipsprovider = AccessController.doPrivileged(new PrivilegedAction<String>() {
+				@Override
+				public String run() {
+					return System.getProperty("com.ibm.jsse2.usefipsProviderName");
+				}
+			});
+			if (ibmjceplusfipsprovider == "IBMJCEPlusFIPS") {
+				ibmJCEPlusFIPSAvailable = true;
+				providerChecked = true;
+				return ibmJCEPlusFIPSAvailable;
+			} else {
+				return false;
+			}
 		}
 
 	}
@@ -167,12 +161,11 @@ public final class LTPAKeyUtil {
 				return System.getProperty("com.ibm.jsse2.usefipsprovider");
 			}
 		});
-                if (fipsON == "true") {
-                    return true;
-                } else {
-                    return false;
-                }             
+		if (fipsON == "true") {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
-
