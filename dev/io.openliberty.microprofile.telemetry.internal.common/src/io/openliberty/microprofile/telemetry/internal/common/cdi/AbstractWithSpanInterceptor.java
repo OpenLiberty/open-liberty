d9656341d151e771ce2ba69eb85a52729b963f24
Andrew Rouse <anrouse@uk.ibm.com>
2023-09-21 16:26:52 +0100

    Revert "Test Telemetry WithSpaninterceptor"
    This reverts commit 7410cf3510355ff1b92d452c84b5db193c6d4e60.

--- dev/io.openliberty.microprofile.telemetry.internal.common/src/io/openliberty/microprofile/telemetry/internal/common/cdi/AbstractWithSpanInterceptor.java
+++ dev/io.openliberty.microprofile.telemetry.internal.common/src/io/openliberty/microprofile/telemetry/internal/common/cdi/AbstractWithSpanInterceptor.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- *
+ * 
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -19,7 +19,6 @@ import java.security.PrivilegedAction;
 
 import com.ibm.websphere.ras.Tr;
 import com.ibm.websphere.ras.TraceComponent;
-import com.ibm.ws.ffdc.annotation.FFDCIgnore;
 
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.SpanKind;
@@ -33,130 +32,124 @@ import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
 import io.opentelemetry.instrumentation.api.instrumenter.InstrumenterBuilder;
 import io.opentelemetry.instrumentation.api.instrumenter.SpanNameExtractor;
 import io.opentelemetry.instrumentation.api.instrumenter.util.SpanNames;
+import jakarta.annotation.Priority;
 import jakarta.inject.Inject;
 import jakarta.interceptor.AroundInvoke;
+import jakarta.interceptor.Interceptor;
 import jakarta.interceptor.InvocationContext;
 
-/**
+/** 
  * This intercepter is responsible for creating Spans and managing the span context for all methods annotated @Span
  */
 public abstract class AbstractWithSpanInterceptor {
-    protected static final TraceComponent tc = Tr.register(AbstractWithSpanInterceptor.class);
+	protected static final TraceComponent tc = Tr.register(AbstractWithSpanInterceptor.class);
 
-    protected final String instrumentationName = "io.openliberty.microprofile.telemetry";
+	protected final String instrumentationName = "io.openliberty.microprofile.telemetry";
 
-    protected final Instrumenter<MethodRequest, Void> instrumenter;
+	protected final Instrumenter<MethodRequest, Void> instrumenter;
 
-    public AbstractWithSpanInterceptor() {
-        // Required public no-arg constructor for interceptor
-        instrumenter = null;
-    }
+	public AbstractWithSpanInterceptor() {
+		// Required public no-arg constructor for interceptor
+		instrumenter = null;
+	}
 
-    @Inject
-    public AbstractWithSpanInterceptor(OpenTelemetry openTelemetry) {
-        MethodSpanAttributesExtractor<MethodRequest, Void> attributesExtractor = getMethodSpanAttributesExtractor();
-        InstrumenterBuilder<MethodRequest, Void> builder = Instrumenter.builder(openTelemetry, instrumentationName, new MethodRequestSpanNameExtractor());
+	@Inject
+	public AbstractWithSpanInterceptor(OpenTelemetry openTelemetry) {
+		MethodSpanAttributesExtractor<MethodRequest, Void> attributesExtractor = getMethodSpanAttributesExtractor();
+		InstrumenterBuilder<MethodRequest, Void> builder = Instrumenter.builder(openTelemetry, instrumentationName, new MethodRequestSpanNameExtractor());
 
-        this.instrumenter = builder.addAttributesExtractor(attributesExtractor).buildInstrumenter(methodRequest -> spanKindFromMethod(methodRequest.getMethod()));
-    }
+		this.instrumenter = builder.addAttributesExtractor(attributesExtractor)
+				.buildInstrumenter(methodRequest -> spanKindFromMethod(methodRequest.getMethod()));
+	}
 
-    //This method is abstract because requires calling an API that changes across different upstream versions of OpenTelemetry
-    protected abstract MethodSpanAttributesExtractor<MethodRequest, Void> getMethodSpanAttributesExtractor();
+	//This method is abstract because requires calling an API that changes across different upstream versions of OpenTelemetry
+	protected abstract MethodSpanAttributesExtractor<MethodRequest, Void> getMethodSpanAttributesExtractor();
 
-    private static SpanKind spanKindFromMethod(Method method) {
-        WithSpan annotation = method.getDeclaredAnnotation(WithSpan.class);
-        if (annotation == null) {
-            return SpanKind.INTERNAL;
-        }
-        return annotation.kind();
-    }
+	private static SpanKind spanKindFromMethod(Method method) {
+		WithSpan annotation = method.getDeclaredAnnotation(WithSpan.class);
+		if (annotation == null) {
+			return SpanKind.INTERNAL;
+		}
+		return annotation.kind();
+	}
 
-    @FFDCIgnore(Throwable.class)
-    @AroundInvoke
-    public Object span(final InvocationContext invocationContext) throws Exception {
-        MethodRequest methodRequest = new MethodRequest(invocationContext.getMethod(), invocationContext.getParameters());
+	@AroundInvoke
+	public Object span(final InvocationContext invocationContext) throws Exception {
+		MethodRequest methodRequest = new MethodRequest(invocationContext.getMethod(), invocationContext.getParameters());
 
-        Context parentContext = Context.current();
-        Context spanContext = null;
-        Scope scope = null;
-        boolean shouldStart = instrumenter != null && instrumenter.shouldStart(parentContext, methodRequest);
-        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
-            Tr.debug(this, tc, "Method " + invocationContext.getMethod().toString() + " Should start: " + shouldStart);
-        }
+		Context parentContext = Context.current();
+		Context spanContext = null;
+		Scope scope = null;
+		boolean shouldStart = instrumenter.shouldStart(parentContext, methodRequest);
+		if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+			Tr.debug(this, tc, "Method " + invocationContext.getMethod().toString() + " Should start: " + shouldStart);
+		}
 
-        if (shouldStart) {
-            spanContext = instrumenter.start(parentContext, methodRequest);
-            scope = spanContext.makeCurrent();
-            if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
-                Tr.debug(this, tc, "spanContext " + spanContext.toString() + " has started and is now the current context");
-            }
-        }
+		if (shouldStart) {
+			spanContext = instrumenter.start(parentContext, methodRequest);
+			scope = spanContext.makeCurrent();
+			if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+				Tr.debug(this, tc, "spanContext " + spanContext.toString() + " has started and is now the current context");
+			}
+		}
 
-        try {
-            Object result = invocationContext.proceed();
-            if (shouldStart) {
-                instrumenter.end(spanContext, methodRequest, null, null);
-                if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
-                    Tr.debug(this, tc, "spanContext " + spanContext.toString() + " has ended");
-                }
-            }
-            return result;
-        } catch (Throwable error) {
-            if (shouldStart) {
-                instrumenter.end(spanContext, methodRequest, null, error);
-                if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
-                    Tr.debug(this, tc, "spanContext " + spanContext.toString() + " has ended with an exception", error);
-                }
-            }
-            throw error;
-        } finally {
-            if (scope != null) {
-                scope.close();
-            }
-        }
-    }
+		try {
+			Object result = invocationContext.proceed();
+			if (shouldStart) {
+				instrumenter.end(spanContext, methodRequest, null, null);
+				if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+					Tr.debug(this, tc, "spanContext " + spanContext.toString() + " has ended");
+				}
+			}
+			return result;
+		} finally {
+			if (scope != null) {
+				scope.close();
+			}
+		}
+	}
 
-    protected static final class MethodRequestSpanNameExtractor implements SpanNameExtractor<MethodRequest> {
-        @Override
-        public String extract(final MethodRequest methodRequest) {
-            return AccessController.doPrivileged((PrivilegedAction<String>) () -> {
-                WithSpan annotation = methodRequest.getMethod().getDeclaredAnnotation(WithSpan.class);
-                String spanName = annotation.value();
-                if (spanName.isEmpty()) {
-                    spanName = SpanNames.fromMethod(methodRequest.getMethod());
-                }
-                return spanName;
-            });
-        }
-    }
+	protected static final class MethodRequestSpanNameExtractor implements SpanNameExtractor<MethodRequest> {
+		@Override
+		public String extract(final MethodRequest methodRequest) {
+			return AccessController.doPrivileged((PrivilegedAction<String>) () -> {
+				WithSpan annotation = methodRequest.getMethod().getDeclaredAnnotation(WithSpan.class);
+				String spanName = annotation.value();
+				if (spanName.isEmpty()) {
+					spanName = SpanNames.fromMethod(methodRequest.getMethod());
+				}
+				return spanName;
+			});
+		}
+	}
 
-    protected static WithSpanParameterAttributeNamesExtractor getNewWithSpanParameterAttributeNamesExtractor() {
-        return new WithSpanParameterAttributeNamesExtractor();
-    }
+	protected static WithSpanParameterAttributeNamesExtractor getNewWithSpanParameterAttributeNamesExtractor() {
+		return new WithSpanParameterAttributeNamesExtractor();
+	}
 
-    protected static final class WithSpanParameterAttributeNamesExtractor implements ParameterAttributeNamesExtractor {
-        protected static String attributeName(Parameter parameter) {
-            SpanAttribute spanAttribute = parameter.getDeclaredAnnotation(SpanAttribute.class);
-            if (spanAttribute == null) {
-                return null;
-            }
-            String value = spanAttribute.value();
-            if (!value.isEmpty()) {
-                return value;
-            } else if (parameter.isNamePresent()) {
-                return parameter.getName();
-            } else {
-                return null;
-            }
-        }
+	protected static final class WithSpanParameterAttributeNamesExtractor implements ParameterAttributeNamesExtractor {
+		protected static String attributeName(Parameter parameter) {
+			SpanAttribute spanAttribute = parameter.getDeclaredAnnotation(SpanAttribute.class);
+			if (spanAttribute == null) {
+				return null;
+			}
+			String value = spanAttribute.value();
+			if (!value.isEmpty()) {
+				return value;
+			} else if (parameter.isNamePresent()) {
+				return parameter.getName();
+			} else {
+				return null;
+			}
+		}
 
-        @Override
-        public String[] extract(final Method method, final Parameter[] parameters) {
-            String[] attributeNames = new String[parameters.length];
-            for (int i = 0; i < parameters.length; i++) {
-                attributeNames[i] = attributeName(parameters[i]);
-            }
-            return attributeNames;
-        }
-    }
+		@Override
+		public String[] extract(final Method method, final Parameter[] parameters) {
+			String[] attributeNames = new String[parameters.length];
+			for (int i = 0; i < parameters.length; i++) {
+				attributeNames[i] = attributeName(parameters[i]);
+			}
+			return attributeNames;
+		}
+	}
 }
