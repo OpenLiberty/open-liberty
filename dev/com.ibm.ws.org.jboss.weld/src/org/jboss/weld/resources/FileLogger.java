cc7477c1b439ac03de9b22fd04cb1b5e9857d94f
Thomas Bitonti <bitonti@us.ibm.com>
2023-10-09 12:38:06 -0400

    Add logging to JBOSS WELD reflection cache.

--- dev/com.ibm.ws.org.jboss.weld/src/org/jboss/weld/resources/FileLogger.java
+++ dev/com.ibm.ws.org.jboss.weld/src/org/jboss/weld/resources/FileLogger.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-package com.ibm.ws.ras.instrument.internal.main;
+package org.jboss.weld.resources;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
---
+++ dev/com.ibm.ws.org.jboss.weld/src/org/jboss/weld/resources/HotspotReflectionCache.java
@@ -0,0 +1,113 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.resources;
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+import java.util.Properties;
+
+import org.jboss.weld.exceptions.WeldException;
+import org.jboss.weld.metadata.TypeStore;
+
+/**
+ * {@link ReflectionCache} implementation that works around possible deadlocks in HotSpot:
+ *
+ * @see https://issues.jboss.org/browse/WELD-1169
+ * @see http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=7122142
+ * @see http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6588239
+ *
+ * @author Jozef Hartinger
+ *
+ */
+public class HotspotReflectionCache extends DefaultReflectionCache {
+
+    private final Class<?> annotationTypeLock;
+
+    public HotspotReflectionCache(TypeStore store) {
+        super(store);
+        try {
+            this.annotationTypeLock = Class.forName("sun.reflect.annotation.AnnotationType");
+        } catch (ClassNotFoundException e) {
+            throw new WeldException(e);
+        }
+    }
+
+    protected static final Properties loggerProperties = new Properties();
+    static {
+        loggerProperties.setProperty(FileLogger.FileLoggerProperties.FILE_PROPERTY_NAME, "LTI_W");
+        loggerProperties.setProperty(FileLogger.FileLoggerProperties.PREFIX_PROPERTY_NAME, "WELD: ");
+    }
+
+    protected static final FileLogger logger = (new FileLogger.FileLoggerProperties(loggerProperties)).create();
+
+    protected static final void dump(Class<?> targetClass) {
+        String className = "HotspotReflectionCache";
+        String methodName = "dump";
+
+        String resourceName = FileLogger.getClassResourceName(targetClass);
+
+        String text = "Class [ " + targetClass.getName() + " ] as [ " + resourceName + " ]";
+        logger.log(className, methodName, text);
+
+        byte[] classBytes;
+        try {
+            classBytes = FileLogger.read(targetClass.getClassLoader(), resourceName);
+        } catch ( IOException e ) {
+            logger.logStack(className, methodName, "Failed to read [ " + resourceName + " ]", e);
+            return;
+        }
+
+        logger.dump(className, methodName, text, classBytes);
+    }
+
+    @Override
+    protected Annotation[] internalGetAnnotations(AnnotatedElement element) {
+        try {
+            if ( element instanceof Class<?> ) {
+                Class<?> clazz = (Class<?>) element;
+                if ( clazz.isAnnotation() ) {
+                    synchronized ( annotationTypeLock ) {
+                        return element.getAnnotations();
+                    }
+                }
+            }
+            return element.getAnnotations();
+
+        } catch ( IllegalArgumentException e ) {
+            String className = "HotspotReflectionCache";
+            String methodName = "internalGetAnnotations";
+
+            String text = "Constant Pool GREP:";
+            Class<?> clazz = null;
+
+            if ( element instanceof Class<?> ) {
+                clazz = (Class<?>) element;
+                text += " Class [ " + clazz.getName() + " ]:";
+            }
+
+            logger.log(className, methodName, text, element);
+            if ( clazz != null ) {
+                dump(clazz);
+            }
+            logger.logStack(className, methodName, text);
+
+            throw e;
+        }
+    }
+
+}
