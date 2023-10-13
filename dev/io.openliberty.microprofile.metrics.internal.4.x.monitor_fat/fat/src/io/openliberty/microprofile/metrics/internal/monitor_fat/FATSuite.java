cbfbba3c48d63449fb44ce98e7418c852b16eaae
Prashanth G <pgunapal@ca.ibm.com>
2023-10-13 17:12:09 -0400

    Port new computed metrics to MpMetrics 3.0 and 4.0
    Added FAT to test new computed metrics for MP Metrics 4.0
    
    Update bnd.bnd
    
    Update MonitorMetrics.java
    
    Update MonitorMetricsHandler.java

--- dev/io.openliberty.microprofile.metrics.internal.4.x.monitor_fat/fat/src/io/openliberty/microprofile/metrics/internal/monitor_fat/FATSuite.java
+++ dev/io.openliberty.microprofile.metrics.internal.4.x.monitor_fat/fat/src/io/openliberty/microprofile/metrics/internal/monitor_fat/FATSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021 IBM Corporation and others.
+ * Copyright (c) 2021, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -12,16 +12,12 @@
  *******************************************************************************/
 package io.openliberty.microprofile.metrics.internal.monitor_fat;
 
-import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import componenttest.rules.repeater.FeatureReplacementAction;
-import componenttest.rules.repeater.RepeatTests;
-
 @RunWith(Suite.class)
-@SuiteClasses({ MetricsMonitorTest.class, TestEnableDisableFeaturesTest.class })
+@SuiteClasses({ComputedMetricsTest.class, MetricsMonitorTest.class, TestEnableDisableFeaturesTest.class })
 public class FATSuite {
 
     /*
