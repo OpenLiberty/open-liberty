850af428d565853a00df352f5ba55c5aee9b0ab4
mansal <mansal@us.ibm.com>
2023-10-16 19:54:42 -0400

    Implementing a chunk input based on WsByteBuffers to go along with Netty's chunked write handler.

---
+++ dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/netty/pipeline/ChunkSizeLoggingHandler.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2023 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package com.ibm.ws.http.netty.pipeline;
+
+import com.ibm.ws.http.netty.MSP;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+
+/**
+ *
+ */
+public class ChunkSizeLoggingHandler extends ChannelOutboundHandlerAdapter {
+
+    @Override
+    public void write(ChannelHandlerContext context, Object msg, ChannelPromise promise) throws Exception {
+        if (msg instanceof ByteBuf) {
+            ByteBuf chunk = (ByteBuf) msg;
+            int chunkSize = chunk.readableBytes();
+            MSP.log("Chunk size: " + chunkSize + " bytes");
+        }
+
+        super.write(context, msg, promise);
+    }
+
+}
