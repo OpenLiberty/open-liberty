c784482375dd4757116b438a5d21d5f4c92e28d8
Paul Nicolucci <pnicolucci@gmail.com>
2023-10-18 09:28:19 -0400

    Use isEE11OrLaterActive rather than isEE11Active in EL 5.0 FAT

--- dev/io.openliberty.org.apache.jasper.expressionLanguage.5.0_fat/fat/src/io/openliberty/org/apache/jasper/expressionLanguage50/fat/tests/EL50LambdaExpressionCoercionTest.java
+++ dev/io.openliberty.org.apache.jasper.expressionLanguage.5.0_fat/fat/src/io/openliberty/org/apache/jasper/expressionLanguage50/fat/tests/EL50LambdaExpressionCoercionTest.java
@@ -41,7 +41,7 @@ public class EL50LambdaExpressionCoercionTest extends FATServletClient {
 
         // Set websphere.java.security.exempt=true because Expression Language 6.0 removed all references
         // to the SecurityManager and related APIs.
-        if (JakartaEEAction.isEE11Active()) {
+        if (JakartaEEAction.isEE11OrLaterActive()) {
             ELUtils.setServerJavaSecurityExempt(server);
         }
 
