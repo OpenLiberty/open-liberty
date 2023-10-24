0f95fcc632a74fdc348f09000d3ac6045e096bf8
Ut Le <utle@us.ibm.com>
2023-10-24 18:32:52 -0500

    Add new method signature and encrypt methods fixed manual merged errors

--- dev/com.ibm.ws.crypto.ltpakeyutil/src/com/ibm/ws/crypto/ltpakeyutil/LTPACrypto.java
+++ dev/com.ibm.ws.crypto.ltpakeyutil/src/com/ibm/ws/crypto/ltpakeyutil/LTPACrypto.java
@@ -12,8 +12,6 @@
  *******************************************************************************/
 package com.ibm.ws.crypto.ltpakeyutil;
 
-import com.ibm.websphere.ras.Tr;
-import com.ibm.websphere.ras.TraceComponent;
 import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
@@ -42,24 +40,33 @@ import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESedeKeySpec;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+
+import com.ibm.websphere.ras.Tr;
+import com.ibm.websphere.ras.TraceComponent;
 import com.ibm.websphere.ras.annotation.Trivial;
 
 final class LTPACrypto {
 
 	private static final TraceComponent tc = Tr.register(LTPACrypto.class);
-	private static final String SIGNATURE_ALGORITHM = "SHA1withRSA";
-	private static final String SIGNATURE_ALGORITHM_SHA256withRSA = "SHA256withRSA";
-	private static final String CRYPTO_ALGORITHM = "RSA";
-	private static final String ENCRYPT_ALGORITHM = "DESede";
-	private static final String ENCRYPT_ALGORITHM_RSA = "RSA";
 	private static final String IBMJCE_NAME = "IBMJCE";
-	private static final String IBMJCEPlusFIPS_NAME = "IBMJCEPlusFIPS";
-	private static final String OPENJCEPLUS_NAME = "OpenJCEPlus";
+	private static final String IBMJCE_PLUS_FIPS_NAME = "IBMJCEPlusFIPS";
+	private static final String OPENJCE_PLUS_NAME = "OpenJCEPlus";
 	private static String provider = getProvider();
+
+	private static final String SIGNATURE_ALGORITHM_SHA1WITHRSA = "SHA1withRSA";
+	private static final String SIGNATURE_ALGORITHM_SHA256WITHRSA = "SHA256withRSA";
+	private static final String signatureAlgorithm = getSignatureAlgorithm();
+
+	private static final String CRYPTO_ALGORITHM_RSA = "RSA";
+
+	private static final String ENCRYPT_ALGORITHM_DESEDE = "DESede";
+	private static final String ENCRYPT_ALGORITHM_RSA = "RSA";
+	private static final String encryptAlgorithm = getEncryptAlgorithm();
 
 	private static int MAX_CACHE = 500;
 	private static IvParameterSpec ivs8 = null;
 	private static IvParameterSpec ivs16 = null;
+
 	@Trivial
 	private static class CachingKey {
 
@@ -71,6 +78,7 @@ final class LTPACrypto {
 		private final int len;
 		private int hashcode;
 		private byte[] result;
+
 		@Trivial
 		private CachingKey(byte[][] key, byte[] data, int off, int len) {
 			this.key = key;
@@ -98,6 +106,7 @@ final class LTPACrypto {
 			}
 			hashcode *= 2;
 		}
+
 		@Trivial
 		@Override
 		public boolean equals(Object to) {
@@ -188,14 +197,10 @@ final class LTPACrypto {
 	/**
 	 * Sign the data.
 	 *
-	 * @param key
-	 *            The key used to sign the data
-	 * @param data
-	 *            The byte representation of the data
-	 * @param off
-	 *            The offset of the data
-	 * @param len
-	 *            The length of the data
+	 * @param key  The key used to sign the data
+	 * @param data The byte representation of the data
+	 * @param off  The offset of the data
+	 * @param len  The length of the data
 	 * @return The signature of the data
 	 */
 	@Trivial
@@ -245,13 +250,9 @@ final class LTPACrypto {
 		BigInteger q = new BigInteger(key[4]);
 		BigInteger d = e.modInverse((p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE)));
 		KeyFactory kFact = null;
-		if (LTPAKeyUtil.isFIPSEnabled() && LTPAKeyUtil.isIBMJCEPlusFIPSAvailable()) {
-			kFact = KeyFactory.getInstance(CRYPTO_ALGORITHM, IBMJCEPlusFIPS_NAME);
-		} else if (LTPAKeyUtil.isIBMJCEAvailable()) {
-			kFact = KeyFactory.getInstance(CRYPTO_ALGORITHM, IBMJCE_NAME);
-		} else {
-			kFact = KeyFactory.getInstance(CRYPTO_ALGORITHM);
-		}
+
+		kFact = (provider == null) ? KeyFactory.getInstance(CRYPTO_ALGORITHM_RSA)
+				: KeyFactory.getInstance(CRYPTO_ALGORITHM_RSA, provider);
 
 		BigInteger pep = new BigInteger(key[5]);
 		BigInteger peq = new BigInteger(key[6]);
@@ -261,13 +262,9 @@ final class LTPACrypto {
 
 		Signature rsaSig = null;
 
-		if (LTPAKeyUtil.isFIPSEnabled() && LTPAKeyUtil.isIBMJCEPlusFIPSAvailable()) {
-			rsaSig = Signature.getInstance(SIGNATURE_ALGORITHM_SHA256withRSA, IBMJCEPlusFIPS_NAME);
-		} else if (LTPAKeyUtil.isIBMJCEAvailable()) {
-			rsaSig = Signature.getInstance(SIGNATURE_ALGORITHM, IBMJCE_NAME);
-		} else {
-			rsaSig = Signature.getInstance(SIGNATURE_ALGORITHM);
-		}
+		rsaSig = (provider == null) ? Signature.getInstance(signatureAlgorithm)
+				: Signature.getInstance(signatureAlgorithm, provider);
+
 		rsaSig.initSign(privKey);
 		rsaSig.update(data, off, len);
 		byte[] sig = rsaSig.sign();
@@ -280,6 +277,7 @@ final class LTPACrypto {
 	}
 
 	private static final ConcurrentHashMap<CachingVerifyKey, CachingVerifyKey> verifyKeysMap = new ConcurrentHashMap<CachingVerifyKey, CachingVerifyKey>();
+
 	@Trivial
 	private static class CachingVerifyKey {
 
@@ -293,6 +291,7 @@ final class LTPACrypto {
 		private final int sigLen;
 		private int hashcode;
 		private boolean result;
+
 		@Trivial
 		private CachingVerifyKey(byte[][] key, byte[] data, int off, int len, byte[] sig, int sigOff, int sigLen) {
 			this.key = key;
@@ -437,6 +436,7 @@ final class LTPACrypto {
 		}
 
 	}
+
 	private static final Comparator<CachingVerifyKey> cachingVerifyKeyComparator = new Comparator<CachingVerifyKey>() {
 		@Override
 		@Trivial
@@ -476,20 +476,13 @@ final class LTPACrypto {
 	/**
 	 * Verify if the signature of the data is correct.
 	 *
-	 * @param key
-	 *            The key used to verify the data
-	 * @param data
-	 *            The byte representation of the data
-	 * @param off
-	 *            The offset of the data
-	 * @param len
-	 *            The length of the data
-	 * @param sig
-	 *            The signature of the data
-	 * @param off
-	 *            The offset of the signature
-	 * @param len
-	 *            The length of the signature
+	 * @param key  The key used to verify the data
+	 * @param data The byte representation of the data
+	 * @param off  The offset of the data
+	 * @param len  The length of the data
+	 * @param sig  The signature of the data
+	 * @param off  The offset of the signature
+	 * @param len  The length of the signature
 	 * @return True if the signature of the data is correct
 	 */
 	@Trivial
@@ -533,24 +526,14 @@ final class LTPACrypto {
 		KeyFactory kFact = null;
 		Signature rsaSig = null;
 
-		if (LTPAKeyUtil.isFIPSEnabled() && LTPAKeyUtil.isIBMJCEPlusFIPSAvailable()) {
-			kFact = KeyFactory.getInstance(CRYPTO_ALGORITHM, IBMJCEPlusFIPS_NAME);
-		} else if (LTPAKeyUtil.isIBMJCEAvailable()) {
-			kFact = KeyFactory.getInstance(CRYPTO_ALGORITHM, IBMJCE_NAME);
-		} else {
-			kFact = KeyFactory.getInstance(CRYPTO_ALGORITHM);
-		}
+		kFact = (provider == null) ? KeyFactory.getInstance(CRYPTO_ALGORITHM_RSA)
+				: KeyFactory.getInstance(CRYPTO_ALGORITHM_RSA, provider);
 
 		RSAPublicKeySpec pubKeySpec = new RSAPublicKeySpec(n, e);
 		PublicKey pubKey = kFact.generatePublic(pubKeySpec);
 
-		if (LTPAKeyUtil.isFIPSEnabled() && LTPAKeyUtil.isIBMJCEPlusFIPSAvailable()) {
-			rsaSig = Signature.getInstance(SIGNATURE_ALGORITHM_SHA256withRSA, IBMJCEPlusFIPS_NAME);
-		} else if (LTPAKeyUtil.isIBMJCEAvailable()) {
-			rsaSig = Signature.getInstance(SIGNATURE_ALGORITHM, IBMJCE_NAME);
-		} else {
-			rsaSig = Signature.getInstance(SIGNATURE_ALGORITHM);
-		}
+		rsaSig = (provider == null) ? Signature.getInstance(signatureAlgorithm)
+				: Signature.getInstance(signatureAlgorithm, provider);
 
 		rsaSig.initVerify(pubKey);
 		rsaSig.update(data, off, len);
@@ -566,8 +549,7 @@ final class LTPACrypto {
 	/**
 	 * Set the key for RSA algorithms.
 	 *
-	 * @param key
-	 *            The key
+	 * @param key The key
 	 */
 	@Trivial
 	protected static final void setRSAKey(byte[][] key) {
@@ -628,13 +610,8 @@ final class LTPACrypto {
 			DESedeKeySpec kSpec = new DESedeKeySpec(key);
 			SecretKeyFactory kFact = null;
 
-			if (LTPAKeyUtil.isFIPSEnabled() && LTPAKeyUtil.isIBMJCEPlusFIPSAvailable()) {
-				kFact = SecretKeyFactory.getInstance(ENCRYPT_ALGORITHM_RSA, IBMJCEPlusFIPS_NAME);
-			} else if (LTPAKeyUtil.isIBMJCEAvailable()) {
-				kFact = SecretKeyFactory.getInstance(ENCRYPT_ALGORITHM, IBMJCE_NAME);
-			} else {
-				kFact = SecretKeyFactory.getInstance(ENCRYPT_ALGORITHM);
-			}
+			kFact = (provider == null) ? SecretKeyFactory.getInstance(encryptAlgorithm)
+					: SecretKeyFactory.getInstance(encryptAlgorithm, provider);
 
 			sKey = kFact.generateSecret(kSpec);
 		}
@@ -657,13 +634,7 @@ final class LTPACrypto {
 			InvalidAlgorithmParameterException, NoSuchProviderException {
 
 		Cipher ci = null;
-		if (LTPAKeyUtil.isFIPSEnabled() && LTPAKeyUtil.isIBMJCEPlusFIPSAvailable()) {
-			ci = Cipher.getInstance(cipher, IBMJCEPlusFIPS_NAME);
-		} else if (LTPAKeyUtil.isIBMJCEAvailable()) {
-			ci = Cipher.getInstance(cipher, IBMJCE_NAME);
-		} else {
-			ci = Cipher.getInstance(cipher);
-		}
+		ci = (provider == null) ? Cipher.getInstance(cipher) : Cipher.getInstance(cipher, provider);
 
 		if (cipher.indexOf("ECB") == -1) {
 			if (cipher.indexOf("AES") != -1) {
@@ -686,12 +657,9 @@ final class LTPACrypto {
 	/**
 	 * Encrypt the data.
 	 *
-	 * @param data
-	 *            The byte representation of the data
-	 * @param key
-	 *            The key used to encrypt the data
-	 * @param cipher
-	 *            The cipher algorithm
+	 * @param data   The byte representation of the data
+	 * @param key    The key used to encrypt the data
+	 * @param cipher The cipher algorithm
 	 * @return The encrypted data (ciphertext)
 	 */
 	@Trivial
@@ -704,12 +672,9 @@ final class LTPACrypto {
 	/**
 	 * Decrypt the specified msg.
 	 *
-	 * @param msg
-	 *            The byte representation of the data
-	 * @param key
-	 *            The key used to decrypt the data
-	 * @param cipher
-	 *            The cipher algorithm
+	 * @param msg    The byte representation of the data
+	 * @param key    The key used to decrypt the data
+	 * @param cipher The cipher algorithm
 	 * @return The decrypted data (plaintext)
 	 */
 	@Trivial
@@ -760,7 +725,8 @@ final class LTPACrypto {
 			ivs16 = new IvParameterSpec(iv16);
 		}
 	}
-    @Trivial
+
+	@Trivial
 	static final int lsbf(byte[] data, int i, int n) {
 		int v = 0;
 		do {
@@ -768,39 +734,47 @@ final class LTPACrypto {
 		} while (n > 0);
 		return v;
 	}
-    @Trivial
+
+	@Trivial
 	static final int lsbf4(byte[] data, int i) {
 		return (data[i] & 0xFF) | ((data[i + 1] & 0xFF) << 8) | ((data[i + 2] & 0xFF) << 16) | (data[i + 3] << 24);
 	}
-    @Trivial
+
+	@Trivial
 	static final void lsbf4(int v, byte[] data, int i) {
 		data[i] = (byte) v;
 		data[i + 1] = (byte) (v >>> 8);
 		data[i + 2] = (byte) (v >>> 16);
 		data[i + 3] = (byte) (v >>> 24);
 	}
-    @Trivial
+
+	@Trivial
 	static void lsbf2(int v, byte[] data, int i) {
 		data[i] = (byte) v;
 		data[i + 1] = (byte) (v >>> 8);
 	}
-    @Trivial
+
+	@Trivial
 	private static final int FF(int a, int b, int c, int d, int x, int l, int r, int ac) {
 		return (((a += ((b & c) | (~b & d)) + x + ac) << l) | (a >>> r)) + b;
 	}
-    @Trivial
+
+	@Trivial
 	private static final int GG(int a, int b, int c, int d, int x, int l, int r, int ac) {
 		return (((a += ((b & d) | (c & ~d)) + x + ac) << l) | (a >>> r)) + b;
 	}
-    @Trivial
+
+	@Trivial
 	private static final int HH(int a, int b, int c, int d, int x, int l, int r, int ac) {
 		return (((a += (b ^ c ^ d) + x + ac) << l) | (a >>> r)) + b;
 	}
-    @Trivial
+
+	@Trivial
 	private static final int II(int a, int b, int c, int d, int x, int l, int r, int ac) {
 		return (((a += (c ^ (b | ~d)) + x + ac) << l) | (a >>> r)) + b;
 	}
-    @Trivial
+
+	@Trivial
 	static final void md5(int[] state, byte[] data, int off, int len, byte[] to, int pos) {
 		int a, b, c, d;
 		{
@@ -938,6 +912,7 @@ final class LTPACrypto {
 	private static int[] samples = new int[56];
 	private static int[] ones = new int[16];
 	private static int[] block = new int[16];
+
 	@Trivial
 	static final void trng(byte[] to, int off, int len) {
 		long accu = 0;
@@ -1022,6 +997,7 @@ final class LTPACrypto {
 
 	static byte[][][] rsaKeys;
 	static byte[][][] dsaKeys;
+
 	@Trivial
 	static final void random(byte[] to, int off, int n) {
 		if (!seedInitialized) {
@@ -1067,7 +1043,8 @@ final class LTPACrypto {
 			}
 		}
 	}
-    @Trivial
+
+	@Trivial
 	static final byte[] generate3DESKey() {
 		byte[] rndSeed = null;
 		int len = 24; // 3DES
@@ -1075,21 +1052,16 @@ final class LTPACrypto {
 		random(rndSeed, 0, len);
 		return rndSeed;
 	}
-    @Trivial
+
+	@Trivial
 	static final byte[][] rsaKey(int len, boolean crt, boolean f4) {
 		byte[][] key = new byte[crt ? 8 : 3][];
 		KeyPair pair = null;
 		KeyPairGenerator keyGen = null;
 		try {
 
-      if (LTPAKeyUtil.isFIPSEnabled() && LTPAKeyUtil.isIBMJCEPlusFIPSAvailable()) {
-        keyGen = KeyPairGenerator.getInstance(CRYPTO_ALGORITHM, IBMJCEPlusFIPS_NAME);
-      } else if (LTPAKeyUtil.isIBMJCEAvailable()) {
-				// IBMJCE_NAME needed for hardware crypto
-				keyGen = KeyPairGenerator.getInstance(CRYPTO_ALGORITHM, IBMJCE_NAME);
-			} else {
-				keyGen = KeyPairGenerator.getInstance(CRYPTO_ALGORITHM);
-			}
+			keyGen = (provider == null) ? KeyPairGenerator.getInstance(CRYPTO_ALGORITHM_RSA)
+					: KeyPairGenerator.getInstance(CRYPTO_ALGORITHM_RSA, provider);
 
 			keyGen.initialize(len * 8, new SecureRandom());
 			pair = keyGen.generateKeyPair();
@@ -1120,10 +1092,11 @@ final class LTPACrypto {
 		} catch (java.security.NoSuchProviderException e) {
 			// instrumented ffdc
 		} catch (java.lang.UnsupportedOperationException uoe) {
-			//This is when hard ware crypto provider is at the top of java.security 
-			//Using the different key creation routines.
-		    System.out.println("DEBUG: UnsupportedOperationException is caught!! Going back to the previous hardware crypto routine for evaluation.");
-            BigInteger p, q, n, d;
+			// This is when hard ware crypto provider is at the top of java.security
+			// Using the different key creation routines.
+			System.out.println(
+					"DEBUG: UnsupportedOperationException is caught!! Going back to the previous hardware crypto routine for evaluation.");
+			BigInteger p, q, n, d;
 			BigInteger e = BigInteger.valueOf(f4 ? 0x10001 : 3);
 			BigInteger one = BigInteger.valueOf(1), two = BigInteger.valueOf(2);
 			byte[] b = new byte[(len /= 2) + 1];
@@ -1181,25 +1154,41 @@ final class LTPACrypto {
 				key[7] = q.modInverse(p).toByteArray(); // getCrtCoefficient /
 			}
 		}
-		
 
 		return key;
 	}
-	
-	private static String getProvider(){
+
+	private static String getProvider() {
 		String provider = null;
-		if (LTPAKeyUtil.isIBMJCEAvailable()) {
+		if (LTPAKeyUtil.isFIPSEnabled() && LTPAKeyUtil.isIBMJCEPlusFIPSAvailable()) {
+			provider = IBMJCE_PLUS_FIPS_NAME;
+		} else if (LTPAKeyUtil.isIBMJCEAvailable()) {
 			provider = IBMJCE_NAME;
-		} else if(LTPAKeyUtil.isZOSandRunningJava11orHigher() && LTPAKeyUtil.isOpenJCEPlusAvailable()){
-			provider = OPENJCEPLUS_NAME;
+		} else if (LTPAKeyUtil.isZOSandRunningJava11orHigher() && LTPAKeyUtil.isOpenJCEPlusAvailable()) {
+			provider = OPENJCE_PLUS_NAME;
 		}
 		if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
-		if (provider == null){
-			Tr.debug(tc, "getProvider" + " Provider configured by JDK" );
+			if (provider == null) {
+				Tr.debug(tc, "getProvider" + " Provider configured by JDK");
+			} else {
+				Tr.debug(tc, "getProvider" + " Provider configured is " + provider);
+			}
 		}
-		else{
-			Tr.debug(tc, "getProvider" + " Provider configured is "+ provider );
-		}}
 		return provider;
 	}
+
+	private static String getSignatureAlgorithm() {
+		if (LTPAKeyUtil.isFIPSEnabled() && LTPAKeyUtil.isIBMJCEPlusFIPSAvailable())
+			return SIGNATURE_ALGORITHM_SHA256WITHRSA;
+		else
+			return SIGNATURE_ALGORITHM_SHA1WITHRSA;
+	}
+
+	private static String getEncryptAlgorithm() {
+		if (LTPAKeyUtil.isFIPSEnabled() && LTPAKeyUtil.isIBMJCEPlusFIPSAvailable())
+			return ENCRYPT_ALGORITHM_RSA;
+		else
+			return ENCRYPT_ALGORITHM_DESEDE;
+	}
+
 }
--- dev/com.ibm.ws.crypto.ltpakeyutil/src/com/ibm/ws/crypto/ltpakeyutil/LTPAKeyUtil.java
+++ dev/com.ibm.ws.crypto.ltpakeyutil/src/com/ibm/ws/crypto/ltpakeyutil/LTPAKeyUtil.java
@@ -14,33 +14,30 @@ package com.ibm.ws.crypto.ltpakeyutil;
 
 import java.security.AccessController;
 import java.security.PrivilegedAction;
-import java.security.PrivilegedActionException;
-import java.security.Provider;
-import java.security.Security;
+
 import com.ibm.ws.kernel.service.util.JavaInfo;
 
 public final class LTPAKeyUtil {
 
 	public static boolean ibmJCEAvailable = false;
 	public static boolean ibmJCEPlusFIPSAvailable = false;
+	public static boolean openJCEPlusAvailable = false;
 	public static boolean providerChecked = false;
-	public static String IBM_JCE_PROVIDER = "com.ibm.crypto.provider.IBMJCE";
-	public static String IBM_JCE_Plus_FIPS_PROVIDER = "com.ibm.crypto.provider.IBMJCEPlusFIPS";
-  public static String OPENJCEPLUS_PROVIDER = "com.ibm.crypto.plus.provider.OpenJCEPlus";
-  public static boolean openJCEPlusAvailable = false;
-  public static boolean zosProviderChecked = false;
+	public static boolean zosProviderChecked = false;
 
-  public static boolean javaVersionChecked = false;
-  public static boolean isJava11orHigher = false;
+	public static boolean javaVersionChecked = false;
+	public static boolean isJava11orHigher = false;
 
-  public static boolean zOSAndJAVA11orHigherChecked = false; 
-  public static boolean iszOSAndJava11orHigher = false; 
+	public static boolean zOSAndJAVA11orHigherChecked = false;
+	public static boolean iszOSAndJava11orHigher = false;
 
-  public static String osName = System.getProperty("os.name");
-  public static boolean isZOS = false;
-  public static boolean osVersionChecked = false;
+	public static String osName = System.getProperty("os.name");
+	public static boolean isZOS = false;
+	public static boolean osVersionChecked = false;
 
-  
+	public static String IBMJCE_PROVIDER = "com.ibm.crypto.provider.IBMJCE";
+	public static String IBMJCE_PLUS_FIPS_PROVIDER = "com.ibm.crypto.provider.IBMJCEPlusFIPS";
+	public static String OPENJCE_PLUS_PROVIDER = "com.ibm.crypto.plus.provider.OpenJCEPlus";
 
 	public static byte[] encrypt(byte[] data, byte[] key, String cipher) throws Exception {
 		return LTPACrypto.encrypt(data, key, cipher);
@@ -62,7 +59,6 @@ public final class LTPAKeyUtil {
 	public static void setRSAKey(byte[][] key) {
 		LTPACrypto.setRSAKey(key);
 	}
- 
 
 	public static byte[][] getRawKey(LTPAPrivateKey privKey) {
 		return privKey.getRawKey();
@@ -84,11 +80,10 @@ public final class LTPAKeyUtil {
 		if (providerChecked) {
 			return ibmJCEAvailable;
 		} else {
-			ibmJCEAvailable = JavaInfo.isSystemClassAvailable(IBM_JCE_PROVIDER);
+			ibmJCEAvailable = JavaInfo.isSystemClassAvailable(IBMJCE_PROVIDER);
 			providerChecked = true;
 			return ibmJCEAvailable;
 		}
-
 	}
 
 	public static boolean isIBMJCEPlusFIPSAvailable() {
@@ -96,31 +91,22 @@ public final class LTPAKeyUtil {
 			return ibmJCEPlusFIPSAvailable;
 		} else {
 
-
-                    String ibmjceplusfipsprovider = AccessController.doPrivileged(new PrivilegedAction<String>() {
-                    @Override
-                    public String run() {
-                            return System.getProperty("com.ibm.jsse2.usefipsProviderName");
-                    }
-                     });
-                    if (ibmjceplusfipsprovider == "IBMJCEPlusFIPS") {
-                         ibmJCEPlusFIPSAvailable = true;
-                         providerChecked = true;
-                         return ibmJCEPlusFIPSAvailable;
-                     } else {
-                         return false;
-                     }             
+			String ibmjceplusfipsprovider = AccessController.doPrivileged(new PrivilegedAction<String>() {
+				@Override
+				public String run() {
+					return System.getProperty("com.ibm.jsse2.usefipsProviderName");
+				}
+			});
+			if (ibmjceplusfipsprovider == "IBMJCEPlusFIPS") {
+				ibmJCEPlusFIPSAvailable = true;
+				providerChecked = true;
+				return ibmJCEPlusFIPSAvailable;
+			} else {
+				return false;
+			}
 		}
 
 	}
-  public static boolean isIBMJCEAvailable() {
-    if (providerChecked) {
-      return ibmJCEAvailable;
-    } else {
-      ibmJCEAvailable = JavaInfo.isSystemClassAvailable(IBM_JCE_PROVIDER);
-      providerChecked = true;
-      return ibmJCEAvailable;
-    }
 
 	public static boolean isFIPSEnabled() {
 		String fipsON = AccessController.doPrivileged(new PrivilegedAction<String>() {
@@ -129,54 +115,52 @@ public final class LTPAKeyUtil {
 				return System.getProperty("com.ibm.jsse2.usefipsprovider");
 			}
 		});
-                if (fipsON == "true") {
-                    return true;
-                } else {
-                    return false;
-                }             
+		if (fipsON == "true") {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-}
-  public static boolean isOpenJCEPlusAvailable() {
-    if (zosProviderChecked) {
-      return openJCEPlusAvailable;
-    } else {
-      openJCEPlusAvailable = JavaInfo.isSystemClassAvailable(OPENJCEPLUS_PROVIDER);
-      zosProviderChecked = true;
-      return openJCEPlusAvailable;
-    }
+	public static boolean isOpenJCEPlusAvailable() {
+		if (zosProviderChecked) {
+			return openJCEPlusAvailable;
+		} else {
+			openJCEPlusAvailable = JavaInfo.isSystemClassAvailable(OPENJCE_PLUS_PROVIDER);
+			zosProviderChecked = true;
+			return openJCEPlusAvailable;
+		}
 
-  }
+	}
 
-  private static boolean isJava11orHigher() {
-    if (javaVersionChecked) {
-      return isJava11orHigher;
-    } else {
-      isJava11orHigher = JavaInfo.majorVersion() >= 11;
-      javaVersionChecked = true;
-      return isJava11orHigher;
-    }
-  }
+	private static boolean isJava11orHigher() {
+		if (javaVersionChecked) {
+			return isJava11orHigher;
+		} else {
+			isJava11orHigher = JavaInfo.majorVersion() >= 11;
+			javaVersionChecked = true;
+			return isJava11orHigher;
+		}
+	}
 
-  private static boolean isZOS() {
-    if (osVersionChecked) {
-      return isZOS;
-    } else {
-      isZOS = (osName.equalsIgnoreCase("z/OS") || osName.equalsIgnoreCase("OS/390"));
-      osVersionChecked = true;
-      return isZOS;
-    }
-  }
+	private static boolean isZOS() {
+		if (osVersionChecked) {
+			return isZOS;
+		} else {
+			isZOS = (osName.equalsIgnoreCase("z/OS") || osName.equalsIgnoreCase("OS/390"));
+			osVersionChecked = true;
+			return isZOS;
+		}
+	}
 
-  public static boolean isZOSandRunningJava11orHigher() {
-    if (zOSAndJAVA11orHigherChecked){
-      return iszOSAndJava11orHigher;
-    }
-    else {
-      iszOSAndJava11orHigher = isJava11orHigher() && isZOS();
-      zOSAndJAVA11orHigherChecked = true;
-      return iszOSAndJava11orHigher;
-    }
-  }
+	public static boolean isZOSandRunningJava11orHigher() {
+		if (zOSAndJAVA11orHigherChecked) {
+			return iszOSAndJava11orHigher;
+		} else {
+			iszOSAndJava11orHigher = isJava11orHigher() && isZOS();
+			zOSAndJAVA11orHigherChecked = true;
+			return iszOSAndJava11orHigher;
+		}
+	}
 
 }
