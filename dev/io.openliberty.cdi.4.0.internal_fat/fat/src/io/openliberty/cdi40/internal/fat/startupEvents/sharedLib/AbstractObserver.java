4825cf51d6e554d8ebebfc36b44170ec54c93615
Benjamin Confino <benjamic@uk.ibm.com>
2023-10-24 17:28:35 +0100

    test for jndi during startup event

--- dev/io.openliberty.cdi.4.0.internal_fat/fat/src/io/openliberty/cdi40/internal/fat/startupEvents/sharedLib/AbstractObserver.java
+++ dev/io.openliberty.cdi.4.0.internal_fat/fat/src/io/openliberty/cdi40/internal/fat/startupEvents/sharedLib/AbstractObserver.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -13,7 +13,12 @@
 package io.openliberty.cdi40.internal.fat.startupEvents.sharedLib;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
+import java.util.Set;
+
+import javax.naming.InitialContext;
 
 import jakarta.annotation.Priority;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -22,6 +27,10 @@ import jakarta.enterprise.context.Initialized;
 import jakarta.enterprise.event.Observes;
 import jakarta.enterprise.event.Shutdown;
 import jakarta.enterprise.event.Startup;
+import jakarta.enterprise.inject.spi.Bean;
+import jakarta.enterprise.inject.spi.BeanManager;
+import jakarta.enterprise.inject.spi.CDI;
+import jakarta.inject.Inject;
 
 /**
  * The expected order of events is...
@@ -55,24 +64,28 @@ public abstract class AbstractObserver {
         }
     }
 
-    public void observeInit(@Observes @Initialized(ApplicationScoped.class) Object event) {
+    public void observeInit(@Observes @Initialized(ApplicationScoped.class) Object event) throws Exception {
         trace("observeInit");
         initObserved = true;
         //@Initialized(ApplicationScoped.class) should be seen before Startup
         assertFalse(logMsg("@Initialized(ApplicationScoped.class) was observed after Startup"), startupObserved1);
         //@Initialized(ApplicationScoped.class) should be seen before any request
         assertFalse(logMsg("@Initialized(ApplicationScoped.class) was observed after test"), testRequested);
+
+        runAllJNDITests();
     }
 
     //note the prefixes on these method names and the order they appear in the class is
     //designed to try and make sure that it is the Priority that decides the order, not some other factor
-    public void a_observeStartup2(@Observes @Priority(2) Startup startupEvent) {
+    public void a_observeStartup2(@Observes @Priority(2) Startup startupEvent) throws Exception {
         trace("a_observeStartup2");
         startupObserved2 = true;
         //@Priority(1) Startup should be seen before @Priority(2) Startup
         assertTrue(logMsg("Startup 2 was observed before Startup 1"), startupObserved1);
         //Startup 2 should be seen before any request
         assertFalse(logMsg("Startup 2 was observed after test"), testRequested);
+
+        runAllJNDITests();
     }
 
     public void c_observeStartup1(@Observes @Priority(1) Startup startupEvent) {
@@ -103,7 +116,7 @@ public abstract class AbstractObserver {
         assertFalse(logMsg("Shutdown observed before test requested"), shutdownObserved);
     }
 
-    public void observeShutdown(@Observes Shutdown shutdownEvent) {
+    public void observeShutdown(@Observes Shutdown shutdownEvent) throws Exception {
         trace("observeShutdown");
         shutdownObserved = true;
         //Shutdown should be seen after any request
@@ -119,6 +132,52 @@ public abstract class AbstractObserver {
         assertTrue(logMsg("@BeforeDestroyed(ApplicationScoped.class) was observed before Shutdown"), shutdownObserved);
         //@BeforeDestroyed(ApplicationScoped.class) should be seen after any request
         assertTrue(logMsg("Shutdown was observed before test"), testRequested);
+    }
+
+    // These tests use JNDI because that had known issues within the context of CDI lifecycle events
+
+    @Inject
+    JNDILookupBean myAppScopedBean;
+
+    @Inject
+    BeanManager injectedBeanManager;
+
+    private void runAllJNDITests() throws Exception {
+        testCDICurrent();
+        testServletJNDILookup();
+        testInjected();
+        testBeanJNDILookup();
+    }
+
+    public void testCDICurrent() throws Exception {
+        CDI cdi = CDI.current();
+        BeanManager cdiCurrentBeanManager = cdi.getBeanManager();
+        testBeanManager("BeanManager from CDI.current()", cdiCurrentBeanManager);
+    }
+
+    public void testServletJNDILookup() throws Exception {
+        BeanManager jndiLookupBeanManager = getBeanMangerViaJNDI();
+        testBeanManager("BeanManager from Servlet JNDI lookup", jndiLookupBeanManager);
+    }
+
+    public void testInjected() throws Exception {
+        testBeanManager("BeanManager injected into AbstractObserver", injectedBeanManager);
+    }
+
+    public void testBeanJNDILookup() throws Exception {
+        BeanManager jndiLookupBeanManager = myAppScopedBean.getBeanMangerViaJNDI();
+        testBeanManager("BeanManager from Bean JNDI lookup", jndiLookupBeanManager);
+    }
+
+    public void testBeanManager(String beanManagerDescription, BeanManager beanManager) {
+        assertNotNull(beanManagerDescription + " was null", beanManager);
+
+        Set<Bean<?>> set = beanManager.getBeans(JNDILookupBean.class);
+        assertFalse(beanManagerDescription + " could not find any Beans for class MyBean", set.isEmpty());
+    }
+
+    private BeanManager getBeanMangerViaJNDI() throws Exception {
+        return (BeanManager) new InitialContext().lookup("java:comp/BeanManager");
     }
 
 }
