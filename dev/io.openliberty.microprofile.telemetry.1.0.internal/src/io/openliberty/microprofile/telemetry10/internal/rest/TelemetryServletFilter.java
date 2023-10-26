9825c3a999cde408d9432b3bee19bb74eb9c5a22
Thomas Watson <tjwatson@us.ibm.com>
2023-10-26 10:05:59 -0500

    Lazily create instrumenter for InstantOn
    This avoids reading config too early on checkpoint

--- dev/io.openliberty.microprofile.telemetry.1.0.internal/src/io/openliberty/microprofile/telemetry10/internal/rest/TelemetryServletFilter.java
+++ dev/io.openliberty.microprofile.telemetry.1.0.internal/src/io/openliberty/microprofile/telemetry10/internal/rest/TelemetryServletFilter.java
@@ -16,6 +16,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
@@ -23,6 +25,7 @@ import org.eclipse.microprofile.config.ConfigProvider;
 import com.ibm.websphere.ras.Tr;
 import com.ibm.websphere.ras.TraceComponent;
 
+import io.openliberty.checkpoint.spi.CheckpointPhase;
 import io.openliberty.microprofile.telemetry.internal.common.AgentDetection;
 import io.openliberty.microprofile.telemetry.internal.common.info.OpenTelemetryInfo;
 import io.openliberty.microprofile.telemetry.internal.common.rest.AbstractTelemetryServletFilter;
@@ -64,6 +67,8 @@ public class TelemetryServletFilter extends AbstractTelemetryServletFilter imple
     private static final NetServerAttributesGetterImpl NET_SERVER_ATTRIBUTES_GETTER = new NetServerAttributesGetterImpl();
 
     private Instrumenter<ServletRequest, ServletResponse> instrumenter;
+    private volatile boolean lazyCreate = false;
+    private final AtomicReference<Instrumenter<ServletRequest, ServletResponse>> lazyInstrumenter = new AtomicReference<>();
 
     private final Config config = ConfigProvider.getConfig();
 
@@ -73,44 +78,70 @@ public class TelemetryServletFilter extends AbstractTelemetryServletFilter imple
     @Override
     public void init(FilterConfig config) {
         if (instrumenter == null) {
-            OpenTelemetryInfo otelInfo = OpenTelemetryAccessor.getOpenTelemetryInfo();
-            if (tc.isDebugEnabled()) {
-                Tr.debug(tc, "otelInfo.getEnabled()=" + otelInfo.getEnabled());
-            }
-            if (otelInfo != null &&
-                otelInfo.getEnabled() &&
-                !AgentDetection.isAgentActive() &&
-                !checkDisabled(getTelemetryProperties())) {
-                InstrumenterBuilder<ServletRequest, ServletResponse> builder = Instrumenter.builder(
-                                                                                                    otelInfo.getOpenTelemetry(),
-                                                                                                    INSTRUMENTATION_NAME,
-                                                                                                    HttpSpanNameExtractor.create(HTTP_SERVER_ATTRIBUTES_GETTER));
-
-                instrumenter = builder.setSpanStatusExtractor(HttpSpanStatusExtractor.create(HTTP_SERVER_ATTRIBUTES_GETTER))
-                                .addAttributesExtractor(HttpServerAttributesExtractor.create(HTTP_SERVER_ATTRIBUTES_GETTER))
-                                .addAttributesExtractor(NetServerAttributesExtractor.create(NET_SERVER_ATTRIBUTES_GETTER))
-                                .buildServerInstrumenter(new ServletRequestContextTextMapGetter());
-                if (tc.isDebugEnabled()) {
-                    Tr.debug(tc, "instrumenter is initialized");
-                }
+            if (!CheckpointPhase.getPhase().restored()) {
+                lazyCreate = true;
             } else {
-                instrumenter = null;
-                if (tc.isDebugEnabled()) {
-                    Tr.debug(tc, "instrumenter is set to null");
-                }
+                instrumenter = createInstrumenter();
             }
-
         }
+    }
+
+    private Instrumenter<ServletRequest, ServletResponse> getInstrumenter() {
+        if (instrumenter != null) {
+            return instrumenter;
+        }
+        if (lazyCreate) {
+            instrumenter = lazyInstrumenter.updateAndGet((i) -> {
+                if (i == null) {
+                    return createInstrumenter();
+                } else {
+                    return i;
+                }
+            });
+        }
+        return instrumenter;
+    }
+
+    private Instrumenter<ServletRequest, ServletResponse> createInstrumenter() {
+        OpenTelemetryInfo otelInfo = OpenTelemetryAccessor.getOpenTelemetryInfo();
+        if (tc.isDebugEnabled()) {
+            Tr.debug(tc, "otelInfo.getEnabled()=" + otelInfo.getEnabled());
+        }
+        if (otelInfo != null &&
+            otelInfo.getEnabled() &&
+            !AgentDetection.isAgentActive() &&
+            !checkDisabled(getTelemetryProperties())) {
+            InstrumenterBuilder<ServletRequest, ServletResponse> builder = Instrumenter.builder(
+                                                                                                otelInfo.getOpenTelemetry(),
+                                                                                                INSTRUMENTATION_NAME,
+                                                                                                HttpSpanNameExtractor.create(HTTP_SERVER_ATTRIBUTES_GETTER));
+
+            Instrumenter<ServletRequest, ServletResponse> result = builder.setSpanStatusExtractor(HttpSpanStatusExtractor.create(HTTP_SERVER_ATTRIBUTES_GETTER))
+                            .addAttributesExtractor(HttpServerAttributesExtractor.create(HTTP_SERVER_ATTRIBUTES_GETTER))
+                            .addAttributesExtractor(NetServerAttributesExtractor.create(NET_SERVER_ATTRIBUTES_GETTER))
+                            .buildServerInstrumenter(new ServletRequestContextTextMapGetter());
+            if (tc.isDebugEnabled()) {
+                Tr.debug(tc, "instrumenter is initialized");
+            }
+            return result;
+        } else {
+            if (tc.isDebugEnabled()) {
+                Tr.debug(tc, "instrumenter is set to null");
+            }
+            return null;
+        }
+
     }
 
     /** {@inheritDoc} */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         Scope scope = null;
-        if (instrumenter != null) {
+        Instrumenter<ServletRequest, ServletResponse> current = getInstrumenter();
+        if (current != null) {
             Context parentContext = Context.current();
-            if (instrumenter.shouldStart(parentContext, request)) {
-                Context spanContext = instrumenter.start(parentContext, request);
+            if (current.shouldStart(parentContext, request)) {
+                Context spanContext = current.start(parentContext, request);
                 scope = spanContext.makeCurrent();
                 request.setAttribute(SPAN_CONTEXT, spanContext);
                 request.setAttribute(SPAN_PARENT_CONTEXT, parentContext);
@@ -134,17 +165,17 @@ public class TelemetryServletFilter extends AbstractTelemetryServletFilter imple
             asyncContext.addListener(new AsyncListener() {
                 @Override
                 public void onComplete(AsyncEvent event) throws IOException {
-                    endSpan(request, response, null);
+                    endSpan(request, response, null, current);
                 }
 
                 @Override
                 public void onTimeout(AsyncEvent event) throws IOException {
-                    endSpan(request, response, event.getThrowable());
+                    endSpan(request, response, event.getThrowable(), current);
                 }
 
                 @Override
                 public void onError(AsyncEvent event) throws IOException {
-                    endSpan(request, response, event.getThrowable());
+                    endSpan(request, response, event.getThrowable(), current);
                 }
 
                 @Override
@@ -152,7 +183,7 @@ public class TelemetryServletFilter extends AbstractTelemetryServletFilter imple
                 }
             });
         } else {
-            endSpan(request, response, null);
+            endSpan(request, response, null, current);
         }
 
         if (scope != null) {
@@ -162,23 +193,21 @@ public class TelemetryServletFilter extends AbstractTelemetryServletFilter imple
 
     }
 
-    private void endSpan(ServletRequest request, ServletResponse response, Throwable throwable) {
-        if (instrumenter != null) {
-            Context spanContext = (Context) request.getAttribute(SPAN_CONTEXT);
-            if (spanContext == null) {
-                return;
-            }
+    private void endSpan(ServletRequest request, ServletResponse response, Throwable throwable, Instrumenter<ServletRequest, ServletResponse> current) {
+        Context spanContext = (Context) request.getAttribute(SPAN_CONTEXT);
+        if (spanContext == null) {
+            return;
+        }
 
-            try {
-                if (tc.isDebugEnabled()) {
-                    Tr.debug(tc, "End span traceId=" + Span.fromContext(spanContext).getSpanContext().getTraceId() + " spanId="
-                                 + Span.fromContext(spanContext).getSpanContext().getSpanId());
-                }
-                instrumenter.end(spanContext, request, response, throwable);
-            } finally {
-                request.removeAttribute(SPAN_CONTEXT);
-                request.removeAttribute(SPAN_PARENT_CONTEXT);
+        try {
+            if (tc.isDebugEnabled()) {
+                Tr.debug(tc, "End span traceId=" + Span.fromContext(spanContext).getSpanContext().getTraceId() + " spanId="
+                             + Span.fromContext(spanContext).getSpanContext().getSpanId());
             }
+            current.end(spanContext, request, response, throwable);
+        } finally {
+            request.removeAttribute(SPAN_CONTEXT);
+            request.removeAttribute(SPAN_PARENT_CONTEXT);
         }
     }
 
