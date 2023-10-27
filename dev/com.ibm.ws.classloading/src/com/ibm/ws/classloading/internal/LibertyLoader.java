8fd8d871b3a9c343d000898759d72917eb1fdbad
Jared Anderson <jhanders@us.ibm.com>
2023-10-27 15:45:35 -0500

    Update parallel classloader locking for Liberty classloaders
    - Update to use our concurrent weak locking store for LibertyLoader
    locks instead of what Java provides.  Since our implementation is
    concurrent and weak we have better concurrency and use less memory
    - Update the locking store so that it uses the generic type in the
    methods and data structures instead of assuming that the key is a String
    - Update WeakLibraryListener to reference AppClassLoader instead of
    Object to avoid unnecessary instanceof checks and casting for no reason.

--- dev/com.ibm.ws.classloading/src/com/ibm/ws/classloading/internal/LibertyLoader.java
+++ dev/com.ibm.ws.classloading/src/com/ibm/ws/classloading/internal/LibertyLoader.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2022 IBM Corporation and others.
+ * Copyright (c) 2014, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -16,10 +16,12 @@ import java.io.IOException;
 import java.net.URL;
 import java.security.SecureClassLoader;
 import java.util.Enumeration;
+import java.util.function.Supplier;
 
 import org.osgi.framework.Bundle;
 
 import com.ibm.ws.classloading.LibertyClassLoader;
+import com.ibm.ws.kernel.service.util.KeyBasedLockStore;
 
 public abstract class LibertyLoader extends SecureClassLoader implements NoClassNotFoundLoader, LibertyClassLoader, DeclaredApiAccess {
     static {
@@ -31,6 +33,25 @@ public abstract class LibertyLoader extends SecureClassLoader implements NoClass
     public LibertyLoader(ClassLoader parent) {
         super(parent);
         this.parent = parent;
+    }
+
+    private static final class NameBasedClassLoaderLock {};
+
+    private final KeyBasedLockStore<String, NameBasedClassLoaderLock> classNameLockStore = new KeyBasedLockStore<>(new Supplier<NameBasedClassLoaderLock>() {
+        @Override
+        public NameBasedClassLoaderLock get() {
+            return new NameBasedClassLoaderLock();
+        }
+    });
+
+    /**
+     * Override the default Java implementation for this method because on HotSpot based implementations the collection of locks
+     * is a hard reference and bloats memory and on J9 based implementations the collection is a Hashtable which doesn't allow for
+     * concurrency when getting the lock.  With this implementation both issues (memory and concurrency) are handled. 
+     */
+    @Override
+    protected final Object getClassLoadingLock(String className) {
+        return classNameLockStore.getLock(className);
     }
 
     @Override
