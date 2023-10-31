aded11083f1bdc776c8ee8db1bd435c564672a92
Evan <evan2@us.ibm.com>
2023-10-31 14:50:36 -0400

    skip session context error
    Signed-off-by: Evan <evan2@us.ibm.com>

--- dev/com.ibm.ws.app.manager.wab.installer_fat/fat/src/com/ibm/ws/app/manager/wab/installer/fat/ConfigurableWABTests.java
+++ dev/com.ibm.ws.app.manager.wab.installer_fat/fat/src/com/ibm/ws/app/manager/wab/installer/fat/ConfigurableWABTests.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -68,7 +68,8 @@ public class ConfigurableWABTests extends AbstractWABTests {
         if (server != null && server.isStarted()) {
             // ignore conflict error from the conflict test
             // ignore warning about virtual host not found
-            server.stopServer("CWWKZ0208E", "SRVE9956W");
+            // ignore session context error
+            server.stopServer("CWWKZ0208E", "SRVE9956W", "SRVE8059E");
         }
     }
 
