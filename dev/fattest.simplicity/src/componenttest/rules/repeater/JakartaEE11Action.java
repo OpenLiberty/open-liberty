fae47cc46aaa35fc7d98c917b21a3be409ac2596
Jared Anderson <jhanders@us.ibm.com>
2023-11-02 16:43:49 -0500

    Update to only support jdbc-4.3 with EE 11.
    - Update jdbc-4.0, 4.1 and 4.2 features to not start with EE 11 features
    - Update EE 11 features to no longer have a preferred version of 4.2 and
    tolerate 4.3
    - Update JakartaEE11Action to remove jdbc versions that are not 4.3 and
    replace with 4.3.
    - Update appropriate jdbc FATs to have an EE 11 repeat

--- dev/fattest.simplicity/src/componenttest/rules/repeater/JakartaEE11Action.java
+++ dev/fattest.simplicity/src/componenttest/rules/repeater/JakartaEE11Action.java
@@ -105,6 +105,8 @@ class JakartaEE11Action extends JakartaEEAction {
                                                   "xmlWS-4.0"
     };
 
+    private static final Set<String> NON_EE11_JDBC_FEATURES = Collections.unmodifiableSet(new HashSet<>(Arrays.asList("jdbc-4.0", "jdbc-4.1", "jdbc-4.2")));
+
     public static final Set<String> EE11_FEATURE_SET = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(EE11_FEATURES_ARRAY)));
 
     JakartaEE11Action() {
@@ -115,6 +117,9 @@ class JakartaEE11Action extends JakartaEEAction {
         removeFeatures(EE8FeatureReplacementAction.EE8_FEATURE_SET);
         removeFeatures(JakartaEE9Action.EE9_FEATURE_SET);
         removeFeatures(JakartaEE10Action.EE10_FEATURE_SET);
+        // Update any previous jdbc versions to jdbc-4.3
+        addFeature("jdbc-4.3");
+        removeFeatures(NON_EE11_JDBC_FEATURES);
         forceAddFeatures(false);
         withMinJavaLevel(SEVersion.JAVA21);
         withID(EE11_ACTION_ID);
