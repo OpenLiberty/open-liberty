5f0278bf718129b45d31409d39e10e34fa9aed65
Gordon Hutchison <Gordon.Hutchison@gmail.com>
2023-11-03 11:54:40 +0000

    html unit casting issue fix
    Signed-off-by: Gordon Hutchison <Gordon.Hutchison@gmail.com>

--- dev/com.ibm.ws.grpc_fat/fat/src/com/ibm/ws/fat/grpc/ClientConfigTests.java
+++ dev/com.ibm.ws.grpc_fat/fat/src/com/ibm/ws/fat/grpc/ClientConfigTests.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -111,7 +111,7 @@ public class ClientConfigTests extends FATServletClient {
     @AfterClass
     public static void tearDown() throws Exception {
         Exception excep = null;
-        
+
         LOG.info("ClientConfigTests : tearDown() : serverConfigurationFile set to null");
         // Setting serverConfigurationFile to null forces a server.xml update (when GrpcTestUtils.setServerConfiguration() is first called) on the repeat run
         // If not set to null, test failures may occur (since the incorrect server.xml could be used)
@@ -142,7 +142,7 @@ public class ClientConfigTests extends FATServletClient {
 
     private static void stopClientServer() throws Exception {
         if (GrpcClientOnly != null && GrpcClientOnly.isStarted()) {
-            
+
             /*
              * CWWKG0083W: expected by testInvalidMaxInboundMessageSize due to invalid message size config
              * CWWKG0076W: expected when a previous config is still in use because an invalid config was rejected
@@ -660,7 +660,8 @@ public class ClientConfigTests extends FATServletClient {
 
             // submit to the grpcClient, and execute the RPC
             HtmlSubmitInput submitButton = form.getInputByName("submit");
-            page = submitButton.click();
+
+            submitButton.click();
 
             //Make sure the expected error is logged
             String hitMeta = GrpcClientOnly.waitForStringInLog("INTERNAL: http2 exception",
