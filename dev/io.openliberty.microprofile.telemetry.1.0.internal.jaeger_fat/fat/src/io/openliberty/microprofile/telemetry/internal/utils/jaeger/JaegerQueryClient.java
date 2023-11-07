2bc729fe753785690065b0afae982e1e82d96333
Andrew Rouse <anrouse@uk.ibm.com>
2023-11-07 14:57:46 +0000

    Emit actual when waiting for expected spans fails
    When we don't find the spans we're expecting in Jaeger, include any
    spans we actually found in the failure message.
    
    This helps debugging which span wasn't emitted, or which additional
    spans were created.

--- dev/io.openliberty.microprofile.telemetry.1.0.internal.jaeger_fat/fat/src/io/openliberty/microprofile/telemetry/internal/utils/jaeger/JaegerQueryClient.java
+++ dev/io.openliberty.microprofile.telemetry.1.0.internal.jaeger_fat/fat/src/io/openliberty/microprofile/telemetry/internal/utils/jaeger/JaegerQueryClient.java
@@ -195,7 +195,7 @@ public class JaegerQueryClient implements AutoCloseable {
                 throw new AssertionError("Interrupted while waiting for spans", e);
             }
         }
-        assertThat("Spans not found within timeout after 3 retries", result, waitCondition);
+        assertThat("Spans not found within timeout after 3 retries: " + result, result, waitCondition);
         return result;
     }
 
