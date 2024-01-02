ac01423984436734a3ebb91750277d742e73b44a
Nathan Rauh <nathan.rauh@us.ibm.com>
2024-01-02 16:28:57 -0600

    Issue #27205 scheduled asynchronous method with multiple schedules

--- dev/com.ibm.ws.concurrent_fat_schedasync/test-applications/SchedAsyncWeb/src/test/concurrency/schedasync/web/SchedAsyncAppScopedBean.java
+++ dev/com.ibm.ws.concurrent_fat_schedasync/test-applications/SchedAsyncWeb/src/test/concurrency/schedasync/web/SchedAsyncAppScopedBean.java
@@ -12,7 +12,9 @@
  *******************************************************************************/
 package test.concurrency.schedasync.web;
 
+import java.time.Month;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import jakarta.enterprise.concurrent.Asynchronous;
@@ -26,7 +28,8 @@ public class SchedAsyncAppScopedBean {
      * Runs every 5 seconds.
      *
      * @param countdown number of executions to stop after.
-     * @return null to continue with more executions; a completed future with the remaining count to stop.
+     * @return null to continue with more executions.
+     *         To stop, returns a completed future with the current time in nanoseconds.
      */
     @Asynchronous(runAt = @Schedule(hours = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
                                               12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 },
@@ -36,12 +39,12 @@ public class SchedAsyncAppScopedBean {
                                                 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
                                                 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59 },
                                     seconds = { 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55 }))
-    CompletableFuture<Integer> everyFiveSeconds(AtomicInteger countdown) {
+    CompletableFuture<Long> everyFiveSeconds(AtomicInteger countdown) {
         System.out.println("> everyFiveSeconds " + countdown);
 
-        CompletableFuture<Integer> result;
+        CompletableFuture<Long> result;
         if (countdown.decrementAndGet() == 0)
-            result = Asynchronous.Result.complete(countdown.get());
+            result = Asynchronous.Result.complete(System.nanoTime());
         else
             result = null;
 
@@ -49,4 +52,46 @@ public class SchedAsyncAppScopedBean {
         return result;
     }
 
+    /**
+     * Runs on seconds that are divisible by 2 or 3.
+     *
+     * @param maxExecutions  number of executions to stop after.
+     * @param executionCount for tracking the total number of executions.
+     * @return null to continue with more executions.
+     *         To stop, returns a completed future with the execution count and current time in nanoseconds.
+     */
+    @Asynchronous(runAt = { @Schedule(cron = "*/3 * * * * *"),
+                            @Schedule(cron = "*/2 * * * * *") })
+    CompletableFuture<long[]> everyThreeOrEvenSeconds(int maxExecutions, AtomicInteger executionCount) {
+        int count = executionCount.incrementAndGet();
+        System.out.println("> everyThreeOrEvenSeconds " + count);
+
+        CompletableFuture<long[]> result = null;
+        if (count >= maxExecutions) {
+            result = Asynchronous.Result.getFuture();
+            result.obtrudeValue(new long[] { count, System.nanoTime() });
+        }
+
+        System.out.println("< everyThreeOrEvenSeconds " + result);
+        return result;
+    }
+
+    /**
+     * Attempt to run on a day that doesn't exist: February 30th.
+     */
+    @Asynchronous(runAt = @Schedule(months = Month.FEBRUARY,
+                                    daysOfMonth = 30))
+    CompletionStage<String> runOnFebruary30() {
+        return Asynchronous.Result.complete("Should not be running runOnFebruary30!");
+    }
+
+    /**
+     * Specify an empty list of seconds to run at.
+     *
+     * @throws IllegalArgumentException for the invalid empty list value on the annotation.
+     */
+    @Asynchronous(runAt = @Schedule(seconds = {}))
+    void withEmptySeconds() {
+        System.out.println("Should not be running withEmptySeconds.");
+    }
 }
--- dev/com.ibm.ws.concurrent_fat_schedasync/test-applications/SchedAsyncWeb/src/test/concurrency/schedasync/web/SchedAsyncTestServlet.java
+++ dev/com.ibm.ws.concurrent_fat_schedasync/test-applications/SchedAsyncWeb/src/test/concurrency/schedasync/web/SchedAsyncTestServlet.java
@@ -15,7 +15,9 @@ package test.concurrency.schedasync.web;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.time.DateTimeException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -39,7 +41,9 @@ public class SchedAsyncTestServlet extends FATServlet {
     @Inject
     private SchedAsyncAppScopedBean bean;
 
-    private static CompletableFuture<Integer> cfEveryFiveSeconds3Times;
+    private static CompletableFuture<Long> cfEveryFiveSeconds3Times;
+
+    private static CompletableFuture<long[]> cfEveryThreeAndEvenSeconds8Times;
 
     /**
      * Nanoseconds at which the init method was invoked on this servlet.
@@ -55,19 +59,68 @@ public class SchedAsyncTestServlet extends FATServlet {
         init_ns = System.nanoTime();
 
         cfEveryFiveSeconds3Times = bean.everyFiveSeconds(new AtomicInteger(3));
+
+        cfEveryThreeAndEvenSeconds8Times = bean.everyThreeOrEvenSeconds(8, new AtomicInteger());
     }
 
     /**
-     * An asynchronous methods that is scheduled to run every 5 seconds for 3 executions
+     */
+    @Test
+    public void testEmptySeconds() throws Exception {
+        try {
+            bean.withEmptySeconds();
+            fail("Should not be able to schedule an asynchronous method with empty list of seconds.");
+        } catch (IllegalArgumentException x) {
+            // expected
+        }
+    }
+
+    /**
+     * An asynchronous method that is scheduled to run every 5 seconds for 3 executions
      * and then complete must run exactly 3 times.
      */
     @Test
     public void testEveryFiveSeconds3Times() throws Exception {
-        assertEquals(Integer.valueOf(0), cfEveryFiveSeconds3Times.get(TIMEOUT_NS, TimeUnit.NANOSECONDS));
+        Long timeOfFinalExecution = cfEveryFiveSeconds3Times.get(TIMEOUT_NS, TimeUnit.NANOSECONDS);
 
-        long elapsed = System.nanoTime() - init_ns;
+        long elapsed = timeOfFinalExecution - init_ns;
         if (elapsed < TimeUnit.SECONDS.toNanos(10L))
             fail("A task that runs every 5 seconds must not complete 3 executions in under 10 seconds. Elapsed nanoseconds: " + elapsed);
     }
 
+    /**
+     * An asynchronous method is scheduled to run on multiple schedules that combine every even second
+     * with every second that is divisible by 3, such that the task runs on seconds
+     * 00 xx 02 03 04 xx 06 xx 08 09 10 xx 12 xx 14 15 16 xx 18 xx 20 21 22 xx 24 xx 26 27 28 xx
+     * 30 xx 32 33 34 xx 36 xx 38 39 40 xx 42 xx 44 45 46 xx 48 xx 50 51 52 xx 54 xx 56 57 58 xx
+     * for a total of 8 executions and then stops running.
+     * Verify that the 8 executions complete no sooner than 10 seconds from when scheduled.
+     */
+    @Test
+    public void testEveryThreeAndEvenNumberedSeconds8Times() throws Exception {
+        long[] result = cfEveryThreeAndEvenSeconds8Times.get(TIMEOUT_NS, TimeUnit.NANOSECONDS);
+
+        long numExecutions = result[0];
+        long timeOfFinalExecution = result[1];
+
+        assertEquals(8L, numExecutions);
+
+        long elapsed = timeOfFinalExecution - init_ns;
+        if (elapsed < TimeUnit.SECONDS.toNanos(10L))
+            fail("A task that runs on seconds divisble by 2 or 3 must not complete 8 executions in under 10 seconds." +
+                 " Elapsed nanoseconds: " + elapsed);
+    }
+
+    /**
+     * Attempt to schedule an asynchronous method for a time that does not exist (February 30).
+     */
+    @Test
+    public void testScheduleForNonExistingTime() {
+        try {
+            CompletionStage<String> cs = bean.runOnFebruary30();
+            fail("Should not be able to schedule an asynchronous method for a non-existing time: " + cs);
+        } catch (DateTimeException x) {
+            // expected
+        }
+    }
 }
