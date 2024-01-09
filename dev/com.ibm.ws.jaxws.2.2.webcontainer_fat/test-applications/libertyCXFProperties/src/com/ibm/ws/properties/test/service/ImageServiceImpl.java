ba05895919813d150f3c7b2cc7b819f5f74d0692
Thomas Johnson <tjjohnso@us.ibm.com>
2024-01-09 11:18:55 -0600

    JAX-WS: Fix Test NPE in ImageServiceImpl

--- dev/com.ibm.ws.jaxws.2.2.webcontainer_fat/test-applications/libertyCXFProperties/src/com/ibm/ws/properties/test/service/ImageServiceImpl.java
+++ dev/com.ibm.ws.jaxws.2.2.webcontainer_fat/test-applications/libertyCXFProperties/src/com/ibm/ws/properties/test/service/ImageServiceImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023 IBM Corporation and others.
+ * Copyright (c) 2023, 2024 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -12,8 +12,11 @@
  *******************************************************************************/
 package com.ibm.ws.properties.test.service;
 
-import java.io.FileOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.activation.DataHandler;
 import javax.jws.WebService;
@@ -29,11 +32,18 @@ public class ImageServiceImpl implements ImageService {
     @Override
     public void uploadImage(String id, DataHandler image) {
         try {
-            FileOutputStream out = new FileOutputStream(id + ".jpg");
-            image.writeTo(out);
-            out.close();
+
+            Path path = Paths.get(id + ".jpg");
+
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            image.writeTo(output);
+
+            Files.write(path, output.toByteArray());
+
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            // Since these tests are concerned only with whether properties are configured, if there's a problem
+            // writing to the file, we can just ignore the failure and continue on with the test.
+            System.out.println("Caught an exception trying to write to the file, continuing test.");
         }
 
     }
