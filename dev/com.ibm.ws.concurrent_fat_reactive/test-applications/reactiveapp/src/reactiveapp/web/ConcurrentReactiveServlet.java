ac0fb308818606188342204ff593eb2fc8a3e2fa
Mark Swatosh <mark.swatosh@gmail.com>
2024-01-25 14:40:06 -0600

    Adding ContextualSubscriber and ContextualProcessor implementations and tests

--- dev/com.ibm.ws.concurrent_fat_reactive/test-applications/reactiveapp/src/reactiveapp/web/ConcurrentReactiveServlet.java
+++ dev/com.ibm.ws.concurrent_fat_reactive/test-applications/reactiveapp/src/reactiveapp/web/ConcurrentReactiveServlet.java
@@ -16,6 +16,7 @@ import java.time.Instant;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Flow;
+import java.util.concurrent.Flow.Processor;
 import java.util.concurrent.Flow.Subscriber;
 import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
@@ -170,8 +171,9 @@ public class ConcurrentReactiveServlet extends FATServlet {
     public void contextualizedFlowTest() throws Exception {
         Flow.Subscriber<ContextCDL> subProxy = contextSvcDefault.createContextualProxy(new ThreadSubscriber(), Flow.Subscriber.class);
 
+        ExecutorService es = Executors.newFixedThreadPool(3); //Non-managed executor to test Context Service
         final ContextCDLImpl continueLatch = new ContextCDLImpl(1);
-        try (ThreadPublisher publisher = new ThreadPublisher(executor, handler)) {
+        try (ThreadPublisher publisher = new ThreadPublisher(es, handler)) {
 
             publisher.subscribe(subProxy);
             publisher.offer(continueLatch, null);
@@ -186,12 +188,91 @@ public class ConcurrentReactiveServlet extends FATServlet {
     }
 
     /**
-     * TODO write this test to cover the ContextService.contextualSubscriber method.
-     * For now, it only covers that the method can be invoked without an error occurring.
+     * Test that a ContextualSubscriber has access to Context.
+     * Publisher -> Subscriber
      */
     @Test
-    public void contextualSubscriber() throws Exception {
-        Subscriber<ContextCDL> contextualSubscriber = contextSvcDefault.contextualSubscriber(new ThreadSubscriber());
+    public void contextualSubscriberTest() throws Exception {
+        ThreadSubscriber subscriber = new ThreadSubscriber();
+        Subscriber<ContextCDL> contextualSubscriber = contextSvcDefault.contextualSubscriber(subscriber);
+
+        ExecutorService es = Executors.newFixedThreadPool(3); //Non-managed executor to test Context Service
+        final ContextCDLImpl continueLatch = new ContextCDLImpl(1);
+        try (ThreadPublisher publisher = new ThreadPublisher(es, handler)) {
+
+            publisher.subscribe(contextualSubscriber);
+            publisher.offer(continueLatch, null);
+
+            if (!continueLatch.await(TIMEOUT_NS, TimeUnit.NANOSECONDS)) {
+                if (publisher.getClosedException() != null)
+                    throw (AssertionError) new AssertionError("Context was not available in Publisher").initCause(publisher.getClosedException());
+                else
+                    throw new AssertionError("Timed out waiting for CountDownLatch, context may not have been available on Subscriber");
+            }
+        }
+
+        //Check for context in onComplete
+        for (long currentTime = Instant.now().toEpochMilli(),
+                        endTime = currentTime + TimeUnit.NANOSECONDS.toMillis(TIMEOUT_NS); currentTime < endTime; currentTime = Instant.now().toEpochMilli()) {
+            if (subscriber.onCompleteResult != null) {
+                if (subscriber.onCompleteResult instanceof NamingException) {
+                    throw (AssertionError) new AssertionError("Context was not available in Subscriber onComplete").initCause((NamingException) subscriber.onCompleteResult);
+                } else {
+                    break;
+                }
+            }
+        }
+        if (subscriber.onCompleteResult == null)
+            throw new AssertionError("Timed out waiting for subscriber.onCompleteResult");
+
+        //Check for context in onError
+        try (ThreadPublisher publisher = new ThreadPublisher(executor, handler)) {
+            publisher.subscribe(subscriber);
+            publisher.closeExceptionally(new Throwable("Ignored Exception"));
+        }
+        for (long currentTime = Instant.now().toEpochMilli(),
+                        endTime = currentTime + TimeUnit.NANOSECONDS.toMillis(TIMEOUT_NS); currentTime < endTime; currentTime = Instant.now().toEpochMilli()) {
+            if (subscriber.onErrorResult != null) {
+                if (subscriber.onErrorResult instanceof NamingException) {
+                    throw (AssertionError) new AssertionError("Context was not available in Subscriber onError").initCause((NamingException) subscriber.onErrorResult);
+                } else {
+                    break;
+                }
+            }
+        }
+        if (subscriber.onErrorResult == null)
+            throw new AssertionError("Timed out waiting for subscriber.onErrorResult");
+    }
+
+    /**
+     * Test that context is available in a ContextualProcessor and it's following ContextualSubscriber:
+     * Publisher -> Processor -> Subscriber
+     */
+    @Test
+    public void contextualProcessorTest() throws Exception {
+        ExecutorService es = Executors.newFixedThreadPool(3); //Non-managed executor to test Context Service
+
+        final ContextCDLImpl continueLatch = new ContextCDLImpl(2);
+
+        try (ThreadPublisher publisher = new ThreadPublisher(es, handler); ThreadProcessor processor = new ThreadProcessor(es, handler);) {
+
+            Processor<ContextCDL, ContextCDL> contextualProcessor = contextSvcDefault.contextualProcessor(processor);
+            Subscriber<ContextCDL> contextualSubscriber = contextSvcDefault.contextualSubscriber(new ThreadSubscriber());
+
+            publisher.subscribe(contextualProcessor);
+            contextualProcessor.subscribe(contextualSubscriber);
+            publisher.offer(continueLatch, null);
+
+            if (!continueLatch.await(TIMEOUT_NS, TimeUnit.NANOSECONDS)) {
+                if (publisher.getClosedException() != null)
+                    throw (AssertionError) new AssertionError("Context was not available in Publisher").initCause(publisher.getClosedException());
+                else if (processor.getClosedException() != null)
+                    throw (AssertionError) new AssertionError("Context was not available in Processor").initCause(publisher.getClosedException());
+                else
+                    throw new AssertionError("Timed out waiting for CountDownLatch");
+            }
+
+        }
     }
 
     /**
--- dev/io.openliberty.concurrent.internal.compat31/src/io/openliberty/concurrent/internal/compat/impl/ContextualProcessor.java
+++ dev/io.openliberty.concurrent.internal.compat31/src/io/openliberty/concurrent/internal/compat/impl/ContextualProcessor.java
@@ -6,41 +6,36 @@
  * http://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package io.openliberty.concurrent.internal.compat.impl;
 
 import java.util.ArrayList;
+import java.util.concurrent.Flow.Processor;
 import java.util.concurrent.Flow.Subscriber;
 import java.util.concurrent.Flow.Subscription;
 
-import com.ibm.websphere.ras.annotation.Trivial;
 import com.ibm.ws.concurrent.ContextualAction;
 import com.ibm.wsspi.threadcontext.ThreadContext;
 import com.ibm.wsspi.threadcontext.ThreadContextDescriptor;
 
 /**
- * Proxy for Subscriber that wraps execution of methods with thread context.
+ *
  */
-public class ContextualSubscriber<T> implements Subscriber<T>, ContextualAction<Subscriber<T>> {
-    private final Subscriber<T> action;
+public class ContextualProcessor<T, R> implements Processor<T, R>, ContextualAction<Processor<T, R>> {
+    private final Processor<T, R> action;
     private final ThreadContextDescriptor threadContextDescriptor;
 
-    public ContextualSubscriber(ThreadContextDescriptor threadContextDescriptor, Subscriber<T> action) {
+    public ContextualProcessor(ThreadContextDescriptor threadContextDescriptor, Processor<T, R> action) {
         this.action = action;
         this.threadContextDescriptor = threadContextDescriptor;
     }
 
     @Override
-    @Trivial
-    public Subscriber<T> getAction() {
+    public Processor<T, R> getAction() {
         return action;
     }
 
     @Override
-    @Trivial
     public ThreadContextDescriptor getContextDescriptor() {
         return threadContextDescriptor;
     }
@@ -84,4 +79,16 @@ public class ContextualSubscriber<T> implements Subscriber<T>, ContextualAction<
             threadContextDescriptor.taskStopping(contextApplied);
         }
     }
+
+    @Override
+    public void subscribe(Subscriber<? super R> subscriber) {
+        ArrayList<ThreadContext> contextApplied = threadContextDescriptor.taskStarting();
+        try {
+            action.subscribe(subscriber);
+        } finally {
+            threadContextDescriptor.taskStopping(contextApplied);
+        }
+
+    }
+
 }
