6d158a4a41c2d3fcf445f7e7464bca565852fc1f
Anija KA <anija.ka@ibm.com>
2024-02-06 20:45:32 +0530

    Copyright update.

--- dev/io.openliberty.persistence.3.1.thirdparty/src/org/eclipse/persistence/asm/ASMFactory.java
+++ dev/io.openliberty.persistence.3.1.thirdparty/src/org/eclipse/persistence/asm/ASMFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023,2024 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
