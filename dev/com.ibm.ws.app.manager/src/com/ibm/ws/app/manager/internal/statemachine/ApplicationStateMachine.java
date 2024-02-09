bda5f70167f98a9d9501679a645a05ab23299920
Thomas Watson <tjwatson@us.ibm.com>
2024-02-09 14:20:05 -0600

    Fix app start message for beforeAppStart

--- dev/com.ibm.ws.app.manager/src/com/ibm/ws/app/manager/internal/statemachine/ApplicationStateMachine.java
+++ dev/com.ibm.ws.app.manager/src/com/ibm/ws/app/manager/internal/statemachine/ApplicationStateMachine.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2022 IBM Corporation and others.
+ * Copyright (c) 2013, 2024 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -79,6 +79,4 @@ public abstract class ApplicationStateMachine {
     public abstract void describe(StringBuilder sb);
 
     public abstract boolean isBlocked();
-
-    public abstract void resetStartTime();
 }
