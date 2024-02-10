6287ae457b9184a251c7da27c6c6abb7dccac098
Phu Dinh <pmdinh@us.ibm.com>
2024-02-10 18:20:29 -0500

    Return 400 for invalid encoded request URI

--- dev/com.ibm.ws.webcontainer/src/com/ibm/ws/webcontainer/WebContainer.java
+++ dev/com.ibm.ws.webcontainer/src/com/ibm/ws/webcontainer/WebContainer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1997, 2023 IBM Corporation and others.
+ * Copyright (c) 1997, 2024 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -813,10 +813,18 @@ public abstract class WebContainer extends BaseContainer {
             String reqURI = req.getRequestURI();
             String decodedReqURI = null;
            
-            if (WCCustomProperties.DECODE_URL_PLUS_SIGN) {
-                decodedReqURI = URLDecoder.decode(reqURI, encoding);
-            } else {
-                decodedReqURI = WSURLDecoder.decode(reqURI, encoding);
+            try {
+                if (WCCustomProperties.DECODE_URL_PLUS_SIGN) {
+                    decodedReqURI = URLDecoder.decode(reqURI, encoding);
+                } else {
+                    decodedReqURI = WSURLDecoder.decode(reqURI, encoding);
+                }
+            }
+            catch (Exception e) {
+                logger.logp(Level.FINE, CLASS_NAME, "handleRequest", "cannot decode URI; sending 400 [" + e.getMessage() + "]");
+
+                sendBadRequestResponse(req, res);
+                return;
             }
 
             //Servlet 6.0 - process after decoded uri
@@ -1893,7 +1901,7 @@ public abstract class WebContainer extends BaseContainer {
         res.addHeader("Content-Type", "text/html");
         res.setStatusCode(400);
 
-        String formattedMessage = nls.getFormattedMessage("bad.request.uri:.{0}", new Object[] { truncateURI(req.getRequestURI()) },
+        String formattedMessage = nls.getFormattedMessage("bad.request.uri:.{0}", new Object[] { ResponseUtils.encodeDataString(truncateURI(req.getRequestURI())) },
                         "Bad request URI");
 
         String output = "<H1>"
