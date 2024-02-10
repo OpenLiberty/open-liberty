78a927bd782492ebf3d310d7a03dd1c1c2761c47
Phu Dinh <pmdinh@us.ibm.com>
2024-02-10 18:20:29 -0500

    Return 400 for invalid encoded request URI

--- dev/com.ibm.ws.webcontainer/src/com/ibm/ws/webcontainer/WebContainer.java
+++ dev/com.ibm.ws.webcontainer/src/com/ibm/ws/webcontainer/WebContainer.java
@@ -814,10 +814,18 @@ public abstract class WebContainer extends BaseContainer {
             String reqURI = req.getRequestURI();
             String decodedReqURI = null;
            
-            if (WCCustomProperties.DECODE_URL_PLUS_SIGN) {
-                decodedReqURI = URLDecoder.decode(reqURI, encoding);
-            } else {
-                decodedReqURI = WSURLDecoder.decode(reqURI, encoding);
+            try {
+                if (WCCustomProperties.DECODE_URL_PLUS_SIGN) {
+                    decodedReqURI = URLDecoder.decode(reqURI, encoding);
+                } else {
+                    decodedReqURI = WSURLDecoder.decode(reqURI, encoding);
+                }
+            }
+            catch (Exception e) {
+                logger.logp(Level.FINE, CLASS_NAME, "handleRequest", "cannot decode URI; sending 400 [" + e.getMessage() + "]");
+
+                sendBadRequestResponse(req, res);
+                return;
             }
 
             //Servlet 6.0 - process after decoded uri
