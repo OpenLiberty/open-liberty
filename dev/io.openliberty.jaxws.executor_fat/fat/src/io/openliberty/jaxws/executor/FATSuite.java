1e3d6ac2fe745ed1660d152e986ccbe8df8285c9
Thomas Johnson <tjjohnso@us.ibm.com>
2024-03-01 16:44:42 -0600

    JAX-WS: Add ManagedExecutor test

---
+++ dev/io.openliberty.jaxws.executor_fat/fat/src/io/openliberty/jaxws/executor/FATSuite.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2024 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package io.openliberty.jaxws.executor;
+
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import componenttest.rules.repeater.JakartaEE10Action;
+import componenttest.rules.repeater.JakartaEE9Action;
+import componenttest.rules.repeater.EmptyAction;
+import componenttest.rules.repeater.RepeatTests;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+                JAXWSExecutorTest.class
+})
+public class FATSuite {
+    @ClassRule
+    public static RepeatTests r = 
+        RepeatTests.with(new EmptyAction().fullFATOnly())
+                   .andWith(new JakartaEE9Action());
+}
