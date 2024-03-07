df1742be08cdbdc6fcb7278873532b94ca482643
Thomas Bitonti <bitonti@us.ibm.com>
2024-03-07 11:52:52 -0500

    Remove container verification step from shared library tests. This step cannot be performed because log output is not guaranteed to be in the order of the base events.  Event sequencing could be provided, but would require synchronizing shared library key steps.  That would have too much of a runtime impact and was not done. Some sequence numbers were added, and new event parsing tests were added.

--- dev/com.ibm.ws.app.manager_fat/fat/src/componenttest/application/manager/test/FATSuite.java
+++ dev/com.ibm.ws.app.manager_fat/fat/src/componenttest/application/manager/test/FATSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018-2023 IBM Corporation and others.
+ * Copyright (c) 2018,2024 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -18,13 +18,14 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-        FATTest.class,
-        AutoExtractTest.class,
-        AppOrderTests.class,
-        DropinsTests.class,
-        AppPrereqTest.class,
-        SharedLibUpdateConfigTest.class,
-        SharedLibUpdateJarTest.class
+                FATTest.class,
+                AutoExtractTest.class,
+                AppOrderTests.class,
+                DropinsTests.class,
+                AppPrereqTest.class,
+                SharedLibParseTest.class,
+                SharedLibUpdateConfigTest.class,
+                SharedLibUpdateJarTest.class
 })
 public class FATSuite {
     // EMPTY
