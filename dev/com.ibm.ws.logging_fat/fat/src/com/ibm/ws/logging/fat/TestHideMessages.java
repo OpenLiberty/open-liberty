961cb2161fefdf0228dacaaf92219bdb178a4aee
Anthony Guirguis <anthony.guirguis@ibm.com>
2024-03-27 11:04:04 -0400

    Ensuring server stops after each test case

--- dev/com.ibm.ws.logging_fat/fat/src/com/ibm/ws/logging/fat/TestHideMessages.java
+++ dev/com.ibm.ws.logging_fat/fat/src/com/ibm/ws/logging/fat/TestHideMessages.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -54,6 +54,14 @@ public class TestHideMessages {
     public void setupTest() throws Exception {
         msgServer.restoreServerConfiguration();
         msgServer.startServer();
+    }
+
+    @After
+    public void cleanUp() throws Exception {
+        if (msgServer != null && msgServer.isStarted()) {
+            msgServer.stopServer("CWMCG0007E", "CWMCG0014E", "CWMCG0015E",
+                                 "CWMCG5003E", "CWPMI2006W", "CWMMC0013E", "CWWKG0033W");
+        }
     }
 
     @Test
