9f5f3911f769c0edc741571364f111bb289a6d93
Isaac Rivera-Rivas <isaacrivriv@gmail.com>
2024-04-03 14:15:59 -0400

    Added changes for SSL in websocket2.1 with Netty

--- dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/netty/pipeline/http2/LibertyNettyALPNHandler.java
+++ dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/netty/pipeline/http2/LibertyNettyALPNHandler.java
@@ -13,6 +13,7 @@ import com.ibm.websphere.ras.Tr;
 import com.ibm.websphere.ras.TraceComponent;
 import com.ibm.ws.http.netty.NettyHttpChannelConfig;
 import com.ibm.ws.http.netty.pipeline.HttpPipelineInitializer;
+import com.ibm.ws.http.netty.pipeline.inbound.LibertyHttpObjectAggregator;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.HttpServerCodec;
@@ -34,13 +35,14 @@ public class LibertyNettyALPNHandler extends ApplicationProtocolNegotiationHandl
      * Default to HTTP 2.0 for now
      */
     public LibertyNettyALPNHandler(NettyHttpChannelConfig httpConfig) {
-        super(ApplicationProtocolNames.HTTP_2);
+//        super(ApplicationProtocolNames.HTTP_2);
+        super(ApplicationProtocolNames.HTTP_1_1);
         this.httpConfig = httpConfig;
     }
 
     @Override
     protected void configurePipeline(ChannelHandlerContext ctx, String protocol) throws Exception {
-        System.out.println("Configuring pipeline!!");
+        System.out.println("Configuring pipeline!! " + protocol);
         if (ApplicationProtocolNames.HTTP_2.equals(protocol)) {
             if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
                 Tr.debug(this, tc, "Configuring pipeline with HTTP 2 for incoming connection " + ctx.channel());
@@ -62,6 +64,9 @@ public class LibertyNettyALPNHandler extends ApplicationProtocolNegotiationHandl
             }
             ctx.pipeline().addAfter(HttpPipelineInitializer.HTTP_ALPN_HANDLER_NAME, HttpPipelineInitializer.NETTY_HTTP_SERVER_CODEC, new HttpServerCodec());
             ctx.pipeline().addAfter(HttpPipelineInitializer.NETTY_HTTP_SERVER_CODEC, HttpPipelineInitializer.HTTP_KEEP_ALIVE_HANDLER_NAME, new HttpServerKeepAliveHandler());
+            //TODO: this is a very large number, check best practice
+            ctx.pipeline().addAfter(HttpPipelineInitializer.HTTP_KEEP_ALIVE_HANDLER_NAME, null,
+                                    new LibertyHttpObjectAggregator(httpConfig.getMessageSizeLimit() == -1 ? HttpPipelineInitializer.maxContentLength : httpConfig.getMessageSizeLimit()));
             System.out.println("Configured Http1 pipeline with " + ctx.pipeline().names());
             if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
                 Tr.debug(this, tc, "Configured pipeline with " + ctx.pipeline().names());
