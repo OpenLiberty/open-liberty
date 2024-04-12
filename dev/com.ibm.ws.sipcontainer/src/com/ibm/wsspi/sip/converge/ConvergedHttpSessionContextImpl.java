d3da455f3057ff0380e929e0a00d53cd43ee3874
Volodymyr Siedlecki <volosied@gmail.com>
2024-04-12 13:52:43 -0400

    Create IConvergedHttpSessionContext for better integration with Servlet 3.1
    Update com.ibm.ws.sipcontainer.servlet.3.1
    
    Fix log export to allow logging in 3.1 bundle
    
    Reorganize / Copyrights

--- dev/com.ibm.ws.sipcontainer/src/com/ibm/wsspi/sip/converge/ConvergedHttpSessionContextImpl.java
+++ dev/com.ibm.ws.sipcontainer/src/com/ibm/wsspi/sip/converge/ConvergedHttpSessionContextImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 IBM Corporation and others.
+ * Copyright (c) 2019, 2024 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -35,7 +35,7 @@ import com.ibm.ws.webcontainer.session.impl.HttpSessionImpl;
 import com.ibm.wsspi.session.ISession;
 import com.ibm.wsspi.session.IStore;
 
-public class ConvergedHttpSessionContextImpl extends HttpSessionContextImpl {
+public class ConvergedHttpSessionContextImpl extends HttpSessionContextImpl implements IConvergedHttpSessionContext {
 
     /**
 	 * Class Logger.
