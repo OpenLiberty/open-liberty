4a775f826c93cf9bfc6dbc9abeb6e23e490b45db
Nathan Rauh <nathan.rauh@us.ibm.com>
2024-04-16 13:52:54 -0500

    Issue #27748 temporarily include copy of source because obtaining compiled code from other project fails

---
+++ dev/io.openliberty.jakarta.data.1.0/src/jakarta/data/repository/Query.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2022,2024 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package jakarta.data.repository;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation methods/properties copied from Jakarta Data repository.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Query {
+
+    String value();
+}
