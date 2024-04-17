fba0a80f5123da58dc7f3c7bd30d86883f486d75
Benjamin Confino <benjamic@uk.ibm.com>
2024-04-17 11:12:18 +0100

    copy project directories as a baseline

---
+++ dev/io.openliberty.microprofile.faulttolerance.4.1.internal_fat_tck/fat/src/io/openliberty/microprofile/faulttolerance40/tck/FATSuite.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2019, 2021 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.microprofile.faulttolerance40.tck;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ FaultToleranceTck40Launcher.class })
+
+public class FATSuite {
+
+}
