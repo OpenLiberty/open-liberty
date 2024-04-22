433953fc8f22c1d68767cf0fb6fd4a4baf46507d
Anthony Guirguis <anthony.guirguis@ibm.com>
2024-04-21 22:13:30 -0400

    Updating copyright

--- dev/io.openliberty.microprofile.metrics.internal.3.0_fat/test-applications/testHistogramApp/src/io/openliberty/microprofile/metrics/internal/fat/histogram/MetricsResource.java
+++ dev/io.openliberty.microprofile.metrics.internal.3.0_fat/test-applications/testHistogramApp/src/io/openliberty/microprofile/metrics/internal/fat/histogram/MetricsResource.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
