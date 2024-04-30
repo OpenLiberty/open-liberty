f80b1d3661af5f8c40c72ca34f2e2e1f23dfe699
Barbara Jensen <barbj@us.ibm.com>
2024-04-30 13:48:27 -0500

    update FeatureWebSecurityConfigImpl

--- dev/com.ibm.ws.webcontainer.security.feature/src/com/ibm/ws/webcontainer/security/feature/internal/FeatureWebSecurityConfigImpl.java
+++ dev/com.ibm.ws.webcontainer.security.feature/src/com/ibm/ws/webcontainer/security/feature/internal/FeatureWebSecurityConfigImpl.java
@@ -6,9 +6,6 @@
  * http://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package com.ibm.ws.webcontainer.security.feature.internal;
 
@@ -51,6 +48,7 @@ class FeatureWebSecurityConfigImpl implements WebAppSecurityConfig {
     private final Boolean useOnlyCustomCookieName = false;
     private final Boolean useContextRootForSSOCookiePath = false;
     private final Boolean partitionedCookie = false;
+    private final long postParamMaxRequestBodySize = 1024 * 1024 * 128L;
 
     FeatureWebSecurityConfigImpl(Map<String, Object> newProperties) {
         //nothing to do, values are hard-coded
@@ -431,5 +429,14 @@ class FeatureWebSecurityConfigImpl implements WebAppSecurityConfig {
             return globalConfig.isUseContextRootForSSOCookiePath();
         else
             return useContextRootForSSOCookiePath;
+    }
+
+    @Override
+    public long postParamMaxRequestBodySize() {
+        WebAppSecurityConfig globalConfig = WebAppSecurityCollaboratorImpl.getGlobalWebAppSecurityConfig();
+        if (globalConfig != null)
+            return globalConfig.postParamMaxRequestBodySize();
+        else
+            return postParamMaxRequestBodySize;
     }
 }
