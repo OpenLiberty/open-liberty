9f20ed52b47b76c0f25a0009f80dd1e403f3d376
Kyle Aure <KyleJAure@gmail.com>
2024-05-09 16:39:24 -0500

    WIP

--- dev/build.example.testcontainers_fat/fat/src/com/ibm/ws/testcontainers/example/DatabaseRotationTest.java
+++ dev/build.example.testcontainers_fat/fat/src/com/ibm/ws/testcontainers/example/DatabaseRotationTest.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -108,7 +108,6 @@ public class DatabaseRotationTest {
      * </pre>
      */
     @Test
-    @SkipIfSysProp(DB_Postgres)
     public void testSkipForPostgreSQL() throws Exception {
         assertTrue(System.getProperty("fat.bucket.db.type") != "Postgres");
     }
