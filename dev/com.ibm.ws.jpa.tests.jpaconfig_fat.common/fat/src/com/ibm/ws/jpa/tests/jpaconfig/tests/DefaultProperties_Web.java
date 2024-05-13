83e9d204ca0ce0db3d484d69a290f3b03684d375
Kyle Aure <KyleJAure@gmail.com>
2024-05-13 16:37:07 -0500

    failure_group-cc166913-e9d4-4038-a13d-561416c9cf10
    PostgreSQL
    Caused by: java.lang.ClassNotFoundException: org.postgresql.util.PGobject

--- dev/com.ibm.ws.jpa.tests.jpaconfig_fat.common/fat/src/com/ibm/ws/jpa/tests/jpaconfig/tests/DefaultProperties_Web.java
+++ dev/com.ibm.ws.jpa.tests.jpaconfig_fat.common/fat/src/com/ibm/ws/jpa/tests/jpaconfig/tests/DefaultProperties_Web.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -201,6 +201,12 @@ public class DefaultProperties_Web extends JPAFATServletClient {
                 loader.getCommonLibraryRefs().add("OpenJPALib");
                 cel.add(loader);
             }
+
+            // Always add the JDBC driver as common library ref as hibername/openjpa might need access to it
+            ConfigElementList<ClassloaderElement> cel = appRecord.getClassloaders();
+            ClassloaderElement loader = new ClassloaderElement();
+            loader.getCommonLibraryRefs().add("AnonymousJDBCLib");
+            cel.add(loader);
 
             server.setMarkToEndOfLog();
             ServerConfiguration sc = server.getServerConfiguration();
