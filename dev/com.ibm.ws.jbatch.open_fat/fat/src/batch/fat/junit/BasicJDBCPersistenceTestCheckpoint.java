5efc86a99519d4b0a95fc7da7696689829c798c5
Smitha Subbarao <smitha.subbarao@ibm.com>
2024-05-14 16:12:35 -0500

    Add Basic Batch tests

---
+++ dev/com.ibm.ws.jbatch.open_fat/fat/src/batch/fat/junit/BasicJDBCPersistenceTestCheckpoint.java
@@ -0,0 +1,57 @@
+package batch.fat.junit;
+
+import org.jboss.shrinkwrap.api.Archive;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import batch.fat.util.BatchFATHelper;
+
+import com.ibm.ws.jbatch.test.BatchAppUtils;
+import com.ibm.ws.jbatch.test.FatUtils;
+
+import com.ibm.websphere.simplicity.ShrinkHelper;
+
+import componenttest.custom.junit.runner.FATRunner;
+import com.ibm.ws.jbatch.test.FatUtils;
+import componenttest.annotation.CheckpointTest;
+import io.openliberty.checkpoint.spi.CheckpointPhase;
+
+@RunWith(FATRunner.class)
+public class BasicJDBCPersistenceTestCheckpoint extends BatchFATHelper {
+
+    @BeforeClass
+    public static void setup() throws Exception {
+
+        BatchFATHelper.setConfig("commonCheckpoint/server.xml", BasicJDBCPersistenceTestCheckpoint.class);
+
+        BatchAppUtils.addDropinsBatchFATWar(server);
+        BatchAppUtils.addDropinsBonusPayoutWar(server);
+        BatchAppUtils.addDropinsDbServletAppWar(server);
+        
+        
+        server.setCheckpoint(CheckpointPhase.AFTER_APP_START, true, null);
+        server.startServer();
+        FatUtils.waitForSmarterPlanet(server);
+
+        //wait for the security keys get generated.
+        FatUtils.waitForLTPA(server);
+
+        createDefaultRuntimeTables();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        if (server != null && server.isStarted()) {
+            server.stopServer();
+        }
+    }
+
+    @Test
+    public void testBasicJDBCPersistence() throws Exception {
+        test("Basic", "jslName=BasicPersistence");
+    }
+
+}
