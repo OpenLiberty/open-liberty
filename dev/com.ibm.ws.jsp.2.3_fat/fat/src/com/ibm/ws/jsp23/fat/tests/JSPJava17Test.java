a4741f99e047be8ca306b57542d2e5c578b53eab
Paul Nicolucci <pnicolucci@gmail.com>
2024-05-21 15:14:41 -0400

    Add AllowFFDC to testJava17viaPreCompile test

--- dev/com.ibm.ws.jsp.2.3_fat/fat/src/com/ibm/ws/jsp23/fat/tests/JSPJava17Test.java
+++ dev/com.ibm.ws.jsp.2.3_fat/fat/src/com/ibm/ws/jsp23/fat/tests/JSPJava17Test.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *******************************************************************************/
 package com.ibm.ws.jsp23.fat.tests;
@@ -21,6 +21,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.ibm.websphere.simplicity.ShrinkHelper;
+import com.ibm.websphere.simplicity.config.ServerConfiguration;
 import com.ibm.ws.jsp23.fat.JSPUtils;
 import com.meterware.httpunit.GetMethodWebRequest;
 import com.meterware.httpunit.WebConversation;
@@ -29,13 +30,11 @@ import com.meterware.httpunit.WebResponse;
 
 import componenttest.annotation.AllowedFFDC;
 import componenttest.annotation.MinimumJavaLevel;
-import componenttest.annotation.MaximumJavaLevel;
 import componenttest.annotation.Server;
 import componenttest.annotation.SkipForRepeat;
 import componenttest.custom.junit.runner.FATRunner;
 import componenttest.topology.impl.LibertyServer;
 
-import com.ibm.websphere.simplicity.config.ServerConfiguration;
 /**
  * JSP 2.3 tests which use Java 17 specific features.
  *
@@ -93,9 +92,9 @@ public class JSPJava17Test {
     }
 
     /**
-     * 
-     *  Try to compile a JSP with Java 21 features.  Compilation error is expected.
-     *  This is to ensure that Java 17 is used, nothing higher.  
+     *
+     * Try to compile a JSP with Java 21 features. Compilation error is expected.
+     * This is to ensure that Java 17 is used, nothing higher.
      *
      * @throws Exception
      */
@@ -121,9 +120,9 @@ public class JSPJava17Test {
      * Same test as testJava17JSP, but using the runtime JDK (via JSP's useJDKCompiler option rather than the default Eclipse Compiler for Java (ECJ))
      *
      * https://openliberty.io/docs/latest/reference/config/jspEngine.html
-     * 
+     *
      * @throws Exception if something goes horribly wrong
-     *                
+     *
      */
     @Test
     public void testJava17viaUseJDKCompiler() throws Exception {
@@ -154,13 +153,16 @@ public class JSPJava17Test {
     }
 
     /*
-     * Verify the precompile and javaSourceLevel interact nicely. 
-     * Precompile is enabled via prepareJSPs. 
-     * Note: testJava21.jsp will fail to compile 
-     * 
+     * Verify the precompile and javaSourceLevel interact nicely.
+     * Precompile is enabled via prepareJSPs.
+     * Note: testJava21.jsp will fail to compile
+     *
+     * Allow an FFDC for the compilation error.
+     *
      * @throws Exception if something goes horribly wrong
      */
     @Test
+    @AllowedFFDC("java.security.PrivilegedActionException") // Occurs in EE10 and lower
     public void testJava17viaPreCompile() throws Exception {
 
         ServerConfiguration configuration = server.getServerConfiguration();
@@ -170,7 +172,7 @@ public class JSPJava17Test {
 
         server.setMarkToEndOfLog();
         server.updateServerConfiguration(configuration);
-        server.stopServer("SRVE8115W","SRVE8094W");
+        server.stopServer("SRVE8115W", "SRVE8094W");
         server.startServer();
 
         WebConversation wc = new WebConversation();
@@ -191,7 +193,7 @@ public class JSPJava17Test {
 
     /*
      * Verifies that javaSourceLevel overrides jdkSourceLevel if both are set. Warning is also logged
-     * 
+     *
      * AllowedFFDC Added due to precompile of testJava21.jsp in the test above
      */
     @Test
@@ -200,7 +202,7 @@ public class JSPJava17Test {
 
         ServerConfiguration configuration = server.getServerConfiguration();
         configuration.getJspEngine().setJdkSourceLevel("18");
-        configuration.getJspEngine().setPrepareJSPs("1000000000"); 
+        configuration.getJspEngine().setPrepareJSPs("1000000000");
         LOG.info("New server configuration used: " + configuration);
 
         server.setMarkToEndOfLog();
