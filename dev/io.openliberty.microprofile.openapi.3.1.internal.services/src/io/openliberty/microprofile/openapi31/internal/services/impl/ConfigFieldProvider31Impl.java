72e4923688f58623f37e2156feb6a3e191054625
Andrew Rouse <anrouse@uk.ibm.com>
2024-05-22 17:27:35 +0100

    Do not cache OpenAPI model if user code called
    Do not cache the OpenAPI model if the user has registered an OASFilter
    or an OASModelReader. The users code could reference anything in the
    environment so we can't safely invalidate the cache.
    
    Additionally store the Jandex index (if created) in the cache. The
    Jandex index can still be used if config which does not affect indexing
    changes or if the user registers an OASFilter or OASModelReader.
    Creating the Jandex index is the slowest part of building the OpenAPI
    model.

--- dev/io.openliberty.microprofile.openapi.3.1.internal.services/src/io/openliberty/microprofile/openapi31/internal/services/impl/ConfigFieldProvider31Impl.java
+++ dev/io.openliberty.microprofile.openapi.3.1.internal.services/src/io/openliberty/microprofile/openapi31/internal/services/impl/ConfigFieldProvider31Impl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022 IBM Corporation and others.
+ * Copyright (c) 2022, 2024 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -12,6 +12,7 @@
  *******************************************************************************/
 package io.openliberty.microprofile.openapi31.internal.services.impl;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Optional;
@@ -112,6 +113,19 @@ public class ConfigFieldProvider31Impl implements ConfigFieldProvider {
     @Override
     public Collection<ConfigField> getConfigFields() {
         return Arrays.asList(ConfigField31.values());
+    }
+
+    @Override
+    public Collection<ConfigField> getIndexingConfigFields() {
+        ArrayList<ConfigField> result = new ArrayList<>();
+        result.add(ConfigField31.SCAN_DISABLE);
+        result.add(ConfigField31.SCAN_CLASSES);
+        result.add(ConfigField31.SCAN_EXCLUDE_CLASSES);
+        result.add(ConfigField31.SCAN_PACKAGES);
+        result.add(ConfigField31.SCAN_EXCLUDE_PACKAGES);
+        result.add(ConfigField31.SCAN_DEPENDENCIES_DISABLE);
+        result.add(ConfigField31.SCAN_DEPENDENCIES_JARS);
+        return result;
     }
 
     @Override
