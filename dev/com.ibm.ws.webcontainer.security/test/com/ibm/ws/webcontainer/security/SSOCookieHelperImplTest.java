25651ef844423ca4efaf3e47ab313cc6d05d6385
Barbara Jensen <barbj@us.ibm.com>
2024-05-29 01:49:57 -0500

    update tests

--- dev/com.ibm.ws.webcontainer.security/test/com/ibm/ws/webcontainer/security/SSOCookieHelperImplTest.java
+++ dev/com.ibm.ws.webcontainer.security/test/com/ibm/ws/webcontainer/security/SSOCookieHelperImplTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2023 IBM Corporation and others.
+ * Copyright (c) 2017, 2024 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -180,6 +180,8 @@ public class SSOCookieHelperImplTest {
                 allowing(config).getSameSiteCookie();
                 will(returnValue("Disabled"));
                 allowing(config).isUseContextRootForSSOCookiePath();
+                allowing(config).getPartitionedCookie();
+                will(returnValue(null));
             }
         });
         Subject subject = new Subject();
