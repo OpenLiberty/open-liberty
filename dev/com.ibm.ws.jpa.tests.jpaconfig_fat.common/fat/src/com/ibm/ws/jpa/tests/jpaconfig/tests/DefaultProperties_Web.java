2cf04b6b936b85a9139876d7322cbe6775506fae
Kyle Aure <KyleJAure@gmail.com>
2024-05-29 10:20:40 -0500

    Fix issues with original PR

--- dev/com.ibm.ws.jpa.tests.jpaconfig_fat.common/fat/src/com/ibm/ws/jpa/tests/jpaconfig/tests/DefaultProperties_Web.java
+++ dev/com.ibm.ws.jpa.tests.jpaconfig_fat.common/fat/src/com/ibm/ws/jpa/tests/jpaconfig/tests/DefaultProperties_Web.java
@@ -55,7 +55,7 @@ import componenttest.topology.utils.PrivHelper;
 @Mode(TestMode.LITE)
 public class DefaultProperties_Web extends JPAFATServletClient {
 
-    @Rule
+    @Rule //TODO This skips all tests during Database Rotation builds - the server will start and stop, but that is all
     public static SkipDatabaseRule skipDBRule = new SkipDatabaseRule();
 
     enum ApplicationConfig {
@@ -193,7 +193,12 @@ public class DefaultProperties_Web extends JPAFATServletClient {
             if (AbstractFATSuite.repeatPhase != null && AbstractFATSuite.repeatPhase.contains("hibernate")) {
                 ConfigElementList<ClassloaderElement> cel = appRecord.getClassloaders();
                 ClassloaderElement loader = new ClassloaderElement();
-                loader.getCommonLibraryRefs().add("HibernateLib");
+                if (DatabaseVendor.POSTGRES.equals(getDbVendor())) {
+                    //Hibernate requires access to a utility class from the PostgreSQL Driver
+                    loader.getCommonLibraryRefs().add("HibernateLib, AnonymousJDBCLib");
+                } else {
+                    loader.getCommonLibraryRefs().add("HibernateLib");
+                }
                 cel.add(loader);
             } else if (AbstractFATSuite.repeatPhase != null && AbstractFATSuite.repeatPhase.contains("openjpa")) {
                 ConfigElementList<ClassloaderElement> cel = appRecord.getClassloaders();
@@ -201,12 +206,6 @@ public class DefaultProperties_Web extends JPAFATServletClient {
                 loader.getCommonLibraryRefs().add("OpenJPALib");
                 cel.add(loader);
             }
-
-            // Always add the JDBC driver as common library ref as hibername/openjpa might need access to it
-            ConfigElementList<ClassloaderElement> cel = appRecord.getClassloaders();
-            ClassloaderElement loader = new ClassloaderElement();
-            loader.getCommonLibraryRefs().add("AnonymousJDBCLib");
-            cel.add(loader);
 
             server.setMarkToEndOfLog();
             ServerConfiguration sc = server.getServerConfiguration();
