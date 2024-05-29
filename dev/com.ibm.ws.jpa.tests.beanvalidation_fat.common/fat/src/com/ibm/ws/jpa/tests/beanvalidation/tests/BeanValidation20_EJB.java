1df52f852332e180757e37893b0fb66d8be01682
Kyle Aure <KyleJAure@gmail.com>
2024-05-29 09:50:11 -0500

    Revert "Fix errors during Database Rotation"

--- dev/com.ibm.ws.jpa.tests.beanvalidation_fat.common/fat/src/com/ibm/ws/jpa/tests/beanvalidation/tests/BeanValidation20_EJB.java
+++ dev/com.ibm.ws.jpa.tests.beanvalidation_fat.common/fat/src/com/ibm/ws/jpa/tests/beanvalidation/tests/BeanValidation20_EJB.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2021, 2024 IBM Corporation and others.
+ * Copyright (c) 2021 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- *
+ * 
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -186,12 +186,6 @@ public class BeanValidation20_EJB extends JPAFATServletClient {
             loader.getCommonLibraryRefs().add("OpenJPALib");
             cel.add(loader);
         }
-
-        // Always add the JDBC driver as common library ref as hibername/openjpa might need access to it
-        ConfigElementList<ClassloaderElement> cel = appRecord.getClassloaders();
-        ClassloaderElement loader = new ClassloaderElement();
-        loader.getCommonLibraryRefs().add("AnonymousJDBCLib");
-        cel.add(loader);
 
         server.setMarkToEndOfLog();
         ServerConfiguration sc = server.getServerConfiguration();
