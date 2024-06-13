7dffe017391d9ee50e72d4fe8864441142768b70
Jared Anderson <jhanders@us.ibm.com>
2024-06-13 11:42:36 -0500

    Restructure com.ibm.ws.kernel.feature.core
    - Pull out interfaces from com.ibm.ws.kernel.feature.core into
    com.ibm.ws.kernel.feature.common to avoid circular dependencies with
    runtime.update so that others can rely on the interfaces.

--- dev/com.ibm.ws.kernel.feature.common/src/com/ibm/ws/kernel/feature/package-info.java
+++ dev/com.ibm.ws.kernel.feature.common/src/com/ibm/ws/kernel/feature/package-info.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -15,8 +15,7 @@
  * @version 1.0.0
  */
 @org.osgi.annotation.versioning.Version("1.0.0")
-@TraceOptions(traceGroup = com.ibm.ws.kernel.feature.internal.ProvisionerConstants.TR_GROUP, messageBundle = com.ibm.ws.kernel.feature.internal.ProvisionerConstants.NLS_PROPS)
+@TraceOptions(traceGroup = "featureManager", messageBundle = "com.ibm.ws.kernel.feature.internal.resources.ProvisionerMessages")
 package com.ibm.ws.kernel.feature;
 
 import com.ibm.websphere.ras.annotation.TraceOptions;
-
