5ca26ccc77e44e6c18572bdd266b2b19f23bc587
Tom Evans <tevans@uk.ibm.com>
2024-06-18 17:31:42 +0100

    Repeat MP Config Tests against MP7

--- dev/io.openliberty.microprofile.config.3.0.internal_fat/fat/src/io/openliberty/microprofile/config/internal_fat/HotAddMPConfig30.java
+++ dev/io.openliberty.microprofile.config.3.0.internal_fat/fat/src/io/openliberty/microprofile/config/internal_fat/HotAddMPConfig30.java
@@ -1,14 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2021 IBM Corporation and others.
+ * Copyright (c) 2019, 2024 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package io.openliberty.microprofile.config.internal_fat;
 
@@ -30,10 +27,10 @@ import componenttest.custom.junit.runner.FATRunner;
 import componenttest.custom.junit.runner.Mode;
 import componenttest.custom.junit.runner.Mode.TestMode;
 import componenttest.rules.repeater.FeatureReplacementAction;
-import componenttest.rules.repeater.MicroProfileActions;
 import componenttest.rules.repeater.RepeatTests;
 import componenttest.topology.impl.LibertyServer;
 import componenttest.topology.utils.FATServletClient;
+import io.openliberty.microprofile.config.fat.repeat.ConfigRepeatActions;
 import io.openliberty.microprofile.config.internal_fat.apps.hotadd.HotAddMPConfig30Servlet;
 
 @RunWith(FATRunner.class)
@@ -47,7 +44,7 @@ public class HotAddMPConfig30 extends FATServletClient {
     public static LibertyServer server;
 
     @ClassRule
-    public static RepeatTests r = MicroProfileActions.repeat(FeatureReplacementAction.ALL_SERVERS, MicroProfileActions.MP61, MicroProfileActions.MP50);
+    public static RepeatTests r = ConfigRepeatActions.repeatDefault30Up(FeatureReplacementAction.ALL_SERVERS);
 
     @BeforeClass
     public static void setUp() throws Exception {
---
+++ dev/io.openliberty.microprofile.config.internal_repeat_tests/src/io/openliberty/microprofile/config/fat/repeat/ConfigRepeatActions.java
@@ -0,0 +1,133 @@
+/*******************************************************************************
+ * Copyright (c) 2024 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package io.openliberty.microprofile.config.fat.repeat;
+
+import componenttest.custom.junit.runner.Mode.TestMode;
+import componenttest.rules.repeater.MicroProfileActions;
+import componenttest.rules.repeater.RepeatTests;
+
+/**
+ * Contains static methods for creating standard RepeatTests rules for Config tests
+ *
+ * MicroProfile 1.2 = Config 1.1
+ * MicroProfile 1.3 = Config 1.2
+ * MicroProfile 1.4 = Config 1.3
+ * MicroProfile 2.0 = Config 1.3
+ * MicroProfile 2.1 = Config 1.3
+ * MicroProfile 2.2 = Config 1.3
+ * MicroProfile 3.0 = Config 1.3
+ * MicroProfile 3.2 = Config 1.3
+ * MicroProfile 3.3 = Config 1.4
+ * MicroProfile 4.0 = Config 2.0
+ * MicroProfile 4.1 = Config 2.0
+ * MicroProfile 5.0 = Config 3.0
+ * MicroProfile 6.0 = Config 3.0
+ * MicroProfile 6.1 = Config 3.1
+ * MicroProfile 7.0 = Config 3.1
+ */
+public class ConfigRepeatActions {
+
+    /**
+     * Return a rule to repeat tests for MicroProfile 7.0, 6.0, 4.1 and 3.3.
+     * This translates to Config 3.1, 3.0, 2.0 and 1.4 respectively.
+     *
+     * This is the default set of repeats for most Config tests.
+     * It provides good coverage of most code paths.
+     *
+     * @param server the server name
+     * @return the RepeatTests rule
+     */
+    public static RepeatTests repeatDefault(String server) {
+        return MicroProfileActions.repeat(server, TestMode.FULL,
+                                          MicroProfileActions.MP70_EE10, //Config 3.1
+                                          MicroProfileActions.MP70_EE11, //Config 3.1
+                                          MicroProfileActions.MP60, //Config 3.0
+                                          MicroProfileActions.MP41, //Config 2.0
+                                          MicroProfileActions.MP33); //Config 1.4
+    }
+
+    /**
+     * Return a rule to repeat tests for MicroProfile 7.0, 6.0 and 4.1.
+     * This translates to Config 3.1, 3.0 and 2.0 respectively.
+     *
+     * This is the default set of repeats for most Config tests.
+     * It provides good coverage of most code paths.
+     *
+     * @param server the server name
+     * @return the RepeatTests rule
+     */
+    public static RepeatTests repeatDefault20Up(String server) {
+        return MicroProfileActions.repeat(server, TestMode.FULL,
+                                          MicroProfileActions.MP70_EE10, //Config 3.1
+                                          MicroProfileActions.MP70_EE11, //Config 3.1
+                                          MicroProfileActions.MP60, //Config 3.0
+                                          MicroProfileActions.MP41); //Config 2.0
+    }
+
+    /**
+     * Return a rule to repeat tests for MicroProfile 7.0 and 6.0.
+     * This translates to Config 3.1 and 3.0.
+     *
+     * This is the default set of repeats for most Config tests.
+     * It provides good coverage of most code paths.
+     *
+     * @param server the server name
+     * @return the RepeatTests rule
+     */
+    public static RepeatTests repeatDefault30Up(String server) {
+        return MicroProfileActions.repeat(server, TestMode.FULL,
+                                          MicroProfileActions.MP70_EE10, //Config 3.1
+                                          MicroProfileActions.MP70_EE11, //Config 3.1
+                                          MicroProfileActions.MP60); //Config 3.0
+    }
+
+    /**
+     * Return a rule to repeat tests for MicroProfile 7.0 and 6.1.
+     * This translates to all versions which contain Config 3.1.
+     *
+     * This is the default set of repeats for most Config tests.
+     * It provides good coverage of most code paths.
+     *
+     * @param server the server name
+     * @return the RepeatTests rule
+     */
+    public static RepeatTests repeatDefault31(String server) {
+        return MicroProfileActions.repeat(server, TestMode.FULL,
+                                          MicroProfileActions.MP70_EE10, //Config 3.1
+                                          MicroProfileActions.MP70_EE11, //Config 3.1
+                                          MicroProfileActions.MP61); //Config 3.1
+    }
+
+    /**
+     * Return a rule to repeat tests for all MP versions which contain Config
+     * <p>
+     * We run a few tests using this rule so that we have some coverage of all implementations
+     *
+     * @param server the server name
+     * @return the RepeatTests rule
+     */
+    public static RepeatTests repeatAll(String server) {
+        return MicroProfileActions.repeat(server, TestMode.FULL,
+                                          MicroProfileActions.MP70_EE10,
+                                          MicroProfileActions.MP70_EE11,
+                                          MicroProfileActions.MP61,
+                                          MicroProfileActions.MP60,
+                                          MicroProfileActions.MP50,
+                                          MicroProfileActions.MP41,
+                                          MicroProfileActions.MP40,
+                                          MicroProfileActions.MP33,
+                                          MicroProfileActions.MP32,
+                                          MicroProfileActions.MP30,
+                                          MicroProfileActions.MP22,
+                                          MicroProfileActions.MP20,
+                                          MicroProfileActions.MP13,
+                                          MicroProfileActions.MP12);
+    }
+}
