945fc3af2589a2748d60022d76698a7327a98eba
David Chan <chdavid@ca.ibm.com>
2024-06-21 11:50:42 -0400

    Disable testcontainer fats for now, add in following PR

--- dev/io.openliberty.http.monitor_fat/fat/src/io/openliberty/http/monitor/fat/FATSuite.java
+++ dev/io.openliberty.http.monitor_fat/fat/src/io/openliberty/http/monitor/fat/FATSuite.java
@@ -18,11 +18,11 @@ import org.junit.runners.Suite.SuiteClasses;
                 NoAppTest.class,
                 JSPApplicationTest.class,
                 RestApplicationTest.class,
-                ServletApplicationTest.class,
-                ContainerServletApplicationTest.class,
-                ContainerJSPApplicationTest.class,
-                ContainerRestApplicationTest.class,
-                ContainerNoAppTest.class
+                ServletApplicationTest.class
+//                ContainerServletApplicationTest.class,
+//                ContainerJSPApplicationTest.class,
+//                ContainerRestApplicationTest.class,
+//                ContainerNoAppTest.class
 })
 
 public class FATSuite {
