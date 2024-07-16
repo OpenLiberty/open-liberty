e06294b93bc9e9b3268cb8358fbf01ad16840c02
Jon Hawkes <jon.hawkes@uk.ibm.com>
2024-07-16 17:25:32 +0100

    Rearrange lease failover tests

--- dev/com.ibm.ws.transaction.fat.util/src/com/ibm/ws/transaction/fat/util/TxTestContainerSuite.java
+++ dev/com.ibm.ws.transaction.fat.util/src/com/ibm/ws/transaction/fat/util/TxTestContainerSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022, 2023 IBM Corporation and others.
+ * Copyright (c) 2022, 2024 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -108,6 +108,10 @@ public class TxTestContainerSuite extends TestContainerSuite {
 
 	public static boolean isDerby() {
 		return databaseContainerType == DatabaseContainerType.Derby;
+	}
+	
+	public static boolean isSQLServer() {
+		return databaseContainerType == DatabaseContainerType.SQLServer;
 	}
 
 	public static void setType(DatabaseContainerType type) {
