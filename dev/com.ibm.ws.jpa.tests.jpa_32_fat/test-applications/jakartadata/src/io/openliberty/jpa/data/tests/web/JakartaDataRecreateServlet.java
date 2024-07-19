8ede48f8d60ed77a86abbf2c1d895cdc89214f94
Kyle Aure <KyleJAure@gmail.com>
2024-07-19 15:26:59 -0500

    Recreate https://github.com/OpenLiberty/open-liberty/issues/28912

--- dev/com.ibm.ws.jpa.tests.jpa_32_fat/test-applications/jakartadata/src/io/openliberty/jpa/data/tests/web/JakartaDataRecreateServlet.java
+++ dev/com.ibm.ws.jpa.tests.jpa_32_fat/test-applications/jakartadata/src/io/openliberty/jpa/data/tests/web/JakartaDataRecreateServlet.java
@@ -9,14 +9,21 @@
  *******************************************************************************/
 package io.openliberty.jpa.data.tests.web;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.UUID;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 import componenttest.app.FATServlet;
+import io.openliberty.jpa.data.tests.models.Coordinate;
+import jakarta.annotation.Resource;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.transaction.UserTransaction;
 
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns = "/JakartaDataRecreate")
@@ -25,9 +32,51 @@ public class JakartaDataRecreateServlet extends FATServlet {
     @PersistenceContext(unitName = "RecreatePersistenceUnit")
     private EntityManager em;
 
+    @Resource
+    private UserTransaction tx;
+
     @Test
     public void alwaysPasses() {
         assertTrue(true);
+    }
+
+    @Test
+    @Ignore("Reference issue: https://github.com/OpenLiberty/open-liberty/issues/28912")
+    public void testOLGH28912() throws Exception {
+        Coordinate original = Coordinate.of("testOLGH28912", 10, 15f);
+        UUID id = original.id;
+        Coordinate result;
+
+        tx.begin();
+
+        try {
+            em.persist(original);
+
+            em.createQuery("UPDATE Coordinate SET x = :newX, y = y / :yDivisor WHERE id = :id") //FAILURE PARSING QUERY HERE
+                            .setParameter("newX", 11)
+                            .setParameter("yDivisor", 5)
+                            .setParameter("id", id)
+                            .executeUpdate();
+
+            result = em.createQuery("SELECT Coordinate WHERE id = :id", Coordinate.class)
+                            .setParameter("id", id)
+                            .getSingleResult();
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+
+            /*
+             * Recreated
+             * java.lang.IllegalArgumentException: An exception occurred while creating a query in EntityManager:
+             * Exception Description: Syntax error parsing [UPDATE Coordinate SET x = :newX, y = y / :yDivisor WHERE id = :id].
+             * [37, 38] The left expression is not an arithmetic expression.
+             */
+            throw e;
+        }
+
+        assertEquals(id, result.id);
+        assertEquals(11, result.x, 0.001);
+        assertEquals(5f, result.y, 0.001);
     }
 
 }
