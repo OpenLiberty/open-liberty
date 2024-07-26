653a8ab0f5cd5139b3fb03186ed70d71e440d5de
Phu Dinh <pmdinh@us.ibm.com>
2024-07-26 16:05:15 -0400

    Faces 4.1 Update Schemas

--- dev/com.ibm.ws.javaee.ddmodel/test/com/ibm/ws/javaee/ddmodel/jsf/JSFAppTestBase.java
+++ dev/com.ibm.ws.javaee.ddmodel/test/com/ibm/ws/javaee/ddmodel/jsf/JSFAppTestBase.java
@@ -1,14 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2022 IBM Corporation and others.
+ * Copyright (c) 2013, 2024 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
  * 
  * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package com.ibm.ws.javaee.ddmodel.jsf;
 
@@ -126,6 +123,14 @@ public class JSFAppTestBase extends DDTestBase {
             " xsi:schemaLocation=\"https://jakarta.ee/xml/ns/jakartaee https://jakarta.ee/xml/ns/jakartaee/web-facesconfig_4_0.xsd\"" + '\n' +
             " version=\"4.0\"" + '\n' +
             ">";
+
+    protected static final String jsf41Head =
+        "<faces-config" + '\n' +
+            " xmlns=\"https://jakarta.ee/xml/ns/jakartaee\"" + '\n' +
+            " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"" + '\n' +
+            " xsi:schemaLocation=\"https://jakarta.ee/xml/ns/jakartaee https://jakarta.ee/xml/ns/jakartaee/web-facesconfig_4_1.xsd\"" + '\n' +
+            " version=\"4.1\"" + '\n' +
+            ">";
             
     protected static final String jsfTail =
         "</faces-config>";
@@ -153,6 +158,8 @@ public class JSFAppTestBase extends DDTestBase {
             jsfHead = jsf30Head;
         } else if ( version == FacesConfig.VERSION_4_0 ) {
             jsfHead = jsf40Head;
+        } else if ( version == FacesConfig.VERSION_4_1 ) {
+            jsfHead = jsf41Head;
 
         } else {
             throw new IllegalArgumentException("Unknown faces config version [ " + version + " ]");
