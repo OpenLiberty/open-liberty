60ed9c43f68ac1b47f4b1ef81eee1bfd44c9deb5
Smitha Subbarao <smitha.subbarao@ibm.com>
2024-07-31 21:53:57 -0500

    Add Batch files updates

---
+++ dev/io.openliberty.checkpoint_fat_batch/fat/src/batch.fat.junit/FATSuite.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2024 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.checkpoint.fat.batch;
+
+import static componenttest.topology.utils.FATServletClient.getTestMethodSimpleName;
+
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import componenttest.custom.junit.runner.AlwaysPassesTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+                AlwaysPassesTest.class,
+                CDITestCheckpoint.class
+})
+
+public class FATSuite {
+
+}
