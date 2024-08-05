aa385a56ceaba8a506b8ca92e40eca7b7069c3d1
Anthony Guirguis <anthony.guirguis@ibm.com>
2024-08-05 17:02:56 -0400

    Initial drop

---
+++ dev/io.openliberty.microprofile.telemetry.2.0.logging.internal_container_fat/test-applications/ServletApp/src/io/openliberty/http/monitor/fat/servletApp/FailServlet.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * Copyright (c) 2024 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.http.monitor.fat.servletApp;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Fail Servlet
+ */
+@WebServlet("/failServlet")
+public class FailServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /** {@inheritDoc} */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        switch (req.getParameter("failMode")) {
+            case "zero":
+                divideArith(resp);
+                break;
+            case "custom":
+                customResponse(resp);
+                break;
+            case "io":
+                throwIO(resp);
+                break;
+            case "iae":
+                throwIAE(resp);
+                break;
+            default:
+                break;
+        }
+        resp.getWriter().append("Hello, let's fail together!");
+    }
+
+    private void divideArith(HttpServletResponse resp) {
+        int x = 4 / 0;
+    }
+
+    private void customResponse(HttpServletResponse resp) {
+        resp.setStatus(456);
+    }
+
+    private void throwIO(HttpServletResponse resp) throws IOException {
+        throw new IOException("I'm an IO Exception");
+    }
+
+    private void throwIAE(HttpServletResponse resp) {
+        throw new IllegalArgumentException("I'm an IA Exception");
+    }
+}
