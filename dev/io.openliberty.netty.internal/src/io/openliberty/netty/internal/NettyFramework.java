e529389e7f78a2ff64ea1c53e90dbf29c9b13139
Isaac Rivera-Rivas <isaacrivriv@gmail.com>
2024-08-12 13:43:30 -0400

    Cleanup and review comments

--- dev/io.openliberty.netty.internal/src/io/openliberty/netty/internal/NettyFramework.java
+++ dev/io.openliberty.netty.internal/src/io/openliberty/netty/internal/NettyFramework.java
@@ -13,7 +13,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import com.ibm.websphere.channelfw.EndPointMgr;
 
