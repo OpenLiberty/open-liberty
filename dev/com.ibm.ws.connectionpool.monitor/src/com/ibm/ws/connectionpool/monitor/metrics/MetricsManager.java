981a7b43a33ae6487fa18aa37883fa336d82d014
David Chan <chdavid@ca.ibm.com>
2024-08-19 14:08:56 -0400

    In the connectionpool.monitor bundle we directly update histograms with MP Telemetry. We need to ensure it checks the config whether connectionPool module is enabled

--- dev/com.ibm.ws.connectionpool.monitor/src/com/ibm/ws/connectionpool/monitor/metrics/MetricsManager.java
+++ dev/com.ibm.ws.connectionpool.monitor/src/com/ibm/ws/connectionpool/monitor/metrics/MetricsManager.java
@@ -9,14 +9,17 @@
  *******************************************************************************/
 package com.ibm.ws.connectionpool.monitor.metrics;
 
-import static org.osgi.service.component.annotations.ConfigurationPolicy.IGNORE;
-
 import java.time.Duration;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
+import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
@@ -26,19 +29,36 @@ import com.ibm.websphere.ras.Tr;
 import com.ibm.websphere.ras.TraceComponent;
 import com.ibm.ws.kernel.productinfo.ProductInfo;
 
-@Component(configurationPolicy = IGNORE, immediate = true)
+@Component(configurationPid = "com.ibm.ws.monitor.internal.MonitoringFrameworkExtender", configurationPolicy = ConfigurationPolicy.OPTIONAL, immediate = true)
 public class MetricsManager {
 
     private static MetricsManager instance;
 
     private static final TraceComponent tc = Tr.register(MetricsManager.class);
 
+    private final static String MONITORING_GROUP_FILTER = "filter";
+
+    /*
+     * By Default, without any monitor-1.0 filters on, all monitor components are enabled
+     */
+    private static volatile boolean isConnPoolEnabled = true;
+
+    public static boolean isConnPoolEnabled() {
+        return isConnPoolEnabled;
+    }
+
     @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC, policyOption = ReferencePolicyOption.GREEDY)
     private volatile List<ConnectionPoolMetricAdapter> metricRuntimes;
 
     @Activate
-    public void activate() {
+    public void activate(ComponentContext context, Map<String, Object> properties) {
+        resolveMonitorFilter(properties);
         instance = this;
+    }
+
+    @Modified
+    protected void modified(ComponentContext context, Map<String, Object> properties) {
+        resolveMonitorFilter(properties);
     }
 
     @Deactivate
@@ -74,7 +94,9 @@ public class MetricsManager {
         if (!ProductInfo.getBetaEdition()) {
             return;
         }
-        metricRuntimes.stream().forEach(adapters -> adapters.updateWaitTimeMetrics(poolName, duration));
+        if (isConnPoolEnabled) {
+            metricRuntimes.stream().forEach(adapters -> adapters.updateWaitTimeMetrics(poolName, duration));
+        }
 
     }
 
@@ -88,7 +110,36 @@ public class MetricsManager {
         if (!ProductInfo.getBetaEdition()) {
             return;
         }
-        metricRuntimes.stream().forEach(adapters -> adapters.updateInUseTimeMetrics(poolName, duration));
+
+        if (isConnPoolEnabled) {
+            metricRuntimes.stream().forEach(adapters -> adapters.updateInUseTimeMetrics(poolName, duration));
+        }
+
+    }
+
+    private void resolveMonitorFilter(Map<String, Object> properties) {
+        String filter;
+
+        if ((filter = (String) properties.get(MONITORING_GROUP_FILTER)) != null && filter.length() != 0) {
+            // Original MonitoringFrameWorkExtender matches case
+
+            if (filter.length() > 0) {
+                isConnPoolEnabled = Stream.of(filter.split(",")).anyMatch(item -> item.equals("ConnectionPool"));
+            } else {
+                // by default, every monitor component is enabled if length is 0
+                isConnPoolEnabled = true;
+            }
+        } else if (filter == null) {
+            /*
+             * This bundle starts automatically with monitor-1.0 and connection pool dependent features
+             * If `filter` is null, we'll assume that there was no config and we will enable by default.
+             */
+            isConnPoolEnabled = true;
+        }
+
+        if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
+            Tr.debug(tc, String.format("ConnectionPool filter is enabled set to: [%s]", isConnPoolEnabled));
+        }
     }
 
 }
