4c71f12d057039890eea5f7fd8bde2afdbec6702
Kyle Aure <KyleJAure@gmail.com>
2024-08-30 09:11:15 -0500

    Update yasson to 3.0.4

--- dev/com.ibm.ws.jsonb_fat/fat/src/com/ibm/ws/jsonb/fat/FATSuite.java
+++ dev/com.ibm.ws.jsonb_fat/fat/src/com/ibm/ws/jsonb/fat/FATSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2023 IBM Corporation and others.
+ * Copyright (c) 2017, 2024 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -190,11 +190,11 @@ public class FATSuite {
             server.addEnvVar("JSONP_JAR", "fake-json-p.jar");
 
             //TODO replace with a third party implementation when one is developed
-            server.addEnvVar("JSONB_PATH", "yasson/3.0.3/");
+            server.addEnvVar("JSONB_PATH", "yasson/3.0.4/");
             server.addEnvVar("JSONB_JAR", "yasson.jar");
             server.addEnvVar("JSONB_ALT_JAR", "yasson.jar");
 
-            server.addEnvVar("YASSON_PATH", "yasson/3.0.3/");
+            server.addEnvVar("YASSON_PATH", "yasson/3.0.4/");
             server.addEnvVar("YASSON_JAR", "yasson.jar");
         } else if (JakartaEEAction.isEE9Active()) {
             server.addEnvVar("JSONP_PATH", "johnzon/1.2.18/jakarta/");
