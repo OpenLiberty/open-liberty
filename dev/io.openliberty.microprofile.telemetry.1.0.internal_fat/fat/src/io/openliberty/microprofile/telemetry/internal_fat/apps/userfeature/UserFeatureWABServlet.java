a7fb31bbc95a0f49834c20afb20e3ff75f3edd67
Benjamin Confino <benjamic@uk.ibm.com>
2024-09-10 17:03:33 +0100

    Create test for servlet inside user feature with telemetry

---
+++ dev/io.openliberty.microprofile.telemetry.1.0.internal_fat/fat/src/io/openliberty/microprofile/telemetry/internal_fat/apps/userfeature/UserFeatureWABServlet.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2024 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package io.openliberty.microprofile.telemetry.internal_fat.apps.userfeature;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("servletInsideApp")
+public class UserFeatureWABServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletOutputStream sos = response.getOutputStream();
+
+        System.out.println("Servlet inside app filtered by WAB");
+        sos.println("telemetry enabled: " + request.getAttribute("telemetryEnabled"));
+    }
+
+}
