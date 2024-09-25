1689bd7e7ef6d5751113f70972e4c45e44a560e7
Tom Evans <tevans@uk.ibm.com>
2024-09-25 12:51:03 +0100

    Fix RepeatWithJPA20 to properly extend EE7 again

--- dev/com.ibm.ws.jpa.tests.jpa_fat/fat/src/io/openliberty/jpa/RepeatWithJPA20.java
+++ dev/com.ibm.ws.jpa.tests.jpa_fat/fat/src/io/openliberty/jpa/RepeatWithJPA20.java
@@ -11,25 +11,41 @@
 package io.openliberty.jpa;
 
 import java.io.File;
+import java.util.HashSet;
 import java.util.Set;
 
 import com.ibm.websphere.simplicity.log.Log;
 
+import componenttest.rules.repeater.EE6FeatureReplacementAction;
 import componenttest.rules.repeater.EE7FeatureReplacementAction;
+import componenttest.rules.repeater.EE8FeatureReplacementAction;
+import componenttest.rules.repeater.FeatureReplacementAction;
+import componenttest.rules.repeater.JakartaEE10Action;
+import componenttest.rules.repeater.JakartaEE11Action;
+import componenttest.rules.repeater.JakartaEE9Action;
 import componenttest.topology.impl.LibertyServer;
 import componenttest.topology.impl.LibertyServerFactory;
 
 /**
  *
  */
-public class RepeatWithJPA20 extends EE7FeatureReplacementAction {
-    public static final String ID = "JPA20_FEATURES";
+public class RepeatWithJPA20 extends FeatureReplacementAction {
+    public static final String ID = EE7FeatureReplacementAction.ID + "_JPA20";
 
     public RepeatWithJPA20() {
-        Set<String> addFeatures = getAddFeatures();
+        Set<String> addFeatures = new HashSet<>(EE7FeatureReplacementAction.EE7_FEATURE_SET);
         addFeatures.remove("jpa-2.1");
         addFeatures.add("jpa-2.0");
-        this.withID(ID);
+
+        addFeatures(addFeatures);
+
+        removeFeatures(EE6FeatureReplacementAction.EE6_FEATURE_SET);
+        removeFeatures(EE8FeatureReplacementAction.EE8_FEATURE_SET);
+        removeFeatures(JakartaEE9Action.EE9_FEATURE_SET);
+        removeFeatures(JakartaEE10Action.EE10_FEATURE_SET);
+        removeFeatures(JakartaEE11Action.EE11_FEATURE_SET);
+        forceAddFeatures(false);
+        withID(ID);
     }
 
     @Override
@@ -39,10 +55,5 @@ public class RepeatWithJPA20 extends EE7FeatureReplacementAction {
         File jpa20Feature = new File(server.getInstallRoot() + "/lib/features/com.ibm.websphere.appserver.jpa-2.0.mf");
         Log.info(getClass(), "isEnabled", "Does the jpa-2.0 feature exist? " + jpa20Feature.exists());
         return jpa20Feature.exists();
-    }
-
-    @Override
-    public String toString() {
-        return "Set JPA feature to 2.0 version";
     }
 }
