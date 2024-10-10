8b18659f566458d411528c75f7cfa5af3f6433fe
yasmin-aumeeruddy <yasmin.aumeeruddy@ibm.com>
2024-10-10 10:42:55 +0100

    Update grpc version

--- dev/com.ibm.ws.grpc_fat/generated-src/com/ibm/test/g3store/grpc/AppConsumerServiceGrpc.java
+++ dev/com.ibm.ws.grpc_fat/generated-src/com/ibm/test/g3store/grpc/AppConsumerServiceGrpc.java
@@ -8,7 +8,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
  * </pre>
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.57.2)",
+    value = "by gRPC proto compiler (version 1.68.0)",
     comments = "Source: ConsumerStore.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class AppConsumerServiceGrpc {
