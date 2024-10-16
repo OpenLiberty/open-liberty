7b0011ab188206f9e97827c5cdcb55164913d75e
Nathan Rauh <nathan.rauh@us.ibm.com>
2024-10-16 09:32:11 -0500

    Remove comparison annotations that are not in Jakarta Data

--- dev/io.openliberty.data.internal_fat_exp/test-applications/DataExperimentalWeb/src/test/jakarta/data/experimental/web/PrimeNumbers.java
+++ dev/io.openliberty.data.internal_fat_exp/test-applications/DataExperimentalWeb/src/test/jakarta/data/experimental/web/PrimeNumbers.java
@@ -12,6 +12,15 @@
  *******************************************************************************/
 package test.jakarta.data.experimental.web;
 
+import static io.openliberty.data.repository.Is.Op.GreaterThan;
+import static io.openliberty.data.repository.Is.Op.GreaterThanEqual;
+import static io.openliberty.data.repository.Is.Op.IgnoreCase;
+import static io.openliberty.data.repository.Is.Op.LessThan;
+import static io.openliberty.data.repository.Is.Op.LessThanEqual;
+import static io.openliberty.data.repository.Is.Op.LikeIgnoreCase;
+import static io.openliberty.data.repository.Is.Op.NotSuffixed;
+import static io.openliberty.data.repository.Is.Op.Substringed;
+import static io.openliberty.data.repository.Is.Op.Suffixed;
 import static jakarta.data.repository.By.ID;
 
 import java.util.List;
@@ -26,18 +35,10 @@ import jakarta.data.repository.Repository;
 
 import io.openliberty.data.repository.Count;
 import io.openliberty.data.repository.Exists;
+import io.openliberty.data.repository.Is;
 import io.openliberty.data.repository.Or;
 import io.openliberty.data.repository.Select;
-import io.openliberty.data.repository.comparison.Contains;
-import io.openliberty.data.repository.comparison.EndsWith;
-import io.openliberty.data.repository.comparison.GreaterThan;
-import io.openliberty.data.repository.comparison.GreaterThanEqual;
-import io.openliberty.data.repository.comparison.LessThan;
-import io.openliberty.data.repository.comparison.LessThanEqual;
-import io.openliberty.data.repository.comparison.Like;
 import io.openliberty.data.repository.function.CharCount;
-import io.openliberty.data.repository.function.IgnoreCase;
-import io.openliberty.data.repository.function.Not;
 import io.openliberty.data.repository.function.Trimmed;
 
 /**
@@ -48,62 +49,62 @@ import io.openliberty.data.repository.function.Trimmed;
 public interface PrimeNumbers {
 
     @Exists
-    boolean anyLessThanEndingWithBitPattern(@By("numberId") @LessThan long upperLimit,
-                                            @By("binaryDigits") @EndsWith String pattern);
+    boolean anyLessThanEndingWithBitPattern(@By("numberId") @Is(LessThan) long upperLimit,
+                                            @By("binaryDigits") @Is(Suffixed) String pattern);
 
     @Count
-    long howManyIn(@By(ID) @GreaterThanEqual long min,
-                   @By(ID) @LessThanEqual long max);
+    long howManyIn(@By(ID) @Is(GreaterThanEqual) long min,
+                   @By(ID) @Is(LessThanEqual) long max);
 
     @Count
-    Long howManyBetweenExclusive(@By("NumberId") @GreaterThan long exclusiveMin,
-                                 @By("NumberId") @LessThan long exclusiveMax);
+    Long howManyBetweenExclusive(@By("NumberId") @Is(GreaterThan) long exclusiveMin,
+                                 @By("NumberId") @Is(LessThan) long exclusiveMax);
 
     @Find
     @OrderBy(value = ID, descending = true)
-    List<Long> inRangeHavingNumeralLikeAndSubstringOfName(@By(ID) @GreaterThanEqual long min,
-                                                          @By(ID) @LessThanEqual long max,
-                                                          @By("romanNumeral") @IgnoreCase @Like String pattern,
-                                                          @By("name") @Contains String nameSuffix);
+    List<Long> inRangeHavingNumeralLikeAndSubstringOfName(@By(ID) @Is(GreaterThanEqual) long min,
+                                                          @By(ID) @Is(LessThanEqual) long max,
+                                                          @By("romanNumeral") @Is(LikeIgnoreCase) String pattern,
+                                                          @By("name") @Is(Substringed) String nameSubstring);
 
     @Exists
     boolean isFoundWith(long numberId, String hex);
 
     @Find
     @OrderBy(value = "numberId", descending = true)
-    Stream<PrimeNum> lessThanWithSuffixOrBetweenWithSuffix(@By(ID) @LessThan long numLessThan,
-                                                           @By("name") @EndsWith String firstSuffix,
-                                                           @Or @By(ID) @GreaterThanEqual long lowerLimit,
-                                                           @By(ID) @LessThanEqual long upperLimit,
-                                                           @By("name") @EndsWith String secondSuffix);
+    Stream<PrimeNum> lessThanWithSuffixOrBetweenWithSuffix(@By(ID) @Is(LessThan) long numLessThan,
+                                                           @By("name") @Is(Suffixed) String firstSuffix,
+                                                           @Or @By(ID) @Is(GreaterThanEqual) long lowerLimit,
+                                                           @By(ID) @Is(LessThanEqual) long upperLimit,
+                                                           @By("name") @Is(Suffixed) String secondSuffix);
 
     @Find
     @OrderBy(ID)
-    List<Long> notWithinButBelow(@By(ID) @LessThan int rangeMin,
-                                 @Or @By(ID) @GreaterThan int rangeMax,
-                                 @By(ID) @LessThan int below);
+    List<Long> notWithinButBelow(@By(ID) @Is(LessThan) int rangeMin,
+                                 @Or @By(ID) @Is(GreaterThan) int rangeMax,
+                                 @By(ID) @Is(LessThan) int below);
 
     @Count
     int numEvenWithSumOfBits(int sumOfBits, boolean even);
 
     @Find
     @OrderBy("name")
-    Stream<PrimeNum> whereNameLengthWithin(@By("name") @CharCount @GreaterThanEqual int minLength,
-                                           @By("name") @CharCount @LessThanEqual int maxLength);
+    Stream<PrimeNum> whereNameLengthWithin(@By("name") @CharCount @Is(GreaterThanEqual) int minLength,
+                                           @By("name") @CharCount @Is(LessThanEqual) int maxLength);
 
     @Find
-    Optional<PrimeNum> withAnyCaseName(@By("name") @Trimmed @IgnoreCase String name);
+    Optional<PrimeNum> withAnyCaseName(@By("name") @Trimmed @Is(IgnoreCase) String name);
 
     @Find
     List<PrimeNum> withNameLengthAndWithin(@By("name") @Trimmed @CharCount int length,
-                                           @By(ID) @GreaterThanEqual long min,
-                                           @By(ID) @LessThanEqual long max);
+                                           @By(ID) @Is(GreaterThanEqual) long min,
+                                           @By(ID) @Is(LessThanEqual) long max);
 
     @Find
     @Select("name")
-    List<String> withRomanNumeralSuffixAndWithoutNameSuffix(@By("romanNumeral") @EndsWith String numeralSuffix,
-                                                            @By("name") @Not @EndsWith String nameSuffixToExclude,
-                                                            @By(ID) @LessThanEqual long max);
+    List<String> withRomanNumeralSuffixAndWithoutNameSuffix(@By("romanNumeral") @Is(Suffixed) String numeralSuffix,
+                                                            @By("name") @Is(NotSuffixed) String nameSuffixToExclude,
+                                                            @By(ID) @Is(LessThanEqual) long max);
 
     @Insert
     void write(PrimeNum... primes);
