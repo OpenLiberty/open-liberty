363c44f229f69e83129dc3bb8670104628823802
Paul Nicolucci <pnicolucci@gmail.com>
2024-10-21 14:23:10 -0400

    Create tests for the BeanELResolver

--- dev/io.openliberty.org.apache.jasper.expressionLanguage.6.0_fat/fat/src/io/openliberty/org/apache/jasper/expressionLanguage60/fat/FATSuite.java
+++ dev/io.openliberty.org.apache.jasper.expressionLanguage.6.0_fat/fat/src/io/openliberty/org/apache/jasper/expressionLanguage60/fat/FATSuite.java
@@ -17,6 +17,8 @@ import org.junit.runners.Suite.SuiteClasses;
 import com.ibm.ws.fat.util.FatLogHandler;
 
 import io.openliberty.org.apache.jasper.expressionLanguage60.fat.tests.EL60ArrayLengthTest;
+import io.openliberty.org.apache.jasper.expressionLanguage60.fat.tests.EL60BeanELResolverBeanSupportFullTest;
+import io.openliberty.org.apache.jasper.expressionLanguage60.fat.tests.EL60BeanELResolverBeanSupportStandaloneTest;
 import io.openliberty.org.apache.jasper.expressionLanguage60.fat.tests.EL60ModuleVisibilityTest;
 import io.openliberty.org.apache.jasper.expressionLanguage60.fat.tests.EL60OptionalELResolverTest;
 import io.openliberty.org.apache.jasper.expressionLanguage60.fat.tests.EL60RecordELResolverTest;
@@ -37,6 +39,8 @@ import io.openliberty.org.apache.jasper.expressionLanguage60.fat.tests.EL60Recor
 @RunWith(Suite.class)
 @SuiteClasses({
                 EL60ArrayLengthTest.class,
+                EL60BeanELResolverBeanSupportFullTest.class,
+                EL60BeanELResolverBeanSupportStandaloneTest.class,
                 EL60ModuleVisibilityTest.class,
                 EL60OptionalELResolverTest.class,
                 EL60RecordELResolverTest.class
