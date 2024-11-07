98ac4c6d12c1428e7edc5b00584250bfffb44b57
Joe Chacko <143064+joe-chacko@users.noreply.github.com>
2024-11-07 00:35:47 +0000

    refactor: use annotations instead of XML for metatype

--- dev/com.ibm.ws.jndi.iiop/src/com/ibm/ws/jndi/iiop/CorbalocUrlContextFactory.java
+++ dev/com.ibm.ws.jndi.iiop/src/com/ibm/ws/jndi/iiop/CorbalocUrlContextFactory.java
@@ -19,13 +19,29 @@ import javax.naming.spi.ObjectFactory;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
+import com.ibm.ws.bnd.metatype.annotation.Ext;
 import com.ibm.ws.transport.iiop.spi.ClientORBRef;
 import com.ibm.wsspi.application.lifecycle.ApplicationPrereq;
 import com.ibm.wsspi.application.lifecycle.ApplicationRecycleComponent;
 
-@Component(configurationPolicy=REQUIRE,property={"service.vendor=ibm","osgi.jndi.url.scheme=corbaloc"})
-public class CorbalocUrlContextFactory extends UrlContextFactory  implements ObjectFactory, ApplicationRecycleComponent, ApplicationPrereq {
+@ObjectClassDefinition(name = "internal", description = "internal use only", localization = "OSGI-INF/l10n/metatype")
+@Ext.ObjectClassClass(ApplicationPrereq.class)
+@interface CorbalocUrlContextFactoryOcd {
+    @AttributeDefinition(name = "internal", description = "internal use only")
+    String id();
+    @AttributeDefinition(name = "internal", description = "internal use only", defaultValue = "${id}")
+    @Ext.Unique(ApplicationPrereq.APP_PREREQ_ID)
+    @Ext.Final
+    String application_prereq_id();
+}
+
+@Component(configurationPolicy = REQUIRE, property = "osgi.jndi.url.scheme=corbaloc")
+@Designate(ocd = CorbalocUrlContextFactoryOcd.class)
+public class CorbalocUrlContextFactory extends UrlContextFactory implements ObjectFactory, ApplicationRecycleComponent, ApplicationPrereq {
     @Activate
     public CorbalocUrlContextFactory(@Reference ClientORBRef orbRef) {
         super(orbRef);
--- dev/com.ibm.ws.jndi.iiop/src/com/ibm/ws/jndi/iiop/CorbanameUrlContextFactory.java
+++ dev/com.ibm.ws.jndi.iiop/src/com/ibm/ws/jndi/iiop/CorbanameUrlContextFactory.java
@@ -14,24 +14,33 @@ package com.ibm.ws.jndi.iiop;
 
 import static org.osgi.service.component.annotations.ConfigurationPolicy.REQUIRE;
 
-import java.nio.charset.StandardCharsets;
-import java.util.BitSet;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import javax.naming.spi.ObjectFactory;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
-import com.ibm.websphere.ras.Tr;
-import com.ibm.websphere.ras.TraceComponent;
+import com.ibm.ws.bnd.metatype.annotation.Ext;
 import com.ibm.ws.transport.iiop.spi.ClientORBRef;
 import com.ibm.wsspi.application.lifecycle.ApplicationPrereq;
 import com.ibm.wsspi.application.lifecycle.ApplicationRecycleComponent;
 
-@Component(configurationPolicy=REQUIRE,property={"service.vendor=ibm","osgi.jndi.url.scheme=corbaname"})
+@ObjectClassDefinition(name = "internal", description = "internal use only", localization = "OSGI-INF/l10n/metatype")
+@Ext.ObjectClassClass(ApplicationPrereq.class)
+@interface CorbanameUrlContextFactoryOcd {
+    @AttributeDefinition(name = "internal", description = "internal use only")
+    String id();
+    @AttributeDefinition(name = "internal", description = "internal use only", defaultValue = "${id}")
+    @Ext.Unique(ApplicationPrereq.APP_PREREQ_ID)
+    @Ext.Final
+    String application_prereq_id();
+}
+
+@Component(configurationPolicy = REQUIRE, property = "osgi.jndi.url.scheme=corbaname")
+@Designate(ocd = CorbanameUrlContextFactoryOcd.class)
 public class CorbanameUrlContextFactory extends UrlContextFactory implements ObjectFactory, ApplicationRecycleComponent, ApplicationPrereq {
     @Activate
     public CorbanameUrlContextFactory(@Reference ClientORBRef orbRef) {
