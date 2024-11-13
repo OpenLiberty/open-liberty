a2a2dc21750c2b7d93c6ff822e484984a5ccca10
Joe Chacko <143064+joe-chacko@users.noreply.github.com>
2024-11-13 14:37:54 +0000

    refactor: encapsulate recovery manager class

--- dev/com.ibm.tx.jta/src/com/ibm/tx/jta/impl/RecoveryManager.java
+++ dev/com.ibm.tx.jta/src/com/ibm/tx/jta/impl/RecoveryManager.java
@@ -126,16 +126,10 @@ public class RecoveryManager implements Runnable {
     // The following relate to the service data recoverable unit in the transaction log
     // This is reserved and holds the servername, applid and epoch.
     //
-    protected RecoverableUnit _tranlogServiceData;
-    protected RecoverableUnitSection _tranlogServerSection;
-    protected RecoverableUnitSection _tranlogApplIdSection;
-    protected RecoverableUnitSection _tranlogEpochSection;
-
-    protected String _classPath; // current classpath for recovery
-    // These are static as they are only initialized from the "server's" own log
-    // We use our own classpaths and not from other servers in case we recover
-    // for a filesystem that does not match our own.
-    protected static String _loggedClassPath; // classpath read from the log at startup
+    private RecoverableUnit _tranlogServiceData;
+    private RecoverableUnitSection _tranlogServerSection;
+    private RecoverableUnitSection _tranlogApplIdSection;
+    private RecoverableUnitSection _tranlogEpochSection;
 
     // Server States logged for serviceability
     private static final int STARTING = 1;
