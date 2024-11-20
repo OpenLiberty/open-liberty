8c4dd963565d8022d8777d9c26888446520ec0b5
Chuck Bridgham <cbridgha@us.ibm.com>
2024-11-20 18:01:15 -0500

    Removing transitive test dep

--- dev/build.depScanner/src/io/openliberty/depScanner/BestMatch.java
+++ dev/build.depScanner/src/io/openliberty/depScanner/BestMatch.java
@@ -296,6 +296,7 @@ public class BestMatch {
         return (library.getGroupId().equals("org.glassfish") && (library.getArtifactId().equals("javax.faces"))) ||
                (library.getArtifactId().equals("tomcat-embed-core")) ||
                (library.getArtifactId().equals("woodstox-core") && (library.getVersion().equals("6.2.6") || library.getVersion().equals("6.2.4"))) ||
+               (library.getArtifactId().equals("commons-io") && (library.getVersion().equals("2.13.0"))) ||
                (library.getArtifactId().equals("netty-codec-http")
                 && (library.getVersion().equals("4.1.73.Final") || library.getVersion().equals("4.1.78.Final") || library.getVersion().equals("4.1.92.Final")))
                ||
