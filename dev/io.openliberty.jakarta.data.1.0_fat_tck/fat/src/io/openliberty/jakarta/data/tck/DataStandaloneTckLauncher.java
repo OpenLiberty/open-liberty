2989d2e75a62795a6f20cf2bf0d27e0c23acc44a
Reshmi S Menon <reshmi.menon@ibm.com>
2025-01-21 15:11:35 +0530

    first_commit

--- dev/io.openliberty.jakarta.data.1.0_fat_tck/fat/src/io/openliberty/jakarta/data/tck/DataStandaloneTckLauncher.java
+++ dev/io.openliberty.jakarta.data.1.0_fat_tck/fat/src/io/openliberty/jakarta/data/tck/DataStandaloneTckLauncher.java
@@ -45,8 +45,7 @@ public class DataStandaloneTckLauncher {
     /**
      * Run the TCK (controlled by autoFVT/publish/tckRunner/tck/*)
      */
-    @Test
-    @Ignore("jnosql does not support static metamodel yet")
+    @Test   
     @AllowedFFDC // The tested exceptions cause FFDC so we have to allow for this.
     public void launchDataTckStandaloneNoSQL() throws Exception {
         // Test groups to run
