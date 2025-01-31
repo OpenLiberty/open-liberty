6bdaab99faaef2c6056c5d6bf828ea2edca555e0
mansal <mansal@us.ibm.com>
2025-01-31 12:35:02 -0500

    Refactoring cookie tests to organize inbound, outbound, and edge cases

--- dev/com.ibm.ws.transport.http/test/com/ibm/ws/netty/cookie/CookieTests.java
+++ dev/com.ibm.ws.transport.http/test/com/ibm/ws/netty/cookie/CookieTests.java
@@ -21,6 +21,8 @@ import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
 import com.ibm.ws.http.channel.internal.HttpChannelConfig;
 import com.ibm.ws.http.channel.internal.inbound.HttpInboundServiceContextImpl;
@@ -29,7 +31,7 @@ import com.ibm.wsspi.channelfw.VirtualConnection;
 import com.ibm.wsspi.http.HttpCookie;
 import com.ibm.wsspi.http.channel.HttpServiceContext;
 import com.ibm.wsspi.http.channel.inbound.HttpInboundServiceContext;
-
+import com.ibm.wsspi.http.channel.values.HttpHeaderKeys;
 import com.ibm.ws.http.netty.message.NettyBaseMessage;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
@@ -44,110 +46,166 @@ import io.netty.handler.codec.http.HttpVersion;
 
 import com.ibm.wsspi.http.channel.HttpServiceContext;
 
+/**
+ * Provides a series of tests verifying the cookie parsing (inbound)
+ * and marshalling (outbound) functionalities of the {@link NettyBaseMessage}.
+ * It uses a {@link TestableNettyMessage}, in place of the formal
+ * {@code NettyRequestMessage} or {@code NettyResponseMessage}, in order
+ * to minimize dependencies and amount of required mocked objects.
+ */
+@RunWith(Enclosed.class)
+public class CookieTests {
 
-public class CookieTests{
-
-    private ChannelHandlerContext context;
-    private Channel channel;
-    private ChannelPipeline pipeline;
-    private VirtualConnection virtualConnection;
-    private HttpInboundServiceContextImpl spyInboundCtx;
-
-    @Before
-    public void setUp() {
-        context = mock(ChannelHandlerContext.class);
-        channel = mock(Channel.class);
-        pipeline = mock(ChannelPipeline.class);
-
-        // Stub so no null addresses
-        when(context.channel()).thenReturn(channel);
-        when(channel.pipeline()).thenReturn(pipeline);
-
-        InetSocketAddress local = new InetSocketAddress("127.0.0.1", 1234);
-        InetSocketAddress remote = new InetSocketAddress("192.168.1.100", 5678);
-        when(channel.localAddress()).thenReturn(local);
-        when(channel.remoteAddress()).thenReturn(remote);
-
-        virtualConnection = mock(VirtualConnection.class);
-
-        HttpInboundServiceContextImpl realImpl = new HttpInboundServiceContextImpl(context, virtualConnection);
-        spyInboundCtx = spy(realImpl);
+    /**
+     * Indicates whether the message should treate cookies as inbound
+     * (parse "Cookie") or outbound (write "Set-Cookie").
+     */
+    private enum Mode{
+        INBOUND, OUTBOUND
     }
+    private static HttpChannelConfig channelConfig;
+    private static HttpServiceContext serviceContext;
 
-    @Test
-    public void testRequestCookiesInbound() {
-    
-        doReturn(true).when(spyInboundCtx).isInboundConnection();
-
-        FullHttpRequest req = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/test");
-        req.headers().add("Cookie", "session=abc123; foo=bar");
-
-        // Construct the NettyRequestMessage
-        NettyRequestMessage message = new NettyRequestMessage(req, spyInboundCtx, context);
-
-        // Verify cookie parsing
-        HttpCookie session = message.getCookie("session");
-        assertThat(session, notNullValue());
-        assertThat(session.getValue(), is("abc123"));
-
-        HttpCookie foo = message.getCookie("foo");
-        assertThat(foo, notNullValue());
-        assertThat(foo.getValue(), is("bar"));
-    }
-
-    @Test
-    public void testInboundCookieWithDomainAndPathAttributes() {
-        doReturn(true).when(spyInboundCtx).isInboundConnection();
-
-        FullHttpRequest req = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/test");
-        req.headers().add("Cookie", "name1=value1; $Version=1; $Domain=myhost; $Path=/servlet_jsh_cookie_web");
-        NettyRequestMessage message = new NettyRequestMessage(req, spyInboundCtx, context);
-
-        List<HttpCookie> cookies = message.getAllCookies();
-
-        assertThat(cookies, hasSize(1));
-
-        HttpCookie cookie = cookies.get(0);
-        assertThat(cookie.getName(), is("name1"));
-        assertThat(cookie.getValue(), is("value1"));
-        assertThat(cookie.getDomain(), is("myhost"));
-        assertThat(cookie.getPath(), is("/servlet_jsh_cookie_web"));
-
-    }
-
-    @Test
-    public void testNettyMessage(){
-        TestableNettyMessage message = new TestableNettyMessage();
-        message.incoming(true);
-
+    /**
+     * Builds a testable message configured to be either inbound or outbound. 
+     * The direction flow allows testing the functionality of cookie parsing
+     * for {@link NettyBaseMessage}.
+     * 
+     * @param inbound {@link Mode} indicating if message is inbound or outbound
+     * @return an initialized testable message object
+     */
+    private static TestableNettyMessage createMessage(Mode mode){
         DefaultHttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/test");
-        request.headers().add("Cookie", "session=abc; foo=bar");
-        HttpChannelConfig mockConfig = mock(HttpChannelConfig.class);
-        when(mockConfig.getLimitOnNumberOfHeaders()).thenReturn(100);
-        when(mockConfig.getLimitOfFieldSize()).thenReturn(1024);
 
-        HttpServiceContext serviceContext = mock(HttpServiceContext.class);
-        message.testInit(request, serviceContext, mockConfig);
+        TestableNettyMessage message = new TestableNettyMessage();
+        message.incoming(Mode.INBOUND.equals(mode)? true:false);
+        message.testInit(request, serviceContext, channelConfig);
 
-        HttpCookie session = message.getCookie("session");
-        assertThat(session, notNullValue());
-        assertThat(session.getValue(), is("abc"));
-
-        HttpCookie foo = message.getCookie("foo");
-        assertThat(foo, notNullValue());
-        assertThat(foo.getValue(), is("bar"));
-        
+        return message;
     }
 
-    private class TestableNettyMessage extends NettyBaseMessage {
+    /**
+     * Configures the mocked configurations and objects that will be
+     * used and shared by the test methods. The {@code NettyBaseMessage.init(...)}
+     * makes use of these objects for each tested scenario.
+     * 
+     * Any additional required mocked behavior should be provided by the specific
+     * test requiring it.
+     */
+    public static void commonSetup(){
+        channelConfig = mock(HttpChannelConfig.class);
+        when(channelConfig.getLimitOnNumberOfHeaders()).thenReturn(100);
+        when(channelConfig.getLimitOfFieldSize()) .thenReturn(1024);
 
-        public TestableNettyMessage(){}
+        serviceContext = mock(HttpServiceContext.class);
+    }
 
-        public void testInit(HttpMessage message, HttpServiceContext context, HttpChannelConfig config){
+    public static class InboundTests{
+        Mode mode = Mode.INBOUND;
+        @Before
+        public void setup(){
+            CookieTests.commonSetup();
+        }
+
+        @Test
+        public void testRequestCookiesInbound() {
+
+            TestableNettyMessage message = createMessage(mode);
+            message.getNettyHeaders().add("Cookie", "session=abc123; foo=bar");
+
+            HttpCookie session = message.getCookie("session"); 
+            assertThat(session, notNullValue());
+            assertThat(session.getValue(), is("abc123"));
+
+            HttpCookie foo = message.getCookie("foo");   
+            assertThat(foo, notNullValue());
+            assertThat(foo.getValue(), is("bar"));
+        }
+
+        @Test
+        public void testInboundCookieWithDomainAndPath() {
+            TestableNettyMessage message = createMessage(mode);
+            message.getNettyHeaders().add("Cookie", "name1=value1; $Version=1; $Domain=myhost; $Path=/servlet_jsh_cookie_web");
+            List<HttpCookie> cookies = message.getAllCookies();
+
+            assertThat(cookies, hasSize(1));
+            HttpCookie cookie = cookies.get(0);
+            assertThat(cookie.getName(), is("name1"));
+            assertThat(cookie.getValue(),  is("value1"));
+            assertThat(cookie.getDomain(), is("myhost"));
+            assertThat(cookie.getPath(), is("/servlet_jsh_cookie_web"));
+    
+        }
+
+
+    }
+
+    public static class OutboundTests{
+        Mode mode = Mode.OUTBOUND;
+        @Before
+        public void setup(){
+            CookieTests.commonSetup();
+        }
+
+        @Test
+        public void testOutboundCookieMarshalling() {
+
+            TestableNettyMessage message = createMessage(mode);
+
+            HttpCookie cookie = new HttpCookie("testCookie", "val123");
+            message.setCookie(cookie, HttpHeaderKeys.HDR_SET_COOKIE);
+            message.processCookies();
+
+            HttpHeaders headers  = message.getNettyHeaders();
+            List<String> setCookies = headers.getAll("Set-Cookie");
+            assertThat(setCookies, hasSize(1));
+            assertThat(setCookies.get(0), containsString("testCookie=val123"));
+        }
+
+    }
+
+    public static class EdgeCaseTests{
+
+        @Before
+        public void setup(){
+            CookieTests.commonSetup();
+        }
+
+        @Test 
+        public void testsMalformedCookieNoName(){
+            TestableNettyMessage message = createMessage(Mode.INBOUND);
+            message.getNettyHeaders().add("Cookie", "=badNameWithValue");
+            List<HttpCookie> cookies = message.getAllCookies();
+            assertThat(cookies, is(empty()));
+        }
+
+        //Backward compatibility
+        //utf-8 values
+        //case sensitivity
+        //trailing comma-semicolon
+        //removing cookie
+        //duplicate cookie
+        //quoted values
+
+    }
+
+    /**
+     * Minimal testing class for {@link NettyBaseMessage} that provides the following:
+     *  -> A toggle for {@code incoming(boolean)} to set inbound or outbound mode, to
+     *     test different cookie handling paths.
+     *  -> A {@code testInit(...)} method to invoke the base class initialization with 
+     *     minimal arguments.
+     * 
+     * For ease, this class also provides a {@code getNettyHeaders()} for direct inspection
+     * of the underlying header lines that would be passed on to the pipeline or container.
+     */
+    private static class TestableNettyMessage extends NettyBaseMessage {
+
+        public void testInit(HttpMessage message, HttpServiceContext context, HttpChannelConfig config) {
             super.init(message, context, config);
         }
 
-        public HttpHeaders getNettyHeaders(){
+        public HttpHeaders getNettyHeaders() {
             return this.headers;
         }
     }
