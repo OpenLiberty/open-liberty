2350cc137c7ccace8ef93930610c43c4695c5d5c
idlewis <idlewis@users.noreply.github.com>
2025-02-10 15:12:29 +0000

    Fully read heartbeat stream in RESTMBeanServerConnection
    The server response to the heartbeat/notification request is
    the class name string 'com.ibm.ws.kernel.boot.jmx.internal.PlatformMBeanServer'.
    This needs to be read, and the stream closed, to allow the HTTPURLConnection
    to be properly pooled/reused. Not reading the stream was causing dangling
    sockets in close_wait state

--- dev/com.ibm.ws.jmx.connector.client.rest/src/com/ibm/ws/jmx/connector/client/rest/internal/RESTMBeanServerConnection.java
+++ dev/com.ibm.ws.jmx.connector.client.rest/src/com/ibm/ws/jmx/connector/client/rest/internal/RESTMBeanServerConnection.java
@@ -12,6 +12,7 @@
  *******************************************************************************/
 package com.ibm.ws.jmx.connector.client.rest.internal;
 
+import java.io.InputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ConnectException;
@@ -2377,6 +2378,13 @@ class RESTMBeanServerConnection implements MBeanServerConnection {
                                     }
                                 } else {
                                     //no-op for failover polling, just break into the sleep segment
+                                    // Server sends a string back that needs to be consumed to close the connection
+                                    InputStream is = connection.getInputStream();
+                                    int data = is.read();
+                                    while (data != -1) {
+                                        data = is.read();
+                                    }
+                                    is.close();
                                 }
                                 break;
                             } catch (ClassNotFoundException cnf) {
