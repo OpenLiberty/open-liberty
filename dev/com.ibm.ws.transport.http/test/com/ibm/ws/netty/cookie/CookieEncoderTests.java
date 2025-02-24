09f7f98ad33e9a8b137128be5f3b1251c4422b4b
mansal <mansal@us.ibm.com>
2025-02-24 15:17:45 -0500

    Updating copyright

--- dev/com.ibm.ws.transport.http/test/com/ibm/ws/netty/cookie/CookieEncoderTests.java
+++ dev/com.ibm.ws.transport.http/test/com/ibm/ws/netty/cookie/CookieEncoderTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 IBM Corporation and others.
+ * Copyright (c) 2024, 2025 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -23,7 +23,6 @@ import java.util.regex.Pattern;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.omg.CORBA.DynAnyPackage.Invalid;
 
 import com.ibm.ws.http.channel.internal.HttpChannelConfig;
 import com.ibm.ws.http.channel.internal.HttpConfigConstants.SameSite;
