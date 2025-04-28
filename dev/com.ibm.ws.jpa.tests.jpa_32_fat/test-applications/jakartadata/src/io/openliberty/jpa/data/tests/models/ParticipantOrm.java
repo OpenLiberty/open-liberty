0fa2eef0bcb8e2b4307c58f7eb75d2f6fc3b738c
Aiswarya R <Aiswarya.R6@ibm.com>
2025-04-28 17:01:17 +0530

    Implement Java Records mapping using orm.xml Configuration

---
+++ dev/com.ibm.ws.jpa.tests.jpa_32_fat/test-applications/jakartadata/src/io/openliberty/jpa/data/tests/models/ParticipantOrm.java
@@ -0,0 +1,45 @@
+
+/*******************************************************************************
+ * Copyright (c) 2024 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package io.openliberty.jpa.data.tests.models;
+
+public class ParticipantOrm {
+
+    private Integer id;
+
+    private NameOrm name;
+
+    public static ParticipantOrm of(String firstName, String lastName, int id) {
+
+        ParticipantOrm p = new ParticipantOrm();
+        p.id = id;
+        p.name = new NameOrm(firstName, lastName);
+        return p;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public NameOrm getName() {
+        return name;
+    }
+
+    public static record NameOrm(String first, String last) {
+//        public String getFirst() {
+//            return first;
+//        }
+//
+//        public String getLast() {
+//            return last;
+//        }
+    }
+}
