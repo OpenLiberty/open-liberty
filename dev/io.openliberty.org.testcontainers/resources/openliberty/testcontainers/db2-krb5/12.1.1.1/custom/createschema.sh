f402a006c5398981d90a3e8bddc7af68965cfefd
Kyle Aure <KyleJAure@gmail.com>
2025-05-14 14:29:33 -0500

    WIP

--- dev/io.openliberty.org.testcontainers/resources/openliberty/testcontainers/db2-krb5/12.1.1.1/custom/createschema.sh
+++ dev/io.openliberty.org.testcontainers/resources/openliberty/testcontainers/db2-krb5/12.1.1.1/custom/createschema.sh
@@ -1,41 +1,36 @@
 #!/bin/sh
 
-source ${SETUPDIR?}/include/db2_constants
-source ${SETUPDIR?}/include/db2_common_functions
+echo "(*) Verify user"
+whoami
 
 echo "(*) Verify hostname"
-sudo hostname db2
 hostname
 
+# Execute as database user
 sudo -i -u db2inst1 bash << EOF
 
 echo "(*) Configure DB2 database manager to use Kerberos"
-db2 UPDATE DATABASE MANAGER CONFIGURATION USING CLNT_KRB_PLUGIN IBMkrb5
-db2 UPDATE DATABASE MANAGER CONFIGURATION USING AUTHENTICATION KERBEROS
+${DB2_ADMIN}/bin/db2 UPDATE DATABASE MANAGER CONFIGURATION USING CLNT_KRB_PLUGIN IBMkrb5 IMMEDIATE
+${DB2_ADMIN}/bin/db2 UPDATE DATABASE MANAGER CONFIGURATION USING AUTHENTICATION KERBEROS IMMEDIATE
 
 echo "(*) Log database manager configuration"
-db2 GET DATABASE MANAGER CONFIGURATION
+${DB2_ADMIN}/bin/db2 GET DATABASE MANAGER CONFIGURATION
 
-echo "(*) Initialize principal ${DB2INSTANCE?}"
-touch /tmp/kinit.log && export KRB5_TRACE=/tmp/kinit.log
-kinit -k -t /etc/krb5.keytab -c FILE:/tmp/krb5cc_1000 ${DB2INSTANCE?}
-cat /tmp/kinit.log
+echo "(*) Initialize kerberos user"
+kinit -k -t /etc/krb5.keytab db2inst1
 
-echo "(*) Debug credential cache at /tmp/krb5cc_1000"
-ls -la /tmp/krb5cc_1000
-klist /tmp/krb5cc_1000
+echo "(*) Debug keytab file"
+klist -k -t /etc/krb5.keytab
 
-echo "(*) Debug keytab file at /etc/krb5.keytab"
-ls -la /etc/krb5.keytab
-klist -k /etc/krb5.keytab
+echo "(*) Debug credential cache"
+klist -e -f -a
 
-echo "(*) Start database manager"
-db2start
+# ${DB2_ADMIN}/adm/db2start
+# ${DB2_ADMIN}/bin/db2 db2stop
+# ${DB2_ADMIN}/bin/db2 db2start
 
-echo "(*) Restart DB2"
-db2 db2stop
-db2 db2start
-
-EOF
+# ${DB2_ADMIN}/bin/db2 CREATE DATABASE TESTDB
 
 echo "SETUP SCRIPT COMPLETE"
+
+EOF
