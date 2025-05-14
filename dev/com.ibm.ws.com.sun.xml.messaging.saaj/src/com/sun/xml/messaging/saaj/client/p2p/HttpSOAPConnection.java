f53c27a89a63da4d540a0aec5892f57d1e8efec8
Paul Nicolucci <pnicolucci@gmail.com>
2025-05-13 22:38:54 -0400

    Remove available check

--- dev/com.ibm.ws.com.sun.xml.messaging.saaj/src/com/sun/xml/messaging/saaj/client/p2p/HttpSOAPConnection.java
+++ dev/com.ibm.ws.com.sun.xml.messaging.saaj/src/com/sun/xml/messaging/saaj/client/p2p/HttpSOAPConnection.java
@@ -8,6 +8,17 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+/*******************************************************************************
+ * Copyright (c) 2025 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ *******************************************************************************/
+
 package com.sun.xml.messaging.saaj.client.p2p;
 
 import java.io.ByteArrayOutputStream;
@@ -468,8 +479,7 @@ class HttpSOAPConnection extends SOAPConnection {
                 //(or skipped over) from this input stream without blocking
                 //or 0 when it reaches the end of the input stream.
                 if ((httpIn == null )
-                        || (httpConnection.getContentLength() == 0)
-                        || (httpIn.available() == 0)) {
+                        || (httpConnection.getContentLength() == 0)) {
                     response = null;
                     log.warning("SAAJ0014.p2p.content.zero");
                 } else {
