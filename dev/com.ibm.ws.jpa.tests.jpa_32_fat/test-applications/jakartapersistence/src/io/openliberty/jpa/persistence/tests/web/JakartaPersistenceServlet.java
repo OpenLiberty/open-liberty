88dee84c721d96becb48490d5fa6247660e5fcbd
Aiswarya R <Aiswarya.R6@ibm.com>
2025-05-27 21:45:23 +0530

    Added Test case to check availability of UUID generation Type primary key from prepersist method

--- dev/com.ibm.ws.jpa.tests.jpa_32_fat/test-applications/jakartapersistence/src/io/openliberty/jpa/persistence/tests/web/JakartaPersistenceServlet.java
+++ dev/com.ibm.ws.jpa.tests.jpa_32_fat/test-applications/jakartapersistence/src/io/openliberty/jpa/persistence/tests/web/JakartaPersistenceServlet.java
@@ -10,12 +10,14 @@
 package io.openliberty.jpa.persistence.tests.web;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import java.util.List;
 
 import org.junit.Test;
 
 import componenttest.app.FATServlet;
+import io.openliberty.jpa.persistence.tests.models.UUIDIdEntity;
 import jakarta.annotation.Resource;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -57,4 +59,30 @@ public class JakartaPersistenceServlet extends FATServlet {
                         .getResultList();
         assertNotNull(exceptResult);
     }
+
+    /**
+     *
+     * https://jakarta.ee/specifications/persistence/3.2/jakarta-persistence-spec-3.2#a2202
+     * Primary key values generated using the SEQUENCE, TABLE, or UUID strategy are
+     * available in the PrePersist method. Primary key values generated using the
+     * IDENTITY strategy are not available in the PrePersist method
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testPrimaryKeyAvailabilityInUUIDGenerationType() throws Exception {
+        UUIDIdEntity uuiIdEntity = UUIDIdEntity.of("uuid entity 1");
+        tx.begin();
+        try {
+            em.persist(uuiIdEntity);
+            tx.commit();
+        } catch (IllegalStateException e) {
+            System.out.println("testPrimaryKeyAvailabilityInUUIDGenerationType: Exception occured while persisting: " + e.getMessage());
+            fail("ID not available in PrePersist method. Primary key values generated using the UUID strategy are expected to be available in the PrePersist method");
+        } catch (Exception e) {
+            System.out.println("testPrimaryKeyAvailabilityInUUIDGenerationType: Unexpected Exception occured while persisting: " + e.getMessage());
+            fail("Unexpected Exception occured while persisting uuiIdEntity");
+        }
+    }
+
 }
