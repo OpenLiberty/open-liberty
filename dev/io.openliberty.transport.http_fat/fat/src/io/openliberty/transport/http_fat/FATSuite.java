1b534c84811bc7bbcbe24a461a04d5eda712c48d
nidhin-cp <nidhin.cp@ibm.com>
2025-06-23 16:30:29 +0530

    adding the xml files

--- dev/io.openliberty.transport.http_fat/fat/src/io/openliberty/transport/http_fat/FATSuite.java
+++ dev/io.openliberty.transport.http_fat/fat/src/io/openliberty/transport/http_fat/FATSuite.java
@@ -28,7 +28,8 @@ import io.openliberty.transport.http_fat.accesslists.AccessListsTests;
                 SoLingerTests.class,
                 SoReuseAddrTest.class,
                 TcpOptionsDefaultTests.class,
-                ContentTypeResponseHeaderTests.class
+                ContentTypeResponseHeaderTests.class,
+                AccessLogRolloverTest.class
 })
 
 public class FATSuite {
