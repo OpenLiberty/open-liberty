2a1fe5281770376b47062b09b2615276c1796aee
Deval Patel <devalpatel@ibm.com>
2025-07-25 13:17:40 +0100

    new-feature : Logic update for custom header

--- dev/io.openliberty.goingpostal.1.0.internal/src/io/openliberty/goingpostal/servlet/filter/HeaderFilter.java
+++ dev/io.openliberty.goingpostal.1.0.internal/src/io/openliberty/goingpostal/servlet/filter/HeaderFilter.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -15,30 +15,28 @@
  */
 package io.openliberty.goingpostal.servlet.filter;
 
+import java.io.IOException;
+
 import javax.servlet.Filter;
-import javax.servlet.FilterConfig;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;  
 
-public class OpenAPIUIFilter implements Filter {
-        /**
+public class HeaderFilter implements Filter {
+    /**
      * Filters out specific requests and takes the appropriate action for each
-     * 
+     *
      * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
      */
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
-       
+
         if (resp instanceof HttpServletResponse) {
             HttpServletResponse httpServletResp = (HttpServletResponse) resp;
-            httpServletResp.setHeader("X-Frame-Options", "DENY");
-            httpServletResp.setHeader("X-Content-Type-Options", "nosniff");
-            httpServletResp.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline'; connect-src *; style-src 'self' 'unsafe-inline'; img-src 'self' data:");
+            httpServletResp.setHeader("X-Clacks-Overhead", "GNU Terry Pratchett");
             chain.doFilter(req, resp);
         } else {
             chain.doFilter(req, resp);
@@ -49,5 +47,5 @@ public class OpenAPIUIFilter implements Filter {
     public void destroy() {}
 
     @Override
-    public void init(FilterConfig arg0) throws ServletException {}  
+    public void init(FilterConfig arg0) throws ServletException {}
 }
