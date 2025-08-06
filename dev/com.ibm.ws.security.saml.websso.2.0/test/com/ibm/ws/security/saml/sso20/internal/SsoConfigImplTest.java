3e5d4f258f7930bf056a97012718afbbad038cc4
Ut Le <utle@us.ibm.com>
2025-08-06 12:53:46 -0500

    use the spec defined name

--- dev/com.ibm.ws.security.saml.websso.2.0/test/com/ibm/ws/security/saml/sso20/internal/SsoConfigImplTest.java
+++ dev/com.ibm.ws.security.saml.websso.2.0/test/com/ibm/ws/security/saml/sso20/internal/SsoConfigImplTest.java
@@ -598,7 +598,7 @@ public class SsoConfigImplTest {
             e.printStackTrace();
             fail("Unexpected exception was thrown: " + e);
         }
-        SAML_CONFIG_PROPS.put(SsoConfigImpl.KEY_signatureMethodAlgorithm, CryptoUtils.MESSAGE_DIGEST_ALGORITHM_SHA256);
+        SAML_CONFIG_PROPS.put(SsoConfigImpl.KEY_signatureMethodAlgorithm, CryptoUtils.MESSAGE_DIGEST_ALGORITHM_SHA_256);
 
         String result = ssoConfig.getSignatureMethodAlgorithm();
 
