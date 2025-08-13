be5683f4957f5f9b7fc9eb7ca7b27d8ac1bc51a9
Wilalberto Rodriguez <wrodrig@us.ibm.com>
2025-08-13 10:24:04 -0500

    Attempt to fix the BC Issue

--- dev/com.ibm.ws.security.spnego.fat.common/fat/src/com/ibm/ws/security/spnego/fat/config/InitClass.java
+++ dev/com.ibm.ws.security.spnego.fat.common/fat/src/com/ibm/ws/security/spnego/fat/config/InitClass.java
@@ -19,10 +19,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import org.apache.sshd.client.SshClient;
 import org.apache.sshd.client.session.ClientSession;
 import org.junit.Assume;
+import java.security.Security;
 
 import com.ibm.websphere.simplicity.ConnectionInfo;
 import com.ibm.websphere.simplicity.Machine;
@@ -36,8 +38,10 @@ import componenttest.topology.utils.ExternalTestService;
 public class InitClass {
     private static final Class<?> c = InitClass.class;
     /**
-     * Active Directory appears to allow user names with a max of 20 characters. We add "_http" or
-     * "_httpsn" to the end of the host name to create the user name, so the host name we use for
+     * Active Directory appears to allow user names with a max of 20 characters. We
+     * add "_http" or
+     * "_httpsn" to the end of the host name to create the user name, so the host
+     * name we use for
      * the user name must be 15 characters or less.
      */
     public static final int CANONICAL_HOST_NAME_CHAR_LIMIT = 13;
@@ -52,7 +56,7 @@ public class InitClass {
     public static String KDCP_VAR = null;
     public static String KDCS_VAR = null;
 
-    //KDC Users
+    // KDC Users
     public static String KDC_USER = null;
     public static String KDC_USER_PWD = null;
     public static String KDC2_USER = null;
@@ -82,14 +86,14 @@ public class InitClass {
     public static long COMMON_TOKEN_CREATION_DATE = 0;
     public static final double TOKEN_REFRESH_LIFETIME_SECONDS = 180;
     public static boolean RUN_TESTS = true;
-    public static boolean LOCALHOST_DEFAULT_IP_ADDRESS = true; //127.0.0.1       localhost
+    public static boolean LOCALHOST_DEFAULT_IP_ADDRESS = true; // 127.0.0.1 localhost
 
     public static boolean IBM_JDK_V8_LOWER = false;
     public static boolean OTHER_SUPPORT_JDKS = false;
     public static boolean SUN_ORACLE_JDK_V8_HIGHER = false;
     public static boolean IBM_HYBRID_JDK = false;
 
-    //Properties to make sure we only send the scripts once
+    // Properties to make sure we only send the scripts once
     public static boolean sendvbs = true;
     public static boolean needToPushaddSPNKeytab = true;
     public static boolean needToPushsetUserSPN = true;
@@ -112,17 +116,17 @@ public class InitClass {
         List<ExternalTestService> services = null;
 
         try {
-            //obtaining kdcp and kdc_r information
+            // obtaining kdcp and kdc_r information
             services = CommonTest.getKDCServices(2, SPNEGOConstants.KDC_HOST_FROM_CONSUL);
             KDC_HOSTNAME = services.get(0).getAddress();
             KDC_USER = services.get(0).getProperties().get(SPNEGOConstants.MS_KDC_USER_CONSUL);
             KDC_USER_PWD = services.get(0).getProperties().get(SPNEGOConstants.MS_KDC_USER_PASSWORD_CONSUL);
             KDC_REALM = services.get(0).getProperties().get(SPNEGOConstants.KDC_REALM_FROM_CONSUL);
             KDC_HOST_SHORTNAME = services.get(0).getProperties().get(SPNEGOConstants.KDC_SHORTNAME_FROM_CONSUL);
-            if(isZOS){
+            if (isZOS) {
                 KRB5_CONF = services.get(0).getProperties().get(SPNEGOConstants.ZKRB5_CONF_FROM_CONSUL);
 
-            }else {
+            } else {
                 KRB5_CONF = services.get(0).getProperties().get(SPNEGOConstants.KRB5_CONF_FROM_CONSUL);
             }
             Z_USER = services.get(0).getProperties().get(SPNEGOConstants.Z_USER_FROM_CONSUL);
@@ -144,9 +148,9 @@ public class InitClass {
                 KDC_USER_PWD = services.get(1).getProperties().get(SPNEGOConstants.MS_KDC_USER_PASSWORD_CONSUL);
                 KDC_REALM = services.get(1).getProperties().get(SPNEGOConstants.KDC_REALM_FROM_CONSUL);
                 KDC_HOST_SHORTNAME = services.get(1).getProperties().get(SPNEGOConstants.KDC_SHORTNAME_FROM_CONSUL);
-                if(isZOS){
+                if (isZOS) {
                     KRB5_CONF = services.get(1).getProperties().get(SPNEGOConstants.ZKRB5_CONF_FROM_CONSUL);
-                }else {
+                } else {
                     KRB5_CONF = services.get(1).getProperties().get(SPNEGOConstants.KRB5_CONF_FROM_CONSUL);
                 }
                 Z_USER = services.get(1).getProperties().get(SPNEGOConstants.Z_USER_FROM_CONSUL);
@@ -155,7 +159,8 @@ public class InitClass {
                 USER_PWD = services.get(1).getProperties().get(SPNEGOConstants.USER_PWD_FROM_CONSUL);
                 Z_USER_PWD = services.get(1).getProperties().get(SPNEGOConstants.USER0_PWD_FROM_CONSUL);
 
-                Log.info(c, thisMethod, "connection to " + failedKdcShortName + " failed. Attempting failover KDC: " + KDC_HOST_SHORTNAME);
+                Log.info(c, thisMethod, "connection to " + failedKdcShortName + " failed. Attempting failover KDC: "
+                        + KDC_HOST_SHORTNAME);
 
                 connInfo = new ConnectionInfo(KDC_HOSTNAME, InitClass.KDC_USER, InitClass.KDC_USER_PWD);
                 kdcMachine = Machine.getMachine(connInfo);
@@ -164,7 +169,7 @@ public class InitClass {
 
             KDCP_VAR = getKDCHostnameMask(KDC_HOSTNAME);
 
-            //obtaining kdcs and kdcs_r information
+            // obtaining kdcs and kdcs_r information
             services = CommonTest.getKDCServices(1, SPNEGOConstants.KDC2_HOST_FROM_CONSUL);
             KDC2_HOSTNAME = services.get(0).getAddress();
             KDC2_USER = services.get(0).getProperties().get(SPNEGOConstants.MS_KDC_USER_REALM_2_CONSUL);
@@ -196,7 +201,8 @@ public class InitClass {
             CommonTest.releaseServices(services);
         }
 
-        Log.info(c, thisMethod, "We were able to retrieve the required information from consul, the test will now continue....");
+        Log.info(c, thisMethod,
+                "We were able to retrieve the required information from consul, the test will now continue....");
         Log.info(c, thisMethod, "The following KDC's are being used: " + KDCP_VAR + " and " + KDCS_VAR + ".");
 
     }
@@ -207,7 +213,8 @@ public class InitClass {
      * @throws Exception
      * @throws InterruptedException
      */
-    private static void establishConnectionToKDC(String thisMethod, Machine kdcMachine) throws Exception, InterruptedException {
+    private static void establishConnectionToKDC(String thisMethod, Machine kdcMachine)
+            throws Exception, InterruptedException {
         for (int i = 1; i <= 3; i++) {
             try {
                 SshClient sshClient = getSshClient();
@@ -240,7 +247,7 @@ public class InitClass {
             } else if (hostname.contains("secondary")) {
                 maskedHostname = "kdcs";
             } else {
-                //Should never reach here, but if it does, this will be useful to know.
+                // Should never reach here, but if it does, this will be useful to know.
                 return "UNABLE TO GET KDC MASK: " + hostname;
             }
 
@@ -256,8 +263,10 @@ public class InitClass {
     }
 
     /**
-     * Gets the fully qualified domain name (i.e. canonical host name) of the local host. If the code is not allowed to
-     * know the hostname for this IP address, the textual representation of the IP address is returned.
+     * Gets the fully qualified domain name (i.e. canonical host name) of the local
+     * host. If the code is not allowed to
+     * know the hostname for this IP address, the textual representation of the IP
+     * address is returned.
      *
      * @return
      * @throws UnknownHostException
@@ -270,14 +279,15 @@ public class InitClass {
         if (canonicalHostName.equals(ipAddress)) {
             String asciiArtLineBreak = "\n=======================================================";
             Log.info(c, methodName, asciiArtLineBreak + "Can not resolve the hostname for IP address " + ipAddress +
-                                    "\n SPNEGO tests cannot run on this machine. This is a machine set up issue with the host name. "
-                                    + "\n This can be fixed by updating the hosts file or DNS server registration."
-                                    + asciiArtLineBreak);
-            Assume.assumeTrue(false); //This will stop the rest of tests from executing
+                    "\n SPNEGO tests cannot run on this machine. This is a machine set up issue with the host name. "
+                    + "\n This can be fixed by updating the hosts file or DNS server registration."
+                    + asciiArtLineBreak);
+            Assume.assumeTrue(false); // This will stop the rest of tests from executing
             return serverCanonicalHostName = "tests cannot run";
         }
         if (canonicalHostName != null && canonicalHostName.length() > CANONICAL_HOST_NAME_CHAR_LIMIT) {
-            Log.info(c, methodName, "Canonical host name [" + canonicalHostName + "] is longer than allowed character limit. Using a substring as the host name");
+            Log.info(c, methodName, "Canonical host name [" + canonicalHostName
+                    + "] is longer than allowed character limit. Using a substring as the host name");
             String tmpHostLowerCase = canonicalHostName.toLowerCase();
             if (tmpHostLowerCase.contains("ebc")) {
                 canonicalHostName = createRandomStringHostNameForEbc(canonicalHostName);
@@ -287,19 +297,26 @@ public class InitClass {
         }
 
         /*
-         * If we can't resolve a canonical hostname other than localhost, we will have problems with
-         * the input and output keytab being named the same. This in itself isn't a big deal,
-         * but multiple hosts using the same keytab file on the shared remote KDCs will cause issues.
+         * If we can't resolve a canonical hostname other than localhost, we will have
+         * problems with
+         * the input and output keytab being named the same. This in itself isn't a big
+         * deal,
+         * but multiple hosts using the same keytab file on the shared remote KDCs will
+         * cause issues.
          *
-         * Perhaps we could create a random hostname above, but I don't think this should be much
+         * Perhaps we could create a random hostname above, but I don't think this
+         * should be much
          * of an issue unless running on your local machine.
          *
-         * This might be due to a DNS not being able to resolve the host name, or perhaps b/c of network
-         * configuration on the local machine. For example on linux, /etc/hosts resolves 127.0.0.1 to
+         * This might be due to a DNS not being able to resolve the host name, or
+         * perhaps b/c of network
+         * configuration on the local machine. For example on linux, /etc/hosts resolves
+         * 127.0.0.1 to
          * localhost before a FQDN.
          */
         if ("localhost".equalsIgnoreCase(canonicalHostName)) {
-            throw new UnknownHostException("The canonical host name of " + canonicalHostName + " is not supported. Ensure that your host name is resolvable.");
+            throw new UnknownHostException("The canonical host name of " + canonicalHostName
+                    + " is not supported. Ensure that your host name is resolvable.");
         }
 
         serverCanonicalHostName = canonicalHostName;
@@ -308,7 +325,8 @@ public class InitClass {
     }
 
     /**
-     * EBC test machines have long host names, create a random string host name for EBC test machine.
+     * EBC test machines have long host names, create a random string host name for
+     * EBC test machine.
      *
      * @param canonicalHostName
      * @return
@@ -329,12 +347,14 @@ public class InitClass {
             libertyHostMap.put(canonicalHostName, rndHostName);
             isRndHostName = true;
         }
-        Log.info(c, methodName, "EBC canonical hostname " + canonicalHostName + " mapped to the random generated hostname " + rndHostName);
+        Log.info(c, methodName, "EBC canonical hostname " + canonicalHostName
+                + " mapped to the random generated hostname " + rndHostName);
         return rndHostName;
     }
 
     /**
-     * Some test machines have long host names, create a random string host name for long named test machines.
+     * Some test machines have long host names, create a random string host name for
+     * long named test machines.
      *
      * @param canonicalHostName
      * @return
@@ -354,19 +374,22 @@ public class InitClass {
             libertyHostMap.put(canonicalHostName, rndHostName);
             isRndHostName = true;
         }
-        Log.info(c, methodName, "Canonical hostname " + canonicalHostName + " mapped to the random generated hostname " + rndHostName);
+        Log.info(c, methodName,
+                "Canonical hostname " + canonicalHostName + " mapped to the random generated hostname " + rndHostName);
         return rndHostName;
     }
 
     final static HashMap<String, String> hostMap = new HashMap<String, String>();
 
     /**
-     * Returns the short host name from the canonical host name provided. If canonicalHostName does not include
+     * Returns the short host name from the canonical host name provided. If
+     * canonicalHostName does not include
      * "ibm.com", the same value provided for canonicalHostName is returned.
      *
      * @param canonicalHostName
-     * @param issueMsg          - Boolean indicating whether a message should be logged if the canonical host name does not
-     *                              include the IBM domain.
+     * @param issueMsg          - Boolean indicating whether a message should be
+     *                          logged if the canonical host name does not
+     *                          include the IBM domain.
      * @return
      */
     public static String getShortHostName(String canonicalHostName, boolean issueMsg) {
@@ -386,7 +409,8 @@ public class InitClass {
     }
 
     /**
-     * Gets the short host name of the local host. If the code is not allowed to know the host name for this IP address,
+     * Gets the short host name of the local host. If the code is not allowed to
+     * know the host name for this IP address,
      * the textual representation of the IP address is returned.
      *
      * @return
@@ -402,6 +426,9 @@ public class InitClass {
      * @return The SshClient.
      */
     protected static SshClient getSshClient() {
+        if (Security.getProvider("BC") == null) {
+            Security.addProvider(new BouncyCastleProvider());
+        }
         SshClient sshClient = SshClient.setUpDefaultClient();
         sshClient.start();
         return sshClient;
@@ -413,10 +440,12 @@ public class InitClass {
      * @param sshClient The SSH client.
      * @param machine   The machine to connect to.
      * @return The session.
-     * @throws IOException If there was an error getting an SSH session to the machine.
+     * @throws IOException If there was an error getting an SSH session to the
+     *                     machine.
      */
     protected static ClientSession getSshSession(SshClient sshClient, Machine machine) throws IOException {
-        ClientSession session = sshClient.connect(machine.getUsername(), machine.getHostname(), 22).verify(30, TimeUnit.SECONDS).getSession();
+        ClientSession session = sshClient.connect(machine.getUsername(), machine.getHostname(), 22)
+                .verify(30, TimeUnit.SECONDS).getSession();
         session.addPasswordIdentity(machine.getPassword());
         session.auth().verify(30, TimeUnit.SECONDS).isSuccess();
         return session;
