656bcc1ec038574ec11338f9f95f30b2d2b48ad0
Neena Jacob <neena.jacob@ibm.com>
2025-08-25 16:13:48 +0530

    Created new fat proejct for microprofile rest client

---
+++ dev/io.openliberty.mpRestClient.4.0_fat/test-applications/prototypeClientApp/src/io/openliberty/mpRestClient/fat/prototypeClient/BasicServiceClient.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2025 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package io.openliberty.mpRestClient.fat.prototypeClient;
+
+import java.util.Set;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+//@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Path("/")
+public interface BasicServiceClient {
+    
+
+    @GET
+    @Path("echo")
+    String hello();
+
+    
+}
