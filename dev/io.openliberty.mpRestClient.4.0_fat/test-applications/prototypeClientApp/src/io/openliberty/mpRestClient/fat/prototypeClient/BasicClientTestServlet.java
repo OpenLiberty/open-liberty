55dc5486aa6ac97ea862018f2edb87a7fe38aaa1
Neena Jacob <neena.jacob@ibm.com>
2025-08-29 16:51:19 +0530

    Upgraded to jakarta and using mpRestClient-4.0

--- dev/io.openliberty.mpRestClient.4.0_fat/test-applications/prototypeClientApp/src/io/openliberty/mpRestClient/fat/prototypeClient/BasicClientTestServlet.java
+++ dev/io.openliberty.mpRestClient.4.0_fat/test-applications/prototypeClientApp/src/io/openliberty/mpRestClient/fat/prototypeClient/BasicClientTestServlet.java
@@ -21,12 +21,10 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.eclipse.microprofile.rest.client.RestClientBuilder;
 import org.junit.Test;
--- dev/io.openliberty.mpRestClient.4.0_fat/test-applications/prototypeClientApp/src/io/openliberty/mpRestClient/fat/prototypeClient/BasicServiceClient.java
+++ dev/io.openliberty.mpRestClient.4.0_fat/test-applications/prototypeClientApp/src/io/openliberty/mpRestClient/fat/prototypeClient/BasicServiceClient.java
@@ -11,22 +11,21 @@ package io.openliberty.mpRestClient.fat.prototypeClient;
 
 import java.util.Set;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
-//@Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 @Path("/")
 public interface BasicServiceClient {
-    
+
 
     @GET
     @Path("echo")
     String hello();
 
-    
+
 }
