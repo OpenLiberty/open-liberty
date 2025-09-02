b9bb2fcee3f08f3f056c4276e764c326eb6e047e
Habib Lawal <Habib.Lawal@ibm.com>
2025-09-02 14:41:56 +0100

    chore: add correct copyright headers

--- dev/io.openliberty.mcp/src/io/openliberty/mcp/annotations/OutputSchemaGenerator.java
+++ dev/io.openliberty.mcp/src/io/openliberty/mcp/annotations/OutputSchemaGenerator.java
@@ -1,12 +1,25 @@
 /*******************************************************************************
- * Copyright (c) 2025 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License 2.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-2.0/
+ * Copyright (c) contributors to https://github.com/quarkiverse/quarkus-mcp-server
+ * Copyright (c) 2025 IBM Corporation and others
  *
- * SPDX-License-Identifier: EPL-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Based on https://github.com/quarkiverse/quarkus-mcp-server/blob/main/core/runtime/src/main/java/io/quarkiverse/mcp/server/OutputSchemaGenerator.java
+ * Modifications have been made.
  *******************************************************************************/
+
 package io.openliberty.mcp.annotations;
 
 public interface OutputSchemaGenerator {
