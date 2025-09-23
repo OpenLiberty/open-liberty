c536dd5f9e0d93bd8d6a3d8a6a0676288d9738e3
nidhin-cp <nidhin.cp@ibm.com>
2025-09-23 15:10:27 +0530

    adding the tests
    editing the CustomizedExceptionText test
    
    updating copyright
    
    resolving the comments
    
    revert changes in fatsuite
    
    resolving comments
    
    Update FATSuite.java
    
    Update FATSuite.java
    
    adding space
    
    updating the tests
    
    updating the tests
    
    updating the tests
    
    resolving the comments
    
    resolving the comments

--- dev/com.ibm.ws.webcontainer.servlet.3.1_fat.2/fat/src/com/ibm/ws/webcontainer/servlet31/fat/tests/CustomizedExceptionText.java
+++ dev/com.ibm.ws.webcontainer.servlet.3.1_fat.2/fat/src/com/ibm/ws/webcontainer/servlet31/fat/tests/CustomizedExceptionText.java
@@ -1,7 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2025 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
 package com.ibm.ws.webcontainer.servlet31.fat.tests;
 
 import static org.junit.Assert.assertTrue;
 
+import java.net.URL;
+import java.util.Collections;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,34 +23,37 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.ibm.websphere.simplicity.ShrinkHelper;
+import com.ibm.websphere.simplicity.config.ServerConfiguration;
+import com.ibm.websphere.simplicity.config.WebContainerElement;
 import com.meterware.httpunit.WebConversation;
 import com.meterware.httpunit.WebResponse;
 
 import componenttest.annotation.Server;
 import componenttest.custom.junit.runner.FATRunner;
+import componenttest.custom.junit.runner.Mode;
 import componenttest.topology.impl.LibertyServer;
+import componenttest.topology.utils.HttpUtils; 
 
 @RunWith(FATRunner.class)
+@Mode(Mode.TestMode.FULL)
 public class CustomizedExceptionText {
 
     private static final Logger LOGGER = Logger.getLogger(CustomizedExceptionText.class.getName());
     private static final String CUSTOM_ERROR_APP_NAME = "CustomErrorTestApp";
 
     @Server("servlet31_customizedExceptionText")
-<<<<<<< HEAD
     public static LibertyServer server;
 
     @BeforeClass
     public static void setup() throws Exception {
-        WebArchive customErrorApp = ShrinkHelper.buildDefaultApp(
-            CUSTOM_ERROR_APP_NAME + ".war",
-            "com.ibm.ws.webcontainer.servlet_31_fat.customerrortest.war.test.servlets"
-        );
-        customErrorApp = (WebArchive) ShrinkHelper.addDirectory(
-            customErrorApp,
-            "test-applications/" + CUSTOM_ERROR_APP_NAME + ".war/resources"
-        );
+        WebArchive customErrorApp = ShrinkHelper.buildDefaultApp(CUSTOM_ERROR_APP_NAME + ".war","com.ibm.ws.webcontainer.servlet_31_fat.customerrortest.war.test.servlets");
+        customErrorApp = (WebArchive) ShrinkHelper.addDirectory(customErrorApp,"test-applications/" + CUSTOM_ERROR_APP_NAME + ".war/resources");
         ShrinkHelper.exportDropinAppToServer(server, customErrorApp);
+
+        if (!server.isStarted()) {
+            server.startServer();
+            server.waitForStringInLogUsingMark("CWWKT0016I.*" + CUSTOM_ERROR_APP_NAME + ".*");
+        }
     }
 
     @AfterClass
@@ -48,45 +62,30 @@ public class CustomizedExceptionText {
             server.stopServer();
         }
     }
-    /**
-     * Verify that the customized exception text configured in server.xml
-     * is returned instead of the default
-     * "SRVE0218E: Forbidden" message
-     * when the application sends a 403 Forbidden error.
-     */
-    @Test
-    public void tes403CustomMessage() throws Throwable {
-        server.stopServer();
-        server.setServerConfigurationFile("customizedExceptionTest/server-test-SRVE0218E-custom-message.xml");
-        server.startServer();
-        server.waitForStringInLogUsingMark("CWWKT0016I.*" + CUSTOM_ERROR_APP_NAME + ".*");
 
-        String requestURL = baseURL() + "/" + CUSTOM_ERROR_APP_NAME + "/forbidden";
-        String result = checkRequest(requestURL, 403, "Custom 403 Forbidden error message", "SRVE0218E");
+    @Test
+    public void test403CustomMessage() throws Throwable {
+        LOGGER.info("Starting test403CustomMessage");
+        updateCustomizedExceptionText("Custom 403 Forbidden error message");
+
+        URL requestURL = HttpUtils.createURL(server, "/" + CUSTOM_ERROR_APP_NAME + "/forbidden");
+        String result = checkRequest(requestURL.toString(), 403, "Custom 403 Forbidden error message", "SRVE0218E");
 
         assertTrue("Custom 403 text not found in response:\n" + result, result.contains("PASS"));
     }
 
-    /**
-     * Verify that the customized exception text configured in server.xml
-     * is returned instead of the default
-     * "SRVE0232E: An exception occurred" message
-     * when a servlet triggers a 500 Internal Server Error.
-     */
     @Test
     public void test500CustomMessage() throws Throwable {
-        server.stopServer();
-        server.setServerConfigurationFile("customizedExceptionTest/server-test-SRVE0232E-custom-message.xml");
-        server.startServer();
-        server.waitForStringInLogUsingMark("CWWKT0016I.*" + CUSTOM_ERROR_APP_NAME + ".*");
+        LOGGER.info("Starting test500CustomMessage");
+        updateCustomizedExceptionText("Custom 500 internal server error message");
 
-        String requestURL = baseURL() + "/" + CUSTOM_ERROR_APP_NAME + "/error";
-        String result = checkRequest(requestURL, 500, "Custom 500 internal server error message", "SRVE0232E");
+        URL requestURL = HttpUtils.createURL(server, "/" + CUSTOM_ERROR_APP_NAME + "/error");
+        String result = checkRequest(requestURL.toString(), 500, "Custom 500 internal server error message", "SRVE0232E");
 
         assertTrue("Custom 500 text not found in response:\n" + result, result.contains("PASS"));
     }
 
-    private String checkRequest(String URL, int responseCode, String expectedResText, String notExpectedResText) throws Throwable {
+    private String checkRequest(String URL, int responseCode, String expectedResText, String notExpectedResText)throws Throwable {
         WebConversation wc = new WebConversation();
         wc.setExceptionsThrownOnErrorStatus(false);
         WebResponse resp = wc.getResponse(URL);
@@ -112,127 +111,35 @@ public class CustomizedExceptionText {
         return result.toString();
     }
 
-    private String baseURL() {
-        return "http://" + server.getHostname() + ":" + server.getHttpDefaultPort();
-    }
-}
-
-package com.ibm.ws.webcontainer.servlet31.fat.tests;
-
-import static org.junit.Assert.assertTrue;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import com.ibm.websphere.simplicity.ShrinkHelper;
-import com.meterware.httpunit.WebConversation;
-import com.meterware.httpunit.WebResponse;
-
-import componenttest.annotation.Server;
-import componenttest.custom.junit.runner.FATRunner;
-import componenttest.topology.impl.LibertyServer;
-
-@RunWith(FATRunner.class)
-public class CustomizedExceptionText {
-
-    private static final Logger LOGGER = Logger.getLogger(CustomizedExceptionText.class.getName());
-    private static final String CUSTOM_ERROR_APP_NAME = "CustomErrorTestApp";
-
-    @Server("servelet31_customizedExceptionText")
-=======
->>>>>>> d5617bf6dbd (add tests)
-    public static LibertyServer server;
-
-    @BeforeClass
-    public static void setup() throws Exception {
-        WebArchive customErrorApp = ShrinkHelper.buildDefaultApp(
-            CUSTOM_ERROR_APP_NAME + ".war",
-            "com.ibm.ws.webcontainer.servlet_31_fat.customerrortest.war.test.servlets"
-        );
-        customErrorApp = (WebArchive) ShrinkHelper.addDirectory(
-            customErrorApp,
-            "test-applications/" + CUSTOM_ERROR_APP_NAME + ".war/resources"
-        );
-        ShrinkHelper.exportDropinAppToServer(server, customErrorApp);
-    }
-
-    @AfterClass
-    public static void cleanup() throws Exception {
-        if (server != null && server.isStarted()) {
-            server.stopServer();
-        }
-    }
     /**
-     * Verify that the customized exception text configured in server.xml
-     * is returned instead of the default
-     * "SRVE0218E: Forbidden" message
-     * when the application sends a 403 Forbidden error.
+     * Method to update <webContainer displayCustomizedExceptionText="..." dynamically
      */
-    @Test
-    public void tes403CustomMessage() throws Throwable {
-        server.stopServer();
-        server.setServerConfigurationFile("customizedExceptionTest/server-test-SRVE0218E-custom-message.xml");
-        server.startServer();
-        server.waitForStringInLogUsingMark("CWWKT0016I.*" + CUSTOM_ERROR_APP_NAME + ".*");
+    private void updateCustomizedExceptionText(String customMessage) throws Exception {
+        ServerConfiguration config = server.getServerConfiguration();
+        WebContainerElement webContainer = config.getWebContainer();
 
-        String requestURL = baseURL() + "/" + CUSTOM_ERROR_APP_NAME + "/forbidden";
-        String result = checkRequest(requestURL, 403, "Custom 403 Forbidden error message", "SRVE0218E");
+        boolean serverXMLChanged = false;
+        String currentValue = webContainer.getDisplayCustomizedExceptionText();
 
-        assertTrue("Custom 403 text not found in response:\n" + result, result.contains("PASS"));
-    }
-
-    /**
-     * Verify that the customized exception text configured in server.xml
-     * is returned instead of the default
-     * "SRVE0232E: An exception occurred" message
-     * when a servlet triggers a 500 Internal Server Error.
-     */
-    @Test
-    public void test500CustomMessage() throws Throwable {
-        server.stopServer();
-        server.setServerConfigurationFile("customizedExceptionTest/server-test-SRVE0232E-custom-message.xml");
-        server.startServer();
-        server.waitForStringInLogUsingMark("CWWKT0016I.*" + CUSTOM_ERROR_APP_NAME + ".*");
-
-        String requestURL = baseURL() + "/" + CUSTOM_ERROR_APP_NAME + "/error";
-        String result = checkRequest(requestURL, 500, "Custom 500 internal server error message", "SRVE0232E");
-
-        assertTrue("Custom 500 text not found in response:\n" + result, result.contains("PASS"));
-    }
-
-    private String checkRequest(String URL, int responseCode, String expectedResText, String notExpectedResText) throws Throwable {
-        WebConversation wc = new WebConversation();
-        wc.setExceptionsThrownOnErrorStatus(false);
-        WebResponse resp = wc.getResponse(URL);
-
-        int code = resp.getResponseCode();
-        String body = resp.getText();
-
-        StringBuilder result = new StringBuilder();
-        result.append("Checking Response Code: Expected [" + responseCode + "], actual [" + code + "]. ");
-        result.append(code == responseCode ? "PASS\n" : "FAIL\n");
-
-        if (!expectedResText.equals("SKIP")) {
-            result.append("Checking Expected Response text: ");
-            result.append(body.contains(expectedResText) ? "PASS\n" : "FAIL\n");
+        if (currentValue == null || !currentValue.equals(customMessage)) {
+            LOGGER.info("Updating displayCustomizedExceptionText from [" + currentValue + "] to [" + customMessage + "]");
+            webContainer.setDisplayCustomizedExceptionText(customMessage);
+            serverXMLChanged = true;
+        } else {
+            LOGGER.info("No update required value already set to [" + currentValue + "]");
         }
+        server.setMarkToEndOfLog();
+        server.updateServerConfiguration(config);
 
-        if (!notExpectedResText.equals("SKIP")) {
-            result.append("Checking NOT Expected Response text: ");
-            result.append(body.contains(notExpectedResText) ? "FAIL\n" : "PASS\n");
+        // Wait for a config update if we actually changed something
+        if (serverXMLChanged) {
+            try {
+                server.waitForConfigUpdateInLogUsingMark(Collections.emptySet(),false,"CWWKT0016I:.*CustomErrorTestApp.*");
+                LOGGER.info("Config update message detected after applying customized exception text.");
+            } 
+            catch (Exception e) {
+                LOGGER.warning("No explicit config update detected");
+            }
         }
-
-        LOGGER.log(Level.INFO, "checkRequest Body:\n" + body);
-        return result.toString();
-    }
-
-    private String baseURL() {
-        return "http://" + server.getHostname() + ":" + server.getHttpDefaultPort();
     }
 }
