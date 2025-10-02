025378e9e0041dfa9d2611ff92b021ed60bec996
Benjamin Confino <benjamic@uk.ibm.com>
2025-10-02 11:21:47 +0100

    Hibernate removed the deprecated extended mean manager interface and we need to adjust to match

--- dev/com.ibm.ws.jpa.container.v21.cdi/src/com/ibm/ws/jpa/container/v21/cdi/internal/CDIJPAEMFPropertyProviderImpl.java
+++ dev/com.ibm.ws.jpa.container.v21.cdi/src/com/ibm/ws/jpa/container/v21/cdi/internal/CDIJPAEMFPropertyProviderImpl.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -12,12 +12,14 @@
  *******************************************************************************/
 package com.ibm.ws.jpa.container.v21.cdi.internal;
 
-import java.lang.reflect.Proxy;
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import javax.enterprise.inject.spi.BeanManager;
@@ -25,14 +27,13 @@ import javax.enterprise.inject.spi.BeanManager;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+import com.ibm.ejs.util.dopriv.SystemGetPropertyPrivileged;
 import com.ibm.websphere.ras.Tr;
 import com.ibm.websphere.ras.TraceComponent;
 import com.ibm.ws.cdi.CDIService;
 import com.ibm.ws.classloading.ClassLoaderIdentifierService;
 import com.ibm.ws.ffdc.annotation.FFDCIgnore;
 import com.ibm.ws.jpa.management.JPAEMFPropertyProvider;
-
-import com.ibm.ejs.util.dopriv.SystemGetPropertyPrivileged;
 import com.ibm.wsspi.classloading.ClassLoadingService;
 
 /**
@@ -49,7 +50,7 @@ public class CDIJPAEMFPropertyProviderImpl implements JPAEMFPropertyProvider, Hi
     private static final String CDI_BEANMANAGER = "javax.persistence.bean.manager";
 
     private CDIService cdiService;
-    private Map<String,IBMHibernateExtendedBeanManager> extendedBeanManagers = new HashMap<String,IBMHibernateExtendedBeanManager>();
+    private final Map<String, IBMHibernateExtendedBeanManager> extendedBeanManagers = new HashMap<String, IBMHibernateExtendedBeanManager>();
 
     @Reference
     protected ClassLoadingService classLoadingService;
@@ -58,13 +59,13 @@ public class CDIJPAEMFPropertyProviderImpl implements JPAEMFPropertyProvider, Hi
 
     private final static String ENABLE_HIBERNATE_COMPATIBILITY = "com.ibm.websphere.jpa.hibernate-cdi-compatibility";
     @SuppressWarnings("unchecked")
-    private static final boolean hibernateEnabled = Boolean.parseBoolean((String) AccessController.doPrivileged(new SystemGetPropertyPrivileged(ENABLE_HIBERNATE_COMPATIBILITY, "false")));
+    private static final boolean hibernateEnabled = Boolean.parseBoolean(AccessController.doPrivileged(new SystemGetPropertyPrivileged(ENABLE_HIBERNATE_COMPATIBILITY, "false")));
 
     @Override
     @FFDCIgnore(ClassNotFoundException.class)
     public void updateProperties(Map<String, Object> props, ClassLoader applicationClassLoader) {
         if (cdiService != null) {
-            Class[] beanManagerInterfaces = null;
+            List<Class<?>> beanManagerInterfaces = new ArrayList<Class<?>>();
             InvocationHandler invocationHandler = null;
             ClassLoader classLoader = CLASSLOADER;
 
@@ -73,13 +74,19 @@ public class CDIJPAEMFPropertyProviderImpl implements JPAEMFPropertyProvider, Hi
                 try {
 
                     String baseClassLoaderId = getBaseClassLoaderId(applicationClassLoader);
-
                     ClassLoader unifiedClassLoader = unify(CLASSLOADER, applicationClassLoader);
 
                     Class<?> extendedBeanManagerInterface = Class.forName("org.hibernate.resource.beans.container.spi.ExtendedBeanManager",
-                                                                             true, unifiedClassLoader);
-                    Class<?> depreciatedExtendedBeanManagerInterface = Class.forName("org.hibernate.jpa.event.spi.jpa.ExtendedBeanManager",
-                                                                             true, unifiedClassLoader); //A bug in hibernate means we need to implement this interface too for now. 
+                                                                          true, unifiedClassLoader);
+                    beanManagerInterfaces.add(extendedBeanManagerInterface);
+                    try {
+                        Class<?> depreciatedExtendedBeanManagerInterface = Class.forName("org.hibernate.jpa.event.spi.jpa.ExtendedBeanManager",
+                                                                                         true, unifiedClassLoader); //A bug in hibernate means we need to implement this interface too for now.
+                        beanManagerInterfaces.add(depreciatedExtendedBeanManagerInterface);
+                    } catch (ClassNotFoundException e) {
+                        //Do nothing, if we can't find the deprecated interface we'll continue with just the main interface.
+                    }
+                    beanManagerInterfaces.add(BeanManager.class);
 
                     //Since extended bean managers only handle CDI lifecycle events, which are scoped to the whole ear we only need one.
                     IBMHibernateExtendedBeanManager extendedBeanManager = null;
@@ -91,19 +98,20 @@ public class CDIJPAEMFPropertyProviderImpl implements JPAEMFPropertyProvider, Hi
                     }
 
                     invocationHandler = new BeanManagerInvocationHandler(cdiService, extendedBeanManager);
-                    beanManagerInterfaces = new Class<?>[] { extendedBeanManagerInterface, depreciatedExtendedBeanManagerInterface, BeanManager.class };
                     classLoader = unifiedClassLoader;
                 } catch (ClassNotFoundException e) {
                     invocationHandler = new BeanManagerInvocationHandler(cdiService);
-                    beanManagerInterfaces = new Class<?>[] { BeanManager.class };
+                    beanManagerInterfaces.clear();
+                    beanManagerInterfaces.add(BeanManager.class);
                 }
             } else {
                 invocationHandler = new BeanManagerInvocationHandler(cdiService);
-                beanManagerInterfaces = new Class<?>[] { BeanManager.class };
+                beanManagerInterfaces.add(BeanManager.class);
             }
 
+            Class<?>[] beanManagerInterfacesArray = beanManagerInterfaces.toArray(new Class<?>[beanManagerInterfaces.size()]);
             Object beanManager = Proxy.newProxyInstance(classLoader,
-                                                        beanManagerInterfaces,
+                                                        beanManagerInterfacesArray,
                                                         invocationHandler);
             props.put(CDI_BEANMANAGER, beanManager);
             if (TraceComponent.isAnyTracingEnabled() && tc.isDebugEnabled()) {
@@ -112,20 +120,22 @@ public class CDIJPAEMFPropertyProviderImpl implements JPAEMFPropertyProvider, Hi
         }
 
     }
-    
+
+    @Override
     public void notifyHibernateAfterBeanDiscovery(BeanManager beanManager, ClassLoader classLoader) {
         String baseClassLoaderId = getBaseClassLoaderId(classLoader);
         for (IBMHibernateExtendedBeanManager extendedBeanManager : extendedBeanManagers.values()) {
-            //We check which is the correct bean manager inside IBMHibernateExtendedBeanManager. 
+            //We check which is the correct bean manager inside IBMHibernateExtendedBeanManager.
             extendedBeanManager.notifyHibernateAfterBeanDiscovery(baseClassLoaderId, beanManager);
         }
     }
 
+    @Override
     public void notifyHibernateBeforeShutdown(BeanManager beanManager) {
-        Iterator<Map.Entry<String,IBMHibernateExtendedBeanManager>> it = extendedBeanManagers.entrySet().iterator();
+        Iterator<Map.Entry<String, IBMHibernateExtendedBeanManager>> it = extendedBeanManagers.entrySet().iterator();
         while (it.hasNext()) {
             IBMHibernateExtendedBeanManager extendedBeanManager = it.next().getValue();
-            //We check which is the correct bean manager inside IBMHibernateExtendedBeanManager. 
+            //We check which is the correct bean manager inside IBMHibernateExtendedBeanManager.
             if (extendedBeanManager.notifyHibernateBeforeShutdown(beanManager)) {
                 it.remove();
             }
@@ -151,7 +161,7 @@ public class CDIJPAEMFPropertyProviderImpl implements JPAEMFPropertyProvider, Hi
             ClassLoader parent = applicationClassLoader.getParent();
             String parentId = classLoaderIdentifierService.getClassLoaderIdentifier(parent) == null ? null : classLoaderIdentifierService.getClassLoaderIdentifier(parent);
             if (parent == null || parentId == null || parentId.equals("Shared Library:global")) {
-            	return id;
+                return id;
             } else {
                 applicationClassLoader = parent;
             }
