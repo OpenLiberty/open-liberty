d14880de928bc4ad8aadbd9c5db5633602986735
Volodymyr Siedlecki <volosied@gmail.com>
2025-10-09 14:53:14 -0400

    Address TODOs in RichClientTransportFactory

--- dev/com.ibm.ws.messaging.comms.client/src/com/ibm/ws/sib/jfapchannel/richclient/framework/impl/RichClientTransportFactory.java
+++ dev/com.ibm.ws.messaging.comms.client/src/com/ibm/ws/sib/jfapchannel/richclient/framework/impl/RichClientTransportFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2023 IBM Corporation and others.
+ * Copyright (c) 2003, 2025 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -92,9 +92,6 @@ public class RichClientTransportFactory implements NetworkTransportFactory
         	if(chain != null) {
         		if(chain.useNetty()) {
         			// If Netty, create new Netty channel factory
-        			// TODO: Consult with team. Getting error difference cause channelfw fails for testSSLFeatureUpdate
-        			// in com.ibm.ws.messaging.open_comms_fat because SSL chain failed to init properly due to no SSL Options
-        			// Check what to do here appropriately see https://github.com/OpenLiberty/open-liberty/issues/24823
         			boolean usingSSL = chain.isSecureChain();
         			if(usingSSL && chain.getSslOptions() == null)
         				throw new InvalidChainNameException("Chain configuration not found in framework, " + chainName);
@@ -143,12 +140,8 @@ public class RichClientTransportFactory implements NetworkTransportFactory
         NetworkConnectionFactory connFactory = null;
         if (endPoint instanceof CFEndPoint)
         {
-        	
         	// Get the virtual connection factory from the EP and wrap it in our implementation of
         	// the NetworkConnectionFactory interface
-        	// TODO Check this out from a Netty endpoint perspective. Used for other types of connects. See CreateNewVirtualConnectionFactory in ConnectionDataGroup
-        	// If NOT Netty do the same as we've done https://github.com/OpenLiberty/open-liberty/issues/22692
-        	// TODO: Check this if its okay for chain name
         	String endPointName = ((CFEndPoint) endPoint).getName();
         	CommsOutboundChain chain = CommsOutboundChain.getChainDetails(endPointName);
         	if(chain != null && !chain.useNetty()) {
@@ -156,12 +149,10 @@ public class RichClientTransportFactory implements NetworkTransportFactory
         		connFactory = new CFWNetworkConnectionFactory(vcFactory);
         	}
         	else {
-        		// If Netty return null until we figure this out
         		if (tc.isDebugEnabled())
         			SibTr.error(tc, "getOutboundNetworkConnectionFactoryFromEndPoint", endPoint);
-        		// TODO: Question It might help more if it blow up here with an explicit string rather than return null "internal logic error"
-        		// Check if we can throw an error here see https://github.com/OpenLiberty/open-liberty/issues/22692
-        		return null;
+
+                throw new UnsupportedOperationException("NetworkConnectionFactory cannot be created for a Netty endpoint");
         	}
         	
         }
