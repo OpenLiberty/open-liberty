a4d98a2fae0ac35ba2e8beebf402c4e7e10f28ca
jim-krueger <jckofbyron@gmail.com>
2025-11-01 19:58:51 -0500

    Temp test PR

--- dev/com.ibm.ws.jaxrs.2.1.client_fat/fat/src/com/ibm/ws/jaxrs21/client/fat/test/JAXRS21ClientLTPATest.java
+++ dev/com.ibm.ws.jaxrs.2.1.client_fat/fat/src/com/ibm/ws/jaxrs21/client/fat/test/JAXRS21ClientLTPATest.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -51,6 +51,7 @@ public class JAXRS21ClientLTPATest extends JAXRS21AbstractTest {
         // already started server
         try {
             server.startServer(true);
+            server.waitForSSLStart();
         } catch (Exception e) {
             System.out.println(e.toString());
         }
