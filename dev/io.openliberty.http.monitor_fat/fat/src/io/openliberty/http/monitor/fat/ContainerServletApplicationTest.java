1fbc7d0299291dbf7df418e0052e10a110ba0462
David Chan <chdavid@ca.ibm.com>
2025-11-03 14:14:10 -0500

    Update with TCp Container and network config options

--- dev/io.openliberty.http.monitor_fat/fat/src/io/openliberty/http/monitor/fat/ContainerServletApplicationTest.java
+++ dev/io.openliberty.http.monitor_fat/fat/src/io/openliberty/http/monitor/fat/ContainerServletApplicationTest.java
@@ -64,13 +64,20 @@ public class ContainerServletApplicationTest extends BaseTestClass {
                     .withFileFromFile("/etc/otelcol/config.yaml", new File(PATH_TO_AUTOFVT_TESTFILES + "config.yaml")))
                     .withLogConsumer(new SimpleLogConsumer(ContainerServletApplicationTest.class, "opentelemetry-collector"))
                     .withExposedPorts(8888, 8889, 4317)
+                    .withPrivilegedMode(true)
                     .withNetwork(network)
                     .withNetworkAliases("otel-collector-metrics")
                     .waitingFor(new LogMessageWaitStrategy().withRegEx(".*Begin running and processing data.*").withStartupTimeout(Duration.ofMinutes(1)));
 
+    public static TcpDumpContainer tcpDumpContainer = new TcpDumpContainer(container)
+                    .withPrivilegedMode(true)
+                    .withDumpFileDestination("mytcpdump.dump")
+                    .withLogConsumer(new SimpleLogConsumer(ContainerServletApplicationTest.class, "tcpdump"));
+
     @ClassRule
     public static RuleChain chain = RuleChain.outerRule(network)
                     .around(container)
+                    .around(tcpDumpContainer)
                     .around(rt);
 
     @BeforeClass
