08a1a0519e7db718fc1b97a8f0beb5dee9b5cd43
Issac Abraham <Issac.Abraham@ibm.com>
2025-12-04 14:59:08 +0000

    package trace update

--- dev/io.openliberty.mcp.internal/src/io/openliberty/mcp/internal/meta/package-info.java
+++ dev/io.openliberty.mcp.internal/src/io/openliberty/mcp/internal/meta/package-info.java
@@ -4,10 +4,11 @@
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *******************************************************************************/
 /**
  *
  */
+@com.ibm.websphere.ras.annotation.TraceOptions(messageBundle = "io.openliberty.mcp.internal.resources.CWMCM", traceGroup = "MCP")
 package io.openliberty.mcp.internal.meta;
