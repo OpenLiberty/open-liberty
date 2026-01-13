d5c8461ee89dd2cacd006a7c81eb699dda1d33c4
Benjamin Confino <benjamic@uk.ibm.com>
2026-01-13 12:59:09 +0000

    new jandex

---
+++ dev/com.ibm.ws.anno_fat/test-applications/NewJandexTestServlet40.war/src/testservlet40/war/servlets/Util_Anno.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package testservlet40.war.servlets;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Simple utility class: Present to force the jar over the write limit, which defaults to 16.
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target({ ElementType.TYPE, ElementType.METHOD })
+public @interface Util_Anno {
+    boolean value();
+}
