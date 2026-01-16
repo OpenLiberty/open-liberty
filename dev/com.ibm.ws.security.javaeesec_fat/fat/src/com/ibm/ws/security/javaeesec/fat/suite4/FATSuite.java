6a7354dd5b780603f52ff4448505f510712e89b0
alex-butcher <alex.butcher@uk.ibm.com>
2026-01-16 11:15:23 +0000

    Consolidate jakartasec fat tests into single FAT project
    Using Jared's multiple suites per FAT, consolidate all the jakartasec fats into a single project given that fat.2, fat.3 and fat.4 all use applications and servers defined in the base fat project

---
+++ dev/com.ibm.ws.security.javaeesec_fat/fat/src/com/ibm/ws/security/javaeesec/fat/suite4/FATSuite.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.security.javaeesec.fat.suite4;
+
+import componenttest.custom.junit.runner.AlwaysPassesTest;
+import componenttest.rules.repeater.EmptyAction;
+import componenttest.rules.repeater.JakartaEE9Action;
+import componenttest.rules.repeater.RepeatTests;
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+                AlwaysPassesTest.class,
+                MultipleModuleGlobalClientCertFailOverTest.class,
+                MultipleModuleGlobalLoginTest.class,
+                NoIdentityStoreTest.class,
+                SSOTest.class,
+                ScopedTest.class,
+                SecurityContextEJBTest.class,
+                SecurityContextJaxRSTest.class,
+                SecurityContextTest.class
+                /*
+                 * Before adding tests to this suite, check how long it is running on a Windows build.
+                 * You may need to start a fat.5 suite. Review the readme.txt included in this project.
+                 */
+})
+
+/**
+ * Purpose: This suite collects and runs all known good test suites.
+ */
+public class FATSuite {
+
+    /*
+     * Run EE9 tests in LITE mode and run all tests in FULL mode.
+     */
+    @ClassRule
+    public static RepeatTests repeat = RepeatTests.with(new EmptyAction().fullFATOnly()).andWith(new JakartaEE9Action());
+}
