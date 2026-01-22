ca1eaaf23a4b5d4908bf8493b7cf9f4c224b8ed0
Benjamin Confino <benjamic@uk.ibm.com>
2026-01-22 13:22:08 +0000

    create fat tests for new things in the latest jandex index format

--- dev/com.ibm.ws.anno_fat/fat/src/com/ibm/ws/tests/anno/jandex/JandexAppDefaultAppMgrDefaultTest.java
+++ dev/com.ibm.ws.anno_fat/fat/src/com/ibm/ws/tests/anno/jandex/JandexAppDefaultAppMgrDefaultTest.java
@@ -16,12 +16,23 @@ import java.util.logging.Logger;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import com.ibm.ws.fat.util.SharedServer;
+import com.ibm.ws.tests.anno.JandexV1RepeatAction;
+import com.ibm.ws.tests.anno.JandexV3RepeatAction;
 
+import componenttest.custom.junit.runner.FATRunner;
+import componenttest.rules.repeater.RepeatTests;
+
+@RunWith(FATRunner.class)
 public class JandexAppDefaultAppMgrDefaultTest extends JandexAppTest {
     private static final Logger LOG = Logger.getLogger(JandexAppDefaultAppMgrDefaultTest.class.getName());
+    
+    @ClassRule
+    public static RepeatTests r = RepeatTests.with(new JandexV1RepeatAction()).andWith(new JandexV3RepeatAction());
 
     public static SharedServer SHARED_SERVER = new SharedServer("annoFat_server", false);
 
