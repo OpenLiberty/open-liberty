af64c4de120a79fe50ac01b86596bcd59f00521b
Riya Arora <riyaarora@ibm.com>
2026-02-06 16:45:48 -0500

    Fix intermittent timeout failures in session cache tests for RTC 308269
    - Increase server startup wait times from 10s to 20s on Windows platforms
    - Affects SessionCacheTwoServerTest and SessionCacheTwoServerTimeoutTest
    - Fixes testWriteContentsWithPersistence and testCookiesWithMultipleAppsAndShortIdsAndPersistence timeouts
    - Windows platforms are slower and need more time for Infinispan cluster formation
    - Intermittent failure rate was 19% (9/48 occurrences) on Windows
    - Updated copyright to 2026

--- dev/com.ibm.ws.session.cache_fat_infinispan/fat/src/com/ibm/ws/session/cache/fat/infinispan/SessionCacheTwoServerTest.java
+++ dev/com.ibm.ws.session.cache_fat_infinispan/fat/src/com/ibm/ws/session/cache/fat/infinispan/SessionCacheTwoServerTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2023 IBM Corporation and others.
+ * Copyright (c) 2018, 2026 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -84,7 +84,9 @@ public class SessionCacheTwoServerTest extends FATServletClient {
 
         serverA.startServer();
 
-        TimeUnit.SECONDS.sleep(10);
+        // Increase wait time for Windows platforms which are slower
+        int initialWaitTime = isWindows() ? 20 : 10;
+        TimeUnit.SECONDS.sleep(initialWaitTime);
 
         // Since we initialize the JCache provider lazily, use an HTTP session on serverA before starting serverB,
         // so that the JCache provider has fully initialized on serverA. Otherwise, serverB might start up its own
@@ -95,7 +97,9 @@ public class SessionCacheTwoServerTest extends FATServletClient {
 
         serverB.startServer();
 
-        TimeUnit.SECONDS.sleep(10);
+        // Increase wait time for Windows platforms to allow cluster formation
+        int clusterWaitTime = isWindows() ? 20 : 10;
+        TimeUnit.SECONDS.sleep(clusterWaitTime);
     }
 
     @AfterClass
@@ -130,6 +134,11 @@ public class SessionCacheTwoServerTest extends FATServletClient {
             return true;
         }
         return false;
+    }
+
+    private static final boolean isWindows() {
+        String osName = System.getProperty("os.name");
+        return osName != null && osName.toLowerCase().contains("windows");
     }
 
     /**
