474919a74b3411e65aa0b7a9323b90ce78ded68f
Phu Dinh <pmdinh@us.ibm.com>
2026-02-26 17:20:20 -0500

    Servlet 6.1 FAT for Cookie

---
+++ dev/io.openliberty.webcontainer.servlet.6.1.internal_fat/fat/src/io/openliberty/webcontainer/servlet61/fat/tests/Servlet61RequestCookieHeaderTest.java
@@ -0,0 +1,155 @@
+/*******************************************************************************
+ * Copyright (c) 2026 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package io.openliberty.webcontainer.servlet61.fat.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.logging.Logger;
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.ibm.websphere.simplicity.ShrinkHelper;
+
+import componenttest.annotation.Server;
+import componenttest.custom.junit.runner.FATRunner;
+import componenttest.custom.junit.runner.Mode;
+import componenttest.custom.junit.runner.Mode.TestMode;
+import componenttest.topology.impl.LibertyServer;
+
+/**
+ * Test Request Cookie header according to RFC 6265:
+ * 1. semicolon is the only delimiter; comma is discarded
+ * 2. quotes are part of cookie value
+ *
+ * request URL: /Test61RequestCookieHeader?testName=xyz
+ */
+@RunWith(FATRunner.class)
+@Mode(TestMode.FULL)
+public class Servlet61RequestCookieHeaderTest {
+    private static final Logger LOG = Logger.getLogger(Servlet61RequestCookieHeaderTest.class.getName());
+    private static final String TEST_APP_NAME = "RequestCookieHeaderTest";
+
+    @Server("servlet61_RequestCookieHeaderTest")
+    public static LibertyServer server;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        ShrinkHelper.defaultDropinApp(server, TEST_APP_NAME + ".war", "requestcookieheader.servlets");
+
+        server.startServer(Servlet61RequestCookieHeaderTest.class.getSimpleName() + ".log");
+    }
+
+    @AfterClass
+    public static void testCleanup() throws Exception {
+        LOG.info("testCleanUp : stop server");
+
+        // Stop the server
+        if (server != null && server.isStarted()) {
+            server.stopServer();
+        }
+    }
+
+    /**
+     * Test COOKIE header with All comma delimiter.
+     * Cookie: $Version=1, name1=value1, $Path=/Dollar_Path, $Domain=localhost, $NAME2=DollarNameValue, Domain=DomainValue
+     *
+     * All Cookie are discard since comma is used for all
+     * Main data are in the response's headers
+     */
+    @Test
+    public void test_Cookie_All_Comma_Delimiter() throws Exception {
+        LOG.info(">>>>> test_Cookie_All_Comma_Delimiter <<<<<<");
+
+        String url = "http://" + server.getHostname() + ":" + server.getHttpDefaultPort() + "/" + TEST_APP_NAME + "/Test61RequestCookieHeader?testName=test_Cookie_All_Comma_Delimiter";
+        LOG.info("Sending Request [" + url + "]");
+        HttpGet getMethod = new HttpGet(url);
+        String EXPECTED_TEXT = "Result [PASS]";
+
+        //Cookie is null since all comma parts are discarded
+        getMethod.addHeader("Cookie", "$Version=1, name1=value1, $Path=/Dollar_Path, $Domain=localhost, $NAME2=DollarNameValue, Domain=DomainValue");
+
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            try (CloseableHttpResponse response = client.execute(getMethod)) {
+                String headerValue = response.getHeader("TestResult").getValue();
+                LOG.info(" TestResult : " + headerValue);
+
+                assertTrue("The response does not contain Result [PASS]. TestResult header [" + headerValue + "]", headerValue.contains(EXPECTED_TEXT));
+            }
+        }
+    }
+
+    /**
+     * Test COOKIE header with mix comma and semicolon delimiters.
+     * Original Cookie: $Version=1, name1=value1; middleSemiColonName=middleSemiColonValue, $Path=/Dollar_Path, $Domain=localhost, $NAME2=DollarNameValue, Domain=DomainValue; endSemiColonName=endSemiColonValue
+     * Processed (remove comma) Cookie:  $Version=1; middleSemiColonName=middleSemiColonValue; endSemiColonName=endSemiColonValue
+     *
+     * All Cookie comma pair are discard.
+     */
+    @Test
+    public void test_Cookie_Mix_Comma_Semicolon_Delimiter() throws Exception {
+        LOG.info(">>>>> test_Cookie_Mix_Comma_Semicolon_Delimiter <<<<<<");
+
+        String url = "http://" + server.getHostname() + ":" + server.getHttpDefaultPort() + "/" + TEST_APP_NAME + "/Test61RequestCookieHeader?testName=test_Cookie_Mix_Comma_Semicolon_Delimiter";
+        LOG.info("Sending Request [" + url + "]");
+        HttpGet getMethod = new HttpGet(url);
+        String EXPECTED_TEXT = "Result [PASS]";
+
+        getMethod.addHeader("Cookie", "$Version=1, name1=value1; middleSemiColonName=middleSemiColonValue, $Path=/Dollar_Path, $Domain=localhost, $NAME2=DollarNameValue, Domain=DomainValue; endSemiColonName=endSemiColonValue");
+
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            try (CloseableHttpResponse response = client.execute(getMethod)) {
+                String headerValue = response.getHeader("TestResult").getValue();
+                LOG.info(" TestResult : " + headerValue);
+
+                assertTrue("The response does not contain Result [PASS]. TestResult header [" + headerValue + "]", headerValue.contains(EXPECTED_TEXT));
+            }
+        }
+    }
+
+    /**
+     * Test Request Cookie header with quote
+     * Original Cookie: $Version=1; badDouble\"InName=NO; testSingleQuote'In_Name=YES; test_MixQuotes_InValue_Name=\"Mix_Double'And'Single_Quotes_Name_YES\"; test_SingleQuote_InValue_Name='Single_Quote_Value_YES'; middleSemiColonName=middleSemiColonValue_YES, quotedName2=\"quotedValue2\"; test_DoubleQuote_InValue_Name=\"DoubleInValue_YES\"
+     * Processed (remove comma) Cookie: $Version=1; badDouble"InName=NO; testSingleQuote'In_Name=YES; test_MixQuotes_InValue_Name="Mix_Double'And'Single_Quotes_Name_YES"; test_SingleQuote_InValue_Name='Single_Quote_Value_YES'; middleSemiColonName=middleSemiColonValue_YES; test_DoubleQuote_InValue_Name="DoubleInValue_YES" Key: Cookie Ordinal: 14 undefined: false
+     *
+     * Cookie Name:
+     *  No - Double quote
+     *  Yes - Single quote
+     *
+     * Cookie Value:
+     *  Yes - Double and Single
+     */
+    @Test
+    public void test_Cookie_Quoted_Value() throws Exception {
+        LOG.info(">>>>> test_Cookie_Quoted_Value <<<<<<");
+
+        String url = "http://" + server.getHostname() + ":" + server.getHttpDefaultPort() + "/" + TEST_APP_NAME + "/Test61RequestCookieHeader?testName=test_Cookie_Quoted_Value";
+        LOG.info("Sending Request [" + url + "]");
+        HttpGet getMethod = new HttpGet(url);
+        String EXPECTED_TEXT = "Result [PASS]";
+
+        getMethod.addHeader("Cookie", "$Version=1; badDouble\"InName=NO; test_SingleQuote'In_Name=YES; \"badDouble2_InName\"=NO; test_MixQuotes_InValue_Name=\"Mix_Double'And'Single_Quotes_Name_YES\"; test_SingleQuote_InValue_Name='Single_Quote_Value_YES'; test_NoQuote_Name=NoQuoteValue_YES, quotedName2=\"To_Be_Removed_Pair_NO\"; test_DoubleQuote_InValue_Name=\"DoubleInValue_YES\"");
+
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            try (CloseableHttpResponse response = client.execute(getMethod)) {
+                String headerValue = response.getHeader("TestResult").getValue();
+                LOG.info(" TestResult : " + headerValue);
+
+                assertTrue("The response does not contain Result [PASS]. TestResult header [" + headerValue + "]", headerValue.contains(EXPECTED_TEXT));
+            }
+        }
+    }
+}
---
+++ dev/io.openliberty.webcontainer.servlet.6.1.internal_fat/test-applications/RequestCookieHeaderTest.war/src/requestcookieheader/servlets/TestRequestCookieHeader.java
@@ -0,0 +1,200 @@
+/*******************************************************************************
+ * Copyright (c) 2026 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package requestcookieheader.servlets;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Test Request Cookie header according to RFC 6265:
+ *
+ * 1. Semicolon is the only delimiter. Command is discarded
+ * 2. Quotes are part of the cookie value
+ *
+ * request URL: /Test61RequestCookieHeader
+ */
+@WebServlet("/Test61RequestCookieHeader")
+public class TestRequestCookieHeader extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static final String CLASS_NAME = TestRequestCookieHeader.class.getName();
+    private static final Logger LOG = Logger.getLogger(CLASS_NAME);
+    private static final String PASS_TEXT = "Result [PASS]";
+    private static final String FAIL_TEXT = "Result [FAIL]";
+
+    public TestRequestCookieHeader() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String testName = request.getParameter("testName");
+
+        if (testName == null) {
+            return;
+        }
+        else if (testName.equalsIgnoreCase("test_Cookie_All_Comma_Delimiter")) {
+            test_Cookie_All_Comma_Delimiter(request,  response);
+        }
+        else if (testName.equalsIgnoreCase("test_Cookie_Mix_Comma_Semicolon_Delimiter")) {
+            test_Cookie_Mix_Comma_Semicolon_Delimiter(request, response); 
+        }
+        else if (testName.equalsIgnoreCase("test_Cookie_Quoted_Value")) {
+            test_Cookie_Quoted_Value(request, response); 
+        }
+    }
+
+    /*
+     * Request with header:
+     * Cookie: $Version=1, commaName1=commaValue1, $Path=/Dollar_Path, $Domain=localhost, $NAME2=DollarNameValue, Domain=DomainValue
+     * 
+     * All comma cookie parts are discard; thus cookie is null
+     *
+     */
+    private void test_Cookie_All_Comma_Delimiter(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.info(">>>>> Test test_Cookie_All_Comma_Delimiter");
+        StringBuilder sBuilderResponse = new StringBuilder("====== TEST test_Cookie_All_Comma_Delimiter ======");
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null){
+            LOG.info("No Cookie is found. Test PASS");
+            sBuilderResponse.append("No Cookie is found. " + PASS_TEXT);
+        }
+        else {
+            LOG.info("Cookie is found but not expected. Test FAIL");
+            sBuilderResponse.append("Cookie is found but not expected. " + FAIL_TEXT); 
+        }
+       
+        //Client check this header.
+        response.setHeader("TestResult", sBuilderResponse.toString());   
+    }
+    
+    /*
+     * Request with header:
+     * Original Cookie: [$Version=1, name1=value1; test_NoQuote_Name=NoQuoteValue_YES=middleSemiColonValue, $Path=/Dollar_Path, $Domain=localhost, $NAME2=DollarNameValue, Domain=DomainValue; endSemiColonName=endSemiColonValue]
+     * Processed (removed comma)  [$Version=1; test_NoQuote_Name=NoQuoteValue_YES=middleSemiColonValue; endSemiColonName=endSemiColonValue]
+     *
+     * Version is not a cookie pair; thus not included in the request.getCookies();
+     */
+    private void test_Cookie_Mix_Comma_Semicolon_Delimiter(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.info(">>>>> Test test_Cookie_Mix_Comma_Semicolon_Delimiter");
+        StringBuilder sBuilderResponse = new StringBuilder("====== TEST test_Cookie_Mix_Comma_Semicolon_Delimiter ======");
+
+        ArrayList<String> expectedCookieList = new ArrayList<>(Arrays.asList("middleSemiColonName=middleSemiColonValue","endSemiColonName=endSemiColonValue"));        
+        int cookieCounter = 0;
+        int expectedNumCookies = expectedCookieList.size();
+        String cookiePair = null;
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                cookiePair = cookie.getName() + "=" + cookie.getValue();
+                LOG.info("Cookie pair [" + cookiePair + "]");
+
+                expectedCookieList.remove(cookiePair);
+                cookieCounter++;
+            }
+        } else {
+            LOG.info("No cookies found");
+        }
+        
+        LOG.info("Expected pairs [" + expectedNumCookies+"] Found [" + cookieCounter + "] . Remaining in expected cookie list [" + expectedCookieList.size() + "]" );
+        
+        if (expectedCookieList.size() > 0) {
+            LOG.info("Remaining item :");
+            for (String item : expectedCookieList) {
+                LOG.info(item);
+            }
+        }
+
+        if (cookieCounter != expectedNumCookies || expectedCookieList.size() != 0) {
+            String message = "Cookie pairs NOT match. Expect [" + expectedNumCookies + "] but found [" + cookieCounter + "]. Or Remaining in cooke list is not 0 [" + expectedCookieList.size() + "]";
+            LOG.info(message + ". TEST FAIL");
+            sBuilderResponse.append(message + FAIL_TEXT); 
+        }
+        else {
+            sBuilderResponse.append(PASS_TEXT);
+        }
+        //Client check this header.
+        response.setHeader("TestResult", sBuilderResponse.toString());   
+    }
+    
+    /**
+     * Test Request Cookie header with quote
+     * Original Cookie: $Version=1; badDouble\"InName=NO; test_SingleQuote'In_Name=YES; \"badDouble2_InName\"=NO; test_MixQuotes_InValue_Name=\"Mix_Double'And'Single_Quotes_Name_YES\"; test_SingleQuote_InValue_Name='Single_Quote_Value_YES'; test_NoQuote_Name=NoQuoteValue_YES, quotedName2=\"To_Be_Removed_Pair_NO\"; test_DoubleQuote_InValue_Name=\"DoubleInValue_YES\""
+     * 
+     * Processed (removed comma) Cookie: $Version=1; badDouble"InName=NO; testSingleQuote'In_Name=YES; test_MixQuotes_InValue_Name="Mix_Double'And'Single_Quotes_Name_YES"; test_SingleQuote_InValue_Name='Single_Quote_Value_YES'; test_NoQuote_Name=NoQuoteValue_YES=middleSemiColonValue_YES; test_DoubleQuote_InValue_Name="DoubleInValue_YES" Key: Cookie Ordinal: 14 undefined: false
+     *
+     * Cookie and Set-Cookie Name:
+     *  No - Double quote
+     *  Yes - Single quote
+     *
+     * Cookie Value:
+     *  Yes - Double and Single
+     */
+    private void test_Cookie_Quoted_Value(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.info(">>>>> Test test_Cookie_Quoted_Value");
+        StringBuilder sBuilderResponse = new StringBuilder("====== TEST test_Cookie_Quoted_Value ======");
+
+        ArrayList<String> expectedCookieList = new ArrayList<>();
+        //use add() to make element more readable...easier to add more
+        expectedCookieList.add("test_SingleQuote'In_Name=YES");
+        expectedCookieList.add("test_MixQuotes_InValue_Name=\"Mix_Double'And'Single_Quotes_Name_YES\"");
+        expectedCookieList.add("test_SingleQuote_InValue_Name='Single_Quote_Value_YES'");
+        expectedCookieList.add("test_NoQuote_Name=NoQuoteValue_YES");
+        expectedCookieList.add("test_DoubleQuote_InValue_Name=\"DoubleInValue_YES\"");
+        
+        int cookieCounter = 0;
+        int expectedNumCookies = expectedCookieList.size();
+        String cookiePair = null;
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                cookiePair = cookie.getName() + "=" + cookie.getValue();
+                LOG.info("Cookie pair [" + cookiePair + "]");
+
+                expectedCookieList.remove(cookiePair);
+                cookieCounter++;
+            }
+        } else {
+            LOG.info("No cookies found. TEST FAIL");
+        }
+        
+        LOG.info("Expected pairs [" + expectedNumCookies+"] Found [" + cookieCounter + "] . Remaining in expected cookie list [" + expectedCookieList.size() + "]" );
+        
+        if (expectedCookieList.size() > 0) {
+            LOG.info("Remaining item :");
+            for (String item : expectedCookieList) {
+                LOG.info(item);
+            }
+        }
+
+        if (cookieCounter != expectedNumCookies || expectedCookieList.size() != 0) {
+            String message = "Cookie pairs NOT match. Expect [" + expectedNumCookies + "] but found [" + cookieCounter + "]. Or Remaining in cooke list is not 0 [" + expectedCookieList.size() + "]";
+            LOG.info(message + ". TEST FAIL");
+            sBuilderResponse.append(message + FAIL_TEXT); 
+        }
+        else {
+            sBuilderResponse.append(PASS_TEXT);
+        }
+        //Client check this header.
+        response.setHeader("TestResult", sBuilderResponse.toString());   
+    }
+}
