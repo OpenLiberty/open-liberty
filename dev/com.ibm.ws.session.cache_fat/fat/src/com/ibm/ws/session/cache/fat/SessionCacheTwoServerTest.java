6399c01672ec07aef8bb01b88f8126755256040f
Riya Arora <riyaarora@ibm.com>
2026-02-27 14:35:03 -0500

    Fix brace structure in session.cache_fat test files for RTC 308269
    - Fix SessionCacheTwoServerTest: correct closing braces for testMaxInactiveInterval
      and isWindows() method
    - Fix SessionCacheTwoServerTimeoutTest: move appA.invokeServlet back into
      testCacheInvalidationTwoServer (was incorrectly placed inside isWindows())

--- dev/com.ibm.ws.session.cache_fat/fat/src/com/ibm/ws/session/cache/fat/SessionCacheTwoServerTest.java
+++ dev/com.ibm.ws.session.cache_fat/fat/src/com/ibm/ws/session/cache/fat/SessionCacheTwoServerTest.java
@@ -356,11 +356,11 @@ public class SessionCacheTwoServerTest extends FATServletClient {
                 }
             }
             fail("The session was not invalidated after 5 attempts.  This is likely due to a slow machine.");
+        }
     }
 
     private static boolean isWindows() {
         String osName = System.getProperty("os.name");
         return osName != null && osName.toLowerCase().contains("windows");
-        }
     }
 }
