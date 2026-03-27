6f1a27ef4b0c0b127cc46f4a66014c3118c187c0
Jim Blye <jimblye@us.ibm.com>
2026-03-26 22:27:01 -0400

    Add dynamic update test for quiesceTimeout

--- dev/com.ibm.ws.kernel.boot_fat/fat/src/com/ibm/ws/kernel/boot/ServerStopTest.java
+++ dev/com.ibm.ws.kernel.boot_fat/fat/src/com/ibm/ws/kernel/boot/ServerStopTest.java
@@ -364,6 +364,56 @@ public class ServerStopTest {
         Log.info(c, METHOD_NAME, EXITING);
     }
 
+    /**
+     * Test - Dynamic update of quiesceTimeout value - Beta Mode.
+     * Starts the server with one quiesceTimeout value, dynamically updates it to a different value,
+     * then stops the server and verifies the new value was used.
+     */
+    @Test
+    public void testQuiesceTimeDynamicUpdateBetaMode() throws Exception {
+        final String METHOD_NAME = "testQuiesceTimeDynamicUpdateBetaMode()";
+        Log.info(c, METHOD_NAME, ENTERING);
+        
+        // Enable beta mode
+        server.setJvmOptions(java.util.Arrays.asList("-Dcom.ibm.ws.beta.edition=true"));
+        
+        // Start with initial timeout value of 47 seconds
+        Utils.createFile(serverXmlFilePath, getServerXmlContents("47s"));
+        startServer();
+        
+        // Wait for server to be fully started
+        assertNotNull("Server should have started", server.waitForStringInLog("CWWKF0011I"));
+        
+        // Dynamically update the quiesceTimeout to 60 seconds
+        Log.info(c, METHOD_NAME, "Updating quiesceTimeout from 47s to 60s");
+        server.setMarkToEndOfLog();
+        Utils.createFile(serverXmlFilePath, getServerXmlContents("60s"));
+        
+        // Wait for config update to complete
+        assertNotNull("Config update should complete", server.waitForStringInLog("CWWKG0017I|CWWKG0018I"));
+        
+        // Now stop the server and verify the new timeout value (60 seconds) is used
+        stopServer();
+        
+        // Verify the quiesce message shows 60 seconds (the updated value)
+        RemoteFile consoleLog = server.getConsoleLogFile();
+        List<String> matches = server.findStringsInLogs("CWWKE1100I", consoleLog);
+        
+        String lastMatch = null;
+        for (String s : matches) {
+            Log.info(c, METHOD_NAME, "Found quiesce message: [" + s + "]");
+            lastMatch = s;
+        }
+        
+        assertNotNull("Quiesce message should be found", lastMatch);
+        String actualTimeout = extractTimeValue(lastMatch);
+        Log.info(c, METHOD_NAME, "Extracted timeout value: [" + actualTimeout + "]");
+        assertTrue("Dynamic update should use new timeout value of 60 seconds, but got: " + actualTimeout,
+                   "60".equals(actualTimeout));
+        
+        Log.info(c, METHOD_NAME, EXITING);
+    }
+
     // -----
 
     public boolean runQuiesceTest(String expectedResult) throws Exception {
