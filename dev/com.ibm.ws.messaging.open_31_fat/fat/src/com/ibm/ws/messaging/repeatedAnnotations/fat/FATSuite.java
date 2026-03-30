6fb61a69b0e3a0f23a19c3c49d5283dfb1cdc9e8
Paul Nicolucci <pnicolucci@gmail.com>
2026-03-30 13:45:52 -0400

    Repeat for Jakarta EE11

--- dev/com.ibm.ws.messaging.open_31_fat/fat/src/com/ibm/ws/messaging/repeatedAnnotations/fat/FATSuite.java
+++ dev/com.ibm.ws.messaging.open_31_fat/fat/src/com/ibm/ws/messaging/repeatedAnnotations/fat/FATSuite.java
@@ -1,22 +1,23 @@
 /*******************************************************************************
- * Copyright (c) 2022 IBM Corporation and others.
+ * Copyright (c) 2022, 2026 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
- * SPDX-License-Identifier: EPL-2.0
  *
- * Contributors:
- *     IBM Corporation - initial API and implementation
+ * SPDX-License-Identifier: EPL-2.0
  *******************************************************************************/
 package com.ibm.ws.messaging.repeatedAnnotations.fat;
 
+import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import componenttest.custom.junit.runner.AlwaysPassesTest;
+import componenttest.rules.repeater.EmptyAction;
+import componenttest.rules.repeater.FeatureReplacementAction;
+import componenttest.rules.repeater.RepeatTests;
 
 // AlwaysPassesTest is required because RepeatedAnnotations are supported for JakartaEE10 and above.
 @RunWith(Suite.class)
@@ -24,5 +25,9 @@ import componenttest.custom.junit.runner.AlwaysPassesTest;
                 RepeatedAnnotationsTest.class
 })
 public class FATSuite {
+
+    @ClassRule
+    public static RepeatTests r = RepeatTests.with(FeatureReplacementAction.NO_REPLACEMENT().conditionalFullFATOnly(EmptyAction.GREATER_THAN_OR_EQUAL_JAVA_17))
+                    .andWith(FeatureReplacementAction.EE11_FEATURES().setSkipTransformation(true));
 
 }
