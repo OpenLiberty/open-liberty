f5094c6fbd2ae219b747b6e83424b89af2ba8d88
Paul Nicolucci <pnicolucci@gmail.com>
2026-03-31 16:24:28 -0400

    Repeat for Jakarta EE11

--- dev/com.ibm.ws.messaging.open_jms20AutoCloseable_fat/fat/src/com/ibm/ws/messaging/JMS20AutoCloseable/fat/FATSuite.java
+++ dev/com.ibm.ws.messaging.open_jms20AutoCloseable_fat/fat/src/com/ibm/ws/messaging/JMS20AutoCloseable/fat/FATSuite.java
@@ -1,14 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2020 IBM Corporation and others.
+ * Copyright (c) 2013, 2026 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
  * 
  * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package com.ibm.ws.messaging.JMS20AutoCloseable.fat;
 
@@ -19,6 +16,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import com.ibm.ws.messaging.JMS20AutoCloseable.fat.Autocloseable.AutoCloseableTest;
 
+import componenttest.rules.repeater.FeatureReplacementAction;
 import componenttest.rules.repeater.JakartaEE9Action;
 import componenttest.rules.repeater.RepeatTests;
 
@@ -34,5 +32,7 @@ public class FATSuite {
     // repeat can be re-enabled in open-liberty.
 
     @ClassRule
-    public static RepeatTests repeater = RepeatTests.with(new JakartaEE9Action());
+    public static RepeatTests repeater = RepeatTests.with(FeatureReplacementAction.EE9_FEATURES().conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_11))
+                    .andWith(FeatureReplacementAction.EE10_FEATURES().conditionalFullFATOnly(FeatureReplacementAction.GREATER_THAN_OR_EQUAL_JAVA_17))
+                    .andWith(FeatureReplacementAction.EE11_FEATURES());
 }
