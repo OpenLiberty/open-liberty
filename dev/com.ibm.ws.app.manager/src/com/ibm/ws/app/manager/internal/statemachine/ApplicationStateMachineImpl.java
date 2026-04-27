81b1cf349d7e2cb2984cd5de1338adf2652d540a
Jim Blye <jimblye@us.ibm.com>
2026-04-27 07:36:23 -0400

    Reset update flag when app stops to send correct notification on restart

--- dev/com.ibm.ws.app.manager/src/com/ibm/ws/app/manager/internal/statemachine/ApplicationStateMachineImpl.java
+++ dev/com.ibm.ws.app.manager/src/com/ibm/ws/app/manager/internal/statemachine/ApplicationStateMachineImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2024 IBM Corporation and others.
+ * Copyright (c) 2012, 2026 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -1284,6 +1284,8 @@ class ApplicationStateMachineImpl extends ApplicationStateMachine implements App
                         throw new IllegalStateException("enterState");
                     case STOPPED:
                         _asmHelper.switchApplicationState(_appConfig.get(), ApplicationState.STOPPED);
+                        // Reset the update flag so that subsequent starts are treated as starts, not updates
+                        _update.set(false);
                         flushQueuedActions();
                         ApplicationDependency stoppedFuture;
                         while ((stoppedFuture = _notifyAppStopped.poll()) != null) {
