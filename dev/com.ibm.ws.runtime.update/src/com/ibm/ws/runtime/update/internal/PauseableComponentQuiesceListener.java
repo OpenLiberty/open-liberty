f20b1ea133cf8e5025a98223fc946bbd56148364
Chris Webster <websterc87@hotmail.com>
2026-05-26 07:55:45 +0100

    Revert "Refactor PauseableComponentQuiesceListener.java"

--- dev/com.ibm.ws.runtime.update/src/com/ibm/ws/runtime/update/internal/PauseableComponentQuiesceListener.java
+++ dev/com.ibm.ws.runtime.update/src/com/ibm/ws/runtime/update/internal/PauseableComponentQuiesceListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2026 IBM Corporation and others.
+ * Copyright (c) 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -12,16 +12,13 @@
  *******************************************************************************/
 package com.ibm.ws.runtime.update.internal;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Collection;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
-import org.osgi.service.component.annotations.FieldOption;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
 
 import com.ibm.websphere.ras.Tr;
 import com.ibm.websphere.ras.TraceComponent;
@@ -37,15 +34,7 @@ import com.ibm.wsspi.kernel.service.utils.ServerQuiesceListener;
 public class PauseableComponentQuiesceListener implements ServerQuiesceListener {
 
     private static final TraceComponent tc = Tr.register(PauseableComponentQuiesceListener.class);
-
-    /**
-     * Note:
-     * Refactored to use a synchronized list of pauseable components which causes lower memory overhead
-     */
-    @Reference(cardinality = ReferenceCardinality.MULTIPLE,
-               policy = ReferencePolicy.DYNAMIC,
-               fieldOption = FieldOption.UPDATE)
-    private final List<PauseableComponent> pauseableComponents = Collections.synchronizedList(new ArrayList<>());
+    private BundleContext bundleContext = null;
 
     /*
      * (non-Javadoc)
@@ -54,18 +43,30 @@ public class PauseableComponentQuiesceListener implements ServerQuiesceListener 
      */
     @Override
     public void serverStopping() {
-        pauseableComponents.forEach(pc -> {
-            if (!pc.isPaused()) {
-                try {
-                    pc.pause();
-                } catch (PauseableComponentException ex) {
-                    Tr.warning(tc, "warn.did.not.pause.on.shutdown", ex.getMessage());
-                } catch (Throwable t) {
-                    // auto-ffdc but still call the rest of the pauseable components
-                    Tr.error(tc, "error.unexpected.pause.failure", pc.getClass().getName(), t);
+        if (bundleContext != null) {
+            try {
+                Collection<ServiceReference<PauseableComponent>> refs = bundleContext.getServiceReferences(PauseableComponent.class, null);
+                for (ServiceReference<PauseableComponent> ref : refs) {
+
+                    PauseableComponent pc = bundleContext.getService(ref);
+                    if (pc != null && !pc.isPaused()) {
+                        try {
+                            pc.pause();
+                        } catch (PauseableComponentException ex) {
+                            Tr.warning(tc, "warn.did.not.pause.on.shutdown", ex.getMessage());
+                        }
+                    }
                 }
+            } catch (InvalidSyntaxException e) {
+                // Should never happen, FFDC and return
+                return;
             }
-        });
+        }
+
+    }
+
+    protected void activate(BundleContext ctx) {
+        this.bundleContext = ctx;
     }
 
 }
