ca87e5e75398fb90aa026917e22ce82747a71ef8
ChithraMini <chithra.mini@ibm.com>
2026-06-03 19:46:44 +0530

    committing new multifat test along with secret update

---
+++ dev/com.ibm.ws.jpa.tests.spec10.embeddable_fat/fat/src/com/ibm/ws/jpa/tests/embeddable/jpa20/RepeatWithJPA20.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2023, 2026 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package com.ibm.ws.jpa.tests.embeddable.jpa20;
+
+import com.ibm.ws.testtooling.jpaprovider.JPAPersistenceProvider;
+
+import componenttest.rules.repeater.EE6FeatureReplacementAction;
+
+public class RepeatWithJPA20 extends EE6FeatureReplacementAction {
+    public static final String ID = "JPA20";
+
+    @Override
+    public String toString() {
+        return "JPA 2.0";
+    }
+
+    @Override
+    public void setup() throws Exception {
+        FATSuite.repeatPhase = "jpa20-cfg.xml";
+        FATSuite.provider = JPAPersistenceProvider.DEFAULT;
+    }
+
+    @Override
+    public String getID() {
+        return ID;
+    }
+}
