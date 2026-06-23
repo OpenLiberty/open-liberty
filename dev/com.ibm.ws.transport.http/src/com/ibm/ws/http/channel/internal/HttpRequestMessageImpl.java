d0a2e568b9bd5d3759f70f93e13e713c83f9286f
Manuel Saldana <mansal@us.ibm.com>
2026-06-23 12:59:37 -0400

    Obsfold improvements

--- dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/channel/internal/HttpRequestMessageImpl.java
+++ dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/channel/internal/HttpRequestMessageImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2025 IBM Corporation and others.
+ * Copyright (c) 2004, 2026 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -126,6 +126,7 @@ public class HttpRequestMessageImpl extends HttpBaseMessageImpl implements HttpR
         super();
         // for requests, we don't care about the validation
         setHeaderValidation(false);
+        setRejectHeaderLineFolding(true);
         setOwner(null);
         setBinaryParseState(HttpInternalConstants.PARSING_BINARY_VERSION);
     }
@@ -161,6 +162,7 @@ public class HttpRequestMessageImpl extends HttpBaseMessageImpl implements HttpR
     public void init(HttpInboundServiceContext sc) {
         // for requests, we don't care about the validation
         setHeaderValidation(false);
+        setRejectHeaderLineFolding(true);
         setOwner(sc);
         setBinaryParseState(HttpInternalConstants.PARSING_BINARY_VERSION);
     }
@@ -188,6 +190,7 @@ public class HttpRequestMessageImpl extends HttpBaseMessageImpl implements HttpR
     public void init(HttpInboundServiceContext sc, BNFHeaders hdrs) {
         // for requests, we don't care about the validation
         setHeaderValidation(false);
+        setRejectHeaderLineFolding(true);
         setOwner(sc);
         setBinaryParseState(HttpInternalConstants.PARSING_BINARY_VERSION);
         if (null != hdrs) {
