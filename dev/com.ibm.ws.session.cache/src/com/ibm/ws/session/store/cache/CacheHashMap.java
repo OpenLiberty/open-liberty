ce22c99273579025e7a402e5dcba3957617b3093
Riya Arora <63314740+riyaaroraa@users.noreply.github.com>
2026-06-25 11:41:00 -0400

    Revert "Add FAT tests for cacheNamePrefix feature (Epic #36500) (#34655)"
    This reverts commit 828fb4e6f2f76f517325b6690a16d6d0f27c646a.

--- dev/com.ibm.ws.session.cache/src/com/ibm/ws/session/store/cache/CacheHashMap.java
+++ dev/com.ibm.ws.session.cache/src/com/ibm/ws/session/store/cache/CacheHashMap.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2026 IBM Corporation and others.
+ * Copyright (c) 2018, 2024 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -77,12 +77,6 @@ public class CacheHashMap extends BackedHashMap {
      * Reusable patterns for String.replaceAll
      */
     private static final Pattern COLON = Pattern.compile(":"), PERCENT = Pattern.compile("%"), SLASH = Pattern.compile("/");
-
-    /**
-     * Cache name prefixes for session metadata and attributes
-     */
-    private static final String SESSION_META_CACHE_PREFIX = "com.ibm.ws.session.meta.";
-    private static final String SESSION_ATTR_CACHE_PREFIX = "com.ibm.ws.session.attr.";
 
     /**
      * The end-of-line marker.
@@ -175,9 +169,7 @@ public class CacheHashMap extends BackedHashMap {
 
             // Session Meta Information Cache
 
-            String prefix = cacheStoreService.cacheNamePrefix;
-            String baseMetaPrefix = prefix.isEmpty() ? SESSION_META_CACHE_PREFIX : prefix + SESSION_META_CACHE_PREFIX;
-            String metaCacheName = new StringBuilder(baseMetaPrefix.length() + a.length()).append(baseMetaPrefix).append(a).toString();
+            String metaCacheName = new StringBuilder(24 + a.length()).append("com.ibm.ws.session.meta.").append(a).toString();
 
             if (trace && tc.isDebugEnabled())
                 tcInvoke(cacheStoreService.tcCacheManager, "getCache", metaCacheName, "String", "ArrayList");
@@ -217,8 +209,7 @@ public class CacheHashMap extends BackedHashMap {
 
             // Session Attributes Cache
 
-            String baseAttrPrefix = prefix.isEmpty() ? SESSION_ATTR_CACHE_PREFIX : prefix + SESSION_ATTR_CACHE_PREFIX;
-            String attrCacheName = new StringBuilder(baseAttrPrefix.length() + a.length()).append(baseAttrPrefix).append(a).toString();
+            String attrCacheName = new StringBuilder(24 + a.length()).append("com.ibm.ws.session.attr.").append(a).toString();
 
             if (trace && tc.isDebugEnabled())
                 tcInvoke(cacheStoreService.tcCacheManager, "getCache", attrCacheName, "String", "byte[]");
