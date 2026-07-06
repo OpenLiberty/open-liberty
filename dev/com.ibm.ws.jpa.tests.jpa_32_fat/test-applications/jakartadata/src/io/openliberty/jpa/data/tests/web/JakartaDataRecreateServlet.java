173b0e985bd525a5dbf2fc17778953504466a295
pardhiv-krishna <pardhiv.krishna@ibm.com>
2026-07-06 16:42:45 +0530

    skip tests using annotation

--- dev/com.ibm.ws.jpa.tests.jpa_32_fat/test-applications/jakartadata/src/io/openliberty/jpa/data/tests/web/JakartaDataRecreateServlet.java
+++ dev/com.ibm.ws.jpa.tests.jpa_32_fat/test-applications/jakartadata/src/io/openliberty/jpa/data/tests/web/JakartaDataRecreateServlet.java
@@ -49,6 +49,7 @@ import java.util.stream.Stream;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import componenttest.annotation.SkipForRepeat;
 import componenttest.annotation.SkipIfSysProp;
 import componenttest.app.FATServlet;
 import io.openliberty.jpa.data.tests.models.Account;
@@ -138,37 +139,6 @@ public class JakartaDataRecreateServlet extends FATServlet {
         return repeatPhase != null && repeatPhase.contains("hibernate");
     }
 
-    /**
-     * Skip test if running with Hibernate and log the reason.
-     * Usage: if (skipTestIfHibernate("reason")) return;
-     *
-     * @param reason the reason for skipping (error type or description)
-     * @return true if test should be skipped (running with Hibernate), false otherwise
-     */
-    private boolean skipTestIfHibernate(String reason) {
-        if (isRunningWithHibernate()) {
-            System.out.println("SKIPPING TEST - Running with Hibernate");
-            System.out.println("REASON: " + reason);
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Skip test if running with Hibernate on SQL Server and log the reason.
-     * Usage: if (skipTestIfHibernateOnSQLServer("reason")) return;
-     *
-     * @param reason the reason for skipping (error type or description)
-     * @return true if test should be skipped, false otherwise
-     */
-    private boolean skipTestIfHibernateOnSQLServer(String reason) {
-        if (isRunningWithHibernate() && isSQLServer()) {
-            System.out.println("SKIPPING TEST - Running with Hibernate on SQL Server");
-            System.out.println("REASON: " + reason);
-            return true;
-        }
-        return false;
-    }
 
     @Test
     public void alwaysPasses() {
@@ -421,10 +391,9 @@ public class JakartaDataRecreateServlet extends FATServlet {
     }
 
     @Test //Original issue: https://github.com/OpenLiberty/open-liberty/issues/28920
+    @SkipForRepeat("JPA32_HIBERNATE")
     @Ignore("Additional issue: https://github.com/OpenLiberty/open-liberty/issues/28874")
     public void testOLGH28920() throws Exception {
-        if (skipTestIfHibernate("Hibernate fails when using constructor query"))
-            return;
         Rebate r1 = Rebate.of(10.00, "testOLGH28920", LocalTime.now().minusHours(1), LocalDate.now(), Status.SUBMITTED,
                               LocalDateTime.now(), 1);
         Rebate r2 = Rebate.of(12.00, "testOLGH28920", LocalTime.now().minusHours(1), LocalDate.now(), Status.PAID,
@@ -714,10 +683,9 @@ public class JakartaDataRecreateServlet extends FATServlet {
     }
 
     @Test
+    @SkipForRepeat("JPA32_HIBERNATE")
     // "Reference issue: https://github.com/OpenLiberty/open-liberty/issues/30093"
     public void testOLGH30093() throws Exception {
-        if (skipTestIfHibernate("ClassCastException: PersistentBag incompatible with ArrayList"))
-            return;
         deleteAllEntities(Prime.class); // Cleanup any left over entities
 
         Prime two = Prime.of(2, "II", "two");
@@ -768,10 +736,9 @@ public class JakartaDataRecreateServlet extends FATServlet {
     }
 
     @Test
+    @SkipForRepeat("JPA32_HIBERNATE")
     @SkipIfSysProp(DB_Oracle) //Reference issue: https://github.com/OpenLiberty/open-liberty/issues/28545
     public void testOLGH28545_1() throws Exception {
-        if (skipTestIfHibernate("SQLGrammarException: Syntax error with 'for update with rs'"))
-            return;
         deleteAllEntities(Package.class); // Cleanup any left over entities
 
         Package p1 = Package.of(1, 1.0f, 1.0f, 1.0f, "testOLGH28545-1");
@@ -819,10 +786,9 @@ public class JakartaDataRecreateServlet extends FATServlet {
     }
 
     @Test
+    @SkipForRepeat("JPA32_HIBERNATE")
     @SkipIfSysProp(DB_Oracle) //Reference issue: https://github.com/OpenLiberty/open-liberty/issues/28545
     public void testOLGH28545_2() throws Exception {
-        if (skipTestIfHibernate("SQLGrammarException: Syntax error with 'for update with rs'"))
-            return;
         deleteAllEntities(Package.class); // Cleanup any left over entities
 
         Package p1 = Package.of(1, 1.0f, 1.0f, 1.0f, "testOLGH28545-1");
@@ -872,10 +838,9 @@ public class JakartaDataRecreateServlet extends FATServlet {
     }
 
     @Test
+    @SkipForRepeat("JPA32_HIBERNATE")
     //Reference issue : https://github.com/OpenLiberty/open-liberty/issues/30444
     public void testOLGH30444() throws Exception {
-        if (skipTestIfHibernate("SQLGrammarException: Syntax error with 'for update with rs'"))
-            return;
         deleteAllEntities(Package.class);
 
         Package p1 = Package.of(1, 1.0f, 1.0f, 1.0f, "testOLGH28545-1");
@@ -906,10 +871,9 @@ public class JakartaDataRecreateServlet extends FATServlet {
     }
 
     @Test //Reference issue: https://github.com/OpenLiberty/open-liberty/issues/28545
+    @SkipForRepeat("JPA32_HIBERNATE")
     @SkipIfSysProp({ DB_Postgres, DB_Oracle })
     public void testOLGH28545_3() throws Exception {
-        if (skipTestIfHibernate("ClassCastException: PersistentBag incompatible with ArrayList"))
-            return;
         deleteAllEntities(Prime.class);
 
         Prime two = Prime.of(2, "II", "two");
@@ -970,9 +934,9 @@ public class JakartaDataRecreateServlet extends FATServlet {
     }
 
     @Test
+    @SkipForRepeat("JPA32_HIBERNATE")
     // @Ignore("Reference issue: https://github.com/OpenLiberty/open-liberty/issues/29073")
     public void testOLGH29073() throws Exception {
-        if (skipTestIfHibernateOnSQLServer("Foreign key constraint violation with collection tables")) return;
         
         deleteAllEntities(City.class);
 
@@ -1012,9 +976,9 @@ public class JakartaDataRecreateServlet extends FATServlet {
     }
 
     @Test
+    @SkipForRepeat("JPA32_HIBERNATE")
     //Reference issue: https://github.com/OpenLiberty/open-liberty/issues/29073
     public void testOLGH29073_WHERECLAUSE() throws Exception {
-        if (skipTestIfHibernateOnSQLServer("Foreign key constraint violation with collection tables")) return;
         
         deleteAllEntities(City.class);
 
@@ -1356,10 +1320,9 @@ public class JakartaDataRecreateServlet extends FATServlet {
     }
 
     @Test // Reference issue: https://github.com/OpenLiberty/open-liberty/issues/28737
+    @SkipForRepeat("JPA32_HIBERNATE")
     @SkipIfSysProp({ DB_Postgres, DB_SQLServer })
     public void testOLGH28737() throws Exception {
-        if (skipTestIfHibernate("EntityExistsException: A different object with the same identifier was already associated"))
-            return;
         deleteAllEntities(Box.class);
 
         Box cube = Box.of("testOLGH28737", 1, 1, 1);
@@ -1398,11 +1361,10 @@ public class JakartaDataRecreateServlet extends FATServlet {
     }
 
     @Test //Reference issue: https://github.com/OpenLiberty/open-liberty/issues/28289
+    @SkipForRepeat("JPA32_HIBERNATE")
     @SkipIfSysProp({ DB_Oracle })
     // DB2 resolved (Oracle outstanding): https://github.com/eclipse-ee4j/eclipselink/issues/2282
     public void testOLGH28289() throws Exception {
-        if (skipTestIfHibernate("SQLGrammarException: Syntax error with 'for update with rs'"))
-            return;
         deleteAllEntities(Package.class);
 
         Package p1 = Package.of(70071, 17.0f, 17.1f, 7.7f, "testOLGH28289#70071"); // tallest and smallest length
@@ -1699,6 +1661,7 @@ public class JakartaDataRecreateServlet extends FATServlet {
     }
 
     @Test //Reference issue: https://github.com/OpenLiberty/open-liberty/issues/29440
+    @SkipForRepeat("JPA32_HIBERNATE")
     @SkipIfSysProp({ DB_Postgres, DB_Oracle })
     public void testOLGH29440() throws Exception {
         deleteAllEntities(DemographicInfo.class);
@@ -1892,8 +1855,6 @@ public class JakartaDataRecreateServlet extends FATServlet {
     @Ignore("Reference issue: https://github.com/OpenLiberty/open-liberty/issues/30501")
     //Issue closed. Error is valid and now provides a meaningful message
     public void testOLGH30501() throws Exception {
-        if (skipTestIfHibernate("Hibernate fails when using constructor query"))
-            return;
         deleteAllEntities(Prime.class);
 
         List<RomanNumeral> result;
@@ -1922,8 +1883,6 @@ public class JakartaDataRecreateServlet extends FATServlet {
     //Original issue: https://github.com/OpenLiberty/open-liberty/issues/29475
     @Ignore("Additional issue: https://github.com/OpenLiberty/open-liberty/issues/28589")
     public void testOLGH29475() throws Exception {
-        if (skipTestIfHibernate("Hibernate fails when using constructor query"))
-            return;
         Rating.Reviewer jimmy = Rating.Reviewer.of("Jimothy", "Scramble", "J.Scramble@example.com");
         Rating.Item blueBerry = Rating.Item.of("BlueBerry 10", 299.99f);
         Rating rating = Rating.of(1001, blueBerry, 4, jimmy,
@@ -1978,10 +1937,9 @@ public class JakartaDataRecreateServlet extends FATServlet {
     }
 
     @Test
+    @SkipForRepeat("JPA32_HIBERNATE")
     //Reference issue: https://github.com/OpenLiberty/open-liberty/issues/29475 .This test includes issues in ElementCollection
     public void test_29475_ElementCollection() throws Exception {
-        if (skipTestIfHibernate("ClassCastException: PersistentBag incompatible with ArrayList"))
-            return;
         ECEntity e1 = new ECEntity();
         e1.setId("EC1");
         e1.setIntArray(new int[] { 14, 12, 1 });
@@ -2147,9 +2105,8 @@ public class JakartaDataRecreateServlet extends FATServlet {
     }
 
     @Test //Reference issue: https://github.com/OpenLiberty/open-liberty/issues/29460
+    @SkipForRepeat("JPA32_HIBERNATE")
     public void testOLGH29460() throws Exception {
-        if (skipTestIfHibernate("EntityExistsException: Detached entity passed to persist"))
-            return;
         // Setup test data using the factory method
         Participant p1 = Participant.of("John", "Doe", 1);
         Participant p2 = Participant.of("Jane", "Smith", 2);
@@ -2397,10 +2354,9 @@ public class JakartaDataRecreateServlet extends FATServlet {
     }
 
     @Test
+    @SkipForRepeat("JPA32_HIBERNATE")
     //Reference issue: https://github.com/OpenLiberty/open-liberty/issues/31558
     public void testOLGH31558() throws Exception {
-        if (skipTestIfHibernate("PropertyAccessException: Could not set value of PersistentBag"))
-            return;
         deleteCollectionTable("ShippingAddress_RECIPIENTINFO");
         deleteAllEntities(ShippingAddress.class);
 
@@ -2588,10 +2544,9 @@ public class JakartaDataRecreateServlet extends FATServlet {
     }
 
     @Test
+    @SkipForRepeat("JPA32_HIBERNATE")
     //Reference issue: https://github.com/OpenLiberty/open-liberty/issues/32867
     public void testOLGH32867() throws Exception {
-        if (skipTestIfHibernate("Hibernate fails when using constructor query"))
-            return;
         deleteAllEntities(Showtime.class);
 
         Showtime t1 = Showtime.of("Spiderman", LocalDateTime.now(),
