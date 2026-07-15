cec8be94d7b072e45bfe3a233f3365e19572b4a5
Adam Anderson <atanderson9383@gmail.com>
2026-07-15 14:55:28 -0500

    add copyright date

--- dev/com.ibm.json4j/src/com/ibm/json/java/internal/Parser.java
+++ dev/com.ibm.json4j/src/com/ibm/json/java/internal/Parser.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006, 2026 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
@@ -19,21 +19,20 @@ import java.io.Reader;
 import com.ibm.json.java.JSONArray;
 import com.ibm.json.java.JSONObject;
 import com.ibm.json.java.OrderedJSONObject;
-import com.ibm.json.java.internal.ParserConfig;
 import com.ibm.json.java.internal.ParserConfig.DuplicateKeyBehavior;
 
 /**
  * Private parser class which handles doing the parsing of the JSON string into tokens.
  */
-public class Parser
-{
+public class Parser {
 
-    private Tokenizer tokenizer;
-    private Token     lastToken;
-    private long      totalBytesProcessed;
+    private final Tokenizer tokenizer;
+    private Token lastToken;
+    private long totalBytesProcessed;
 
     /**
      * Contructor
+     *
      * @param reader The Reader to use when reading in the JSON stream/string.
      *
      * @throws IOException Thrown if an error occurs in tokenizing the JSON string.
@@ -54,22 +53,22 @@ public class Parser
      */
     private void checkTotalSize(long additionalSize) throws IOException {
         long maxTotalSize = ParserConfig.getMaxTotalSize();
-        
+
         // Check if adding would exceed limit (with overflow protection)
         if (additionalSize > maxTotalSize || totalBytesProcessed > maxTotalSize - additionalSize) {
             throw new IOException("Total parsed content size exceeds maximum allowed size of " +
-                maxTotalSize + " bytes at line " + tokenizer.getLine() +
-                ", column " + tokenizer.getCol() +
-                ". Configure with system property: com.ibm.json4j.max.total.size");
+                                  maxTotalSize + " bytes at line " + tokenizer.getLine() +
+                                  ", column " + tokenizer.getCol() +
+                                  ". Configure with system property: com.ibm.json4j.max.total.size");
         }
-        
+
         totalBytesProcessed += additionalSize;
     }
 
     /**
      * Method to initiate the parse of the toplevel JSON object, which will in turn parse all child JSON objects contained within.
      * Same as calling parse(false);
-     * 
+     *
      * @throws IOException Thrown if an IO error occurd during parse of the JSON object(s).
      */
     public JSONObject parse() throws IOException {
@@ -78,8 +77,9 @@ public class Parser
 
     /**
      * Method to initiate the parse of the toplevel JSON object, which will in turn parse all child JSON objects contained within.
+     *
      * @param ordered Flag to denote if the parse should contruct a JSON object which maintains serialization order of the attributes.
-     * 
+     *
      * @throws IOException Thrown if an IO error occurd during parse of the JSON object(s).
      */
     public JSONObject parse(boolean ordered) throws IOException {
@@ -89,6 +89,7 @@ public class Parser
 
     /**
      * Method to parse a JSON object out of the current JSON string position.
+     *
      * @return JSONObject Returns the parsed out JSON object.
      *
      * @throws IOException Thrown if an IO error occurs during parse, such as a malformed JSON object.
@@ -99,6 +100,7 @@ public class Parser
 
     /**
      * Method to parse a JSON object out of the current JSON string position.
+     *
      * @param ordered Flag to denote if the parse should contruct a JSON object which maintains serialization order of the attributes.
      * @return JSONObject Returns the parsed out JSON object.
      *
@@ -110,8 +112,9 @@ public class Parser
 
     /**
      * Method to parse a JSON object out of the current JSON string position with depth tracking.
+     *
      * @param ordered Flag to denote if the parse should contruct a JSON object which maintains serialization order of the attributes.
-     * @param depth Current nesting depth
+     * @param depth   Current nesting depth
      * @return JSONObject Returns the parsed out JSON object.
      *
      * @throws IOException Thrown if an IO error occurs during parse, such as a malformed JSON object.
@@ -119,12 +122,9 @@ public class Parser
     private JSONObject parseObject(boolean ordered, int depth) throws IOException {
         JSONObject result = null;
 
-        if (!ordered)
-        {
+        if (!ordered) {
             result = new JSONObject();
-        }
-        else
-        {
+        } else {
             result = new OrderedJSONObject();
         }
 
@@ -135,15 +135,15 @@ public class Parser
         int currentMembers = 0;
         DuplicateKeyBehavior dupBehavior = ParserConfig.getDuplicateKeyBehavior();
 
-        if (lastToken != Token.TokenBraceL) throw new IOException("Expecting '{' " + tokenizer.onLineCol() + " instead, obtained token: '" + lastToken + "'");
+        if (lastToken != Token.TokenBraceL)
+            throw new IOException("Expecting '{' " + tokenizer.onLineCol() + " instead, obtained token: '" + lastToken + "'");
         lastToken = tokenizer.next();
 
-        while (true)
-        {
-            if (lastToken == Token.TokenEOF) throw new IOException("Unterminated object " + tokenizer.onLineCol());
+        while (true) {
+            if (lastToken == Token.TokenEOF)
+                throw new IOException("Unterminated object " + tokenizer.onLineCol());
 
-            if (lastToken == Token.TokenBraceR)
-            {
+            if (lastToken == Token.TokenBraceR) {
                 lastToken = tokenizer.next();
                 break;
             }
@@ -151,25 +151,26 @@ public class Parser
             // Check member count limit BEFORE adding
             if (currentMembers >= maxMembers) {
                 throw new IOException("Object member count exceeds maximum allowed count of " +
-                    maxMembers + " members at line " + tokenizer.getLine() +
-                    ", column " + tokenizer.getCol() +
-                    ". Configure with system property: com.ibm.json4j.max.object.members");
+                                      maxMembers + " members at line " + tokenizer.getLine() +
+                                      ", column " + tokenizer.getCol() +
+                                      ". Configure with system property: com.ibm.json4j.max.object.members");
             }
 
-            if (!lastToken.isString()) throw new IOException("Expecting string key " + tokenizer.onLineCol());
+            if (!lastToken.isString())
+                throw new IOException("Expecting string key " + tokenizer.onLineCol());
             String key = lastToken.getString();
-            
+
             // Track key size (string overhead + chars + map entry overhead)
             checkTotalSize(key.length() * 2L + 24); // 24 bytes for string + map entry overhead
 
             // Check for duplicate keys
             if (result.containsKey(key)) {
                 String dupMsg = "Duplicate key '" + key + "' found at line " +
-                    tokenizer.getLine() + ", column " + tokenizer.getCol();
-                
+                                tokenizer.getLine() + ", column " + tokenizer.getCol();
+
                 if (dupBehavior == DuplicateKeyBehavior.ERROR) {
                     throw new IOException(dupMsg +
-                        ". Configure with system property: com.ibm.json4j.duplicate.key.behavior");
+                                          ". Configure with system property: com.ibm.json4j.duplicate.key.behavior");
                 } else if (dupBehavior == DuplicateKeyBehavior.WARN) {
                     System.err.println("WARNING: " + dupMsg + " (overwriting previous value)");
                 }
@@ -177,7 +178,8 @@ public class Parser
             }
 
             lastToken = tokenizer.next();
-            if (lastToken != Token.TokenColon) throw new IOException("Expecting colon " + tokenizer.onLineCol());
+            if (lastToken != Token.TokenColon)
+                throw new IOException("Expecting colon " + tokenizer.onLineCol());
 
             lastToken = tokenizer.next();
             Object val = parseValue(ordered, depth);
@@ -185,13 +187,11 @@ public class Parser
             result.put(key, val);
             currentMembers++;
 
-            if (lastToken == Token.TokenComma)
-            {
+            if (lastToken == Token.TokenComma) {
                 lastToken = tokenizer.next();
             }
 
-            else if (lastToken != Token.TokenBraceR)
-            {
+            else if (lastToken != Token.TokenBraceR) {
                 throw new IOException("expecting either ',' or '}' " + tokenizer.onLineCol());
             }
         }
@@ -202,14 +202,16 @@ public class Parser
     /**
      * Method to parse out a JSON array from a JSON string
      * Same as calling parseArray(false)
-     * 
+     *
      * @throws IOException Thrown if a parse error occurs, such as a malformed JSON array.
      */
     public JSONArray parseArray() throws IOException {
         return parseArray(false);
     }
+
     /**
      * Method to parse out a JSON array from a JSON string
+     *
      * @param ordered Flag to denote if the parse should contruct JSON objects which maintain serialization order of the attributes for all JSONOjects in the array.
      *
      * @throws IOException Thrown if a parse error occurs, such as a malformed JSON array.
@@ -220,32 +222,33 @@ public class Parser
 
     /**
      * Method to parse out a JSON array from a JSON string with depth tracking.
+     *
      * @param ordered Flag to denote if the parse should contruct JSON objects which maintain serialization order of the attributes for all JSONOjects in the array.
-     * @param depth Current nesting depth
+     * @param depth   Current nesting depth
      *
      * @throws IOException Thrown if a parse error occurs, such as a malformed JSON array.
      */
     private JSONArray parseArray(boolean ordered, int depth) throws IOException {
         JSONArray result = new JSONArray();
-        
+
         // Track array overhead (ArrayList base size)
         checkTotalSize(40);
-        
+
         int maxSize = ParserConfig.getMaxArraySize();
         int currentSize = 0;
 
-        if (lastToken != Token.TokenBrackL) throw new IOException("Expecting '[' " + tokenizer.onLineCol());
+        if (lastToken != Token.TokenBrackL)
+            throw new IOException("Expecting '[' " + tokenizer.onLineCol());
         lastToken = tokenizer.next();
 
-        while (true)
-        {
-            if (lastToken == Token.TokenEOF) throw new IOException("Unterminated array " + tokenizer.onLineCol());
+        while (true) {
+            if (lastToken == Token.TokenEOF)
+                throw new IOException("Unterminated array " + tokenizer.onLineCol());
 
             /**
              * End of the array.
              */
-            if (lastToken == Token.TokenBrackR)
-            {
+            if (lastToken == Token.TokenBrackR) {
                 lastToken = tokenizer.next();
                 break;
             }
@@ -253,24 +256,21 @@ public class Parser
             // Check size limit BEFORE adding element
             if (currentSize >= maxSize) {
                 throw new IOException("Array size exceeds maximum allowed size of " +
-                    maxSize + " elements at line " + tokenizer.getLine() +
-                    ", column " + tokenizer.getCol() +
-                    ". Configure with system property: com.ibm.json4j.max.array.size");
+                                      maxSize + " elements at line " + tokenizer.getLine() +
+                                      ", column " + tokenizer.getCol() +
+                                      ". Configure with system property: com.ibm.json4j.max.array.size");
             }
 
             Object val = parseValue(ordered, depth);
             result.add(val);
             currentSize++;
-            
+
             // Track array element reference overhead (8 bytes per reference)
             checkTotalSize(8);
 
-            if (lastToken == Token.TokenComma)
-            {
+            if (lastToken == Token.TokenComma) {
                 lastToken = tokenizer.next();
-            }
-            else if (lastToken != Token.TokenBrackR)
-            {
+            } else if (lastToken != Token.TokenBrackR) {
                 throw new IOException("expecting either ',' or ']' " + tokenizer.onLineCol());
             }
         }
@@ -279,7 +279,8 @@ public class Parser
     }
 
     /**
-     * Method to parse the current JSON property value from the last token. 
+     * Method to parse the current JSON property value from the last token.
+     *
      * @return The java object type that represents the JSON value.
      *
      * @throws IOException Thrown if an IO error (read incomplete token) occurs.
@@ -290,6 +291,7 @@ public class Parser
 
     /**
      * Method to parse the current JSON property value from the last token.
+     *
      * @return The java object type that represents the JSON value.
      * @param ordered Flag to denote if the parse should contruct JSON objects and arrays which maintain serialization order of the attributes.
      *
@@ -301,27 +303,28 @@ public class Parser
 
     /**
      * Method to parse the current JSON property value from the last token with depth tracking.
+     *
      * @return The java object type that represents the JSON value.
      * @param ordered Flag to denote if the parse should contruct JSON objects and arrays which maintain serialization order of the attributes.
-     * @param depth Current nesting depth
+     * @param depth   Current nesting depth
      *
      * @throws IOException Thrown if an IO error (read incomplete token) occurs.
      */
     private Object parseValue(boolean ordered, int depth) throws IOException {
         int maxDepth = ParserConfig.getMaxNestingDepth();
-        
+
         // Check depth limit BEFORE recursing
         if (depth >= maxDepth) {
             throw new IOException("JSON nesting depth exceeds maximum allowed depth of " +
-                maxDepth + " levels at line " + tokenizer.getLine() +
-                ", column " + tokenizer.getCol() +
-                ". Configure with system property: com.ibm.json4j.max.nesting.depth");
+                                  maxDepth + " levels at line " + tokenizer.getLine() +
+                                  ", column " + tokenizer.getCol() +
+                                  ". Configure with system property: com.ibm.json4j.max.nesting.depth");
         }
-        
-        if (lastToken == Token.TokenEOF) throw new IOException("Expecting property value " + tokenizer.onLineCol());
 
-        if (lastToken.isNumber())
-        {
+        if (lastToken == Token.TokenEOF)
+            throw new IOException("Expecting property value " + tokenizer.onLineCol());
+
+        if (lastToken.isNumber()) {
             Object result = lastToken.getNumber();
             // Track number size - estimate based on string representation
             // Numbers are stored as Long or Double, so we estimate memory impact
@@ -334,8 +337,7 @@ public class Parser
             return result;
         }
 
-        if (lastToken.isString())
-        {
+        if (lastToken.isString()) {
             String result = lastToken.getString();
             // Track string size - 2 bytes per char in Java (UTF-16) plus object overhead
             checkTotalSize(result.length() * 2L + 24); // 24 bytes for String object overhead
@@ -343,26 +345,25 @@ public class Parser
             return result;
         }
 
-        if (lastToken == Token.TokenFalse)
-        {
+        if (lastToken == Token.TokenFalse) {
             lastToken = tokenizer.next();
             return Boolean.FALSE;
         }
 
-        if (lastToken == Token.TokenTrue)
-        {
+        if (lastToken == Token.TokenTrue) {
             lastToken = tokenizer.next();
             return Boolean.TRUE;
         }
 
-        if (lastToken == Token.TokenNull)
-        {
+        if (lastToken == Token.TokenNull) {
             lastToken = tokenizer.next();
             return null;
         }
 
-        if (lastToken == Token.TokenBrackL) return parseArray(ordered, depth + 1);
-        if (lastToken == Token.TokenBraceL) return parseObject(ordered, depth + 1);
+        if (lastToken == Token.TokenBrackL)
+            return parseArray(ordered, depth + 1);
+        if (lastToken == Token.TokenBraceL)
+            return parseObject(ordered, depth + 1);
 
         throw new IOException("Invalid token " + tokenizer.onLineCol());
     }
