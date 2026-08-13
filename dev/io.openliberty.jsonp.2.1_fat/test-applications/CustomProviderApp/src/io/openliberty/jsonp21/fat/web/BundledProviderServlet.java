3aa58080bad2cd8fb54fac23540e6d1635d69212
Mark Swatosh <mark.swatosh@gmail.com>
2026-08-13 14:31:52 -0500

    Adding BundledProvider collision test for Json-P
    Co-authored-by-AI: IBM Bob 2.0.2

---
+++ dev/io.openliberty.jsonp.2.1_fat/test-applications/CustomProviderApp/src/io/openliberty/jsonp21/fat/web/BundledProviderServlet.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2026 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.jsonp21.fat.web;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import componenttest.app.FATServlet;
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.spi.JsonProvider;
+import jakarta.servlet.annotation.WebServlet;
+
+/**
+ * Deployed to ProviderAppBundledApp (Johnzon bundled in WEB-INF/lib). Asserts that
+ * Johnzon overrides the feature provider for this WAR's classloader only.
+ */
+@WebServlet("/BundledProviderServlet")
+@SuppressWarnings("serial")
+public class BundledProviderServlet extends FATServlet {
+
+    @Test
+    public void testBundledProvider() {
+        String providerName = JsonProvider.provider().getClass().getName();
+        assertEquals("org.apache.johnzon.core.JsonProviderImpl", providerName);
+
+        JsonObject obj = Json.createObjectBuilder().add("key", "value").build();
+        assertEquals("value", obj.getString("key"));
+    }
+}
---
+++ dev/io.openliberty.jsonp.2.1_fat/test-applications/CustomProviderApp/src/io/openliberty/jsonp21/fat/web/FeatureProviderServlet.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2026 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package io.openliberty.jsonp21.fat.web;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import componenttest.app.FATServlet;
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.spi.JsonProvider;
+import jakarta.servlet.annotation.WebServlet;
+
+/**
+ * Deployed to ProviderFeatureApp (no bundled provider). Asserts that the Liberty
+ * jsonp-2.1 feature's built-in Parsson provider is active for this WAR's classloader.
+ */
+@WebServlet("/FeatureProviderServlet")
+@SuppressWarnings("serial")
+public class FeatureProviderServlet extends FATServlet {
+
+    @Test
+    public void testFeatureProvider() {
+        String providerName = JsonProvider.provider().getClass().getName();
+        assertEquals("org.eclipse.parsson.JsonProviderImpl", providerName);
+
+        JsonObject obj = Json.createObjectBuilder().add("key", "value").build();
+        assertEquals("value", obj.getString("key"));
+    }
+}
