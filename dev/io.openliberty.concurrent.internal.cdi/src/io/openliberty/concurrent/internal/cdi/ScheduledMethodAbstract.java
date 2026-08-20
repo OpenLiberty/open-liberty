884ff632fdf2b5653cb4ee65dc4bdd8183b8d730
Nathan Rauh <nathan.rauh@us.ibm.com>
2026-08-20 11:05:37 -0500

    Issue #35587 fix race condition in scheduled task init

--- dev/io.openliberty.concurrent.internal.cdi/src/io/openliberty/concurrent/internal/cdi/ScheduledMethodAbstract.java
+++ dev/io.openliberty.concurrent.internal.cdi/src/io/openliberty/concurrent/internal/cdi/ScheduledMethodAbstract.java
@@ -85,9 +85,6 @@ public abstract class ScheduledMethodAbstract implements //
         this.virtualThreadExecutor = managedExecutor //
                         .getNormalPolicyExecutor() //
                         .getVirtualThreadExecutor();
-
-        ConcurrencyExtensionMetadata.scheduledExecutor //
-                        .schedule(this, computeDelayNanos(), TimeUnit.NANOSECONDS);
     }
 
     /**
@@ -210,7 +207,7 @@ public abstract class ScheduledMethodAbstract implements //
      * @return nanoseconds until the next execution.
      */
     @Trivial
-    long computeDelayNanos() {
+    public long computeDelayNanos() {
         final boolean trace = TraceComponent.isAnyTracingEnabled();
         if (trace && tc.isEntryEnabled())
             Tr.entry(this, tc, "computeDelayNanos");
