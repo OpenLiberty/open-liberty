b50a3341fed3fa1891f1dea4c6fc0f5d8b02f31d
Benjamin Confino <benjamic@uk.ibm.com>
2026-09-14 23:09:14 +0100

    Split fat bucket into three to avoid build breaks from resource saturation
    Co-authored-by-AI: IBM Bob 2.1.0

---
+++ dev/com.ibm.ws.microprofile.config.1.1_fat_stress/fat/src/com/ibm/ws/microprofile/config/fat/suite/FATSuite.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2025 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.microprofile.config.fat.suite;
+
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.ibm.ws.fat.util.FatLogHandler;
+import com.ibm.ws.microprofile.config.fat.tests.StressTest;
+
+import componenttest.custom.junit.runner.AlwaysPassesTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+                AlwaysPassesTest.class,
+                StressTest.class //FULL
+})
+
+public class FATSuite {
+
+    /**
+     * @see {@link FatLogHandler#generateHelpFile()}
+     */
+    @BeforeClass
+    public static void generateHelpFile() {
+        FatLogHandler.generateHelpFile();
+    }
+
+}
---
+++ dev/com.ibm.ws.microprofile.config.1.1_fat_stress/test-applications/testAppUtils.jar/src/com/ibm/ws/microprofile/appConfig/test/utils/TestUtils.java
@@ -0,0 +1,98 @@
+/*******************************************************************************
+ * Copyright (c) 2016, 2025 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.microprofile.appConfig.test.utils;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import org.eclipse.microprofile.config.Config;
+
+/**
+ *
+ */
+public class TestUtils {
+
+    public static void assertContains(Config config, Map<String, String> props) {
+        for (Map.Entry<String, String> entry : props.entrySet()) {
+            assertContains(config, entry.getKey(), entry.getValue());
+        }
+    }
+
+    public static <T> void assertContains(Config config, String key, T expected) {
+        //a bit of a naughty cast but I know it's a set and that's easier to work on
+        Set<String> keys = (Set<String>) config.getPropertyNames();
+        if (!keys.contains(key)) {
+            throw new AssertionError("Key '" + key + "' was not found");
+        }
+        @SuppressWarnings("unchecked")
+        Optional<T> opt = config.getOptionalValue(key, (Class<T>) expected.getClass());
+        assertContains(opt, key, expected);
+    }
+
+    public static <T> void assertContains(Optional<T> optional, String key, T expected) {
+        T actual = optional.orElse(null);
+        if (actual == null) {
+            throw new AssertionError("Value for key '" + key + "' was null");
+        }
+        if (!expected.equals(actual)) {
+            throw new AssertionError("Value for key '" + key + "' was '" + actual + "'. Expected: '" + expected + "'");
+        }
+    }
+
+    public static void assertNotContains(Config config, String key) {
+        assertNotContains(config, key, String.class);
+    }
+
+    public static <T> void assertNotContains(Config config, String key, Class<T> clazz) {
+        //a bit of a naughty cast but I know it's a set and that's easier to work on
+        Set<String> keys = (Set<String>) config.getPropertyNames();
+        if (keys.contains(key)) {
+            T opt = config.getValue(key, clazz);
+            throw new AssertionError("Key '" + key + "' was found. Value was '" + opt + "'");
+        }
+    }
+
+    public static <T> void assertNotContains(Optional<T> optional, String key) {
+        if (optional.isPresent()) {
+            T value = optional.get();
+            throw new AssertionError("Key '" + key + "' was found. Value was '" + value + "'");
+        }
+    }
+
+    public static void assertEquals(Object expected, Object actual) {
+        if (expected != null && !expected.equals(actual)) {
+            throw new AssertionError("Expected: " + expected + ". Actual: " + actual);
+        }
+        if (expected == null && actual != null) {
+            throw new AssertionError("Expected: " + expected + ". Actual: " + actual);
+        }
+    }
+
+    public static void fail(Throwable t) {
+        t.printStackTrace();
+        StringBuilder builder = new StringBuilder("FAILED: ");
+        builder.append(t);
+        builder.append(" at ");
+        StackTraceElement[] stack = t.getStackTrace();
+        for (int i = 0; i < 5 && i < stack.length; i++) {
+            builder.append(stack[i]);
+            builder.append("\n");
+        }
+        throw new AssertionError(builder.toString());
+    }
+
+    public static void fail(String message) {
+        throw new AssertionError("FAILED: " + message);
+    }
+}
